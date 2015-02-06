82293aefd9164e197d24b0bd6974a1515b402acd
Rick Mak <rick.mak@gmail.com>
2015-02-06 19:09:58 +0800

    Add Login test case with hard code Handler to start with

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -16,4 +18,44 @@ func main() {
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello Developer"))
+}
+
+type RequestJson struct {
+	Action   interface{} `json:"action"`
+	ApiKey   interface{} `json:"api_key"`
+	Email    interface{} `json:"email"`
+	Password interface{} `json:"password"`
+}
+
+type ResponseJson struct {
+	UserId      interface{} `json:"user_id"`
+	AccessToken interface{} `json:"access_token,omitempty"`
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		httpStatus = http.StatusOK
+		reqJson    RequestJson
+		respJson   ResponseJson
+		errString  string
+	)
+	defer func() {
+		if httpStatus != http.StatusOK {
+			w.Write([]byte(errString))
+		}
+	}()
+	body, _ := ioutil.ReadAll(r.Body)
+	if err := json.Unmarshal(body, &reqJson); err != nil {
+		httpStatus = http.StatusBadRequest
+		errString = err.Error()
+		return
+	}
+
+	respJson.UserId = "rickmak-oursky"
+	b, err := json.Marshal(respJson)
+	if err != nil {
+		httpStatus = http.StatusBadRequest
+		errString = err.Error()
+	}
+	w.Write(b)
 }
--- main_test.go
+++ main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -21,5 +23,31 @@ func TestHomeHandler(t *testing.T) {
 
 	if resp.Body.String() != "Hello Developer" {
 		t.Errorf("got body %v, want `Hello Developer`", resp.Body)
+	}
+}
+
+func TestAuthHandler(t *testing.T) {
+	var authJson = `{
+	"action": "auth:login",
+	"api_key": "oursky",
+	"email": "rickmak@gmail.com",
+	"password": "validpassword"
+}`
+
+	req, _ := http.NewRequest(
+		"POST",
+		"http://ourd.dev/api/v1",
+		strings.NewReader(authJson),
+	)
+	req.Header.Set("Content-Type", "application/json")
+	resp := httptest.NewRecorder()
+	LoginHandler(resp, req)
+
+	var result ResponseJson
+	if err := json.Unmarshal(resp.Body.Bytes(), &result); err != nil {
+		t.Errorf("Repsonse is not a valid json: %v", err.Error())
+	}
+	if result.UserId != "rickmak-oursky" {
+		t.Errorf("UserId mismatch, expecting `rickmak-oursky`, got %v", result.UserId)
 	}
 }
