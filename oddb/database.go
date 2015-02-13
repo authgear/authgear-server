ed9b4cfbfcd651a08713d30fcd556ac4ec4699b6
Kenji Pa <limouren@gmail.com>
2015-02-13 15:36:16 +0800

    Add a Query method to Database interface

--- oddb/database.go
+++ oddb/database.go
@@ -5,7 +5,7 @@ import (
 )
 
 // A RecordNotFoundError is an implementation of error which represents
-// a record is not found in a Database
+// that a record is not found in a Database.
 type RecordNotFoundError struct {
 	Key  string
 	Conn Conn
@@ -15,11 +15,25 @@ func (e *RecordNotFoundError) Error() string {
 	return fmt.Sprintf("Record of %v not found in Database", e.Key)
 }
 
-// A Database represents a collection of record (either public or private)
-// in a container
+// Database represents a collection of record (either public or private)
+// in a container.
 type Database interface {
 	ID() string
 	Get(key string, record *Record) error
 	Save(record *Record) error
 	Delete(key string) error
+
+	Query(query string, args ...interface{}) (Rows, error)
+}
+
+// Rows is a cursor returned by execution of a query.
+type Rows interface {
+	// Close closes the rows iterator
+	Close() error
+
+	// Next populates the next Record in the current rows iterator into
+	// the provided record.
+	//
+	// Next should return io.EOF when there are no more rows
+	Next(record *Record) error
 }
