b9da4ffe82dfd66782980ca3ab043e9fd9e3d24f
Kenji Pa <limouren@gmail.com>
2015-02-27 15:17:37 +0800

    Add Token and TokenStore with a fs implementation, #4

---
+++ auth/auth.go
@@ -0,0 +1,105 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/twinj/uuid"
+)
+
+// Token is an expiry access token associated to a UserInfo.
+type Token struct {
+	AccessToken string    `json:"accessToken"`
+	ExpiredAt   time.Time `json:"expiredAt"`
+	UserInfoID  string    `json:"userInfoID"`
+}
+
+// NewToken creates a new Token ready for use given a userInfoID and
+// expiredAt date. If expiredAt is passed an empty Time, it
+// will be set to 30 days from now.
+func NewToken(userInfoID string, expiredAt time.Time) Token {
+	if expiredAt.IsZero() {
+		expiredAt = time.Now().Add(24 * 30 * time.Hour)
+	}
+
+	return Token{
+		// NOTE(limouren): I am not sure if it is good to use UUID
+		// as access token.
+		AccessToken: uuid.NewV4().String(),
+		ExpiredAt:   expiredAt,
+		UserInfoID:  userInfoID,
+	}
+}
+
+// IsExpired determines whether the Token has expired now or not.
+func (t *Token) IsExpired() bool {
+	return t.ExpiredAt.Before(time.Now())
+}
+
+// TokenNotFoundError is the error returned by Get if a TokenStore
+// cannot find the requested token or the fetched token is expired.
+type TokenNotFoundError struct {
+	AccessToken string
+	Err         error
+}
+
+func (e *TokenNotFoundError) Error() string {
+	return fmt.Sprintf("get %v: %v", e.AccessToken, e.Err)
+}
+
+// TokenStore represents a persistent storage for Token.
+type TokenStore interface {
+	Get(accessToken string, token *Token) error
+	Put(token *Token) error
+}
+
+// FileStore implements TokenStore by saving users' Token under
+// a directory specified by a string. Each access token is
+// stored in a separate file.
+type FileStore string
+
+// Get tries to read the specified access token from file and
+// writes to the supplied Token.
+//
+// Get returns an TokenNotFoundError if no such access token exists or
+// such access token is expired. In the latter case the expired
+// access token is still written onto the supplied Token.
+func (f FileStore) Get(accessToken string, token *Token) error {
+	tokenPath := filepath.Join(string(f), accessToken)
+
+	file, err := os.Open(tokenPath)
+	if err != nil {
+		return &TokenNotFoundError{accessToken, err}
+	}
+	defer file.Close()
+
+	if err := json.NewDecoder(file).Decode(token); err != nil {
+		return &TokenNotFoundError{accessToken, err}
+	}
+
+	if token.IsExpired() {
+		os.Remove(tokenPath)
+		return &TokenNotFoundError{accessToken, fmt.Errorf("token expired at %v", token.ExpiredAt)}
+	}
+
+	return nil
+}
+
+// Put writes the specified token into a file and overwrites existing
+// Token if any.
+func (f FileStore) Put(token *Token) error {
+	file, err := os.Create(filepath.Join(string(f), token.AccessToken))
+	if err != nil {
+		return &TokenNotFoundError{token.AccessToken, err}
+	}
+	defer file.Close()
+
+	if err := json.NewEncoder(file).Encode(token); err != nil {
+		return &TokenNotFoundError{token.AccessToken, err}
+	}
+
+	return nil
+}
---
+++ auth/auth_test.go
@@ -0,0 +1,99 @@
+package auth
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func tempDir() string {
+	dir, err := ioutil.TempDir("", "oddb.auth.test")
+	if err != nil {
+		panic(err)
+	}
+	return dir
+}
+
+func TestNewToken(t *testing.T) {
+	token := NewToken("46709394", time.Time{})
+
+	if token.UserInfoID != "46709394" {
+		t.Fatalf("got token.UserInfoID = %v, want 46709394", token)
+	}
+
+	if token.AccessToken == "" {
+		t.Fatalf("got token = %v, want non-empty AccessToken value", token)
+	}
+
+	if token.ExpiredAt.IsZero() {
+		t.Fatalf("got token = %v, want non-zero ExpiredAt value", token)
+	}
+}
+
+func TestNewTokenWithExpiry(t *testing.T) {
+	expiredAt := time.Unix(0, 1)
+
+	token := NewToken("46709394", expiredAt)
+
+	if !token.ExpiredAt.Equal(expiredAt) {
+		t.Fatalf("got token.ExpiredAt = %v, want %v", token.ExpiredAt, expiredAt)
+	}
+}
+
+func TestTokenIsExpired(t *testing.T) {
+	now := time.Now()
+	token := Token{}
+
+	token.ExpiredAt = now.Add(1 * time.Second)
+	if token.IsExpired() {
+		t.Fatalf("got expired token = %v, now = %v, want it not expired", token, now)
+	}
+
+	token.ExpiredAt = now.Add(-1 * time.Second)
+	if !token.IsExpired() {
+		t.Fatalf("got non-expired token = %v, now = %v, want it expired", token, now)
+	}
+}
+
+func TestEmptyTokenIsExpired(t *testing.T) {
+	token := Token{}
+	if !token.IsExpired() {
+		t.Fatalf("got non-expired empty token = %v, want it expired", token)
+	}
+}
+
+func TestFileStorePut(t *testing.T) {
+	const savedFileContent = `{"accessToken":"sometoken","expiredAt":"1970-01-01T00:00:01Z","userInfoID":"someuserinfoid"}
+`
+	token := Token{
+		AccessToken: "sometoken",
+		ExpiredAt:   time.Unix(1, 0).UTC(),
+		UserInfoID:  "someuserinfoid",
+	}
+
+	dir := tempDir()
+	defer os.RemoveAll(dir)
+
+	store := FileStore(dir)
+	if err := store.Put(&token); err != nil {
+		t.Fatalf("got err = %v, want nil", err)
+	}
+
+	filePath := filepath.Join(dir, "sometoken")
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	if !bytes.Equal(fileBytes, []byte(savedFileContent)) {
+		t.Fatalf("got file content = %#v, want %#v", string(fileBytes), savedFileContent)
+	}
+}
