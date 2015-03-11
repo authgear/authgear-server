8a30f66673532c4f6f7eabdfa9120bd26e7b94fd
Kenji Pa <limouren@gmail.com>
2015-03-11 15:43:20 +0800

    Add Get and Set methods to Record supporting reserved fields, #12

--- oddb/record.go
+++ oddb/record.go
@@ -2,6 +2,7 @@ package oddb
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 )
 
@@ -13,6 +14,45 @@ type Record struct {
 	Type string `json:"_type"`
 	Key  string `json:"_id"`
 	Data `json:"data"`
+}
+
+// Get returns the value specified by key. If no value is associated
+// with the specified key, it returns nil.
+//
+// Get also supports getting reserved fields starting with "_". If such
+// reserved field does not exists, it returns nil.
+func (r *Record) Get(key string) interface{} {
+	if key[0] == '_' {
+		switch key {
+		case "_type":
+			return r.Type
+		case "_id":
+			return r.Key
+		default:
+			return nil
+		}
+	} else {
+		return r.Data[key]
+	}
+}
+
+// Set associates key with the value i in this record.
+//
+// Set is able to associate reserved key name starting with "_" as well.
+// If there is no such key, it panics.
+func (r *Record) Set(key string, i interface{}) {
+	if key[0] == '_' {
+		switch key {
+		case "_type":
+			r.Type = i.(string)
+		case "_id":
+			r.Key = i.(string)
+		default:
+			panic(fmt.Sprintf("unknown reserved key: %v", key))
+		}
+	} else {
+		r.Data[key] = i
+	}
 }
 
 // A Datetime represent an instance in time.
