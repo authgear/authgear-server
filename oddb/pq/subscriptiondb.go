6475eb86958ace3a8541e245cc312039debc185e
Kenji Pa <limouren@gmail.com>
2015-05-06 16:25:43 +0800

    oddb/pq: implement SaveSubscription, #69

--- oddb/pq/subscriptiondb.go
+++ oddb/pq/subscriptiondb.go
@@ -1,13 +1,100 @@
 package pq
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/lib/pq"
+
 	"github.com/oursky/ourd/oddb"
 )
+
+func isDeviceNotFound(err error) bool {
+	if pqErr, ok := err.(*pq.Error); ok {
+		return pqErr.Code == "23503" && pqErr.Constraint == "_subscription_device_id_fkey"
+	}
+
+	return false
+}
+
+type notificationInfoValue oddb.NotificationInfo
+
+func (info notificationInfoValue) Value() (driver.Value, error) {
+	return json.Marshal(info)
+}
+
+func (info *notificationInfoValue) Scan(value interface{}) error {
+	if value == nil {
+		*info = notificationInfoValue{}
+		return nil
+	}
+
+	b, ok := value.([]byte)
+	if !ok {
+		fmt.Errorf("oddb: unsupported Scan pair: %T -> %T", value, info)
+	}
+
+	return json.Unmarshal(b, info)
+}
+
+type queryValue oddb.Query
+
+func (query queryValue) Value() (driver.Value, error) {
+	return json.Marshal(query)
+}
+
+func (query *queryValue) Scan(value interface{}) error {
+	if value == nil {
+		*query = queryValue{}
+		return nil
+	}
+
+	b, ok := value.([]byte)
+	if !ok {
+		fmt.Errorf("oddb: unsupported Scan pair: %T -> %T", value, query)
+	}
+
+	return json.Unmarshal(b, query)
+}
 
 func (db *database) GetSubscription(key string, subscription *oddb.Subscription) error { return nil }
 
 func (db *database) SaveSubscription(subscription *oddb.Subscription) error {
-	return nil
+	if subscription.ID == "" {
+		return errors.New("empty id")
+	}
+	if subscription.Type == "" {
+		return errors.New("empty type")
+	}
+	if subscription.Query.Type == "" {
+		return errors.New("empty query type")
+	}
+	if subscription.DeviceID == "" {
+		return errors.New("empty device id")
+	}
+
+	pkData := map[string]interface{}{
+		"id":      subscription.ID,
+		"user_id": db.userID,
+	}
+
+	data := map[string]interface{}{
+		"device_id":         subscription.DeviceID,
+		"type":              subscription.Type,
+		"notification_info": notificationInfoValue(subscription.NotificationInfo),
+		"query":             queryValue(subscription.Query),
+	}
+
+	sql, args := upsertQuery(db.tableName("_subscription"), pkData, data)
+	_, err := db.Db.Exec(sql, args...)
+
+	if isDeviceNotFound(err) {
+		return oddb.ErrDeviceNotFound
+	}
+
+	return err
 }
 
 func (db *database) DeleteSubscription(key string) error { return nil }
---
+++ oddb/pq/subscriptiondb_test.go
@@ -0,0 +1,130 @@
+package pq
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+
+	"github.com/oursky/ourd/oddb"
+)
+
+func addDevice(t *testing.T, c *conn, userID string, deviceID string) {
+	_, err := c.Db.Exec("INSERT INTO app_com_oursky_ourd._device (id, user_id, type, token) VALUES ($1, $2, '', '')", deviceID, userID)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSubscriptionCRUD(t *testing.T) {
+	Convey("Database", t, func() {
+		c := getTestConn(t)
+		db := c.PrivateDB("userid")
+
+		// fixture
+		addUser(t, c, "userid")
+		addDevice(t, c, "userid", "deviceid")
+
+		notificationInfo := oddb.NotificationInfo{
+			APS: oddb.APSSetting{
+				Alert: oddb.AppleAlert{
+					Body:                  "somebody",
+					LocalizationKey:       "somelocalizationkey",
+					LocalizationArgs:      []string{"arg0", "arg1"},
+					LaunchImage:           "somelaunchimage",
+					ActionLocalizationKey: "someactionlocalizationkey",
+				},
+				SoundName:                  "somesoundname",
+				ShouldBadge:                true,
+				ShouldSendContentAvailable: true,
+			},
+		}
+		query := oddb.Query{
+			Type: "recordtype",
+		}
+		subscription := oddb.Subscription{
+			ID:               "subscriptionid",
+			Type:             "query",
+			DeviceID:         "deviceid",
+			NotificationInfo: notificationInfo,
+			Query:            query,
+		}
+
+		Convey("create new subscription", func() {
+			err := db.SaveSubscription(&subscription)
+			So(err, ShouldBeNil)
+
+			var (
+				deviceID, queryType    string
+				resultNotificationInfo oddb.NotificationInfo
+				resultQuery            oddb.Query
+			)
+			err = c.Db.QueryRow(`
+				SELECT device_id, type, notification_info, query FROM app_com_oursky_ourd._subscription
+				WHERE id = $1 AND user_id = $2`, "subscriptionid", "userid").
+				Scan(&deviceID, &queryType, (*notificationInfoValue)(&resultNotificationInfo), (*queryValue)(&resultQuery))
+			So(err, ShouldBeNil)
+
+			So(deviceID, ShouldEqual, "deviceid")
+			So(queryType, ShouldEqual, "query")
+			So(resultNotificationInfo, ShouldResemble, notificationInfo)
+			So(resultQuery, ShouldResemble, query)
+		})
+
+		Convey("modify existing subscription", func() {
+			So(db.SaveSubscription(&subscription), ShouldBeNil)
+
+			subscription.Query.Type = "otherrecordtype"
+			err := db.SaveSubscription(&subscription)
+			So(err, ShouldBeNil)
+
+			var (
+				deviceID, queryType    string
+				resultNotificationInfo oddb.NotificationInfo
+				resultQuery            oddb.Query
+			)
+			err = c.Db.QueryRow(`
+				SELECT device_id, type, notification_info, query FROM app_com_oursky_ourd._subscription
+				WHERE id = $1 AND user_id = $2`, "subscriptionid", "userid").
+				Scan(&deviceID, &queryType, (*notificationInfoValue)(&resultNotificationInfo), (*queryValue)(&resultQuery))
+			So(err, ShouldBeNil)
+
+			So(deviceID, ShouldEqual, "deviceid")
+			So(queryType, ShouldEqual, "query")
+			So(resultNotificationInfo, ShouldResemble, notificationInfo)
+
+			query.Type = "otherrecordtype"
+			So(resultQuery, ShouldResemble, query)
+		})
+
+		Convey("cannot save subscription with empty id", func() {
+			subscription.ID = ""
+			err := db.SaveSubscription(&subscription)
+			So(err.Error(), ShouldEqual, "empty id")
+		})
+
+		Convey("cannot save subscription with empty type", func() {
+			subscription.Type = ""
+			err := db.SaveSubscription(&subscription)
+			So(err.Error(), ShouldEqual, "empty type")
+		})
+
+		Convey("cannot save subscription with empty query type", func() {
+			subscription.Query.Type = ""
+			err := db.SaveSubscription(&subscription)
+			So(err.Error(), ShouldEqual, "empty query type")
+		})
+
+		Convey("cannot save subscription with empty device id", func() {
+			subscription.DeviceID = ""
+			err := db.SaveSubscription(&subscription)
+			So(err.Error(), ShouldEqual, "empty device id")
+		})
+
+		Convey("returns ErrDeviceNotFound if a subscription with non-exist device is saved", func() {
+			subscription.DeviceID = "notexistdeviceid"
+			err := db.SaveSubscription(&subscription)
+			So(err, ShouldEqual, oddb.ErrDeviceNotFound)
+		})
+
+		cleanupDB(t, c)
+	})
+}
