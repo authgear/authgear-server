f262c7e98a0c71edc39385d75c0b1d791aec6dd4
Kwok-kuen Cheung <cheungpat@oursky.com>
2015-07-03 08:21:54 -0700

    handler: Rename handler.newSingleRouteRouter to handlertest.NewSingleRouteRouter

--- handler/handlertest/handlertest.go
+++ handler/handlertest/handlertest.go
@@ -1,4 +1,4 @@
-package handler
+package handlertest
 
 import (
 	"net/http"
@@ -8,9 +8,13 @@ import (
 	"github.com/oursky/ourd/router"
 )
 
+// SingleRouteRouter is a router that only serves with a single handler,
+// regardless of the requested action.
 type SingleRouteRouter router.Router
 
-func newSingleRouteRouter(handler router.Handler, prepareFunc func(*router.Payload)) *SingleRouteRouter {
+// NewSingleRouteRouter creates a SingleRouteRouter, mapping the specified
+// handler as the only route.
+func NewSingleRouteRouter(handler router.Handler, prepareFunc func(*router.Payload)) *SingleRouteRouter {
 	r := router.NewRouter()
 	r.Map("", handler, func(p *router.Payload, _ *router.Response) int {
 		prepareFunc(p)
@@ -19,6 +23,7 @@ func newSingleRouteRouter(handler router.Handler, prepareFunc func(*router.Paylo
 	return (*SingleRouteRouter)(r)
 }
 
+// POST invoke the only route mapped on the SingleRouteRouter.
 func (r *SingleRouteRouter) POST(body string) *httptest.ResponseRecorder {
 	req, _ := http.NewRequest("POST", "", strings.NewReader(body))
 	resp := httptest.NewRecorder()
