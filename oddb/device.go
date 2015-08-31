1622be398715fe926b6c53f5bf73728bef062e5a
Kenji Pa <limouren@gmail.com>
2015-08-31 15:54:58 +0800

    Add Device.LastRegisteredAt

--- oddb/device.go
+++ oddb/device.go
@@ -1,9 +1,12 @@
 package oddb
 
+import "time"
+
 // Device represents a device owned by a user and ready to receive notification.
 type Device struct {
-	ID         string
-	Type       string
-	Token      string
-	UserInfoID string
+	ID               string
+	Type             string
+	Token            string
+	UserInfoID       string
+	LastRegisteredAt time.Time
 }
