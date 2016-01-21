048095836bc50525a8e5eb143a3ac9f668eac51a
Rick Mak <rick.mak@gmail.com>
2016-01-21 12:59:32 +0800

    Update the fabfile for auto-build migrate docker image
    refs #520

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 # fabric 1.9.0
-from fabric.operations import run
+from fabric.operations import local
 from fabric.api import env
 
 
@@ -7,60 +7,19 @@ from fabric.api import env
 This file is collection of commands regarding deployment
 '''
 
-env.user = 'ourd'
 env.roledefs.update({
-    'pandawork': ['ourd.pandawork.com'], #54.159.147.211
+    'hub-migrate': ['hub.docker.com'],
 })
 
-print(env.host)
-
-# Heaven will execute fab -R edge deploy:branch_name=edge
+# Heaven will execute fab -R hub-migrate deploy:branch_name=master
 def deploy(branch_name):
-    print("Executing on %s as %s" % (env.host, env.user))
-    run('docker pull oursky/ourd:%s' % branch_name)
-    run('docker stop ourd')
-    run('docker rm ourd')
-    init_ourd()
+    print("Executing on %(host)s as %(user)s" % env)
+    if env.host == 'hub.docker.com':
+        build_migrate()
+    else:
+        raise ValueError('Not supported deployment target')
 
 
-def restart():
-    run('docker restart ourd')
-
-
-### Below is init image as refs. Normal flow will only need above command.
-### Assume the machine with docker installed,
-### ubuntu: wget -qO- https://get.docker.com/ | sh
-### adduser ourd
-### passwd -l ourd
-### adduser ourd docker
-
-def init_ourd():
-    '''
-    Run ourd containter
-    '''
-    run('''docker run --name ourd \
--v /home/ourd:/etc/ourd:ro \
--v /home/ourd/data:/etc/data:rw \
---expose 3000 \
--p 3000:3000 \
--e "OD_CONFIG=/etc/ourd/docker.ini" \
---link ourpg:postgres \
---restart=always \
--d oursky/ourd''')
-
-
-def start_db():
-    run('''docker run --name ourpg \
--v /home/ourd/pgdata:/var/lib/postgresql/data \
--p 5432:5432 \
--d mdillon/postgis:9.4''')
-    run('''docker exec -it ourpg \
-psql -U postgres --dbname postgres --command 'CREATE EXTENSION postgis;' ''')
-
-
-def start_nginx():
-    sudo('''docker run -d --name nginx --restart=always \
--v /var/lib/docker/data/nginx/etc:/etc/nginx \
--v /var/lib/docker/data/nginx/log:/var/log/nginx \
--p 80:80 -p 443:443 \
-nginx:1.9.2''')
+def build_migrate():
+    local('docker build -f Dockerfile-migrate -t oursky/skygear-migrate .')
+    local('docker push oursky/skygear-migrate')
