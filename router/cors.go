37d4eec082f45af8f8910e3542d1763bc8512515
Ben Lei <benlei@oursky.com>
2016-02-04 12:29:45 +0800

    Handle CORS preflight request directly #273

--- router/cors.go
+++ router/cors.go
@@ -8,17 +8,28 @@ import (
 
 func CorsMiddleware(corsOrigin string, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestMethod := r.Method
 		corsMethod := r.Header.Get("Access-Control-Request-Method")
 		corsHeaders := r.Header.Get("Access-Control-Request-Headers")
 
-		log.Debugf("CORS Method: %s", corsMethod)
-		log.Debugf("CORS Headers: %s", corsHeaders)
-
 		w.Header().Set("Access-Control-Allow-Origin", corsOrigin)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Methods", corsMethod)
-		w.Header().Set("Access-Control-Allow-Headers", corsHeaders)
 
-		next.ServeHTTP(w, r)
+		if corsMethod != "" {
+			log.Debugf("CORS Method: %s", corsMethod)
+			w.Header().Set("Access-Control-Allow-Methods", corsMethod)
+		}
+
+		if corsHeaders != "" {
+			log.Debugf("CORS Headers: %s", corsHeaders)
+			w.Header().Set("Access-Control-Allow-Headers", corsHeaders)
+		}
+
+		if requestMethod == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte{})
+		} else {
+			next.ServeHTTP(w, r)
+		}
 	})
 }
