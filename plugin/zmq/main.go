f25646d32536cdacc45a679b3f9861df9519e5a7
Kwok-kuen Cheung <cheungpat@oursky.com>
2016-02-16 18:48:32 +0800

    Fix panic when not building zmq

--- plugin/zmq/main.go
+++ plugin/zmq/main.go
@@ -1,7 +1,3 @@
-// +build !zmq
-
 package zmq
 
-func init() {
-	panic("zmq transport is not included during compilation, please compile with `go build -tags zmq`.")
-}
+// This file is intentionally left empty.
