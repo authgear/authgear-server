authgear/authgear-server

commit 107708789b8f0e64bcde1987e0ff57155f662c8f

Inject dependency into handlers
Handler function is now converted to struct with references to
services injected by github.com/facebookgo/inject

refs #482

main.go
@@ -8,6 +8,7 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/Sirupsen/logrus/hooks/sentry"
+	"github.com/facebookgo/inject"
 	"github.com/robfig/cron"
 
 	"github.com/oursky/skygear/asset"
@@ -55,16 +56,14 @@ func main() {
 	// Init all the services
 	r := router.NewRouter()
 	pushSender := initPushSender(config, connOpener)
-	store := initAssetStore(config)
+
 	tokenStore := authtoken.InitTokenStore(config.TokenStore.ImplName, config.TokenStore.Path)
 
-	providerRegistry := provider.NewRegistry()
-	hookRegistry := hook.NewRegistry()
 	c := cron.New()
 	initContext := plugin.InitContext{
 		Router:           r,
-		HookRegistry:     hookRegistry,
-		ProviderRegistry: providerRegistry,
+		HookRegistry:     hook.NewRegistry(),
+		ProviderRegistry: provider.NewRegistry(),
 		Scheduler:        c,
 	}
 	c.Start()
@@ -79,17 +78,9 @@ func main() {
 		NotificationSender: pushSender,
 	}
 
-	assetStorePreprocessor := pp.AssetStorePreprocessor{
-		Store: store,
-	}
-
 	naiveAPIKeyPreprocessor := pp.AccessKeyValidatonPreprocessor{
 		Key:     config.App.APIKey,
 		AppName: config.App.Name,
-	}
-
-	tokenStorePreprocessor := pp.TokenStorePreprocessor{
-		Store: tokenStore,
 	}
 
 	authenticator := pp.UserAuthenticator{
@@ -105,59 +96,36 @@ func main() {
 		Option:   config.DB.Option,
 	}
 
-	providerRegistryPreprocessor := pp.ProviderRegistryPreprocessor{
-		PluginInitContext: &initContext,
-	}
-
-	hookRegistryPreprocessor := pp.HookRegistryPreprocessor{
-		PluginInitContext: &initContext,
-	}
+	pluginReadyPreprocessor := &pp.EnsurePluginReadyPreprocessor{&initContext}
 
 	baseAuthPreprocessors := []router.Processor{
 		authenticator.Preprocess,
 		dbConnPreprocessor.Preprocess,
-		assetStorePreprocessor.Preprocess,
 		pp.InjectUserIfPresent,
 		pp.InjectDatabase,
 	}
 
 	assetGetPreprocessors := []router.Processor{
 		dbConnPreprocessor.Preprocess,
-		assetStorePreprocessor.Preprocess,
 	}
 
 	assetPutPreprocessors := []router.Processor{
 		naiveAPIKeyPreprocessor.Preprocess,
 		dbConnPreprocessor.Preprocess,
-		assetStorePreprocessor.Preprocess,
 	}
 
 	authPreprocessors := []router.Processor{
 		naiveAPIKeyPreprocessor.Preprocess,
 		dbConnPreprocessor.Preprocess,
-		tokenStorePreprocessor.Preprocess,
-		providerRegistryPreprocessor.Preprocess,
+		pluginReadyPreprocessor.Preprocess,
 	}
 
 	recordWritePreprocessors := append(baseAuthPreprocessors,
-		hookRegistryPreprocessor.Preprocess,
 		pp.RequireUserForWrite,
+		pluginReadyPreprocessor.Preprocess,
 	)
 
-	userWritePreprocessors := append(baseAuthPreprocessors,
-		pp.RequireUserForWrite,
-	)
-
-	userLinkPreprocessors := append(baseAuthPreprocessors,
-		providerRegistryPreprocessor.Preprocess,
-		pp.RequireUserForWrite,
-	)
-
-	devicePreprocessors := append(baseAuthPreprocessors,
-		pp.RequireUserForWrite,
-	)
-
-	subscriptionPreprocessors := append(baseAuthPreprocessors,
+	requireUserWritePreprocessors := append(baseAuthPreprocessors,
 		pp.RequireUserForWrite,
 	)
 
@@ -172,58 +140,67 @@ func main() {
 		naiveAPIKeyPreprocessor.Preprocess,
 	}
 
-	r.Map("", handler.HomeHandler)
+	r.Map("", &handler.HomeHandler{})
 
-	r.Map("auth:signup", handler.SignupHandler, authPreprocessors...)
-	r.Map("auth:login", handler.LoginHandler, authPreprocessors...)
-	r.Map("auth:logout", handler.LogoutHandler,
-		tokenStorePreprocessor.Preprocess,
-		authenticator.Preprocess,
-		providerRegistryPreprocessor.Preprocess,
+	g := &inject.Graph{}
+	injectErr := g.Provide(
+		&inject.Object{Value: initContext.ProviderRegistry, Complete: true, Name: "ProviderRegistry"},
+		&inject.Object{Value: initContext.HookRegistry, Complete: true, Name: "HookRegistry"},
+		&inject.Object{Value: tokenStore, Complete: true, Name: "TokenStore"},
+		&inject.Object{Value: initAssetStore(config), Complete: true, Name: "AssetStore"},
+		&inject.Object{Value: pushSender, Complete: true, Name: "PushSender"},
 	)
-	r.Map("auth:password", handler.PasswordHandler, baseAuthPreprocessors...)
+	if injectErr != nil {
+		panic(fmt.Sprintf("Unable to set up handler: %v", injectErr))
+	}
 
-	r.Map("record:fetch", handler.RecordFetchHandler, baseAuthPreprocessors...)
-	r.Map("record:query", handler.RecordQueryHandler, baseAuthPreprocessors...)
-	r.Map("record:save", handler.RecordSaveHandler, recordWritePreprocessors...)
-	r.Map("record:delete", handler.RecordDeleteHandler, recordWritePreprocessors...)
+	r.Map("auth:signup", injectedHandler(g, &handler.SignupHandler{}), authPreprocessors...)
+	r.Map("auth:login", injectedHandler(g, &handler.LoginHandler{}), authPreprocessors...)
+	r.Map("auth:logout", injectedHandler(g, &handler.LogoutHandler{}),
+		authenticator.Preprocess,
+		pluginReadyPreprocessor.Preprocess,
+	)
+	r.Map("auth:password", injectedHandler(g, &handler.PasswordHandler{}), baseAuthPreprocessors...)
 
-	r.Map("device:register", handler.DeviceRegisterHandler, devicePreprocessors...)
+	r.Map("record:fetch", injectedHandler(g, &handler.RecordFetchHandler{}), baseAuthPreprocessors...)
+	r.Map("record:query", injectedHandler(g, &handler.RecordQueryHandler{}), baseAuthPreprocessors...)
+	r.Map("record:save", injectedHandler(g, &handler.RecordSaveHandler{}), recordWritePreprocessors...)
+	r.Map("record:delete", injectedHandler(g, &handler.RecordDeleteHandler{}), recordWritePreprocessors...)
+
+	r.Map("device:register", injectedHandler(g, &handler.DeviceRegisterHandler{}), requireUserWritePreprocessors...)
 
 	// subscription shares the same set of preprocessor as record read at the moment
-	r.Map("subscription:fetch_all", handler.SubscriptionFetchAllHandler, subscriptionPreprocessors...)
-	r.Map("subscription:fetch", handler.SubscriptionFetchHandler, subscriptionPreprocessors...)
-	r.Map("subscription:save", handler.SubscriptionSaveHandler, subscriptionPreprocessors...)
-	r.Map("subscription:delete", handler.SubscriptionDeleteHandler, subscriptionPreprocessors...)
+	r.Map("subscription:fetch_all", injectedHandler(g, &handler.SubscriptionFetchAllHandler{}), requireUserWritePreprocessors...)
+	r.Map("subscription:delete", injectedHandler(g, &handler.SubscriptionDeleteHandler{}), requireUserWritePreprocessors...)
 
 	// relation shares the same setof preprocessor
-	r.Map("relation:query", handler.RelationQueryHandler, baseAuthPreprocessors...)
-	r.Map("relation:add", handler.RelationAddHandler, baseAuthPreprocessors...)
-	r.Map("relation:remove", handler.RelationRemoveHandler, baseAuthPreprocessors...)
+	r.Map("relation:query", injectedHandler(g, &handler.RelationQueryHandler{}), baseAuthPreprocessors...)
+	r.Map("relation:add", injectedHandler(g, &handler.RelationAddHandler{}), baseAuthPreprocessors...)
+	r.Map("relation:remove", injectedHandler(g, &handler.RelationRemoveHandler{}), baseAuthPreprocessors...)
 
-	r.Map("user:query", handler.UserQueryHandler, baseAuthPreprocessors...)
-	r.Map("user:update", handler.UserUpdateHandler, userWritePreprocessors...)
-	r.Map("user:link", handler.UserLinkHandler, userLinkPreprocessors...)
+	r.Map("user:query", injectedHandler(g, &handler.UserQueryHandler{}), baseAuthPreprocessors...)
+	r.Map("user:update", injectedHandler(g, &handler.UserUpdateHandler{}), requireUserWritePreprocessors...)
+	r.Map("user:link", injectedHandler(g, &handler.UserLinkHandler{}), requireUserWritePreprocessors...)
 
-	r.Map("push:user", handler.PushToUserHandler, notificationPreprocessors...)
-	r.Map("push:device", handler.PushToDeviceHandler, notificationPreprocessors...)
+	r.Map("push:user", injectedHandler(g, &handler.PushToUserHandler{}), notificationPreprocessors...)
+	r.Map("push:device", injectedHandler(g, &handler.PushToDeviceHandler{}), notificationPreprocessors...)
 
 	// Following section is for Gateway
 	pubSub := pubsub.NewWsPubsub(nil)
 	pubSubGateway := router.NewGateway(`pubSub`)
-	pubSubGateway.GET(handler.NewPubSubHandler(pubSub), pubSubPreprocessors...)
+	pubSubGateway.GET(&handler.PubSubHandler{pubSub}, pubSubPreprocessors...)
 
 	internalPubSub := pubsub.NewWsPubsub(internalHub)
 	internalPubSubGateway := router.NewGateway(`internalpubSub`)
-	internalPubSubGateway.GET(handler.NewPubSubHandler(internalPubSub), pubSubPreprocessors...)
+	internalPubSubGateway.GET(&handler.PubSubHandler{internalPubSub}, pubSubPreprocessors...)
 
 	http.Handle("/", router.LoggingMiddleware(r, false))
 	http.Handle("/pubsub", router.LoggingMiddleware(pubSubGateway, false))
 	http.Handle("/_/pubsub", router.LoggingMiddleware(internalPubSubGateway, false))
 
 	fileGateway := router.NewGateway(`files/(.+)`)
-	fileGateway.GET(handler.AssetGetURLHandler, assetGetPreprocessors...)
-	fileGateway.PUT(handler.AssetUploadURLHandler, assetPutPreprocessors...)
+	fileGateway.GET(injectedHandler(g, &handler.AssetGetURLHandler{}), assetGetPreprocessors...)
+	fileGateway.PUT(injectedHandler(g, &handler.AssetUploadURLHandler{}), assetPutPreprocessors...)
 	http.Handle("/files/", router.LoggingMiddleware(fileGateway, true))
 
 	// Bootstrap finished, binding port.
@@ -232,6 +209,19 @@ func main() {
 	if err != nil {
 		log.Printf("Failed: %v", err)
 	}
+}
+
+func injectedHandler(g *inject.Graph, h router.Handler) router.Handler {
+	err := g.Provide(&inject.Object{Value: h})
+	if err != nil {
+		panic(fmt.Sprintf("Unable to set up handler: %v", err))
+	}
+
+	err = g.Populate()
+	if err != nil {
+		panic(fmt.Sprintf("Unable to set up handler: %v", err))
+	}
+	return h
 }
 
 func ensureDB(config Configuration) func() (skydb.Conn, error) {
@@ -311,10 +301,6 @@ func initPushSender(config Configuration, connOpener func() (skydb.Conn, error))
 		gcm := initGCMPusher(config)
 		routeSender.Route("gcm", gcm)
 		routeSender.Route("android", gcm)
-	}
-
-	if routeSender.Len() == 0 {
-		return nil
 	}
 	return routeSender
 }

handler/asset.go
@@ -52,10 +52,14 @@ func validateAssetGetRequest(assetStore skyAsset.Store, fileName string, expired
 	return nil
 }
 
-func AssetGetURLHandler(payload *router.Payload, response *router.Response) {
+type AssetGetURLHandler struct {
+	AssetStore skyAsset.Store `inject:"AssetStore"`
+}
+
+func (h *AssetGetURLHandler) Handle(payload *router.Payload, response *router.Response) {
 	payload.Req.ParseForm()
 
-	store := payload.AssetStore
+	store := h.AssetStore
 	fileName := clean(payload.Params[0])
 	if store.(skyAsset.URLSigner).IsSignatureRequired() {
 		expiredAtUnix, err := strconv.ParseInt(payload.Req.Form.Get("expiredAt"), 10, 64)
@@ -65,7 +69,7 @@ func AssetGetURLHandler(payload *router.Payload, response *router.Response) {
 		}
 
 		signature := payload.Req.Form.Get("signature")
-		requestErr := validateAssetGetRequest(payload.AssetStore, fileName, expiredAtUnix, signature)
+		requestErr := validateAssetGetRequest(h.AssetStore, fileName, expiredAtUnix, signature)
 		if requestErr != nil {
 			response.Err = requestErr
 			return
@@ -110,7 +114,11 @@ func AssetGetURLHandler(payload *router.Payload, response *router.Response) {
 //		-H 'Content-Type: text/plain' \
 //		--data-binary '@file.txt' \
 //		http://localhost:3000/files/filename
-func AssetUploadURLHandler(payload *router.Payload, response *router.Response) {
+type AssetUploadURLHandler struct {
+	AssetStore skyAsset.Store `inject:"AssetStore"`
+}
+
+func (h *AssetUploadURLHandler) Handle(payload *router.Payload, response *router.Response) {
 	var (
 		fileName, contentType string
 	)
@@ -143,7 +151,7 @@ func AssetUploadURLHandler(payload *router.Payload, response *router.Response) {
 		return
 	}
 
-	assetStore := payload.AssetStore
+	assetStore := h.AssetStore
 	if err := assetStore.PutFileReader(fileName, tempFile, written, contentType); err != nil {
 		response.Err = skyerr.NewUnknownErr(err)
 		return
@@ -161,7 +169,7 @@ func AssetUploadURLHandler(payload *router.Payload, response *router.Response) {
 		return
 	}
 
-	if signer, ok := payload.AssetStore.(skyAsset.URLSigner); ok {
+	if signer, ok := h.AssetStore.(skyAsset.URLSigner); ok {
 		asset.Signer = signer
 	} else {
 		log.Warnf("Failed to acquire asset URLSigner, please check configuration")

handler/asset_test.go
@@ -120,9 +120,10 @@ func TestAssetUploadURLHandler(t *testing.T) {
 		store := newBufferedStore()
 
 		r := newmodGateway("(.+)")
-		r.Handle("PUT", AssetUploadURLHandler, func(p *router.Payload) {
+		r.Handle("PUT", &AssetUploadURLHandler{
+			AssetStore: store,
+		}, func(p *router.Payload) {
 			p.DBConn = assetConn
-			p.AssetStore = store
 		})
 
 		Convey("uploads a file", func() {
@@ -248,9 +249,10 @@ func TestAssetGetURLHandler(t *testing.T) {
 		signparser := newNaiveStoreSignatureParser(store)
 
 		r := newmodGateway("(.+)")
-		r.Handle("GET", AssetGetURLHandler, func(p *router.Payload) {
+		r.Handle("GET", &AssetGetURLHandler{
+			AssetStore: signparser,
+		}, func(p *router.Payload) {
 			p.DBConn = assetConn
-			p.AssetStore = signparser
 		})
 
 		Convey("GET a signed URL", func() {

handler/auth.go
@@ -6,6 +6,7 @@ import (
 	log "github.com/Sirupsen/logrus"
 
 	"github.com/oursky/skygear/authtoken"
+	"github.com/oursky/skygear/provider"
 	"github.com/oursky/skygear/router"
 	"github.com/oursky/skygear/skydb"
 	"github.com/oursky/skygear/skyerr"
@@ -81,8 +82,13 @@ func (p *signupPayload) AuthData() map[string]interface{} {
 //	    "password": "123456"
 //	}
 //	EOF
-func SignupHandler(payload *router.Payload, response *router.Response) {
-	store := payload.TokenStore
+type SignupHandler struct {
+	TokenStore       authtoken.Store    `inject:"TokenStore"`
+	ProviderRegistry *provider.Registry `inject:"ProviderRegistry"`
+}
+
+func (h *SignupHandler) Handle(payload *router.Payload, response *router.Response) {
+	store := h.TokenStore
 
 	p := signupPayload{
 		AppName: payload.AppName,
@@ -96,7 +102,7 @@ func SignupHandler(payload *router.Payload, response *router.Response) {
 	} else if p.Provider() != "" {
 		// Get AuthProvider and authenticates the user
 		log.Debugf(`Client requested auth provider: "%v".`, p.Provider())
-		authProvider := payload.ProviderRegistry.GetAuthProvider(p.Provider())
+		authProvider := h.ProviderRegistry.GetAuthProvider(p.Provider())
 		principalID, authData, err := authProvider.Login(p.AuthData())
 		if err != nil {
 			response.Err = skyerr.NewError(skyerr.InvalidCredentials, "unable to login with the given credentials")
@@ -192,8 +198,16 @@ curl -X POST -H "Content-Type: application/json" \
 }
 EOF
 */
-func LoginHandler(payload *router.Payload, response *router.Response) {
-	store := payload.TokenStore
+type LoginHandler struct {
+	TokenStore       authtoken.Store    `inject:"TokenStore"`
+	ProviderRegistry *provider.Registry `inject:"ProviderRegistry"`
+}
+
+func (h *LoginHandler) Handle(payload *router.Payload, response *router.Response) {
+	if h.TokenStore == nil {
+		panic("token store is nil")
+	}
+	store := h.TokenStore
 
 	p := loginPayload{
 		AppName: payload.AppName,
@@ -206,7 +220,7 @@ func LoginHandler(payload *router.Payload, response *router.Response) {
 	if p.Provider() != "" {
 		// Get AuthProvider and authenticates the user
 		log.Debugf(`Client requested auth provider: "%v".`, p.Provider())
-		authProvider := payload.ProviderRegistry.GetAuthProvider(p.Provider())
+		authProvider := h.ProviderRegistry.GetAuthProvider(p.Provider())
 		principalID, authData, err := authProvider.Login(p.AuthData())
 		if err != nil {
 			response.Err = skyerr.NewError(skyerr.InvalidCredentials, "invalid authentication information")
@@ -270,8 +284,12 @@ func LoginHandler(payload *router.Payload, response *router.Response) {
 }
 
 // LogoutHandler receives an access token and invalidates it
-func LogoutHandler(payload *router.Payload, response *router.Response) {
-	store := payload.TokenStore
+type LogoutHandler struct {
+	TokenStore authtoken.Store `inject:"TokenStore"`
+}
+
+func (h *LogoutHandler) Handle(payload *router.Payload, response *router.Response) {
+	store := h.TokenStore
 	accessToken := payload.AccessToken()
 
 	var err error
@@ -344,7 +362,10 @@ func (p *passwordPayload) Invalidate() bool {
 // If `user_id` is supplied, will check authorization policy and see if existing
 // accept `invalidate` and invaldate all existing access token.
 // Return userInfoID with new AccessToken if the invalidate is true
-func PasswordHandler(payload *router.Payload, response *router.Response) {
+type PasswordHandler struct {
+}
+
+func (h *PasswordHandler) Handle(payload *router.Payload, response *router.Response) {
 	log.Debugf("changing password")
 	p := passwordPayload{
 		AppName:    payload.AppName,

handler/auth_test.go
@@ -51,7 +51,8 @@ func TestHomeHandler(t *testing.T) {
 	req := router.Payload{}
 	resp := router.Response{}
 
-	HomeHandler(&req, &resp)
+	handler := &HomeHandler{}
+	handler.Handle(&req, &resp)
 	var s statusResponse
 
 	switch pt := resp.Result.(type) {
@@ -76,11 +77,13 @@ func TestSignupHandler(t *testing.T) {
 			"email":    "john.doe@example.com",
 			"password": "secret",
 		},
-		DBConn:     conn,
-		TokenStore: &tokenStore,
+		DBConn: conn,
 	}
 	resp := router.Response{}
-	SignupHandler(&req, &resp)
+	handler := &SignupHandler{
+		TokenStore: &tokenStore,
+	}
+	handler.Handle(&req, &resp)
 
 	authResp, ok := resp.Result.(authResponse)
 	if !ok {
@@ -122,11 +125,13 @@ func TestSignupHandlerDuplicatedUsername(t *testing.T) {
 			"email":    "john.doe@example.com",
 			"password": "secret",
 		},
-		DBConn:     conn,
-		TokenStore: &tokenStore,
+		DBConn: conn,
 	}
 	resp := router.Response{}
-	SignupHandler(&req, &resp)
+	handler := &SignupHandler{
+		TokenStore: &tokenStore,
+	}
+	handler.Handle(&req, &resp)
 
 	errorResponse, ok := resp.Err.(skyerr.Error)
 	if !ok {
@@ -151,11 +156,13 @@ func TestSignupHandlerDuplicatedEmail(t *testing.T) {
 			"email":    "john.doe@example.com",
 			"password": "secret",
 		},
-		DBConn:     conn,
-		TokenStore: &tokenStore,
+		DBConn: conn,
 	}
 	resp := router.Response{}
-	SignupHandler(&req, &resp)
+	handler := &SignupHandler{
+		TokenStore: &tokenStore,
+	}
+	handler.Handle(&req, &resp)
 
 	errorResponse, ok := resp.Err.(skyerr.Error)
 	if !ok {
@@ -179,11 +186,13 @@ func TestLoginHandler(t *testing.T) {
 			"username": "john.doe",
 			"password": "secret",
 		},
-		DBConn:     conn,
-		TokenStore: &tokenStore,
+		DBConn: conn,
 	}
 	resp := router.Response{}
-	LoginHandler(&req, &resp)
+	handler := &LoginHandler{
+		TokenStore: &tokenStore,
+	}
+	handler.Handle(&req, &resp)
 
 	authResp, ok := resp.Result.(authResponse)
 	if !ok {
@@ -224,11 +233,13 @@ func TestLoginHandlerWrongPassword(t *testing.T) {
 			"username": "john.doe",
 			"password": "wrongsecret",
 		},
-		DBConn:     conn,
-		TokenStore: &tokenStore,
+		DBConn: conn,
 	}
 	resp := router.Response{}
-	LoginHandler(&req, &resp)
+	handler := &LoginHandler{
+		TokenStore: &tokenStore,
+	}
+	handler.Handle(&req, &resp)
 
 	errorResponse, ok := resp.Err.(skyerr.Error)
 	if !ok {
@@ -249,11 +260,13 @@ func TestLoginHandlerNotFound(t *testing.T) {
 			"username": "john.doe",
 			"password": "secret",
 		},
-		DBConn:     conn,
-		TokenStore: &tokenStore,
+		DBConn: conn,
 	}
 	resp := router.Response{}
-	LoginHandler(&req, &resp)
+	handler := &LoginHandler{
+		TokenStore: &tokenStore,
+	}
+	handler.Handle(&req, &resp)
 
 	errorResponse, ok := resp.Err.(skyerr.Error)
 	if !ok {
@@ -272,10 +285,11 @@ func TestLoginHandlerWithProvider(t *testing.T) {
 		providerRegistry := provider.NewRegistry()
 		providerRegistry.RegisterAuthProvider("com.example", handlertest.NewSingleUserAuthProvider("com.example", "johndoe"))
 
-		r := handlertest.NewSingleRouteRouter(LoginHandler, func(p *router.Payload) {
-			p.TokenStore = &tokenStore
+		r := handlertest.NewSingleRouteRouter(&LoginHandler{
+			TokenStore:       &tokenStore,
+			ProviderRegistry: providerRegistry,
+		}, func(p *router.Payload) {
 			p.DBConn = &conn
-			p.ProviderRegistry = providerRegistry
 		})
 
 		Convey("login in existing", func() {
@@ -366,8 +380,9 @@ func TestSignupHandlerAsAnonymous(t *testing.T) {
 		tokenStore := singleTokenStore{}
 		conn := singleUserConn{}
 
-		r := handlertest.NewSingleRouteRouter(SignupHandler, func(p *router.Payload) {
-			p.TokenStore = &tokenStore
+		r := handlertest.NewSingleRouteRouter(&SignupHandler{
+			TokenStore: &tokenStore,
+		}, func(p *router.Payload) {
 			p.DBConn = &conn
 		})
 
@@ -426,10 +441,11 @@ func TestSignupHandlerWithProvider(t *testing.T) {
 		providerRegistry := provider.NewRegistry()
 		providerRegistry.RegisterAuthProvider("com.example", handlertest.NewSingleUserAuthProvider("com.example", "johndoe"))
 
-		r := handlertest.NewSingleRouteRouter(SignupHandler, func(p *router.Payload) {
-			p.TokenStore = &tokenStore
+		r := handlertest.NewSingleRouteRouter(&SignupHandler{
+			TokenStore:       &tokenStore,
+			ProviderRegistry: providerRegistry,
+		}, func(p *router.Payload) {
 			p.DBConn = &conn
-			p.ProviderRegistry = providerRegistry
 		})
 
 		Convey("signs up with user", func() {
@@ -489,8 +505,9 @@ func TestLogoutHandler(t *testing.T) {
 		tokenStore := &deleteTokenStore{}
 		conn := skydbtest.NewMapConn()
 
-		r := handlertest.NewSingleRouteRouter(LogoutHandler, func(p *router.Payload) {
-			p.TokenStore = tokenStore
+		r := handlertest.NewSingleRouteRouter(&LogoutHandler{
+			TokenStore: tokenStore,
+		}, func(p *router.Payload) {
 			p.DBConn = conn
 		})
 
@@ -545,8 +562,7 @@ func TestPasswordHandlerWithProvider(t *testing.T) {
 		token := authtoken.New("_", userinfo.ID, time.Time{})
 		tokenStore.Put(&token)
 
-		r := handlertest.NewSingleRouteRouter(PasswordHandler, func(p *router.Payload) {
-			p.TokenStore = &tokenStore
+		r := handlertest.NewSingleRouteRouter(&PasswordHandler{}, func(p *router.Payload) {
 			p.DBConn = &conn
 		})
 

handler/device.go
@@ -62,7 +62,10 @@ type DeviceReigsterResult struct {
 //	}
 //	EOF
 //
-func DeviceRegisterHandler(rpayload *router.Payload, response *router.Response) {
+type DeviceRegisterHandler struct {
+}
+
+func (h *DeviceRegisterHandler) Handle(rpayload *router.Payload, response *router.Response) {
 	payload := deviceRegisterPayload{}
 	if err := mapstructure.Decode(rpayload.Data, &payload); err != nil {
 		response.Err = skyerr.NewError(skyerr.BadRequest, err.Error())

handler/device_test.go
@@ -59,7 +59,8 @@ func TestDeviceRegisterHandler(t *testing.T) {
 				"device_token": "some-awesome-token",
 			}
 
-			DeviceRegisterHandler(&payload, &resp)
+			handler := &DeviceRegisterHandler{}
+			handler.Handle(&payload, &resp)
 
 			result := resp.Result.(DeviceReigsterResult)
 			So(result.ID, ShouldNotBeEmpty)
@@ -88,7 +89,8 @@ func TestDeviceRegisterHandler(t *testing.T) {
 				"device_token": "newtoken",
 			}
 
-			DeviceRegisterHandler(&payload, &resp)
+			handler := &DeviceRegisterHandler{}
+			handler.Handle(&payload, &resp)
 
 			result := resp.Result.(DeviceReigsterResult)
 			So(result.ID, ShouldEqual, "deviceid")
@@ -107,7 +109,8 @@ func TestDeviceRegisterHandler(t *testing.T) {
 				"device_token": "token",
 			}
 
-			DeviceRegisterHandler(&payload, &resp)
+			handler := &DeviceRegisterHandler{}
+			handler.Handle(&payload, &resp)
 
 			err := resp.Err.(skyerr.Error)
 			So(err, ShouldResemble, skyerr.NewError(skyerr.InvalidArgument, "empty device type"))
@@ -119,7 +122,8 @@ func TestDeviceRegisterHandler(t *testing.T) {
 				"device_token": "token",
 			}
 
-			DeviceRegisterHandler(&payload, &resp)
+			handler := &DeviceRegisterHandler{}
+			handler.Handle(&payload, &resp)
 
 			err := resp.Err.(skyerr.Error)
 			So(err, ShouldResemble, skyerr.NewError(skyerr.InvalidArgument, "unknown device type = invalidtype"))
@@ -130,7 +134,8 @@ func TestDeviceRegisterHandler(t *testing.T) {
 				"type": "android",
 			}
 
-			DeviceRegisterHandler(&payload, &resp)
+			handler := &DeviceRegisterHandler{}
+			handler.Handle(&payload, &resp)
 
 			result := resp.Result.(DeviceReigsterResult)
 			So(result.ID, ShouldNotBeEmpty)
@@ -152,7 +157,8 @@ func TestDeviceRegisterHandler(t *testing.T) {
 				"device_token": "newtoken",
 			}
 
-			DeviceRegisterHandler(&payload, &resp)
+			handler := &DeviceRegisterHandler{}
+			handler.Handle(&payload, &resp)
 
 			err := resp.Err.(skyerr.Error)
 			So(err, ShouldResemble, skyerr.NewError(skyerr.ResourceNotFound, "device not found"))
@@ -165,7 +171,8 @@ func TestDeviceRegisterHandler(t *testing.T) {
 				"type": "unknown-type",
 			}
 
-			DeviceRegisterHandler(&payload, &resp)
+			handler := &DeviceRegisterHandler{}
+			handler.Handle(&payload, &resp)
 
 			err := resp.Err.(skyerr.Error)
 			So(err, ShouldResemble, skyerr.NewError(skyerr.InvalidArgument, "unknown device type = unknown-type"))

handler/home.go
@@ -9,7 +9,10 @@ type statusResponse struct {
 }
 
 // HomeHandler temp landing. FIXME
-func HomeHandler(playload *router.Payload, response *router.Response) {
+type HomeHandler struct {
+}
+
+func (h *HomeHandler) Handle(playload *router.Payload, response *router.Response) {
 	var (
 		rep statusResponse
 	)

commit b83695ff02dd16690ed9de062d0ef2707bda50b7

Bring back subscription:{save,fetch}
regression since 107708789b8f0e64bcde1987e0ff57155f662c8f

main.go
@@ -145,6 +145,8 @@ func main() {
 
 	// subscription shares the same set of preprocessor as record read at the moment
 	r.Map("subscription:fetch_all", injector.Inject(&handler.SubscriptionFetchAllHandler{}))
+	r.Map("subscription:fetch", injector.Inject(&handler.SubscriptionFetchHandler{}))
+	r.Map("subscription:save", injector.Inject(&handler.SubscriptionSaveHandler{}))
 	r.Map("subscription:delete", injector.Inject(&handler.SubscriptionDeleteHandler{}))
 
 	// relation shares the same setof preprocessor