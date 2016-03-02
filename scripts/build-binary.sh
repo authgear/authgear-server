a989666bd1371066bbb6b5b46a22efeda22bb0a9
Kwok-kuen Cheung <cheungpat@oursky.com>
2016-03-02 17:26:20 +0800

    Update scripts for building binaries

--- scripts/build-binary.sh
+++ scripts/build-binary.sh
@@ -1,36 +1,27 @@
 #!/bin/sh
 
-go get github.com/tools/godep
-go get golang.org/x/tools/cmd/stringer
-godep restore
-go-wrapper download
-go-wrapper install
+set -e
 
-mkdir -p dist
+DAEMON_NAME=skygear-server
+DIST=dist
+
+mkdir -p $DIST
 
 # build skygear server without C bindings
 for GOOS in darwin linux windows; do
   for GOARCH in 386 amd64; do
-    if [ -n "$VERSION" ]; then
-        FILENAME=skygear-$VERSION-$GOOS-$GOARCH
-   else
-        FILENAME=skygear-$GOOS-$GOARCH
-    fi
-    GOOS=$GOOS GOARCH=$GOARCH go build -o dist/$FILENAME github.com/oursky/skygear
+    FILENAME=$DAEMON_NAME-$GOOS-$GOARCH
+    GOOS=$GOOS GOARCH=$GOARCH go build -o $DIST/$FILENAME github.com/oursky/skygear
   done
 done
 
 # build skygear server with zmq
-ldconfig -p | grep libczma
+ldconfig -p | grep libczmq
 if [ $? -eq 0 ]; then
   GOOS=linux
   GOARCH=amd64
-  if [ -n "$VERSION" ]; then
-    FILENAME=skygear-zmq-$VERSION-$GOOS-$GOARCH
-  else
-    FILENAME=skygear-zmq-$GOOS-$GOARCH
-  fi
-  GOOS=$GOOS GOARCH=$GOARCH go build --tags zmq -o dist/$FILENAME github.com/oursky/skygear
+  FILENAME=$DAEMON_NAME-zmq-$GOOS-$GOARCH
+  GOOS=$GOOS GOARCH=$GOARCH go build --tags zmq -o $DIST/$FILENAME github.com/oursky/skygear
 else
   >&2 echo "Did not build skygear with zmq because libczmq library is not installed."
 fi
--- scripts/build-release-docker.sh
+++ scripts/build-release-docker.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-: ${SKYGEAR_VERSION:=latest}
+set -e
 
-# Run this script in the project root directory.
+: ${SKYGEAR_VERSION:=latest}
+IMAGE_NAME=skygeario/skygear-server:v$SKYGEAR_VERSION
+
+if [ -d dist ]; then
+    echo "Error: Directory 'dist' exists."
+    exit 1
+fi
+
+if [ -f skygear-server ]; then
+    echo "Error: File 'skygear-server' exists."
+    exit 1
+fi
 
 docker build -t skygear-build -f Dockerfile-development .
-docker run -it --rm -v `pwd`:/go/src/app -w /go/src/app -e GOOS=linux -e GOARCH=amd64 skygear-build go build --tags zmq -o skygear-server github.com/oursky/skygear
-docker build -t skygeario/skygear-server:$SKYGEAR_VERSION -f Dockerfile-release .
-docker push skygeario/skygear-server:$SKYGEAR_VERSION
+docker run -it --rm -v `pwd`:/go/src/app -w /go/src/app skygear-build /go/src/app/scripts/build-binary.sh
+cp dist/skygear-server-zmq-linux-amd64 skygear-server
+docker build --pull -t $IMAGE_NAME -f Dockerfile-release .
 
-
+echo "Done. Run \`docker push $IMAGE_NAME\` to push image."
