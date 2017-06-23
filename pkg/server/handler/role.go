e91ec6fb6eb412ee975fe1b2f7ea1696f2b16e66
Rick Mak <rick.mak@gmail.com>
2017-06-23 19:33:29 +0800

    Add role:assign implementation

--- pkg/server/handler/role.go
+++ pkg/server/handler/role.go
@@ -153,3 +153,116 @@ func (h *RoleAdminHandler) Handle(rpayload *router.Payload, response *router.Res
 	}
 	response.Result = payload.Roles
 }
+
+type roleBatchPayload struct {
+	Roles   []string `mapstructure:"roles"`
+	UserIDs []string `mapstructure:"users"`
+}
+
+func (payload *roleBatchPayload) Decode(data map[string]interface{}) skyerr.Error {
+	if err := mapstructure.Decode(data, payload); err != nil {
+		return skyerr.NewError(skyerr.BadRequest, "fails to decode the request payload")
+	}
+	return payload.Validate()
+}
+
+func (payload *roleBatchPayload) Validate() skyerr.Error {
+	if payload.Roles == nil {
+		return skyerr.NewInvalidArgument("unspecified roles in request", []string{"roles"})
+	}
+	if payload.UserIDs == nil {
+		return skyerr.NewInvalidArgument("unspecified users in request", []string{"users"})
+	}
+	return nil
+}
+
+// RoleAssignHandler allow system administractor to batch assign roles to
+// users
+//
+// RoleAssignHandler required user with admin role.
+// All specified users will assign to all roles specified. Roles not already
+// exisited in DB will be created. Any users not found will result in a fails
+// request.
+//
+// curl -X POST -H "Content-Type: application/json" \
+//   -d @- http://localhost:3000/ <<EOF
+// {
+//     "action": "role:assign",
+//     "master_key": "MASTER_KEY",
+//     "access_token": "ACCESS_TOKEN",
+//     "roles": [
+//        "writer",
+//        "user"
+//     ],
+//     "users": [
+//        "95db1e34-0cc0-47b0-8a97-3948633ce09f",
+//        "3df4b52b-bd58-4fa2-8aee-3d44fd7f974d"
+//     ]
+// }
+// EOF
+//
+// {
+//     "result": {
+//       "roles": [
+//          "writer",
+//          "user"
+//       ],
+//       "users": [
+//          "95db1e34-0cc0-47b0-8a97-3948633ce09f",
+//          "3df4b52b-bd58-4fa2-8aee-3d44fd7f974d"
+//       ]
+//     }
+// }
+type RoleAssignHandler struct {
+	Authenticator router.Processor `preprocessor:"authenticator"`
+	DBConn        router.Processor `preprocessor:"dbconn"`
+	InjectUser    router.Processor `preprocessor:"inject_user"`
+	PluginReady   router.Processor `preprocessor:"plugin_ready"`
+	preprocessors []router.Processor
+}
+
+func (h *RoleAssignHandler) Setup() {
+	h.preprocessors = []router.Processor{
+		h.Authenticator,
+		h.DBConn,
+		h.InjectUser,
+		h.PluginReady,
+	}
+}
+
+func (h *RoleAssignHandler) GetPreprocessors() []router.Processor {
+	return h.preprocessors
+}
+
+func (h *RoleAssignHandler) Handle(rpayload *router.Payload, response *router.Response) {
+	log.Debugf("RoleAssignHandler %v", h)
+	payload := &roleBatchPayload{}
+	skyErr := payload.Decode(rpayload.Data)
+	if skyErr != nil {
+		response.Err = skyErr
+		return
+	}
+
+	userinfo := rpayload.UserInfo
+	adminRoles, err := rpayload.DBConn.GetAdminRoles()
+	if err != nil {
+		response.Err = skyerr.MakeError(err)
+		return
+	}
+	if !userinfo.HasAnyRoles(adminRoles) && !rpayload.HasMasterKey() {
+		response.Err = skyerr.NewError(skyerr.PermissionDenied, "no permission to modify other users")
+		return
+	}
+
+	err = rpayload.DBConn.AssignRoles(payload.UserIDs, payload.Roles)
+	if err != nil {
+		response.Err = skyerr.MakeError(err)
+	}
+	response.Result = struct {
+		Roles   []string `mapstructure:"roles"`
+		UserIDs []string `mapstructure:"users"`
+	}{
+		payload.Roles,
+		payload.UserIDs,
+	}
+}
