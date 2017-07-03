Steven-Chan/skygear-server

Replace _user to _auth, and related tables and columns
This is a half-manual global replace in source code, and this version
will not work since data structures are not updated.

pkg/server/skydb/pq/device.go
@@ -25,7 +25,7 @@ import (
 )
 
 func (c *conn) GetDevice(id string, device *skydb.Device) error {
-	builder := psql.Select("type", "token", "user_id", "topic", "last_registered_at").
+	builder := psql.Select("type", "token", "auth_id", "topic", "last_registered_at").
 		From(c.tableName("_device")).
 		Where("id = ?", id)
 
@@ -56,9 +56,9 @@ func (c *conn) GetDevice(id string, device *skydb.Device) error {
 }
 
 func (c *conn) QueryDevicesByUser(user string) ([]skydb.Device, error) {
-	builder := psql.Select("id", "type", "token", "user_id", "topic", "last_registered_at").
+	builder := psql.Select("id", "type", "token", "auth_id", "topic", "last_registered_at").
 		From(c.tableName("_device")).
-		Where("user_id = ?", user)
+		Where("auth_id = ?", user)
 
 	rows, err := c.QueryWith(builder)
 	if err != nil {
@@ -90,9 +90,9 @@ func (c *conn) QueryDevicesByUser(user string) ([]skydb.Device, error) {
 }
 
 func (c *conn) QueryDevicesByUserAndTopic(user, topic string) ([]skydb.Device, error) {
-	builder := psql.Select("id", "type", "token", "user_id", "topic", "last_registered_at").
+	builder := psql.Select("id", "type", "token", "auth_id", "topic", "last_registered_at").
 		From(c.tableName("_device")).
-		Where("user_id = ? AND topic = ?", user, topic)
+		Where("auth_id = ? AND topic = ?", user, topic)
 
 	rows, err := c.QueryWith(builder)
 	if err != nil {
@@ -129,12 +129,12 @@ func (c *conn) SaveDevice(device *skydb.Device) error {
 	pkData := map[string]interface{}{"id": device.ID}
 	data := map[string]interface{}{
 		"type":               device.Type,
-		"user_id":            nil,
+		"auth_id":            nil,
 		"last_registered_at": device.LastRegisteredAt.UTC(),
 	}
 
 	if device.UserInfoID != "" {
-		data["user_id"] = device.UserInfoID
+		data["auth_id"] = device.UserInfoID
 	}
 
 	if device.Token != "" {

pkg/server/skydb/pq/device_test.go
@@ -70,7 +70,7 @@ func TestDevice(t *testing.T) {
 				deviceType, token, topic, userInfoID string
 				lastRegisteredAt                     time.Time
 			)
-			err = c.QueryRowx("SELECT type, token, topic, user_id, last_registered_at FROM _device WHERE id = 'deviceid'").Scan(&deviceType, &token, &topic, &userInfoID, &lastRegisteredAt)
+			err = c.QueryRowx("SELECT type, token, topic, auth_id, last_registered_at FROM _device WHERE id = 'deviceid'").Scan(&deviceType, &token, &topic, &userInfoID, &lastRegisteredAt)
 			So(err, ShouldBeNil)
 			So(deviceType, ShouldEqual, "ios")
 			So(token, ShouldEqual, "devicetoken")
@@ -99,7 +99,7 @@ func TestDevice(t *testing.T) {
 				deviceType, token, topic, userInfoID string
 				lastRegisteredAt                     time.Time
 			)
-			err = c.QueryRowx("SELECT type, token, topic, user_id, last_registered_at FROM _device WHERE id = 'deviceid'").Scan(&deviceType, &token, &topic, &userInfoID, &lastRegisteredAt)
+			err = c.QueryRowx("SELECT type, token, topic, auth_id, last_registered_at FROM _device WHERE id = 'deviceid'").Scan(&deviceType, &token, &topic, &userInfoID, &lastRegisteredAt)
 			So(err, ShouldBeNil)
 			So(deviceType, ShouldEqual, "ios")
 			So(token, ShouldEqual, "anotherdevicetoken")

pkg/server/skydb/pq/role.go
@@ -27,7 +27,7 @@ import (
 )
 
 const batchUserRoleInsertTemplate = `
-INSERT INTO {{.UserRoleTable}} (user_id, role_id)
+INSERT INTO {{.UserRoleTable}} (auth_id, role_id)
 SELECT $1, id
 FROM {{.RoleTable}}
 WHERE id IN ({{range $i, $_ := .In}}{{inDollar $i}}{{end}});
@@ -53,7 +53,7 @@ type batchUserRole struct {
 func (c *conn) batchUserRoleSQL(id string, roles []string) (string, []interface{}) {
 	b := bytes.Buffer{}
 	batchUserRoleInsert.Execute(&b, batchUserRole{
-		c.tableName("_user_role"),
+		c.tableName("_auth_role"),
 		c.tableName("_role"),
 		roles,
 	})
@@ -249,7 +249,7 @@ func (c *conn) setRoleType(roles []string, col string) error {
 
 func (c *conn) UpdateUserRoles(userinfo *skydb.UserInfo) error {
 	log.Debugf("UpdateRoles %v", userinfo)
-	builder := psql.Delete(c.tableName("_user_role")).Where("user_id = ?", userinfo.ID)
+	builder := psql.Delete(c.tableName("_auth_role")).Where("auth_id = ?", userinfo.ID)
 	_, err := c.ExecWith(builder)
 	if err != nil {
 		return skyerr.NewError(skyerr.ConstraintViolated,

authgear/authgear-server

pkg/server/skydb/pq/device.go
@@ -25,7 +25,7 @@ import (
 )
 
 func (c *conn) GetDevice(id string, device *skydb.Device) error {
-	builder := psql.Select("type", "token", "user_id", "topic", "last_registered_at").
+	builder := psql.Select("type", "token", "auth_id", "topic", "last_registered_at").
 		From(c.tableName("_device")).
 		Where("id = ?", id)
 
@@ -56,9 +56,9 @@ func (c *conn) GetDevice(id string, device *skydb.Device) error {
 }
 
 func (c *conn) QueryDevicesByUser(user string) ([]skydb.Device, error) {
-	builder := psql.Select("id", "type", "token", "user_id", "topic", "last_registered_at").
+	builder := psql.Select("id", "type", "token", "auth_id", "topic", "last_registered_at").
 		From(c.tableName("_device")).
-		Where("user_id = ?", user)
+		Where("auth_id = ?", user)
 
 	rows, err := c.QueryWith(builder)
 	if err != nil {
@@ -90,9 +90,9 @@ func (c *conn) QueryDevicesByUser(user string) ([]skydb.Device, error) {
 }
 
 func (c *conn) QueryDevicesByUserAndTopic(user, topic string) ([]skydb.Device, error) {
-	builder := psql.Select("id", "type", "token", "user_id", "topic", "last_registered_at").
+	builder := psql.Select("id", "type", "token", "auth_id", "topic", "last_registered_at").
 		From(c.tableName("_device")).
-		Where("user_id = ? AND topic = ?", user, topic)
+		Where("auth_id = ? AND topic = ?", user, topic)
 
 	rows, err := c.QueryWith(builder)
 	if err != nil {
@@ -129,12 +129,12 @@ func (c *conn) SaveDevice(device *skydb.Device) error {
 	pkData := map[string]interface{}{"id": device.ID}
 	data := map[string]interface{}{
 		"type":               device.Type,
-		"user_id":            nil,
+		"auth_id":            nil,
 		"last_registered_at": device.LastRegisteredAt.UTC(),
 	}
 
 	if device.AuthInfoID != "" {
-		data["user_id"] = device.AuthInfoID
+		data["auth_id"] = device.AuthInfoID
 	}
 
 	if device.Token != "" {

pkg/server/skydb/pq/device_test.go
@@ -70,7 +70,7 @@ func TestDevice(t *testing.T) {
 				deviceType, token, topic, authInfoID string
 				lastRegisteredAt                     time.Time
 			)
-			err = c.QueryRowx("SELECT type, token, topic, user_id, last_registered_at FROM _device WHERE id = 'deviceid'").Scan(&deviceType, &token, &topic, &authInfoID, &lastRegisteredAt)
+			err = c.QueryRowx("SELECT type, token, topic, auth_id, last_registered_at FROM _device WHERE id = 'deviceid'").Scan(&deviceType, &token, &topic, &authInfoID, &lastRegisteredAt)
 			So(err, ShouldBeNil)
 			So(deviceType, ShouldEqual, "ios")
 			So(token, ShouldEqual, "devicetoken")
@@ -99,7 +99,7 @@ func TestDevice(t *testing.T) {
 				deviceType, token, topic, authInfoID string
 				lastRegisteredAt                     time.Time
 			)
-			err = c.QueryRowx("SELECT type, token, topic, user_id, last_registered_at FROM _device WHERE id = 'deviceid'").Scan(&deviceType, &token, &topic, &authInfoID, &lastRegisteredAt)
+			err = c.QueryRowx("SELECT type, token, topic, auth_id, last_registered_at FROM _device WHERE id = 'deviceid'").Scan(&deviceType, &token, &topic, &authInfoID, &lastRegisteredAt)
 			So(err, ShouldBeNil)
 			So(deviceType, ShouldEqual, "ios")
 			So(token, ShouldEqual, "anotherdevicetoken")

pkg/server/skydb/pq/role.go
@@ -27,7 +27,7 @@ import (
 )
 
 const batchUserRoleInsertTemplate = `
-INSERT INTO {{.UserRoleTable}} (user_id, role_id)
+INSERT INTO {{.UserRoleTable}} (auth_id, role_id)
 SELECT $1, id
 FROM {{.RoleTable}}
 WHERE id IN ({{range $i, $_ := .In}}{{inDollar $i}}{{end}});
@@ -53,7 +53,7 @@ type batchUserRole struct {
 func (c *conn) batchUserRoleSQL(id string, roles []string) (string, []interface{}) {
 	b := bytes.Buffer{}
 	batchUserRoleInsert.Execute(&b, batchUserRole{
-		c.tableName("_user_role"),
+		c.tableName("_auth_role"),
 		c.tableName("_role"),
 		roles,
 	})
@@ -249,7 +249,7 @@ func (c *conn) setRoleType(roles []string, col string) error {
 
 func (c *conn) UpdateUserRoles(authinfo *skydb.AuthInfo) error {
 	log.Debugf("UpdateRoles %v", authinfo)
-	builder := psql.Delete(c.tableName("_user_role")).Where("user_id = ?", authinfo.ID)
+	builder := psql.Delete(c.tableName("_auth_role")).Where("auth_id = ?", authinfo.ID)
 	_, err := c.ExecWith(builder)
 	if err != nil {
 		return skyerr.NewError(skyerr.ConstraintViolated,