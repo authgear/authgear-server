f8a23cf26f68137bc679961c2680c2b70fdbd619
Rick Mak <rick.mak@gmail.com>
2017-07-19 19:16:37 +0800

    Add fsStore test on signed URL and validation
    refs #427

---
+++ pkg/server/asset/fs_test.go
@@ -0,0 +1,62 @@
+package asset
+
+import (
+	"net/url"
+	"strconv"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFileStore(t *testing.T) {
+
+	Convey("FS Asset Store", t, func() {
+		fsStore := &fileStore{
+			"data/asset",
+			"http://skygear.dev/files",
+			"asset_secret",
+			false,
+		}
+		Convey("Sign the Parse Signature correctly", func() {
+			s, err := fsStore.SignedURL("index.html")
+			So(err, ShouldBeNil)
+			parsedURL, urlErr := url.Parse(s)
+			So(urlErr, ShouldBeNil)
+			qs := parsedURL.Query()
+			expiredAtUnix, expiredErr := strconv.ParseInt(qs.Get("expiredAt"), 10, 64)
+			So(expiredErr, ShouldBeNil)
+			expiredAt := time.Unix(expiredAtUnix, 0)
+			valid, matchErr := fsStore.ParseSignature(
+				qs.Get("signature"),
+				"index.html",
+				expiredAt,
+			)
+			So(matchErr, ShouldBeNil)
+			So(valid, ShouldBeTrue)
+		})
+
+		Convey("Parse Signature correctly", func() {
+			expiredAt := time.Unix(1481096834, 0)
+			valid, matchErr := fsStore.ParseSignature(
+				"R5kMq2neUkCGBjQD6zSv99PajRvI0EqMesuRHQS4hNA=",
+				"index.html",
+				expiredAt,
+			)
+			So(matchErr, ShouldBeNil)
+			So(valid, ShouldBeTrue)
+		})
+
+		Convey("Reject incorrectly Signature correctly", func() {
+			expiredAt := time.Unix(1481096834, 0)
+			valid, matchErr := fsStore.ParseSignature(
+				"limouren",
+				"index.html",
+				expiredAt,
+			)
+			So(matchErr, ShouldBeNil)
+			So(valid, ShouldBeFalse)
+		})
+
+	})
+}
