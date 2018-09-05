c48f1ec3b0c24a88a4d4a38b3051fcde1e8838fd
Steven Chan <stevenchan@oursky.com>
2018-09-05 16:26:04 +0800

    Check for token validity in authz policy

--- pkg/core/auth/authz/policy.go
+++ pkg/core/auth/authz/policy.go
@@ -2,6 +2,7 @@ package authz
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/skygeario/skygear-server/pkg/server/skyerr"
 
@@ -76,6 +77,21 @@ func RequireMasterKey(r *http.Request, ctx handler.AuthenticationContext) error 
 func RequireAuthenticated(r *http.Request, ctx handler.AuthenticationContext) error {
 	if ctx.AuthInfo == nil {
 		return skyerr.NewError(skyerr.NotAuthenticated, "require authenticated user")
+	}
+
+	if ctx.AuthInfo.TokenValidSince != nil {
+		tokenValidSince := *ctx.AuthInfo.TokenValidSince
+
+		// Not all types of access token support this field. The token is
+		// still considered if it does not have an issue time.
+		//
+		// Due to precision, the issue time of the token can be before
+		// AuthInfo.TokenValidSince. We consider the token still valid
+		// if the token is issued within 1 second before tokenValidSince.
+		if !ctx.Token.IssuedAt().IsZero() &&
+			ctx.Token.IssuedAt().After(tokenValidSince.Add(-1*time.Second)) {
+			return skyerr.NewError(skyerr.NotAuthenticated, "require authenticated user")
+		}
 	}
 
 	return nil
