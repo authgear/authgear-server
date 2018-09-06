a65ab2a0d8fbe5feb3a50037e83f11a689140a00
Steven Chan <stevenchan@oursky.com>
2018-09-06 15:35:37 +0800

    Handle master key in AuthInfoResolver

--- pkg/core/auth/authn/authn.go
+++ pkg/core/auth/authn/authn.go
@@ -8,6 +8,7 @@ import (
 	"github.com/skygeario/skygear-server/pkg/core/config"
 	"github.com/skygeario/skygear-server/pkg/core/handler"
 	"github.com/skygeario/skygear-server/pkg/core/inject"
+	"github.com/skygeario/skygear-server/pkg/gateway/model"
 	"github.com/skygeario/skygear-server/pkg/server/skydb"
 
 	"github.com/skygeario/skygear-server/pkg/server/authtoken"
@@ -37,6 +38,62 @@ type StatefulJWTAuthInfoResolver struct {
 }
 
 func (r StatefulJWTAuthInfoResolver) Resolve(req *http.Request) (authInfo auth.AuthInfo, err error) {
+	keyType := model.GetAccessKeyType(req)
+
+	var resolver AuthInfoResolver
+	if keyType == model.MasterAccessKey {
+		resolver = masterkeyAuthInfoResolver{
+			TokenStore:    r.TokenStore,
+			AuthInfoStore: r.AuthInfoStore,
+		}
+	} else {
+		resolver = nonMasterkeyAuthInfoResolver{
+			TokenStore:    r.TokenStore,
+			AuthInfoStore: r.AuthInfoStore,
+		}
+	}
+
+	authInfo, err = resolver.Resolve(req)
+	authInfo.AccessKeyType = keyType
+
+	return
+}
+
+type masterkeyAuthInfoResolver struct {
+	auth.TokenStore    `dependency:"TokenStore"`
+	auth.AuthInfoStore `dependency:"AuthInfoStore"`
+}
+
+func (r masterkeyAuthInfoResolver) Resolve(req *http.Request) (authInfo auth.AuthInfo, err error) {
+	tokenStr := auth.GetAccessToken(req)
+	token := &authtoken.Token{}
+	r.TokenStore.Get(tokenStr, token)
+
+	if token.AuthInfoID == "" {
+		token.AuthInfoID = "_god"
+	}
+
+	info := &skydb.AuthInfo{}
+	if err = r.AuthInfoStore.GetAuth(token.AuthInfoID, info); err == skydb.ErrUserNotFound {
+		info.ID = token.AuthInfoID
+
+		if err = r.AuthInfoStore.CreateAuth(info); err == skydb.ErrUserDuplicated {
+			// user already exists, error can be ignored
+			err = nil
+		}
+	}
+
+	authInfo.AuthInfo = info
+
+	return
+}
+
+type nonMasterkeyAuthInfoResolver struct {
+	auth.TokenStore    `dependency:"TokenStore"`
+	auth.AuthInfoStore `dependency:"AuthInfoStore"`
+}
+
+func (r nonMasterkeyAuthInfoResolver) Resolve(req *http.Request) (authInfo auth.AuthInfo, err error) {
 	tokenStr := auth.GetAccessToken(req)
 
 	token := &authtoken.Token{}
