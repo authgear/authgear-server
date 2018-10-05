987a45c06557e520ae011acac4796762fe0dad16
Steven Chan <stevenchan@oursky.com>
2018-10-05 15:00:55 +0800

    Update api handler error status code

--- pkg/core/handler/api.go
+++ pkg/core/handler/api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/sirupsen/logrus"
 	"github.com/skygeario/skygear-server/pkg/core/handler/context"
 	"github.com/skygeario/skygear-server/pkg/server/skyerr"
 )
@@ -20,11 +21,17 @@ type APIResponse struct {
 
 func APIHandlerToHandler(apiHandler APIHandler) Handler {
 	return HandlerFunc(func(rw http.ResponseWriter, r *http.Request, ctx context.AuthContext) {
+		httpStatus := http.StatusOK
 		response := APIResponse{}
 		encoder := json.NewEncoder(rw)
 
 		defer func() {
+			if response.Err != nil {
+				httpStatus = defaultStatusCode(response.Err)
+			}
+
 			rw.Header().Set("Content-Type", "application/json")
+			rw.WriteHeader(httpStatus)
 			encoder.Encode(response)
 		}()
 
@@ -46,4 +53,42 @@ func APIHandlerToHandler(apiHandler APIHandler) Handler {
 			response.Err = skyerr.MakeError(err)
 		}
 	})
+}
+
+func defaultStatusCode(err skyerr.Error) int {
+	httpStatus, ok := map[skyerr.ErrorCode]int{
+		skyerr.NotAuthenticated:        http.StatusUnauthorized,
+		skyerr.PermissionDenied:        http.StatusForbidden,
+		skyerr.AccessKeyNotAccepted:    http.StatusUnauthorized,
+		skyerr.AccessTokenNotAccepted:  http.StatusUnauthorized,
+		skyerr.InvalidCredentials:      http.StatusUnauthorized,
+		skyerr.InvalidSignature:        http.StatusUnauthorized,
+		skyerr.BadRequest:              http.StatusBadRequest,
+		skyerr.InvalidArgument:         http.StatusBadRequest,
+		skyerr.IncompatibleSchema:      http.StatusConflict,
+		skyerr.AtomicOperationFailure:  http.StatusConflict,
+		skyerr.PartialOperationFailure: http.StatusOK,
+		skyerr.Duplicated:              http.StatusConflict,
+		skyerr.ConstraintViolated:      http.StatusConflict,
+		skyerr.ResourceNotFound:        http.StatusNotFound,
+		skyerr.UndefinedOperation:      http.StatusNotFound,
+		skyerr.NotSupported:            http.StatusNotImplemented,
+		skyerr.NotImplemented:          http.StatusNotImplemented,
+		skyerr.PluginUnavailable:       http.StatusServiceUnavailable,
+		skyerr.PluginTimeout:           http.StatusGatewayTimeout,
+		skyerr.RecordQueryInvalid:      http.StatusBadRequest,
+		skyerr.ResponseTimeout:         http.StatusServiceUnavailable,
+		skyerr.DeniedArgument:          http.StatusForbidden,
+		skyerr.RecordQueryDenied:       http.StatusForbidden,
+		skyerr.NotConfigured:           http.StatusServiceUnavailable,
+		skyerr.UserDisabled:            http.StatusForbidden,
+		skyerr.VerificationRequired:    http.StatusForbidden,
+	}[err.Code()]
+	if !ok {
+		if err.Code() < 10000 {
+			logrus.Warnf("Error code %d (%v) does not have a default status code set. Assumed 500.", err.Code(), err.Code())
+		}
+		httpStatus = http.StatusInternalServerError
+	}
+	return httpStatus
 }
