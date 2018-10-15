ec22455ff7ab6c415c71a5697110e8e24d340f52
Steven Chan <stevenchan@oursky.com>
2018-10-15 12:14:53 +0800

    Update me to update token and user last_seen_at

--- pkg/auth/handler/me.go
+++ pkg/auth/handler/me.go
@@ -4,13 +4,18 @@ import (
 	"net/http"
 
 	"github.com/skygeario/skygear-server/pkg/auth"
+	"github.com/skygeario/skygear-server/pkg/auth/response"
 	coreAuth "github.com/skygeario/skygear-server/pkg/core/auth"
+	"github.com/skygeario/skygear-server/pkg/core/auth/authinfo"
+	"github.com/skygeario/skygear-server/pkg/core/auth/authtoken"
 	"github.com/skygeario/skygear-server/pkg/core/auth/authz"
 	"github.com/skygeario/skygear-server/pkg/core/auth/authz/policy"
 	"github.com/skygeario/skygear-server/pkg/core/db"
 	"github.com/skygeario/skygear-server/pkg/core/handler"
 	"github.com/skygeario/skygear-server/pkg/core/inject"
 	"github.com/skygeario/skygear-server/pkg/core/server"
+	"github.com/skygeario/skygear-server/pkg/server/skydb"
+	"github.com/skygeario/skygear-server/pkg/server/skyerr"
 )
 
 func AttachMeHandler(
@@ -43,8 +48,10 @@ func (f MeHandlerFactory) ProvideAuthzPolicy() authz.Policy {
 
 // MeHandler handles me request
 type MeHandler struct {
-	AuthContext coreAuth.ContextGetter `dependency:"AuthContextGetter"`
-	TxContext   db.TxContext           `dependency:"TxContext"`
+	AuthContext   coreAuth.ContextGetter `dependency:"AuthContextGetter"`
+	TxContext     db.TxContext           `dependency:"TxContext"`
+	TokenStore    authtoken.Store        `dependency:"TokenStore"`
+	AuthInfoStore authinfo.Store         `dependency:"AuthInfoStore"`
 }
 
 func (h MeHandler) WithTx() bool {
@@ -57,6 +64,26 @@ func (h MeHandler) DecodeRequest(request *http.Request) (payload handler.Request
 }
 
 func (h MeHandler) Handle(req interface{}) (resp interface{}, err error) {
-	resp = h.AuthContext.AuthInfo()
+	authInfo := h.AuthContext.AuthInfo()
+
+	// refresh access token with a newly generated one
+	token, err := h.TokenStore.NewToken(authInfo.ID)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = h.TokenStore.Put(&token); err != nil {
+		panic(err)
+	}
+
+	resp = response.NewAuthResponse(*authInfo, skydb.Record{}, token.AccessToken)
+
+	// Populate the activity time to user
+	now := timeNow()
+	authInfo.LastSeenAt = &now
+	if err := h.AuthInfoStore.UpdateAuth(authInfo); err != nil {
+		err = skyerr.MakeError(err)
+	}
+
 	return
 }
