cd5360f462761d51652f57c42de825a849c29d92
Carmen <carmen@oursky.com>
2018-11-01 16:35:54 +0800

    Add decide schema create request payload

--- pkg/record/handler/schema_create.go
+++ pkg/record/handler/schema_create.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/skygeario/skygear-server/pkg/core/auth/authz"
 	"github.com/skygeario/skygear-server/pkg/core/auth/authz/policy"
@@ -11,6 +12,8 @@ import (
 	"github.com/skygeario/skygear-server/pkg/core/inject"
 	"github.com/skygeario/skygear-server/pkg/core/server"
 	recordGear "github.com/skygeario/skygear-server/pkg/record"
+	"github.com/skygeario/skygear-server/pkg/record/dependency/record"
+	"github.com/skygeario/skygear-server/pkg/server/skyerr"
 )
 
 func AttachSchemaCreateHandler(
@@ -42,9 +45,21 @@ func (f SchemaCreateHandlerFactory) ProvideAuthzPolicy() authz.Policy {
 }
 
 type SchemaCreateRequestPayload struct {
+	RawSchemas map[string]schemaFieldList `json:"record_types"`
+	Schemas    map[string]record.Schema
 }
 
 func (s SchemaCreateRequestPayload) Validate() error {
+	for recordType, schema := range s.Schemas {
+		if strings.HasPrefix(recordType, "_") {
+			return skyerr.NewInvalidArgument("attempts to create reserved table", []string{recordType})
+		}
+		for fieldName := range schema {
+			if strings.HasPrefix(fieldName, "_") {
+				return skyerr.NewInvalidArgument("attempts to create reserved field", []string{fieldName})
+			}
+		}
+	}
 	return nil
 }
 
@@ -57,7 +72,7 @@ curl -X POST -H "Content-Type: application/json" \
 		"student": {
 			"fields":[
 				{"name": "age", "type": "number"},
-				{"name": "nickname" "type": "string"}
+				{"name": "nickname", "type": "string"}
 			]
 		}
 	}
@@ -76,6 +91,18 @@ func (h SchemaCreateHandler) DecodeRequest(request *http.Request) (handler.Reque
 	payload := SchemaCreateRequestPayload{}
 	if err := json.NewDecoder(request.Body).Decode(&payload); err != nil {
 		return nil, err
+	}
+
+	payload.Schemas = make(map[string]record.Schema)
+	for recordType, schema := range payload.RawSchemas {
+		payload.Schemas[recordType] = make(record.Schema)
+		for _, field := range schema.Fields {
+			var err error
+			payload.Schemas[recordType][field.Name], err = record.SimpleNameToFieldType(field.TypeName)
+			if err != nil {
+				return nil, skyerr.NewInvalidArgument("unexpected field type", []string{field.TypeName})
+			}
+		}
 	}
 
 	return payload, nil
