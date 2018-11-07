44745e3c5a0a49de9804b88c3c60c52e22c4792e
Steven Chan <stevenchan@oursky.com>
2018-11-07 15:35:16 +0800

    Add test for default access handler

---
+++ pkg/record/handler/schema_default_access_test.go
@@ -0,0 +1,77 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/skygeario/skygear-server/pkg/core/auth/authinfo"
+	"github.com/skygeario/skygear-server/pkg/core/db"
+	"github.com/skygeario/skygear-server/pkg/core/handler"
+	"github.com/skygeario/skygear-server/pkg/core/logging"
+	"github.com/skygeario/skygear-server/pkg/record/dependency/record"
+	. "github.com/skygeario/skygear-server/pkg/server/skytest"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSchemaDefaultAccessHandler(t *testing.T) {
+	getRecordStore := func() (recordStore *mockSchemaDefaultAccessRecordStore) {
+		return &mockSchemaDefaultAccessRecordStore{
+			Store: record.NewMockStore(),
+		}
+	}
+
+	Convey("Test SchemaDefaultAccessHandler", t, func() {
+		// fixture
+		sh := &SchemaDefaultAccessHandler{}
+		recordStore := getRecordStore()
+		sh.RecordStore = recordStore
+		sh.Logger = logging.LoggerEntry("handler")
+		sh.TxContext = db.NewMockTxContext()
+
+		h := handler.APIHandlerToHandler(sh, sh.TxContext)
+
+		Convey("set default access", func() {
+			req, _ := http.NewRequest("POST", "", strings.NewReader(`{
+				"type": "script",
+				"default_access": [
+					{"public": true, "level": "read"},
+					{"role": "admin", "level": "write"}
+				]
+			}`))
+			resp := httptest.NewRecorder()
+			h.ServeHTTP(resp, req)
+			So(resp.Body.Bytes(), ShouldEqualJSON, `{
+				"result": {
+					"type": "script",
+					"default_access": [
+						{"public": true, "level": "read"},
+						{"role": "admin", "level": "write"}
+					]
+				}
+			}`)
+
+			So(recordStore.recordType, ShouldEqual, "script")
+
+			admin := authinfo.AuthInfo{
+				Roles: []string{"admin"},
+			}
+			So(recordStore.acl.Accessible(&admin, record.WriteLevel), ShouldEqual, true)
+			So(recordStore.acl.Accessible(nil, record.ReadLevel), ShouldEqual, true)
+		})
+	})
+}
+
+type mockSchemaDefaultAccessRecordStore struct {
+	recordType string
+	acl        record.ACL
+
+	record.Store
+}
+
+func (s *mockSchemaDefaultAccessRecordStore) SetRecordDefaultAccess(recordType string, acl record.ACL) error {
+	s.recordType = recordType
+	s.acl = acl
+	return s.Store.SetRecordDefaultAccess(recordType, acl)
+}
