b989c81f09595162041d76f9153b67be8aa06544
Steven Chan <stevenchan@oursky.com>
2018-11-12 18:25:38 +0800

    Add mail sending function in core

---
+++ pkg/core/mail/send.go
@@ -0,0 +1,97 @@
+package mail
+
+import (
+	"errors"
+
+	"github.com/go-gomail/gomail"
+)
+
+type SendRequest struct {
+	Dialer      *gomail.Dialer
+	Sender      string
+	SenderName  string
+	Recipient   string
+	Subject     string
+	ReplyTo     string
+	ReplyToName string
+	TextBody    string
+	HTMLBody    string
+}
+
+func (r *SendRequest) Execute() error {
+	message := gomail.NewMessage()
+
+	r.applyFrom(message)
+	r.applyTo(message)
+	r.applyReplyTo(message)
+	r.applySubject(message)
+
+	r.applyTextBody(message)
+	r.applyHTMLBody(message)
+
+	return r.Dialer.DialAndSend(message)
+}
+
+func (r *SendRequest) applyFrom(message *gomail.Message) error {
+	if r.Sender == "" {
+		return errors.New("sender address is missing")
+	}
+
+	if r.SenderName == "" {
+		message.SetHeader("From", r.Sender)
+	} else {
+		message.SetAddressHeader("From", r.Sender, r.SenderName)
+	}
+
+	return nil
+}
+
+func (r *SendRequest) applyTo(message *gomail.Message) error {
+	if r.Recipient == "" {
+		return errors.New("recipient address is missing")
+	}
+
+	message.SetHeader("To", r.Recipient)
+	return nil
+}
+
+func (r *SendRequest) applyReplyTo(message *gomail.Message) error {
+	if r.ReplyTo == "" {
+		return nil
+	}
+
+	if r.SenderName == "" {
+		message.SetHeader("Reply-To", r.ReplyTo)
+	} else {
+		message.SetAddressHeader("Reply-To", r.ReplyTo, r.ReplyToName)
+	}
+
+	return nil
+}
+
+func (r *SendRequest) applySubject(message *gomail.Message) error {
+	if r.Subject == "" {
+		return errors.New("subject is missing")
+	}
+
+	message.SetHeader("Subject", r.Subject)
+	return nil
+}
+
+func (r *SendRequest) applyTextBody(message *gomail.Message) error {
+	if r.TextBody == "" {
+		return errors.New("text body is missing")
+	}
+
+	message.SetBody("text/plain", r.TextBody)
+	return nil
+}
+
+func (r *SendRequest) applyHTMLBody(message *gomail.Message) error {
+	if r.HTMLBody == "" {
+		return nil
+	}
+
+	message.AddAlternative("text/html", r.HTMLBody)
+	return nil
+}
