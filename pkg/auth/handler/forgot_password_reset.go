aa9be3dabc7a87eba67e1c3c9866cf9a31955f60
Steven Chan <stevenchan@oursky.com>
2018-12-04 15:59:49 +0800

    Implement forgot password reset password handler

--- pkg/auth/handler/forgot_password_reset.go
+++ pkg/auth/handler/forgot_password_reset.go
@@ -3,8 +3,18 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/skygeario/skygear-server/pkg/auth/dependency/forgotpwdemail"
+	"github.com/skygeario/skygear-server/pkg/auth/dependency/userprofile"
 
 	"github.com/skygeario/skygear-server/pkg/auth"
+	"github.com/skygeario/skygear-server/pkg/auth/dependency"
+	"github.com/skygeario/skygear-server/pkg/auth/dependency/provider/password"
+	"github.com/skygeario/skygear-server/pkg/core/audit"
+	"github.com/skygeario/skygear-server/pkg/core/auth/authinfo"
+	"github.com/skygeario/skygear-server/pkg/core/auth/authtoken"
 	"github.com/skygeario/skygear-server/pkg/core/auth/authz"
 	"github.com/skygeario/skygear-server/pkg/core/auth/authz/policy"
 	"github.com/skygeario/skygear-server/pkg/core/db"
@@ -43,9 +53,30 @@ func (f ForgotPasswordResetHandlerFactory) ProvideAuthzPolicy() authz.Policy {
 }
 
 type ForgotPasswordResetPayload struct {
+	UserID       string `json:"user_id"`
+	Code         string `json:"code"`
+	ExpireAt     int64  `json:"expire_at"`
+	ExpireAtTime time.Time
+	NewPassword  string `json:"new_password"`
 }
 
 func (payload ForgotPasswordResetPayload) Validate() error {
+	if payload.UserID == "" {
+		return skyerr.NewInvalidArgument("empty user_id", []string{"user_id"})
+	}
+
+	if payload.Code == "" {
+		return skyerr.NewInvalidArgument("empty code", []string{"code"})
+	}
+
+	if payload.ExpireAt == 0 {
+		return skyerr.NewInvalidArgument("empty expire_at", []string{"expire_at"})
+	}
+
+	if payload.NewPassword == "" {
+		return skyerr.NewInvalidArgument("empty password", []string{"new_password"})
+	}
+
 	return nil
 }
 
@@ -61,7 +92,14 @@ func (payload ForgotPasswordResetPayload) Validate() error {
 //  }
 //  EOF
 type ForgotPasswordResetHandler struct {
-	TxContext db.TxContext `dependency:"TxContext"`
+	CodeGenerator        *forgotpwdemail.CodeGenerator `dependency:"ForgotPasswordCodeGenerator"`
+	PasswordChecker      dependency.PasswordChecker    `dependency:"PasswordChecker"`
+	TokenStore           authtoken.Store               `dependency:"TokenStore"`
+	AuthInfoStore        authinfo.Store                `dependency:"AuthInfoStore"`
+	PasswordAuthProvider password.Provider             `dependency:"PasswordAuthProvider"`
+	UserProfileStore     userprofile.Store             `dependency:"UserProfileStore"`
+	TxContext            db.TxContext                  `dependency:"TxContext"`
+	Logger               *logrus.Entry                 `dependency:"HandlerLogger"`
 }
 
 func (h ForgotPasswordResetHandler) WithTx() bool {
@@ -75,9 +113,91 @@ func (h ForgotPasswordResetHandler) DecodeRequest(request *http.Request) (handle
 		return nil, skyerr.NewError(skyerr.BadRequest, "fails to decode the request payload")
 	}
 
+	payload.ExpireAtTime = time.Unix(payload.ExpireAt, 0).UTC()
+
 	return payload, nil
 }
 
 func (h ForgotPasswordResetHandler) Handle(req interface{}) (resp interface{}, err error) {
+	payload := req.(ForgotPasswordResetPayload)
+
+	// check code expiration
+	if time.Now().UTC().After(payload.ExpireAtTime) {
+		h.Logger.Error("forgot password code expired")
+		err = h.genericError()
+		return
+	}
+
+	authInfo := authinfo.AuthInfo{}
+	if e := h.AuthInfoStore.GetAuth(payload.UserID, &authInfo); e != nil {
+		h.Logger.WithFields(map[string]interface{}{
+			"user_id": payload.UserID,
+		}).WithError(e).Error("user not found")
+		err = h.genericError()
+		return
+	}
+
+	// generate access-token
+	token, err := h.TokenStore.NewToken(authInfo.ID)
+	if err != nil {
+		return
+	}
+
+	// Get Profile
+	var userProfile userprofile.UserProfile
+	if userProfile, err = h.UserProfileStore.GetUserProfile(authInfo.ID, token.AccessToken); err != nil {
+		h.Logger.WithFields(map[string]interface{}{
+			"user_id": payload.UserID,
+		}).WithError(err).Error("unable to get user profile")
+		err = h.genericError()
+		return
+	}
+
+	// Get password auth principals
+	principals, err := h.PasswordAuthProvider.GetPrincipalsByUserID(authInfo.ID)
+	if err != nil {
+		h.Logger.WithFields(map[string]interface{}{
+			"user_id": payload.UserID,
+		}).WithError(err).Error("unable to get password auth principals")
+		err = h.genericError()
+		return
+	}
+
+	hashedPassword := principals[0].HashedPassword
+	expectedCode := h.CodeGenerator.Generate(authInfo, userProfile, hashedPassword, payload.ExpireAtTime)
+	if payload.Code != expectedCode {
+		h.Logger.WithFields(map[string]interface{}{
+			"user_id":       payload.UserID,
+			"code":          payload.Code,
+			"expected_code": expectedCode,
+		}).Error("wrong forgot password reset password code")
+		err = h.genericError()
+		return
+	}
+
+	if err = h.PasswordChecker.ValidatePassword(audit.ValidatePasswordPayload{
+		PlainPassword: payload.NewPassword,
+	}); err != nil {
+		return
+	}
+
+	// reset password
+	for _, p := range principals {
+		p.PlainPassword = payload.NewPassword
+		err = h.PasswordAuthProvider.UpdatePrincipal(*p)
+		if err != nil {
+			return
+		}
+	}
+
+	if err = h.TokenStore.Put(&token); err != nil {
+		return
+	}
+
+	resp = "OK"
 	return
+}
+
+func (h ForgotPasswordResetHandler) genericError() error {
+	return skyerr.NewError(skyerr.ResourceNotFound, "user not found or code invalid")
 }
