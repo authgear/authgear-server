0b3b55bc7e08cc93eb55074199502fcc722244c6
Carmen <carmen@oursky.com>
2018-12-04 15:31:08 +0800

    Add SSO iframe handler

---
+++ pkg/auth/handler/ssohandler/iframe_handler_test.go
@@ -0,0 +1,45 @@
+package ssohandler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"regexp"
+	"testing"
+
+	"github.com/skygeario/skygear-server/pkg/auth/dependency/sso"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestIFrameHandler(t *testing.T) {
+	Convey("Test IFrameHandler", t, func() {
+		ih := &IFrameHandler{}
+		ih.IFrameHTMLProvider = sso.NewIFrameHTMLProvider(
+			"https://api.example.com",
+			"https://api.example.com/skygear.js",
+		)
+
+		Convey("should use provided js sdk", func() {
+			req, _ := http.NewRequest("GET", "", nil)
+			resp := httptest.NewRecorder()
+			ih.ServeHTTP(resp, req)
+
+			JSSKDURLPattern := `<script type="text/javascript" src="https://api.example.com/skygear.js"></script>`
+			matched, err := regexp.MatchString(JSSKDURLPattern, resp.Body.String())
+
+			So(err, ShouldBeNil)
+			So(matched, ShouldBeTrue)
+		})
+
+		Convey("should use provided endpoint", func() {
+			req, _ := http.NewRequest("GET", "", nil)
+			resp := httptest.NewRecorder()
+			ih.ServeHTTP(resp, req)
+
+			apiEndpointPattern := `'endPoint': 'https://api.example.com'`
+			matched, err := regexp.MatchString(apiEndpointPattern, resp.Body.String())
+
+			So(err, ShouldBeNil)
+			So(matched, ShouldBeTrue)
+		})
+	})
+}
