1211eca0d2c381d7a170750cbf4a0b0030c3604d
Johnny Kao <kaojohnny@gmail.com>
2019-02-22 12:28:57 +0800

    Remove unnecessarily mockgen

--- pkg/core/skydb/conn.go
+++ pkg/core/skydb/conn.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate mockgen -package skydb -source=conn.go -destination=mockgen_conn_test.go
-
 package skydb
 
 import (
