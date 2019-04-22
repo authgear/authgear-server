7ee4b59860e3280122caf32d6d0c9822cf4ddbf6
Johnny Kao <kaojohnny@gmail.com>
2019-04-22 11:52:09 +0800

    Add test case about multiple hooks to update metadata

--- pkg/auth/handler/signup_test.go
+++ pkg/auth/handler/signup_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -690,6 +691,109 @@ func TestSingupHandler(t *testing.T) {
 						"username":"john.doe"
 					},
 					"metadata": {}
+				}
+			}`,
+				userID,
+				token.AccessToken,
+				userID,
+				userID))
+		})
+
+		Convey("should invoke multiple before signup hook", func(c C) {
+			server1 := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				decoder := json.NewDecoder(req.Body)
+				var payload hook.AuthPayload
+				err := decoder.Decode(&payload)
+				if err != nil {
+					panic(err)
+				}
+
+				user := payload.Data
+				metadata := user["metadata"].(map[string]interface{})
+				metadata["email"] = "john.doe@example.com"
+
+				body, err := json.Marshal(user)
+				if err != nil {
+					panic(err)
+				}
+
+				rw.WriteHeader(http.StatusOK)
+				rw.Write(body)
+			}))
+			defer server1.Close()
+
+			server2 := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				decoder := json.NewDecoder(req.Body)
+				var payload hook.AuthPayload
+				err := decoder.Decode(&payload)
+				if err != nil {
+					panic(err)
+				}
+
+				user := payload.Data
+				metadata := user["metadata"].(map[string]interface{})
+				metadata["name"] = "john.doe"
+
+				body, err := json.Marshal(user)
+				if err != nil {
+					panic(err)
+				}
+
+				rw.WriteHeader(http.StatusOK)
+				rw.Write(body)
+			}))
+			defer server2.Close()
+
+			executor := hook.ExecutorImpl{}
+			hooks := []config.Hook{
+				config.Hook{
+					Event: hook.BeforeSignup,
+					URL:   server1.URL,
+				},
+				config.Hook{
+					Event: hook.BeforeSignup,
+					URL:   server2.URL,
+				},
+			}
+			sh.HookStore = hook.NewHookProvider(hooks, executor, logrus.NewEntry(logrus.New()), requestID)
+			h := auth.HookHandlerToHandler(sh, sh.TxContext)
+
+			req, _ := http.NewRequest("POST", "", strings.NewReader(`
+			{
+				"login_ids": {
+					"email": "john.doe@example.com",
+					"username": "john.doe"
+				},
+				"password": "123456"
+			}`))
+			resp := httptest.NewRecorder()
+			h.ServeHTTP(resp, req)
+
+			So(resp.Code, ShouldEqual, 200)
+
+			var p password.Principal
+			err := sh.PasswordAuthProvider.GetPrincipalByLoginID("email", "john.doe@example.com", &p)
+			So(err, ShouldBeNil)
+			userID := p.UserID
+			token := mockTokenStore.GetTokensByAuthInfoID(userID)[0]
+			So(resp.Body.Bytes(), ShouldEqualJSON, fmt.Sprintf(`{
+				"result": {
+					"user_id": "%s",
+					"access_token": "%s",
+					"verified": false,
+					"verify_info": {},
+					"created_at": "0001-01-01T00:00:00Z",
+					"created_by": "%s",
+					"updated_at": "0001-01-01T00:00:00Z",
+					"updated_by": "%s",
+					"login_ids": {
+						"email":"john.doe@example.com",
+						"username":"john.doe"
+					},
+					"metadata": {
+						"name": "john.doe",
+						"email": "john.doe@example.com"
+					}
 				}
 			}`,
 				userID,
