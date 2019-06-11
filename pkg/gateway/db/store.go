2e5e44e07ac50518de4f1510b4a49bc6d57a440e
Carmen <carmen@oursky.com>
2019-06-11 20:01:48 +0800

    Remove not used cloud code

--- pkg/gateway/db/store.go
+++ pkg/gateway/db/store.go
@@ -10,10 +10,6 @@ type GatewayStore interface {
 	// GetAppByDomain fetches the App with domain
 	GetAppByDomain(domain string, app *model.App) error
 
-	// FindLongestMatchedCloudCode find the longest matched cloud code by the
-	// given path
-	FindLongestMatchedCloudCode(path string, app model.App, cloudCode *model.CloudCode) error
-
 	// GetLastDeploymentRoutes return all routes of last deployment
 	GetLastDeploymentRoutes(app model.App) ([]*model.DeploymentRoute, error)
 
