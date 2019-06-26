bfc89013fa3b3748fd1a4469af7e04b87e6a3f03
kiootic <kiootic@oursky.com>
2019-06-26 16:11:10 +0800

    Add raw login ID list parser

--- pkg/auth/dependency/provider/password/login_id.go
+++ pkg/auth/dependency/provider/password/login_id.go
@@ -2,6 +2,24 @@ package password
 
 import "github.com/skygeario/skygear-server/pkg/core/utils"
 
+type LoginID struct {
+	Key   string
+	Value string
+}
+
+func ParseLoginIDs(rawLoginIDList []map[string]string) []LoginID {
+	loginIDList := []LoginID{}
+	for _, loginIDs := range rawLoginIDList {
+		for loginIDKey, loginID := range loginIDs {
+			loginIDList = append(loginIDList, LoginID{
+				Key:   loginIDKey,
+				Value: loginID,
+			})
+		}
+	}
+	return loginIDList
+}
+
 type loginIDChecker interface {
 	isValid(loginID map[string]string) bool
 }
--- pkg/auth/dependency/provider/password/login_id_test.go
+++ pkg/auth/dependency/provider/password/login_id_test.go
@@ -7,6 +7,44 @@ import (
 )
 
 func TestLoginID(t *testing.T) {
+	Convey("Test ParseLoginIDs", t, func() {
+		Convey("parse raw login ID list", func() {
+			var loginIDs []LoginID
+
+			loginIDs = ParseLoginIDs([]map[string]string{
+				map[string]string{"username": "johndoe"},
+			})
+			So(loginIDs, ShouldResemble, []LoginID{
+				LoginID{Key: "username", Value: "johndoe"},
+			})
+
+			loginIDs = ParseLoginIDs([]map[string]string{
+				map[string]string{"username": "johndoe"},
+				map[string]string{"email": "johndoe@example.com"},
+			})
+			So(loginIDs, ShouldResemble, []LoginID{
+				LoginID{Key: "username", Value: "johndoe"},
+				LoginID{Key: "email", Value: "johndoe@example.com"},
+			})
+
+			loginIDs = ParseLoginIDs([]map[string]string{
+				map[string]string{
+					"username": "johndoe",
+					"email":    "johndoe@example.com",
+				},
+				map[string]string{"phone": "+85299999999"},
+			})
+			So(loginIDs, ShouldResemble, []LoginID{
+				LoginID{Key: "username", Value: "johndoe"},
+				LoginID{Key: "email", Value: "johndoe@example.com"},
+				LoginID{Key: "phone", Value: "+85299999999"},
+			})
+
+			loginIDs = ParseLoginIDs([]map[string]string{})
+			So(loginIDs, ShouldResemble, []LoginID{})
+		})
+	})
+
 	Convey("Test isValid", t, func() {
 		Convey("validate by loginIDsKeyWhitelist: [username, email]", func() {
 			keys := []string{"username", "email"}
