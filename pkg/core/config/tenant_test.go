9f4b1df28ffccea66983a3658ee16da9538a33c8
Louis Chan <louischan@oursky.com>
2019-07-08 16:29:59 +0800

    Add losslessness test on TenantConfiguration

--- pkg/core/config/tenant_test.go
+++ pkg/core/config/tenant_test.go
@@ -10,8 +10,6 @@ import (
 
 	. "github.com/smartystreets/goconvey/convey"
 	"gopkg.in/yaml.v2"
-
-	. "github.com/skygeario/skygear-server/pkg/core/skytest"
 )
 
 const inputMinimalYAML = `version: '1'
@@ -39,6 +37,211 @@ const inputMinimalJSON = `
   }
 }
 `
+
+func newInt(i int) *int {
+	return &i
+}
+
+var fullTenantConfig = TenantConfiguration{
+	Version: "1",
+	AppName: "myapp",
+	AppConfig: AppConfiguration{
+		DatabaseURL: "postgres://user:password@localhost:5432/db?sslmode=disable",
+		SMTP: SMTPConfiguration{
+			Host:     "localhost",
+			Port:     465,
+			Mode:     "ssl",
+			Login:    "user",
+			Password: "password",
+		},
+		Twilio: TwilioConfiguration{
+			AccountSID: "mytwilioaccountsid",
+			AuthToken:  "mytwilioauthtoken",
+			From:       "mytwilio",
+		},
+		Nexmo: NexmoConfiguration{
+			APIKey:    "mynexmoapikey",
+			APISecret: "mynexmoapisecret",
+			From:      "mynexmo",
+		},
+	},
+	UserConfig: UserConfiguration{
+		APIKey:    "myapikey",
+		MasterKey: "mymasterkey",
+		URLPrefix: "http://localhost:3000",
+		CORS: CORSConfiguration{
+			Origin: "localhost:3000",
+		},
+		Auth: AuthConfiguration{
+			LoginIDKeys: map[string]LoginIDKeyConfiguration{
+				"email": LoginIDKeyConfiguration{
+					Type:    LoginIDKeyType("email"),
+					Minimum: newInt(0),
+					Maximum: newInt(1),
+				},
+				"phone": LoginIDKeyConfiguration{
+					Type:    LoginIDKeyType("phone"),
+					Minimum: newInt(0),
+					Maximum: newInt(1),
+				},
+				"username": LoginIDKeyConfiguration{
+					Type:    LoginIDKeyTypeRaw,
+					Minimum: newInt(0),
+					Maximum: newInt(1),
+				},
+			},
+			AllowedRealms: []string{"default"},
+		},
+		TokenStore: TokenStoreConfiguration{
+			Secret: "mytokenstoresecret",
+			Expiry: 0,
+		},
+		UserAudit: UserAuditConfiguration{
+			Enabled:         true,
+			TrailHandlerURL: "http://localhost:3000/useraudit",
+			Password: PasswordConfiguration{
+				MinLength:             8,
+				UppercaseRequired:     true,
+				LowercaseRequired:     true,
+				DigitRequired:         true,
+				SymbolRequired:        true,
+				MinimumGuessableLevel: 4,
+				ExcludedKeywords:      []string{"admin", "password", "secret"},
+				HistorySize:           10,
+				HistoryDays:           90,
+				ExpiryDays:            30,
+			},
+		},
+		ForgotPassword: ForgotPasswordConfiguration{
+			AppName:             "myapp",
+			URLPrefix:           "http://localhost:3000/forgotpassword",
+			SecureMatch:         true,
+			SenderName:          "myforgotpasswordsendername",
+			Sender:              "myforgotpasswordsender",
+			Subject:             "myforgotpasswordsubject",
+			ReplyToName:         "myforgotpasswordreplytoname",
+			ReplyTo:             "myforgotpasswordreplyto",
+			ResetURLLifetime:    60,
+			SuccessRedirect:     "http://localhost:3000/forgotpassword/success",
+			ErrorRedirect:       "http://localhost:3000/forgotpassword/error",
+			EmailTextURL:        "http://localhost:3000/forgotpassword/text",
+			EmailHTMLURL:        "http://localhost:3000/forgotpassword/html",
+			ResetHTMLURL:        "http://localhost:3000/forgotpassword/reset",
+			ResetSuccessHTMLURL: "http://localhost:3000/forgotpassword/reset/success",
+			ResetErrorHTMLURL:   "http://localhost:3000/forgotpassword/reset/error",
+		},
+		WelcomeEmail: WelcomeEmailConfiguration{
+			Enabled:     true,
+			URLPrefix:   "http://localhost:3000/welcomeemail",
+			SenderName:  "welcomeemailsendername",
+			Sender:      "welcomeemailsender",
+			Subject:     "welcomeemailsubject",
+			ReplyToName: "welcomeemailreplytoname",
+			ReplyTo:     "welcomeemailreplyto",
+			TextURL:     "http://localhost:3000/welcomeemail/text",
+			HTMLURL:     "http://localhost:3000/welcomeemail/html",
+			Destination: "first",
+		},
+		SSO: SSOConfiguration{
+			CustomToken: CustomTokenConfiguration{
+				Enabled:                    true,
+				Issuer:                     "customtokenissuer",
+				Audience:                   "customtokenaudience",
+				Secret:                     "customtokensecret",
+				OnUserDuplicateAllowMerge:  true,
+				OnUserDuplicateAllowCreate: true,
+			},
+			OAuth: OAuthConfiguration{
+				URLPrefix:      "http://localhost:3000/oauth",
+				JSSDKCDNURL:    "http://localhost:3000/oauth/sdk.js",
+				StateJWTSecret: "oauthstatejwtsecret",
+				AllowedCallbackURLs: []string{
+					"http://localhost:3000/oauth/callback",
+				},
+				ExternalAccessTokenFlowEnabled: true,
+				OnUserDuplicateAllowMerge:      true,
+				OnUserDuplicateAllowCreate:     true,
+				Providers: []OAuthProviderConfiguration{
+					OAuthProviderConfiguration{
+						ID:           "google",
+						Type:         "google",
+						ClientID:     "googleclientid",
+						ClientSecret: "googleclientsecret",
+						Scope:        "email profile",
+					},
+					OAuthProviderConfiguration{
+						ID:           "azure-id-1",
+						Type:         "azureadv2",
+						ClientID:     "azureclientid",
+						ClientSecret: "azureclientsecret",
+						Scope:        "email",
+						Tenant:       "azure-id-1",
+					},
+				},
+			},
+		},
+		UserVerification: UserVerificationConfiguration{
+			URLPrefix:        "http://localhost:3000/userverification",
+			AutoSendOnSignup: true,
+			Criteria:         "any",
+			ErrorRedirect:    "http://localhost:3000/userverification/error",
+			ErrorHTMLURL:     "http://localhost:3000/userverification/error.html",
+			LoginIDKeys: map[string]UserVerificationKeyConfiguration{
+				"email": UserVerificationKeyConfiguration{
+
+					CodeFormat:      "complex",
+					Expiry:          3600,
+					SuccessRedirect: "http://localhost:3000/userverification/success",
+					SuccessHTMLURL:  "http://localhost:3000/userverification/success.html",
+					ErrorRedirect:   "http://localhost:3000/userverification/error",
+					ErrorHTMLURL:    "http://localhost:3000/userverification/error.html",
+					Provider:        "twilio",
+					ProviderConfig: UserVerificationProviderConfiguration{
+						Subject:     "userverificationsubject",
+						Sender:      "userverificationsender",
+						SenderName:  "userverificationsendername",
+						ReplyTo:     "userverificationreplyto",
+						ReplyToName: "userverificationreplytoname",
+						TextURL:     "http://localhost:3000/userverification/text",
+						HTMLURL:     "http://localhost:3000/userverification/html",
+					},
+				},
+			},
+		},
+	},
+	Hooks: []Hook{
+		Hook{
+			Async:   true,
+			Event:   "after_signup",
+			URL:     "http://localhost:3000/after_signup",
+			Timeout: 60,
+		},
+		Hook{
+			Async:   false,
+			Event:   "after_signup",
+			URL:     "http://localhost:3000/after_signup",
+			Timeout: 60,
+		},
+	},
+	DeploymentRoutes: []DeploymentRoute{
+		DeploymentRoute{
+			Version: "a",
+			Path:    "/",
+			Type:    "http-service",
+			TypeConfig: map[string]interface{}{
+				"backend_url": "http://localhost:3000",
+			},
+		},
+		DeploymentRoute{
+			Version: "a",
+			Path:    "/api",
+			Type:    "http-service",
+			TypeConfig: map[string]interface{}{
+				"backend_url": "http://localhost:3001",
+			},
+		},
+	},
+}
 
 func TestTenantConfig(t *testing.T) {
 	Convey("Test TenantConfiguration", t, func() {
@@ -117,79 +320,28 @@ user_config:
 		})
 		// Conversion
 		Convey("should be losslessly converted between Go and msgpack", func() {
-			c, _ := NewTenantConfigurationFromScratch(FromScratchOptions{
-				AppName:     "myapp",
-				DatabaseURL: "postgres://",
-				APIKey:      "apikey",
-				MasterKey:   "masterkey",
-			})
+			c := &fullTenantConfig
 			base64msgpack, err := c.StdBase64Msgpack()
 			So(err, ShouldBeNil)
-
 			cc, err := NewTenantConfigurationFromStdBase64Msgpack(base64msgpack)
 			So(err, ShouldBeNil)
 			So(c, ShouldResemble, cc)
 		})
 		Convey("should be losslessly converted between Go and JSON", func() {
-			c, _ := NewTenantConfigurationFromScratch(FromScratchOptions{
-				AppName:     "myapp",
-				DatabaseURL: "postgres://",
-				APIKey:      "apikey",
-				MasterKey:   "masterkey",
-			})
+			c := &fullTenantConfig
 			b, err := json.Marshal(c)
 			So(err, ShouldBeNil)
-
 			cc, err := NewTenantConfigurationFromJSON(bytes.NewReader(b))
 			So(err, ShouldBeNil)
 			So(c, ShouldResemble, cc)
 		})
 		Convey("should be losslessly converted between Go and YAML", func() {
-			c, _ := NewTenantConfigurationFromScratch(FromScratchOptions{
-				AppName:     "myapp",
-				DatabaseURL: "postgres://",
-				APIKey:      "apikey",
-				MasterKey:   "masterkey",
-			})
+			c := &fullTenantConfig
 			b, err := yaml.Marshal(c)
 			So(err, ShouldBeNil)
-
 			cc, err := NewTenantConfigurationFromYAML(bytes.NewReader(b))
 			So(err, ShouldBeNil)
-			So(c, ShouldNonRecursiveDataDeepEqual, cc)
-		})
-		// DeploymentRoutes
-		Convey("should serialize deployment routes", func() {
-			c, _ := NewTenantConfigurationFromScratch(FromScratchOptions{
-				AppName:     "myapp",
-				DatabaseURL: "postgres://",
-				APIKey:      "apikey",
-				MasterKey:   "masterkey",
-			})
-			c.DeploymentRoutes = []DeploymentRoute{
-				DeploymentRoute{
-					Version: "a",
-					Path:    "/",
-					Type:    "http-service",
-					TypeConfig: map[string]interface{}{
-						"backend_url": "http://localhost:3000",
-					},
-				},
-				DeploymentRoute{
-					Version: "a",
-					Path:    "/api",
-					Type:    "http-service",
-					TypeConfig: map[string]interface{}{
-						"backend_url": "http://localhost:3001",
-					},
-				},
-			}
-			b, err := yaml.Marshal(c)
-			So(err, ShouldBeNil)
-
-			cc, err := NewTenantConfigurationFromYAML(bytes.NewReader(b))
-			So(err, ShouldBeNil)
-			So(c, ShouldNonRecursiveDataDeepEqual, cc)
+			So(c, ShouldResemble, cc)
 		})
 		// Env
 		Convey("should load tenant config from env", func() {
