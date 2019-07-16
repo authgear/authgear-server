3fb3e91505471f0a2fc301db36e7043f61baeed5
Louis Chan <louischan@oursky.com>
2019-07-16 16:36:20 +0800

    Add package "hash"

---
+++ pkg/core/hash/sha256.go
@@ -0,0 +1,14 @@
+package hash
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+// SHA256String returns the hex hash of s.
+func SHA256String(s string) string {
+	b := []byte(s)
+	outArr := sha256.Sum256(b)
+	outSlice := outArr[:]
+	return hex.EncodeToString(outSlice)
+}
