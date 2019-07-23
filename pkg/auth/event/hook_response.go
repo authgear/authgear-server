584fbb1a093e0bf8307041626dc1b36d4174a04a
kiootic <kiootic@oursky.com>
2019-07-23 14:06:10 +0800

    Add hook response model

---
+++ pkg/auth/event/hook_response.go
@@ -0,0 +1,25 @@
+package event
+
+import "github.com/skygeario/skygear-server/pkg/core/skyerr"
+
+type HookResponse struct {
+	IsAllowed bool       `json:"is_allowed"`
+	Reason    string     `json:"reason"`
+	Mutations *Mutations `json:"mutations"`
+}
+
+func (resp HookResponse) Validate() error {
+	if resp.IsAllowed {
+		if resp.Mutations != nil {
+			return skyerr.NewInvalidArgument("mutations must not exist", []string{"mutations"})
+		}
+		if resp.Reason != "" {
+			return skyerr.NewInvalidArgument("reason must not exist", []string{"reason"})
+		}
+	} else {
+		if resp.Reason == "" {
+			return skyerr.NewInvalidArgument("reason must be provided", []string{"mutations"})
+		}
+	}
+	return nil
+}
