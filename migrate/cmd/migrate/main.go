46323a90ea2847f3aa822d19bced95535ea6a410
kiootic <kiootic@oursky.com>
2019-08-20 17:23:40 +0800

    Update migration procedure

--- migrate/cmd/migrate/main.go
+++ migrate/cmd/migrate/main.go
@@ -181,7 +181,7 @@ func runMultiTenantMigrations(
 	for _, a := range apps {
 		for _, migration := range migrations {
 			databaseURL := a.DatabaseURL
-			schema := fmt.Sprintf("app_%s", a.Name)
+			schema := "app"
 
 			sourceURL := ""
 			if s, ok := sources[migration]; ok {
@@ -212,23 +212,18 @@ func runCmd(req commandRequest) (string, error) {
 		sourceURL = fmt.Sprintf("file://%s", sourceURL)
 	}
 
-	schema := req.Schema
-	if req.Migration == "gateway" {
-		schema = "app_config"
-	}
-
 	purl, _ := nurl.Parse(req.DatabaseURL)
 	l := log.WithFields(log.Fields{
 		"migration":  req.Migration,
 		"db_name":    purl.EscapedPath(),
 		"db_host":    purl.Hostname(),
 		"source_url": sourceURL,
-		"schema":     schema,
+		"schema":     req.Schema,
 	})
 
 	result, err := command.Run(
 		req.Migration,
-		schema,
+		req.Schema,
 		req.DatabaseURL,
 		sourceURL,
 		req.DryRun,
