4364672efe7ed6424f6b90945ded06e79a2630be
Louis Chan <louischan@oursky.com>
2019-09-17 14:50:38 +0800

    Group headers

--- pkg/core/http/header.go
+++ pkg/core/http/header.go
@@ -6,23 +6,34 @@ import (
 
 // Header names
 const (
-	HeaderAPIKey                   = "x-skygear-api-key"
-	HeaderAccessToken              = "x-skygear-access-token"
-	HeaderAccessKeyType            = "x-skygear-access-key-type"
-	HeaderClientID                 = "x-skygear-client-id"
+	// Headers appearing in client request
+	HeaderAPIKey           = "x-skygear-api-key"
+	HeaderAccessToken      = "x-skygear-access-token"
+	HeaderSessionExtraInfo = "x-skygear-extra-info"
+
+	// Headers appearing in proxied request
+	HeaderRequestID = "x-skygear-request-id"
+
+	// Headers appearing in proxied microservice request
 	HeaderUserDisabled             = "x-skygear-user-disabled"
 	HeaderUserID                   = "x-skygear-user-userid"
 	HeaderUserVerified             = "x-skygear-user-verified"
 	HeaderSessionIdentityType      = "x-skygear-session-identity-type"
 	HeaderSessionAuthenticatorType = "x-skygear-session-authenticator-type"
-	HeaderGear                     = "x-skygear-gear"
-	HeaderGearEndpoint             = "x-skygear-gear-endpoint"
-	HeaderGearVersion              = "x-skygear-gear-version"
 	HeaderHTTPPath                 = "x-skygear-http-path"
-	HeaderRequestID                = "x-skygear-request-id"
-	HeaderTenantConfig             = "x-skygear-app-config"
-	HeaderRequestBodySignature     = "x-skygear-body-signature"
-	HeaderSessionExtraInfo         = "x-skygear-extra-info"
+
+	// Headers appearing in proxied gear request
+	HeaderGear         = "x-skygear-gear"
+	HeaderGearEndpoint = "x-skygear-gear-endpoint"
+	HeaderGearVersion  = "x-skygear-gear-version"
+	HeaderTenantConfig = "x-skygear-app-config"
+
+	// Internal headers
+	HeaderAccessKeyType = "x-skygear-access-key-type"
+	HeaderClientID      = "x-skygear-client-id"
+
+	// Outbound webhook request
+	HeaderRequestBodySignature = "x-skygear-body-signature"
 )
 
 func GetHost(req *gohttp.Request) (host string) {
