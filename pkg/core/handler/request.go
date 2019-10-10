964bd4eb7442b05973e6e65e00397158fe654d47
Louis Chan <louischan@oursky.com>
2019-10-10 18:31:36 +0800

    Add ParseJSONBody

--- pkg/core/handler/request.go
+++ pkg/core/handler/request.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/skygeario/skygear-server/pkg/core/skyerr"
@@ -20,13 +21,19 @@ func (p EmptyRequestPayload) Validate() error {
 const BodyMaxSize = 1024 * 1024 * 10
 
 func DecodeJSONBody(r *http.Request, w http.ResponseWriter, payload interface{}) error {
+	return ParseJSONBody(r, w, func(r io.Reader, p interface{}) error {
+		if err := json.NewDecoder(r).Decode(payload); err != nil {
+			return skyerr.NewBadRequest("failed to decode the request payload")
+		}
+		return nil
+	}, payload)
+}
+
+func ParseJSONBody(r *http.Request, w http.ResponseWriter, parse func(io.Reader, interface{}) error, payload interface{}) error {
 	if contentType := r.Header.Get("Content-Type"); contentType != "application/json" {
 		return skyerr.NewBadRequest("request content type is invalid")
 	}
 	body := http.MaxBytesReader(w, r.Body, BodyMaxSize)
 	defer body.Close()
-	if err := json.NewDecoder(body).Decode(payload); err != nil {
-		return skyerr.NewBadRequest("failed to decode the request payload")
-	}
-	return nil
+	return parse(body, payload)
 }
