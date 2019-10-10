67c7438ed6a6790397a346d80e3fd56467012080
Louis Chan <louischan@oursky.com>
2019-10-10 19:54:41 +0800

    Add PresignUploadRequest

---
+++ pkg/core/cloudstorage/presign_upload_request.go
@@ -0,0 +1,87 @@
+package cloudstorage
+
+import (
+	"fmt"
+	"mime"
+	"net/http"
+
+	"github.com/skygeario/skygear-server/pkg/core/uuid"
+)
+
+type PresignUploadRequest struct {
+	ExactName string                 `json:"exact_name"`
+	Prefix    string                 `json:"prefix"`
+	Access    AccessType             `json:"access"`
+	Headers   map[string]interface{} `json:"headers"`
+}
+
+func (r *PresignUploadRequest) SetDefaultValue() {
+	if r.Access == "" {
+		r.Access = AccessTypeDefault
+	}
+	if _, ok := r.Headers["content-type"]; !ok {
+		r.Headers["content-type"] = "application/octet-stream"
+	}
+}
+
+func (r *PresignUploadRequest) IsCustomName() bool {
+	return r.ExactName != ""
+}
+
+func (r *PresignUploadRequest) DeriveAssetName() (assetName string, err error) {
+	// Derive file extension
+	contentType, ok := r.Headers["content-type"].(string)
+	var ext string
+	if ok {
+		exts, err := mime.ExtensionsByType(contentType)
+		if err != nil {
+			return "", err
+		}
+		if len(exts) > 0 {
+			ext = exts[0]
+		}
+	}
+
+	// Use exact name if given
+	if r.ExactName != "" {
+		assetName = r.ExactName
+		return
+	}
+
+	assetName = fmt.Sprintf("%s%s%s", r.Prefix, uuid.New(), ext)
+	return
+}
+
+func (r *PresignUploadRequest) SetCacheControl() {
+	if r.IsCustomName() {
+		if _, ok := r.Headers["cache-control"]; !ok {
+			r.Headers["cache-control"] = "no-cache"
+		}
+	} else {
+		if _, ok := r.Headers["cache-control"]; !ok {
+			r.Headers["cache-control"] = "max-age: 3600"
+		}
+	}
+}
+
+func (r *PresignUploadRequest) RemoveEmptyHeaders() {
+	// Remove any header whose value is empty string
+	headers := make(map[string]interface{})
+	for key, value := range r.Headers {
+		if v, ok := value.(string); ok && v == "" {
+			continue
+		}
+		headers[key] = value
+	}
+	r.Headers = headers
+}
+
+func (r *PresignUploadRequest) HTTPHeader() http.Header {
+	httpHeader := http.Header{}
+	for key, value := range r.Headers {
+		if v, ok := value.(string); ok {
+			httpHeader.Add(key, v)
+		}
+	}
+	return httpHeader
+}
