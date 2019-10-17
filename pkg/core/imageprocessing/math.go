db8145d111fa100538db87e12e213272d9b736f7
Louis Chan <louischan@oursky.com>
2019-10-17 19:52:21 +0800

    Add some math utils

---
+++ pkg/core/imageprocessing/math.go
@@ -0,0 +1,19 @@
+package imageprocessing
+
+import (
+	"math"
+)
+
+func ratio(x int, y int) float64 {
+	if x == y {
+		return 1
+	}
+	return float64(x) / float64(y)
+}
+
+func roundFloat(f float64) int {
+	if f < 0 {
+		return int(math.Ceil(f - 0.5))
+	}
+	return int(math.Floor(f + 0.5))
+}
