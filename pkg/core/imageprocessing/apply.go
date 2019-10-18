811c33f71b545bf38388288ce2a5331a9983fed6
Louis Chan <louischan@oursky.com>
2019-10-18 17:11:47 +0800

    Add ApplyToHTTPResponse

--- pkg/core/imageprocessing/apply.go
+++ pkg/core/imageprocessing/apply.go
@@ -1,14 +1,20 @@
 package imageprocessing
 
 import (
+	"bytes"
+	"io/ioutil"
+	"mime"
+	"net/http"
+	"strconv"
+
 	"github.com/davidbyttow/govips/pkg/vips"
 )
 
 // Apply applies operations to image.
-func Apply(image []byte, operations []Operation) ([]byte, error) {
+func Apply(image []byte, operations []Operation) ([]byte, ImageFormat, error) {
 	imageRef, err := vips.NewImageFromBuffer(image)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 	defer imageRef.Close()
 
@@ -24,7 +30,7 @@ func Apply(image []byte, operations []Operation) ([]byte, error) {
 	for _, op := range operations {
 		err := op.Apply(ctx)
 		if err != nil {
-			return nil, err
+			return nil, "", err
 		}
 	}
 
@@ -39,14 +45,66 @@ func Apply(image []byte, operations []Operation) ([]byte, error) {
 		quality = ctx.Quality
 	}
 
-	output, _, err := ctx.Image.Export(vips.ExportParams{
+	output, finalImageType, err := ctx.Image.Export(vips.ExportParams{
 		Format:         imageType,
 		Quality:        quality,
 		Interpretation: interpretation,
 	})
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 
-	return output, nil
+	return output, ImageFormatFromVIPS(finalImageType), nil
+}
+
+func ApplyToHTTPResponse(resp *http.Response, ops []Operation) error {
+	originalBody := resp.Body
+	input, err := ioutil.ReadAll(originalBody)
+	if err != nil {
+		return err
+	}
+	defer originalBody.Close()
+
+	output, imageFormat, err := Apply(input, ops)
+	if err != nil {
+		resp.Body = &bytesReaderCloser{Reader: bytes.NewReader(input)}
+		return nil
+	}
+
+	resp.ContentLength = int64(len(output))
+	resp.Header.Set("Content-Length", strconv.Itoa(len(output)))
+	resp.Header.Set("Content-Type", imageFormat.MediaType())
+	resp.Body = &bytesReaderCloser{Reader: bytes.NewReader(output)}
+	return nil
+}
+
+func IsApplicableToHTTPResponse(resp *http.Response) bool {
+	contentType := resp.Header.Get("Content-Type")
+	contentLength := resp.ContentLength
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return false
+	}
+	// content-length must be known and less than 20MiB.
+	if contentLength < 0 || contentLength > 20*1024*1024 {
+		return false
+	}
+	switch mediaType {
+	case "image/png":
+		return true
+	case "image/jpeg":
+		return true
+	case "image/webp":
+		return true
+	default:
+		return false
+	}
+}
+
+type bytesReaderCloser struct {
+	*bytes.Reader
+}
+
+func (r *bytesReaderCloser) Close() error {
+	return nil
 }
