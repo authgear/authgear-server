72f03ae152050e470615b98a6aa242beb7bf49b0
Louis Chan <louischan@oursky.com>
2019-10-25 14:26:43 +0800

    Include Key URI in /mfa/totp/new

--- pkg/auth/handler/mfa/create_totp.go
+++ pkg/auth/handler/mfa/create_totp.go
@@ -47,6 +47,8 @@ func (h *CreateTOTPHandler) ProvideAuthzPolicy() authz.Policy {
 type CreateTOTPRequest struct {
 	AuthnSessionToken string `json:"authn_session_token"`
 	DisplayName       string `json:"display_name"`
+	AccountName       string `json:"account_name"`
+	Issuer            string `json:"issuer"`
 }
 
 func (r CreateTOTPRequest) Validate() error {
@@ -60,6 +62,7 @@ type CreateTOTPResponse struct {
 	AuthenticatorID   string `json:"authenticator_id"`
 	AuthenticatorType string `json:"authenticator_type"`
 	Secret            string `json:"secret"`
+	OTPAuthURI        string `json:"otpauth_uri"`
 }
 
 // @JSONSchema
@@ -69,6 +72,8 @@ const CreateTOTPRequestSchema = `
 	"type": "object",
 	"properties": {
 		"display_name": { "type": "string" },
+		"account_name": { "type": "string" },
+		"issuer": { "type": "string" },
 		"authn_session_token": { "type": "string" }
 	},
 	"required": ["display_name"]
@@ -86,7 +91,8 @@ const CreateTOTPResponseSchema = `
 			"properties": {
 				"authenticator_id": { "type": "string" },
 				"authenticator_type": { "type": "string" },
-				"secret": { "type": "string" }
+				"secret": { "type": "string" },
+				"otpauth_uri": { "type": "string" }
 			}
 		}
 	}
@@ -138,10 +144,12 @@ func (h *CreateTOTPHandler) Handle(req interface{}) (resp interface{}, err error
 	if err != nil {
 		return
 	}
+	keyURI := mfa.NewKeyURI(payload.Issuer, payload.AccountName, a.Secret)
 	resp = CreateTOTPResponse{
 		AuthenticatorID:   a.ID,
 		AuthenticatorType: string(a.Type),
 		Secret:            a.Secret,
+		OTPAuthURI:        keyURI.String(),
 	}
 	return
 }
