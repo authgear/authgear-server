d816c9514e9fa135e80da14b65a173cda237ece0
Louis Chan <louischan@oursky.com>
2019-10-30 15:18:26 +0800

    Define Auth Gear template types

---
+++ pkg/auth/template/types.go
@@ -0,0 +1,23 @@
+package template
+
+import (
+	"github.com/skygeario/skygear-server/pkg/core/config"
+)
+
+const (
+	TemplateItemTypeForgotPasswordEmailTXT           config.TemplateItemType = "forgot_password_email.txt"
+	TemplateItemTypeForgotPasswordEmailHTML          config.TemplateItemType = "forgot_password_email.html"
+	TemplateItemTypeForgotPasswordResetHTML          config.TemplateItemType = "forgot_password_reset.html"
+	TemplateItemTypeForgotPasswordSuccessHTML        config.TemplateItemType = "forgot_password_success.html"
+	TemplateItemTypeForgotPasswordErrorHTML          config.TemplateItemType = "forgot_password_error.html"
+	TemplateItemTypeWelcomeEmailTXT                  config.TemplateItemType = "welcome_email.txt"
+	TemplateItemTypeWelcomeEmailHTML                 config.TemplateItemType = "welcome_email.html"
+	TemplateItemTypeUserVerificationGeneralErrorHTML config.TemplateItemType = "user_verification_general_error.html"
+	TemplateItemTypeUserVerificationMessageTXT       config.TemplateItemType = "user_verification_message.txt"
+	TemplateItemTypeUserVerificationMessageHTML      config.TemplateItemType = "user_verification_message.html"
+	TemplateItemTypeUserVerificationSuccessHTML      config.TemplateItemType = "user_verification_success.html"
+	TemplateItemTypeUserVerificationErrorHTML        config.TemplateItemType = "user_verification_error.html"
+	TemplateItemTypeMFAOOBCodeSMSTXT                 config.TemplateItemType = "mfa_oob_code_sms.txt"
+	TemplateItemTypeMFAOOBCodeEmailTXT               config.TemplateItemType = "mfa_oob_code_email.txt"
+	TemplateItemTypeMFAOOBCodeEmailHTML              config.TemplateItemType = "mfa_oob_code_email.html"
+)
