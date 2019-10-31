620b6edd9c0efea1f4335df1def33a51365424cd
kiootic <kiootic@oursky.com>
2019-10-31 17:51:17 +0800

    Add payload binding helper function

---
+++ pkg/core/handler/bind.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/skygeario/skygear-server/pkg/core/skyerr"
+
+	"github.com/skygeario/skygear-server/pkg/core/validation"
+)
+
+type bodyDefaulter interface {
+	SetDefaultValue()
+}
+
+type bodyValidator interface {
+	Validate() []validation.ErrorCause
+}
+
+func BindJSONBody(r *http.Request, w http.ResponseWriter, v *validation.Validator, schemaID string, payload interface{}) error {
+	const message = "invalid request body"
+	return ParseJSONBody(r, w, func(reader io.Reader, value interface{}) error {
+		err := v.WithMessage(message).ParseReader(schemaID, reader, value)
+		if err != nil {
+			if !skyerr.IsKind(err, validation.ValidationFailed) {
+				return skyerr.NewBadRequest("invalid request body")
+			}
+			return err
+		}
+		if value, ok := value.(bodyDefaulter); ok {
+			value.SetDefaultValue()
+		}
+		if value, ok := value.(bodyValidator); ok {
+			causes := value.Validate()
+			if len(causes) > 0 {
+				return validation.NewValidationFailed(message, causes)
+			}
+		}
+		return nil
+	}, payload)
+}
