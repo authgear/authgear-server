f4edec91920263e9d604a9e5316ca5282be1500c
kiootic <kiootic@oursky.com>
2019-11-20 18:04:50 +0800

    Update app name format

--- pkg/core/name/name.go
+++ pkg/core/name/name.go
@@ -1,25 +1,27 @@
 package name
 
 import (
-	"errors"
 	"regexp"
+
+	"github.com/skygeario/skygear-server/pkg/core/errors"
 )
+
+// A DNS label must have at most 63 characters
+// We limit the maximum length to 40, reserving some space for internal use,
+// e.g. k8s pod suffix
+const appNameFormat = `^[a-z0-9]([-a-z0-9]{0,38}[a-z0-9])?$`
 
 var (
 	// ErrInvalidAppName tells the accepted format of app name.
-	// App name will be used in domain name so it must
-	// start with a letter.
-	// Kubernetes only accepts name of 63 letter long,
-	// so we have to limit the length.
-	ErrInvalidAppName = errors.New("app name must be ^[a-zA-Z][a-zA-Z0-9]{0,11}$")
+	// App name will be used in domain name so it must be a valid DNS label.
+	ErrInvalidAppName = errors.Newf("app name must match '%s'", appNameFormat)
 )
-
 var (
-	regexAppName = regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9]{0,11}$`)
+	appNameRegex = regexp.MustCompile(appNameFormat)
 )
 
 func ValidateAppName(appName string) error {
-	if !regexAppName.MatchString(appName) {
+	if !appNameRegex.MatchString(appName) {
 		return ErrInvalidAppName
 	}
 	return nil
