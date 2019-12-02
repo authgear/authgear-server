3b8f884339df65a8217130445704b8875db02bf2
kiootic <kiootic@oursky.com>
2019-12-02 16:42:49 +0800

    Overwrite CORS header in response with upstream

--- pkg/asset/handler/get.go
+++ pkg/asset/handler/get.go
@@ -115,17 +115,17 @@ func (h *GetHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return nil
 		}
 
-		// Remove existing CORS header in response
-		for name := range w.Header() {
-			if strings.HasPrefix(name, "Access-Control-") {
-				w.Header().Del(name)
-			}
-		}
-
 		resp.Header = h.CloudStorageProvider.ProprietaryToStandard(resp.Header)
 		// Do not support range request if image processing query is present.
 		if hasPipeline {
 			resp.Header.Del("Accept-Ranges")
+		}
+
+		// Delete existing CORS header if exists in response
+		for name := range resp.Header {
+			if strings.HasPrefix(name, "Access-Control-") {
+				w.Header().Del(name)
+			}
 		}
 
 		// Check access
