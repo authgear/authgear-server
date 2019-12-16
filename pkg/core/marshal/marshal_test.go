092f0bc2f567b37823b1d466be9c0b846f4d6039
Louis Chan <louischan@oursky.com>
2019-12-16 15:57:45 +0800

    Add OmitEmpty

--- pkg/core/marshal/marshal_test.go
+++ pkg/core/marshal/marshal_test.go
@@ -43,26 +43,57 @@ func TestShouldNotHaveDuplicatedTypeInSamePath(t *testing.T) {
 }
 
 func TestUpdateNilFieldsWithZeroValue(t *testing.T) {
-	Convey("Test UpdateNilFieldsWithZeroValue", t, func() {
+	type ChildStruct struct {
+		Num1 *int
+		Num2 *int `default_zero_value:"true"`
+	}
+
+	type TestStruct struct {
+		ChildNode1 *ChildStruct `default_zero_value:"true"`
+		ChildNode2 *ChildStruct
+	}
+
+	Convey("UpdateNilFieldsWithZeroValue", t, func() {
 		Convey("should update nil fields with tag", func() {
-			type ChildStruct struct {
-				Num1 *int
-				Num2 *int `default_zero_value:"true"`
-			}
-
-			type TestStruct struct {
-				ChildNode1 *ChildStruct `default_zero_value:"true"`
-				ChildNode2 *ChildStruct
-			}
-
 			s := &TestStruct{}
-			UpdateNilFieldsWithZeroValue(s)
 
+			UpdateNilFieldsWithZeroValue(s)
 			So(s.ChildNode1, ShouldNotBeNil)
 			So(s.ChildNode2, ShouldBeNil)
-
 			So(s.ChildNode1.Num1, ShouldBeNil)
 			So(s.ChildNode1.Num2, ShouldNotBeNil)
+		})
+	})
+}
+
+func TestOmitEmpty(t *testing.T) {
+	type A struct {
+		Str string
+	}
+	type B struct {
+		A *A
+	}
+	Convey("OmitEmpty", t, func() {
+		Convey("Map", func() {
+			zero := 0
+			m1 := map[string]*int{
+				"a": &zero,
+			}
+			OmitEmpty(m1)
+			So(len(m1), ShouldEqual, 0)
+		})
+		Convey("Slice", func() {
+			slice := []*A{&A{}, &A{}}
+			OmitEmpty(slice)
+			So(slice[0], ShouldBeNil)
+			So(slice[1], ShouldBeNil)
+		})
+		Convey("Struct", func() {
+			b := &B{
+				A: &A{},
+			}
+			OmitEmpty(b)
+			So(b.A, ShouldBeNil)
 		})
 	})
 }
