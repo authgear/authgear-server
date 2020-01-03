7595f438704c0216aafd63a8ef18cf3af1691984
Louis Chan <louischan@oursky.com>
2020-01-03 10:56:29 +0800

    Remove APIHandler
    refs #1031

--- pkg/auth/dependency/hook/provider.go
+++ pkg/auth/dependency/hook/provider.go
@@ -4,20 +4,12 @@ import (
 	"github.com/skygeario/skygear-server/pkg/auth/event"
 	"github.com/skygeario/skygear-server/pkg/auth/model"
 	"github.com/skygeario/skygear-server/pkg/core/db"
-	"github.com/skygeario/skygear-server/pkg/core/handler"
 )
 
 type Provider interface {
 	WillCommitTx() error
 	DidCommitTx()
 	DispatchEvent(payload event.Payload, user *model.User) error
-}
-
-func WrapHandler(provider Provider, handler handler.APIHandler) handler.APIHandler {
-	return hookHandler{
-		APIHandler: handler,
-		provider:   provider,
-	}
 }
 
 func WithTx(provider Provider, ctx db.TxContext, do func() error) error {
--- pkg/core/handler/api.go
+++ pkg/core/handler/api.go
@@ -8,17 +8,6 @@ import (
 	"github.com/skygeario/skygear-server/pkg/core/skyerr"
 )
 
-type APIHandler interface {
-	DecodeRequest(request *http.Request, resp http.ResponseWriter) (RequestPayload, error)
-	WithTx() bool
-	Handle(requestPayload interface{}) (interface{}, error)
-}
-
-type APITxHandler interface {
-	WillCommitTx() error
-	DidCommitTx()
-}
-
 type APIResponse struct {
 	Result interface{}
 	Error  error
@@ -29,61 +18,6 @@ func (r APIResponse) MarshalJSON() ([]byte, error) {
 		Result interface{}      `json:"result,omitempty"`
 		Error  *skyerr.APIError `json:"error,omitempty"`
 	}{r.Result, skyerr.AsAPIError(r.Error)})
-}
-
-func APIHandlerToHandler(apiHandler APIHandler, txContext db.TxContext) http.Handler {
-	txHandler, _ := apiHandler.(APITxHandler)
-
-	handleAPICall := func(r *http.Request, resp http.ResponseWriter) (response APIResponse) {
-		payload, err := apiHandler.DecodeRequest(r, resp)
-		if err != nil {
-			response.Error = err
-			return
-		}
-
-		if err := payload.Validate(); err != nil {
-			response.Error = err
-			return
-		}
-
-		defer func() {
-			if err != nil {
-				response.Error = err
-			}
-		}()
-
-		if apiHandler.WithTx() {
-			// assume txContext != nil if apiHandler.WithTx() is true
-			if err := txContext.BeginTx(); err != nil {
-				response.Error = err
-				return
-			}
-
-			defer func() {
-				err = db.EndTx(txContext, err)
-				if err == nil && txHandler != nil {
-					txHandler.DidCommitTx()
-				}
-			}()
-		}
-
-		responsePayload, err := apiHandler.Handle(payload)
-
-		if err == nil && txHandler != nil {
-			err = txHandler.WillCommitTx()
-		}
-
-		if err == nil {
-			response.Result = responsePayload
-		}
-
-		return
-	}
-
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		response := handleAPICall(r, rw)
-		WriteResponse(rw, response)
-	})
 }
 
 // HandledError represents a handled (i.e. API responded with error) unexpected
@@ -115,8 +49,6 @@ func WriteResponse(rw http.ResponseWriter, response APIResponse) {
 // Transactional runs f within a transaction.
 // If err is non-nil, the transaction is rolled back.
 // Otherwise the transaction is committed.
-// It is a lightweight and flexible alternative to APIHandler
-// because it is not coupled with http.
 func Transactional(txContext db.TxContext, f func() (interface{}, error)) (result interface{}, err error) {
 	err = db.WithTx(txContext, func() error {
 		result, err = f()
