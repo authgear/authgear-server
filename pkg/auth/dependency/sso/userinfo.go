b3c860941e73eaf7f19aec3f9b38e59ad75b26fc
Louis Chan <louischan@oursky.com>
2020-03-06 10:47:43 +0800

    Avoid nil pointer dereference

--- pkg/auth/dependency/sso/userinfo.go
+++ pkg/auth/dependency/sso/userinfo.go
@@ -25,17 +25,17 @@ func NewUserInfoDecoder(loginIDNormalizerFactory loginid.LoginIDNormalizerFactor
 func (d *UserInfoDecoderImpl) DecodeUserInfo(providerType config.OAuthProviderType, userInfo map[string]interface{}) (providerUserInfo *ProviderUserInfo, err error) {
 	switch providerType {
 	case config.OAuthProviderTypeGoogle:
-		*providerUserInfo = d.decodeDefault(userInfo)
+		providerUserInfo = d.decodeDefault(userInfo)
 	case config.OAuthProviderTypeFacebook:
-		*providerUserInfo = d.decodeDefault(userInfo)
+		providerUserInfo = d.decodeDefault(userInfo)
 	case config.OAuthProviderTypeInstagram:
-		*providerUserInfo = d.decodeInstagram(userInfo)
+		providerUserInfo = d.decodeInstagram(userInfo)
 	case config.OAuthProviderTypeLinkedIn:
-		*providerUserInfo = d.decodeDefault(userInfo)
+		providerUserInfo = d.decodeDefault(userInfo)
 	case config.OAuthProviderTypeAzureADv2:
-		*providerUserInfo = d.decodeAzureADv2(userInfo)
+		providerUserInfo = d.decodeAzureADv2(userInfo)
 	case config.OAuthProviderTypeApple:
-		*providerUserInfo = d.decodeApple(userInfo)
+		providerUserInfo = d.decodeApple(userInfo)
 	default:
 		panic(fmt.Sprintf("sso: unknown provider type: %v", providerType))
 	}
@@ -50,44 +50,45 @@ func (d *UserInfoDecoderImpl) DecodeUserInfo(providerType config.OAuthProviderTy
 	return
 }
 
-func (d *UserInfoDecoderImpl) decodeDefault(userInfo map[string]interface{}) ProviderUserInfo {
+func (d *UserInfoDecoderImpl) decodeDefault(userInfo map[string]interface{}) *ProviderUserInfo {
 	id, _ := userInfo["id"].(string)
 	email, _ := userInfo["email"].(string)
 
-	return ProviderUserInfo{
+	return &ProviderUserInfo{
 		ID:    id,
 		Email: email,
 	}
 }
 
-func (d *UserInfoDecoderImpl) decodeInstagram(userInfo map[string]interface{}) (info ProviderUserInfo) {
+func (d *UserInfoDecoderImpl) decodeInstagram(userInfo map[string]interface{}) *ProviderUserInfo {
 	// Check GET /users/self response
 	// https://www.instagram.com/developer/endpoints/users/
+	info := &ProviderUserInfo{}
 	data, ok := userInfo["data"].(map[string]interface{})
 	if !ok {
-		return
+		return info
 	}
 
 	info.ID, _ = data["id"].(string)
 	info.Email, _ = data["email"].(string)
-	return
+	return info
 }
 
-func (d *UserInfoDecoderImpl) decodeAzureADv2(userInfo map[string]interface{}) ProviderUserInfo {
+func (d *UserInfoDecoderImpl) decodeAzureADv2(userInfo map[string]interface{}) *ProviderUserInfo {
 	id, _ := userInfo["oid"].(string)
 	email, _ := userInfo["email"].(string)
 
-	return ProviderUserInfo{
+	return &ProviderUserInfo{
 		ID:    id,
 		Email: email,
 	}
 }
 
-func (d *UserInfoDecoderImpl) decodeApple(userInfo map[string]interface{}) ProviderUserInfo {
+func (d *UserInfoDecoderImpl) decodeApple(userInfo map[string]interface{}) *ProviderUserInfo {
 	id, _ := userInfo["sub"].(string)
 	email, _ := userInfo["email"].(string)
 
-	return ProviderUserInfo{
+	return &ProviderUserInfo{
 		ID:    id,
 		Email: email,
 	}
