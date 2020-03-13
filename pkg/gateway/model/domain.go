e8ba7b00f7c9a3c67f67bc8d060b959b0afd9dd8
Carmen <carmen@oursky.com>
2020-03-13 18:43:38 +1100

    Add comment for AssignmentTypeDefault description

--- pkg/gateway/model/domain.go
+++ pkg/gateway/model/domain.go
@@ -6,7 +6,14 @@ const (
 	AssignmentTypeAuth          AssignmentType = "auth"
 	AssignmentTypeAsset         AssignmentType = "asset"
 	AssignmentTypeMicroservices AssignmentType = "microservices"
-	AssignmentTypeDefault       AssignmentType = "default"
+
+	// AssignmentTypeDefault is used for app default domain only
+	// Default domain supports gear subdomains
+	// e.g. If an app has default domain `app1.skygearapp.com`, gear subdomains
+	// `accounts.app1.skygearapp.com` and `asset.app1.skygearapp.com` will be
+	// support automatically
+	// Custom domain doesn't support default assignment
+	AssignmentTypeDefault AssignmentType = "default"
 )
 
 type Domain struct {
