bf3c53033f33e204a00a3bd5acede8333e48a88e
Louis Chan <louischan@oursky.com>
2020-03-17 17:28:18 +0800

    Handle submit login ID step

--- pkg/auth/dependency/webapp/authenticate_provider_impl.go
+++ pkg/auth/dependency/webapp/authenticate_provider_impl.go
@@ -16,17 +16,52 @@ func (p *AuthenticateProviderImpl) ServeHTTP(w http.ResponseWriter, r *http.Requ
 		// TODO(webapp): render err in html
 		panic(err)
 	}
+
+	p.ValidateProvider.Prevalidate(r.Form)
+
 	var writeResponse func(err error)
 	var err error
-	writeResponse, err = p.Default(w, r)
+	step := r.Form.Get("x_step")
+	switch step {
+	case "submit_password":
+		writeResponse, err = p.SubmitPassword(w, r)
+	case "submit_login_id":
+		writeResponse, err = p.SubmitLoginID(w, r)
+	case "choose_idp":
+		writeResponse, err = p.ChooseIdentityProvider(w, r)
+	default:
+		writeResponse, err = p.Default(w, r)
+	}
 	writeResponse(err)
 }
 
 func (p *AuthenticateProviderImpl) Default(w http.ResponseWriter, r *http.Request) (writeResponse func(err error), err error) {
-	p.ValidateProvider.Prevalidate(r.Form)
 	err = p.ValidateProvider.Validate("#WebAppAuthenticateRequest", r.Form)
 	writeResponse = func(err error) {
 		p.RenderProvider.WritePage(w, r, TemplateItemTypeAuthUISignInHTML, err)
 	}
 	return
+}
+
+func (p *AuthenticateProviderImpl) SubmitLoginID(w http.ResponseWriter, r *http.Request) (writeResponse func(err error), err error) {
+	err = p.ValidateProvider.Validate("#WebAppAuthenticateLoginIDRequest", r.Form)
+	writeResponse = func(err error) {
+		t := TemplateItemTypeAuthUISignInHTML
+		if err == nil {
+			t = TemplateItemTypeAuthUISignInPasswordHTML
+		}
+		p.RenderProvider.WritePage(w, r, t, err)
+	}
+	return
+}
+
+func (p *AuthenticateProviderImpl) SubmitPassword(w http.ResponseWriter, r *http.Request) (writeResponse func(err error), err error) {
+	// TODO(webapp): Enter the authentication process
+	return p.Default(w, r)
+}
+
+func (p *AuthenticateProviderImpl) ChooseIdentityProvider(w http.ResponseWriter, r *http.Request) (writeResponse func(err error), err error) {
+	// TODO(webapp): Prepare IdP authorization URL and respond 302
+	// TODO(webapp): Add a new endpoint to be redirect_uri
+	return p.Default(w, r)
 }
