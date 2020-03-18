e40cbf68014e3e409ed6122379f658fea7890ca7
kiootic <kiootic@oursky.com>
2020-03-18 15:26:53 +0800

    Add grant models

---
+++ pkg/auth/dependency/oauth/grant_code.go
@@ -0,0 +1,24 @@
+package oauth
+
+import "time"
+
+type CodeGrant struct {
+	AppID           string `json:"app_id"`
+	AuthorizationID string `json:"authz_id"`
+	SessionID       string `json:"session_id"`
+
+	CreatedAt time.Time `json:"created_at"`
+	ExpireAt  time.Time `json:"expire_at"`
+	Scopes    []string  `json:"scopes"`
+	CodeHash  string    `json:"code_hash"`
+
+	RedirectURI   string `json:"redirect_uri"`
+	OIDCNonce     string `json:"nonce,omitempty"`
+	PKCEChallenge string `json:"challenge,omitempty"`
+}
+
+var _ Grant = CodeGrant{}
+
+func (g CodeGrant) Session() (kind GrantSessionKind, id string) {
+	return GrantSessionKindSession, g.SessionID
+}
