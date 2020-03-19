8040237fa159dc4df4d1c0b36e3ed1eee1333d75
Louis Chan <louischan@oursky.com>
2020-03-19 14:21:25 +0800

    Only allow plain http localhost to be frame ancestor

--- pkg/auth/dependency/webapp/csp_middleware.go
+++ pkg/auth/dependency/webapp/csp_middleware.go
@@ -2,6 +2,7 @@ package webapp
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"net/url"
 	"strings"
@@ -16,12 +17,15 @@ func deriveFrameAncestors(clients []config.OAuthClientConfiguration) (out []stri
 				if s, ok := redirectURI.(string); ok {
 					u, err := url.Parse(s)
 					if err == nil {
-						if u.Host != "" && (u.Scheme == "http" || u.Scheme == "https") {
-							ancestor := url.URL{
-								Scheme: u.Scheme,
-								Host:   u.Host,
-							}
-							out = append(out, ancestor.String())
+						ancestor := (&url.URL{
+							Scheme: u.Scheme,
+							Host:   u.Host,
+						}).String()
+						if u.Scheme == "https" && u.Host != "" {
+							out = append(out, ancestor)
+						}
+						if u.Scheme == "http" && isLocalhost(u.Host) {
+							out = append(out, ancestor)
 						}
 					}
 				}
@@ -29,6 +33,31 @@ func deriveFrameAncestors(clients []config.OAuthClientConfiguration) (out []stri
 		}
 	}
 	return
+}
+
+func isLocalhost(host string) bool {
+	// Trim the port if it is present.
+	if h, _, err := net.SplitHostPort(host); err == nil {
+		host = h
+	}
+
+	// net.ParseIP does not accept IPv6 in square brackets.
+	// Remove them first.
+	if strings.HasPrefix(host, "[") && strings.HasSuffix(host, "]") {
+		host = host[1 : len(host)-1]
+	}
+
+	ip := net.ParseIP(host)
+	// host is either IPv4 or IPv6
+	if ip != nil {
+		return ip.IsLoopback()
+	}
+
+	// host is a hostname.
+	if host == "localhost" || strings.HasSuffix(host, ".localhost") {
+		return true
+	}
+	return false
 }
 
 // CSPMiddleware derives frame-ancestors from clients and
--- pkg/auth/dependency/webapp/csp_middleware_test.go
+++ pkg/auth/dependency/webapp/csp_middleware_test.go
@@ -59,12 +59,12 @@ func TestCSPMiddleware(t *testing.T) {
 			So(w.Result().Header.Get("Content-Security-Policy"), ShouldEqual, "frame-ancestors https://example.com https://app.com 'self';")
 		})
 
-		Convey("include only https? redirect URIs", func() {
+		Convey("include https redirect URIs", func() {
 			middleware.Clients = []config.OAuthClientConfiguration{
 				config.OAuthClientConfiguration{
 					"redirect_uris": []interface{}{
 						"https://example.com/path?q=1",
-						"http://localhost/path?q=1",
+						"http://example.com/path?q=1",
 						"com.example://host/path",
 					},
 				},
@@ -73,7 +73,33 @@ func TestCSPMiddleware(t *testing.T) {
 			r, _ := http.NewRequest("GET", "/", nil)
 			h.ServeHTTP(w, r)
 
-			So(w.Result().Header.Get("Content-Security-Policy"), ShouldEqual, "frame-ancestors https://example.com http://localhost 'self';")
+			So(w.Result().Header.Get("Content-Security-Policy"), ShouldEqual, "frame-ancestors https://example.com 'self';")
+		})
+
+		Convey("include http redirect URIs if host is localhost", func() {
+			middleware.Clients = []config.OAuthClientConfiguration{
+				config.OAuthClientConfiguration{
+					"redirect_uris": []interface{}{
+						"http://127.0.0.1/path?q=1",
+						"http://127.0.0.1:8080/path?q=1",
+						"http://[::1]/path?q=1",
+						"http://[::1]:8080/path?q=1",
+						"http://localhost/path?q=1",
+						"http://localhost:8080/path?q=1",
+						"http://skygear.localhost/path?q=1",
+						"http://skygear.localhost:8080/path?q=1",
+
+						"http://example.com/path?q=1",
+						"http://192.168.1.1/path?q=1",
+						"http://skygearlocalhost/path?q=1",
+					},
+				},
+			}
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest("GET", "/", nil)
+			h.ServeHTTP(w, r)
+
+			So(w.Result().Header.Get("Content-Security-Policy"), ShouldEqual, "frame-ancestors http://127.0.0.1 http://127.0.0.1:8080 http://[::1] http://[::1]:8080 http://localhost http://localhost:8080 http://skygear.localhost http://skygear.localhost:8080 'self';")
 		})
 	})
 }
