c7e26b317cfcc2b4fd6025a8d66ebbd747faf7cf
kiootic <kiootic@oursky.com>
2020-03-31 11:24:02 +0800

    Delete old code

--- pkg/core/auth/session/event_store.go
+++
@@ -1,8 +0,0 @@
-package session
-
-import "github.com/skygeario/skygear-server/pkg/core/auth"
-
-type EventStore interface {
-	// AppendAccessEvent appends an access event to the session event stream
-	AppendAccessEvent(s *auth.Session, e *auth.SessionAccessEvent) error
-}
