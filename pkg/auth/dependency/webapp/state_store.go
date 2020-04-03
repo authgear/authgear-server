f7f8c221c9c510c2dc917849ca48dce6bbaffe5b
Louis Chan <louischan@oursky.com>
2020-04-03 20:35:32 +0800

    Add StateStore

--- pkg/auth/dependency/webapp/state_store.go
+++ pkg/auth/dependency/webapp/state_store.go
@@ -1,5 +1,11 @@
 package webapp
 
+import (
+	"errors"
+)
+
+var ErrStateNotFound = errors.New("state not found")
+
 type StateStore interface {
 	Get(id string) (*State, error)
 	Set(state *State) error
