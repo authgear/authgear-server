728f3cfc11f5efef6ab273202120105a9693b101
Louis Chan <louischan@oursky.com>
2020-04-16 16:14:03 +0800

    Rewrite resolveTemplateItem test

--- pkg/core/template/engine_test.go
+++ pkg/core/template/engine_test.go
@@ -8,186 +8,104 @@ import (
 	"github.com/skygeario/skygear-server/pkg/core/config"
 )
 
-func TestEngine(t *testing.T) {
+func TestResolveTemplateItem(t *testing.T) {
 	const typeA config.TemplateItemType = "typeA"
 	const typeB config.TemplateItemType = "typeB"
 	const keyA = "keyA"
 	const keyB = "keyB"
 	specA := Spec{Type: typeA, IsKeyed: true}
-
-	Convey("Engine", t, func() {
-		Convey("resolveTemplateItem", func() {
-			cases := []struct {
-				TemplateItems         []config.TemplateItem
-				PreferredLanguageTags []string
-
-				Spec Spec
-				Key  string
-
-				Expected *config.TemplateItem
-			}{
-				{
-					// No TemplateItems
-					Spec:     specA,
-					Expected: nil,
-				},
-				{
-					// Select type
-					TemplateItems: []config.TemplateItem{
-						config.TemplateItem{
-							Type: typeA,
-						},
-						config.TemplateItem{
-							Type: typeB,
-						},
-					},
-					Spec: specA,
-					Expected: &config.TemplateItem{
-						Type: typeA,
-					},
-				},
-				{
-					// Select key
-					TemplateItems: []config.TemplateItem{
-						config.TemplateItem{
-							Type: typeA,
-						},
-						config.TemplateItem{
-							Type: typeB,
-						},
-						config.TemplateItem{
-							Type: typeA,
-							Key:  keyA,
-						},
-						config.TemplateItem{
-							Type: typeA,
-							Key:  keyB,
-						},
-					},
-					Spec: specA,
-					Key:  keyA,
-					Expected: &config.TemplateItem{
-						Type: typeA,
-						Key:  keyA,
-					},
-				},
-				{
-					// Select the empty language tag
-					// If no preferred languages are given.
-					TemplateItems: []config.TemplateItem{
-						config.TemplateItem{
-							Type: typeA,
-							URI:  "default",
-						},
-						config.TemplateItem{
-							Type:        typeA,
-							LanguageTag: "en-US",
-							URI:         "American English",
-						},
-						config.TemplateItem{
-							Type:        typeA,
-							LanguageTag: "zh-Hant-HK",
-							URI:         "Traditional Chinese in Hong Kong",
-						},
-					},
-					Spec: specA,
-					Expected: &config.TemplateItem{
-						Type: typeA,
-						URI:  "default",
-					},
-				},
-				{
-					// Select the empty language tag
-					// If no preferred languages can be matched.
-					TemplateItems: []config.TemplateItem{
-						config.TemplateItem{
-							Type: typeA,
-							URI:  "default",
-						},
-						config.TemplateItem{
-							Type:        typeA,
-							LanguageTag: "en-US",
-							URI:         "American English",
-						},
-						config.TemplateItem{
-							Type:        typeA,
-							LanguageTag: "zh-Hant-HK",
-							URI:         "Traditional Chinese in Hong Kong",
-						},
-					},
-					PreferredLanguageTags: []string{"ja-JP"},
-					Spec:                  specA,
-					Expected: &config.TemplateItem{
-						Type: typeA,
-						URI:  "default",
-					},
-				},
-				{
-					// Select the best language tag.
-					TemplateItems: []config.TemplateItem{
-						config.TemplateItem{
-							Type: typeA,
-							URI:  "default",
-						},
-						config.TemplateItem{
-							Type:        typeA,
-							LanguageTag: "en-US",
-							URI:         "American English",
-						},
-						config.TemplateItem{
-							Type:        typeA,
-							LanguageTag: "zh-Hant-HK",
-							URI:         "Traditional Chinese in Hong Kong",
-						},
-					},
-					PreferredLanguageTags: []string{"en"},
-					Spec:                  specA,
-					Expected: &config.TemplateItem{
-						Type:        typeA,
-						LanguageTag: "en-US",
-						URI:         "American English",
-					},
-				},
-				{
-					// Select the best language tag.
-					TemplateItems: []config.TemplateItem{
-						config.TemplateItem{
-							Type: typeA,
-							URI:  "default",
-						},
-						config.TemplateItem{
-							Type:        typeA,
-							LanguageTag: "en-US",
-							URI:         "American English",
-						},
-						config.TemplateItem{
-							Type:        typeA,
-							LanguageTag: "zh-Hant-HK",
-							URI:         "Traditional Chinese in Hong Kong",
-						},
-					},
-					PreferredLanguageTags: []string{"ja-JP", "en-GB"},
-					Spec:                  specA,
-					Expected: &config.TemplateItem{
-						Type:        typeA,
-						LanguageTag: "en-US",
-						URI:         "American English",
-					},
-				},
+	Convey("resolveTemplateItem", t, func() {
+		test := func(templateItems []config.TemplateItem, tags []string, key string, expected *config.TemplateItem) {
+			e := NewEngine(NewEngineOptions{
+				TemplateItems: templateItems,
+			})
+			e = e.WithPreferredLanguageTags(tags)
+			actual, err := e.resolveTemplateItem(specA, key)
+			if expected == nil {
+				So(err, ShouldBeError)
+			} else {
+				So(actual, ShouldResemble, expected)
 			}
-			for _, c := range cases {
-				e := NewEngine(NewEngineOptions{
-					TemplateItems: c.TemplateItems,
-				})
-				e = e.WithPreferredLanguageTags(c.PreferredLanguageTags)
+		}
 
-				actual, err := e.resolveTemplateItem(c.Spec, c.Key)
-				if c.Expected == nil {
-					So(err, ShouldBeError)
-				} else {
-					So(actual, ShouldResemble, c.Expected)
-				}
-			}
+		// No TemplateItems
+		test(nil, nil, "", nil)
+
+		// Select type
+		test([]config.TemplateItem{
+			config.TemplateItem{
+				Type: typeA,
+			},
+			config.TemplateItem{
+				Type: typeB,
+			},
+		}, nil, "", &config.TemplateItem{
+			Type: typeA,
+		})
+
+		// Select key
+		test([]config.TemplateItem{
+			config.TemplateItem{
+				Type: typeA,
+			},
+			config.TemplateItem{
+				Type: typeB,
+			},
+			config.TemplateItem{
+				Type: typeA,
+				Key:  keyA,
+			},
+			config.TemplateItem{
+				Type: typeA,
+				Key:  keyB,
+			},
+		}, nil, keyA, &config.TemplateItem{
+			Type: typeA,
+			Key:  keyA,
+		})
+
+		// Select the empty language tag
+		// If no preferred languages are given.
+		test([]config.TemplateItem{
+			config.TemplateItem{
+				Type: typeA,
+				URI:  "default",
+			},
+			config.TemplateItem{
+				Type:        typeA,
+				LanguageTag: "en-US",
+				URI:         "American English",
+			},
+			config.TemplateItem{
+				Type:        typeA,
+				LanguageTag: "zh-Hant-HK",
+				URI:         "Traditional Chinese in Hong Kong",
+			},
+		}, nil, "", &config.TemplateItem{
+			Type: typeA,
+			URI:  "default",
+		})
+
+		// Select the best language tag.
+		test([]config.TemplateItem{
+			config.TemplateItem{
+				Type: typeA,
+				URI:  "default",
+			},
+			config.TemplateItem{
+				Type:        typeA,
+				LanguageTag: "en-US",
+				URI:         "American English",
+			},
+			config.TemplateItem{
+				Type:        typeA,
+				LanguageTag: "zh-Hant-HK",
+				URI:         "Traditional Chinese in Hong Kong",
+			},
+		}, []string{"en"}, "", &config.TemplateItem{
+			Type:        typeA,
+			LanguageTag: "en-US",
+			URI:         "American English",
 		})
 	})
 }
