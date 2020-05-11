3f6213d0a13526cd7f14e8740aa822c3ebc3544d
kiootic <kiootic@oursky.com>
2020-05-11 17:22:14 +0800

    Handle prompt=login in Auth UI web app

--- pkg/auth/dependency/webapp/auth_entry_point_middleware.go
+++ pkg/auth/dependency/webapp/auth_entry_point_middleware.go
@@ -6,12 +6,13 @@ import (
 	"github.com/skygeario/skygear-server/pkg/auth/dependency/auth"
 )
 
-type RequireNotAuthenticatedMiddleware struct{}
+type AuthEntryPointMiddleware struct{}
 
-func (m RequireNotAuthenticatedMiddleware) Handle(next http.Handler) http.Handler {
+func (m AuthEntryPointMiddleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := auth.GetUser(r.Context())
-		if user != nil {
+		requireLoginPrompt := r.URL.Query().Get("prompt") == "login"
+		if user != nil && !requireLoginPrompt {
 			RedirectToRedirectURI(w, r)
 		} else {
 			next.ServeHTTP(w, r)
