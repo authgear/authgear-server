4f7b2a35f565bb9d7464c60bfe1de792ded9610f
Carmen <carmen@oursky.com>
2020-05-13 15:31:03 +1000

    Add interaction flow test for adding identity

--- pkg/auth/dependency/interaction/provider_flow_test.go
+++ pkg/auth/dependency/interaction/provider_flow_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/skygeario/skygear-server/pkg/auth/dependency/hook"
 	"github.com/skygeario/skygear-server/pkg/auth/dependency/identity"
 	"github.com/skygeario/skygear-server/pkg/auth/dependency/interaction"
+	"github.com/skygeario/skygear-server/pkg/auth/model"
 	"github.com/skygeario/skygear-server/pkg/core/authn"
 	"github.com/skygeario/skygear-server/pkg/core/config"
 	coretime "github.com/skygeario/skygear-server/pkg/core/time"
@@ -425,6 +426,194 @@ func TestProviderFlow(t *testing.T) {
 			So(err, ShouldBeNil)
 		})
 
+		Convey("Add identity", func() {
+			userID := "user_id_1"
+			p.Config = &config.AuthenticationConfiguration{
+				PrimaryAuthenticators: []string{"password"},
+			}
+
+			Convey("should not need to setup authenticator", func() {
+				// setup
+				loginIDClaims := map[string]interface{}{
+					identity.IdentityClaimLoginIDKey:   "email",
+					identity.IdentityClaimLoginIDValue: "second@example.com",
+				}
+				ii := &identity.Info{
+					ID:     "identity_id_2",
+					Type:   authn.IdentityTypeLoginID,
+					Claims: loginIDClaims,
+				}
+				oii := &identity.Info{
+					ID:   "identity_id_1",
+					Type: authn.IdentityTypeLoginID,
+					Claims: map[string]interface{}{
+						identity.IdentityClaimLoginIDKey:   "email",
+						identity.IdentityClaimLoginIDValue: "user@example.com",
+					},
+				}
+				ai := &authenticator.Info{
+					ID:     "authenticator_id_1",
+					Type:   authn.AuthenticatorTypePassword,
+					Props:  map[string]interface{}{},
+					Secret: "password",
+				}
+				as := &authenticator.Spec{
+					Type:  authn.AuthenticatorTypePassword,
+					Props: map[string]interface{}{},
+				}
+				identityProvider.EXPECT().New(
+					gomock.Eq(userID), gomock.Eq(authn.IdentityTypeLoginID), gomock.Eq(loginIDClaims),
+				).Return(ii)
+				// return user's existing identities
+				identityProvider.EXPECT().ListByUser(
+					gomock.Eq(userID),
+				).Return([]*identity.Info{oii}, nil)
+				// should include both old and new identities in validation
+				identityProvider.EXPECT().Validate(
+					gomock.Eq([]*identity.Info{ii, oii}),
+				).Return(nil)
+
+				// return new identity related authenticator spec
+				identityProvider.EXPECT().RelateIdentityToAuthenticator(
+					gomock.Eq(ii.ToSpec()), gomock.Eq(as),
+				).Return(as)
+
+				// user has setup authenticator before, no need to setup
+				// authenticator
+				authenticatorProvider.EXPECT().ListByIdentity(
+					gomock.Eq(userID), gomock.Eq(ii),
+				).Return([]*authenticator.Info{ai}, nil)
+
+				// start flow
+				i, err := p.NewInteractionAddIdentity(&interaction.IntentAddIdentity{
+					Identity: identity.Spec{
+						Type:   authn.IdentityTypeLoginID,
+						Claims: loginIDClaims,
+					},
+				}, "", userID)
+
+				So(err, ShouldBeNil)
+
+				state, err := p.GetInteractionState(i)
+				So(err, ShouldBeNil)
+				So(state.Steps, ShouldHaveLength, 1)
+				So(state.Steps[0].Step, ShouldEqual, interaction.StepCommit)
+			})
+
+			Convey("should setup authenticator", func() {
+				// user who had oauth identity without authenticator add login id identity
+
+				// setup
+				loginIDClaims := map[string]interface{}{
+					identity.IdentityClaimLoginIDKey:   "email",
+					identity.IdentityClaimLoginIDValue: "second@example.com",
+				}
+				ii := &identity.Info{
+					ID:     "identity_id_2",
+					Type:   authn.IdentityTypeLoginID,
+					Claims: loginIDClaims,
+				}
+				oii := &identity.Info{
+					ID:   "identity_id_1",
+					Type: authn.IdentityTypeOAuth,
+					Claims: map[string]interface{}{
+						identity.IdentityClaimOAuthProvider: map[string]interface{}{
+							"type":   "azureadv2",
+							"tenant": "example",
+						},
+						identity.IdentityClaimOAuthSubjectID: "9A8822AA-4F18-4E4C-84AF-E0FD9AB86CB2",
+						identity.IdentityClaimOAuthProfile:   map[string]interface{}{},
+					},
+				}
+				as := &authenticator.Spec{
+					Type:  authn.AuthenticatorTypePassword,
+					Props: map[string]interface{}{},
+				}
+				ai := &authenticator.Info{
+					ID:     "authenticator_id_1",
+					Type:   authn.AuthenticatorTypePassword,
+					Props:  map[string]interface{}{},
+					Secret: "password",
+				}
+				identityProvider.EXPECT().New(
+					gomock.Eq(userID), gomock.Eq(ii.Type), gomock.Eq(ii.Claims),
+				).Return(ii)
+				// return user's existing identities
+				identityProvider.EXPECT().ListByUser(
+					gomock.Eq(userID),
+				).Return([]*identity.Info{oii}, nil)
+				// validation should have the new identity only, since the
+				// existing identity is in different type
+				identityProvider.EXPECT().Validate(
+					gomock.Eq([]*identity.Info{ii}),
+				).Return(nil)
+
+				// return new identity related authenticator spec
+				identityProvider.EXPECT().RelateIdentityToAuthenticator(
+					gomock.Eq(ii.ToSpec()), gomock.Eq(as),
+				).Return(as).AnyTimes()
+
+				identityProvider.EXPECT().CreateAll(gomock.Any(), gomock.Eq([]*identity.Info{ii})).Return(nil)
+				var emptyIdentityInfoList []*identity.Info
+				identityProvider.EXPECT().UpdateAll(gomock.Any(), gomock.Eq(emptyIdentityInfoList)).Return(nil)
+				identityProvider.EXPECT().DeleteAll(gomock.Any(), gomock.Eq(emptyIdentityInfoList)).Return(nil)
+				identityProvider.EXPECT().Get(gomock.Any(), ii.Type, ii.ID).Return(ii, nil)
+
+				// no existing authenticator for new login id identity
+				authenticatorProvider.EXPECT().ListByIdentity(
+					gomock.Eq(userID), gomock.Eq(ii),
+				).Return([]*authenticator.Info{}, nil).AnyTimes()
+
+				authenticatorProvider.EXPECT().New(
+					gomock.Any(), gomock.Eq(*as), gomock.Eq("password"),
+				).Return([]*authenticator.Info{ai}, nil)
+				authenticatorProvider.EXPECT().CreateAll(gomock.Any(), gomock.Eq([]*authenticator.Info{ai})).Return(nil)
+				var emptyAuthenticatorInfoList []*authenticator.Info
+				authenticatorProvider.EXPECT().DeleteAll(gomock.Any(), gomock.Eq(emptyAuthenticatorInfoList)).Return(nil)
+
+				// get user for hook
+				userProvider.EXPECT().Get(gomock.Eq(userID)).Return(&model.User{}, nil)
+
+				store.EXPECT().Delete(gomock.Any()).Return(nil)
+
+				// start flow
+				i, err := p.NewInteractionAddIdentity(&interaction.IntentAddIdentity{
+					Identity: identity.Spec{
+						Type:   authn.IdentityTypeLoginID,
+						Claims: loginIDClaims,
+					},
+				}, "", userID)
+
+				So(err, ShouldBeNil)
+
+				state, err := p.GetInteractionState(i)
+				So(err, ShouldBeNil)
+				So(state.Steps, ShouldHaveLength, 1)
+				So(state.Steps[0].Step, ShouldEqual, interaction.StepSetupPrimaryAuthenticator)
+				So(state.Steps[0].AvailableAuthenticators, ShouldNotBeEmpty)
+				So(state.Steps[0].AvailableAuthenticators[0], ShouldResemble, authenticator.Spec{
+					Type:  authn.AuthenticatorTypePassword,
+					Props: map[string]interface{}{},
+				})
+
+				// setup primary authenticator
+				err = p.PerformAction(i, interaction.StepSetupPrimaryAuthenticator, &interaction.ActionSetupAuthenticator{
+					Authenticator: state.Steps[0].AvailableAuthenticators[0],
+					Secret:        "password",
+				})
+				So(err, ShouldBeNil)
+
+				state, err = p.GetInteractionState(i)
+				So(err, ShouldBeNil)
+				So(state.Steps, ShouldHaveLength, 2)
+				So(state.Steps[0].Step, ShouldEqual, interaction.StepSetupPrimaryAuthenticator)
+				So(state.Steps[1].Step, ShouldEqual, interaction.StepCommit)
+
+				_, err = p.Commit(i)
+				So(err, ShouldBeNil)
+			})
+
+		})
 	})
 
 }
