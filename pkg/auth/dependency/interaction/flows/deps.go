9153854408238dcfa99c7c81947941f700af99ba
Carmen <carmen@oursky.com>
2020-05-14 14:08:38 +1000

    Add password interaction flow

--- pkg/auth/dependency/interaction/flows/deps.go
+++ pkg/auth/dependency/interaction/flows/deps.go
@@ -36,5 +36,6 @@ var DependencySet = wire.NewSet(
 	wire.Struct(new(WebAppFlow), "*"),
 	wire.Struct(new(AuthAPIFlow), "*"),
 	wire.Struct(new(AnonymousFlow), "*"),
+	wire.Struct(new(PasswordFlow), "*"),
 	ProvideUserController,
 )
