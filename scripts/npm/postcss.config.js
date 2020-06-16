55e9cb80cf5b333795ba92127dd6084bf984729e
Louis Chan <louischan@oursky.com>
2020-06-16 15:54:13 +0800

    Extract browserslist

--- scripts/npm/postcss.config.js
+++ scripts/npm/postcss.config.js
@@ -4,16 +4,7 @@ var cssnano = require("cssnano");
 
 module.exports = {
   plugins: [
-    presetEnv({
-      browsers: [
-        "last 2 chrome versions",
-        "last 2 firefox versions",
-        "Firefox ESR",
-        "IE >= 11",
-        "iOS >= 11",
-        "Android >= 5.0",
-      ],
-    }),
+    presetEnv(),
     normalize(),
     cssnano(),
   ],
