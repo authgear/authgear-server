373fe84fa03221c9da19611d4a2d08cbd046469d
Louis Chan <louischan@oursky.com>
2020-06-29 16:12:38 +0800

    Remove irrelevant files

--- scripts/deploy-gh-pages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -eu
-
-scripts/update-gh-pages.sh
-git push https://skygear-bot:$GITHUB_TOKEN@github.com/SkygearIO/skygear-server.git gh-pages > /dev/null 2>/dev/null
--- scripts/preview-doc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -eu
-MODULE=$1
-
-mkdir -p tmp
-make -e DOC_PATH="tmp/doc-$MODULE.yaml" "generate-doc-$MODULE"
-docker run --rm -p 9001:8080 -e URL="/doc-$MODULE.yaml" -v "$PWD/tmp/doc-$MODULE.yaml:/usr/share/nginx/html/doc-$MODULE.yaml" swaggerapi/swagger-ui &
-sleep 1
-python -m webbrowser http://localhost:9001
-wait
