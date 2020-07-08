e067e916edfe52d74e3b5f31ac8fefeddf607a60
Louis Chan <louischan@oursky.com>
2020-07-08 18:06:52 +0800

    GET /logout

--- pkg/auth/handler/webapp/logout.go
+++ pkg/auth/handler/webapp/logout.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/authgear/authgear-server/pkg/auth/config"
 	"github.com/authgear/authgear-server/pkg/auth/dependency/auth"
-	"github.com/authgear/authgear-server/pkg/auth/dependency/webapp"
 	"github.com/authgear/authgear-server/pkg/db"
 	"github.com/authgear/authgear-server/pkg/httproute"
 	"github.com/authgear/authgear-server/pkg/template"
@@ -54,21 +53,32 @@ type LogoutSessionManager interface {
 }
 
 type LogoutHandler struct {
+	Database       *db.Handle
 	ServerConfig   *config.ServerConfig
 	SessionManager LogoutSessionManager
-	Database       *db.Handle
+	BaseViewModel  *BaseViewModeler
+	Renderer       Renderer
 }
 
 func (h *LogoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.Database.WithTx(func() error {
-		if r.Method == "POST" && r.Form.Get("x_action") == "logout" {
-			sess := auth.GetSession(r.Context())
-			h.SessionManager.Logout(sess, w)
-			webapp.RedirectToRedirectURI(w, r, h.ServerConfig.TrustProxy)
-		} else {
-			// FIXME(webapp): logout
-			// h.RenderProvider.WritePage(w, r, webapp.TemplateItemTypeAuthUILogoutHTML, nil)
-		}
-		return nil
-	})
+	if r.Method == "GET" {
+		baseViewModel := h.BaseViewModel.ViewModel(r, nil)
+
+		data := map[string]interface{}{}
+
+		Embed(data, baseViewModel)
+
+		h.Renderer.Render(w, r, TemplateItemTypeAuthUILogoutHTML, data)
+		return
+	}
+
+	// FIXME(webapp): logout
+	// h.Database.WithTx(func() error {
+	// 	if r.Method == "POST" && r.Form.Get("x_action") == "logout" {
+	// 		sess := auth.GetSession(r.Context())
+	// 		h.SessionManager.Logout(sess, w)
+	// 		webapp.RedirectToRedirectURI(w, r, h.ServerConfig.TrustProxy)
+	// 	}
+	// 	return nil
+	// })
 }
