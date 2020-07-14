345f874f190c107d63f598538d3aecee01816b82
Louis Chan <louischan@oursky.com>
2020-07-14 11:22:26 +0800

    Do not type cast to APIError
    skyerr.AsAPIError does this for us.

--- pkg/auth/handler/webapp/as_api_error.go
+++ pkg/auth/handler/webapp/as_api_error.go
@@ -5,9 +5,6 @@ import (
 )
 
 func asAPIError(anyError interface{}) *skyerr.APIError {
-	if apiError, ok := anyError.(*skyerr.APIError); ok {
-		return apiError
-	}
 	if err, ok := anyError.(error); ok {
 		return skyerr.AsAPIError(err)
 	}
