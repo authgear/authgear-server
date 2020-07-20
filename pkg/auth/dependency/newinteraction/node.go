de7f0022ebc26c69d829b85234a94f42437b5a5a
kiootic <kiootic@oursky.com>
2020-07-20 20:18:57 +0800

    Add comments on function semantics

--- pkg/auth/dependency/newinteraction/node.go
+++ pkg/auth/dependency/newinteraction/node.go
@@ -7,13 +7,23 @@ import (
 )
 
 var ErrIncompatibleInput = errors.New("incompatible input type for this node")
+var ErrSameNode = errors.New("the edge points to the same node")
 
 type Node interface {
+	// Apply the effects of this node to context.
+	// This may be ran multiple times, due to replaying the graph.
+	// So no external visible side effect is allowed.
 	Apply(ctx *Context, graph *Graph) error
 	DeriveEdges(ctx *Context, graph *Graph) ([]Edge, error)
 }
 
 type Edge interface {
+	// Instantiate instantiates the node pointed by the edge.
+	// It is ran once only for the pointed node, so side effects visible
+	// outside the transaction (e.g. sending messages) is allowed.
+	// It may return ErrSameNode if the source & destination node of the edge
+	// is the same (no nodes are added to graph in this case).
+	// This is used to model OOB trigger, session creation, etc..
 	Instantiate(ctx *Context, graph *Graph, input interface{}) (Node, error)
 }
 
