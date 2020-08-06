92eed9d729cf0a59c73f7743cb2b7d573abdead3
kiootic <kiootic@oursky.com>
2020-08-06 15:46:52 +0800

    Ignore current user if has state at entry points

--- pkg/auth/dependency/webapp/auth_entry_point_middleware.go
+++ pkg/auth/dependency/webapp/auth_entry_point_middleware.go
@@ -14,8 +14,8 @@ type AuthEntryPointMiddleware struct {
 func (m AuthEntryPointMiddleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := auth.GetUser(r.Context())
-		requireLoginPrompt := r.URL.Query().Get("prompt") == "login"
-		if user != nil && !requireLoginPrompt {
+		hasState := r.URL.Query().Get("x_sid") != ""
+		if user != nil && !hasState {
 			redirectURI := GetRedirectURI(r, m.ServerConfig.TrustProxy)
 			http.Redirect(w, r, redirectURI, http.StatusFound)
 		} else {
