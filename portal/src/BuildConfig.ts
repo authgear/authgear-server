bdc06798b7c28658364418d3a6f674244b0ceee1
Louis Chan <louischan@oursky.com>
2020-08-19 15:41:40 +0800

    Clarify BuildConfig

--- portal/src/BuildConfig.ts
+++ portal/src/BuildConfig.ts
@@ -3,3 +3,4 @@
 export const BUILD_TIME_VARIABLE: string = process.env.BUILD_TIME_VARIABLE;
 // Parcel supports process.env.* to inject build time variable via environment variable.
 // However, we are not going to use much build time variable.
+// This above one is just for documentation purpose.
