784099912336456891ff7361564fd14c08b04ca4
Louis Chan <louischan@oursky.com>
2020-08-20 15:36:10 +0800

    Fetch runtime config on app launch

--- portal/src/index.tsx
+++ portal/src/index.tsx
@@ -8,12 +8,27 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 import "normalize.css";
-
 import "./index.scss";
 
 import React from "react";
 import { render } from "react-dom";
+import authgear from "@authgear/web";
 
 import App from "./App";
 
-render(<App />, document.getElementById("react-app-root"));
+fetch("/api/runtime-config.json")
+  .then(async (response) => {
+    const runtimeConfig = await response.json();
+    await authgear.configure({
+      clientID: runtimeConfig.authgear_client_id,
+      endpoint: runtimeConfig.authgear_endpoint,
+    });
+    render(<App />, document.getElementById("react-app-root"));
+  })
+  .catch((err) => {
+    console.error(err);
+    render(
+      <p>Failed to initialize the app. Please refresh this page to retry</p>,
+      document.getElementById("react-app-root")
+    );
+  });
