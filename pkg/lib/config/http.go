ba2d8c15f704b1ebfff72c8d7ed97732993570e9
kiootic <kiootic@oursky.com>
2020-08-24 13:32:31 +0800

    Replace host config with public origin

--- pkg/lib/config/http.go
+++ pkg/lib/config/http.go
@@ -5,17 +5,16 @@ var _ = Schema.Add("HTTPConfig", `
 	"type": "object",
 	"additionalProperties": false,
 	"properties": {
-		"hosts": { "type": "array", "items": { "type": "string" } },
-		"admin_hosts": { "type": "array", "items": { "type": "string" } },
+		"public_origin": { "type": "string" },
 		"allowed_origins": { "type": "array", "items": { "type": "string" } },
 		"cookie_prefix": { "type": "string" }
-	}
+	},
+	"required": [ "public_origin" ]
 }
 `)
 
 type HTTPConfig struct {
-	Hosts          []string `json:"hosts,omitempty"`
-	AdminHosts     []string `json:"admin_hosts,omitempty"`
+	PublicOrigin   string   `json:"public_origin"`
 	AllowedOrigins []string `json:"allowed_origins,omitempty"`
 	CookiePrefix   string   `json:"cookie_prefix,omitempty"`
 }
