f4191617236f066b9dc7cbec93a14720737e3615
Louis Chan <louischan@oursky.com>
2020-08-27 17:22:58 +0800

    Convert non-dummy labels to use localization

--- portal/src/ShowLoading.tsx
+++ portal/src/ShowLoading.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { FormattedMessage } from "@oursky/react-messageformat";
 
 const ShowLoading: React.FC = function ShowLoading() {
-  return <div>Loading...</div>;
+  return (
+    <p>
+      <FormattedMessage id="loading" />
+    </p>
+  );
 };
 
 export default ShowLoading;
