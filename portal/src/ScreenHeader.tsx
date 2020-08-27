0510849cbfc4fce2b59aeae9b02c7a0e0ce0a0fa
Louis Chan <louischan@oursky.com>
2020-08-27 14:04:00 +0800

    Switch ScreenHeader to use fluent UI

--- portal/src/ScreenHeader.tsx
+++ portal/src/ScreenHeader.tsx
@@ -1,7 +1,12 @@
 import React, { useCallback } from "react";
 import authgear from "@authgear/web";
-
+import { IconButton } from "@fluentui/react";
 import styles from "./ScreenHeader.module.scss";
+import srcLogo from "./image/screen-header-logo@3x.png";
+
+const iconProps = {
+  iconName: "SignOut",
+};
 
 const ScreenHeader: React.FC = function ScreenHeader() {
   const redirectURI = window.location.origin + "/";
@@ -18,14 +23,14 @@ const ScreenHeader: React.FC = function ScreenHeader() {
 
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Authgear Portal</h1>
-      <button
-        className={styles.logoutButton}
+      <img className={styles.logo} alt="Authgear" src={srcLogo} />
+      <IconButton
         type="button"
+        iconProps={iconProps}
         onClick={onClickLogout}
-      >
-        Click here to logout
-      </button>
+        title="Logout"
+        ariaLabel="Logout"
+      />
     </header>
   );
 };
