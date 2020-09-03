abb23706bb107826688c71f6644717530f282898
Louis Chan <louischan@oursky.com>
2020-09-03 17:57:15 +0800

    Add breadcrumb to /users

--- portal/src/graphql/adminapi/UsersScreen.tsx
+++ portal/src/graphql/adminapi/UsersScreen.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { FormattedMessage } from "@oursky/react-messageformat";
+import NavBreadcrumb from "../../NavBreadcrumb";
 import UsersList from "./UsersList";
 
 const UsersScreen: React.FC = function UsersScreen() {
-  return <UsersList />;
+  const items = useMemo(() => {
+    return [{ to: ".", label: <FormattedMessage id="UsersScreen.title" /> }];
+  }, []);
+  return (
+    <>
+      <NavBreadcrumb items={items} />
+      <UsersList />
+    </>
+  );
 };
 
 export default UsersScreen;
