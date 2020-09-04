18e3370713324aa9c4597db599305a42b23c6e81
axolotl0212 <allenl0212@gmail.com>
2020-09-04 15:44:13 +0800

    Setup tabs in Authentication screen

---
+++ portal/src/graphql/portal/AuthenticationAuthenticatorSettings.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./AuthenticationAuthenticatorSettings.module.scss";
+
+const AuthenticationAuthenticatorSettings: React.FC = function AuthenticationAuthenticatorSettings() {
+  return <div className={styles.root}></div>;
+};
+
+export default AuthenticationAuthenticatorSettings;
---
+++ portal/src/graphql/portal/AuthenticationLoginIDSettings.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./AuthenticationLoginIDSettings.module.scss";
+
+const AuthenticationLoginIDSettings: React.FC = function AuthenticationLoginIDSettings() {
+  return <div className={styles.root}></div>;
+};
+
+export default AuthenticationLoginIDSettings;
