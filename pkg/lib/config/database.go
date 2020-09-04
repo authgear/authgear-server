4273638c6b711ac0da7228e77a0d8b5c3c8f0eca
Louis Chan <louischan@oursky.com>
2020-09-04 18:54:37 +0800

    Support db.ConnMaxIdleTime
    ref #217

--- pkg/lib/config/database.go
+++ pkg/lib/config/database.go
@@ -7,6 +7,7 @@ var _ = Schema.Add("DatabaseConfig", `
 	"properties": {
 		"max_open_connection": { "type": "integer", "minimum": 0 },
 		"max_idle_connection": { "type": "integer", "minimum": 0 },
+		"idle_connection_timeout_seconds": { "type": "integer", "minimum": 0 },
 		"max_connection_lifetime_seconds": { "type": "integer", "minimum": 0 }
 	}
 }
@@ -15,6 +16,7 @@ var _ = Schema.Add("DatabaseConfig", `
 type DatabaseConfig struct {
 	MaxOpenConnection     *int             `json:"max_open_connection,omitempty"`
 	MaxIdleConnection     *int             `json:"max_idle_connection,omitempty"`
+	IdleConnectionTimeout *DurationSeconds `json:"idle_connection_timeout_seconds,omitempty"`
 	MaxConnectionLifetime *DurationSeconds `json:"max_connection_lifetime_seconds,omitempty"`
 }
 
@@ -22,12 +24,20 @@ func (c *DatabaseConfig) SetDefaults() {
 	if c.MaxOpenConnection == nil {
 		c.MaxOpenConnection = newInt(2)
 	}
+
 	if c.MaxIdleConnection == nil {
 		c.MaxIdleConnection = newInt(2)
 	}
+
 	if c.MaxConnectionLifetime == nil {
 		// 30 minutes
 		t := DurationSeconds(1800)
 		c.MaxConnectionLifetime = &t
+	}
+
+	if c.IdleConnectionTimeout == nil {
+		// 5 minutes
+		t := DurationSeconds(300)
+		c.IdleConnectionTimeout = &t
 	}
 }
