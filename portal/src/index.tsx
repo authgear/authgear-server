b8aaabde9fd82fcc8c6f1bd3e97b2971986c115a
Louis Chan <louischan@oursky.com>
2020-09-05 14:06:06 +0800

    Run axe in development

--- portal/src/index.tsx
+++ portal/src/index.tsx
@@ -1,3 +1,4 @@
+/* global process */
 // This following 2 lines are extremely important.
 // Since we do not provide our babel.config.json,
 // Parcel provides a default one for us.
@@ -20,4 +21,42 @@ import { theme } from "./theme";
 initializeIcons();
 
 loadTheme(theme);
+
+// Run axe every 3 seconds to discover accessibility issues.
+// Ideally we should use react-axe instead but it has the following issue
+// https://github.com/dequelabs/react-axe/issues/122
+// making it useless in our setup.
+// So we use axe-core directly with a periodic check.
+if (process.env.NODE_ENV === "development") {
+  // Use dynamic import so that parcel can exclude it from production build.
+  import("axe-core").then(
+    (axe) => {
+      let violations: any[] = [];
+      const checkAndReport = () => {
+        axe.run().then(
+          (results) => {
+            const a = JSON.stringify(violations);
+            const b = JSON.stringify(results.violations);
+            if (a !== b) {
+              violations = results.violations;
+              if (violations.length > 0) {
+                console.warn("axe violations", violations);
+              } else {
+                console.warn("axe violations are all cleared!");
+              }
+            }
+          },
+          (err) => console.error(err)
+        );
+      };
+
+      checkAndReport();
+      setInterval(checkAndReport, 3000);
+    },
+    (err) => {
+      console.error(err);
+    }
+  );
+}
+
 render(<ReactApp />, document.getElementById("react-app-root"));
