5bde2eac533c188ff393001a45c1063cb352761b
axolotl0212 <allenl0212@gmail.com>
2020-09-09 23:44:59 +0800

    Layout user detail screen session tab

--- portal/src/graphql/adminapi/UserDetailsSession.tsx
+++ portal/src/graphql/adminapi/UserDetailsSession.tsx
@@ -1,9 +1,83 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
+import { DetailsList, IColumn, SelectionMode } from "@fluentui/react";
+import { Context, FormattedMessage } from "@oursky/react-messageformat";
+
+import { formatDatetime } from "../../util/formatDatetime";
 
 import styles from "./UserDetailsSession.module.scss";
 
+interface UserDetailsSession {
+  deviceName: string | null;
+  lastActivity: string | null;
+  ipAddress: string | null;
+}
+
+// TODO: replace with actual data
+const mockSessionData: UserDetailsSession[] = [
+  {
+    deviceName: "iPhone 11 Pro",
+    lastActivity: "2021-12-15T15:55:20.309775Z",
+    ipAddress: "10.2.4.2",
+  },
+  {
+    deviceName: "Chrome 83.0.4.000.84 (DESKTOP-L)",
+    lastActivity: "2021-12-15T15:55:20.309775Z",
+    ipAddress: "10.2.4.2",
+  },
+  {
+    deviceName: "Pixel XL",
+    lastActivity: "2021-12-15T15:55:20.309775Z",
+    ipAddress: "10.2.4.2",
+  },
+];
+
 const UserDetailsSession: React.FC = function UserDetailsSession() {
-  return <div className={styles.root}></div>;
+  const { locale, renderToString } = useContext(Context);
+  const sessionColumns: IColumn[] = [
+    {
+      key: "deviceName",
+      fieldName: "deviceName",
+      name: renderToString("UserDetails.session.devices"),
+      minWidth: 300,
+      maxWidth: 300,
+    },
+    {
+      key: "lastActivity",
+      fieldName: "lastActivity",
+      name: renderToString("UserDetails.session.last-activity"),
+      minWidth: 150,
+      maxWidth: 150,
+    },
+    {
+      key: "ipAddress",
+      fieldName: "ipAddress",
+      name: renderToString("UserDetails.session.ip-address"),
+      minWidth: 75,
+      maxWidth: 75,
+    },
+  ];
+
+  const sessionListItems = useMemo(
+    () =>
+      mockSessionData.map((session) => {
+        const lastActivityText = formatDatetime(locale, session.lastActivity);
+        return { ...session, lastActivity: lastActivityText };
+      }),
+    [locale]
+  );
+
+  return (
+    <div className={styles.root}>
+      <h3 className={styles.header}>
+        <FormattedMessage id="UserDetails.session.header" />
+      </h3>
+      <DetailsList
+        items={sessionListItems}
+        columns={sessionColumns}
+        selectionMode={SelectionMode.none}
+      />
+    </div>
+  );
 };
 
 export default UserDetailsSession;
