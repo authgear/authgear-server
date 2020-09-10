a75c48b8746b9c2eaf4ac050d543fcb7a3806c24
axolotl0212 <allenl0212@gmail.com>
2020-09-10 15:32:10 +0800

    Use CSS grid to simplify style

--- portal/src/graphql/adminapi/UserDetailSummary.tsx
+++ portal/src/graphql/adminapi/UserDetailSummary.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import cn from "classnames";
-import { Persona } from "@fluentui/react";
+import { Persona, Text } from "@fluentui/react";
 import { Context, FormattedMessage } from "@oursky/react-messageformat";
 
 import { formatDatetime } from "../../util/formatDatetime";
@@ -30,29 +30,23 @@ const UserDetailSummary: React.FC<UserDetailSummaryProps> = function UserDetailS
   }, [locale, lastLoginAtISO]);
 
   return (
-    <div className={cn(styles.root, className)}>
+    <section className={cn(styles.root, className)}>
       <Persona className={styles.profilePic} />
-      <div className={styles.userData}>
-        <div className={styles.userInfo}>
-          {email && <div className={styles.userInfoRow}>{email}</div>}
-          {username && <div className={styles.userInfoRow}>{username}</div>}
-        </div>
-        <div className={styles.userUpdates}>
-          <div className={styles.userInfoRow}>
-            <FormattedMessage
-              id="UserDetails.signed-up"
-              values={{ datetime: formatedSignedUp ?? "" }}
-            />
-          </div>
-          <div className={styles.userInfoRow}>
-            <FormattedMessage
-              id="UserDetails.last-login-at"
-              values={{ datetime: formatedLastLogin ?? "" }}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+      <Text className={styles.email}>{email ?? ""}</Text>
+      <Text className={styles.username}>{username ?? ""}</Text>
+      <Text className={styles.createdAt}>
+        <FormattedMessage
+          id="UserDetails.signed-up"
+          values={{ datetime: formatedSignedUp ?? "" }}
+        />
+      </Text>
+      <Text className={styles.lastLoginAt}>
+        <FormattedMessage
+          id="UserDetails.last-login-at"
+          values={{ datetime: formatedLastLogin ?? "" }}
+        />
+      </Text>
+    </section>
   );
 };
 
