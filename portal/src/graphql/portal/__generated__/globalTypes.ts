bab89521006ff4d79c8f1d8227c9cb0829e0aa2b
axolotl0212 <allenl0212@gmail.com>
2020-09-12 01:49:09 +0800

    Fix input type is not correctly generated
    Cause:
    We have two schema, one for portal API, one for admin API. Hence the is
    2 codegen command run sequentially. However both command have no
      `globalTypesFile` specified, hence both command using exactly same
    path, namely `__generated__/globalTypes.ts`. Hence the global types
    generated by second command overwrites the first one.
    
    In previous commit a mutation in portal API is added, and codegen of portal API is run before admin API,
    so admin API global types, which is empty, overwrites the needed input
    type definition, causes typecheck throwing error

---
+++ portal/src/graphql/portal/__generated__/globalTypes.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+/**
+ * A configuration file to update/create.
+ */
+export interface AppConfigFile {
+  content: string;
+  path: string;
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
--- portal/src/graphql/portal/mutations/__generated__/UpdateAppConfigMutation.ts
+++ portal/src/graphql/portal/mutations/__generated__/UpdateAppConfigMutation.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { AppConfigFile } from "./../../../../../__generated__/globalTypes";
+import { AppConfigFile } from "./../../__generated__/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: UpdateAppConfigMutation
