946d54d36933fcacd735cab5cfff20b801a1733a
Louis Chan <louischan@oursky.com>
2020-09-16 12:28:02 +0800

    Add change password intent

---
+++ pkg/lib/interaction/intents/change_password.go
@@ -0,0 +1,51 @@
+package intents
+
+import (
+	"fmt"
+
+	"github.com/authgear/authgear-server/pkg/lib/interaction"
+	"github.com/authgear/authgear-server/pkg/lib/interaction/nodes"
+)
+
+func init() {
+	interaction.RegisterIntent(&IntentChangePassword{})
+}
+
+type IntentChangePassword struct {
+	UserID string                          `json:"user_id"`
+	Stage  interaction.AuthenticationStage `json:"stage"`
+}
+
+func NewIntentChangePrimaryPassword(userID string) *IntentChangePassword {
+	return &IntentChangePassword{
+		UserID: userID,
+		Stage:  interaction.AuthenticationStagePrimary,
+	}
+}
+
+func NewIntentChangeSecondaryPassword(userID string) *IntentChangePassword {
+	return &IntentChangePassword{
+		UserID: userID,
+		Stage:  interaction.AuthenticationStageSecondary,
+	}
+}
+
+func (i *IntentChangePassword) InstantiateRootNode(ctx *interaction.Context, graph *interaction.Graph) (interaction.Node, error) {
+	edge := nodes.EdgeDoUseUser{UseUserID: i.UserID}
+	return edge.Instantiate(ctx, graph, i)
+}
+
+func (i *IntentChangePassword) DeriveEdgesForNode(graph *interaction.Graph, node interaction.Node) ([]interaction.Edge, error) {
+	switch node := node.(type) {
+	case *nodes.NodeDoUseUser:
+		return []interaction.Edge{
+			&nodes.EdgeChangePassword{
+				Stage: i.Stage,
+			},
+		}, nil
+	case *nodes.NodeDoUpdateAuthenticator:
+		return nil, nil
+	default:
+		panic(fmt.Errorf("interaction: unexpected node: %T", node))
+	}
+}
---
+++ pkg/lib/interaction/nodes/change_password.go
@@ -0,0 +1,128 @@
+package nodes
+
+import (
+	"fmt"
+
+	"github.com/authgear/authgear-server/pkg/api/apierrors"
+	"github.com/authgear/authgear-server/pkg/lib/authn"
+	"github.com/authgear/authgear-server/pkg/lib/authn/authenticator"
+	"github.com/authgear/authgear-server/pkg/lib/interaction"
+)
+
+var ChangePasswordFailed = apierrors.Invalid.WithReason("ChangePasswordFailed")
+var ErrNoPassword = ChangePasswordFailed.NewWithCause("the user does not have a password", apierrors.StringCause("NoPassword"))
+
+func init() {
+	interaction.RegisterNode(&NodeChangePasswordBegin{})
+	interaction.RegisterNode(&NodeChangePasswordEnd{})
+}
+
+type EdgeChangePasswordBegin struct {
+	Stage interaction.AuthenticationStage `json:"stage"`
+}
+
+func (e *EdgeChangePasswordBegin) Instantiate(ctx *interaction.Context, graph *interaction.Graph, rawInput interface{}) (interaction.Node, error) {
+	return &NodeChangePasswordBegin{
+		Stage: e.Stage,
+	}, nil
+}
+
+type NodeChangePasswordBegin struct {
+	Stage interaction.AuthenticationStage `json:"stage"`
+}
+
+func (n *NodeChangePasswordBegin) Prepare(ctx *interaction.Context, graph *interaction.Graph) error {
+	return nil
+}
+
+func (n *NodeChangePasswordBegin) Apply(perform func(eff interaction.Effect) error, graph *interaction.Graph) error {
+	return nil
+}
+
+func (n *NodeChangePasswordBegin) DeriveEdges(graph *interaction.Graph) ([]interaction.Edge, error) {
+	return []interaction.Edge{&EdgeChangePassword{
+		Stage: n.Stage,
+	}}, nil
+}
+
+type InputChangePassword interface {
+	GetNewPassword() string
+}
+
+type EdgeChangePassword struct {
+	Stage interaction.AuthenticationStage
+}
+
+func (e *EdgeChangePassword) Instantiate(ctx *interaction.Context, graph *interaction.Graph, rawInput interface{}) (node interaction.Node, err error) {
+	input, ok := rawInput.(InputChangePassword)
+	if !ok {
+		return nil, interaction.ErrIncompatibleInput
+	}
+
+	newPassword := input.GetNewPassword()
+
+	userID := graph.MustGetUserID()
+	ais, err := ctx.Authenticators.List(
+		userID,
+		authenticator.KeepType(authn.AuthenticatorTypePassword),
+		authenticator.KeepKind(stageToAuthenticatorKind(e.Stage)),
+	)
+	if err != nil {
+		return
+	}
+
+	if len(ais) == 0 {
+		err = ErrNoPassword
+		return
+	}
+
+	if len(ais) != 1 {
+		err = fmt.Errorf("changepassword: detected user %s having more than 1 password", userID)
+		return
+	}
+
+	oldInfo := ais[0]
+	changed, newInfo, err := ctx.Authenticators.WithSecret(oldInfo, newPassword)
+	if err != nil {
+		return
+	}
+
+	newNode := &NodeChangePasswordEnd{
+		Stage:            e.Stage,
+		OldAuthenticator: oldInfo,
+	}
+	if changed {
+		newNode.NewAuthenticator = newInfo
+	}
+
+	node = newNode
+	return node, nil
+}
+
+type NodeChangePasswordEnd struct {
+	Stage            interaction.AuthenticationStage `json:"stage"`
+	OldAuthenticator *authenticator.Info             `json:"old_authenticator"`
+	NewAuthenticator *authenticator.Info             `json:"new_authenticator,omitempty"`
+}
+
+func (n *NodeChangePasswordEnd) Apply(perform func(eff interaction.Effect) error, graph *interaction.Graph) error {
+	return nil
+}
+
+func (n *NodeChangePasswordEnd) Prepare(ctx *interaction.Context, graph *interaction.Graph) error {
+	return nil
+}
+
+func (n *NodeChangePasswordEnd) DeriveEdges(graph *interaction.Graph) ([]interaction.Edge, error) {
+	if n.NewAuthenticator != nil {
+		return []interaction.Edge{
+			&EdgeDoUpdateAuthenticator{
+				Stage:                     n.Stage,
+				AuthenticatorBeforeUpdate: n.OldAuthenticator,
+				AuthenticatorAfterUpdate:  n.NewAuthenticator,
+			},
+		}, nil
+	}
+	// Password was not changed, ends the interaction
+	return nil, nil
+}
