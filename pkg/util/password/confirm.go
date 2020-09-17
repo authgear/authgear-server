502296643b436ee80288f86961e1a3c0ee0bda5c
Louis Chan <louischan@oursky.com>
2020-09-17 18:32:46 +0800

    Remove constant time comparison in ConfirmPassword

--- pkg/util/password/confirm.go
+++ pkg/util/password/confirm.go
@@ -1,15 +1,15 @@
 package password
 
 import (
-	"crypto/subtle"
-
 	"github.com/authgear/authgear-server/pkg/api/apierrors"
 )
 
 var ErrNewPasswordTypo = apierrors.Invalid.WithReason("NewPasswordTypo").New("new password typo")
 
+// ConfirmPassword checks if the user has made a typo mistake when creating their new password.
+// Both input are given by the same person, constant time comparison is not needed here.
 func ConfirmPassword(newPassword, confirmPassword string) error {
-	typo := subtle.ConstantTimeCompare([]byte(newPassword), []byte(confirmPassword)) == 0
+	typo := newPassword != confirmPassword
 	if typo {
 		return ErrNewPasswordTypo
 	}
