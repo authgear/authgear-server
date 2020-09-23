feaf397f065067355d7b57afbc2a25e7a62347f3
axolotl0212 <allenl0212@gmail.com>
2020-09-23 17:38:23 +0800

    Integrate create identity to add username screen

--- portal/src/graphql/adminapi/AddUsernameScreen.tsx
+++ portal/src/graphql/adminapi/AddUsernameScreen.tsx
@@ -1,16 +1,45 @@
-import React, { useCallback, useContext, useMemo } from "react";
-import { PrimaryButton, TextField } from "@fluentui/react";
+import React, { useCallback, useContext, useMemo, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { TextField } from "@fluentui/react";
 import { Context, FormattedMessage } from "@oursky/react-messageformat";
 import deepEqual from "deep-equal";
 
 import UserDetailCommandBar from "./UserDetailCommandBar";
 import NavBreadcrumb from "../../NavBreadcrumb";
 import NavigationBlockerDialog from "../../NavigationBlockerDialog";
+import ButtonWithLoading from "../../ButtonWithLoading";
+import { useCreateLoginIDIdentityMutation } from "./mutations/createIdentityMutation";
 import { useTextField } from "../../hook/useInput";
+import {
+  defaultFormatErrorMessageList,
+  Violation,
+} from "../../util/validation";
+import { parseError } from "../../util/error";
 
 import styles from "./AddUsernameScreen.module.scss";
+import ShowError from "../../ShowError";
 
 const AddUsernameScreen: React.FC = function AddUsernameScreen() {
+  const { userID } = useParams();
+  const navigate = useNavigate();
+
+  const {
+    createIdentity,
+    loading: creatingIdentity,
+    error: createIdentityError,
+  } = useCreateLoginIDIdentityMutation(userID);
+  const { renderToString } = useContext(Context);
+
+  const [violations, setViolations] = useState<Violation[]>([]);
+  const [unhandledViolations, setUnhandledViolations] = useState<Violation[]>(
+    []
+  );
+  const [disableBlockNavigation, setDisableBlockNavigation] = useState<boolean>(
+    false
+  );
+
+  const { value: username, onChange: onUsernameChange } = useTextField("");
+
   const navBreadcrumbItems = useMemo(() => {
     return [
       { to: "../../..", label: <FormattedMessage id="UsersScreen.title" /> },
@@ -18,10 +47,6 @@ const AddUsernameScreen: React.FC = function AddUsernameScreen() {
       { to: ".", label: <FormattedMessage id="AddUsernameScreen.title" /> },
     ];
   }, []);
-
-  const { value: username, onChange: onUsernameChange } = useTextField("");
-
-  const { renderToString } = useContext(Context);
 
   const screenState = useMemo(
     () => ({
@@ -35,24 +60,70 @@ const AddUsernameScreen: React.FC = function AddUsernameScreen() {
   }, [screenState]);
 
   const onAddClicked = useCallback(() => {
-    // TODO: mutation to be integrated
-  }, []);
+    setDisableBlockNavigation(true);
+    createIdentity({ key: "username", value: username })
+      .then((identity) => {
+        if (identity != null) {
+          navigate("../");
+        } else {
+          throw new Error();
+        }
+      })
+      .catch((err) => {
+        setDisableBlockNavigation(false);
+        const violations = parseError(err);
+        setViolations(violations);
+      });
+  }, [username, navigate, createIdentity]);
+
+  const errorMessage = useMemo(() => {
+    const usernameFieldErrorMessages: string[] = [];
+    const unknownViolations: Violation[] = [];
+    for (const violation of violations) {
+      if (violation.kind === "Invalid" || violation.kind === "format") {
+        usernameFieldErrorMessages.push(
+          renderToString("AddUsernameScreen.error.invalid-username")
+        );
+      } else if (violation.kind === "DuplicatedIdentity") {
+        usernameFieldErrorMessages.push(
+          renderToString("AddUsernameScreen.error.duplicated-username")
+        );
+      } else {
+        unknownViolations.push(violation);
+      }
+    }
+
+    setUnhandledViolations(unknownViolations);
+
+    return {
+      username: defaultFormatErrorMessageList(usernameFieldErrorMessages),
+    };
+  }, [violations, renderToString]);
 
   return (
     <div className={styles.root}>
       <UserDetailCommandBar />
       <NavBreadcrumb className={styles.breadcrumb} items={navBreadcrumbItems} />
       <section className={styles.content}>
-        <NavigationBlockerDialog blockNavigation={isFormModified} />
+        {unhandledViolations.length > 0 && (
+          <ShowError error={createIdentityError} />
+        )}
+        <NavigationBlockerDialog
+          blockNavigation={!disableBlockNavigation && isFormModified}
+        />
         <TextField
           className={styles.usernameField}
           label={renderToString("AddUsernameScreen.username.label")}
           value={username}
           onChange={onUsernameChange}
+          errorMessage={errorMessage.username}
         />
-        <PrimaryButton onClick={onAddClicked} disabled={!isFormModified}>
-          <FormattedMessage id="add" />
-        </PrimaryButton>
+        <ButtonWithLoading
+          onClick={onAddClicked}
+          disabled={!isFormModified}
+          labelId="add"
+          loading={creatingIdentity}
+        />
       </section>
     </div>
   );
