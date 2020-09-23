1385186cf6a02259fc6204f758deca19842e21b6
nicolaslee2775 <13070797d@connect.polyu.hk>
2020-09-23 12:25:29 +0800

    Introduce CodeEditor #348

---
+++ portal/src/CodeEditor.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback } from "react";
+import { ControlledEditor } from "@monaco-editor/react";
+
+export interface CodeEditorOnChange {
+  (e: unknown, value: string | undefined): void;
+}
+
+export interface Props {
+  className?: string;
+  language: string;
+  value: string;
+  onChange: CodeEditorOnChange;
+}
+
+const CodeEditor: React.FC<Props> = function CodeEditor(props) {
+  const {
+    className,
+    language,
+    value,
+    onChange,
+  } = props;
+
+  const callback = useCallback(
+    (e: unknown, value: string | undefined) => {
+      onChange(e, value);
+      return undefined;
+    },
+    [onChange]
+  );
+
+  return (
+    <div className={className}>
+      <ControlledEditor
+        height="100%"
+        value={value}
+        language={language}
+        onChange={callback}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
