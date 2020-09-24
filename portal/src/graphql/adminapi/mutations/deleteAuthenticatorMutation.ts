826d823e94bbb424d281bf76099e3e89eb28b6f5
axolotl0212 <allenl0212@gmail.com>
2020-09-24 18:40:44 +0800

    Add delete authenticator hook

---
+++ portal/src/graphql/adminapi/mutations/deleteAuthenticatorMutation.ts
@@ -0,0 +1,39 @@
+import { gql, useMutation } from "@apollo/client";
+import { useCallback } from "react";
+import { DeleteAuthenticatorMutation } from "./__generated__/DeleteAuthenticatorMutation";
+
+const deleteAuthenticatorMutation = gql`
+  mutation DeleteAuthenticatorMutation($authenticatorID: ID!) {
+    deleteAuthenticator(input: { authenticatorID: $authenticatorID }) {
+      success
+    }
+  }
+`;
+
+export function useDeleteAuthenticatorMutation(): {
+  deleteAuthenticator: (authenticatorID: string) => Promise<boolean>;
+  loading: boolean;
+  error: unknown;
+} {
+  const [mutationFunction, { error, loading }] = useMutation<
+    DeleteAuthenticatorMutation
+  >(deleteAuthenticatorMutation);
+  const deleteAuthenticator = useCallback(
+    async (authenticatorID: string) => {
+      const result = await mutationFunction({
+        variables: {
+          authenticatorID,
+        },
+      });
+
+      return result.data?.deleteAuthenticator.success ?? false;
+    },
+    [mutationFunction]
+  );
+
+  return {
+    deleteAuthenticator,
+    loading,
+    error,
+  };
+}
