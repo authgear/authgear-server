982a0a1f6e40e317e78a6525aee45acaabab2760
nicolaslee2775 <13070797d@connect.polyu.hk>
2020-09-24 17:08:05 +0800

    Directly pass props to monaco editor component #348

--- portal/src/CodeEditor.tsx
+++ portal/src/CodeEditor.tsx
@@ -1,36 +1,16 @@
-import React, { useCallback } from "react";
-import { ControlledEditor } from "@monaco-editor/react";
+import React from "react";
+import { ControlledEditor, ControlledEditorProps } from "@monaco-editor/react";
 
-export interface CodeEditorOnChange {
-  (e: unknown, value: string | undefined): void;
-}
-
-export interface Props {
+export interface CodeEditorProps extends ControlledEditorProps {
   className?: string;
-  language: string;
-  value: string;
-  onChange: CodeEditorOnChange;
 }
 
-const CodeEditor: React.FC<Props> = function CodeEditor(props) {
-  const { className, language, value, onChange } = props;
-
-  const callback = useCallback(
-    (e: unknown, value: string | undefined) => {
-      onChange(e, value);
-      return undefined;
-    },
-    [onChange]
-  );
+const CodeEditor: React.FC<CodeEditorProps> = function CodeEditor(props) {
+  const { className, ...rest } = props;
 
   return (
     <div className={className}>
-      <ControlledEditor
-        height="100%"
-        value={value}
-        language={language}
-        onChange={callback}
-      />
+      <ControlledEditor height="100%" {...rest} />
     </div>
   );
 };
