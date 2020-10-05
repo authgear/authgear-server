1ee2caa1080eb3b7bb23393ef540eb35e2f338f6
nicolaslee2775 <13070797d@connect.polyu.hk>
2020-10-05 14:43:37 +0800

    Block navigation if there are unsaved changes #380

--- portal/src/graphql/portal/PasswordlessAuthenticatorScreen.tsx
+++ portal/src/graphql/portal/PasswordlessAuthenticatorScreen.tsx
@@ -14,6 +14,7 @@ import CodeEditor from "../../CodeEditor";
 import { PortalAPIAppConfig } from "../../types";
 import { clearEmptyObject } from "../../util/misc";
 import ButtonWithLoading from "../../ButtonWithLoading";
+import NavigationBlockerDialog from "../../NavigationBlockerDialog";
 
 import styles from "./PasswordlessAuthenticatorScreen.module.scss";
 
@@ -204,6 +205,7 @@ const PasswordlessAuthenticatorScreen: React.FC = function PasswordlessAuthentic
           </div>
         </div>
       </div>
+      <NavigationBlockerDialog blockNavigation={isFormModified} />
     </main>
   );
 };
