dafbff57368f06c1bf7cb7cba792cc1211a7e8b8
axolotl0212 <allenl0212@gmail.com>
2020-10-07 11:07:28 +0800

    Remove unnecessary condition for gen random hex string function

--- portal/src/util/random.ts
+++ portal/src/util/random.ts
@@ -1,8 +1,5 @@
 export function genRandomHexadecimalString(length: number = 16): string {
   let result = "";
-  if (length < 0) {
-    return result;
-  }
   for (let i = 0; i < length; i++) {
     result += Math.floor(Math.random() * 16).toString(16);
   }
