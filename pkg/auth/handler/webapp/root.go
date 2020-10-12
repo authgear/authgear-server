ffd58b4b33a43a4256d2ef92abf17fa2ba1cd516
kiootic <kiootic@oursky.com>
2020-10-12 18:54:40 +0800

    Discard queries when redirecting within Auth UI
    Preserving queries would restore errors from a previous operation when
    finishing another operation successfully.

--- pkg/auth/handler/webapp/root.go
+++ pkg/auth/handler/webapp/root.go
@@ -3,9 +3,7 @@ package webapp
 import (
 	"net/http"
 
-	"github.com/authgear/authgear-server/pkg/auth/webapp"
 	"github.com/authgear/authgear-server/pkg/util/httproute"
-	"github.com/authgear/authgear-server/pkg/util/httputil"
 )
 
 func ConfigureRootRoute(route httproute.Route) httproute.Route {
@@ -17,10 +15,5 @@ func ConfigureRootRoute(route httproute.Route) httproute.Route {
 type RootHandler struct{}
 
 func (h *RootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	u := *r.URL
-	q := u.Query()
-	webapp.RemoveX(q)
-	u.RawQuery = q.Encode()
-	u.Path = "/login"
-	http.Redirect(w, r, httputil.HostRelative(&u).String(), http.StatusFound)
+	http.Redirect(w, r, "/login", http.StatusFound)
 }
