6e8a020aedd7b52d56e86b1026ec038600faa67b
kiootic <kiootic@oursky.com>
2020-10-12 14:43:09 +0800

    Implement TOTP authenticator removal

--- pkg/auth/handler/webapp/settings_totp.go
+++ pkg/auth/handler/webapp/settings_totp.go
@@ -8,8 +8,10 @@ import (
 	"github.com/authgear/authgear-server/pkg/lib/authn"
 	"github.com/authgear/authgear-server/pkg/lib/authn/authenticator"
 	"github.com/authgear/authgear-server/pkg/lib/infra/db"
+	"github.com/authgear/authgear-server/pkg/lib/interaction/intents"
 	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/util/httproute"
+	"github.com/authgear/authgear-server/pkg/util/httputil"
 	"github.com/authgear/authgear-server/pkg/util/template"
 )
 
@@ -75,6 +77,10 @@ func (h *SettingsTOTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
+	redirectURI := httputil.HostRelative(r.URL).String()
+	authenticatorID := r.Form.Get("x_authenticator_id")
+	userID := session.GetUserID(r.Context())
+
 	if r.Method == "GET" {
 		err := h.Database.WithTx(func() error {
 			state, err := h.WebApp.GetState(StateID(r))
@@ -94,4 +100,39 @@ func (h *SettingsTOTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) 
 			panic(err)
 		}
 	}
+
+	if r.Method == "POST" && r.Form.Get("x_action") == "remove" {
+		err := h.Database.WithTx(func() error {
+			intent := &webapp.Intent{
+				RedirectURI: redirectURI,
+				Intent:      intents.NewIntentRemoveAuthenticator(*userID),
+			}
+			result, err := h.WebApp.PostIntent(intent, func() (input interface{}, err error) {
+				input = &SettingsTOTPRemove{
+					AuthenticatorID: authenticatorID,
+				}
+				return
+			})
+			if err != nil {
+				return err
+			}
+			result.WriteResponse(w, r)
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+type SettingsTOTPRemove struct {
+	AuthenticatorID string
+}
+
+func (i *SettingsTOTPRemove) GetAuthenticatorType() authn.AuthenticatorType {
+	return authn.AuthenticatorTypeTOTP
+}
+
+func (i *SettingsTOTPRemove) GetAuthenticatorID() string {
+	return i.AuthenticatorID
 }
