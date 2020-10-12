c8fbbcfe1b283d513a35cb88d9157be014c9456e
axolotl0212 <allenl0212@gmail.com>
2020-10-12 22:23:03 +0800

    Handle form field error

---
+++ portal/src/error/FormFieldContext.ts
@@ -0,0 +1,95 @@
+import { Context, Values } from "@oursky/react-messageformat";
+import { useContext, useEffect } from "react";
+
+import { useFormContext } from "./FormContext";
+import { ValidationFailedErrorInfoCause } from "./validation";
+
+const errorMessageID: Record<ValidationFailedErrorInfoCause["kind"], string> = {
+  required: "validation-error.required",
+  general: "validation-error.general",
+  format: "validation-error.format",
+  minItems: "validation-error.minItems",
+  minimum: "validation-error.minimum",
+  maximum: "validation-error.maximum",
+};
+
+interface FormFieldData {
+  errorMessage: string | undefined;
+}
+
+function getReactMessageFormatValues(
+  cause: ValidationFailedErrorInfoCause
+): Values {
+  return cause.details;
+}
+
+function constructErrorMessageFromValidationErrorCause(
+  renderToString: (messageID: string, values?: Values) => string,
+  cause: ValidationFailedErrorInfoCause,
+  fieldName: string,
+  fieldNameMessageID?: string
+): string | undefined {
+  // special handle required violation, needs to match missing field
+  if (cause.kind === "required") {
+    if (cause.details.missing.includes(fieldName)) {
+      // fallback to raw field name if field name message ID not exist
+      let localizedFieldName = fieldName;
+      if (fieldNameMessageID != null) {
+        localizedFieldName = renderToString(fieldNameMessageID);
+      } else {
+        console.warn(
+          "[Construct validation error message]: Expect fieldNameMessageID in rules for `required` violation error"
+        );
+      }
+      return renderToString(errorMessageID["required"], {
+        fieldName: localizedFieldName,
+      });
+    }
+  }
+  // other than required violation, matching json pointer => matching field
+  // unrecognized error kind (violation type) => throw error in get message value
+  try {
+    const errorMessageValues = getReactMessageFormatValues(cause);
+    return renderToString(errorMessageID[cause.kind], errorMessageValues);
+  } catch {
+    console.warn(
+      "[Unhandled validation error cause]: Unrecognized cause kind\n",
+      cause
+    );
+    return undefined;
+  }
+}
+
+export function useFormField(
+  jsonPointer: RegExp | string,
+  parentJSONPointer: RegExp | string,
+  fieldName: string,
+  fieldNameMessageID?: string
+): FormFieldData {
+  const { causes: formErrorCauses, registerField } = useFormContext();
+  const { renderToString } = useContext(Context);
+  // register field
+  useEffect(() => {
+    registerField(jsonPointer, parentJSONPointer, fieldName);
+  }, [registerField, fieldName, jsonPointer, parentJSONPointer]);
+
+  // handle error cause
+  const fieldErrorCauses = formErrorCauses[String(jsonPointer)] ?? [];
+  const errorMessageList: string[] = [];
+  for (const cause of fieldErrorCauses) {
+    const errorMessage = constructErrorMessageFromValidationErrorCause(
+      renderToString,
+      cause,
+      fieldName,
+      fieldNameMessageID
+    );
+    if (errorMessage != null) {
+      errorMessageList.push(errorMessage);
+    } else {
+      console.warn("[Form field]: Unrecognized error cause");
+    }
+  }
+  const fieldErrorMessage = errorMessageList.join("\n");
+
+  return { errorMessage: fieldErrorMessage };
+}
---
+++ portal/src/error/useValidationError.ts
@@ -0,0 +1,110 @@
+import { useCallback, useState } from "react";
+
+import { FormContextValue } from "./FormContext";
+import { extractAPIError, isApolloError } from "./error";
+import { ValidationFailedErrorInfoCause } from "./validation";
+
+interface FieldRegister {
+  jsonPointer: RegExp | string;
+  parentJSONPointer: RegExp | string;
+  fieldName: string;
+}
+
+type ErrorCausesMap = Partial<Record<string, ValidationFailedErrorInfoCause[]>>;
+
+function isLocationMatchWithJSONPointer(
+  jsonPointer: RegExp | string,
+  location: string
+) {
+  if (typeof jsonPointer === "string") {
+    return location.startsWith(jsonPointer);
+  }
+  return jsonPointer.test(location);
+}
+
+function commonSelector(
+  cause: ValidationFailedErrorInfoCause,
+  jsonPointer: RegExp | string
+): boolean {
+  return isLocationMatchWithJSONPointer(jsonPointer, cause.location);
+}
+
+function requiredCauseSelector(
+  cause: ValidationFailedErrorInfoCause,
+  parentJSONPointer: string | RegExp,
+  fieldName: string
+) {
+  if (cause.kind !== "required") {
+    return false;
+  }
+  if (!isLocationMatchWithJSONPointer(parentJSONPointer, cause.location)) {
+    return false;
+  }
+  return cause.details.missing.includes(fieldName);
+}
+
+function handleValidationErrorCause(
+  cause: ValidationFailedErrorInfoCause,
+  fields: FieldRegister[],
+  matchedCauses: ErrorCausesMap,
+  unhandledCauses: ValidationFailedErrorInfoCause[]
+) {
+  for (const field of fields) {
+    const isMatch =
+      requiredCauseSelector(cause, field.parentJSONPointer, field.fieldName) ||
+      commonSelector(cause, field.jsonPointer);
+    const jsonPointerString = String(field.jsonPointer);
+    if (isMatch) {
+      matchedCauses[jsonPointerString] = matchedCauses[jsonPointerString] ?? [];
+      matchedCauses[jsonPointerString]?.push(cause);
+      return;
+    }
+  }
+
+  // no matching fields
+  unhandledCauses.push(cause);
+}
+
+export function useValidationError(
+  error: unknown
+): {
+  unhandledCauses?: ValidationFailedErrorInfoCause[];
+  otherError?: unknown;
+  value: FormContextValue;
+} {
+  const [fields, setFields] = useState<FieldRegister[]>([]);
+  const registerField = useCallback(
+    (
+      jsonPointer: RegExp | string,
+      parentJSONPointer: RegExp | string,
+      fieldName: string
+    ) => {
+      setFields((prev) => [
+        ...prev,
+        { jsonPointer, parentJSONPointer, fieldName },
+      ]);
+    },
+    []
+  );
+  const unhandledCauses: ValidationFailedErrorInfoCause[] = [];
+  const matchedCauses: ErrorCausesMap = {};
+
+  if (error == null) {
+    return { unhandledCauses, value: { registerField, causes: {} } };
+  }
+  if (!isApolloError(error)) {
+    return { otherError: error, value: { registerField, causes: {} } };
+  }
+  const { graphQLErrors } = error;
+  for (const graphQLError of graphQLErrors) {
+    const apiError = extractAPIError(graphQLError);
+    if (apiError == null || apiError.reason !== "ValidationFailed") {
+      return { otherError: error, value: { registerField, causes: {} } };
+    }
+    const { causes } = apiError.info;
+    for (const cause of causes) {
+      handleValidationErrorCause(cause, fields, matchedCauses, unhandledCauses);
+    }
+  }
+  return { unhandledCauses, value: { registerField, causes: matchedCauses } };
+}
