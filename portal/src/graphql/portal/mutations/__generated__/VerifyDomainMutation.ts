2d79490a124553c465f4bd785f78fa6cb993058c
axolotl0212 <allenl0212@gmail.com>
2020-10-14 18:00:44 +0800

    Add hook to verify domain

---
+++ portal/src/graphql/portal/mutations/__generated__/VerifyDomainMutation.ts
@@ -0,0 +1,57 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: VerifyDomainMutation
+// ====================================================
+
+export interface VerifyDomainMutation_verifyDomain_app_domains {
+  __typename: "Domain";
+  id: string;
+  createdAt: GQL_DateTime;
+  domain: string;
+  apexDomain: string;
+  isCustom: boolean;
+  isVerified: boolean;
+  verificationDNSRecord: string;
+}
+
+export interface VerifyDomainMutation_verifyDomain_app {
+  __typename: "App";
+  /**
+   * The ID of an object
+   */
+  id: string;
+  domains: VerifyDomainMutation_verifyDomain_app_domains[];
+}
+
+export interface VerifyDomainMutation_verifyDomain_domain {
+  __typename: "Domain";
+  id: string;
+  createdAt: GQL_DateTime;
+  domain: string;
+  apexDomain: string;
+  isCustom: boolean;
+  isVerified: boolean;
+  verificationDNSRecord: string;
+}
+
+export interface VerifyDomainMutation_verifyDomain {
+  __typename: "VerifyDomainPayload";
+  app: VerifyDomainMutation_verifyDomain_app;
+  domain: VerifyDomainMutation_verifyDomain_domain;
+}
+
+export interface VerifyDomainMutation {
+  /**
+   * Request verification of a domain of target app
+   */
+  verifyDomain: VerifyDomainMutation_verifyDomain;
+}
+
+export interface VerifyDomainMutationVariables {
+  appID: string;
+  domainID: string;
+}
