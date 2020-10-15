d26e8121ba9ad3b97449287a989a6a4b6be703c8
nicolaslee2775 <13070797d@connect.polyu.hk>
2020-10-15 11:35:22 +0800

    Keep poratl admins tab only #496

--- portal/src/graphql/portal/SettingsScreen.tsx
+++ portal/src/graphql/portal/SettingsScreen.tsx
@@ -6,20 +6,12 @@ import { usePivotNavigation } from "../../hook/usePivot";
 
 import styles from "./SettingsScreen.module.scss";
 
-const GENERAL_PIVOT_KEY = "general";
 const PORTAL_ADMINS_PIVOT_KEY = "portal_admins";
-const SMTP_PIVOT_KEY = "smtp";
-const SMS_PROVIDER_PIVOT_KEY = "sms_provider";
-const SESSION_PIVOT_KEY = "session";
 
 const SettingsScreen: React.FC = function SettingsScreen() {
   const { renderToString } = useContext(Context);
   const { selectedKey, onLinkClick } = usePivotNavigation([
-    GENERAL_PIVOT_KEY,
     PORTAL_ADMINS_PIVOT_KEY,
-    SMTP_PIVOT_KEY,
-    SMS_PROVIDER_PIVOT_KEY,
-    SESSION_PIVOT_KEY,
   ]);
 
   return (
@@ -31,24 +23,8 @@ const SettingsScreen: React.FC = function SettingsScreen() {
         <div className={styles.tabsContainer}>
           <Pivot onLinkClick={onLinkClick} selectedKey={selectedKey}>
             <PivotItem
-              headerText={renderToString("SettingsScreen.general.title")}
-              itemKey={GENERAL_PIVOT_KEY}
-            />
-            <PivotItem
               headerText={renderToString("SettingsScreen.portal_admins.title")}
               itemKey={PORTAL_ADMINS_PIVOT_KEY}
-            />
-            <PivotItem
-              headerText={renderToString("SettingsScreen.smtp.title")}
-              itemKey={SMTP_PIVOT_KEY}
-            />
-            <PivotItem
-              headerText={renderToString("SettingsScreen.sms_provider.title")}
-              itemKey={SMS_PROVIDER_PIVOT_KEY}
-            />
-            <PivotItem
-              headerText={renderToString("SettingsScreen.session.title")}
-              itemKey={SESSION_PIVOT_KEY}
             />
           </Pivot>
         </div>
