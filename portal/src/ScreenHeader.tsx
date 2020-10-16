beda25c8019d3028913156362a06b7cca3ad831d
axolotl0212 <allenl0212@gmail.com>
2020-10-17 02:24:01 +0800

    Update screen header UI

--- portal/src/ScreenHeader.tsx
+++ portal/src/ScreenHeader.tsx
@@ -1,18 +1,46 @@
 import React, { useCallback, useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Context } from "@oursky/react-messageformat";
 import authgear from "@authgear/web";
-import { IconButton } from "@fluentui/react";
-import styles from "./ScreenHeader.module.scss";
+import { Icon, IconButton, Text } from "@fluentui/react";
 import srcLogo from "./image/screen-header-logo@3x.png";
 import { invertedTheme } from "./theme";
+import { useAppConfigQuery } from "./graphql/portal/query/appConfigQuery";
+
+import styles from "./ScreenHeader.module.scss";
+
+interface ScreenHeaderAppSectionProps {
+  appID: string;
+}
 
 const iconProps = {
   iconName: "SignOut",
 };
 
+const ScreenHeaderAppSection: React.FC<ScreenHeaderAppSectionProps> = function ScreenHeaderAppSection(
+  props: ScreenHeaderAppSectionProps
+) {
+  const { appID } = props;
+  const { effectiveAppConfig, loading } = useAppConfigQuery(appID);
+
+  if (loading) {
+    return null;
+  }
+
+  return (
+    <>
+      <Icon className={styles.headerArrow} iconName="ChevronRight" />
+      <Text className={styles.headerAppID}>
+        {/* TODO: update app name */}
+        {effectiveAppConfig?.http?.public_origin ?? appID}
+      </Text>
+    </>
+  );
+};
+
 const ScreenHeader: React.FC = function ScreenHeader() {
   const { renderToString } = useContext(Context);
+  const { appID } = useParams();
 
   const labelSignOut = renderToString("header.sign-out");
 
@@ -30,9 +58,12 @@ const ScreenHeader: React.FC = function ScreenHeader() {
 
   return (
     <header className={styles.header}>
-      <Link to="/">
-        <img className={styles.logo} alt="Authgear" src={srcLogo} />
-      </Link>
+      <div className={styles.headerLeft}>
+        <Link to="/" className={styles.logoLink}>
+          <img className={styles.logo} alt="Authgear" src={srcLogo} />
+        </Link>
+        {appID && <ScreenHeaderAppSection appID={appID} />}
+      </div>
       <IconButton
         type="button"
         iconProps={iconProps}
