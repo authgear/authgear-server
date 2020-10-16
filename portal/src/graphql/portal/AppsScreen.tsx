1811c0733b9b4f7cca81fac847e232e8a87353a1
axolotl0212 <allenl0212@gmail.com>
2020-10-17 01:24:39 +0800

    Update App list UI

--- portal/src/graphql/portal/AppsScreen.tsx
+++ portal/src/graphql/portal/AppsScreen.tsx
@@ -1,94 +1,86 @@
-import React, { useContext, useMemo } from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  Context as LocaleContext,
-  FormattedMessage,
-} from "@oursky/react-messageformat";
-import {
-  CommandBar,
-  ICommandBarItemProps,
-  INavLink,
-  INavLinkGroup,
-  Nav,
-  Text,
-} from "@fluentui/react";
+import React, { useCallback, useMemo } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FormattedMessage } from "@oursky/react-messageformat";
+import { DefaultEffects, PrimaryButton, Text } from "@fluentui/react";
 import ShowError from "../../ShowError";
 import ShowLoading from "../../ShowLoading";
 import ScreenHeader from "../../ScreenHeader";
-import { AppsScreenQuery } from "./__generated__/AppsScreenQuery";
-import { useAppListQuery } from "./query/appListQuery";
+import { App, useAppListQuery } from "./query/appListQuery";
 import styles from "./AppsScreen.module.scss";
 
-const AppList: React.FC<AppsScreenQuery> = function AppList(
-  props: AppsScreenQuery
-) {
+interface AppListProps {
+  apps: App[] | null;
+}
+
+interface AppCardData {
+  appName: string;
+  appID: string;
+  url: string;
+}
+const AppCard: React.FC<AppCardData> = function AppCard(props: AppCardData) {
+  const { appName, appID, url } = props;
+
+  return (
+    <Link
+      to={url}
+      style={{ boxShadow: DefaultEffects.elevation4 }}
+      className={styles.card}
+    >
+      <Text className={styles.cardAppID}>{appID}</Text>
+      <Text className={styles.cardAppName}>{appName}</Text>
+    </Link>
+  );
+};
+
+const AppList: React.FC<AppListProps> = function AppList(props: AppListProps) {
+  const { apps } = props;
   const navigate = useNavigate();
-  const { renderToString } = useContext(LocaleContext);
 
-  const commands: ICommandBarItemProps[] = useMemo(
-    () => [
-      {
-        key: "create",
-        text: renderToString("AppsScreen.create-app"),
-        href: "/apps/create",
-        onClick: (e) => {
-          e?.preventDefault();
-          e?.stopPropagation();
-          navigate("/apps/create");
-        },
-        iconProps: { iconName: "NewFolder" },
-      },
-    ],
-    [renderToString, navigate]
+  const onCreateClick = useCallback(
+    (e) => {
+      e?.preventDefault();
+      e?.stopPropagation();
+      navigate("/apps/create");
+    },
+    [navigate]
   );
 
-  const groups: INavLinkGroup[] = useMemo(
-    () => [
-      {
-        links:
-          props.apps?.edges?.map(
-            (edge): INavLink => {
-              const appID = String(edge?.node?.id);
-              const appOrigin =
-                edge?.node?.effectiveAppConfig.http?.public_origin;
-              const relPath = "/app/" + encodeURIComponent(appID);
-              return {
-                name: appOrigin ?? appID,
-                url: relPath,
-                key: appID,
-                onClick: (e) => {
-                  e?.preventDefault();
-                  e?.stopPropagation();
-                  navigate(relPath);
-                },
-              };
-            }
-          ) ?? [],
-      },
-    ],
-    [props.apps, navigate]
-  );
+  const appCardsData: AppCardData[] = useMemo(() => {
+    return (apps ?? []).map((app) => {
+      const appID = String(app.id);
+      const appOrigin = app.effectiveAppConfig.http?.public_origin;
+      const relPath = "/app/" + encodeURIComponent(appID);
+      // TODO: update app name
+      return {
+        appID,
+        appName: appOrigin ?? appID,
+        url: relPath,
+      };
+    });
+  }, [apps]);
 
   return (
     <main className={styles.root}>
       <ScreenHeader />
-      <CommandBar
-        className={styles.commandBar}
-        items={[]}
-        farItems={commands}
-      />
       <section className={styles.body}>
-        <Text as="h1" variant="xLarge" block={true}>
+        <Text as="h1" className={styles.header} variant="xLarge" block={true}>
           <FormattedMessage id="AppsScreen.title" />
         </Text>
-        <Nav groups={groups} />
+        <section className={styles.cardsContainer}>
+          {appCardsData.map((appCardData) => {
+            return <AppCard key={appCardData.appID} {...appCardData} />;
+          })}
+        </section>
+        <PrimaryButton onClick={onCreateClick}>
+          <FormattedMessage id="AppsScreen.create-app" />
+        </PrimaryButton>
       </section>
     </main>
   );
 };
 
 const AppsScreen: React.FC = function AppsScreen() {
-  const { loading, error, data, refetch } = useAppListQuery();
+  const { loading, error, apps, refetch } = useAppListQuery();
 
   if (loading) {
     return <ShowLoading />;
@@ -98,7 +90,7 @@ const AppsScreen: React.FC = function AppsScreen() {
     return <ShowError error={error} onRetry={refetch} />;
   }
 
-  return <AppList apps={data?.apps ?? null} />;
+  return <AppList apps={apps ?? null} />;
 };
 
 export default AppsScreen;
