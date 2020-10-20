203a3139e0fbfdaca20ebd3261ded857dcd6ddc3
nicolaslee2775 <13070797d@connect.polyu.hk>
2020-10-20 10:58:23 +0800

    Display app ID instead of app name #497

--- portal/src/graphql/portal/GeneralSettings.tsx
+++ portal/src/graphql/portal/GeneralSettings.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useContext, useMemo, useState } from "react";
 import { TextField } from "@fluentui/react";
 import { Context } from "@oursky/react-messageformat";
+import { useParams } from "react-router-dom";
 import deepEqual from "deep-equal";
 
 import ButtonWithLoading from "../../ButtonWithLoading";
@@ -9,48 +10,39 @@ import NavigationBlockerDialog from "../../NavigationBlockerDialog";
 import styles from "./GeneralSettings.module.scss";
 
 interface State {
-  appName: string;
+  appID: string;
 }
 
 const GeneralSettings: React.FC = function GeneralSettings() {
   const { renderToString } = useContext(Context);
+  const { appID } = useParams();
 
   const initialState = useMemo<State>(() => {
     return {
-      appName: "",
+      appID: appID,
     };
-  }, []);
+  }, [appID]);
 
-  const [state, setState] = useState<State>(initialState);
+  const [state] = useState<State>(initialState);
 
   const isFormModified = useMemo(() => {
     return !deepEqual(initialState, state, { strict: true });
   }, [initialState, state]);
 
-  const onAppNameChange = useCallback((_event, value?: string) => {
-    if (value === undefined) {
-      return;
-    }
-    setState((prevState) => ({
-      ...prevState,
-      appName: value,
-    }));
-  }, []);
-
   const onFormSubmit = useCallback((ev: React.SyntheticEvent<HTMLElement>) => {
     ev.preventDefault();
     ev.stopPropagation();
 
-    alert("App name cannot be saved currently");
+    alert("App ID cannot be saved currently");
   }, []);
 
   return (
     <form className={styles.root} onSubmit={onFormSubmit}>
       <TextField
         className={styles.textField}
-        label={renderToString("GeneralSettings.app-name.label")}
-        value={state.appName}
-        onChange={onAppNameChange}
+        disabled={true}
+        label={renderToString("GeneralSettings.app-id.label")}
+        value={state.appID}
       />
       <div className={styles.saveButtonContainer}>
         <ButtonWithLoading
