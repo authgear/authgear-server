518d382d63484081eef1f0d8f8da18dddf344826
axolotl0212 <allenl0212@gmail.com>
2020-10-29 23:42:17 +0800

    Add validation when parsing phone number
    Also added test case

--- pkg/util/phone/phone.go
+++ pkg/util/phone/phone.go
@@ -3,6 +3,7 @@ package phone
 import (
 	"errors"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -11,6 +12,9 @@ import (
 
 // ErrNotInE164Format means the given phone number is not in E.164 format.
 var ErrNotInE164Format = errors.New("not in E.164 format")
+
+// ErrPhoneNumberInvalid means phone number doesn't pass validation
+var ErrPhoneNumberInvalid = errors.New("invalid phone number")
 
 // EnsureE164 ensures the given phone is in E.164 format.
 func EnsureE164(phone string) error {
@@ -25,16 +29,34 @@ func EnsureE164(phone string) error {
 	return nil
 }
 
-// Parse is a very lenient function to parse nationalNumber and callingCode into e164.
-func Parse(nationalNumber string, callingCode string) (e164 string, err error) {
+func combineCallingCodeWithNumber(nationalNumber string, callingCode string) string {
 	nationalNumber = strings.TrimSpace(nationalNumber)
 	callingCode = strings.TrimSpace(callingCode)
 	if !strings.HasPrefix(callingCode, "+") {
 		callingCode = fmt.Sprintf("+%s", callingCode)
 	}
-	rawInput := fmt.Sprintf("%s%s", callingCode, nationalNumber)
+	return fmt.Sprintf("%s%s", callingCode, nationalNumber)
+}
+
+// According to https://godoc.org/github.com/nyaruka/phonenumbers#Parse,
+// no validation is performed during parsing, so we need to call IsValidNumber to check
+func Parse(nationalNumber string, callingCode string) (e164 string, err error) {
+	rawInput := combineCallingCodeWithNumber(nationalNumber, callingCode)
+	// check if rawInput contains non-numeric character(s)
+	// The nationalNumber part of phone is parsed to uint64,
+	// letters in input phone number will be parsed successfully.
+	isNumericString, _ := regexp.Match(`^\+[0-9\ \-]*$`, []byte(rawInput))
+	if !isNumericString {
+		err = ErrNotInE164Format
+		return
+	}
 	num, err := phonenumbers.Parse(rawInput, "")
 	if err != nil {
+		return
+	}
+	isPhoneValid := phonenumbers.IsValidNumber(num)
+	if !isPhoneValid {
+		err = ErrPhoneNumberInvalid
 		return
 	}
 	e164 = phonenumbers.Format(num, phonenumbers.E164)
--- pkg/util/phone/phone_test.go
+++ pkg/util/phone/phone_test.go
@@ -20,37 +20,54 @@ func TestPhone(t *testing.T) {
 		})
 
 		Convey("Parse", func() {
-			check := func(nationalNumber, callingCode, e164 string) {
-				actual, err := Parse(nationalNumber, callingCode)
-				if e164 == "" {
-					So(err, ShouldNotBeNil)
-				} else {
-					So(actual, ShouldEqual, e164)
+			Convey("valid cases", func() {
+				check := func(nationalNumber, callingCode, e164 string) {
+					actual, err := Parse(nationalNumber, callingCode)
+					if e164 == "" {
+						So(err, ShouldNotBeNil)
+					} else {
+						So(actual, ShouldEqual, e164)
+					}
 				}
-			}
-			// calling code can have optional + sign
-			check("99887766", "+852", "+85299887766")
-			check("99887766", "852", "+85299887766")
+				// calling code can have optional + sign
+				check("98887766", "+852", "+85298887766")
+				check("98887766", "852", "+85298887766")
 
-			// national number can have spaces in between
-			check("9988 7766", "852", "+85299887766")
-			check(" 9988 7766 ", "852", "+85299887766")
+				// national number can have spaces in between
+				check("9888 7766", "852", "+85298887766")
+				check(" 9888 7766 ", "852", "+85298887766")
 
-			// national number can have hyphens in between
-			check("9988-7766", "852", "+85299887766")
-			check(" 9988-7766 ", "852", "+85299887766")
-			check("99-88-77-66", "852", "+85299887766")
-			check("9-9-8-8-7-7-6-6", "852", "+85299887766")
-			check("9 - 9 - 8 - 8 - 7 - 7 - 6 - 6 ", "852", "+85299887766")
+				// national number can have hyphens in between
+				check("9888-7766", "852", "+85298887766")
+				check(" 9888-7766 ", "852", "+85298887766")
+				check("98-88-77-66", "852", "+85298887766")
+				check("9-8-8-8-7-7-6-6", "852", "+85298887766")
+				check("9 - 8 - 8 - 8 - 7 - 7 - 6 - 6 ", "852", "+85298887766")
 
-			// calling code can have leading or trailing spaces
-			check("99887766", " +852 ", "+85299887766")
-			check("99887766", " 852 ", "+85299887766")
+				// calling code can have leading or trailing spaces
+				check("98887766", " +852 ", "+85298887766")
+				check("98887766", " 852 ", "+85298887766")
 
-			// calling code can have spaces or hyphens in between
-			check("99887766", "8 52", "+85299887766")
-			check("99887766", "8-52", "+85299887766")
-			check("99887766", " 8-5-2- ", "+85299887766")
+				// calling code can have spaces or hyphens in between
+				check("98887766", "8 52", "+85298887766")
+				check("98887766", "8-52", "+85298887766")
+				check("98887766", " 8-5-2- ", "+85298887766")
+			})
+
+			Convey("should not accept non-numeric character(s)", func() {
+				_, err := Parse("asdf", "+852")
+				So(err, ShouldBeError, "not in E.164 format")
+			})
+
+			Convey("invalid, (+852) phone number does not begin with 1", func() {
+				_, err := Parse("12345678", "+852")
+				So(err, ShouldBeError, "invalid phone number")
+			})
+
+			Convey("invalid, (+852) phone number is 8 digit long", func() {
+				_, err := Parse("6234567", "+852")
+				So(err, ShouldBeError, "invalid phone number")
+			})
 		})
 
 		Convey("Mask", func() {
