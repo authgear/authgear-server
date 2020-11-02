169ef6a1c7859db86d967261d64e5c893a04870f
kiootic <kiootic@oursky.com>
2020-11-02 14:09:18 +0800

    Consolidate Admin API input characteristics

--- pkg/admin/facade/interaction.go
+++ pkg/admin/facade/interaction.go
@@ -12,7 +12,33 @@ type InteractionService interface {
 	Perform(intent interaction.Intent, input interface{}) (*interaction.Graph, error)
 }
 
+// adminAPIOp represents common characteristics applicable to Admin API operations.
+type adminAPIOp struct{}
+
+// BypassMFARequirement indicates Admin API operations does not check
+// for MFA requirement invariant; Admin API can create/delete secondary
+// authenticators freely.
+func (adminAPIOp) BypassMFARequirement() bool { return true }
+
+// SkipVerification indicates Admin API operations does not check
+// for verification requirement; user would be prompt to verify identities
+// if required next login.
+func (adminAPIOp) SkipVerification() bool { return true }
+
+// SkipMFASetup indicates Admin API operations does not setup required
+// secondary authenticator; user would be prompt to setup MFA if required
+// next login.
+func (adminAPIOp) SkipMFASetup() bool { return true }
+
+// BypassPublicSignupDisabled indicates Admin API operations bypass
+// disabled public signup; creating users through admin APIs do not count as
+// public signup.
+func (adminAPIOp) BypassPublicSignupDisabled() bool {
+	return true
+}
+
 type removeIdentityInput struct {
+	adminAPIOp
 	identityInfo *identity.Info
 }
 
@@ -24,17 +50,16 @@ func (i *removeIdentityInput) GetIdentityID() string {
 }
 
 type addIdentityInput struct {
+	adminAPIOp
 	identityDef model.IdentityDef
 }
 
-func (i *addIdentityInput) SkipVerification() bool {
-	return true
-}
 func (i *addIdentityInput) Input() interface{} {
 	return i.identityDef
 }
 
 type removeAuthenticatorInput struct {
+	adminAPIOp
 	authenticatorInfo *authenticator.Info
 }
 
@@ -44,11 +69,9 @@ func (i *removeAuthenticatorInput) GetAuthenticatorType() authn.AuthenticatorTyp
 func (i *removeAuthenticatorInput) GetAuthenticatorID() string {
 	return i.authenticatorInfo.ID
 }
-func (i *removeAuthenticatorInput) BypassMFARequirement() bool {
-	return true
-}
 
 type addPasswordInput struct {
+	adminAPIOp
 	inner    interface{}
 	password string
 }
@@ -61,6 +84,7 @@ func (i *addPasswordInput) Input() interface{} {
 }
 
 type resetPasswordInput struct {
+	adminAPIOp
 	userID   string
 	password string
 }
@@ -68,26 +92,16 @@ type resetPasswordInput struct {
 func (i *resetPasswordInput) GetResetPasswordUserID() string {
 	return i.userID
 }
-
 func (i *resetPasswordInput) GetNewPassword() string {
 	return i.password
 }
 
 type createUserInput struct {
+	adminAPIOp
 	identityDef model.IdentityDef
 	password    string
 }
 
-func (i *createUserInput) BypassPublicSignupDisabled() bool {
-	return true
-}
-
-func (i *createUserInput) SkipVerification() bool {
-	return true
-}
-func (i *createUserInput) SkipMFASetup() bool {
-	return true
-}
 func (i *createUserInput) GetPassword() string {
 	return i.password
 }
