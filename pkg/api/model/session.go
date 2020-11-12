920507c9416d67ab7507ff733e6cee242301c71f
kiootic <kiootic@oursky.com>
2020-11-12 12:26:20 +0800

    Remove unused JSON schemas

--- pkg/api/model/session.go
+++ pkg/api/model/session.go
@@ -15,34 +15,3 @@ type Session struct {
 	LastAccessedByIP string    `json:"last_accessed_by_ip"`
 	UserAgent        UserAgent `json:"user_agent"`
 }
-
-// @JSONSchema
-const SessionSchema = `
-{
-	"$id": "#Session",
-	"type": "object",
-	"properties": {
-		"id": { "type": "string" },
-		"identity_type": { "type": "string" },
-		"identity_claims": { "type": "object" },
-		"acr": { "type": "string" },
-		"amr": { "type": "array", "items": { "type": "string" } },
-		"created_at": { "type": "string" },
-		"last_accessed_at": { "type": "string" },
-		"created_by_ip": { "type": "string" },
-		"last_accessed_by_ip": { "type": "string" },
-		"user_agent": { "$ref": "#UserAgent" }
-	}
-}
-`
-
-// @JSONSchema
-const SessionResponseSchema = `
-{
-	"$id": "#SessionResponse",
-	"type": "object",
-	"properties": {
-		"result": { "$ref": "#Session" }
-	}
-}
-`
