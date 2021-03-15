bc881a8b88e62fadd8b64844aadc0c7cb81fed0b
Louis Chan <louischan@oursky.com>
2021-03-15 19:35:47 +0800

    Enforce the last identity invariant

---
+++ pkg/lib/authn/identity/filters.go
@@ -0,0 +1,49 @@
+package identity
+
+import (
+	"fmt"
+
+	"github.com/authgear/authgear-server/pkg/lib/authn"
+)
+
+type Filter interface {
+	Keep(ii *Info) bool
+}
+
+type FilterFunc func(ii *Info) bool
+
+func (f FilterFunc) Keep(ii *Info) bool {
+	return f(ii)
+}
+
+func ApplyFilters(iis []*Info, filters ...Filter) (out []*Info) {
+	for _, ii := range iis {
+		keep := true
+		for _, f := range filters {
+			if !f.Keep(ii) {
+				keep = false
+				break
+			}
+		}
+		if keep {
+			out = append(out, ii)
+		}
+	}
+	return
+}
+
+// KeepIdentifiable keeps Login ID identity and OAuth identity.
+var KeepIdentifiable FilterFunc = func(ii *Info) bool {
+	switch ii.Type {
+	case authn.IdentityTypeLoginID:
+		return true
+	case authn.IdentityTypeOAuth:
+		return true
+	case authn.IdentityTypeAnonymous:
+		return false
+	case authn.IdentityTypeBiometric:
+		return false
+	default:
+		panic(fmt.Sprintf("identity: unexpected identity type: %s", ii.Type))
+	}
+}
