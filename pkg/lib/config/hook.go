b927803fe4ca73d4d520bffd92176461b7d5ceec
Carmen <carmen@oursky.com>
2021-03-23 18:23:59 +1100

    Add blocking and non-blocking hooks config

--- pkg/lib/config/hook.go
+++ pkg/lib/config/hook.go
@@ -7,15 +7,19 @@ var _ = Schema.Add("HookConfig", `
 	"properties": {
 		"sync_hook_timeout_seconds": { "$ref": "#/$defs/DurationSeconds" },
 		"sync_hook_total_timeout_seconds": { "$ref": "#/$defs/DurationSeconds" },
-		"handlers": { "type": "array", "items": { "$ref": "#/$defs/HookHandlerConfig" } }
+		"handlers": { "type": "array", "items": { "$ref": "#/$defs/HookHandlerConfig" } },
+		"blocking_handlers": { "type": "array", "items": { "$ref": "#/$defs/BlockingHookHandlersConfig" } },
+		"non_blocking_handlers": { "type": "array", "items": { "$ref": "#/$defs/NonBlockingHookHandlersConfig" } }
 	}
 }
 `)
 
 type HookConfig struct {
-	SyncTimeout      DurationSeconds     `json:"sync_hook_timeout_seconds,omitempty"`
-	SyncTotalTimeout DurationSeconds     `json:"sync_hook_total_timeout_seconds,omitempty"`
-	Handlers         []HookHandlerConfig `json:"handlers,omitempty"`
+	SyncTimeout         DurationSeconds             `json:"sync_hook_timeout_seconds,omitempty"`
+	SyncTotalTimeout    DurationSeconds             `json:"sync_hook_total_timeout_seconds,omitempty"`
+	Handlers            []HookHandlerConfig         `json:"handlers,omitempty"`
+	BlockingHandlers    []BlockingHandlersConfig    `json:"blocking_handlers,omitempty"`
+	NonBlockingHandlers []NonBlockingHandlersConfig `json:"non_blocking_handlers,omitempty"`
 }
 
 func (c *HookConfig) SetDefaults() {
@@ -42,4 +46,60 @@ var _ = Schema.Add("HookHandlerConfig", `
 type HookHandlerConfig struct {
 	Event string `json:"event"`
 	URL   string `json:"url"`
+}
+
+var _ = Schema.Add("BlockingHookHandlersConfig", `
+{
+	"type": "object",
+	"additionalProperties": false,
+	"properties": {
+		"event": { "type": "string", "enum" : ["pre_signup", "admin_api_create_user"] },
+		"url": { "type": "string", "format": "uri" }
+	},
+	"required": ["event", "url"]
+}
+`)
+
+type BlockingHandlersConfig struct {
+	Event string `json:"event"`
+	URL   string `json:"url"`
+}
+
+var _ = Schema.Add("NonBlockingHookHandlersConfig", `
+{
+	"type": "object",
+	"additionalProperties": false,
+	"properties": {
+		"events": {
+			"type": "array",
+			"items": {
+				"type": "string",
+				"enum" : [
+					"*",
+					"user.created.user_signup",
+					"user.created.admin_api_create_user",
+					"identity.created.user_add_identity",
+					"identity.created.admin_api_add_identity",
+					"identity.deleted.user_remove_identity",
+					"identity.deleted.admin_api_remove_identity",
+					"identity.updated.user_update_identity",
+					"session.created.user_signup",
+					"session.created.user_login",
+					"session.created.user_promote_themselves",
+					"session.deleted.user_revoke_session",
+					"session.deleted.user_logout",
+					"session.deleted.admin_api_revoke_session",
+					"user.promoted.user_promote_themselves"
+				]
+			}
+		},
+		"url": { "type": "string", "format": "uri" }
+	},
+	"required": ["events", "url"]
+}
+`)
+
+type NonBlockingHandlersConfig struct {
+	Events []string `json:"events"`
+	URL    string   `json:"url"`
 }
