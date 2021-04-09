7341fceb18e6232cce7b85bfbee97d1451c1ce5b
Louis Chan <louischan@oursky.com>
2021-04-09 20:35:02 +0800

    Remove unused method IssueTokens

--- pkg/lib/oauth/handler/handler_token_issue.go
+++ pkg/lib/oauth/handler/handler_token_issue.go
@@ -1,48 +1,12 @@
 package handler
 
 import (
-	"github.com/authgear/authgear-server/pkg/lib/config"
 	"github.com/authgear/authgear-server/pkg/lib/oauth"
 	"github.com/authgear/authgear-server/pkg/lib/oauth/protocol"
-	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/util/duration"
 )
 
 const AppSessionTokenDuration = duration.Short
-
-func (h *TokenHandler) IssueTokens(
-	client *config.OAuthClientConfig,
-	attrs *session.Attrs,
-) (session.Session, protocol.TokenResponse, error) {
-	scopes := []string{"openid", oauth.FullAccessScope}
-
-	authz, err := checkAuthorization(
-		h.Authorizations,
-		h.Clock.NowUTC(),
-		h.AppID,
-		client.ClientID,
-		attrs.UserID,
-		scopes,
-	)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	resp := protocol.TokenResponse{}
-
-	offlineGrant, err := h.issueOfflineGrant(client, scopes, authz.ID, attrs, resp)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	err = h.issueAccessGrant(client, scopes, authz.ID, authz.UserID,
-		offlineGrant.ID, oauth.GrantSessionKindOffline, resp)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return offlineGrant, resp, nil
-}
 
 func (h *TokenHandler) IssueAppSessionToken(refreshToken string) (string, *oauth.AppSessionToken, error) {
 	authz, grant, err := h.parseRefreshToken(refreshToken)
