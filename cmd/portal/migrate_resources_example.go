5ba10dfd3834195f714f175c747d4f38170027cf
Carmen <carmen@oursky.com>
2021-04-26 17:51:32 +1000

    Add migrate resources example

--- cmd/portal/migrate_resources_example.go
+++ cmd/portal/migrate_resources_example.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/base64"
+	"fmt"
 	"log"
 
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 
 	"github.com/authgear/authgear-server/cmd/portal/internal"
 )
@@ -28,10 +31,45 @@ var cmdInternalMigrateExample = &cobra.Command{
 }
 
 func migrateResourcesExample(appID string, configSourceData map[string]string) error {
-	// FIXME: more concrete example
-	if appID == "accounts" {
-		configSourceData["portal"] = "true"
+	// example update app accounts' dark_theme_disabled
+	if appID != "accounts" {
+		return nil
 	}
+	encodedData := configSourceData["authgear.yaml"]
+	decoded, err := base64.StdEncoding.DecodeString(encodedData)
+	if err != nil {
+		return fmt.Errorf("failed decode authgear.yaml: %w", err)
+	}
+
+	log.Printf("Before updated:")
+	log.Printf("\n%s\n", string(decoded))
+
+	m := make(map[string]interface{})
+	err = yaml.Unmarshal(decoded, &m)
+	if err != nil {
+		return fmt.Errorf("failed unmarshal yaml: %w", err)
+	}
+
+	uiSettingMap := m["ui"]
+	var uiSetting map[interface{}]interface{}
+	if uiSettingMap != nil {
+		uiSetting = uiSettingMap.(map[interface{}]interface{})
+	} else {
+		uiSetting = make(map[interface{}]interface{})
+	}
+
+	uiSetting["dark_theme_disabled"] = true
+	m["ui"] = uiSetting
+
+	authgearyaml, err := yaml.Marshal(&m)
+	if err != nil {
+		return fmt.Errorf("failed marshal yaml: %w", err)
+	}
+
+	log.Printf("After updated:")
+	log.Printf("\n%s\n", string(authgearyaml))
+
+	configSourceData["authgear.yaml"] = base64.StdEncoding.EncodeToString(authgearyaml)
 	return nil
 }
 
