b7c7cd077481fb5a9380be631628fe4c1ce8d070
Louis Chan <louischan@oursky.com>
2021-06-04 17:19:08 +0800

    Add simplified AuditLogList

---
+++ portal/src/graphql/adminapi/AuditLogList.tsx
@@ -0,0 +1,120 @@
+import React, { useContext, useMemo } from "react";
+import cn from "classnames";
+import {
+  IColumn,
+  ColumnActionsMode,
+  SelectionMode,
+  DetailsListLayoutMode,
+  ShimmeredDetailsList,
+} from "@fluentui/react";
+import { Context } from "@oursky/react-messageformat";
+import PaginationWidget from "../../PaginationWidget";
+import { AuditLogListQuery_auditLogs } from "./__generated__/AuditLogListQuery";
+import { formatDatetime } from "../../util/formatDatetime";
+import useDelayedValue from "../../hook/useDelayedValue";
+
+import styles from "./AuditLogList.module.scss";
+
+export interface AuditLogListProps {
+  className?: string;
+  loading: boolean;
+  auditLogs: AuditLogListQuery_auditLogs | null;
+  offset: number;
+  pageSize: number;
+  totalCount?: number;
+  onChangeOffset?: (offset: number) => void;
+}
+
+interface AuditLogListItem {
+  id: string;
+  activityType: string;
+  createdAt: string;
+  userID: string | null;
+}
+
+const AuditLogList: React.FC<AuditLogListProps> = function AuditLogList(props) {
+  const {
+    className,
+    loading: rawLoading,
+    offset,
+    pageSize,
+    totalCount,
+    onChangeOffset,
+  } = props;
+  const edges = props.auditLogs?.edges;
+
+  const loading = useDelayedValue(rawLoading, 500);
+
+  const { renderToString, locale } = useContext(Context);
+
+  const columns: IColumn[] = [
+    {
+      key: "activityType",
+      fieldName: "activityType",
+      name: renderToString("AuditLogList.column.activity-type"),
+      maxWidth: 300,
+      minWidth: 300,
+      columnActionsMode: ColumnActionsMode.disabled,
+    },
+    {
+      key: "createdAt",
+      fieldName: "createdAt",
+      name: renderToString("AuditLogList.column.created-at"),
+      maxWidth: 150,
+      minWidth: 150,
+      columnActionsMode: ColumnActionsMode.disabled,
+    },
+    {
+      key: "userID",
+      fieldName: "userID",
+      name: renderToString("AuditLogList.column.user-id"),
+      minWidth: 430,
+      columnActionsMode: ColumnActionsMode.disabled,
+    },
+  ];
+
+  const items: AuditLogListItem[] = useMemo(() => {
+    const items = [];
+    if (edges != null) {
+      for (const edge of edges) {
+        const node = edge?.node;
+        if (node != null) {
+          items.push({
+            id: node.id,
+            createdAt: formatDatetime(locale, node.createdAt)!,
+            activityType: node.activityType,
+            userID: node.user?.id ?? null,
+          });
+        }
+      }
+    }
+    return items;
+  }, [edges, locale]);
+
+  return (
+    <>
+      <div className={cn(styles.root, className)}>
+        <div className={styles.listWrapper}>
+          <ShimmeredDetailsList
+            className={styles.list}
+            enableShimmer={loading}
+            enableUpdateAnimations={false}
+            selectionMode={SelectionMode.none}
+            layoutMode={DetailsListLayoutMode.justified}
+            columns={columns}
+            items={items}
+          />
+        </div>
+        <PaginationWidget
+          className={styles.pagination}
+          offset={offset}
+          pageSize={pageSize}
+          totalCount={totalCount}
+          onChangeOffset={onChangeOffset}
+        />
+      </div>
+    </>
+  );
+};
+
+export default AuditLogList;
--- portal/src/graphql/adminapi/AuditLogScreen.tsx
+++ portal/src/graphql/adminapi/AuditLogScreen.tsx
@@ -1,7 +1,94 @@
-import React from "react";
+import React, { useState, useMemo, useCallback } from "react";
+import { FormattedMessage } from "@oursky/react-messageformat";
+import { gql, useQuery } from "@apollo/client";
+import NavBreadcrumb from "../../NavBreadcrumb";
+import AuditLogList from "./AuditLogList";
+import CommandBarContainer from "../../CommandBarContainer";
+import ShowError from "../../ShowError";
+import { encodeOffsetToCursor } from "../../util/pagination";
+import {
+  AuditLogListQuery,
+  AuditLogListQueryVariables,
+} from "./__generated__/AuditLogListQuery";
+
+import styles from "./AuditLogScreen.module.scss";
+
+const pageSize = 10;
+
+const QUERY = gql`
+  query AuditLogListQuery($pageSize: Int!, $cursor: String) {
+    auditLogs(first: $pageSize, after: $cursor) {
+      edges {
+        node {
+          id
+          createdAt
+          activityType
+          user {
+            id
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`;
 
 const AuditLogScreen: React.FC = function AuditLogScreen() {
-  return <div>Audit Log Screen</div>;
+  const [offset, setOffset] = useState(0);
+
+  const items = useMemo(() => {
+    return [{ to: ".", label: <FormattedMessage id="AuditLogScreen.title" /> }];
+  }, []);
+
+  const cursor = useMemo(() => {
+    if (offset === 0) {
+      return null;
+    }
+    return encodeOffsetToCursor(offset - 1);
+  }, [offset]);
+
+  const onChangeOffset = useCallback((offset) => {
+    setOffset(offset);
+  }, []);
+
+  const { data, error, loading, refetch } = useQuery<
+    AuditLogListQuery,
+    AuditLogListQueryVariables
+  >(QUERY, {
+    variables: {
+      pageSize,
+      cursor,
+    },
+    fetchPolicy: "network-only",
+  });
+
+  const messageBar = useMemo(() => {
+    if (error != null) {
+      return <ShowError error={error} onRetry={refetch} />;
+    }
+    return null;
+  }, [error, refetch]);
+
+  return (
+    <CommandBarContainer
+      isLoading={loading}
+      className={styles.root}
+      messageBar={messageBar}
+    >
+      <main className={styles.content}>
+        <NavBreadcrumb items={items} />
+        <AuditLogList
+          className={styles.list}
+          loading={loading}
+          auditLogs={data?.auditLogs ?? null}
+          offset={offset}
+          pageSize={pageSize}
+          totalCount={data?.auditLogs?.totalCount ?? undefined}
+          onChangeOffset={onChangeOffset}
+        />
+      </main>
+    </CommandBarContainer>
+  );
 };
 
 export default AuditLogScreen;
