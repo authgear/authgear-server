bef31a83502718a4a8222220b9f3f2b7c4dbbd81
Louis Chan <louischan@oursky.com>
2021-06-04 14:20:39 +0800

    Document the reason why JSON keys are in camel case

--- pkg/portal/model/domain.go
+++ pkg/portal/model/domain.go
@@ -4,6 +4,10 @@ import (
 	"time"
 )
 
+// Domain represents a domain of an app.
+// The keys in JSON struct tags are in camel case
+// because this struct is directly returned in the GraphQL endpoint.
+// Making the keys in camel case saves us from writing boilerplate resolver code.
 type Domain struct {
 	ID                    string    `json:"id"`
 	AppID                 string    `json:"appID"`
