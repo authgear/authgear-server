4134ceec07393485c7220f4d8348884d77f3354a
Louis Chan <louischan@oursky.com>
2021-06-06 22:05:30 +0800

    Add date range control

--- portal/src/graphql/adminapi/AuditLogScreen.tsx
+++ portal/src/graphql/adminapi/AuditLogScreen.tsx
@@ -1,5 +1,14 @@
 import React, { useState, useMemo, useCallback, useContext } from "react";
-import { ICommandBarItemProps, IDropdownOption } from "@fluentui/react";
+import {
+  ICommandBarItemProps,
+  IDropdownOption,
+  Dialog,
+  DialogFooter,
+  PrimaryButton,
+  DefaultButton,
+  DatePicker,
+  TextField,
+} from "@fluentui/react";
 import { FormattedMessage, Context } from "@oursky/react-messageformat";
 import { gql, useQuery } from "@apollo/client";
 import NavBreadcrumb from "../../NavBreadcrumb";
@@ -10,6 +19,7 @@ import CommandBarDropdown, {
 import CommandBarContainer from "../../CommandBarContainer";
 import ShowError from "../../ShowError";
 import { encodeOffsetToCursor } from "../../util/pagination";
+import useTranactionalState from "../../hook/useTranactionalState";
 import {
   AuditLogListQuery,
   AuditLogListQueryVariables,
@@ -50,6 +60,27 @@ function CommandBarDropdownWrapper(props: ICommandBarItemProps) {
 const AuditLogScreen: React.FC = function AuditLogScreen() {
   const [offset, setOffset] = useState(0);
   const [selectedKey, setSelectedKey] = useState("ALL");
+  const [dateRangeDialogHidden, setDateRangeDialogHidden] = useState(true);
+
+  const {
+    committedValue: rangeFrom,
+    uncommittedValue: uncommittedRangeFrom,
+    setValue: setRangeFrom,
+    setCommittedValue: setRangeFromImmediately,
+    commit: commitRangeFrom,
+    rollback: rollbackRangeFrom,
+  } = useTranactionalState<Date | null>(null);
+
+  const {
+    committedValue: rangeTo,
+    uncommittedValue: uncommittedRangeTo,
+    setValue: setRangeTo,
+    setCommittedValue: setRangeToImmediately,
+    commit: commitRangeTo,
+    rollback: rollbackRangeTo,
+  } = useTranactionalState<Date | null>(null);
+
+  const isCustomDateRange = rangeFrom != null || rangeTo != null;
 
   const { renderToString } = useContext(Context);
 
@@ -133,36 +164,176 @@ const AuditLogScreen: React.FC = function AuditLogScreen() {
     };
   }, [selectedKey, onChangeSelectedKey, activityTypeOptions]);
 
+  const onClickAllDateRange = useCallback(
+    (e?: React.MouseEvent<unknown> | React.KeyboardEvent<unknown>) => {
+      e?.stopPropagation();
+      setRangeFromImmediately(null);
+      setRangeToImmediately(null);
+    },
+    [setRangeFromImmediately, setRangeToImmediately]
+  );
+
+  const onClickCustomDateRange = useCallback(
+    (e?: React.MouseEvent<unknown> | React.KeyboardEvent<unknown>) => {
+      e?.stopPropagation();
+      setDateRangeDialogHidden(false);
+    },
+    []
+  );
+
   const commandBarFarItems: ICommandBarItemProps[] = useMemo(() => {
+    const allDateRangeLabel = renderToString("AuditLogScreen.date-range.all");
+    const customDateRangeLabel = renderToString(
+      "AuditLogScreen.date-range.custom"
+    );
     return [
+      {
+        key: "dateRange",
+        text: isCustomDateRange ? customDateRangeLabel : allDateRangeLabel,
+        iconProps: { iconName: "Calendar" },
+        subMenuProps: {
+          items: [
+            {
+              key: "allDateRange",
+              text: allDateRangeLabel,
+              onClick: onClickAllDateRange,
+            },
+            {
+              key: "customDateRange",
+              text: customDateRangeLabel,
+              onClick: onClickCustomDateRange,
+            },
+          ],
+        },
+      },
       {
         key: "activityTypes",
         commandBarButtonAs: CommandBarDropdownWrapper,
         dropdownProps,
       },
     ];
-  }, [dropdownProps]);
+  }, [
+    dropdownProps,
+    renderToString,
+    isCustomDateRange,
+    onClickAllDateRange,
+    onClickCustomDateRange,
+  ]);
+
+  const onDismissDateRangeDialog = useCallback(
+    (e?: React.MouseEvent<unknown>) => {
+      e?.stopPropagation();
+      setDateRangeDialogHidden(true);
+      rollbackRangeFrom();
+      rollbackRangeTo();
+    },
+    [rollbackRangeFrom, rollbackRangeTo]
+  );
+
+  const commitDateRange = useCallback(
+    (e?: React.MouseEvent<unknown>) => {
+      e?.preventDefault();
+      e?.stopPropagation();
+      setDateRangeDialogHidden(true);
+      commitRangeFrom();
+      commitRangeTo();
+      setOffset(0);
+    },
+    [commitRangeFrom, commitRangeTo]
+  );
+
+  const dateRangeDialogContentProps = useMemo(() => {
+    const title = renderToString("AuditLogScreen.date-range.custom");
+    return {
+      title,
+    };
+  }, [renderToString]);
+
+  const onSelectRangeFrom = useCallback(
+    (value: Date | null | undefined) => {
+      if (value == null) {
+        setRangeFrom(null);
+      } else {
+        if (uncommittedRangeTo != null && value > uncommittedRangeTo) {
+          setRangeTo(value);
+          setRangeFrom(uncommittedRangeTo);
+        } else {
+          setRangeFrom(value);
+        }
+      }
+    },
+    [setRangeFrom, setRangeTo, uncommittedRangeTo]
+  );
+
+  const onSelectRangeTo = useCallback(
+    (value: Date | null | undefined) => {
+      if (value == null) {
+        setRangeTo(null);
+      } else {
+        if (uncommittedRangeFrom != null && value < uncommittedRangeFrom) {
+          setRangeFrom(value);
+          setRangeTo(uncommittedRangeFrom);
+        } else {
+          setRangeTo(value);
+        }
+      }
+    },
+    [setRangeTo, setRangeFrom, uncommittedRangeFrom]
+  );
 
   return (
-    <CommandBarContainer
-      isLoading={loading}
-      className={styles.root}
-      messageBar={messageBar}
-      farItems={commandBarFarItems}
-    >
-      <main className={styles.content}>
-        <NavBreadcrumb items={items} />
-        <AuditLogList
-          className={styles.list}
-          loading={loading}
-          auditLogs={data?.auditLogs ?? null}
-          offset={offset}
-          pageSize={pageSize}
-          totalCount={data?.auditLogs?.totalCount ?? undefined}
-          onChangeOffset={onChangeOffset}
+    <>
+      <CommandBarContainer
+        isLoading={loading}
+        className={styles.root}
+        messageBar={messageBar}
+        farItems={commandBarFarItems}
+      >
+        <main className={styles.content}>
+          <NavBreadcrumb items={items} />
+          <AuditLogList
+            className={styles.list}
+            loading={loading}
+            auditLogs={data?.auditLogs ?? null}
+            offset={offset}
+            pageSize={pageSize}
+            totalCount={data?.auditLogs?.totalCount ?? undefined}
+            onChangeOffset={onChangeOffset}
+          />
+        </main>
+      </CommandBarContainer>
+      <Dialog
+        hidden={dateRangeDialogHidden}
+        onDismiss={onDismissDateRangeDialog}
+        dialogContentProps={dateRangeDialogContentProps}
+        /* https://developer.microsoft.com/en-us/fluentui#/controls/web/dialog
+         * Best practice says the max width is 340 */
+        minWidth={340}
+      >
+        {/* Dialog is based on Modal, which will focus the first child on open. *
+        However, we do not want the date picker to be opened at the same time. *
+        So we make the first focusable element a hidden TextField */}
+        <TextField className={styles.hidden} />
+        <DatePicker
+          label={renderToString("AuditLogScreen.date-range.start-date")}
+          value={uncommittedRangeFrom ?? undefined}
+          onSelectDate={onSelectRangeFrom}
         />
-      </main>
-    </CommandBarContainer>
+        <DatePicker
+          label={renderToString("AuditLogScreen.date-range.end-date")}
+          value={uncommittedRangeTo ?? undefined}
+          onSelectDate={onSelectRangeTo}
+        />
+        <DialogFooter>
+          <PrimaryButton onClick={commitDateRange}>
+            <FormattedMessage id="done" />
+          </PrimaryButton>
+          <DefaultButton onClick={onDismissDateRangeDialog}>
+            <FormattedMessage id="cancel" />
+          </DefaultButton>
+        </DialogFooter>
+      </Dialog>
+    </>
   );
 };
 
