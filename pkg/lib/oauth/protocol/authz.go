1fe51d01736b732d8e70da18af031fa2407ee08c
Louis Chan <louischan@oursky.com>
2021-06-23 17:45:11 +0800

    Re-organize the methods of AuthorizationRequest

--- pkg/lib/oauth/protocol/authz.go
+++ pkg/lib/oauth/protocol/authz.go
@@ -3,37 +3,33 @@ package protocol
 import "strings"
 
 type AuthorizationRequest map[string]string
-type AuthorizationResponse map[string]string
 
 // OAuth 2.0
-
-func (r AuthorizationRequest) ResponseMode() string { return r["response_mode"] }
-func (r AuthorizationRequest) ResponseType() string { return r["response_type"] }
 func (r AuthorizationRequest) ClientID() string     { return r["client_id"] }
 func (r AuthorizationRequest) RedirectURI() string  { return r["redirect_uri"] }
+func (r AuthorizationRequest) ResponseType() string { return r["response_type"] }
+func (r AuthorizationRequest) ResponseMode() string { return r["response_mode"] }
 func (r AuthorizationRequest) Scope() []string      { return parseSpaceDelimitedString(r["scope"]) }
-func (r AuthorizationRequest) Prompt() []string     { return parseSpaceDelimitedString(r["prompt"]) }
 func (r AuthorizationRequest) State() string        { return r["state"] }
-func (r AuthorizationRequest) LoginHint() string    { return r["login_hint"] }
-
-func (r AuthorizationRequest) SetPrompt(prompt []string)     { r["prompt"] = strings.Join(prompt, " ") }
-func (r AuthorizationRequest) SetLoginHint(loginHint string) { r["login_hint"] = loginHint }
-
-func (r AuthorizationResponse) Code(v string)  { r["code"] = v }
-func (r AuthorizationResponse) State(v string) { r["state"] = v }
 
 // OIDC extension
-
-func (r AuthorizationRequest) Nonce() string       { return r["nonce"] }
-func (r AuthorizationRequest) UILocales() []string { return parseSpaceDelimitedString(r["ui_locales"]) }
+func (r AuthorizationRequest) Prompt() []string              { return parseSpaceDelimitedString(r["prompt"]) }
+func (r AuthorizationRequest) SetPrompt(prompt []string)     { r["prompt"] = strings.Join(prompt, " ") }
+func (r AuthorizationRequest) Nonce() string                 { return r["nonce"] }
+func (r AuthorizationRequest) UILocales() []string           { return parseSpaceDelimitedString(r["ui_locales"]) }
+func (r AuthorizationRequest) LoginHint() string             { return r["login_hint"] }
+func (r AuthorizationRequest) SetLoginHint(loginHint string) { r["login_hint"] = loginHint }
 
 // PKCE extension
-
 func (r AuthorizationRequest) CodeChallenge() string       { return r["code_challenge"] }
 func (r AuthorizationRequest) CodeChallengeMethod() string { return r["code_challenge_method"] }
 
 // Proprietary
-
 func (r AuthorizationRequest) Platform() string          { return r["x_platform"] }
 func (r AuthorizationRequest) WeChatRedirectURI() string { return r["x_wechat_redirect_uri"] }
 func (r AuthorizationRequest) Page() string              { return r["x_page"] }
+
+type AuthorizationResponse map[string]string
+
+func (r AuthorizationResponse) Code(v string)  { r["code"] = v }
+func (r AuthorizationResponse) State(v string) { r["state"] = v }
