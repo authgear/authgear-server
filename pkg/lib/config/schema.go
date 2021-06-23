601f284f57d9d4c831e58b996cd15b70b316a972
Carmen <carmen@oursky.com>
2021-06-23 12:40:34 +1000

    Add FeatureConfig model

--- pkg/lib/config/schema.go
+++ pkg/lib/config/schema.go
@@ -8,9 +8,12 @@ var Schema = validation.NewMultipartSchema("AppConfig")
 
 var SecretConfigSchema = validation.NewMultipartSchema("SecretConfig")
 
+var FeatureConfigSchema = validation.NewMultipartSchema("FeatureConfig")
+
 func init() {
 	Schema.Instantiate()
 	SecretConfigSchema.Instantiate()
+	FeatureConfigSchema.Instantiate()
 }
 
 func DumpSchema() (string, error) {
