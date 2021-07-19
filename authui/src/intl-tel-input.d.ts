9ea091523f9e050d7ecd3fcb3c8978e55c06f257
Louis Chan <louischan@oursky.com>
2021-07-19 14:18:35 +0800

    Add type definition for intl-tel-input

---
+++ authui/src/intl-tel-input.d.ts
@@ -0,0 +1,69 @@
+// Type definition for intl-tel-input v17.0.13
+
+declare type IntlTelInputAlpha2 = string;
+
+declare interface IntlTelInputCountryData {
+  name: string;
+  iso2: IntlTelInputAlpha2;
+  dialCode: string;
+}
+
+declare type IntlTelInputPlaceholderNumberType = "MOBILE" | "FIXED_LINE";
+
+declare interface IntlTelInputInstance {
+  getExtension(): string | undefined | null;
+  getNumber(): string | undefined | null;
+  getNumberType(): number | undefined | null;
+  getSelectedCountryData(): IntlTelInputCountryData;
+  getValidationError(): number | undefined | null;
+  isValidNumber(): boolean;
+
+  destroy();
+  setCountry(alpha2: IntlTelInputAlpha2);
+  setNumber(e164: string);
+  setPlaceholderNumberType(typ: IntlTelInputPlaceholderNumberType);
+}
+
+declare interface IntlTelInputInitOptions {
+  // UI
+  autoPlaceholder?: "polite" | "aggressive" | "off";
+  placeholderNumberType?: IntlTelInputPlaceholderNumberType;
+  customContainer?: string;
+  customPlaceholder?: (selectedCountryPlaceholder: string, selectedCountryData: IntlTelInputCountryData) => string;
+  dropdownContainer?: HTMLElement;
+
+  // Functionality
+  utilsScript?: string;
+  nationalMode?: boolean;
+  autoHideDialCode?: boolean;
+  separateDialCode?: boolean;
+  allowDropdown?: boolean;
+  formatOnDisplay?: boolean;
+  geoIpLookup?: (success: (alpha2: IntlTelInputAlpha2) => void, failure: (err: unknown) => void) => void;
+  initialCountry?: "auto" | IntlTelInputAlpha2;
+  excludeCountries?: IntlTelInputAlpha2[];
+  onlyCountries?: IntlTelInputAlpha2[];
+  preferredCountries?: IntlTelInputAlpha2[];
+
+  // Localization
+  localizedCountries?: Record<IntlTelInputAlpha2, string>;
+
+  // Form integration
+  hiddenInput?: string;
+}
+
+declare interface IntlTelInputInitFunction {
+  (element: HTMLElement, options?: IntlTelInputInitOptions): IntlTelInputInstance;
+}
+
+declare interface IntlTelInputGlobals {
+  getCountryData(): IntlTelInputCountryData[];
+  getInstance(element: HTMLElement): IntlTelInputInstance | null | undefined;
+  loadUtils(s: string): Promise<void>;
+}
+
+// Inject things into window.
+declare interface Window {
+  intlTelInputGlobals: IntlTelInputGlobals;
+  intlTelInput: IntlTelInputInitFunction;
+}
