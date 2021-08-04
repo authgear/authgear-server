01a1f2532f2d40741df22f7ba47a6132873c3795
Sam Chan <samchan0221@gmail.com>
2021-08-04 16:00:10 +0800

    Added ResourcTooLarge error on frontend

--- portal/src/error/resources.ts
+++ portal/src/error/resources.ts
@@ -2,3 +2,13 @@ export interface APIResourceNotFoundError {
   errorName: "ResourceNotFound";
   reason: "ResourceNotFound";
 }
+
+export interface APIResourceTooLargeError {
+  errorName: string;
+  reason: "ResourceTooLarge";
+  info: {
+    size: number;
+    max_size: number;
+    path: string;
+  };
+}
