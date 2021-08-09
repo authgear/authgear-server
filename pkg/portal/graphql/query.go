de340c9276f13b72b61cc6856df742abfbace818
Sam Chan <samchan0221@gmail.com>
2021-08-09 14:24:26 +0800

    Removed isCodeValid in checkCollaboratorInvitation

--- pkg/portal/graphql/query.go
+++ pkg/portal/graphql/query.go
@@ -4,6 +4,7 @@ import (
 	relay "github.com/authgear/graphql-go-relay"
 	"github.com/graphql-go/graphql"
 
+	"github.com/authgear/authgear-server/pkg/api/apierrors"
 	"github.com/authgear/authgear-server/pkg/portal/session"
 	"github.com/authgear/authgear-server/pkg/util/graphqlutil"
 )
@@ -11,9 +12,8 @@ import (
 var checkCollaboratorInvitationPayload = graphql.NewObject(graphql.ObjectConfig{
 	Name: "CheckCollaboratorInvitationPayload",
 	Fields: graphql.Fields{
-		"isCodeValid": &graphql.Field{Type: graphql.NewNonNull(graphql.Boolean)},
 		"isInvitee": &graphql.Field{Type: graphql.NewNonNull(graphql.Boolean)},
-		"appID": &graphql.Field{Type: graphql.NewNonNull(graphql.String)},
+		"appID":     &graphql.Field{Type: graphql.NewNonNull(graphql.String)},
 	},
 })
 
@@ -66,45 +66,38 @@ var query = graphql.NewObject(graphql.ObjectConfig{
 		},
 		"checkCollaboratorInvitation": &graphql.Field{
 			Description: "Check whether the viewer can accept the collaboration invitation",
-			Type:        graphql.NewNonNull(checkCollaboratorInvitationPayload),
-			Args:        graphql.FieldConfigArgument{"code": &graphql.ArgumentConfig{Type:graphql.NewNonNull(graphql.String)}},
+			Type:        checkCollaboratorInvitationPayload,
+			Args:        graphql.FieldConfigArgument{"code": &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String)}},
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 				ctx := GQLContext(p.Context)
 
 				code := p.Args["code"].(string)
 
 				invitation, err := ctx.CollaboratorService.GetInvitationWithCode(code)
-				if err != nil {
-					return graphqlutil.NewLazyValue(map[string]interface{}{
-						"isCodeValid": false,
-						"isInvitee": false,
-						"appID": "",
-					}).Value, nil
+				if err != nil && apierrors.IsKind(err, apierrors.Invalid.WithReason("CollaboratorInvitationInvalidCode")) {
+					return nil, nil
 				}
 
 				sessionInfo := session.GetValidSessionInfo(p.Context)
 				if sessionInfo == nil {
 					return graphqlutil.NewLazyValue(map[string]interface{}{
-						"isCodeValid": true,
 						"isInvitee": false,
-						"appID": invitation.AppID,
+						"appID":     invitation.AppID,
 					}).Value, nil
 				}
 				actorID := sessionInfo.UserID
 
 				err = ctx.CollaboratorService.CheckInviteeEmail(invitation, actorID)
-				if err != nil {
+				if err != nil && apierrors.IsKind(err, apierrors.Invalid.WithReason("CollaboratorInvitationInvalidEmail")) {
 					return graphqlutil.NewLazyValue(map[string]interface{}{
-						"isCodeValid": true,
 						"isInvitee": false,
-						"appID": invitation.AppID,
+						"appID":     invitation.AppID,
 					}).Value, nil
 				}
 
 				return graphqlutil.NewLazyValue(map[string]interface{}{
-						"isCodeValid": true,
-						"isInvitee": true,
-						"appID": invitation.AppID,
+					"isInvitee": true,
+					"appID":     invitation.AppID,
 				}).Value, nil
 			},
 		},
