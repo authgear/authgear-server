f1f24fdafe5e45037472c0e1034d5f5bbb34d7c8
Carmen <carmen@oursky.com>
2021-09-09 13:41:03 +1000

    Extract generic redis handle

--- pkg/lib/infra/redis/appredis/handle.go
+++ pkg/lib/infra/redis/appredis/handle.go
@@ -1,11 +1,7 @@
 package appredis
 
 import (
-	"context"
-	"time"
-
 	goredis "github.com/go-redis/redis/v8"
-	"github.com/go-redsync/redsync/v4"
 
 	"github.com/authgear/authgear-server/pkg/lib/config"
 	"github.com/authgear/authgear-server/pkg/lib/infra/redis"
@@ -13,59 +9,29 @@ import (
 )
 
 type Handle struct {
-	pool *redis.Pool
-	hub  *redis.Hub
+	*redis.Handle
 
-	cfg         *config.RedisConfig
-	credentials *config.RedisCredentials
-	logger      *log.Logger
+	hub *redis.Hub
 }
 
 func NewHandle(pool *redis.Pool, hub *redis.Hub, cfg *config.RedisConfig, credentials *config.RedisCredentials, lf *log.Factory) *Handle {
 	return &Handle{
-		pool:        pool,
-		hub:         hub,
-		cfg:         cfg,
-		logger:      lf.New("redis-handle"),
-		credentials: credentials,
+		Handle: redis.NewHandle(
+			pool,
+			redis.ConnectionOptions{
+				RedisURL:              credentials.RedisURL,
+				MaxOpenConnection:     cfg.MaxOpenConnection,
+				MaxIdleConnection:     cfg.MaxIdleConnection,
+				IdleConnectionTimeout: cfg.IdleConnectionTimeout,
+				MaxConnectionLifetime: cfg.MaxConnectionLifetime,
+			},
+			lf.New("appredis-handle"),
+		),
+		hub: hub,
 	}
 }
 
-func (h *Handle) WithConn(f func(conn *goredis.Conn) error) error {
-	h.logger.WithFields(map[string]interface{}{
-		"max_open_connection":             *h.cfg.MaxOpenConnection,
-		"max_idle_connection":             *h.cfg.MaxIdleConnection,
-		"idle_connection_timeout_seconds": *h.cfg.IdleConnectionTimeout,
-		"max_connection_lifetime_seconds": *h.cfg.MaxConnectionLifetime,
-	}).Debug("open redis connection")
-
-	ctx := context.Background()
-	conn := h.Client().Conn(ctx)
-	defer func() {
-		err := conn.Close()
-		if err != nil {
-			h.logger.WithError(err).Error("failed to close connection")
-		}
-	}()
-
-	return f(conn)
-}
-
 func (h *Handle) Subscribe(channelName string) (chan *goredis.Message, func()) {
-	sub := h.hub.Subscribe(h.cfg, h.credentials, channelName)
+	sub := h.hub.Subscribe(&h.ConnectionOptions, channelName)
 	return sub.MessageChannel, sub.Cancel
-}
-
-func (h *Handle) Client() *goredis.Client {
-	return h.pool.Client(h.cfg, h.credentials)
-}
-
-func (h *Handle) NewMutex(name string) *redsync.Mutex {
-	redsyncInstance := h.pool.Instance(h.cfg, h.credentials).Redsync
-	mutex := redsyncInstance.NewMutex(
-		name,
-		redsync.WithExpiry(5*time.Second),
-		redsync.WithTries(5),
-	)
-	return mutex
 }
---
+++ pkg/lib/infra/redis/handle.go
@@ -0,0 +1,60 @@
+package redis
+
+import (
+	"context"
+	"time"
+
+	goredis "github.com/go-redis/redis/v8"
+	"github.com/go-redsync/redsync/v4"
+
+	"github.com/authgear/authgear-server/pkg/util/log"
+)
+
+type Handle struct {
+	pool *Pool
+
+	ConnectionOptions ConnectionOptions
+	logger            *log.Logger
+}
+
+func NewHandle(pool *Pool, connectionOptions ConnectionOptions, logger *log.Logger) *Handle {
+	return &Handle{
+		pool:              pool,
+		ConnectionOptions: connectionOptions,
+		logger:            logger,
+	}
+}
+
+func (h *Handle) WithConn(f func(conn *goredis.Conn) error) error {
+	h.logger.WithFields(map[string]interface{}{
+		"max_open_connection":             *h.ConnectionOptions.MaxOpenConnection,
+		"max_idle_connection":             *h.ConnectionOptions.MaxIdleConnection,
+		"idle_connection_timeout_seconds": *h.ConnectionOptions.IdleConnectionTimeout,
+		"max_connection_lifetime_seconds": *h.ConnectionOptions.MaxConnectionLifetime,
+	}).Debug("open redis connection")
+
+	ctx := context.Background()
+	conn := h.Client().Conn(ctx)
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			h.logger.WithError(err).Error("failed to close connection")
+		}
+	}()
+
+	return f(conn)
+}
+
+func (h *Handle) Client() *goredis.Client {
+	return h.pool.Client(&h.ConnectionOptions)
+}
+
+func (h *Handle) NewMutex(name string) *redsync.Mutex {
+	redsyncInstance := h.pool.instance(&h.ConnectionOptions).Redsync
+	mutex := redsyncInstance.NewMutex(
+		name,
+		redsync.WithExpiry(5*time.Second),
+		redsync.WithTries(5),
+	)
+	return mutex
+}
