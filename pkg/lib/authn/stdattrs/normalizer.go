563fb52af44599f0149127e38fa8dcee0d337334
Louis Chan <louischan@oursky.com>
2021-09-16 16:00:27 +0800

    Normalize all recognized standard attributes

--- pkg/lib/authn/stdattrs/normalizer.go
+++ pkg/lib/authn/stdattrs/normalizer.go
@@ -1,10 +1,14 @@
 package stdattrs
 
 import (
+	"strings"
+	"time"
+
 	"golang.org/x/text/language"
 
 	"github.com/authgear/authgear-server/pkg/lib/authn/identity/loginid"
 	"github.com/authgear/authgear-server/pkg/lib/config"
+	"github.com/authgear/authgear-server/pkg/util/phone"
 	"github.com/authgear/authgear-server/pkg/util/validation"
 )
 
@@ -18,7 +22,23 @@ type Normalizer struct {
 	LoginIDNormalizerFactory LoginIDNormalizerFactory
 }
 
-func (n *Normalizer) Normalize(t T) error {
+func normalizeString(t T, key string) {
+	if value, ok := t[key].(string); ok && value != "" {
+		// noop
+	} else {
+		delete(t, key)
+	}
+}
+
+func normalizeBool(t T, key string) {
+	if _, ok := t[key].(bool); ok {
+		// noop
+	} else {
+		delete(t, key)
+	}
+}
+
+func (n *Normalizer) normalizeEmail(t T) error {
 	if email, ok := t[Email].(string); ok && email != "" {
 		emailNormalizer := n.LoginIDNormalizerFactory.NormalizerWithLoginIDType(config.LoginIDKeyTypeEmail)
 		email, err := emailNormalizer.Normalize(email)
@@ -31,7 +51,34 @@ func (n *Normalizer) Normalize(t T) error {
 		delete(t, Email)
 	}
 
-	// Verify locale is BCP47.
+	return nil
+}
+
+func normalizePhoneNumber(t T) error {
+	if phoneNumber, ok := t[PhoneNumber].(string); ok && phoneNumber != "" {
+		err := phone.EnsureE164(phoneNumber)
+		if err != nil {
+			return err
+		}
+	} else {
+		delete(t, PhoneNumber)
+	}
+
+	return nil
+}
+
+func normalizeURL(t T, key string) {
+	if value, ok := t[key].(string); ok && value != "" {
+		err := validation.FormatURI{}.CheckFormat(value)
+		if err != nil {
+			delete(t, key)
+		}
+	} else {
+		delete(t, key)
+	}
+}
+
+func normalizeLocale(t T) {
 	if locale, ok := t[Locale].(string); ok {
 		_, err := language.Parse(locale)
 		if err != nil {
@@ -40,46 +87,109 @@ func (n *Normalizer) Normalize(t T) error {
 	} else {
 		delete(t, Locale)
 	}
+}
 
-	// Verify family_name, given_name, name are nonempty string
-	if familyName, ok := t[FamilyName].(string); ok && familyName != "" {
-		// noop
-	} else {
-		delete(t, FamilyName)
-	}
-	if givenName, ok := t[GivenName].(string); ok && givenName != "" {
-		// noop
-	} else {
-		delete(t, GivenName)
-	}
-	if name, ok := t[Name].(string); ok && name != "" {
-		// noop
-	} else {
-		delete(t, Name)
-	}
-	if nickname, ok := t[Nickname].(string); ok && nickname != "" {
-		// noop
-	} else {
-		delete(t, Nickname)
-	}
-
-	// Verify picture and profile are URL.
-	if picture, ok := t[Picture].(string); ok && picture != "" {
-		err := validation.FormatURI{}.CheckFormat(picture)
-		if err != nil {
-			delete(t, Picture)
+func normalizeZoneinfo(t T) {
+	if value, ok := t[Zoneinfo].(string); ok {
+		if value == "" {
+			delete(t, Zoneinfo)
+		} else {
+			// Canonical tz database name must contain at least one /
+			hasSlash := strings.Contains(value, "/")
+			if !hasSlash {
+				delete(t, Zoneinfo)
+			} else {
+				_, err := time.LoadLocation(value)
+				if err != nil {
+					delete(t, Zoneinfo)
+				}
+			}
 		}
 	} else {
-		delete(t, Picture)
+		delete(t, Zoneinfo)
 	}
-	if profile, ok := t[Profile].(string); ok && profile != "" {
-		err := validation.FormatURI{}.CheckFormat(profile)
-		if err != nil {
-			delete(t, Profile)
+}
+
+func IsValidBirthdate(value string) bool {
+	if _, err := time.Parse("2006-01-02", value); err == nil {
+		return true
+	}
+
+	if _, err := time.Parse("0000-01-02", value); err == nil {
+		return true
+	}
+
+	if _, err := time.Parse("--01-02", value); err == nil {
+		return true
+	}
+
+	if _, err := time.Parse("2006", value); err == nil {
+		return true
+	}
+
+	return false
+}
+
+func normalizeBirthdate(t T) {
+	if value, ok := t[Birthdate].(string); ok {
+		ok = IsValidBirthdate(value)
+		if !ok {
+			delete(t, Birthdate)
 		}
 	} else {
-		delete(t, Profile)
+		delete(t, Birthdate)
 	}
+}
+
+func normalizeAddress(t T) {
+	if value, ok := t[Address].(map[string]interface{}); ok {
+		normalizeString(T(value), Formatted)
+		normalizeString(T(value), StreetAddress)
+		normalizeString(T(value), Locality)
+		normalizeString(T(value), Region)
+		normalizeString(T(value), PostalCode)
+		normalizeString(T(value), Country)
+		if len(value) <= 0 {
+			delete(t, Address)
+		}
+	} else {
+		delete(t, Address)
+	}
+}
+
+func (n *Normalizer) Normalize(t T) error {
+	err := n.normalizeEmail(t)
+	if err != nil {
+		return err
+	}
+
+	err = normalizePhoneNumber(t)
+	if err != nil {
+		return err
+	}
+
+	normalizeString(t, Name)
+	normalizeString(t, GivenName)
+	normalizeString(t, FamilyName)
+	normalizeString(t, MiddleName)
+	normalizeString(t, Nickname)
+	normalizeString(t, PreferredUsername)
+	normalizeString(t, Gender)
+
+	normalizeBool(t, EmailVerified)
+	normalizeBool(t, PhoneNumberVerified)
+
+	normalizeURL(t, Picture)
+	normalizeURL(t, Profile)
+	normalizeURL(t, Website)
+
+	normalizeBirthdate(t)
+
+	normalizeZoneinfo(t)
+
+	normalizeLocale(t)
+
+	normalizeAddress(t)
 
 	return nil
 }
--- pkg/lib/authn/stdattrs/normalizer_test.go
+++ pkg/lib/authn/stdattrs/normalizer_test.go
@@ -42,46 +42,156 @@ func TestNormalizer(t *testing.T) {
 			"email": "user@example.com",
 		})
 
-		// Omit invalid locale
+		// Normalize phone_number
 		test(T{
-			"locale": "NONSENSE",
-		}, T{})
-		test(T{
-			"locale": "en",
+			"phone_number": "+85298765432",
 		}, T{
-			"locale": "en",
+			"phone_number": "+85298765432",
 		})
 
-		// Omit empty names
+		// Normalize strings
 		test(T{
-			"name":        "",
-			"family_name": "",
-			"given_name":  "",
-			"nickname":    "",
+			"name":               "",
+			"given_name":         "",
+			"family_name":        "",
+			"middle_name":        "",
+			"nickname":           "",
+			"preferred_username": "",
+			"gender":             "",
 		}, T{})
 		test(T{
-			"name":        "John Doe",
-			"family_name": "Doe",
-			"given_name":  "John",
-			"nickname":    "John",
+			"name":               0,
+			"given_name":         0,
+			"family_name":        0,
+			"middle_name":        0,
+			"nickname":           0,
+			"preferred_username": 0,
+			"gender":             0,
+		}, T{})
+		test(T{
+			"name":               "John Doe",
+			"given_name":         "John",
+			"family_name":        "Doe",
+			"middle_name":        "Wick",
+			"nickname":           "John",
+			"preferred_username": "johndoe",
+			"gender":             "other",
 		}, T{
-			"name":        "John Doe",
-			"family_name": "Doe",
-			"given_name":  "John",
-			"nickname":    "John",
+			"name":               "John Doe",
+			"given_name":         "John",
+			"family_name":        "Doe",
+			"middle_name":        "Wick",
+			"nickname":           "John",
+			"preferred_username": "johndoe",
+			"gender":             "other",
 		})
 
-		// Omit invalid URLs
+		// Normalize bools
+		test(T{
+			"email_verified":        true,
+			"phone_number_verified": true,
+		}, T{
+			"email_verified":        true,
+			"phone_number_verified": true,
+		})
+		test(T{
+			"email_verified":        "",
+			"phone_number_verified": "",
+		}, T{})
+
+		// Normalize URLs
 		test(T{
 			"picture": "NONSENSE",
 			"profile": "NONSENSE",
+			"website": "NONSENSE",
 		}, T{})
 		test(T{
 			"picture": "http://example.com",
 			"profile": "http://example.com",
+			"website": "http://example.com",
 		}, T{
 			"picture": "http://example.com",
 			"profile": "http://example.com",
+			"website": "http://example.com",
+		})
+
+		// Normalize birthdate
+		test(T{
+			"birthdate": "1990-01-01",
+		}, T{
+			"birthdate": "1990-01-01",
+		})
+		test(T{
+			"birthdate": "0000-01-01",
+		}, T{
+			"birthdate": "0000-01-01",
+		})
+		test(T{
+			"birthdate": "--01-01",
+		}, T{
+			"birthdate": "--01-01",
+		})
+		test(T{
+			"birthdate": "1990",
+		}, T{
+			"birthdate": "1990",
+		})
+		test(T{
+			"birthdate": "1990-01-01T00:00:00Z",
+		}, T{})
+
+		// Normalize locale
+		test(T{
+			"locale": "en",
+		}, T{
+			"locale": "en",
+		})
+		test(T{
+			"locale": 1,
+		}, T{})
+		test(T{
+			"locale": "NONSENSE",
+		}, T{})
+
+		// Normalize address
+		test(T{
+			"address": 1,
+		}, T{})
+		test(T{
+			"address": map[string]interface{}{},
+		}, T{})
+		test(T{
+			"address": map[string]interface{}{
+				"formatted": "",
+			},
+		}, T{})
+		test(T{
+			"address": map[string]interface{}{
+				"formatted": "1 Unnamed Road, Central, Hong Kong Island, HK",
+			},
+		}, T{
+			"address": map[string]interface{}{
+				"formatted": "1 Unnamed Road, Central, Hong Kong Island, HK",
+			},
+		})
+		test(T{
+			"address": map[string]interface{}{
+				"formatted":      "1 Unnamed Road, Central, Hong Kong Island, HK",
+				"street_address": "1 Unnamed Road",
+				"locality":       "Central",
+				"region":         "Hong Kong Island",
+				"postal_code":    "N/A",
+				"country":        "HK",
+			},
+		}, T{
+			"address": map[string]interface{}{
+				"formatted":      "1 Unnamed Road, Central, Hong Kong Island, HK",
+				"street_address": "1 Unnamed Road",
+				"locality":       "Central",
+				"region":         "Hong Kong Island",
+				"postal_code":    "N/A",
+				"country":        "HK",
+			},
 		})
 	})
 }
