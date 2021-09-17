0e76651a194e29b958739a3a519a717637a33a54
Carmen <carmen@oursky.com>
2021-09-17 18:42:23 +1000

    Add GetDateListByRangeInclusive to generate date list for chart

---
+++ pkg/lib/analytic/time_test.go
@@ -0,0 +1,64 @@
+package analytic_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/authgear/authgear-server/pkg/lib/analytic"
+	"github.com/authgear/authgear-server/pkg/util/periodical"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestGetDateListByRangeInclusive(t *testing.T) {
+	Convey("GetDateListByRangeInclusive", t, func() {
+		list := analytic.GetDateListByRangeInclusive(
+			time.Date(2006, 1, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(2006, 1, 1, 0, 0, 0, 0, time.UTC),
+			periodical.Monthly,
+		)
+		So(len(list), ShouldEqual, 1)
+
+		list = analytic.GetDateListByRangeInclusive(
+			time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC),
+			time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC),
+			periodical.Monthly,
+		)
+		So(len(list), ShouldEqual, 0)
+
+		list = analytic.GetDateListByRangeInclusive(
+			time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC),
+			time.Date(2007, 1, 2, 0, 0, 0, 0, time.UTC),
+			periodical.Monthly,
+		)
+		So(len(list), ShouldEqual, 12)
+		So(list[0], ShouldResemble, time.Date(2006, 2, 1, 0, 0, 0, 0, time.UTC))
+		So(list[len(list)-1], ShouldResemble, time.Date(2007, 1, 1, 0, 0, 0, 0, time.UTC))
+
+		list = analytic.GetDateListByRangeInclusive(
+			time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC),
+			time.Date(2007, 1, 2, 0, 0, 0, 0, time.UTC),
+			periodical.Weekly,
+		)
+		So(len(list), ShouldEqual, 53)
+		So(list[0], ShouldResemble, time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC))
+		So(list[len(list)-1], ShouldResemble, time.Date(2007, 1, 1, 0, 0, 0, 0, time.UTC))
+
+		list = analytic.GetDateListByRangeInclusive(
+			time.Date(2006, 1, 6, 0, 0, 0, 0, time.UTC),
+			time.Date(2007, 1, 6, 0, 0, 0, 0, time.UTC),
+			periodical.Weekly,
+		)
+		So(len(list), ShouldEqual, 52)
+		So(list[0], ShouldResemble, time.Date(2006, 1, 9, 0, 0, 0, 0, time.UTC))
+		So(list[len(list)-1], ShouldResemble, time.Date(2007, 1, 1, 0, 0, 0, 0, time.UTC))
+
+		list = analytic.GetDateListByRangeInclusive(
+			time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC),
+			time.Date(2007, 1, 2, 0, 0, 0, 0, time.UTC),
+			periodical.Daily,
+		)
+		So(len(list), ShouldEqual, 366)
+		So(list[0], ShouldResemble, time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC))
+		So(list[len(list)-1], ShouldResemble, time.Date(2007, 1, 2, 0, 0, 0, 0, time.UTC))
+	})
+}
