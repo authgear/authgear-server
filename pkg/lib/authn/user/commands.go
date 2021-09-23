babadc5ffc8044c37392571a84bf3344f000a82d
Louis Chan <louischan@oursky.com>
2021-09-23 19:25:56 +0800

    Populate non identity-aware std attrs on signup

--- pkg/lib/authn/user/commands.go
+++ pkg/lib/authn/user/commands.go
@@ -7,6 +7,8 @@ import (
 	"github.com/authgear/authgear-server/pkg/api/model"
 	"github.com/authgear/authgear-server/pkg/lib/authn/authenticator"
 	"github.com/authgear/authgear-server/pkg/lib/authn/identity"
+	"github.com/authgear/authgear-server/pkg/lib/authn/stdattrs"
+	"github.com/authgear/authgear-server/pkg/lib/config"
 )
 
 type EventService interface {
@@ -15,8 +17,28 @@ type EventService interface {
 
 type Commands struct {
 	*RawCommands
-	Events       EventService
-	Verification VerificationService
+	RawQueries        *RawQueries
+	Events            EventService
+	Verification      VerificationService
+	UserProfileConfig *config.UserProfileConfig
+}
+
+func (c *Commands) PopulateStandardAttributes(userID string, iden *identity.Info) error {
+	user, err := c.RawQueries.GetRaw(userID)
+	if err != nil {
+		return err
+	}
+
+	stdAttrsFromIden := stdattrs.T(iden.Claims).NonIdentityAware()
+	originalStdAttrs := stdattrs.T(user.StandardAttributes)
+	stdAttrs := originalStdAttrs.MergedWith(stdAttrsFromIden)
+
+	err = c.RawCommands.UpdateStandardAttributes(userID, stdAttrs.ToClaims())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *Commands) AfterCreate(
