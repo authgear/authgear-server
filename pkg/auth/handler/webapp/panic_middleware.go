2e5e889fa6b8c58d88e4eb9cc3b495b31fc7d115
Louis Chan <louischan@oursky.com>
2021-09-29 15:12:09 +0800

    Handle non-error panic in web app panic middleware
    Previously, if the panic value is non-error, nothing will be shown.

--- pkg/auth/handler/webapp/panic_middleware.go
+++ pkg/auth/handler/webapp/panic_middleware.go
@@ -1,6 +1,7 @@
 package webapp
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
@@ -20,12 +21,19 @@ type PanicMiddleware struct {
 func (m *PanicMiddleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
-			if err := recover(); err != nil {
+			if e := recover(); e != nil {
 				data := make(map[string]interface{})
 				baseViewModel := m.BaseViewModel.ViewModel(r, w)
-				if err, ok := err.(error); ok {
-					baseViewModel.SetError(err)
+
+				// Make error
+				var err error
+				if ee, isErr := e.(error); isErr {
+					err = ee
+				} else {
+					err = fmt.Errorf("%+v", e)
 				}
+
+				baseViewModel.SetError(err)
 				viewmodels.Embed(data, baseViewModel)
 				m.Renderer.RenderHTML(w, r, TemplateWebFatalErrorHTML, data)
 
