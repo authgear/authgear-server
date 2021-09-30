0a062cdff2c705704cc4baf4ae49e6c75688b4fa
Louis Chan <louischan@oursky.com>
2021-09-30 18:25:49 +0800

    Enable `unsafe-eval` to work around regeneratorRuntime issue

--- pkg/portal/routes.go
+++ pkg/portal/routes.go
@@ -22,7 +22,12 @@ func NewRouter(p *deps.RootProvider) *httproute.Router {
 	// TODO(csp): improve security
 	secMiddleware := &web.SecHeadersMiddleware{
 		CSPDirectives: []string{
-			"script-src 'self' 'unsafe-inline' cdn.jsdelivr.net",
+			// FIXME(regeneratorRuntime)
+			// parcel-2.0.0-rc.0 requires us to use ES6 module when the browser supports it.
+			// ES6 module assumes strict mode.
+			// regeneratorRuntime is not compatible with strict mode because
+			// it uses Function to generate function, which is considered as eval.
+			"script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.jsdelivr.net",
 			"worker-src 'self' 'unsafe-inline' cdn.jsdelivr.net",
 			"object-src 'none'",
 			"base-uri 'none'",
