6fc2a059524c67cb6cb3818d77dd2d22eac4829c
Louis Chan <louischan@oursky.com>
2021-10-05 12:48:20 +0800

    Show simple string standard attributes

--- portal/src/graphql/adminapi/UserDetailsStandardAttributes.tsx
+++ portal/src/graphql/adminapi/UserDetailsStandardAttributes.tsx
@@ -1,3 +1,64 @@
-export default function UserDetailsStandardAttributes() {
-  return null;
+import React, { useContext } from "react";
+import { TextField } from "@fluentui/react";
+import { Context } from "@oursky/react-messageformat";
+import { StandardAttributes } from "../../types";
+
+import styles from "./UserDetailsStandardAttributes.module.scss";
+
+export interface UserDetailsStandardAttributesProps {
+  standardAttributes: StandardAttributes;
 }
+
+const UserDetailsStandardAttributes: React.FC<UserDetailsStandardAttributesProps> =
+  function UserDetailsStandardAttributes(
+    props: UserDetailsStandardAttributesProps
+  ) {
+    const { standardAttributes } = props;
+    const { renderToString } = useContext(Context);
+    return (
+      <div className={styles.root}>
+        <TextField
+          className={styles.control}
+          label={renderToString("standard-attribute.name")}
+          value={standardAttributes.name ?? ""}
+        />
+        <TextField
+          className={styles.control}
+          label={renderToString("standard-attribute.given_name")}
+          value={standardAttributes.given_name ?? ""}
+        />
+        <TextField
+          className={styles.control}
+          label={renderToString("standard-attribute.family_name")}
+          value={standardAttributes.family_name ?? ""}
+        />
+        <TextField
+          className={styles.control}
+          label={renderToString("standard-attribute.middle_name")}
+          value={standardAttributes.middle_name ?? ""}
+        />
+        <TextField
+          className={styles.control}
+          label={renderToString("standard-attribute.nickname")}
+          value={standardAttributes.nickname ?? ""}
+        />
+        <TextField
+          className={styles.control}
+          label={renderToString("standard-attribute.picture")}
+          value={standardAttributes.picture ?? ""}
+        />
+        <TextField
+          className={styles.control}
+          label={renderToString("standard-attribute.profile")}
+          value={standardAttributes.profile ?? ""}
+        />
+        <TextField
+          className={styles.control}
+          label={renderToString("standard-attribute.website")}
+          value={standardAttributes.website ?? ""}
+        />
+      </div>
+    );
+  };
+
+export default UserDetailsStandardAttributes;
