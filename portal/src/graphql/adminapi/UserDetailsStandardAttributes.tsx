abe5a4be53d7f5cc27b16ea854fd24824e7763a4
Louis Chan <louischan@oursky.com>
2021-10-06 16:32:38 +0800

    Support editing text standard attributes

--- portal/src/graphql/adminapi/UserDetailsStandardAttributes.tsx
+++ portal/src/graphql/adminapi/UserDetailsStandardAttributes.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext, useMemo, useCallback } from "react";
 import cn from "classnames";
 import {
   TextField,
@@ -17,7 +17,7 @@ import {
 import { Context, FormattedMessage } from "@oursky/react-messageformat";
 import { useSystemConfig } from "../../context/SystemConfigContext";
 import { parseBirthdate, toBirthdate } from "../../util/birthdate";
-import { StandardAttributes } from "../../types";
+import { StandardAttributes, StandardAttributesAddress } from "../../types";
 import { makeTimezoneOptions } from "../../util/timezone";
 import { makeAlpha2Options } from "../../util/alpha2";
 import { formatDatetime } from "../../util/formatDatetime";
@@ -26,6 +26,7 @@ import styles from "./UserDetailsStandardAttributes.module.scss";
 
 export interface UserDetailsStandardAttributesProps {
   standardAttributes: StandardAttributes;
+  onChangeStandardAttributes?: (attrs: StandardAttributes) => void;
 }
 
 function formatDate(date?: Date): string {
@@ -49,9 +50,104 @@ const UserDetailsStandardAttributes: React.FC<UserDetailsStandardAttributesProps
   function UserDetailsStandardAttributes(
     props: UserDetailsStandardAttributesProps
   ) {
-    const { standardAttributes } = props;
+    const { standardAttributes, onChangeStandardAttributes } = props;
     const { availableLanguages } = useSystemConfig();
     const { renderToString, locale: appLocale } = useContext(Context);
+
+    const makeOnChangeText = useCallback(
+      (fieldName: keyof StandardAttributes) => {
+        return (_e: React.FormEvent<unknown>, newValue?: string) => {
+          if (newValue == null || onChangeStandardAttributes == null) {
+            return;
+          }
+
+          onChangeStandardAttributes({
+            ...standardAttributes,
+            [fieldName]: newValue,
+          });
+        };
+      },
+      [standardAttributes, onChangeStandardAttributes]
+    );
+
+    const makeOnChangeAddressText = useCallback(
+      (fieldName: keyof StandardAttributesAddress) => {
+        return (_e: React.FormEvent<unknown>, newValue?: string) => {
+          if (newValue == null || onChangeStandardAttributes == null) {
+            return;
+          }
+
+          onChangeStandardAttributes({
+            ...standardAttributes,
+            address: {
+              ...standardAttributes.address,
+              [fieldName]: newValue,
+            },
+          });
+        };
+      },
+      [standardAttributes, onChangeStandardAttributes]
+    );
+
+    const onChangeName = useMemo(
+      () => makeOnChangeText("name"),
+      [makeOnChangeText]
+    );
+
+    const onChangeGiveName = useMemo(
+      () => makeOnChangeText("given_name"),
+      [makeOnChangeText]
+    );
+
+    const onChangeFamilyName = useMemo(
+      () => makeOnChangeText("family_name"),
+      [makeOnChangeText]
+    );
+
+    const onChangeMiddleName = useMemo(
+      () => makeOnChangeText("middle_name"),
+      [makeOnChangeText]
+    );
+
+    const onChangeNickname = useMemo(
+      () => makeOnChangeText("nickname"),
+      [makeOnChangeText]
+    );
+
+    const onChangePicture = useMemo(
+      () => makeOnChangeText("picture"),
+      [makeOnChangeText]
+    );
+
+    const onChangeProfile = useMemo(
+      () => makeOnChangeText("profile"),
+      [makeOnChangeText]
+    );
+
+    const onChangeWebsite = useMemo(
+      () => makeOnChangeText("website"),
+      [makeOnChangeText]
+    );
+
+    const onChangeStreetAddress = useMemo(
+      () => makeOnChangeAddressText("street_address"),
+      [makeOnChangeAddressText]
+    );
+
+    const onChangeLocality = useMemo(
+      () => makeOnChangeAddressText("locality"),
+      [makeOnChangeAddressText]
+    );
+
+    const onChangePostalCode = useMemo(
+      () => makeOnChangeAddressText("postal_code"),
+      [makeOnChangeAddressText]
+    );
+
+    const onChangeRegion = useMemo(
+      () => makeOnChangeAddressText("region"),
+      [makeOnChangeAddressText]
+    );
 
     const email = standardAttributes.email;
     const emailOptions: IDropdownOption[] = useMemo(() => {
@@ -152,41 +248,49 @@ const UserDetailsStandardAttributes: React.FC<UserDetailsStandardAttributesProps
           className={styles.control}
           label={renderToString("standard-attribute.name")}
           value={standardAttributes.name ?? ""}
+          onChange={onChangeName}
         />
         <TextField
           className={styles.control}
           label={renderToString("standard-attribute.given_name")}
           value={standardAttributes.given_name ?? ""}
+          onChange={onChangeGiveName}
         />
         <TextField
           className={styles.control}
           label={renderToString("standard-attribute.family_name")}
           value={standardAttributes.family_name ?? ""}
+          onChange={onChangeFamilyName}
         />
         <TextField
           className={styles.control}
           label={renderToString("standard-attribute.middle_name")}
           value={standardAttributes.middle_name ?? ""}
+          onChange={onChangeMiddleName}
         />
         <TextField
           className={styles.control}
           label={renderToString("standard-attribute.nickname")}
           value={standardAttributes.nickname ?? ""}
+          onChange={onChangeNickname}
         />
         <TextField
           className={styles.control}
           label={renderToString("standard-attribute.picture")}
           value={standardAttributes.picture ?? ""}
+          onChange={onChangePicture}
         />
         <TextField
           className={styles.control}
           label={renderToString("standard-attribute.profile")}
           value={standardAttributes.profile ?? ""}
+          onChange={onChangeProfile}
         />
         <TextField
           className={styles.control}
           label={renderToString("standard-attribute.website")}
           value={standardAttributes.website ?? ""}
+          onChange={onChangeWebsite}
         />
         <Dropdown
           className={styles.control}
@@ -243,22 +347,26 @@ const UserDetailsStandardAttributes: React.FC<UserDetailsStandardAttributesProps
             label={renderToString("standard-attribute.street_address")}
             multiline={true}
             value={standardAttributes.address?.street_address}
+            onChange={onChangeStreetAddress}
           />
           <TextField
             className={styles.addressInput}
             label={renderToString("standard-attribute.locality")}
             value={standardAttributes.address?.locality}
+            onChange={onChangeLocality}
           />
           <div className={styles.addressInputGroup}>
             <TextField
               className={cn(styles.addressInput, styles.postalCode)}
               label={renderToString("standard-attribute.postal_code")}
               value={standardAttributes.address?.postal_code}
+              onChange={onChangePostalCode}
             />
             <TextField
               className={cn(styles.addressInput, styles.region)}
               label={renderToString("standard-attribute.region")}
               value={standardAttributes.address?.region}
+              onChange={onChangeRegion}
             />
             <Dropdown
               className={cn(styles.addressInput, styles.country)}
