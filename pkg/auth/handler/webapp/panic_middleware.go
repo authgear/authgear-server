20c1e14c5249fa20027ac7362773944e266c22bb
Louis Chan <louischan@oursky.com>
2021-10-11 16:57:35 +0800

    Avoid superfluous write when panic
    We wrap http.Handler inside withTx.
    It could happen that the http.Handler finish its job without any error,
    but the transaction cannot be committed.
    In that case, a panic will occur.
    The panic middleware logs the panic AND attempts to write a response again.
    
    Some http.Handler write Content-Length explicitly.
    When the panic middleware writes a response again,
    ErrContentLength will occur.
    
    Wrapping http.ResponseWriter is non-trivial because it is not the sole
    interface. httpsnoop is a module specific for this non-trivial task.

--- pkg/auth/handler/webapp/panic_middleware.go
+++ pkg/auth/handler/webapp/panic_middleware.go
@@ -3,6 +3,8 @@ package webapp
 import (
 	"net/http"
 
+	"github.com/felixge/httpsnoop"
+
 	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
 	"github.com/authgear/authgear-server/pkg/util/log"
 	"github.com/authgear/authgear-server/pkg/util/panicutil"
@@ -28,18 +30,35 @@ type PanicMiddleware struct {
 
 func (m *PanicMiddleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		written := false
+
+		w = httpsnoop.Wrap(w, httpsnoop.Hooks{
+			WriteHeader: func(f httpsnoop.WriteHeaderFunc) httpsnoop.WriteHeaderFunc {
+				return func(code int) {
+					written = true
+					f(code)
+				}
+			},
+			Write: func(f httpsnoop.WriteFunc) httpsnoop.WriteFunc {
+				return func(b []byte) (int, error) {
+					written = true
+					return f(b)
+				}
+			},
+		})
+
 		defer func() {
 			if e := recover(); e != nil {
-				data := make(map[string]interface{})
-				baseViewModel := m.BaseViewModel.ViewModel(r, w)
-
 				err := panicutil.MakeError(e)
-
 				log.PanicValue(m.Logger.Logger, err)
 
-				baseViewModel.SetError(err)
-				viewmodels.Embed(data, baseViewModel)
-				m.Renderer.RenderHTML(w, r, TemplateWebFatalErrorHTML, data)
+				if !written {
+					data := make(map[string]interface{})
+					baseViewModel := m.BaseViewModel.ViewModel(r, w)
+					baseViewModel.SetError(err)
+					viewmodels.Embed(data, baseViewModel)
+					m.Renderer.RenderHTML(w, r, TemplateWebFatalErrorHTML, data)
+				}
 			}
 		}()
 		next.ServeHTTP(w, r)
