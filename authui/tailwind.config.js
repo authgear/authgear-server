7d184eb3c91d8d294407c02dc71e2f5fafc2f3dd
Louis Chan <louischan@oursky.com>
2021-10-15 19:01:42 +0800

    Show standard attributes in /settings/profile

--- authui/tailwind.config.js
+++ authui/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["../resources/authgear/templates/en/web/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        18: "4.5rem",
+      },
+    },
     screens: {
       tablet: "640px",
       desktop: "1024px",
