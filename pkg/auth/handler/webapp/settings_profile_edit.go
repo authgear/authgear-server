f70c63759479934be8a4f6ac63b1b3bd512703fb
Louis Chan <louischan@oursky.com>
2021-10-21 15:14:07 +0800

    Handle edit profile form POST

--- pkg/auth/handler/webapp/settings_profile_edit.go
+++ pkg/auth/handler/webapp/settings_profile_edit.go
@@ -3,7 +3,11 @@ package webapp
 import (
 	"net/http"
 
+	"github.com/authgear/authgear-server/pkg/api/apierrors"
 	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
+	"github.com/authgear/authgear-server/pkg/auth/webapp"
+	"github.com/authgear/authgear-server/pkg/lib/authn/stdattrs"
+	"github.com/authgear/authgear-server/pkg/lib/authn/user"
 	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/util/httproute"
 	"github.com/authgear/authgear-server/pkg/util/template"
@@ -20,11 +24,18 @@ func ConfigureSettingsProfileEditRoute(route httproute.Route) httproute.Route {
 		WithPathPattern("/settings/profile/:variant/edit")
 }
 
+type SettingsProfileEditUserService interface {
+	GetRaw(id string) (*user.User, error)
+	UpdateStandardAttributes(userID string, stdAttrs map[string]interface{}) error
+}
+
 type SettingsProfileEditHandler struct {
 	ControllerFactory        ControllerFactory
 	BaseViewModel            *viewmodels.BaseViewModeler
 	SettingsProfileViewModel *viewmodels.SettingsProfileViewModeler
 	Renderer                 Renderer
+	Users                    SettingsProfileEditUserService
+	ErrorCookie              *webapp.ErrorCookie
 }
 
 func (h *SettingsProfileEditHandler) GetData(r *http.Request, rw http.ResponseWriter) (map[string]interface{}, error) {
@@ -62,6 +73,43 @@ func (h *SettingsProfileEditHandler) ServeHTTP(w http.ResponseWriter, r *http.Re
 		}
 
 		h.Renderer.RenderHTML(w, r, TemplateWebSettingsProfileEditHTML, data)
+		return nil
+	})
+
+	ctrl.PostAction("save", func() error {
+		userID := *session.GetUserID(r.Context())
+		m := JSONPointerFormToMap(r.Form)
+
+		writeErr := func(inputErr error) error {
+			result := webapp.Result{
+				RedirectURI: r.URL.Path,
+			}
+			cookie, err := h.ErrorCookie.SetError(r, apierrors.AsAPIError(inputErr))
+			if err != nil {
+				return err
+			}
+			result.Cookies = append(result.Cookies, cookie)
+			result.WriteResponse(w, r)
+			return nil
+		}
+
+		u, err := h.Users.GetRaw(userID)
+		if err != nil {
+			return writeErr(err)
+		}
+
+		attrs, err := stdattrs.T(u.StandardAttributes).MergedWithJSONPointer(m)
+		if err != nil {
+			return writeErr(err)
+		}
+
+		err = h.Users.UpdateStandardAttributes(userID, attrs)
+		if err != nil {
+			return writeErr(err)
+		}
+
+		result := webapp.Result{RedirectURI: "/settings/profile"}
+		result.WriteResponse(w, r)
 		return nil
 	})
 }
