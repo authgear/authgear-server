27b7dedfe3521c02491ade0611c539c8b3c6eae6
Louis Chan <louischan@oursky.com>
2021-11-03 18:07:46 +0800

    Introduce __wide_page_frame.html

--- pkg/auth/handler/webapp/template.go
+++ pkg/auth/handler/webapp/template.go
@@ -12,6 +12,7 @@ var TemplateWebMessageBarHTML = template.RegisterHTML("web/__message_bar.html")
 var TemplateWebAlternativeStepsHTML = template.RegisterHTML("web/__alternatives.html")
 var TemplateWebPasswordPolicyHTML = template.RegisterHTML("web/__password_policy.html")
 var TemplateWebPageFrameHTML = template.RegisterHTML("web/__page_frame.html")
+var TemplateWebWidePageFrameHTML = template.RegisterHTML("web/__wide_page_frame.html")
 var TemplateWebModalHTML = template.RegisterHTML("web/__modal.html")
 var TemplateWebWatermarkHTML = template.RegisterHTML("web/__watermark.html")
 var TemplateWebRecoveryCodeHTML = template.RegisterHTML("web/__recovery_code.html")
@@ -27,6 +28,7 @@ var components = []*template.HTML{
 	TemplateWebAlternativeStepsHTML,
 	TemplateWebPasswordPolicyHTML,
 	TemplateWebPageFrameHTML,
+	TemplateWebWidePageFrameHTML,
 	TemplateWebModalHTML,
 	TemplateWebWatermarkHTML,
 	TemplateWebRecoveryCodeHTML,
