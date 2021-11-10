05de43a624041422e381e720448b2a6cf4c45aa3
Louis Chan <louischan@oursky.com>
2021-11-10 19:04:23 +0800

    Build tailwind bundle

---
+++ authui/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["../resources/authgear/templates/en/web/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
