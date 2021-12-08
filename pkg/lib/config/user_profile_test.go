2df544569f659a615d51ac595e00e70020844a4a
Louis Chan <louischan@oursky.com>
2021-12-08 12:41:06 +0800

    Move CustomAttributeConfigToSchema to ToJSONSchema

--- pkg/lib/config/user_profile_test.go
+++ pkg/lib/config/user_profile_test.go
@@ -23,3 +23,107 @@ func TestStandardAttributesConfig(t *testing.T) {
 		So(accessControl.GetLevel("/family_name", RolePortalUI, 0), ShouldEqual, AccessControlLevelReadwrite)
 	})
 }
+
+func TestCustomAttributesAttributeConfig(t *testing.T) {
+	newFloat := func(f float64) *float64 {
+		return &f
+	}
+
+	newInt64 := func(i int64) int64 {
+		return i
+	}
+
+	Convey("CustomAttributesAttributeConfig", t, func() {
+		test := func(c *CustomAttributesAttributeConfig, schema map[string]interface{}) {
+			actual, err := c.ToJSONSchema()
+			So(err, ShouldBeNil)
+			So(actual, ShouldResemble, schema)
+		}
+
+		test(&CustomAttributesAttributeConfig{
+			Type: CustomAttributeTypeString,
+		}, map[string]interface{}{
+			"type": "string",
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type: CustomAttributeTypeNumber,
+		}, map[string]interface{}{
+			"type": "number",
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type:    CustomAttributeTypeNumber,
+			Minimum: newFloat(0),
+		}, map[string]interface{}{
+			"type":    "number",
+			"minimum": 0.0,
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type:    CustomAttributeTypeNumber,
+			Maximum: newFloat(1),
+		}, map[string]interface{}{
+			"type":    "number",
+			"maximum": 1.0,
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type: CustomAttributeTypeInteger,
+		}, map[string]interface{}{
+			"type": "integer",
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type:    CustomAttributeTypeInteger,
+			Minimum: newFloat(0),
+		}, map[string]interface{}{
+			"type":    "integer",
+			"minimum": newInt64(0),
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type:    CustomAttributeTypeInteger,
+			Maximum: newFloat(1),
+		}, map[string]interface{}{
+			"type":    "integer",
+			"maximum": newInt64(1),
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type: CustomAttributeTypeEnum,
+			Enum: []string{"a", "b"},
+		}, map[string]interface{}{
+			"type": "string",
+			"enum": []string{"a", "b"},
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type: CustomAttributeTypePhoneNumber,
+		}, map[string]interface{}{
+			"type":   "string",
+			"format": "phone",
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type: CustomAttributeTypeEmail,
+		}, map[string]interface{}{
+			"type":   "string",
+			"format": "email",
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type: CustomAttributeTypeURL,
+		}, map[string]interface{}{
+			"type":   "string",
+			"format": "uri",
+		})
+
+		test(&CustomAttributesAttributeConfig{
+			Type: CustomAttributeTypeAlpha2,
+		}, map[string]interface{}{
+			"type":   "string",
+			"format": "iso3166-1-alpha-2",
+		})
+	})
+}
