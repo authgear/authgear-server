1af5ff6f987610e2c0f744d462f3caa40b0563d1
Louis Chan <louischan@oursky.com>
2021-12-08 18:51:43 +0800

    Add event-aware custom attributes service

---
+++ pkg/lib/feature/customattrs/service.go
@@ -0,0 +1,98 @@
+package customattrs
+
+import (
+	"github.com/authgear/authgear-server/pkg/api/event"
+	"github.com/authgear/authgear-server/pkg/api/event/blocking"
+	"github.com/authgear/authgear-server/pkg/api/event/nonblocking"
+	"github.com/authgear/authgear-server/pkg/api/model"
+	"github.com/authgear/authgear-server/pkg/lib/config"
+	"github.com/authgear/authgear-server/pkg/util/accesscontrol"
+)
+
+type EventService interface {
+	DispatchEvent(payload event.Payload) error
+}
+
+type Service struct {
+	Config         *config.UserProfileConfig
+	ServiceNoEvent *ServiceNoEvent
+	Events         EventService
+}
+
+func (s *Service) UpdateAllCustomAttributes(role accesscontrol.Role, userID string, reprForm map[string]interface{}) error {
+	err := s.ServiceNoEvent.UpdateAllCustomAttributes(role, userID, reprForm)
+	if err != nil {
+		return err
+	}
+
+	eventPayloads := []event.Payload{
+		&blocking.UserProfilePreUpdateBlockingEventPayload{
+			UserRef: model.UserRef{
+				Meta: model.Meta{
+					ID: userID,
+				},
+			},
+			AdminAPI: role == accesscontrol.RoleGreatest,
+		},
+		&nonblocking.UserProfileUpdatedEventPayload{
+			UserRef: model.UserRef{
+				Meta: model.Meta{
+					ID: userID,
+				},
+			},
+			AdminAPI: role == accesscontrol.RoleGreatest,
+		},
+	}
+
+	for _, eventPayload := range eventPayloads {
+		err = s.Events.DispatchEvent(eventPayload)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Service) UpdateCustomAttributes(role accesscontrol.Role, userID string, pointers []string, reprForm map[string]interface{}) error {
+	err := s.ServiceNoEvent.UpdateCustomAttributes(role, userID, pointers, reprForm)
+	if err != nil {
+		return err
+	}
+
+	eventPayloads := []event.Payload{
+		&blocking.UserProfilePreUpdateBlockingEventPayload{
+			UserRef: model.UserRef{
+				Meta: model.Meta{
+					ID: userID,
+				},
+			},
+			AdminAPI: role == accesscontrol.RoleGreatest,
+		},
+		&nonblocking.UserProfileUpdatedEventPayload{
+			UserRef: model.UserRef{
+				Meta: model.Meta{
+					ID: userID,
+				},
+			},
+			AdminAPI: role == accesscontrol.RoleGreatest,
+		},
+	}
+
+	for _, eventPayload := range eventPayloads {
+		err = s.Events.DispatchEvent(eventPayload)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Service) ReadCustomAttributesInStorageForm(
+	role accesscontrol.Role,
+	userID string,
+	storageForm map[string]interface{},
+) (map[string]interface{}, error) {
+	return s.ServiceNoEvent.ReadCustomAttributesInStorageForm(role, userID, storageForm)
+}
