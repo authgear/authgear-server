ab75233c37b87a84cffe656e9967eabc9e1986fb
Louis Chan <louischan@oursky.com>
2021-12-14 15:49:04 +0800

    Support custom attribute type "number" and "integer"

--- portal/src/util/input.ts
+++ portal/src/util/input.ts
@@ -12,3 +12,20 @@ export function parseIntegerAllowLeadingZeros(
   }
   return undefined;
 }
+
+export function checkNumberInput(value: string): boolean {
+  // Allow empty.
+  if (value === "") {
+    return true;
+  }
+  // Allow "-"
+  if (value === "-") {
+    return true;
+  }
+  return /^-?(0|[1-9][0-9]*)(\.[0-9]*)?$/.test(value);
+}
+
+export function checkIntegerInput(value: string): boolean {
+  // Allow "", "-", or valid integer.
+  return /^-?(|0|[1-9][0-9]*)$/.test(value);
+}
