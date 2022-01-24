dcc53db204ad47951828bf0716d0273816110b4f
Louis Chan <louischan@oursky.com>
2022-01-24 18:26:22 +0800

    Add ensureTime
    time.Time in error info is serialized into RFC3339 string.
    But messageformat expects time.Time only.

--- pkg/util/template/func.go
+++ pkg/util/template/func.go
@@ -11,6 +11,7 @@ func MakeTemplateFuncMap() map[string]interface{} {
 	var templateFuncMap = sprig.HermeticHtmlFuncMap()
 	templateFuncMap[messageformat.TemplateRuntimeFuncName] = messageformat.TemplateRuntimeFunc
 	templateFuncMap["rfc3339"] = RFC3339
+	templateFuncMap["ensureTime"] = EnsureTime
 	return templateFuncMap
 }
 
@@ -22,6 +23,29 @@ func RFC3339(date interface{}) interface{} {
 		return date.UTC().Format(time.RFC3339)
 	default:
 		return "INVALID_DATE"
+	}
+}
+
+func EnsureTime(anyValue interface{}) interface{} {
+	switch anyValue := anyValue.(type) {
+	case *time.Time:
+		return anyValue
+	case time.Time:
+		return anyValue
+	case string:
+		t, err := time.Parse(time.RFC3339, anyValue)
+		if err != nil {
+			panic(err)
+		}
+		return t
+	case *string:
+		t, err := time.Parse(time.RFC3339, *anyValue)
+		if err != nil {
+			panic(err)
+		}
+		return t
+	default:
+		return anyValue
 	}
 }
 
