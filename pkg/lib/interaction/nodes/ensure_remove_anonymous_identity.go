7c18a0ddde7fe4a577d9ad1f19368f90e58be4ac
Carmen <carmen@oursky.com>
2022-01-26 16:04:31 +0000

    Dispatch AnonymousPromotedEvent when admin add new identity to anonymous user

--- pkg/lib/interaction/nodes/ensure_remove_anonymous_identity.go
+++ pkg/lib/interaction/nodes/ensure_remove_anonymous_identity.go
@@ -1,6 +1,7 @@
 package nodes
 
 import (
+	"github.com/authgear/authgear-server/pkg/api/event/nonblocking"
 	"github.com/authgear/authgear-server/pkg/api/model"
 	"github.com/authgear/authgear-server/pkg/lib/authn/identity"
 	"github.com/authgear/authgear-server/pkg/lib/interaction"
@@ -12,12 +13,15 @@ func init() {
 
 type EdgeEnsureRemoveAnonymousIdentity struct{}
 
-func (e *EdgeEnsureRemoveAnonymousIdentity) Instantiate(ctx *interaction.Context, graph *interaction.Graph, rawInput interface{}) (interaction.Node, error) {
-	return &NodeEnsureRemoveAnonymousIdentity{}, nil
+func (e *EdgeEnsureRemoveAnonymousIdentity) Instantiate(ctx *interaction.Context, graph *interaction.Graph, input interface{}) (interaction.Node, error) {
+	return &NodeEnsureRemoveAnonymousIdentity{
+		IsAdminAPI: interaction.IsAdminAPI(input),
+	}, nil
 }
 
 type NodeEnsureRemoveAnonymousIdentity struct {
 	AnonymousIdentity *identity.Info `json:"anonymous_identity,omitempty"`
+	IsAdminAPI        bool           `json:"is_admin_api"`
 }
 
 func (n *NodeEnsureRemoveAnonymousIdentity) Prepare(ctx *interaction.Context, graph *interaction.Graph) error {
@@ -34,6 +38,12 @@ func (n *NodeEnsureRemoveAnonymousIdentity) Prepare(ctx *interaction.Context, gr
 		panic("interaction: more than 1 anonymous identities: " + userID)
 	}
 	if len(anonymousIdentities) == 1 {
+		if !n.IsAdminAPI {
+			// This node is used when adding new identity
+			// when admin add a new identifiable identity to the anonymous user
+			// anonymous will be removed
+			panic("interaction: unexpected anonymous user adding identity")
+		}
 		n.AnonymousIdentity = anonymousIdentities[0]
 	}
 	return nil
@@ -63,6 +73,35 @@ func (n *NodeEnsureRemoveAnonymousIdentity) GetEffects() ([]interaction.Effect, 
 			if err != nil {
 				return err
 			}
+			return nil
+		}),
+		interaction.EffectOnCommit(func(ctx *interaction.Context, graph *interaction.Graph, nodeIndex int) error {
+			if n.AnonymousIdentity == nil {
+				return nil
+			}
+			// if anonymous identity is removed during adding identity
+			// it is a promotion flow
+			userID := graph.MustGetUserID()
+			anonUserRef := model.UserRef{
+				Meta: model.Meta{
+					ID: userID,
+				},
+			}
+			var identityModels []model.Identity
+			for _, info := range graph.GetUserNewIdentities() {
+				identityModels = append(identityModels, info.ToModel())
+			}
+
+			err := ctx.Events.DispatchEvent(&nonblocking.UserAnonymousPromotedEventPayload{
+				AnonymousUserRef: anonUserRef,
+				UserRef:          anonUserRef,
+				Identities:       identityModels,
+				AdminAPI:         n.IsAdminAPI,
+			})
+			if err != nil {
+				return err
+			}
+
 			return nil
 		}),
 	}, nil
