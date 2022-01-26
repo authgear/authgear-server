682300f10e281414765f3699c3887dac5533a5fb
Louis Chan <louischan@oursky.com>
2022-01-26 12:18:09 +0800

    Support more datetime formatting options

--- authui/src/date.ts
+++ authui/src/date.ts
@@ -4,6 +4,55 @@ import {
 } from "./feature";
 import { DateTime } from "luxon";
 
+// In order to be backward compatible,
+// <span data-date> is equivalent to
+// <span data-date data-date-type="relative" data-date-date-style="medium" data-date-time-style="short">
+// To suppress time, specify data-date-time-style as an empty attribute.
+
+const DATE_TYPES = ["relative", "absolute"] as const;
+type DateType = typeof DATE_TYPES[number];
+
+const DATE_STYLES = ["full", "long", "medium", "short"] as const;
+type DateStyle = typeof DATE_STYLES[number];
+
+const TIME_STYLES = ["full", "long", "medium", "short"] as const;
+type TimeStyle = typeof TIME_STYLES[number];
+
+function parseDateType(s: string | null): DateType {
+  for (const variant of DATE_TYPES) {
+    if (variant === s) {
+      return variant;
+    }
+  }
+  return "relative";
+}
+
+function parseDateStyle(s: string | null): DateStyle | undefined {
+  if (s === "") {
+    return undefined;
+  }
+
+  for (const variant of DATE_STYLES) {
+    if (variant === s) {
+      return variant;
+    }
+  }
+  return "medium";
+}
+
+function parseTimeStyle(s: string | null): TimeStyle | undefined {
+  if (s === "") {
+    return undefined;
+  }
+
+  for (const variant of TIME_STYLES) {
+    if (variant === s) {
+      return variant;
+    }
+  }
+  return "short";
+}
+
 export function formatDateRelative() {
   const dateSpans = document.querySelectorAll("[data-date]");
   const lang = document.documentElement.lang;
@@ -18,13 +67,21 @@ export function formatDateRelative() {
   for (let i = 0; i < dateSpans.length; i++) {
     const dateSpan = dateSpans[i];
     const rfc3339 = dateSpan.getAttribute("data-date");
+    const dateType = parseDateType(dateSpan.getAttribute("data-date-type"));
+    const dateStyle = parseDateStyle(
+      dateSpan.getAttribute("data-date-date-style")
+    );
+    const timeStyle = parseTimeStyle(
+      dateSpan.getAttribute("data-date-time-style")
+    );
+
     if (typeof rfc3339 === "string") {
       const luxonDatetime = DateTime.fromISO(rfc3339);
       const abs = hasAbs
         ? luxonDatetime.toLocaleString(
             {
-              dateStyle: "medium",
-              timeStyle: "short",
+              dateStyle,
+              timeStyle,
             },
             {
               locale: lang,
@@ -51,12 +108,18 @@ export function formatDateRelative() {
         }
       }
 
-      // Prefer showing relative date time,
-      // and fallback to absolute date time.
-      if (rel != null) {
-        dateSpan.textContent = rel;
-      } else if (abs != null) {
-        dateSpan.textContent = abs;
+      if (dateType === "relative") {
+        // Prefer showing relative date time,
+        // and fallback to absolute date time.
+        if (rel != null) {
+          dateSpan.textContent = rel;
+        } else if (abs != null) {
+          dateSpan.textContent = abs;
+        }
+      } else {
+        if (abs != null) {
+          dateSpan.textContent = abs;
+        }
       }
     }
   }
