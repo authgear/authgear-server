551de6ff5440888752f745eccc82a61cc9da5f61
Carmen <carmen@oursky.com>
2022-01-27 21:39:59 +0500

    Add number of active user chart

--- portal/src/graphql/portal/AnalyticsActivityWidget.tsx
+++ portal/src/graphql/portal/AnalyticsActivityWidget.tsx
@@ -1,17 +1,96 @@
-import React from "react";
-import { FormattedMessage } from "@oursky/react-messageformat";
+import React, { useContext, useMemo } from "react";
+import { Context, FormattedMessage } from "@oursky/react-messageformat";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import {
+  AnalyticChartsQuery_activeUserChart,
+  AnalyticChartsQuery_totalUserCountChart,
+} from "./query/__generated__/AnalyticChartsQuery";
+import { Periodical } from "./__generated__/globalTypes";
+import { isoWeekLabel, monthLabel } from "../../util/date";
 import WidgetTitle from "../../WidgetTitle";
 import Widget from "../../Widget";
 import styles from "./AnalyticsActivityWidget.module.scss";
 
-const AnalyticsActivityWidget: React.FC = function AnalyticsActivityWidget() {
-  return (
-    <Widget className={styles.widget}>
-      <WidgetTitle>
-        <FormattedMessage id="AnalyticsActivityWidget.title" />
-      </WidgetTitle>
-    </Widget>
-  );
-};
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
+
+interface AnalyticsActivityWidgetActiveUserChartProps {
+  chartData: AnalyticChartsQuery_activeUserChart | null;
+  periodical: Periodical;
+}
+
+const AnalyticsActivityWidgetActiveUserChart: React.FC<AnalyticsActivityWidgetActiveUserChartProps> =
+  function AnalyticsActivityWidgetActiveUserChart(props) {
+    const { renderToString } = useContext(Context);
+    const { chartData, periodical } = props;
+    const options = {
+      responsive: true,
+      scales: {
+        y: {
+          title: {
+            display: true,
+            text: renderToString("AnalyticsActivityWidget.active-user.label"),
+          },
+          min: 0,
+        },
+        x: {
+          ticks: {
+            maxTicksLimit: 12,
+          },
+        },
+      },
+    };
+    const data = useMemo(() => {
+      let labalFn = (iosDate: string) => iosDate;
+      switch (periodical) {
+        case Periodical.MONTHLY:
+          labalFn = monthLabel;
+          break;
+        case Periodical.WEEKLY:
+          labalFn = isoWeekLabel;
+          break;
+      }
+
+      return {
+        labels: chartData?.dataset.map((pt) => (pt ? labalFn(pt.label) : "")),
+        datasets: [
+          {
+            label: renderToString("AnalyticsActivityWidget.active-user.label"),
+            data: chartData?.dataset.map((pt) => pt?.data),
+            backgroundColor: "#176DF3",
+          },
+        ],
+      };
+    }, [chartData, periodical, renderToString]);
+    return chartData ? <Bar options={options} data={data} /> : <></>;
+  };
+interface AnalyticsActivityWidgetProps {
+  periodical: Periodical;
+  activeUserChartData: AnalyticChartsQuery_activeUserChart | null;
+  totalUserCountChartData: AnalyticChartsQuery_totalUserCountChart | null;
+}
+
+const AnalyticsActivityWidget: React.FC<AnalyticsActivityWidgetProps> =
+  function AnalyticsActivityWidget(props) {
+    return (
+      <Widget className={styles.widget}>
+        <WidgetTitle>
+          <FormattedMessage id="AnalyticsActivityWidget.title" />
+        </WidgetTitle>
+        <>
+          <AnalyticsActivityWidgetActiveUserChart
+            chartData={props.activeUserChartData}
+            periodical={props.periodical}
+          />
+        </>
+      </Widget>
+    );
+  };
 
 export default AnalyticsActivityWidget;
--- portal/src/graphql/portal/AnalyticsScreen.tsx
+++ portal/src/graphql/portal/AnalyticsScreen.tsx
@@ -1,17 +1,46 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { FormattedMessage } from "@oursky/react-messageformat";
+import { useAnalyticChartsQuery } from "./query/analyticChartsQuery";
+import { Periodical } from "./__generated__/globalTypes";
 import ScreenContent from "../../ScreenContent";
 import ScreenTitle from "../../ScreenTitle";
 import AnalyticsActivityWidget from "./AnalyticsActivityWidget";
+import ShowLoading from "../../ShowLoading";
+import ShowError from "../../ShowError";
 import styles from "./AnalyticsScreen.module.scss";
 
 const AnalyticsScreen: React.FC = function AnalyticsScreen() {
+  // FIXME: support user input
+  const periodical = Periodical.MONTHLY;
+  const to = new Date();
+  const from = new Date();
+  from.setFullYear(from.getFullYear() - 1);
+  const rangeTo = to.toISOString().split("T")[0];
+  const rangeFrom = from.toISOString().split("T")[0];
+
+  const { appID } = useParams();
+  const { loading, error, refetch, activeUserChart, totalUserCountChart } =
+    useAnalyticChartsQuery(appID, periodical, rangeFrom, rangeTo);
+
+  if (loading) {
+    return <ShowLoading />;
+  }
+
+  if (error != null) {
+    return <ShowError error={error} onRetry={refetch} />;
+  }
+
   return (
     <ScreenContent>
       <ScreenTitle>
         <FormattedMessage id="AnalyticsScreen.title" />
       </ScreenTitle>
-      <AnalyticsActivityWidget />
+      <AnalyticsActivityWidget
+        periodical={periodical}
+        activeUserChartData={activeUserChart}
+        totalUserCountChartData={totalUserCountChart}
+      />
     </ScreenContent>
   );
 };
