6ffa71b417d66d367da35ff43811a1ca64d7dfe3
Carmen <carmen@oursky.com>
2022-02-07 14:52:01 +0000

    Add monthly and weekly pivot in analytics page

--- portal/src/graphql/portal/AnalyticsScreen.tsx
+++ portal/src/graphql/portal/AnalyticsScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { FormattedMessage } from "@oursky/react-messageformat";
 import { useAnalyticChartsQuery } from "./query/analyticChartsQuery";
@@ -6,26 +6,22 @@ import { Periodical } from "./__generated__/globalTypes";
 import ScreenContent from "../../ScreenContent";
 import ScreenTitle from "../../ScreenTitle";
 import AnalyticsActivityWidget from "./AnalyticsActivityWidget";
-import ShowLoading from "../../ShowLoading";
 import ShowError from "../../ShowError";
 import styles from "./AnalyticsScreen.module.scss";
 
 const AnalyticsScreen: React.FC = function AnalyticsScreen() {
   // FIXME: support user input
-  const periodical = Periodical.MONTHLY;
   const to = new Date();
   const from = new Date();
   from.setFullYear(from.getFullYear() - 1);
   const rangeTo = to.toISOString().split("T")[0];
   const rangeFrom = from.toISOString().split("T")[0];
 
+  const [periodical, setPeriodical] = useState<Periodical>(Periodical.MONTHLY);
+
   const { appID } = useParams();
   const { loading, error, refetch, activeUserChart, totalUserCountChart } =
     useAnalyticChartsQuery(appID, periodical, rangeFrom, rangeTo);
-
-  if (loading) {
-    return <ShowLoading />;
-  }
 
   if (error != null) {
     return <ShowError error={error} onRetry={refetch} />;
@@ -37,7 +33,9 @@ const AnalyticsScreen: React.FC = function AnalyticsScreen() {
         <FormattedMessage id="AnalyticsScreen.title" />
       </ScreenTitle>
       <AnalyticsActivityWidget
+        loading={loading}
         periodical={periodical}
+        onPeriodicalChange={setPeriodical}
         activeUserChartData={activeUserChart}
         totalUserCountChartData={totalUserCountChart}
       />
