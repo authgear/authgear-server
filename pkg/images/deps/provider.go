232d7993f5fb77ee02422c95ac6de03ea426d18a
Carmen <carmen@oursky.com>
2022-03-02 12:03:59 +0000

    Add empty get image handler

--- pkg/images/deps/provider.go
+++ pkg/images/deps/provider.go
@@ -1,9 +1,12 @@
 package deps
 
 import (
+	"net/http"
+
 	getsentry "github.com/getsentry/sentry-go"
 
 	imagesconfig "github.com/authgear/authgear-server/pkg/images/config"
+	"github.com/authgear/authgear-server/pkg/util/httproute"
 	"github.com/authgear/authgear-server/pkg/util/log"
 	"github.com/authgear/authgear-server/pkg/util/sentry"
 )
@@ -38,4 +41,34 @@ func NewRootProvider(
 		LoggerFactory:     loggerFactory,
 		SentryHub:         sentryHub,
 	}, nil
+}
+
+type RequestProvider struct {
+	RootProvider *RootProvider
+	Request      *http.Request
+}
+
+func (p *RootProvider) Middleware(f func(*RequestProvider) httproute.Middleware) httproute.Middleware {
+	return httproute.MiddlewareFunc(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			requestProvider := &RequestProvider{
+				RootProvider: p,
+				Request:      r,
+			}
+			m := f(requestProvider)
+			h := m.Handle(next)
+			h.ServeHTTP(w, r)
+		})
+	})
+}
+
+func (p *RootProvider) Handler(f func(*RequestProvider) http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestProvider := &RequestProvider{
+			RootProvider: p,
+			Request:      r,
+		}
+		h := f(requestProvider)
+		h.ServeHTTP(w, r)
+	})
 }
---
+++ pkg/images/wire.go
@@ -0,0 +1,38 @@
+//go:build wireinject
+// +build wireinject
+
+package images
+
+import (
+	"net/http"
+
+	"github.com/google/wire"
+
+	"github.com/authgear/authgear-server/pkg/images/deps"
+	"github.com/authgear/authgear-server/pkg/images/handler"
+	"github.com/authgear/authgear-server/pkg/lib/infra/middleware"
+	"github.com/authgear/authgear-server/pkg/util/httproute"
+)
+
+func newPanicMiddleware(p *deps.RequestProvider) httproute.Middleware {
+	panic(wire.Build(
+		deps.DependencySet,
+		middleware.DependencySet,
+		wire.Bind(new(httproute.Middleware), new(*middleware.PanicMiddleware)),
+	))
+}
+
+func newSentryMiddleware(p *deps.RequestProvider) httproute.Middleware {
+	panic(wire.Build(
+		deps.DependencySet,
+		wire.Struct(new(middleware.SentryMiddleware), "*"),
+		wire.Bind(new(httproute.Middleware), new(*middleware.SentryMiddleware)),
+	))
+}
+
+func newGetHandler(p *deps.RequestProvider) http.Handler {
+	panic(wire.Build(
+		wire.Struct(new(handler.GetHandler), "*"),
+		wire.Bind(new(http.Handler), new(*handler.GetHandler)),
+	))
+}
