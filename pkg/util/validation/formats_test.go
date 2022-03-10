d06158d5ce5f8fbd82124f7bfff76bf7d228b7c4
Louis Chan <louischan@oursky.com>
2022-03-10 18:15:30 +0800

    Allow picture to contain authgearimages URI

--- pkg/util/validation/formats_test.go
+++ pkg/util/validation/formats_test.go
@@ -63,6 +63,32 @@ func TestFormatURI(t *testing.T) {
 	})
 }
 
+func TestFormatPicture(t *testing.T) {
+	Convey("FormatPicture", t, func() {
+		f := FormatPicture{}.CheckFormat
+
+		So(f(1), ShouldBeNil)
+		So(f(""), ShouldBeError, "invalid scheme: ")
+		So(f("foobar:"), ShouldBeError, "invalid scheme: foobar")
+
+		So(f("http://example.com"), ShouldBeNil)
+		So(f("http://example.com/"), ShouldBeNil)
+		So(f("http://example.com/a"), ShouldBeNil)
+		So(f("http://example.com/a/"), ShouldBeNil)
+
+		So(f("http://example.com?"), ShouldBeNil)
+		So(f("http://example.com?a"), ShouldBeNil)
+		So(f("http://example.com?a=b"), ShouldBeNil)
+
+		So(f("http://example.com#"), ShouldBeNil)
+		So(f("http://example.com#a"), ShouldBeNil)
+
+		So(f("authgearimages:///app/object"), ShouldBeNil)
+		So(f("authgearimages:///../app/object"), ShouldBeError, "authgearimages URI must be normalized")
+		So(f("authgearimages://host/"), ShouldBeError, "authgearimages URI does not have host")
+	})
+}
+
 func TestFormatHTTPOrigin(t *testing.T) {
 	Convey("FormatHTTPOrigin", t, func() {
 		f := FormatHTTPOrigin{}.CheckFormat
