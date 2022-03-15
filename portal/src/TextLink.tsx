71b983fa0c13db9823b80f6b0e9b05ef502bef92
Louis Chan <louischan@oursky.com>
2022-03-15 13:50:20 +0800

    Introduce TextLink

---
+++ portal/src/TextLink.tsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { Context } from "@oursky/react-messageformat";
+import { Label, Text, Link } from "@fluentui/react";
+
+export interface TextLinkProps {
+  className?: string;
+  label: React.ReactNode;
+  value?: string | null;
+}
+
+// TextLink looks like TextField.
+const TextLink: React.FC<TextLinkProps> = function TextLink(props) {
+  const { className, label, value } = props;
+  const { renderToString } = useContext(Context);
+  return (
+    <div className={className}>
+      <Label>{label}</Label>
+      {value != null && value !== "" ? (
+        <Link href={value} target="_blank">
+          {value}
+        </Link>
+      ) : (
+        <Text>{renderToString("not-set")}</Text>
+      )}
+    </div>
+  );
+};
+
+export default TextLink;
