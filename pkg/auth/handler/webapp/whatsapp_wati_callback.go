af2995eb4437038f2d982c66e42edd93eaebcb33
Carmen <carmen@oursky.com>
2022-05-17 17:45:31 +0100

    Parse the wati callback payload and update the whatsapp code store

--- pkg/auth/handler/webapp/whatsapp_wati_callback.go
+++ pkg/auth/handler/webapp/whatsapp_wati_callback.go
@@ -1,11 +1,68 @@
 package webapp
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/authgear/authgear-server/pkg/util/httproute"
+	"github.com/authgear/authgear-server/pkg/util/httputil"
 	"github.com/authgear/authgear-server/pkg/util/log"
+	"github.com/authgear/authgear-server/pkg/util/validation"
 )
+
+var WhatsappWATICallbackSchema = validation.NewMultipartSchema("WhatsappWATICallback")
+var _ = WhatsappWATICallbackSchema.Add("WhatsappWATICallback", `
+{
+	"type": "object",
+	"additionalProperties": true,
+	"properties": {
+		"messages": {
+			"type": "array",
+			"items": { "$ref": "#/$defs/WhatsappWATICallbackMessage" }
+		}
+	},
+	"required": ["messages"]
+}
+`)
+var _ = WhatsappWATICallbackSchema.Add("WhatsappWATICallbackMessage", `
+{
+	"type": "object",
+	"additionalProperties": true,
+	"properties": {
+		"from": { "type": "string" },
+		"text": {  "$ref": "#/$defs/WhatsappWATICallbackMessageText" }
+	},
+	"required": ["from"]
+}
+`)
+var _ = WhatsappWATICallbackSchema.Add("WhatsappWATICallbackMessageText", `
+{
+	"type": "object",
+	"additionalProperties": true,
+	"properties": {
+		"body": { "type": "string" }
+	}
+}
+`)
+
+func init() {
+	WhatsappWATICallbackSchema.Instantiate()
+}
+
+type WhatsappWATICallbackMessageText struct {
+	Body string `json:"body"`
+}
+
+type WhatsappWATICallbackMessage struct {
+	From string                          `json:"from"`
+	Text WhatsappWATICallbackMessageText `json:"text"`
+}
+
+type WhatsappWATICallbackRequest struct {
+	Messages []WhatsappWATICallbackMessage `json:"messages"`
+}
 
 func ConfigureWhatsappWATICallbackRoute(route httproute.Route) httproute.Route {
 	return route.
@@ -26,4 +83,41 @@ func NewWhatsappWATICallbackHandlerLogger(lf *log.Factory) WhatsappWATICallbackH
 
 func (h *WhatsappWATICallbackHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.Logger.Info("wati callback received")
+
+	var err error
+	defer func() {
+		// always return OK and logs the error if any
+		if err != nil {
+			h.Logger.WithError(err).Warn("failed to consume message")
+		}
+		w.WriteHeader(http.StatusOK)
+	}()
+	var payload WhatsappWATICallbackRequest
+	err = httputil.BindJSONBody(r, w, WhatsappWATICallbackSchema.Validator(), &payload)
+	if err != nil {
+		return
+	}
+	if len(payload.Messages) < 1 {
+		err = errors.New("missing messages")
+		return
+	}
+	message := payload.Messages[0]
+	if message.From == "" {
+		err = errors.New("missing message from")
+		return
+	}
+	if message.Text.Body == "" {
+		err = errors.New("missing message body")
+		return
+	}
+
+	phone := message.From
+	if !strings.HasPrefix(phone, "+") {
+		phone = fmt.Sprintf("+%s", phone)
+	}
+	code := message.Text.Body
+	_, err = h.WhatsappCodeProvider.SetUserInputtedCode(phone, code)
+	if err != nil {
+		return
+	}
 }
