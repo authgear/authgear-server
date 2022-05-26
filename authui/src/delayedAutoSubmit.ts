4cb81ccc1cc95b039049af2e0dbc6e00a4db75a5
Carmen <carmen@oursky.com>
2022-05-26 12:18:31 +0100

    Remove visual count down from the next button of whatsapp otp page

--- authui/src/delayedAutoSubmit.ts
+++ authui/src/delayedAutoSubmit.ts
@@ -1,89 +1,58 @@
 import { Controller } from "@hotwired/stimulus";
 
 export class DelayedAutoSubmitController extends Controller {
-  static targets = ["button"];
-
   declare buttonTarget: HTMLButtonElement;
-  countDownSec: number = 0;
-
-  animationReq: number | null = null;
   intervalReq: number | null = null;
-  visibilityChangeListener: (() => void) | null = null;
 
   connect() {
-    this.countDownSec = Number(
-      this.buttonTarget.getAttribute("data-countdown-sec")
-    );
+    this.buttonTarget = this.element as HTMLButtonElement;
     if (!this.startCountDownWhenVisible()) {
-      this.visibilityChangeListener = this.startCountDownWhenVisible.bind(this);
       document.addEventListener(
         "visibilitychange",
-        this.visibilityChangeListener
+        this.startCountDownWhenVisible
       );
     }
   }
 
   disconnect() {
-    if (this.animationReq) {
-      cancelAnimationFrame(this.animationReq);
-      this.animationReq = null;
-    }
+    this.cancelAllListener();
+  }
+
+  cancelAllListener() {
     if (this.intervalReq) {
       clearInterval(this.intervalReq);
       this.intervalReq = null;
     }
-    if (this.visibilityChangeListener) {
-      document.removeEventListener(
-        "visibilitychange",
-        this.visibilityChangeListener
-      );
-      this.visibilityChangeListener = null;
-    }
+    document.removeEventListener(
+      "visibilitychange",
+      this.startCountDownWhenVisible
+    );
   }
 
-  startCounter() {
-    const label = this.buttonTarget.getAttribute("data-label");
-    const labelUnit = this.buttonTarget.getAttribute("data-label-unit");
-    const endAt = new Date().getTime() + this.countDownSec * 1000;
-    const that = this;
-
-    function count() {
-      const remainingTime = endAt - new Date().getTime();
-      let displaySeconds = Math.round(remainingTime / 1000);
-      if (displaySeconds < 0) {
-        that.buttonTarget.disabled = true;
-        that.buttonTarget.textContent = label;
-        that.animationReq = null;
-      } else {
-        that.buttonTarget.textContent =
-          labelUnit && labelUnit.replace("%d", String(displaySeconds));
-        that.animationReq = requestAnimationFrame(count);
-      }
-    }
-    this.animationReq = requestAnimationFrame(count);
-  }
+  onClick = (e: Event) => {
+    this.cancelAllListener();
+  };
 
   setupAutoSubmit() {
+    let countDownSec = Number(
+      this.buttonTarget.getAttribute("data-countdown-sec")
+    );
     this.intervalReq = setInterval(() => {
       this.buttonTarget.click();
       this.intervalReq = null;
-    }, this.countDownSec * 1000);
+    }, countDownSec * 1000);
   }
 
-  startCountDownWhenVisible() {
+  startCountDownWhenVisible = () => {
     if (document.visibilityState == "visible") {
-      this.startCounter();
       this.setupAutoSubmit();
       // remove the listener once the counter is started
-      if (this.visibilityChangeListener) {
-        document.removeEventListener(
-          "visibilitychange",
-          this.visibilityChangeListener
-        );
-        this.visibilityChangeListener = null;
-      }
+      document.removeEventListener(
+        "visibilitychange",
+        this.startCountDownWhenVisible
+      );
       return true;
     }
     return false;
-  }
+  };
 }
