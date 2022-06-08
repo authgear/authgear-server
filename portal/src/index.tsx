1a1a730cbc40f34fc209cbe263b57eed7c00f1a5
Louis Chan <louischan@oursky.com>
2022-06-08 18:14:55 +0800

    Remove Babel polyfill

--- portal/src/index.tsx
+++ portal/src/index.tsx
@@ -1,14 +1,4 @@
-// comment out next line for disabling axe
 // /* global process */
-// This following 2 lines are extremely important.
-// Since we do not provide our babel.config.json,
-// Parcel provides a default one for us.
-// The default config uses preset-env with useBuiltins: entry.
-// Therefore, we have to include the following imports to
-// let Babel rewrite them into polyfill imports according to .browserslistrc.
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
 import "normalize.css";
 import "./index.scss";
 import "intl-tel-input/build/css/intlTelInput.css";
