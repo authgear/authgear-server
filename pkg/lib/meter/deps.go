5202d2d602372ce7930125a5173ef947ff885097
Carmen <carmen@oursky.com>
2022-06-14 16:46:31 +0100

    Add meter audit db store

--- pkg/lib/meter/deps.go
+++ pkg/lib/meter/deps.go
@@ -9,5 +9,6 @@ var DependencySet = wire.NewSet(
 	wire.Struct(new(Service), "*"),
 	wire.Struct(new(ReadStoreRedis), "*"),
 	wire.Struct(new(WriteStoreRedis), "*"),
+	wire.Struct(new(AuditDBReadStore), "*"),
 	wire.Bind(new(CounterStore), new(*WriteStoreRedis)),
 )
