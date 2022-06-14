d6f0e27256833149299987b415a1e9b61c5095c8
Louis Chan <louischan@oursky.com>
2022-06-14 13:03:35 +0800

    Rename builtin rate limit buckets

--- pkg/lib/feature/verification/ratelimit.go
+++ pkg/lib/feature/verification/ratelimit.go
@@ -7,9 +7,7 @@ import (
 	"github.com/authgear/authgear-server/pkg/util/duration"
 )
 
-// TODO(rate-limit): allow configuration of bucket size & reset period
-
-func VerifyRateLimitBucket(ip string) ratelimit.Bucket {
+func AutiBruteForceVerifyBucket(ip string) ratelimit.Bucket {
 	return ratelimit.Bucket{
 		Key:         fmt.Sprintf("verification-verify-code:%s", ip),
 		Size:        10,
