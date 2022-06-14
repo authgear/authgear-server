8b5dce84b9b27bd7347a0ce8dc6f4096134be6c8
Louis Chan <louischan@oursky.com>
2022-06-14 14:30:40 +0800

    Distinguish between request rate limit and usage limit

--- pkg/lib/ratelimit/error.go
+++ pkg/lib/ratelimit/error.go
@@ -4,3 +4,6 @@ import "github.com/authgear/authgear-server/pkg/api/apierrors"
 
 var ErrTooManyRequests = apierrors.TooManyRequest.WithReason("RateLimited").
 	New("request rate limited")
+
+var ErrUsageLimitExceeded = apierrors.ServiceUnavailable.WithReason("UsageLimitExceeded").
+	New("usage limit exceeded")
