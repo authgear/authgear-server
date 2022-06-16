2e055aa1e30c8f0d94a8247c50c4e60cdb73fe43
Louis Chan <louischan@oursky.com>
2022-06-16 16:15:58 +0800

    Cache subscription plans for 1 hour

--- pkg/portal/libstripe/service.go
+++ pkg/portal/libstripe/service.go
@@ -2,14 +2,21 @@ package libstripe
 
 import (
 	"context"
+	"encoding/json"
 
+	goredis "github.com/go-redis/redis/v8"
 	"github.com/stripe/stripe-go/v72"
 	"github.com/stripe/stripe-go/v72/client"
 
+	"github.com/authgear/authgear-server/pkg/lib/infra/redis/globalredis"
 	portalconfig "github.com/authgear/authgear-server/pkg/portal/config"
 	"github.com/authgear/authgear-server/pkg/portal/model"
+	"github.com/authgear/authgear-server/pkg/util/duration"
 	"github.com/authgear/authgear-server/pkg/util/log"
+	"github.com/authgear/authgear-server/pkg/util/redisutil"
 )
+
+const RedisCacheKeySubscriptionPlans = "cache:portal:subscription-plans"
 
 type Logger struct{ *log.Logger }
 
@@ -29,14 +36,45 @@ type PlanService interface {
 	ListPlans() ([]*model.Plan, error)
 }
 
-type Service struct {
-	ClientAPI *client.API
-	Logger    Logger
-	Context   context.Context
-	Plans     PlanService
+type Cache interface {
+	Get(context.Context, redisutil.SimpleCmdable, redisutil.Item) ([]byte, error)
 }
 
-func (s *Service) FetchSubscriptionPlans() ([]*SubscriptionPlan, error) {
+type Service struct {
+	ClientAPI         *client.API
+	Logger            Logger
+	Context           context.Context
+	Plans             PlanService
+	GlobalRedisHandle *globalredis.Handle
+	Cache             Cache
+}
+
+func (s *Service) FetchSubscriptionPlans() (subscriptionPlans []*SubscriptionPlan, err error) {
+	item := redisutil.Item{
+		Key:        RedisCacheKeySubscriptionPlans,
+		Expiration: duration.PerHour,
+		Do:         s.fetchSubscriptionPlans,
+	}
+
+	err = s.GlobalRedisHandle.WithConn(func(conn *goredis.Conn) error {
+		bytes, err := s.Cache.Get(s.Context, conn, item)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(bytes, &subscriptionPlans)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (s *Service) fetchSubscriptionPlans() ([]byte, error) {
 	plans, err := s.Plans.ListPlans()
 	if err != nil {
 		return nil, err
@@ -50,7 +88,13 @@ func (s *Service) FetchSubscriptionPlans() ([]*SubscriptionPlan, error) {
 	if err != nil {
 		return nil, err
 	}
-	return subscriptionPlans, nil
+
+	bytes, err := json.Marshal(subscriptionPlans)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
 }
 
 func (s *Service) fetchProducts() ([]*stripe.Product, error) {
