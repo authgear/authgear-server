62da755a5cb523f5a895a8b167fd04353f9999f4
Louis Chan <louischan@oursky.com>
2022-06-16 16:15:58 +0800

    Cache subscription plans for 1 hour

--- pkg/portal/libstripe/deps.go
+++ pkg/portal/libstripe/deps.go
@@ -2,10 +2,24 @@ package libstripe
 
 import (
 	"github.com/google/wire"
+
+	"github.com/authgear/authgear-server/pkg/util/redisutil"
 )
+
+type StripeCache struct {
+	*redisutil.Cache
+}
+
+func NewStripeCache() *StripeCache {
+	return &StripeCache{
+		Cache: &redisutil.Cache{},
+	}
+}
 
 var DependencySet = wire.NewSet(
 	NewLogger,
 	NewClientAPI,
+	NewStripeCache,
 	wire.Struct(new(Service), "*"),
+	wire.Bind(new(Cache), new(*StripeCache)),
 )
