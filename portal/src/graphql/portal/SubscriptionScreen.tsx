e5019f8c5c4b454d5ee3206b8bfe0288ac6a1c12
Louis Chan <louischan@oursky.com>
2022-06-21 14:13:26 +0800

    Show billing information for custom plan

--- portal/src/graphql/portal/SubscriptionScreen.tsx
+++ portal/src/graphql/portal/SubscriptionScreen.tsx
@@ -1,177 +1,24 @@
-import React, { useContext, useMemo } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import { PrimaryButton, Text } from "@fluentui/react";
-import { FormattedMessage, Context } from "@oursky/react-messageformat";
-import ScreenContent from "../../ScreenContent";
+import { Text } from "@fluentui/react";
+import { FormattedMessage } from "@oursky/react-messageformat";
 import ScreenTitle from "../../ScreenTitle";
 import ShowError from "../../ShowError";
 import ShowLoading from "../../ShowLoading";
 import { useAppFeatureConfigQuery } from "./query/appFeatureConfigQuery";
 import styles from "./SubscriptionScreen.module.scss";
-import Widget from "../../Widget";
-import WidgetTitle from "../../WidgetTitle";
+import SubscriptionCurrentPlanSummary, {
+  CostItem,
+  CostItemSeparator,
+} from "./SubscriptionCurrentPlanSummary";
+
+const DEFAULT_PLAN_NAME = "free";
 
 const contactUsLink = "https://oursky.typeform.com/to/PecQiGfc";
-
-const paidPlansForBlocksList = ["startups", "business", "enterprise"];
-
-function isCustomPlan(planName: string): boolean {
-  return (
-    ["free", "startups", "business", "enterprise"].indexOf(planName) === -1
-  );
-}
-interface SubscriptionPlanSummaryProps {
-  planName: string;
-}
-
-const SubscriptionPlanSummary: React.FC<SubscriptionPlanSummaryProps> =
-  function SubscriptionPlanSummary(props) {
-    const { planName } = props;
-    const isCustom = useMemo(() => {
-      return isCustomPlan(planName);
-    }, [planName]);
-
-    return (
-      <Widget className={styles.widget}>
-        <WidgetTitle>
-          <FormattedMessage id="SubscriptionScreen.current-plan-summary.title" />
-        </WidgetTitle>
-        <div className={styles.summaryList}>
-          <div className={styles.summaryItem}>
-            <Text variant="medium" block={true} className={styles.label}>
-              <FormattedMessage id="SubscriptionScreen.subscription.label" />
-            </Text>
-            <Text variant="xLarge" block={true}>
-              {isCustom ? (
-                <>{planName}</>
-              ) : (
-                <FormattedMessage
-                  id={`SubscriptionScreen.plan-name.${planName}`}
-                />
-              )}
-            </Text>
-          </div>
-          <div className={styles.summaryItem}>
-            <Text variant="medium" block={true} className={styles.label}>
-              <FormattedMessage id="SubscriptionScreen.monthly-active-users.label" />
-            </Text>
-            <Text variant="xLarge" block={true}>
-              {isCustom ? (
-                <>-</>
-              ) : (
-                <FormattedMessage
-                  id={`SubscriptionScreen.summary-mau.${planName}`}
-                />
-              )}
-            </Text>
-          </div>
-          <div className={styles.summaryItem}>
-            <Text variant="medium" block={true} className={styles.label}>
-              <FormattedMessage id="SubscriptionScreen.plan-cost.label" />
-            </Text>
-            <Text variant="xLarge">
-              {isCustom ? (
-                <>-</>
-              ) : (
-                <FormattedMessage
-                  id={`SubscriptionScreen.summary-price.${planName}`}
-                />
-              )}
-            </Text>
-          </div>
-        </div>
-      </Widget>
-    );
-  };
-
-interface SubscriptionPlanBlockProps {
-  planName: string;
-}
-
-const SubscriptionPlanBlock: React.FC<SubscriptionPlanBlockProps> =
-  function SubscriptionPlanBlock(props) {
-    const { planName } = props;
-    const { renderToString } = useContext(Context);
-
-    const planCopy = useMemo(() => {
-      return {
-        title: renderToString(`SubscriptionScreen.plan-name.${planName}`),
-        desc: renderToString(`SubscriptionScreen.des.${planName}`),
-        price: renderToString(`SubscriptionScreen.price.${planName}`),
-        mau: renderToString(`SubscriptionScreen.mau.${planName}`),
-        extraCost: renderToString(`SubscriptionScreen.extra-cost.${planName}`),
-        features: renderToString(`SubscriptionScreen.features.${planName}`),
-      };
-    }, [planName, renderToString]);
-
-    return (
-      <div className={styles.planBlockItem}>
-        <Text as="h3" variant="xLarge" block={true} className={styles.title}>
-          {planCopy.title}
-        </Text>
-        <Text variant="medium" block={true} className={styles.desc}>
-          {planCopy.desc}
-        </Text>
-        <Text variant="xLarge" block={true} className={styles.price}>
-          {planCopy.price}
-        </Text>
-        <Text variant="medium" block={true} className={styles.mau}>
-          {planCopy.mau}
-        </Text>
-        {planCopy.extraCost && (
-          <Text variant="medium" block={true} className={styles.extraCost}>
-            {planCopy.extraCost}
-          </Text>
-        )}
-        <div className={styles.separator} />
-        <Text variant="medium" block={true} className={styles.features}>
-          {planCopy.features}
-        </Text>
-        <PrimaryButton
-          target="_blank"
-          rel="noreferrer"
-          href={contactUsLink}
-          className={styles.contactUsLink}
-        >
-          <FormattedMessage id="SubscriptionScreen.contact-us.label" />
-        </PrimaryButton>
-      </div>
-    );
-  };
-
-const SubscriptionPlanBlocks: React.FC = function SubscriptionPlanBlocks() {
-  return (
-    <Widget className={styles.widget}>
-      <WidgetTitle>
-        <FormattedMessage id="SubscriptionScreen.upgrade-your-plan.title" />
-      </WidgetTitle>
-      <div className={styles.planBlockList}>
-        {paidPlansForBlocksList.map((planName) => (
-          <SubscriptionPlanBlock key={planName} planName={planName} />
-        ))}
-      </div>
-    </Widget>
-  );
-};
-
-const SubscriptionPlanFeatures: React.FC = function SubscriptionPlanFeatures() {
-  return (
-    <div className={styles.widget}>
-      <Text as="p" variant="medium" block={true}>
-        <FormattedMessage id="SubscriptionScreen.plan-features.desc" />
-      </Text>
-    </div>
-  );
-};
 
 const SubscriptionScreen: React.FC = function SubscriptionScreen() {
   const { appID } = useParams();
   const featureConfig = useAppFeatureConfigQuery(appID);
-
-  const planName = useMemo(
-    () => featureConfig.planName ?? "-",
-    [featureConfig.planName]
-  );
 
   if (featureConfig.loading) {
     return <ShowLoading />;
@@ -188,15 +35,54 @@ const SubscriptionScreen: React.FC = function SubscriptionScreen() {
     );
   }
 
+  const planName = featureConfig.planName ?? DEFAULT_PLAN_NAME;
+
   return (
-    <ScreenContent>
-      <ScreenTitle className={styles.widget}>
+    <div className={styles.root}>
+      <ScreenTitle className={styles.section}>
         <FormattedMessage id="SubscriptionScreen.title" />
       </ScreenTitle>
-      <SubscriptionPlanSummary planName={planName} />
-      <SubscriptionPlanBlocks />
-      <SubscriptionPlanFeatures />
-    </ScreenContent>
+      <SubscriptionCurrentPlanSummary
+        className={styles.section}
+        planName={planName}
+      >
+        <CostItem
+          title={
+            <FormattedMessage id="SubscriptionCurrentPlanSummary.total-cost.title" />
+          }
+          kind="non-applicable"
+          tooltip={
+            <FormattedMessage id="SubscriptionCurrentPlanSummary.total-cost.tooltip" />
+          }
+        />
+        <CostItemSeparator />
+        <CostItem
+          title={
+            <FormattedMessage id="SubscriptionCurrentPlanSummary.whatsapp.title" />
+          }
+          kind="non-applicable"
+        />
+        <CostItem
+          title={
+            <FormattedMessage id="SubscriptionCurrentPlanSummary.sms.title" />
+          }
+          kind="non-applicable"
+        />
+      </SubscriptionCurrentPlanSummary>
+      <div className={styles.footer}>
+        <Text block={true}>
+          <FormattedMessage
+            id="SubscriptionScreen.footer.enterprise-plan"
+            values={{
+              link: contactUsLink,
+            }}
+          />
+        </Text>
+        <Text block={true}>
+          <FormattedMessage id="SubscriptionScreen.footer.pricing-details" />
+        </Text>
+      </div>
+    </div>
   );
 };
 
