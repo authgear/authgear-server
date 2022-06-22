f50594eac8ce20ddf405c5f6335c663a8dc12b87
Carmen <carmen@oursky.com>
2022-06-22 15:51:31 +0100

    Add _portal_subscription_checkout and update status
    - Insert the record when createCheckoutSession
    - Update status and customer id when reconcileCheckoutSession

--- pkg/portal/model/subscription.go
+++ pkg/portal/model/subscription.go
@@ -1,9 +1,30 @@
 package model
 
+import (
+	"time"
+)
+
+type SubscriptionCheckoutStatus string
+
+const (
+	SubscriptionCheckoutStatusOpen       SubscriptionCheckoutStatus = "open"
+	SubscriptionCheckoutStatusCompleted  SubscriptionCheckoutStatus = "completed"
+	SubscriptionCheckoutStatusSubscribed SubscriptionCheckoutStatus = "subscribed"
+)
+
 type Subscription struct {
 	ID                      string
 	AppID                   string
 	StripeCheckoutSessionID string
 	StripeCustomerID        string
 	StripeSubscriptionID    string
+}
+
+type SubscriptionCheckout struct {
+	ID                      string
+	AppID                   string
+	StripeCheckoutSessionID string
+	StripeCustomerID        *string
+	Status                  SubscriptionCheckoutStatus
+	ExpireAt                time.Time
 }
--- pkg/portal/service/subscription.go
+++ pkg/portal/service/subscription.go
@@ -1,11 +1,17 @@
 package service
 
 import (
+	"time"
+
+	"github.com/authgear/authgear-server/pkg/api/apierrors"
 	"github.com/authgear/authgear-server/pkg/lib/infra/db/globaldb"
 	"github.com/authgear/authgear-server/pkg/portal/libstripe"
 	"github.com/authgear/authgear-server/pkg/portal/model"
 	"github.com/authgear/authgear-server/pkg/util/uuid"
 )
+
+var ErrSubscriptionCheckoutNotFound = apierrors.NotFound.WithReason("ErrSubscriptionCheckoutNotFound").
+	New("subscription checkout not found")
 
 type SubscriptionService struct {
 	SQLBuilder  *globaldb.SQLBuilder
@@ -28,6 +34,45 @@ func (s *SubscriptionService) CreateSubscription(stripeSubscription *libstripe.S
 	return subscription, nil
 }
 
+func (s *SubscriptionService) CreateSubscriptionCheckout(checkoutSession *libstripe.CheckoutSession) (*model.SubscriptionCheckout, error) {
+	cs := &model.SubscriptionCheckout{
+		ID:                      uuid.New(),
+		StripeCheckoutSessionID: checkoutSession.StripeCheckoutSessionID,
+		AppID:                   checkoutSession.AppID,
+		Status:                  model.SubscriptionCheckoutStatusOpen,
+		ExpireAt:                time.Unix(checkoutSession.ExpiresAt, 0).UTC(),
+	}
+	if err := s.createSubscriptionCheckout(cs); err != nil {
+		return nil, err
+	}
+	return cs, nil
+}
+
+func (s *SubscriptionService) UpdateSubscriptionCheckoutStatusAndCustomerID(appID string, stripCheckoutSessionID string, status model.SubscriptionCheckoutStatus, customerID string) error {
+	q := s.SQLBuilder.
+		Update(s.SQLBuilder.TableName("_portal_subscription_checkout")).
+		Set("status", status).
+		Set("stripe_customer_id", customerID).
+		Where("stripe_checkout_session_id = ?", stripCheckoutSessionID).
+		Where("app_id = ?", appID)
+
+	result, err := s.SQLExecutor.ExecWith(q)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrSubscriptionCheckoutNotFound
+	}
+
+	return nil
+}
+
 func (s *SubscriptionService) createSubscription(sub *model.Subscription) error {
 	_, err := s.SQLExecutor.ExecWith(s.SQLBuilder.
 		Insert(s.SQLBuilder.TableName("_portal_subscription")).
@@ -44,6 +89,33 @@ func (s *SubscriptionService) createSubscription(sub *model.Subscription) error 
 			sub.StripeCheckoutSessionID,
 			sub.StripeCustomerID,
 			sub.StripeSubscriptionID,
+		),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *SubscriptionService) createSubscriptionCheckout(sc *model.SubscriptionCheckout) error {
+	_, err := s.SQLExecutor.ExecWith(s.SQLBuilder.
+		Insert(s.SQLBuilder.TableName("_portal_subscription_checkout")).
+		Columns(
+			"id",
+			"app_id",
+			"stripe_checkout_session_id",
+			"stripe_customer_id",
+			"status",
+			"expire_at",
+		).
+		Values(
+			sc.ID,
+			sc.AppID,
+			sc.StripeCheckoutSessionID,
+			sc.StripeCustomerID,
+			sc.Status,
+			sc.ExpireAt,
 		),
 	)
 	if err != nil {
