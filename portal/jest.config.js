8f2652edd05bbef2d4a93df4c5d0514051f0cf87
andychow326 <andychchow326@gmail.com>
2022-06-22 12:36:02 +0800

    Add npm test for portal in CI

--- portal/jest.config.js
+++ portal/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
-    "^.+\\.ts?$": "ts-jest",
+    "^.+\\.tsx?$": "ts-jest",
   },
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
