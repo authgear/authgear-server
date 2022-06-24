ec44c62131dc65d97be9c618241a2804a9b3cc7d
Carmen <carmen@oursky.com>
2022-06-24 16:58:25 +0100

    Show loading when isProcessingSubscription and check again in every 5 sec

--- portal/src/ShowLoading.tsx
+++ portal/src/ShowLoading.tsx
@@ -3,14 +3,20 @@ import { Spinner } from "@fluentui/react";
 import { Context } from "@oursky/react-messageformat";
 import styles from "./ShowLoading.module.scss";
 
+interface ShowLoadingProps {
+  label?: string;
+}
+
 // ShowLoading show a 100% width and 100% height spinner.
 // For better UX, please use Shimmer instead.
-const ShowLoading: React.FC = function ShowLoading() {
+const ShowLoading: React.FC<ShowLoadingProps> = function ShowLoading({
+  label,
+}) {
   const { renderToString } = useContext(Context);
 
   return (
     <div className={styles.loading}>
-      <Spinner label={renderToString("loading")} />
+      <Spinner label={label ?? renderToString("loading")} />
     </div>
   );
 };
