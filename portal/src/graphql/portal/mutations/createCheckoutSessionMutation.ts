7db44518f83948f84a6593025803130946ddab05
Carmen <carmen@oursky.com>
2022-06-24 15:14:51 +0100

    Add createCheckoutSessionMutation in the portal

---
+++ portal/src/graphql/portal/mutations/createCheckoutSessionMutation.ts
@@ -0,0 +1,35 @@
+import React from "react";
+import { useMutation } from "@apollo/client";
+
+import { client } from "../../portal/apollo";
+import {
+  CreateCheckoutSessionMutationMutation,
+  CreateCheckoutSessionMutationDocument,
+} from "./createCheckoutSessionMutation.generated";
+
+export function useCreateCheckoutSessionMutation(): {
+  createCheckoutSession: (
+    appID: string,
+    planName: string
+  ) => Promise<string | null>;
+  loading: boolean;
+  error: unknown;
+} {
+  const [mutationFunction, { error, loading }] =
+    useMutation<CreateCheckoutSessionMutationMutation>(
+      CreateCheckoutSessionMutationDocument,
+      {
+        client,
+      }
+    );
+  const createCheckoutSession = React.useCallback(
+    async (appID: string, planName: string) => {
+      const result = await mutationFunction({
+        variables: { appID, planName },
+      });
+      return result.data?.createCheckoutSession.url ?? null;
+    },
+    [mutationFunction]
+  );
+  return { createCheckoutSession, error, loading };
+}
