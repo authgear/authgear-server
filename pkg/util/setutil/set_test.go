43cc4b3e3258738a41c01658ac510165a0554289
Louis Chan <louischan@oursky.com>
2022-07-04 12:24:42 +0800

    Add setutil

---
+++ pkg/util/setutil/set_test.go
@@ -0,0 +1,20 @@
+package setutil
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSet(t *testing.T) {
+	Convey("Set", t, func() {
+		slice1 := []string{"1", "3", "5"}
+		slice2 := []string{"1", "2", "6"}
+
+		s1 := NewSetFromSlice(slice1, Identity[string])
+		s2 := NewSetFromSlice(slice2, Identity[string])
+
+		So(SetToSlice(slice1, s1.Subtract(s2), Identity[string]), ShouldResemble, []string{"3", "5"})
+		So(SetToSlice(slice2, s2.Subtract(s1), Identity[string]), ShouldResemble, []string{"2", "6"})
+	})
+}
