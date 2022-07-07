66e733ba295e728ce35ff60495f55b87fcd1f43d
Carmen <carmen@oursky.com>
2022-07-07 12:39:54 +0100

    Remove basic password policy from the feature config

--- pkg/lib/authn/authenticator/password/deps.go
+++ pkg/lib/authn/authenticator/password/deps.go
@@ -14,21 +14,12 @@ func ProvideChecker(
 	checker := &Checker{
 		PasswordHistoryStore: s,
 	}
-	if !*featureCfg.Password.Policy.MinLength.Disabled {
-		checker.PwMinLength = *cfg.Policy.MinLength
-	}
-	if !*featureCfg.Password.Policy.UppercaseRequired.Disabled {
-		checker.PwUppercaseRequired = cfg.Policy.UppercaseRequired
-	}
-	if !*featureCfg.Password.Policy.LowercaseRequired.Disabled {
-		checker.PwLowercaseRequired = cfg.Policy.LowercaseRequired
-	}
-	if !*featureCfg.Password.Policy.DigitRequired.Disabled {
-		checker.PwDigitRequired = cfg.Policy.DigitRequired
-	}
-	if !*featureCfg.Password.Policy.SymbolRequired.Disabled {
-		checker.PwSymbolRequired = cfg.Policy.SymbolRequired
-	}
+	checker.PwMinLength = *cfg.Policy.MinLength
+	checker.PwUppercaseRequired = cfg.Policy.UppercaseRequired
+	checker.PwLowercaseRequired = cfg.Policy.LowercaseRequired
+	checker.PwDigitRequired = cfg.Policy.DigitRequired
+	checker.PwSymbolRequired = cfg.Policy.SymbolRequired
+
 	if !*featureCfg.Password.Policy.History.Disabled {
 		checker.PwHistorySize = cfg.Policy.HistorySize
 		checker.PwHistoryDays = cfg.Policy.HistoryDays
