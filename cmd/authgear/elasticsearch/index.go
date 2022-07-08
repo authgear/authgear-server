aa1fc315cfd98742403d50a1d7de5114321648cf
Louis Chan <louischan@oursky.com>
2022-07-08 17:33:00 +0800

    Improve elasticsearch accuracy
    ref #2071

--- cmd/authgear/elasticsearch/index.go
+++ cmd/authgear/elasticsearch/index.go
@@ -18,8 +18,41 @@ func CreateIndex(es *elasticsearch.Client) error {
 
 	// If we even need to adjust index_prefixes.min_chars,
 	// we have to make sure that PrefixMinChars in pkg/lib/elasticsearch is also updated.
+
+	// The default analyzer "standard" breaks email address into 3 parts (the local part, the @, and the domain part).
+	// For example, suppose the standard attribute "name" of UserA is "usera@example.com".
+	// Suppose we are searching for "userb@example.com".
+	// The result will have 2 hits, one for UserA, one for UserB.
+	// UserA is a hit because the search keyword is being broken into 3 parts,
+	// and the last part matches the "name" field.
+	//
+	// To prevent this, we want to prevent the search query from being broken into 3 parts,
+	// and instead treat the whole query as a term.
+	//
+	// uax_url_email is standard tokenizer except it treats URL and email address as single tokens. See https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-uaxurlemail-tokenizer.html
+	//
+	// The official way to customize the standard analyzer is documented at https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-standard-analyzer.html#_definition_4
+	//
+	// Our approach is to create a new analyzer that is equivalent to the standard analyzer with URL and email address treated as single tokens.
+	// In the index, we create an analyzer named "default_search".
+	// This name has a special meaning. See https://www.elastic.co/guide/en/elasticsearch/reference/current/specify-analyzer.html#specify-search-default-analyzer
+	// Note that the standard analyzer is still being used to analyze the field.
+	// So if the query search is "example.com", it can still match the "name" field in the above example.
+	// By applying the analyzer to the search query instead of the field,
+	// we keep the characteristics of "more specific search query gives more specific search result".
 	bodyStr := `
 	{
+		"settings": {
+			"analysis": {
+				"analyzer": {
+					"default_search": {
+						"type": "custom",
+						"tokenizer": "uax_url_email",
+						"filter": ["lowercase"]
+					}
+				}
+			}
+		},
 		"mappings": {
 			"properties": {
 				"app_id": { "type": "keyword" },
