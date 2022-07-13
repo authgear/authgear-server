b1d2bf946c90909af189fc738b07384c179e0601
Louis Chan <louischan@oursky.com>
2022-07-13 10:32:01 +0800

    Handle more polyfill cases

--- portal/packages/parcel-resolver-authgear/src/index.js
+++ portal/packages/parcel-resolver-authgear/src/index.js
@@ -12,6 +12,17 @@ function specifierToPackage(specifier) {
   return parts[0];
 }
 
+const POLYFILL_PREFIX = ["@swc/helpers", "@parcel/transformer-"];
+
+function isPolyfill(package) {
+  for (const prefix of POLYFILL_PREFIX) {
+    if (package.startsWith(prefix)) {
+      return true;
+    }
+  }
+  return false;
+}
+
 // The purpose of this plugin is to prevent our JavaScript files
 // from importing unlisted dependencies.
 // If such thing happen, code splitted bundle will NOT import
@@ -37,8 +48,9 @@ module.exports = new Resolver({
       // importing packages from node_modules
       if (specifier != null && !specifier.startsWith(".")) {
         const package = specifierToPackage(specifier);
+        const polyfill = isPolyfill(package);
         // and it is not polyfill.
-        if (package !== "@swc/helpers") {
+        if (!polyfill) {
           // Make sure the dependency is listed in package.json
           const version = packageJSON["dependencies"][package];
           if (version == null) {
