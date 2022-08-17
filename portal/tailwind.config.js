87908fc73ae30ce56d6665a700191701d01e5921
andychow326 <andychchow326@gmail.com>
2022-08-17 18:37:32 +0800

    Install tailwind

---
+++ portal/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    screens: {
+      mobile: { max: "640px" },
+    },
+  },
+  plugins: [],
+};
