ccfb6da5c3006af915f23bc0f68436b6671f3024
andychow326 <andychchow326@gmail.com>
2022-08-17 19:22:54 +0800

    Hide nav bar when width is less than 640px

--- portal/src/ScreenLayout.tsx
+++ portal/src/ScreenLayout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ScreenHeader from "./ScreenHeader";
 import ScreenNav from "./ScreenNav";
 import styles from "./ScreenLayout.module.css";
+import cn from "classnames";
 
 interface ScreenLayoutProps {
   children: React.ReactElement;
@@ -14,7 +15,7 @@ const ScreenLayout: React.FC<ScreenLayoutProps> = function ScreenLayout(
     <div className={styles.root}>
       <ScreenHeader />
       <div className={styles.body}>
-        <div className={styles.nav}>
+        <div className={cn(styles.nav, "mobile:hidden")}>
           <ScreenNav />
         </div>
         <div className={styles.content}>{props.children}</div>
