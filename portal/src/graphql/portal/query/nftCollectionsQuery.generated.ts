3c453bb6b8585a0d3d346dcf10e98d936c5be328
Rico <hochiw@oursky.com>
2022-09-01 21:23:46 +0800

    Integrate nft collection queries and mutations to portal
    refs #2310

---
+++ portal/src/graphql/portal/query/nftCollectionsQuery.generated.ts
@@ -0,0 +1,62 @@
+import * as Types from '../globalTypes.generated';
+
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
+export type NftCollectionsQueryQueryVariables = Types.Exact<{
+  appID: Types.Scalars['ID'];
+}>;
+
+
+export type NftCollectionsQueryQuery = { __typename?: 'Query', node?: { __typename: 'App', id: string, nftCollections: Array<{ __typename?: 'NFTCollection', id?: string | null, name: string, blockchain: string, network: string, contractAddress: string, blockHeight: number, totalSupply: number, type: string, createdAt: any }> } | { __typename: 'User' } | null };
+
+
+export const NftCollectionsQueryDocument = gql`
+    query nftCollectionsQuery($appID: ID!) {
+  node(id: $appID) {
+    __typename
+    ... on App {
+      id
+      nftCollections {
+        id
+        name
+        blockchain
+        network
+        contractAddress
+        blockHeight
+        totalSupply
+        type
+        createdAt
+      }
+    }
+  }
+}
+    `;
+
+/**
+ * __useNftCollectionsQueryQuery__
+ *
+ * To run a query within a React component, call `useNftCollectionsQueryQuery` and pass it any options that fit your needs.
+ * When your component renders, `useNftCollectionsQueryQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useNftCollectionsQueryQuery({
+ *   variables: {
+ *      appID: // value for 'appID'
+ *   },
+ * });
+ */
+export function useNftCollectionsQueryQuery(baseOptions: Apollo.QueryHookOptions<NftCollectionsQueryQuery, NftCollectionsQueryQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<NftCollectionsQueryQuery, NftCollectionsQueryQueryVariables>(NftCollectionsQueryDocument, options);
+      }
+export function useNftCollectionsQueryLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<NftCollectionsQueryQuery, NftCollectionsQueryQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<NftCollectionsQueryQuery, NftCollectionsQueryQueryVariables>(NftCollectionsQueryDocument, options);
+        }
+export type NftCollectionsQueryQueryHookResult = ReturnType<typeof useNftCollectionsQueryQuery>;
+export type NftCollectionsQueryLazyQueryHookResult = ReturnType<typeof useNftCollectionsQueryLazyQuery>;
+export type NftCollectionsQueryQueryResult = Apollo.QueryResult<NftCollectionsQueryQuery, NftCollectionsQueryQueryVariables>;
---
+++ portal/src/graphql/portal/query/nftContractMetadataQuery.generated.ts
@@ -0,0 +1,54 @@
+import * as Types from '../globalTypes.generated';
+
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
+export type NftContractMetadataQueryQueryVariables = Types.Exact<{
+  appID: Types.Scalars['ID'];
+  contractID: Types.Scalars['String'];
+}>;
+
+
+export type NftContractMetadataQueryQuery = { __typename?: 'Query', nftContractMetadata?: { __typename?: 'NFTContractMetadata', address: string, name: string, symbol: string, totalSupply: string, tokenType: string } | null };
+
+
+export const NftContractMetadataQueryDocument = gql`
+    query nftContractMetadataQuery($appID: ID!, $contractID: String!) {
+  nftContractMetadata(appID: $appID, contractID: $contractID) {
+    address
+    name
+    symbol
+    totalSupply
+    tokenType
+  }
+}
+    `;
+
+/**
+ * __useNftContractMetadataQueryQuery__
+ *
+ * To run a query within a React component, call `useNftContractMetadataQueryQuery` and pass it any options that fit your needs.
+ * When your component renders, `useNftContractMetadataQueryQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useNftContractMetadataQueryQuery({
+ *   variables: {
+ *      appID: // value for 'appID'
+ *      contractID: // value for 'contractID'
+ *   },
+ * });
+ */
+export function useNftContractMetadataQueryQuery(baseOptions: Apollo.QueryHookOptions<NftContractMetadataQueryQuery, NftContractMetadataQueryQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<NftContractMetadataQueryQuery, NftContractMetadataQueryQueryVariables>(NftContractMetadataQueryDocument, options);
+      }
+export function useNftContractMetadataQueryLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<NftContractMetadataQueryQuery, NftContractMetadataQueryQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<NftContractMetadataQueryQuery, NftContractMetadataQueryQueryVariables>(NftContractMetadataQueryDocument, options);
+        }
+export type NftContractMetadataQueryQueryHookResult = ReturnType<typeof useNftContractMetadataQueryQuery>;
+export type NftContractMetadataQueryLazyQueryHookResult = ReturnType<typeof useNftContractMetadataQueryLazyQuery>;
+export type NftContractMetadataQueryQueryResult = Apollo.QueryResult<NftContractMetadataQueryQuery, NftContractMetadataQueryQueryVariables>;
