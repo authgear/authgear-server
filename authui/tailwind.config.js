c24eba33479c93d86cef12f86f1827f95963b9fe
Louis Chan <louischan@oursky.com>
2022-09-02 11:32:33 +0800

    Safelist OAuth provider type classnames
    ref #2395

--- authui/tailwind.config.js
+++ authui/tailwind.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   content: ["../resources/authgear/templates/en/web/**/*.html"],
+  // We do not actually mention the oauth provider types in the HTML.
+  // So we have to safelist them here to ensure we generate the CSS.
+  safelist: [
+    "apple",
+    "google",
+    "facebook",
+    "github",
+    "linkedin",
+    "azureadv2",
+    "azureadb2c",
+    "adfs",
+    "wechat",
+  ],
   darkMode: "class",
   theme: {
     extend: {
