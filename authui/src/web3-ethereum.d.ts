41d8e82815e7254a3011d8b1a7a547aa02d636fa
Rico <hochiw@oursky.com>
2022-09-06 17:31:13 +0800

    Handle performing SIWE in front end
    refs #2310

--- authui/src/web3-ethereum.d.ts
+++ authui/src/web3-ethereum.d.ts
@@ -1,3 +1,13 @@
+declare interface SIWENonce {
+  nonce: string;
+  expireAt: Date;
+}
+
+declare interface SIWEVerifiedData {
+  message: string;
+  signature: string;
+  encodedPubKey: string;
+}
 // Define ethereum interface in window
 declare interface Window {
   ethereum?: any;
