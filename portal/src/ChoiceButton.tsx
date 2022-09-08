42e5935244b689f40aa91b9ccbbc95f47e757546
Louis Chan <louischan@oursky.com>
2022-09-08 17:29:00 +0800

    Add ChoiceButton

---
+++ portal/src/ChoiceButton.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode, ReactElement } from "react";
+import { CompoundButton, IButtonStyles, useTheme } from "@fluentui/react";
+
+export interface ChoiceButtonProps {
+  className?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  text?: ReactNode;
+  secondaryText?: ReactNode;
+}
+
+export default function ChoiceButton(props: ChoiceButtonProps): ReactElement {
+  const originalTheme = useTheme();
+  const styles: IButtonStyles = {
+    rootChecked: {
+      borderColor: originalTheme.palette.themePrimary,
+      backgroundColor: originalTheme.semanticColors.buttonBackground,
+    },
+  };
+  // @ts-expect-error
+  return <CompoundButton {...props} toggle={true} styles={styles} />;
+}
