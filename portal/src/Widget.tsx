861b96c25141ebc57cbbaeea0c1187873ce50563
Louis Chan <louischan@oursky.com>
2022-09-13 16:29:57 +0800

    Augment Widget to be extendable

--- portal/src/Widget.tsx
+++ portal/src/Widget.tsx
@@ -1,23 +1,80 @@
-import React from "react";
+import React, {
+  useState,
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useMemo,
+} from "react";
+import { IconButton, DefaultEffects } from "@fluentui/react";
 import cn from "classnames";
-import { DefaultEffects } from "@fluentui/react";
+
 import styles from "./Widget.module.css";
 
-export interface WidgetProps {
+interface WidgetProps {
   className?: string;
+  extendable?: boolean;
   children?: React.ReactNode;
 }
 
+const ICON_PROPS = {
+  iconName: "ChevronDown",
+};
+
+// 16px top padding + 32px icon button height + 16px bottom padding
+const COLLAPSED_HEIGHT = 64;
+
 const Widget: React.VFC<WidgetProps> = function Widget(props: WidgetProps) {
-  const { className, children } = props;
+  const { className, extendable, children } = props;
+  const [extended, setExtended] = useState(extendable ?? true);
+  const [measuredHeight, setMeasureHeight] = useState<number | null>(null);
+  const divRef = useRef<HTMLDivElement | null>(null);
+
+  const onClick = useCallback(() => {
+    setExtended((prev) => {
+      return !prev;
+    });
+  }, []);
+
+  const buttonStyles = useMemo(() => {
+    return {
+      icon: {
+        // https://tailwindcss.com/docs/transition-property
+        transitionProperty: "transform",
+        transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+        transitionDuration: "150ms",
+        transform: extended ? "rotate(-180deg)" : undefined,
+      },
+    };
+  }, [extended]);
+
+  useLayoutEffect(() => {
+    if (divRef.current instanceof HTMLDivElement) {
+      setMeasureHeight(divRef.current.clientHeight);
+    }
+  }, []);
+
   return (
     <div
+      ref={divRef}
       className={cn(className, styles.root)}
       style={{
         boxShadow: DefaultEffects.elevation4,
+        maxHeight:
+          measuredHeight == null
+            ? undefined
+            : extended
+            ? `${measuredHeight}px`
+            : `${COLLAPSED_HEIGHT}px`,
       }}
     >
       {children}
+      <IconButton
+        styles={buttonStyles}
+        className={cn(styles.button, extendable == null ? styles.hide : "")}
+        onClick={onClick}
+        disabled={extendable ?? false}
+        iconProps={ICON_PROPS}
+      />
     </div>
   );
 };
