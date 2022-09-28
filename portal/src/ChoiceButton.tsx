30ef1c1d790a51396c2e4834f46b6b96e632fe76
Louis Chan <louischan@oursky.com>
2022-09-28 11:54:38 +0800

    Support title only in ChoiceButton

--- portal/src/ChoiceButton.tsx
+++ portal/src/ChoiceButton.tsx
@@ -20,6 +20,9 @@ export default function ChoiceButton(props: ChoiceButtonProps): ReactElement {
   const styles: IButtonStyles = {
     root: {
       maxWidth: "auto",
+      // Remove minHeight so that ChoiceButton looks nice if it does not have secondaryText,
+      // otherwise, it is too tall.
+      minHeight: "0",
     },
     rootChecked: {
       borderColor: originalTheme.palette.themePrimary,
@@ -27,6 +30,10 @@ export default function ChoiceButton(props: ChoiceButtonProps): ReactElement {
     },
     description: {
       color: "inherit",
+    },
+    label: {
+      // Make the label center aligned when there is no secondaryText.
+      margin: props.secondaryText == null ? "0" : undefined,
     },
     // When ChoiceButton is taller than its intrinsic height,
     // make sure the content is still center aligned vertically.
