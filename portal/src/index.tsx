51c8ace291ff88ba37473273ac59ec6703cb5057
Louis Chan <louischan@oursky.com>
2022-10-11 18:27:19 +0800

    Turn off auto freeze
    ref #2561

--- portal/src/index.tsx
+++ portal/src/index.tsx
@@ -12,10 +12,17 @@ import {
   LineElement,
   ArcElement,
 } from "chart.js";
+import { setAutoFreeze } from "immer";
 
 import ReactApp from "./ReactApp";
 
 initializeIcons();
+// We sometimes use immer in forms.
+// It seems that frozen object is problematic if we use
+// produce more than once.
+// Interestingly, this bug only happens in production build :(
+// https://github.com/authgear/authgear-server/issues/2561
+setAutoFreeze(false);
 
 // ChartJS registration for Bar chart in the AnalyticsActivityWidget
 ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
