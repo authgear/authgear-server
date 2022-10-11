cd1486a24e2b47dfa1a8c8f3c257830d277321f0
Rico <hochiw@oursky.com>
2022-10-11 18:48:15 +0800

    Introduce bignumberjs and hex util
    refs #2530

--- portal/src/util/hex.ts
+++ portal/src/util/hex.ts
@@ -1,6 +1,28 @@
+import BigNumber from "bignumber.js";
+
 export function truncateAddress(address: string): string {
   const delimiter = "...";
 
   // We only show the first 6 and last 4 characters of the address
   return address.slice(0, 6) + delimiter + address.slice(address.length - 4);
+}
+
+export function parseHexstring(hex: string): string {
+  const bn = BigNumber(hex);
+
+  if (bn.isNaN()) {
+    return "";
+  }
+
+  return bn.toString(10);
+}
+
+export function convertToHexstring(dec: string): string {
+  const bn = BigNumber(dec);
+
+  if (bn.isNaN()) {
+    return "";
+  }
+
+  return "0x" + bn.toString(16);
 }
