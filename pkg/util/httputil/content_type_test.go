7616b40745964dcebc2f76ed73caccf10a482ff8
Louis Chan <louischan@oursky.com>
2022-10-11 10:36:43 +0800

    Allow content-type to have valid charset specified
    ref #2558

--- pkg/util/httputil/content_type_test.go
+++ pkg/util/httputil/content_type_test.go
@@ -48,6 +48,44 @@ func TestCheckContentType(t *testing.T) {
 		So(w.Body.String(), ShouldEqual, "good")
 	})
 
+	Convey("CheckContentType good content type with charset=UTF-8", t, func() {
+		originalHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte("good"))
+		})
+
+		middleware := httputil.CheckContentType([]string{"application/json"})
+
+		handler := middleware.Handle(originalHandler)
+
+		r, _ := http.NewRequest("POST", "/", strings.NewReader(`{}`))
+		r.Header.Set("Content-Type", "application/json; charset=UTF-8")
+		w := httptest.NewRecorder()
+
+		handler.ServeHTTP(w, r)
+
+		So(w.Result().StatusCode, ShouldEqual, http.StatusOK)
+		So(w.Body.String(), ShouldEqual, "good")
+	})
+
+	Convey("CheckContentType good content type with charset=utf-8", t, func() {
+		originalHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte("good"))
+		})
+
+		middleware := httputil.CheckContentType([]string{"application/json"})
+
+		handler := middleware.Handle(originalHandler)
+
+		r, _ := http.NewRequest("POST", "/", strings.NewReader(`{}`))
+		r.Header.Set("Content-Type", "application/json; charset=utf-8")
+		w := httptest.NewRecorder()
+
+		handler.ServeHTTP(w, r)
+
+		So(w.Result().StatusCode, ShouldEqual, http.StatusOK)
+		So(w.Body.String(), ShouldEqual, "good")
+	})
+
 	Convey("CheckContentType ignores requests without body", t, func() {
 		originalHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			_, _ = w.Write([]byte("good"))
