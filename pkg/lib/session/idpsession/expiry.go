9d8115b8c142e4624729160a72192962dd2cecb3
Carmen <carmen@oursky.com>
2022-11-09 12:15:00 +0000

    Move CheckSessionExpired to idp session provider

--- pkg/lib/session/idpsession/expiry.go
+++ pkg/lib/session/idpsession/expiry.go
@@ -16,21 +16,3 @@ func computeSessionStorageExpiry(session *IDPSession, cfg *config.SessionConfig)
 	}
 	return
 }
-
-func checkSessionExpired(session *IDPSession, now time.Time, cfg *config.SessionConfig) (expired bool) {
-	sessionExpiry := session.CreatedAt.Add(cfg.Lifetime.Duration())
-	if now.After(sessionExpiry) {
-		expired = true
-		return
-	}
-
-	if *cfg.IdleTimeoutEnabled {
-		sessionIdleExpiry := session.AccessInfo.LastAccess.Timestamp.Add(cfg.IdleTimeout.Duration())
-		if now.After(sessionIdleExpiry) {
-			expired = true
-			return
-		}
-	}
-
-	return false
-}
