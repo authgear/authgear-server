6856134568a52c29524192feb756ac1bcff0dc47
Rubikium <30288416+Rubikium@users.noreply.github.com>
2023-01-16 21:25:34 +0800

    Attach bucket name as error info

--- pkg/lib/ratelimit/error.go
+++ pkg/lib/ratelimit/error.go
@@ -1,9 +1,22 @@
 package ratelimit
 
-import "github.com/authgear/authgear-server/pkg/api/apierrors"
+import (
+	"github.com/authgear/authgear-server/pkg/api/apierrors"
+)
 
-var ErrTooManyRequests = apierrors.TooManyRequest.WithReason("RateLimited").
-	New("request rate limited")
+var errTooManyRequestsMsg = "request rate limited"
+var RateLimited = apierrors.TooManyRequest.WithReason("RateLimited")
 
+var ErrTooManyRequests = RateLimited.New(errTooManyRequestsMsg)
 var ErrUsageLimitExceeded = apierrors.ServiceUnavailable.WithReason("UsageLimitExceeded").
 	New("usage limit exceeded")
+
+func ErrTooManyRequestsFrom(bucket Bucket) error {
+	if bucket.Name == "" {
+		return ErrTooManyRequests
+	} else {
+		return RateLimited.NewWithInfo(errTooManyRequestsMsg, apierrors.Details{
+			"bucket_name": bucket.Name,
+		})
+	}
+}
