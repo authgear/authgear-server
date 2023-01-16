b22458cbd3f204a64295c11b5012612bbedea673
Newman Chow <newman@oursky.com>
2023-01-16 14:24:38 +0800

    Extract verify magic link to endpoints lib
    ref: #2748

--- pkg/lib/authn/otp/sender.go
+++ pkg/lib/authn/otp/sender.go
@@ -23,6 +23,7 @@ type SendOptions struct {
 
 type EndpointsProvider interface {
 	BaseURL() *url.URL
+	MagicLinkVerificationEndpointURL() *url.URL
 }
 
 type TranslationService interface {
@@ -75,9 +76,7 @@ func (s *MessageSender) SendEmail(email string, opts SendOptions) error {
 	data.Email = email
 
 	if opts.OTPMode == OTPModeMagicLink {
-		url := s.Endpoints.BaseURL()
-		url = url.JoinPath("/flows/verify_magic_link")
-
+		url := s.Endpoints.MagicLinkVerificationEndpointURL()
 		query := url.Query()
 		query.Set("token", data.Code)
 		query.Set("target", data.Email)
