1a9a3c3fd6378e850d1b17c8fcdae836dc421200
Louis Chan <louischan@oursky.com>
2023-02-08 18:42:06 +0800

    Add IntentAuthenticate to split between IntentLogin and IntentSignup

---
+++ pkg/latte/intent_signup.go
@@ -0,0 +1,47 @@
+package latte
+
+import (
+	"context"
+
+	"github.com/authgear/authgear-server/pkg/lib/workflow"
+	"github.com/authgear/authgear-server/pkg/util/validation"
+)
+
+func init() {
+	workflow.RegisterIntent(&IntentSignup{})
+}
+
+var IntentSignupSchema = validation.NewSimpleSchema(`
+	{
+		"type": "object",
+		"additionalProperties": false
+	}
+`)
+
+type IntentSignup struct{}
+
+func (*IntentSignup) Kind() string {
+	return "latte.IntentSignup"
+}
+
+func (*IntentSignup) JSONSchema() *validation.SimpleSchema {
+	return IntentSignupSchema
+}
+
+func (*IntentSignup) CanReactTo(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) ([]workflow.Input, error) {
+	// TODO(workflow): signup
+	return nil, workflow.ErrEOF
+}
+
+func (*IntentSignup) ReactTo(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow, input workflow.Input) (*workflow.Node, error) {
+	return nil, workflow.ErrIncompatibleInput
+}
+
+func (*IntentSignup) GetEffects(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) (effs []workflow.Effect, err error) {
+	// TODO(workflow): Fire user.created.
+	return nil, nil
+}
+
+func (*IntentSignup) OutputData(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) (interface{}, error) {
+	return nil, nil
+}
--- pkg/lib/workflow/dependencies.go
+++ pkg/lib/workflow/dependencies.go
@@ -1,3 +1,13 @@
 package workflow
 
-type Dependencies struct{}
+import (
+	"github.com/authgear/authgear-server/pkg/lib/authn/identity"
+)
+
+type IdentityService interface {
+	SearchBySpec(spec *identity.Spec) (exactMatch *identity.Info, otherMatches []*identity.Info, err error)
+}
+
+type Dependencies struct {
+	Identities IdentityService
+}
