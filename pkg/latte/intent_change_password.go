40b00f824812c6be3cca770de68c73ef0e3bd855
Rubikium <30288416+Rubikium@users.noreply.github.com>
2023-02-17 19:20:23 +0800

    Get userID from session

--- pkg/latte/intent_change_password.go
+++ pkg/latte/intent_change_password.go
@@ -3,9 +3,9 @@ package latte
 import (
 	"context"
 
-	"github.com/authgear/authgear-server/pkg/api/model"
+	"github.com/authgear/authgear-server/pkg/api/apierrors"
 	"github.com/authgear/authgear-server/pkg/lib/authn/authenticator"
-	"github.com/authgear/authgear-server/pkg/lib/authn/identity"
+	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/lib/workflow"
 	"github.com/authgear/authgear-server/pkg/util/validation"
 )
@@ -14,7 +14,12 @@ func init() {
 	workflow.RegisterPublicIntent(&IntentChangePassword{})
 }
 
-var IntentChangePasswordSchema = validation.NewSimpleSchema(`{}`)
+var IntentChangePasswordSchema = validation.NewSimpleSchema(`
+	{
+		"type": "object",
+		"additionalProperties": false
+	}
+`)
 
 type IntentChangePassword struct {
 }
@@ -28,41 +33,23 @@ func (*IntentChangePassword) JSONSchema() *validation.SimpleSchema {
 }
 
 func (*IntentChangePassword) CanReactTo(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) ([]workflow.Input, error) {
-	if len(w.Nodes) == 0 {
-		return []workflow.Input{
-			&InputTakeLoginID{},
-		}, nil
+	switch len(w.Nodes) {
+	case 0:
+		return nil, nil
 	}
 	return nil, workflow.ErrEOF
 }
 
 func (i *IntentChangePassword) ReactTo(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow, input workflow.Input) (*workflow.Node, error) {
-	var inputTakeLoginID inputTakeLoginID
-
-	switch {
-	case workflow.AsInput(input, &inputTakeLoginID):
-		loginID := inputTakeLoginID.GetLoginID()
-		spec := &identity.Spec{
-			Type: model.IdentityTypeLoginID,
-			LoginID: &identity.LoginIDSpec{
-				Value: loginID,
-			},
-		}
-		identity, _, err := deps.Identities.SearchBySpec(spec)
-		if err != nil {
-			return nil, err
-		}
-		if identity == nil {
-			return nil, workflow.ErrIncompatibleInput
-		}
-
-		return workflow.NewNodeSimple(&NodeChangePassword{
-			UserID:            identity.LoginID.UserID,
-			AuthenticatorKind: authenticator.KindPrimary,
-		}), nil
-	default:
-		return nil, workflow.ErrIncompatibleInput
+	userID := session.GetUserID(ctx)
+	if userID == nil {
+		return nil, apierrors.NewUnauthorized("authentication required")
 	}
+
+	return workflow.NewNodeSimple(&NodeChangePassword{
+		UserID:            *userID,
+		AuthenticatorKind: authenticator.KindPrimary,
+	}), nil
 }
 
 func (i *IntentChangePassword) GetEffects(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) (effs []workflow.Effect, err error) {
