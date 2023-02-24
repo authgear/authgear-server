284c29994ed1a52bfc2d71456c966dea8160ee93
Rubikium <30288416+Rubikium@users.noreply.github.com>
2023-02-24 16:12:18 +0800

    Add missing subworkflows
    - Populate standard attributes
    - Verify the identity
    - Create the OOB OTP authenticator

--- pkg/latte/intent_change_email.go
+++ pkg/latte/intent_change_email.go
@@ -2,6 +2,7 @@ package latte
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/authgear/authgear-server/pkg/api"
 	"github.com/authgear/authgear-server/pkg/api/apierrors"
@@ -40,6 +41,12 @@ func (*IntentChangeEmail) CanReactTo(ctx context.Context, deps *workflow.Depende
 		return []workflow.Input{
 			&InputTakeCurrentLoginID{},
 		}, nil
+	case 1:
+		return nil, nil
+	case 2:
+		return nil, nil
+	case 3:
+		return nil, nil
 	}
 	return nil, workflow.ErrEOF
 }
@@ -50,32 +57,60 @@ func (i *IntentChangeEmail) ReactTo(ctx context.Context, deps *workflow.Dependen
 		return nil, apierrors.NewUnauthorized("authentication required")
 	}
 
-	var inputTakeCurrentLoginID inputTakeCurrentLoginID
-	switch {
-	case workflow.AsInput(input, &inputTakeCurrentLoginID):
-		loginID := inputTakeCurrentLoginID.GetCurrentLoginID()
-		spec := &identity.Spec{
-			Type: model.IdentityTypeLoginID,
-			LoginID: &identity.LoginIDSpec{
-				Type:  model.LoginIDKeyTypeEmail,
-				Key:   string(model.LoginIDKeyTypeEmail),
-				Value: loginID,
-			},
-		}
-		exactMatch, _, err := deps.Identities.SearchBySpec(spec)
-		if err != nil {
-			return nil, err
-		}
-		if exactMatch == nil || exactMatch.UserID != *userID {
-			return nil, api.ErrIdentityNotFound
-		}
+	switch len(w.Nodes) {
+	case 0:
+		var inputTakeCurrentLoginID inputTakeCurrentLoginID
+		switch {
+		case workflow.AsInput(input, &inputTakeCurrentLoginID):
+			loginID := inputTakeCurrentLoginID.GetCurrentLoginID()
+			spec := &identity.Spec{
+				Type: model.IdentityTypeLoginID,
+				LoginID: &identity.LoginIDSpec{
+					Type:  model.LoginIDKeyTypeEmail,
+					Key:   string(model.LoginIDKeyTypeEmail),
+					Value: loginID,
+				},
+			}
+			exactMatch, _, err := deps.Identities.SearchBySpec(spec)
+			if err != nil {
+				return nil, err
+			}
+			if exactMatch == nil || exactMatch.UserID != *userID {
+				return nil, api.ErrIdentityNotFound
+			}
 
-		return workflow.NewNodeSimple(&NodeChangeEmail{
-			UserID:               *userID,
-			IdentityBeforeUpdate: exactMatch,
+			return workflow.NewNodeSimple(&NodeChangeEmail{
+				UserID:               *userID,
+				IdentityBeforeUpdate: exactMatch,
+			}), nil
+		}
+	case 1:
+		iden := i.newIdentityInfo(w)
+		return workflow.NewNodeSimple(&NodePopulateStandardAttributes{
+			Identity: iden,
+		}), nil
+	case 2:
+		iden := i.newIdentityInfo(w)
+		return workflow.NewSubWorkflow(&IntentVerifyIdentity{
+			Identity:     iden,
+			IsFromSignUp: false,
+		}), nil
+	case 3:
+		iden := i.newIdentityInfo(w)
+		return workflow.NewSubWorkflow(&IntentCreateOOBOTPAuthenticatorForLoginID{
+			Identity: iden,
 		}), nil
 	}
 	return nil, workflow.ErrIncompatibleInput
+}
+
+func (i *IntentChangeEmail) newIdentityInfo(w *workflow.Workflow) *identity.Info {
+	node, ok := workflow.FindSingleNode[*NodeDoUpdateIdentity](w)
+	if !ok {
+		panic(fmt.Errorf("workflow: expected NodeDoUpdateIdentity"))
+	}
+
+	return node.IdentityAfterUpdate
 }
 
 func (i *IntentChangeEmail) GetEffects(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) (effs []workflow.Effect, err error) {
