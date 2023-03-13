3db719b8f31b6dfd9f8d44d56a54d8a30855606c
Louis Chan <louischan@oursky.com>
2023-03-13 18:48:47 +0900

    Do not fallback to index.html for name-hashed file
    ref #2990

--- pkg/util/httputil/file_server.go
+++ pkg/util/httputil/file_server.go
@@ -5,7 +5,40 @@ import (
 	"io/fs"
 	"net/http"
 	"path"
+	"regexp"
+	"strings"
 )
+
+var hashRegexp = regexp.MustCompile(`^[0-9a-fA-F]{8,}$`)
+
+func IsNameHashed(p string) bool {
+	// In general, a name-hashed filename looks like
+	// name.hash.ext0[.ext1][.ext2]...
+	// where
+	//   name is non-empty. That is, it is not a hidden file.
+	//   ext0, ext1, ext2 are less than 8 characters
+	//   hash is 8 or more hex characters.
+	base := path.Base(p)
+	parts := strings.Split(base, ".")
+	// So len(parts) must be at least 3.
+	if len(parts) < 3 {
+		return false
+	}
+	// name must be non-empty.
+	if parts[0] == "" {
+		return false
+	}
+	// Start from the end of the slice to find hash
+	// i >= 1 because name and hash must present at the same time.
+	for i := len(parts) - 1; i >= 1; i-- {
+		part := parts[i]
+		if hashRegexp.MatchString(part) {
+			return true
+		}
+	}
+
+	return false
+}
 
 // FileServer is a specialized version of http.FileServer
 // that assumes files rooted at FileSystem are name-hashed.
@@ -16,15 +49,19 @@ type FileServer struct {
 }
 
 func (s *FileServer) writeError(w http.ResponseWriter, err error) {
+	// http.Error is NOT used intentionally to avoid returning a text/plain response.
+	// The desired response is WITHOUT content-type, and with content-length: 0
+	w.Header().Del("Content-Type")
+	w.Header().Set("Content-Length", "0")
 	if errors.Is(err, fs.ErrNotExist) {
-		http.Error(w, "404 page not found", http.StatusNotFound)
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 	if errors.Is(err, fs.ErrPermission) {
-		http.Error(w, "403 Forbidden", http.StatusForbidden)
+		w.WriteHeader(http.StatusForbidden)
 		return
 	}
-	http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+	w.WriteHeader(http.StatusInternalServerError)
 	return
 }
 
@@ -46,15 +83,21 @@ func (s *FileServer) open(name string) (http.File, fs.FileInfo, error) {
 	return file, stat, nil
 }
 
-func (s *FileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *FileServer) serveNameHashed(w http.ResponseWriter, r *http.Request) {
+	file, stat, err := s.open(r.URL.Path)
+	if err != nil {
+		s.writeError(w, err)
+		return
+	}
+	defer file.Close()
+
+	w.Header().Set("Cache-Control", "public, max-age=604800")
+	http.ServeContent(w, r, stat.Name(), stat.ModTime(), file)
+}
+
+func (s *FileServer) serveOther(w http.ResponseWriter, r *http.Request) {
 	indexHTML := "/index.html"
 
-	// The treatment of path is different from http.FileServer.
-	// We always normalize the path before we pass it to FileSystem.
-	r.URL.Path = path.Clean("/" + r.URL.Path)
-
-	// Rewrite path to index.html to handle HTML5 history routing convention.
-	// err is used to determine whether we can cache-control header.
 	file, stat, err := s.open(r.URL.Path)
 	if s.FallbackToIndexHTML && errors.Is(err, fs.ErrNotExist) {
 		r.URL.Path = indexHTML
@@ -67,15 +110,29 @@ func (s *FileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	// We only write cache-control header only when there is no error.
-	isIndexHTML := r.URL.Path == indexHTML
-	if isIndexHTML {
-		// Force the browser to validate index.html
-		w.Header().Set("Cache-Control", "no-cache")
-	} else {
-		// 7 Days
-		w.Header().Set("Cache-Control", "public, max-age=604800")
-	}
-
 	http.ServeContent(w, r, stat.Name(), stat.ModTime(), file)
+}
+
+func (s *FileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// By default, all response requires validation.
+	// So a 404 response also requires validation.
+	w.Header().Set("Cache-Control", "no-cache")
+
+	// The treatment of path is different from http.FileServer.
+	// We always normalize the path before we pass it to FileSystem.
+	r.URL.Path = path.Clean("/" + r.URL.Path)
+
+	// First of all we need to identity whether the path
+	// seems like fetching a name-hashed file.
+	//
+	// If the request fetches a name-hashed file,
+	// we return 404 for not found, 200 cache-control: public for found.
+	//
+	// If the request fetches a non-name-hashed file,
+	// we fallback to index.html for not found.
+	if IsNameHashed(r.URL.Path) {
+		s.serveNameHashed(w, r)
+	} else {
+		s.serveOther(w, r)
+	}
 }
--- pkg/util/httputil/file_server_test.go
+++ pkg/util/httputil/file_server_test.go
@@ -1,12 +1,42 @@
 package httputil
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
+
+func TestIsNameHashed(t *testing.T) {
+	Convey("IsNameHashed", t, func() {
+		test := func(p string, expected bool) {
+			Convey(fmt.Sprintf("%v", p), func() {
+				actual := IsNameHashed(p)
+				So(actual, ShouldEqual, expected)
+			})
+		}
+
+		test("", false)
+		test("/", false)
+		test("/a", false)
+		test("/a.js", false)
+		test("/a.js.map", false)
+
+		test("/a.deadbeef.js", true)
+		test("/a.deadbeef.js.map", true)
+
+		test("/.deadbeef.js", false)
+		test("/.deadbeef.js.map", false)
+
+		test("/nested/a.deadbeef.js", true)
+		test("/nested/a.deadbeef.js.map", true)
+
+		test("/a.0123456.js", false)
+		test("/a.0123456.js.map", false)
+	})
+}
 
 func TestFileServer(t *testing.T) {
 	Convey("FileServer", t, func() {
@@ -18,7 +48,7 @@ func TestFileServer(t *testing.T) {
 				FallbackToIndexHTML: false,
 			}
 
-			r, _ := http.NewRequest("GET", "/a.js", nil)
+			r, _ := http.NewRequest("GET", "/a.deadbeef.js", nil)
 			w := httptest.NewRecorder()
 			h.ServeHTTP(w, r)
 			So(w.Code, ShouldEqual, 200)
@@ -30,7 +60,9 @@ func TestFileServer(t *testing.T) {
 			w = httptest.NewRecorder()
 			h.ServeHTTP(w, r)
 			So(w.Code, ShouldEqual, 404)
-			So(w.Result().Header.Get("cache-control"), ShouldEqual, "")
+			So(w.Result().Header.Get("cache-control"), ShouldEqual, "no-cache")
+			So(w.Result().Header.Get("content-type"), ShouldEqual, "")
+			So(w.Result().Header.Get("content-length"), ShouldEqual, "0")
 		})
 
 		Convey("fallback to index.html", func() {
@@ -41,13 +73,21 @@ func TestFileServer(t *testing.T) {
 				FallbackToIndexHTML: true,
 			}
 
-			r, _ := http.NewRequest("GET", "/a.js", nil)
+			r, _ := http.NewRequest("GET", "/a.deadbeef.js", nil)
 			w := httptest.NewRecorder()
 			h.ServeHTTP(w, r)
 			So(w.Code, ShouldEqual, 200)
 			// Depending on mime.types on the system, the result could be application/javascript or text/javascript.
 			So(w.Result().Header.Get("content-type"), ShouldContainSubstring, "javascript")
 			So(w.Result().Header.Get("cache-control"), ShouldEqual, "public, max-age=604800")
+
+			r, _ = http.NewRequest("GET", "/b.deadbeef.js", nil)
+			w = httptest.NewRecorder()
+			h.ServeHTTP(w, r)
+			So(w.Code, ShouldEqual, 404)
+			So(w.Result().Header.Get("cache-control"), ShouldEqual, "no-cache")
+			So(w.Result().Header.Get("content-type"), ShouldEqual, "")
+			So(w.Result().Header.Get("content-length"), ShouldEqual, "0")
 
 			r, _ = http.NewRequest("GET", "/", nil)
 			w = httptest.NewRecorder()
