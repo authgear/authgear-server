071eced4b6dc17eae95e8b7d86b4cf3b79bd5167
Tung Wu <tung@oursky.com>
2023-04-17 16:07:44 +0800

    Read audit_context from admin api jwt

---
+++ pkg/lib/admin/authz/audit.go
@@ -0,0 +1,29 @@
+package authz
+
+import "context"
+
+type auditContextKeyType struct{}
+
+var auditContextKey = auditContextKeyType{}
+
+type T struct {
+	AuditContext interface{}
+}
+
+func WithAdminAuthzAudit(ctx context.Context, content interface{}) context.Context {
+	v, ok := ctx.Value(auditContextKey).(*T)
+	if ok {
+		v.AuditContext = content
+		return ctx
+	}
+	t := &T{AuditContext: content}
+	return context.WithValue(ctx, auditContextKey, t)
+}
+
+func GetAdminAuthzAudit(ctx context.Context) interface{} {
+	v, ok := ctx.Value(auditContextKey).(*T)
+	if ok {
+		return v.AuditContext
+	}
+	return nil
+}
