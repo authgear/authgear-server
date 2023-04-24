2bcb4dd0d9483c02276557023aeb3461c13c96df
Tung Wu <tung@oursky.com>
2023-04-24 18:10:48 +0800

    Change index order of audit log

--- migrations/audit/20230420194938-add_audit_log_index.sql
+++ migrations/audit/20230420194938-add_audit_log_index.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 CREATE INDEX _audit_log_idx_app_id_activity_type_created_at
-  ON _audit_log (app_id, activity_type, created_at);
+  ON _audit_log (app_id, activity_type, created_at DESC);
 
 -- +migrate Down
 DROP INDEX _audit_log_idx_app_id_activity_type_created_at;
