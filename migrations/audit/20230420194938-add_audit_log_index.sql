9f6bffcec98273ec6d5f2364eabb548cb4ae4086
Tung Wu <tung@oursky.com>
2023-04-24 18:05:21 +0800

    Fix index name mismatch in migration downgrade

--- migrations/audit/20230420194938-add_audit_log_index.sql
+++ migrations/audit/20230420194938-add_audit_log_index.sql
@@ -3,4 +3,4 @@ CREATE INDEX _audit_log_idx_app_id_activity_type_created_at
   ON _audit_log (app_id, activity_type, created_at);
 
 -- +migrate Down
-DROP INDEX _audit_log_idx_app_id_activity_type_created_at_brin;
+DROP INDEX _audit_log_idx_app_id_activity_type_created_at;
