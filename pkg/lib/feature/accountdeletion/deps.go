393f2df91ff0a1740af726469a77fd92f228df70
Louis Chan <louischan@oursky.com>
2023-04-28 11:27:43 +0900

    Use wire to write factory

--- pkg/lib/feature/accountdeletion/deps.go
+++ pkg/lib/feature/accountdeletion/deps.go
@@ -1,9 +1,15 @@
 package accountdeletion
 
 import (
+	"context"
+
 	"github.com/google/wire"
 
+	"github.com/authgear/authgear-server/pkg/lib/config"
+	"github.com/authgear/authgear-server/pkg/lib/infra/db"
+	"github.com/authgear/authgear-server/pkg/lib/infra/db/globaldb"
 	"github.com/authgear/authgear-server/pkg/util/backgroundjob"
+	"github.com/authgear/authgear-server/pkg/util/clock"
 	"github.com/authgear/authgear-server/pkg/util/log"
 )
 
@@ -14,7 +20,30 @@ func NewRunner(loggerFactory *log.Factory, runnableFactory backgroundjob.Runnabl
 	)
 }
 
+func NewRunnableFactory(
+	pool *db.Pool,
+	globalDBCredentials *config.GlobalDatabaseCredentialsEnvironmentConfig,
+	databaseCfg *config.DatabaseEnvironmentConfig,
+	logFactory *log.Factory,
+	clock clock.Clock,
+	appContextResolver AppContextResolver,
+	userServiceFactory UserServiceFactory,
+) backgroundjob.RunnableFactory {
+	factory := func(ctx context.Context) backgroundjob.Runnable {
+		return newRunnable(ctx, pool, globalDBCredentials, databaseCfg, logFactory, clock, appContextResolver, userServiceFactory)
+	}
+	return factory
+}
+
 var DependencySet = wire.NewSet(
 	NewRunnableFactory,
 	NewRunner,
+)
+
+var RunnableDependencySet = wire.NewSet(
+	globaldb.DependencySet,
+	wire.Struct(new(Store), "*"),
+	wire.Struct(new(Runnable), "*"),
+	NewRunnableLogger,
+	wire.Bind(new(backgroundjob.Runnable), new(*Runnable)),
 )
