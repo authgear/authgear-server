b8873c99f5d1d56bfef13aefbc812ecfacd2c752
Tung Wu <tung@oursky.com>
2023-05-30 13:40:52 +0800

    Validate template languages

--- pkg/lib/config/messaging_whatsapp.go
+++ pkg/lib/config/messaging_whatsapp.go
@@ -38,6 +38,30 @@ const (
 	WhatsappTemplateTypeAuthentication WhatsappTemplateType = "authentication"
 )
 
+// ref: https://developers.facebook.com/docs/whatsapp/api/messages/message-templates
+var _ = Schema.Add("WhatsappTemplateLanguage", `
+{
+	"type": "string",
+	"enum": [
+		"af", "sq", "ar", "az", "bn",
+		"bg", "ca", "zh_CN", "zh_HK", "zh_TW",
+		"hr", "cs", "da", "nl", "en",
+		"en_GB", "en_US", "et", "fil", "fi",
+		"fr", "ka", "de", "el", "gu",
+		"ha", "he", "hi", "hu", "id",
+		"ga", "it", "ja", "kn", "kk",
+		"rw_RW", "ko", "ky_KG", "lo", "lv",
+		"lt", "mk", "ms", "ml", "mr",
+		"nb", "fa", "pl", "pt_BR", "pt_PT",
+		"pa", "ro", "ru", "sr", "sk",
+		"sl", "es", "es_AR", "es_ES", "es_MX",
+		"sw", "sv", "ta", "te", "th",
+		"tr", "uk", "ur", "uz", "vi",
+		"zu"
+	]
+}
+`)
+
 var _ = Schema.Add("WhatsappTemplateType", `
 {
 	"type": "string",
@@ -61,7 +85,7 @@ var _ = Schema.Add("WhatsappTemplateConfig", `
 		"languages": {
 			"type": "array",
 			"items": {
-				"type": "string",
+				"$ref": "#/$defs/WhatsappTemplateLanguage",
 				"minLength": 1
 			}
 		}
