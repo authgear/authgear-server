68336db58b5eba6e13d406ef36957f59b7edfa90
Louis Chan <louischan@oursky.com>
2023-06-27 15:06:06 +0800

    Add JSON schema for WorkflowSignupFlow

--- pkg/lib/config/workflow.go
+++ pkg/lib/config/workflow.go
@@ -44,3 +44,147 @@ var _ = Schema.Add("WorkflowAuthenticationMethod", `
 	]
 }
 `)
+
+var _ = Schema.Add("WorkflowSignupFlow", `
+{
+	"type": "object",
+	"required": ["id", "steps"],
+	"properties": {
+		"id": { "$ref": "#/$defs/WorkflowObjectID" },
+		"steps": {
+			"type": "array",
+			"minItems": 1,
+			"items": { "$ref": "#/$defs/WorkflowSignupFlowStep" }
+		}
+	}
+}
+`)
+
+var _ = Schema.Add("WorkflowSignupFlowStep", `
+{
+	"type": "object",
+	"required": ["type"],
+	"properties": {
+		"id": { "$ref": "#/$defs/WorkflowObjectID" },
+		"type": {
+			"type": "string",
+			"enum": [
+				"identify",
+				"authenticate",
+				"verify",
+				"user_profile"
+			]
+		}
+	},
+	"allOf": [
+		{
+			"if": {
+				"required": ["type"],
+				"properties": {
+					"type": { "const": "identify" }
+				}
+			},
+			"then": {
+				"required": ["one_of"],
+				"properties": {
+					"one_of": {
+						"type": "array",
+						"items": { "$ref": "#/$defs/WorkflowSignupFlowIdentify" }
+					}
+				}
+			}
+		},
+		{
+			"if": {
+				"required": ["type"],
+				"properties": {
+					"type": { "const": "authenticate" }
+				}
+			},
+			"then": {
+				"required": ["one_of"],
+				"properties": {
+					"one_of": {
+						"type": "array",
+						"items": { "$ref": "#/$defs/WorkflowSignupFlowAuthenticate" }
+					}
+				}
+			}
+		},
+		{
+			"if": {
+				"required": ["type"],
+				"properties": {
+					"type": { "const": "verify" }
+				}
+			},
+			"then": {
+				"required": ["target_step"],
+				"properties": {
+					"target_step": { "$ref": "#/$defs/WorkflowObjectID" }
+				}
+			}
+		},
+		{
+			"if": {
+				"required": ["type"],
+				"properties": {
+					"type": { "const": "user_profile" }
+				}
+			},
+			"then": {
+				"required": ["user_profile"],
+				"properties": {
+					"user_profile": {
+						"type": "array",
+						"items": { "$ref": "#/$defs/WorkflowSignupFlowUserProfile" }
+					}
+				}
+			}
+		}
+	]
+}
+`)
+
+var _ = Schema.Add("WorkflowSignupFlowIdentify", `
+{
+	"type": "object",
+	"required": ["identification"],
+	"properties": {
+		"identification": { "$ref": "#/$defs/WorkflowIdentificationMethod" },
+		"steps": {
+			"type": "array",
+			"items": { "$ref": "#/$defs/WorkflowSignupFlowStep" }
+		}
+	}
+}
+`)
+
+var _ = Schema.Add("WorkflowSignupFlowAuthenticate", `
+{
+	"type": "object",
+	"required": ["authentication"],
+	"properties": {
+		"authentication": { "$ref": "#/$defs/WorkflowAuthenticationMethod" },
+		"target_step": { "$ref": "#/$defs/WorkflowObjectID" },
+		"steps": {
+			"type": "array",
+			"items": { "$ref": "#/$defs/WorkflowSignupFlowStep" }
+		}
+	}
+}
+`)
+
+var _ = Schema.Add("WorkflowSignupFlowUserProfile", `
+{
+	"type": "object",
+	"required": ["pointer", "required"],
+	"properties": {
+		"pointer": {
+			"type": "string",
+			"format": "json-pointer"
+		},
+		"required": { "type": "boolean" }
+	}
+}
+`)
