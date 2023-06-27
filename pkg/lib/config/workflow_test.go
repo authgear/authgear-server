691b836e45a8a94972dc2045db1d4c0d05f8eabe
Louis Chan <louischan@oursky.com>
2023-06-27 19:39:07 +0800

    Define structs for LoginFlow

--- pkg/lib/config/workflow_test.go
+++ pkg/lib/config/workflow_test.go
@@ -59,3 +59,51 @@ signup_flows:
 `)
 	})
 }
+
+func TestWorkflowLoginFlow(t *testing.T) {
+	Convey("WorkflowLoginFlow", t, func() {
+		test := func(inputYAML string) {
+			inputJSON, err := yaml.YAMLToJSON([]byte(inputYAML))
+			So(err, ShouldBeNil)
+
+			err = Schema.PartValidator("WorkflowConfig").Validate(bytes.NewReader(inputJSON))
+			So(err, ShouldBeNil)
+
+			var cfg WorkflowConfig
+			err = json.Unmarshal([]byte(inputJSON), &cfg)
+			So(err, ShouldBeNil)
+
+			var input interface{}
+			err = json.Unmarshal([]byte(inputJSON), &input)
+			So(err, ShouldBeNil)
+
+			encodedCfg, err := json.Marshal(cfg)
+			So(err, ShouldBeNil)
+
+			encodedInput, err := json.Marshal(input)
+			So(err, ShouldBeNil)
+
+			err = Schema.PartValidator("WorkflowConfig").Validate(bytes.NewReader(encodedCfg))
+			So(err, ShouldBeNil)
+
+			So(string(encodedInput), ShouldEqualJSON, string(encodedCfg))
+		}
+
+		test(`
+login_flows:
+- id: login_flow
+  steps:
+  - type: identify
+    id: my_step
+    one_of:
+    - identification: email
+  - type: authenticate
+    one_of:
+    - authentication: primary_password
+  - type: authenticate
+    optional: true
+    one_of:
+    - authentication: secondary_totp
+`)
+	})
+}
