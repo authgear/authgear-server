cd8179da70c1f137964a57aa3cb3b42244886c55
Louis Chan <louischan@oursky.com>
2023-07-03 19:28:23 +0800

    Create identity in SignupFlow

---
+++ pkg/lib/workflow/workflowconfig/node_do_create_identity.go
@@ -0,0 +1,45 @@
+package workflowconfig
+
+import (
+	"context"
+
+	"github.com/authgear/authgear-server/pkg/lib/authn/identity"
+	"github.com/authgear/authgear-server/pkg/lib/workflow"
+)
+
+func init() {
+	workflow.RegisterNode(&NodeDoCreateIdentity{})
+}
+
+type NodeDoCreateIdentity struct {
+	Identity *identity.Info `json:"identity,omitempty"`
+}
+
+func (n *NodeDoCreateIdentity) Kind() string {
+	return "workflowconfig.NodeDoCreateIdentity"
+}
+
+func (n *NodeDoCreateIdentity) GetEffects(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) (effs []workflow.Effect, err error) {
+	return []workflow.Effect{
+		workflow.RunEffect(func(ctx context.Context, deps *workflow.Dependencies) error {
+			err := deps.Identities.Create(n.Identity)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}),
+	}, nil
+}
+
+func (*NodeDoCreateIdentity) CanReactTo(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) ([]workflow.Input, error) {
+	return nil, workflow.ErrEOF
+}
+
+func (*NodeDoCreateIdentity) ReactTo(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow, input workflow.Input) (*workflow.Node, error) {
+	return nil, workflow.ErrIncompatibleInput
+}
+
+func (n *NodeDoCreateIdentity) OutputData(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) (interface{}, error) {
+	return nil, nil
+}
