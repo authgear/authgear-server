79b8c3a5036eae16717b2469eb1a5e7b466d7bc5
Louis Chan <louischan@oursky.com>
2023-07-07 11:02:55 +0800

    Adjust workflowconfig to new interface

--- pkg/lib/workflow/workflowconfig/node_verify_claim.go
+++ pkg/lib/workflow/workflowconfig/node_verify_claim.go
@@ -21,21 +21,21 @@ func (n *NodeVerifyClaim) Kind() string {
 	return "workflowconfig.NodeVerifyClaim"
 }
 
-func (n *NodeVerifyClaim) GetEffects(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) (effs []workflow.Effect, err error) {
+func (n *NodeVerifyClaim) GetEffects(ctx context.Context, deps *workflow.Dependencies, workflows workflow.Workflows) (effs []workflow.Effect, err error) {
 	return nil, nil
 }
 
-func (n *NodeVerifyClaim) CanReactTo(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) ([]workflow.Input, error) {
+func (n *NodeVerifyClaim) CanReactTo(ctx context.Context, deps *workflow.Dependencies, workflows workflow.Workflows) ([]workflow.Input, error) {
 	// FIXME(workflow): resend code and verify incoming code.
 	return nil, nil
 }
 
-func (n *NodeVerifyClaim) ReactTo(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow, input workflow.Input) (*workflow.Node, error) {
+func (n *NodeVerifyClaim) ReactTo(ctx context.Context, deps *workflow.Dependencies, workflows workflow.Workflows, input workflow.Input) (*workflow.Node, error) {
 	// FIXME(workflow): resend code and verify incoming code.
 	return nil, workflow.ErrIncompatibleInput
 }
 
-func (n *NodeVerifyClaim) OutputData(ctx context.Context, deps *workflow.Dependencies, w *workflow.Workflow) (interface{}, error) {
+func (n *NodeVerifyClaim) OutputData(ctx context.Context, deps *workflow.Dependencies, workflows workflow.Workflows) (interface{}, error) {
 	// FIXME(workflow): output state about verification.
 	return nil, nil
 }
