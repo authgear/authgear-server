5dc2f6dd70c5dc5254b491f4eaa67cee3a8f1d81
Tung Wu <tung@oursky.com>
2023-07-14 22:56:00 +0800

    Fix oob otp authenticator do not return lockout error

--- pkg/lib/interaction/nodes/authn_oob.go
+++ pkg/lib/interaction/nodes/authn_oob.go
@@ -1,12 +1,16 @@
 package nodes
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/authgear/authgear-server/pkg/api"
+	"github.com/authgear/authgear-server/pkg/api/apierrors"
 	"github.com/authgear/authgear-server/pkg/api/model"
 	"github.com/authgear/authgear-server/pkg/lib/authn"
 	"github.com/authgear/authgear-server/pkg/lib/authn/authenticator"
 	"github.com/authgear/authgear-server/pkg/lib/interaction"
+	"github.com/authgear/authgear-server/pkg/util/errorutil"
 )
 
 func init() {
@@ -36,7 +40,12 @@ func (e *EdgeAuthenticationOOB) Instantiate(ctx *interaction.Context, graph *int
 		},
 	})
 	if err != nil {
-		info = nil
+		if errors.Is(err, authenticator.ErrInvalidCredentials) {
+			return nil, errorutil.WithDetails(api.ErrInvalidCredentials, errorutil.Details{
+				"AuthenticationType": apierrors.APIErrorDetail.Value(info.Type),
+			})
+		}
+		return nil, err
 	}
 
 	return &NodeAuthenticationOOB{Stage: e.Stage, Authenticator: info, AuthenticatorType: e.Authenticator.Type}, nil
