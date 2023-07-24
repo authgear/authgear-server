b61f11265ef515e7de89467d78f210b09da680bf
Louis Chan <louischan@oursky.com>
2023-07-24 14:37:23 +0800

    Add event project.app.created

---
+++ pkg/api/event/nonblocking/project.go
@@ -0,0 +1,5 @@
+package nonblocking
+
+// AppConfig is a placeholder for *config.AppConfig.
+// It is because we cannot import pkg/lib/config from pkg/api/event/nonblocking.
+type AppConfig interface{}
