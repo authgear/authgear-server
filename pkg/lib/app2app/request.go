64b8b9a06c0f5bcaa57c6fd3311f3268337fa5aa
Tung Wu <tung@oursky.com>
2023-07-28 19:56:31 +0800

    Rename some structs to follow convention

--- pkg/lib/app2app/request.go
+++ pkg/lib/app2app/request.go
@@ -5,9 +5,9 @@ import (
 )
 
 // nolint:gosec
-const TokenType = "vnd.authgear.app2app-device-key"
+const RequestTokenType = "vnd.authgear.app2app-request"
 
-type Token struct {
+type Request struct {
 	Key       jwk.Key `json:"-"`
 	Challenge string  `json:"challenge"`
 }
