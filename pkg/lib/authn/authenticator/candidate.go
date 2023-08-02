8de99aeedc4572516c9d4650b2a7ffa70aa30991
Louis Chan <louischan@oursky.com>
2023-08-02 19:46:26 +0800

    Return candidates instead authentication methods

---
+++ pkg/lib/authn/authenticator/candidate.go
@@ -0,0 +1,27 @@
+package authenticator
+
+import (
+	"github.com/authgear/authgear-server/pkg/lib/config"
+)
+
+type Candidate map[string]interface{}
+
+const (
+	CandidateKeyAuthenticationMethod = "authentication_method"
+	CandidateKeyAuthenticatorID      = "authenticator_id"
+	CandidateKeyAuthenticatorType    = "authenticator_type"
+	CandidateKeyAuthenticatorKind    = "authenticator_kind"
+	CandidateKeyMaskedDisplayID      = "masked_display_id"
+)
+
+func NewCandidateRecoveryCode() Candidate {
+	return Candidate{
+		CandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodRecoveryCode,
+	}
+}
+
+func NewCandidateDeviceToken() Candidate {
+	return Candidate{
+		CandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodDeviceToken,
+	}
+}
