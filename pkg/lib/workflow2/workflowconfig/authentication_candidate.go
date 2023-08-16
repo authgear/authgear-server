6206803d8fc1f9b544b8d4800c0a02a16b7eebc9
Louis Chan <louischan@oursky.com>
2023-08-16 12:56:34 +0800

    Remove unnecessary fields from AuthenticationCandidate

--- pkg/lib/workflow2/workflowconfig/authentication_candidate.go
+++ pkg/lib/workflow2/workflowconfig/authentication_candidate.go
@@ -17,9 +17,7 @@ type AuthenticationCandidate map[AuthenticationCandidateKey]interface{}
 const (
 	AuthenticationCandidateKeyAuthenticationMethod AuthenticationCandidateKey = "authentication_method"
 	AuthenticationCandidateKeyAuthenticatorID      AuthenticationCandidateKey = "authenticator_id"
-	AuthenticationCandidateKeyAuthenticatorType    AuthenticationCandidateKey = "authenticator_type"
-	AuthenticationCandidateKeyAuthenticatorKind    AuthenticationCandidateKey = "authenticator_kind"
-	AuthenticationCandidateKeyMaskedDisplayID      AuthenticationCandidateKey = "masked_display_id"
+	AuthenticationCandidateKeyMaskedDisplayName    AuthenticationCandidateKey = "masked_display_name"
 )
 
 func NewAuthenticationCandidateFromMethod(m config.WorkflowAuthenticationMethod) AuthenticationCandidate {
@@ -40,32 +38,22 @@ func NewAuthenticationCandidateFromInfo(i *authenticator.Info) AuthenticationCan
 		case model.AuthenticatorTypePassword:
 			return AuthenticationCandidate{
 				AuthenticationCandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodPrimaryPassword,
-				// No ID is needed.
-				AuthenticationCandidateKeyAuthenticatorKind: i.Kind,
-				AuthenticationCandidateKeyAuthenticatorType: i.Type,
 			}
 		case model.AuthenticatorTypePasskey:
 			return AuthenticationCandidate{
 				AuthenticationCandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodPrimaryPasskey,
-				// No ID is needed.
-				AuthenticationCandidateKeyAuthenticatorKind: i.Kind,
-				AuthenticationCandidateKeyAuthenticatorType: i.Type,
 			}
 		case model.AuthenticatorTypeOOBEmail:
 			return AuthenticationCandidate{
 				AuthenticationCandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodPrimaryOOBOTPEmail,
 				AuthenticationCandidateKeyAuthenticatorID:      i.ID,
-				AuthenticationCandidateKeyAuthenticatorKind:    i.Kind,
-				AuthenticationCandidateKeyAuthenticatorType:    i.Type,
-				AuthenticationCandidateKeyMaskedDisplayID:      mail.MaskAddress(i.OOBOTP.Email),
+				AuthenticationCandidateKeyMaskedDisplayName:    mail.MaskAddress(i.OOBOTP.Email),
 			}
 		case model.AuthenticatorTypeOOBSMS:
 			return AuthenticationCandidate{
 				AuthenticationCandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodPrimaryOOBOTPSMS,
 				AuthenticationCandidateKeyAuthenticatorID:      i.ID,
-				AuthenticationCandidateKeyAuthenticatorKind:    i.Kind,
-				AuthenticationCandidateKeyAuthenticatorType:    i.Type,
-				AuthenticationCandidateKeyMaskedDisplayID:      phone.Mask(i.OOBOTP.Phone),
+				AuthenticationCandidateKeyMaskedDisplayName:    phone.Mask(i.OOBOTP.Phone),
 			}
 		}
 	case model.AuthenticatorKindSecondary:
@@ -73,32 +61,22 @@ func NewAuthenticationCandidateFromInfo(i *authenticator.Info) AuthenticationCan
 		case model.AuthenticatorTypePassword:
 			return AuthenticationCandidate{
 				AuthenticationCandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodSecondaryPassword,
-				// No ID is needed.
-				AuthenticationCandidateKeyAuthenticatorKind: i.Kind,
-				AuthenticationCandidateKeyAuthenticatorType: i.Type,
 			}
 		case model.AuthenticatorTypeOOBEmail:
 			return AuthenticationCandidate{
 				AuthenticationCandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodSecondaryOOBOTPEmail,
 				AuthenticationCandidateKeyAuthenticatorID:      i.ID,
-				AuthenticationCandidateKeyAuthenticatorKind:    i.Kind,
-				AuthenticationCandidateKeyAuthenticatorType:    i.Type,
-				AuthenticationCandidateKeyMaskedDisplayID:      mail.MaskAddress(i.OOBOTP.Email),
+				AuthenticationCandidateKeyMaskedDisplayName:    mail.MaskAddress(i.OOBOTP.Email),
 			}
 		case model.AuthenticatorTypeOOBSMS:
 			return AuthenticationCandidate{
 				AuthenticationCandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodSecondaryOOBOTPSMS,
 				AuthenticationCandidateKeyAuthenticatorID:      i.ID,
-				AuthenticationCandidateKeyAuthenticatorKind:    i.Kind,
-				AuthenticationCandidateKeyAuthenticatorType:    i.Type,
-				AuthenticationCandidateKeyMaskedDisplayID:      phone.Mask(i.OOBOTP.Phone),
+				AuthenticationCandidateKeyMaskedDisplayName:    phone.Mask(i.OOBOTP.Phone),
 			}
 		case model.AuthenticatorTypeTOTP:
 			return AuthenticationCandidate{
 				AuthenticationCandidateKeyAuthenticationMethod: config.WorkflowAuthenticationMethodSecondaryTOTP,
-				// No ID is needed.
-				AuthenticationCandidateKeyAuthenticatorKind: i.Kind,
-				AuthenticationCandidateKeyAuthenticatorType: i.Type,
 			}
 		}
 	}
