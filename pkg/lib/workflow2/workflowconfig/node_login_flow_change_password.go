4b95bbd8a13c7c3178f01bf8442c31f0ee3c8758
Louis Chan <louischan@oursky.com>
2023-08-16 15:21:21 +0800

    Output data for NodeLoginFlowChangePassword

--- pkg/lib/workflow2/workflowconfig/node_login_flow_change_password.go
+++ pkg/lib/workflow2/workflowconfig/node_login_flow_change_password.go
@@ -3,7 +3,9 @@ package workflowconfig
 import (
 	"context"
 
+	"github.com/authgear/authgear-server/pkg/api/model"
 	"github.com/authgear/authgear-server/pkg/lib/authn/authenticator"
+	"github.com/authgear/authgear-server/pkg/lib/config"
 	workflow "github.com/authgear/authgear-server/pkg/lib/workflow2"
 )
 
@@ -11,12 +13,19 @@ func init() {
 	workflow.RegisterNode(&NodeLoginFlowChangePassword{})
 }
 
+type NodeLoginFlowChangePasswordData struct {
+	Candidates []AuthenticationCandidate `json:"candidates"`
+}
+
+func (NodeLoginFlowChangePasswordData) Data() {}
+
 type NodeLoginFlowChangePassword struct {
 	Authenticator *authenticator.Info `json:"authenticator,omitempty"`
 }
 
 var _ workflow.NodeSimple = &NodeLoginFlowChangePassword{}
 var _ workflow.InputReactor = &NodeLoginFlowChangePassword{}
+var _ workflow.DataOutputer = &NodeLoginFlowChangePassword{}
 
 func (*NodeLoginFlowChangePassword) Kind() string {
 	return "workflowconfig.NodeLoginFlowChangePassword"
@@ -26,12 +35,12 @@ func (*NodeLoginFlowChangePassword) CanReactTo(ctx context.Context, deps *workfl
 	return []workflow.Input{&InputTakeNewPassword{}}, nil
 }
 
-func (i *NodeLoginFlowChangePassword) ReactTo(ctx context.Context, deps *workflow.Dependencies, workflows workflow.Workflows, input workflow.Input) (*workflow.Node, error) {
+func (n *NodeLoginFlowChangePassword) ReactTo(ctx context.Context, deps *workflow.Dependencies, workflows workflow.Workflows, input workflow.Input) (*workflow.Node, error) {
 	var inputTakeNewPassword inputTakeNewPassword
 	if workflow.AsInput(input, &inputTakeNewPassword) {
 		newPassword := inputTakeNewPassword.GetNewPassword()
 
-		oldInfo := i.Authenticator
+		oldInfo := n.Authenticator
 		changed, newInfo, err := deps.Authenticators.WithSpec(oldInfo, &authenticator.Spec{
 			Password: &authenticator.PasswordSpec{
 				PlainPassword: newPassword,
@@ -52,4 +61,18 @@ func (i *NodeLoginFlowChangePassword) ReactTo(ctx context.Context, deps *workflo
 	}
 
 	return nil, workflow.ErrIncompatibleInput
+}
+
+func (n *NodeLoginFlowChangePassword) OutputData(ctx context.Context, deps *workflow.Dependencies, workflows workflow.Workflows) (workflow.Data, error) {
+	var candidate AuthenticationCandidate
+	switch n.Authenticator.Kind {
+	case model.AuthenticatorKindPrimary:
+		candidate = NewAuthenticationCandidateFromMethod(config.WorkflowAuthenticationMethodPrimaryPassword)
+	case model.AuthenticatorKindSecondary:
+		candidate = NewAuthenticationCandidateFromMethod(config.WorkflowAuthenticationMethodSecondaryPassword)
+	}
+
+	return NodeLoginFlowChangePasswordData{
+		Candidates: []AuthenticationCandidate{candidate},
+	}, nil
 }
