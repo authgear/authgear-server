d6e66825c432984191cc1edbb49ea5b7259ef87d
Louis Chan <louischan@oursky.com>
2023-08-17 17:36:10 +0800

    Adapt IntentSignupFlowStepVerify

--- pkg/lib/workflow2/workflowconfig/error.go
+++ pkg/lib/workflow2/workflowconfig/error.go
@@ -10,7 +10,6 @@ var ErrStepNotFound = apierrors.NotFound.WithReason("WorkflowConfigStepNotFound"
 var InvalidIdentificationMethod = apierrors.BadRequest.WithReason("WorkflowConfigInvalidIdentificationMethod")
 var InvalidAuthenticationMethod = apierrors.BadRequest.WithReason("WorkflowConfigInvalidAuthenticationMethod")
 var InvalidTargetStep = apierrors.InternalError.WithReason("WorkflowConfigInvalidTargetStep")
-var InvalidOOBOTPChannel = apierrors.BadRequest.WithReason("WorkflowConfigInvalidOOBOTPChannel")
 var InvalidUserProfile = apierrors.BadRequest.WithReason("WorkflowConfigInvalidUserProfile")
 var InvalidAuthenticationCandidateIndex = apierrors.BadRequest.WithReason("WorkflowConfigInvalidAuthenticationCandidateIndex")
 
