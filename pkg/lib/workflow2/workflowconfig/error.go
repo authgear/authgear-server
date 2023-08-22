90975a009300b01d3a0c82fcc288aaf2d70088d3
Louis Chan <louischan@oursky.com>
2023-08-22 11:21:35 +0800

    Remove unnecessary error InvalidUserProfile

--- pkg/lib/workflow2/workflowconfig/error.go
+++ pkg/lib/workflow2/workflowconfig/error.go
@@ -8,7 +8,6 @@ var ErrFlowNotFound = apierrors.NotFound.WithReason("WorkflowConfigFlowNotFound"
 var ErrStepNotFound = apierrors.NotFound.WithReason("WorkflowConfigStepNotFound").New("step not found")
 
 var InvalidTargetStep = apierrors.InternalError.WithReason("WorkflowConfigInvalidTargetStep")
-var InvalidUserProfile = apierrors.BadRequest.WithReason("WorkflowConfigInvalidUserProfile")
 
 var ErrDifferentUserID = apierrors.BadRequest.WithReason("WorkflowConfigDifferentUserID").New("different user ID")
 var ErrNoUserID = apierrors.BadRequest.WithReason("WorkflowConfigNoUserID").New("no user ID")
