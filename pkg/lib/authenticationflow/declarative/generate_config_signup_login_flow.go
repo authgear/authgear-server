0f8b672ed0801b71e0d1908531e1f81349eff287
Louis Chan <louischan@oursky.com>
2023-09-20 09:19:54 +0800

    Generate config for SignupLoginFlow

---
+++ pkg/lib/authenticationflow/declarative/generate_config_signup_login_flow.go
@@ -0,0 +1,93 @@
+package declarative
+
+import (
+	"github.com/authgear/authgear-server/pkg/api/model"
+	"github.com/authgear/authgear-server/pkg/lib/config"
+)
+
+func GenerateSignupLoginFlowConfig(cfg *config.AppConfig) *config.AuthenticationFlowSignupLoginFlow {
+	flow := &config.AuthenticationFlowSignupLoginFlow{
+		Name: nameGeneratedFlow,
+		Steps: []*config.AuthenticationFlowSignupLoginFlowStep{
+			generateSignupLoginFlowStepIdentify(cfg),
+		},
+	}
+
+	return flow
+}
+
+func generateSignupLoginFlowStepIdentify(cfg *config.AppConfig) *config.AuthenticationFlowSignupLoginFlowStep {
+	step := &config.AuthenticationFlowSignupLoginFlowStep{
+		Name: nameStepIdentify,
+		Type: config.AuthenticationFlowSignupLoginFlowStepTypeIdentify,
+	}
+
+	for _, identityType := range cfg.Authentication.Identities {
+		switch identityType {
+		case model.IdentityTypeLoginID:
+			oneOf := generateSignupLoginFlowStepIdentifyLoginID(cfg)
+			step.OneOf = append(step.OneOf, oneOf...)
+		case model.IdentityTypeOAuth:
+			oneOf := generateSignupLoginFlowStepIdentifyOAuth(cfg)
+			step.OneOf = append(step.OneOf, oneOf...)
+		case model.IdentityTypePasskey:
+			oneOf := generateSignupLoginFlowStepIdentifyPasskey(cfg)
+			step.OneOf = append(step.OneOf, oneOf...)
+		}
+	}
+
+	return step
+}
+
+func newSignupLoginFlowOneOf(i config.AuthenticationFlowIdentification) *config.AuthenticationFlowSignupLoginFlowOneOf {
+	return &config.AuthenticationFlowSignupLoginFlowOneOf{
+		Identification: i,
+		SignupFlow:     nameGeneratedFlow,
+		LoginFlow:      nameGeneratedFlow,
+	}
+}
+
+func generateSignupLoginFlowStepIdentifyLoginID(cfg *config.AppConfig) []*config.AuthenticationFlowSignupLoginFlowOneOf {
+	var output []*config.AuthenticationFlowSignupLoginFlowOneOf
+
+	email := false
+	phone := false
+	username := false
+	for _, keyConfig := range cfg.Identity.LoginID.Keys {
+		switch {
+		case keyConfig.Type == model.LoginIDKeyTypeEmail && !email:
+			email = true
+			output = append(output, newSignupLoginFlowOneOf(config.AuthenticationFlowIdentificationEmail))
+
+		case keyConfig.Type == model.LoginIDKeyTypePhone && !phone:
+			phone = true
+			output = append(output, newSignupLoginFlowOneOf(config.AuthenticationFlowIdentificationPhone))
+
+		case keyConfig.Type == model.LoginIDKeyTypeUsername && !username:
+			username = true
+			output = append(output, newSignupLoginFlowOneOf(config.AuthenticationFlowIdentificationUsername))
+		}
+	}
+
+	return output
+}
+
+func generateSignupLoginFlowStepIdentifyOAuth(cfg *config.AppConfig) []*config.AuthenticationFlowSignupLoginFlowOneOf {
+	if len(cfg.Identity.OAuth.Providers) == 0 {
+		return nil
+	}
+
+	return []*config.AuthenticationFlowSignupLoginFlowOneOf{
+		newSignupLoginFlowOneOf(config.AuthenticationFlowIdentificationOAuth),
+	}
+}
+
+func generateSignupLoginFlowStepIdentifyPasskey(cfg *config.AppConfig) []*config.AuthenticationFlowSignupLoginFlowOneOf {
+	oneOf := newSignupLoginFlowOneOf(config.AuthenticationFlowIdentificationPasskey)
+	// We do not allow signup with a passkey.
+	oneOf.SignupFlow = ""
+
+	return []*config.AuthenticationFlowSignupLoginFlowOneOf{
+		oneOf,
+	}
+}
---
+++ pkg/lib/authenticationflow/declarative/generate_config_signup_login_flow_test.go
@@ -0,0 +1,227 @@
+package declarative
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"sigs.k8s.io/yaml"
+
+	"github.com/authgear/authgear-server/pkg/lib/config"
+)
+
+func TestGenerateSignupLoginFlowConfig(t *testing.T) {
+	Convey("GenerateSignupLoginFlowConfig", t, func() {
+		test := func(cfgStr string, expected string) {
+			jsonData, err := yaml.YAMLToJSON([]byte(cfgStr))
+			So(err, ShouldBeNil)
+
+			var appConfig config.AppConfig
+			decoder := json.NewDecoder(bytes.NewReader(jsonData))
+			err = decoder.Decode(&appConfig)
+			So(err, ShouldBeNil)
+
+			config.PopulateDefaultValues(&appConfig)
+
+			flow := GenerateSignupLoginFlowConfig(&appConfig)
+			flowJSON, err := json.Marshal(flow)
+			So(err, ShouldBeNil)
+
+			expectedJSON, err := yaml.YAMLToJSON([]byte(expected))
+			So(err, ShouldBeNil)
+
+			So(string(flowJSON), ShouldEqualJSON, string(expectedJSON))
+		}
+
+		// email
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - password
+identity:
+  login_id:
+    keys:
+    - type: email
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: email
+    signup_flow: default
+    login_flow: default
+`)
+
+		// phone
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - oob_otp_sms
+identity:
+  login_id:
+    keys:
+    - type: phone
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: phone
+    signup_flow: default
+    login_flow: default
+`)
+
+		// username
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - password
+identity:
+  login_id:
+    keys:
+    - type: username
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: username
+    signup_flow: default
+    login_flow: default
+`)
+
+		// email,phone
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - password
+  - oob_otp_email
+  - oob_otp_sms
+identity:
+  login_id:
+    keys:
+    - type: email
+    - type: phone
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: email
+    signup_flow: default
+    login_flow: default
+  - identification: phone
+    signup_flow: default
+    login_flow: default
+`)
+
+		// oauth
+		test(`
+authentication:
+  identities:
+  - oauth
+identity:
+  oauth:
+    providers:
+    - alias: google
+      type: google
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: oauth
+    signup_flow: default
+    login_flow: default
+`)
+
+		// passkey
+		test(`
+authentication:
+  identities:
+  - login_id
+  - passkey
+  primary_authenticators:
+  - password
+  - passkey
+identity:
+  login_id:
+    keys:
+    - type: email
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: email
+    signup_flow: default
+    login_flow: default
+  - identification: passkey
+    login_flow: default
+`)
+
+		// all
+		test(`
+authentication:
+  identities:
+  - login_id
+  - oauth
+  - passkey
+  primary_authenticators:
+  - password
+  - passkey
+  secondary_authenticators:
+  - totp
+  secondary_authentication_mode: required
+  device_token:
+    disabled: true
+  recovery_code:
+    disabled: true
+identity:
+  login_id:
+    keys:
+    - type: email
+    - type: phone
+    - type: username
+  oauth:
+    providers:
+    - alias: google
+      type: google
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: email
+    signup_flow: default
+    login_flow: default
+  - identification: phone
+    signup_flow: default
+    login_flow: default
+  - identification: username
+    signup_flow: default
+    login_flow: default
+  - identification: oauth
+    signup_flow: default
+    login_flow: default
+  - identification: passkey
+    login_flow: default
+`)
+	})
+}
