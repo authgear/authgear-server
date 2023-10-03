47a74d3cfec6acd76634be0febedf931d28e3e10
Louis Chan <louischan@oursky.com>
2023-10-03 15:48:28 +0800

    Handle oauth callback for authflow

--- pkg/auth/handler/webapp/sso_callback.go
+++ pkg/auth/handler/webapp/sso_callback.go
@@ -13,37 +13,84 @@ func ConfigureSSOCallbackRoute(route httproute.Route) httproute.Route {
 }
 
 type SSOCallbackHandler struct {
-	ControllerFactory ControllerFactory
+	AuthflowController *AuthflowController
+	ControllerFactory  ControllerFactory
 }
 
 func (h *SSOCallbackHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctrl, err := h.ControllerFactory.New(r, w)
-	if err != nil {
+	if err := r.ParseForm(); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	defer ctrl.Serve()
 
-	data := InputOAuthCallback{
-		ProviderAlias: httproute.GetParam(r, "alias"),
+	state := r.Form.Get("state")
+	code := r.Form.Get("code")
+	error_ := r.Form.Get("error")
+	errorDescription := r.Form.Get("error_description")
+	errorURI := r.Form.Get("error_uri")
 
-		Code:             r.Form.Get("code"),
-		Error:            r.Form.Get("error"),
-		ErrorDescription: r.Form.Get("error_description"),
-		ErrorURI:         r.Form.Get("error_uri"),
-	}
-
-	handler := func() error {
-		result, err := ctrl.InteractionPost(func() (input interface{}, err error) {
-			input = &data
-			return
-		})
+	switch {
+	case state != "":
+		// authflow
+		s, err := h.AuthflowController.GetWebSession(r)
 		if err != nil {
-			return err
+			h.AuthflowController.RenderError(w, r, err)
+			return
 		}
+
+		xStep := state
+		screen, err := h.AuthflowController.GetScreen(s, xStep)
+		if err != nil {
+			h.AuthflowController.RenderError(w, r, err)
+			return
+		}
+
+		input := map[string]interface{}{}
+		switch {
+		case code != "":
+			input["code"] = code
+		case error_ != "":
+			input["error"] = error_
+			input["error_description"] = errorDescription
+			input["error_uri"] = errorURI
+		}
+		result, err := h.AuthflowController.FeedInput(r, s, screen, input)
+		if err != nil {
+			h.AuthflowController.RenderError(w, r, err)
+			return
+		}
+
 		result.WriteResponse(w, r)
-		return nil
+	default:
+		// interaction
+		ctrl, err := h.ControllerFactory.New(r, w)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer ctrl.Serve()
+
+		data := InputOAuthCallback{
+			ProviderAlias: httproute.GetParam(r, "alias"),
+
+			Code:             code,
+			Error:            error_,
+			ErrorDescription: errorDescription,
+			ErrorURI:         errorURI,
+		}
+
+		handler := func() error {
+			result, err := ctrl.InteractionPost(func() (input interface{}, err error) {
+				input = &data
+				return
+			})
+			if err != nil {
+				return err
+			}
+			result.WriteResponse(w, r)
+			return nil
+		}
+		ctrl.Get(handler)
+		ctrl.PostAction("", handler)
 	}
-	ctrl.Get(handler)
-	ctrl.PostAction("", handler)
 }
