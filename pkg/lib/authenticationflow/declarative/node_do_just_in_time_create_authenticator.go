5a2b6fd0d546c66a782c9d2b9527554b2f077f91
Louis Chan <louischan@oursky.com>
2023-10-19 16:35:18 +0800

    Just-in-time create missing primary OOB OTP authenticator

---
+++ pkg/lib/authenticationflow/declarative/node_do_just_in_time_create_authenticator.go
@@ -0,0 +1,42 @@
+package declarative
+
+import (
+	"context"
+
+	authflow "github.com/authgear/authgear-server/pkg/lib/authenticationflow"
+	"github.com/authgear/authgear-server/pkg/lib/authn/authenticator"
+)
+
+func init() {
+	authflow.RegisterNode(&NodeDoJustInTimeCreateAuthenticator{})
+}
+
+type NodeDoJustInTimeCreateAuthenticator struct {
+	Authenticator *authenticator.Info `json:"authenticator,omitempty"`
+}
+
+var _ authflow.NodeSimple = &NodeDoJustInTimeCreateAuthenticator{}
+var _ authflow.Milestone = &NodeDoJustInTimeCreateAuthenticator{}
+var _ MilestoneDoCreateAuthenticator = &NodeDoJustInTimeCreateAuthenticator{}
+var _ MilestoneDidSelectAuthenticator = &NodeDoJustInTimeCreateAuthenticator{}
+var _ authflow.EffectGetter = &NodeDoJustInTimeCreateAuthenticator{}
+
+func (*NodeDoJustInTimeCreateAuthenticator) Kind() string {
+	return "NodeDoJustInTimeCreateAuthenticator"
+}
+
+func (n *NodeDoJustInTimeCreateAuthenticator) Milestone() {}
+func (n *NodeDoJustInTimeCreateAuthenticator) MilestoneDidSelectAuthenticator() *authenticator.Info {
+	return n.Authenticator
+}
+func (n *NodeDoJustInTimeCreateAuthenticator) MilestoneDoCreateAuthenticator() *authenticator.Info {
+	return n.Authenticator
+}
+
+func (n *NodeDoJustInTimeCreateAuthenticator) GetEffects(ctx context.Context, deps *authflow.Dependencies, flows authflow.Flows) (effs []authflow.Effect, err error) {
+	return []authflow.Effect{
+		authflow.RunEffect(func(ctx context.Context, deps *authflow.Dependencies) error {
+			return deps.Authenticators.Create(n.Authenticator, false)
+		}),
+	}, nil
+}
