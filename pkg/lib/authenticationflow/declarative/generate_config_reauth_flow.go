b32eccd55a581f3c8b2a3b4af3e78a08cdd40d96
Louis Chan <louischan@oursky.com>
2023-10-27 15:57:13 +0800

    Generate ReauthFlow config

---
+++ pkg/lib/authenticationflow/declarative/generate_config_reauth_flow.go
@@ -0,0 +1,74 @@
+package declarative
+
+import (
+	"github.com/authgear/authgear-server/pkg/api/model"
+	"github.com/authgear/authgear-server/pkg/lib/config"
+)
+
+func GenerateReauthFlowConfig(cfg *config.AppConfig) *config.AuthenticationFlowReauthFlow {
+	flow := &config.AuthenticationFlowReauthFlow{
+		Name: nameGeneratedFlow,
+		Steps: []*config.AuthenticationFlowReauthFlowStep{
+			generateReauthFlowStepIdentify(cfg),
+			generateReauthFlowStepAuthenticate(cfg),
+		},
+	}
+
+	return flow
+}
+
+func generateReauthFlowStepIdentify(cfg *config.AppConfig) *config.AuthenticationFlowReauthFlowStep {
+	step := &config.AuthenticationFlowReauthFlowStep{
+		Name: nameStepIdentify,
+		Type: config.AuthenticationFlowReauthFlowStepTypeIdentify,
+		OneOf: []*config.AuthenticationFlowReauthFlowOneOf{
+			{
+				Identification: config.AuthenticationFlowIdentificationIDToken,
+			},
+		},
+	}
+	return step
+}
+
+func generateReauthFlowStepAuthenticate(cfg *config.AppConfig) *config.AuthenticationFlowReauthFlowStep {
+	step := &config.AuthenticationFlowReauthFlowStep{
+		Name: nameStepReauthenticate,
+		Type: config.AuthenticationFlowReauthFlowStepTypeAuthenticate,
+	}
+
+	addOneOf := func(authentication config.AuthenticationFlowAuthentication) {
+		step.OneOf = append(step.OneOf, &config.AuthenticationFlowReauthFlowOneOf{
+			Authentication: authentication,
+		})
+	}
+
+	for _, authenticatorType := range *cfg.Authentication.PrimaryAuthenticators {
+		switch authenticatorType {
+		case model.AuthenticatorTypePassword:
+			addOneOf(config.AuthenticationFlowAuthenticationPrimaryPassword)
+		case model.AuthenticatorTypePasskey:
+			addOneOf(config.AuthenticationFlowAuthenticationPrimaryPasskey)
+		case model.AuthenticatorTypeOOBEmail:
+			addOneOf(config.AuthenticationFlowAuthenticationPrimaryOOBOTPEmail)
+		case model.AuthenticatorTypeOOBSMS:
+			addOneOf(config.AuthenticationFlowAuthenticationPrimaryOOBOTPSMS)
+		}
+	}
+
+	if !cfg.Authentication.SecondaryAuthenticationMode.IsDisabled() {
+		for _, authenticatorType := range *cfg.Authentication.SecondaryAuthenticators {
+			switch authenticatorType {
+			case model.AuthenticatorTypePassword:
+				addOneOf(config.AuthenticationFlowAuthenticationSecondaryPassword)
+			case model.AuthenticatorTypeOOBEmail:
+				addOneOf(config.AuthenticationFlowAuthenticationSecondaryOOBOTPEmail)
+			case model.AuthenticatorTypeOOBSMS:
+				addOneOf(config.AuthenticationFlowAuthenticationSecondaryOOBOTPSMS)
+			case model.AuthenticatorTypeTOTP:
+				addOneOf(config.AuthenticationFlowAuthenticationSecondaryTOTP)
+			}
+		}
+	}
+
+	return step
+}
---
+++ pkg/lib/authenticationflow/declarative/generate_config_reauth_flow_test.go
@@ -0,0 +1,286 @@
+package declarative
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"sigs.k8s.io/yaml"
+
+	"github.com/authgear/authgear-server/pkg/lib/config"
+)
+
+func TestGenerateReauthFlowConfig(t *testing.T) {
+	Convey("GenerateReauthFlowConfig", t, func() {
+		test := func(cfgStr string, expected string) {
+			jsonData, err := yaml.YAMLToJSON([]byte(cfgStr))
+			So(err, ShouldBeNil)
+
+			var appConfig config.AppConfig
+			decoder := json.NewDecoder(bytes.NewReader(jsonData))
+			err = decoder.Decode(&appConfig)
+			So(err, ShouldBeNil)
+
+			config.PopulateDefaultValues(&appConfig)
+
+			flow := GenerateReauthFlowConfig(&appConfig)
+			flowJSON, err := json.Marshal(flow)
+			So(err, ShouldBeNil)
+
+			expectedJSON, err := yaml.YAMLToJSON([]byte(expected))
+			So(err, ShouldBeNil)
+
+			So(string(flowJSON), ShouldEqualJSON, string(expectedJSON))
+		}
+
+		// email, password
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - password
+  secondary_authenticators: []
+identity:
+  login_id:
+    keys:
+    - type: email
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: id_token
+- name: reauthenticate
+  type: authenticate
+  one_of:
+  - authentication: primary_password
+`)
+
+		// email, otp
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - oob_otp_email
+  secondary_authenticators: []
+identity:
+  login_id:
+    keys:
+    - type: email
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: id_token
+- name: reauthenticate
+  type: authenticate
+  one_of:
+  - authentication: primary_oob_otp_email
+`)
+
+		// phone, otp
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - oob_otp_sms
+  secondary_authenticators: []
+identity:
+  login_id:
+    keys:
+    - type: phone
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: id_token
+- name: reauthenticate
+  type: authenticate
+  one_of:
+  - authentication: primary_oob_otp_sms
+`)
+
+		// username, password
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - password
+  secondary_authenticators: []
+identity:
+  login_id:
+    keys:
+    - type: username
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: id_token
+- name: reauthenticate
+  type: authenticate
+  one_of:
+  - authentication: primary_password
+`)
+
+		// email,phone, password,otp
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - password
+  - oob_otp_email
+  - oob_otp_sms
+  secondary_authenticators: []
+identity:
+  login_id:
+    keys:
+    - type: email
+    - type: phone
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: id_token
+- name: reauthenticate
+  type: authenticate
+  one_of:
+  - authentication: primary_password
+  - authentication: primary_oob_otp_email
+  - authentication: primary_oob_otp_sms
+`)
+
+		// email,password, totp,recovery_code
+		test(`
+authentication:
+  identities:
+  - login_id
+  primary_authenticators:
+  - password
+  secondary_authenticators:
+  - totp
+  secondary_authentication_mode: required
+identity:
+  login_id:
+    keys:
+    - type: email
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: id_token
+- name: reauthenticate
+  type: authenticate
+  one_of:
+  - authentication: primary_password
+  - authentication: secondary_totp
+`)
+
+		// oauth
+		test(`
+authentication:
+  identities:
+  - oauth
+  primary_authenticators: []
+  secondary_authenticators: []
+identity:
+  oauth:
+    providers:
+    - alias: google
+      type: google
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: id_token
+- name: reauthenticate
+  type: authenticate
+`)
+
+		// passkey
+		test(`
+authentication:
+  identities:
+  - login_id
+  - passkey
+  primary_authenticators:
+  - password
+  - passkey
+  secondary_authenticators: []
+identity:
+  login_id:
+    keys:
+    - type: email
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: id_token
+- name: reauthenticate
+  type: authenticate
+  one_of:
+  - authentication: primary_password
+  - authentication: primary_passkey
+`)
+
+		// passkey and oauth and 2fa
+		test(`
+authentication:
+  identities:
+  - login_id
+  - oauth
+  - passkey
+  primary_authenticators:
+  - password
+  - passkey
+  secondary_authenticators:
+  - totp
+  secondary_authentication_mode: required
+  device_token:
+    disabled: true
+  recovery_code:
+    disabled: true
+identity:
+  login_id:
+    keys:
+    - type: email
+  oauth:
+    providers:
+    - alias: google
+      type: google
+`, `
+name: default
+steps:
+- name: identify
+  type: identify
+  one_of:
+  - identification: id_token
+- name: reauthenticate
+  type: authenticate
+  one_of:
+  - authentication: primary_password
+  - authentication: primary_passkey
+  - authentication: secondary_totp
+`)
+	})
+}
