c30973b503dc51db43b556ddc892287eb7ec58ae
Louis Chan <louischan@oursky.com>
2023-10-31 15:23:09 +0800

    Add back FlowType

--- pkg/auth/handler/webapp/viewmodels/authflow_branch.go
+++ pkg/auth/handler/webapp/viewmodels/authflow_branch.go
@@ -22,6 +22,8 @@ func isAuthflowBranchSame(a AuthflowBranch, b AuthflowBranch) bool {
 }
 
 type AuthflowBranchViewModel struct {
+	// FlowType is mainly for pages to tell if the flow is reauthentication or not.
+	FlowType           authflow.FlowType
 	ActionType         authflow.FlowActionType
 	DeviceTokenEnabled bool
 	Branches           []AuthflowBranch
@@ -45,6 +47,7 @@ func NewAuthflowBranchViewModel(screen *webapp.AuthflowScreenWithFlowResponse) A
 	}
 
 	return AuthflowBranchViewModel{
+		FlowType:           screen.StateTokenFlowResponse.Type,
 		ActionType:         screen.StateTokenFlowResponse.Action.Type,
 		DeviceTokenEnabled: deviceTokenEnabled,
 		Branches:           branches,
