9d282df63dfc621d93a0827295de5074ddc8d3c9
Louis Chan <louischan@oursky.com>
2023-11-14 16:19:03 +0800

    Move recovery code

--- pkg/lib/web/plain_text.go
+++ pkg/lib/web/plain_text.go
@@ -6,6 +6,11 @@ import (
 
 var TemplateWebRecoveryCodeTXT = template.RegisterPlainText("web/__recovery_code.txt")
 
+var TemplateWebDownloadRecoveryCodeTXT = template.RegisterPlainText(
+	"web/download_recovery_code.txt",
+	ComponentsPlainText...,
+)
+
 var ComponentsPlainText = []*template.PlainText{
 	TemplateWebRecoveryCodeTXT,
 }
