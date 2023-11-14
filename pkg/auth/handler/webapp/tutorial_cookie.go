3713c9035477db8bb52fd879b28262cb7043430a
Louis Chan <louischan@oursky.com>
2023-11-14 15:14:23 +0800

    Move TutorialCookie interface

---
+++ pkg/auth/handler/webapp/tutorial_cookie.go
@@ -0,0 +1,11 @@
+package webapp
+
+import (
+	"net/http"
+
+	"github.com/authgear/authgear-server/pkg/util/httputil"
+)
+
+type TutorialCookie interface {
+	Pop(r *http.Request, rw http.ResponseWriter, name httputil.TutorialCookieName) bool
+}
