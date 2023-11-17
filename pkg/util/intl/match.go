6cbdcbf8f1b0cbb40deb9e7d113a193a3c8117bb
Louis Chan <louischan@oursky.com>
2023-11-17 12:20:22 +0800

    Cache language.Matcher

--- pkg/util/intl/match.go
+++ pkg/util/intl/match.go
@@ -1,8 +1,44 @@
 package intl
 
 import (
+	"strings"
+	"time"
+
+	"github.com/patrickmn/go-cache"
 	"golang.org/x/text/language"
 )
+
+var matcherCache = cache.New(5*time.Minute, 10*time.Minute)
+
+func toLanguageTags(ss []string) []language.Tag {
+	tags := make([]language.Tag, len(ss))
+	for i, item := range ss {
+		tags[i] = language.Make(item)
+	}
+	return tags
+}
+
+func getMatcherKey(supported []language.Tag) string {
+	ss := make([]string, len(supported))
+	for i, item := range supported {
+		ss[i] = item.String()
+	}
+	return strings.Join(ss, ",")
+}
+
+func GetMatcher(supported []language.Tag) language.Matcher {
+	key := getMatcherKey(supported)
+	matcherIface, ok := matcherCache.Get(key)
+	if ok {
+		return matcherIface.(language.Matcher)
+	}
+
+	// pprof shows golang.org/x/text@0.1{3,4}.0 language.NewMatcher is a CPU hot spot.
+	// So we cache intances of matcher.
+	matcher := language.NewMatcher(supported)
+	matcherCache.Set(key, matcher, cache.DefaultExpiration)
+	return matcher
+}
 
 // Match matches preferredLanguageTags to supportedLanguageTags
 // using fallbackLanguageTag as fallback.
@@ -11,16 +47,10 @@ func Match(preferredLanguageTags []string, supportedLanguageTags SupportedLangua
 		return -1, language.Und
 	}
 
-	supported := make([]language.Tag, len(supportedLanguageTags))
-	for i, item := range supportedLanguageTags {
-		supported[i] = language.Make(item)
-	}
-	matcher := language.NewMatcher(supported)
+	supported := toLanguageTags(supportedLanguageTags)
+	preferred := toLanguageTags(preferredLanguageTags)
 
-	preferred := make([]language.Tag, len(preferredLanguageTags))
-	for i, tagStr := range preferredLanguageTags {
-		preferred[i] = language.Make(tagStr)
-	}
+	matcher := GetMatcher(supported)
 
 	_, idx, _ := matcher.Match(preferred...)
 	tag := supported[idx]
