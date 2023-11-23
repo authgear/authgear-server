3fe7619eb6e69e4427e6082c1c9aadadfa6536c8
Tung Wu <tung@oursky.com>
2023-11-23 22:10:34 +0800

    Add allowed channels to account recovery authflow config

--- pkg/lib/config/authentication_flow_test.go
+++ pkg/lib/config/authentication_flow_test.go
@@ -236,11 +236,23 @@ account_recovery_flows:
         steps:
         - type: select_destination
           enumerate_destinations: true
+          allowed_channels:
+            - channel: email
+              form: link
+            - channel: sms
+              form: otp
+            - channel: sms
+              form: link
       - identification: phone
         on_failure: ignore
         steps:
         - type: select_destination
           enumerate_destinations: true
+          allowed_channels:
+            - channel: sms
+              form: otp
+            - channel: sms
+              form: link
     - type: verify_account_recovery_code
     - type: reset_password
 `)
