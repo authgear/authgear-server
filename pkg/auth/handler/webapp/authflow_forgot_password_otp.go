e16f996bbdcf5a97cd56923a1f2a83794c75fda2
Tung Wu <tung@oursky.com>
2023-11-27 15:51:42 +0800

    Implement AuthflowForgotPasswordOTPHandler

---
+++ pkg/auth/handler/webapp/authflow_forgot_password_otp.go
@@ -0,0 +1,136 @@
+package webapp
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
+	"github.com/authgear/authgear-server/pkg/auth/webapp"
+	"github.com/authgear/authgear-server/pkg/lib/authenticationflow/declarative"
+	"github.com/authgear/authgear-server/pkg/util/clock"
+	"github.com/authgear/authgear-server/pkg/util/httproute"
+	"github.com/authgear/authgear-server/pkg/util/template"
+	"github.com/authgear/authgear-server/pkg/util/validation"
+)
+
+var TemplateWebAuthflowForgotPasswordOTPHTML = template.RegisterHTML(
+	"web/authflow_forgot_password_otp.html",
+	components...,
+)
+
+var AuthflowForgotPasswordOTPSchema = validation.NewSimpleSchema(`
+	{
+		"type": "object",
+		"properties": {
+			"x_code": {
+				"type": "string",
+				"format": "x_oob_otp_code"
+			}
+		},
+		"required": ["x_code"]
+	}
+`)
+
+func ConfigureAuthflowForgotPasswordOTPRoute(route httproute.Route) httproute.Route {
+	return route.
+		WithMethods("OPTIONS", "POST", "GET").
+		WithPathPattern(webapp.AuthflowRouteForgotPasswordOTP)
+}
+
+type AuthflowForgotPasswordOTPViewModel struct {
+	Channel                        string
+	MaskedClaimValue               string
+	CodeLength                     int
+	FailedAttemptRateLimitExceeded bool
+	ResendCooldown                 int
+}
+
+func NewAuthflowForgotPasswordOTPViewModel(s *webapp.Session, screen *webapp.AuthflowScreenWithFlowResponse, now time.Time) AuthflowForgotPasswordOTPViewModel {
+	data := screen.StateTokenFlowResponse.Action.Data.(declarative.IntentAccountRecoveryFlowStepVerifyAccountRecoveryCodeData)
+	channel := data.Channel
+	maskedClaimValue := data.MaskedDisplayName
+	// FIXME(tung): IntentAccountRecoveryFlowStepVerifyAccountRecoveryCodeData is missing these data
+	// codeLength := data.CodeLength
+	// failedAttemptRateLimitExceeded := data.FailedAttemptRateLimitExceeded
+	// resendCooldown := int(data.CanResendAt.Sub(now).Seconds())
+	// if resendCooldown < 0 {
+	// 	resendCooldown = 0
+	// }
+
+	return AuthflowForgotPasswordOTPViewModel{
+		Channel:          string(channel),
+		MaskedClaimValue: maskedClaimValue,
+		// CodeLength:                     codeLength,
+		// FailedAttemptRateLimitExceeded: failedAttemptRateLimitExceeded,
+		// ResendCooldown:                 resendCooldown,
+	}
+}
+
+type AuthflowForgotPasswordOTPHandler struct {
+	Controller    *AuthflowController
+	BaseViewModel *viewmodels.BaseViewModeler
+	Renderer      Renderer
+	FlashMessage  FlashMessage
+	Clock         clock.Clock
+}
+
+func (h *AuthflowForgotPasswordOTPHandler) GetData(w http.ResponseWriter, r *http.Request, s *webapp.Session, screen *webapp.AuthflowScreenWithFlowResponse) (map[string]interface{}, error) {
+	now := h.Clock.NowUTC()
+	data := make(map[string]interface{})
+
+	baseViewModel := h.BaseViewModel.ViewModelForAuthFlow(r, w)
+	viewmodels.Embed(data, baseViewModel)
+
+	screenViewModel := NewAuthflowForgotPasswordOTPViewModel(s, screen, now)
+	viewmodels.Embed(data, screenViewModel)
+
+	return data, nil
+}
+
+func (h *AuthflowForgotPasswordOTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var handlers AuthflowControllerHandlers
+	handlers.Get(func(s *webapp.Session, screen *webapp.AuthflowScreenWithFlowResponse) error {
+		data, err := h.GetData(w, r, s, screen)
+		if err != nil {
+			return err
+		}
+
+		h.Renderer.RenderHTML(w, r, TemplateWebAuthflowEnterOOBOTPHTML, data)
+		return nil
+	})
+	handlers.PostAction("resend", func(s *webapp.Session, screen *webapp.AuthflowScreenWithFlowResponse) error {
+		input := map[string]interface{}{
+			"resend": true,
+		}
+
+		result, err := h.Controller.UpdateWithInput(r, s, screen, input)
+		if err != nil {
+			return err
+		}
+
+		h.FlashMessage.Flash(w, string(webapp.FlashMessageTypeResendCodeSuccess))
+		result.WriteResponse(w, r)
+		return nil
+	})
+	handlers.PostAction("submit", func(s *webapp.Session, screen *webapp.AuthflowScreenWithFlowResponse) error {
+		err := AuthflowEnterOOBOTPSchema.Validator().ValidateValue(FormToJSON(r.Form))
+		if err != nil {
+			return err
+		}
+
+		code := r.Form.Get("x_code")
+
+		input := map[string]interface{}{
+			"code": code,
+		}
+
+		result, err := h.Controller.AdvanceWithInput(r, s, screen, input)
+		if err != nil {
+			return err
+		}
+
+		result.WriteResponse(w, r)
+		return nil
+	})
+	h.Controller.HandleStep(w, r, &handlers)
+}
