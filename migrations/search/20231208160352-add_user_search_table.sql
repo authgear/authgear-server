0998725775322f2f7e7e4510daafb14178962937
Tung Wu <tung@oursky.com>
2023-12-13 15:16:53 +0800

    Tuning search user table

--- migrations/search/20231208160352-add_user_search_table.sql
+++ migrations/search/20231208160352-add_user_search_table.sql
@@ -3,31 +3,48 @@ CREATE TABLE _search_user
 (
     id                            text PRIMARY KEY,
     app_id                        text                        NOT NULL,
+    app_ids                       text[]
+      GENERATED ALWAYS AS (ARRAY[app_id]) STORED,
     created_at                    timestamp without time zone NOT NULL,
     updated_at                    timestamp without time zone NOT NULL,
     last_login_at                 timestamp without time zone,
     is_disabled                   boolean                     NOT NULL,
-    emails                        jsonb                       NOT NULL DEFAULT '[]'::jsonb,
-    email_local_parts             jsonb                       NOT NULL DEFAULT '[]'::jsonb,
-    email_domains                 jsonb                       NOT NULL DEFAULT '[]'::jsonb,
-    preferred_usernames           jsonb                       NOT NULL DEFAULT '[]'::jsonb,
-    phone_numbers                 jsonb                       NOT NULL DEFAULT '[]'::jsonb,
-    phone_number_country_codes    jsonb                       NOT NULL DEFAULT '[]'::jsonb,
-    phone_number_national_numbers jsonb                       NOT NULL DEFAULT '[]'::jsonb,
-    oauth_subject_ids             jsonb                       NOT NULL DEFAULT '[]'::jsonb,
-    gender                        text                        NOT NULL,
-    zoneinfo                      text                        NOT NULL,
-    locale                        text                        NOT NULL,
-    postal_code                   text                        NOT NULL,
-    country                       text                        NOT NULL,
+    emails                        text[]                      NOT NULL DEFAULT '{}',
+    email_local_parts             text[]                      NOT NULL DEFAULT '{}',
+    email_domains                 text[]                      NOT NULL DEFAULT '{}',
+    preferred_usernames           text[]                      NOT NULL DEFAULT '{}',
+    phone_numbers                 text[]                      NOT NULL DEFAULT '{}',
+    phone_number_country_codes    text[]                      NOT NULL DEFAULT '{}',
+    phone_number_national_numbers text[]                      NOT NULL DEFAULT '{}',
+    oauth_subject_ids             text[]                      NOT NULL DEFAULT '{}',
+    gender                        text[]                      NOT NULL DEFAULT '{}',
+    zoneinfo                      text[]                      NOT NULL DEFAULT '{}',
+    locale                        text[]                      NOT NULL DEFAULT '{}',
+    postal_code                   text[]                      NOT NULL DEFAULT '{}',
+    country                       text[]                      NOT NULL DEFAULT '{}',
     details                       jsonb                       NOT NULL DEFAULT '{}'::jsonb,
     details_tsvector              tsvector
-      GENERATED ALWAYS AS  (jsonb_to_tsvector('english', details, '["string", "numeric"]')) STORED
+      GENERATED ALWAYS AS (jsonb_to_tsvector('english', details, '["string", "numeric"]')) STORED
 );
 CREATE INDEX _search_user_app_id ON _search_user (app_id);
 CREATE INDEX _search_user_app_id_created_at ON _search_user (app_id, created_at);
 CREATE INDEX _search_user_app_id_last_login_at ON _search_user (app_id, last_login_at);
-CREATE INDEX _search_user_details_tsvector ON _search_user USING GIN (details_tsvector);
+CREATE INDEX _search_user_gin ON _search_user USING GIN (
+  app_ids,
+  emails,
+  email_domains,
+  email_local_parts,
+  preferred_usernames,
+  phone_numbers,
+  phone_number_country_codes,
+  phone_number_national_numbers,
+  oauth_subject_ids,
+  gender,
+  zoneinfo,
+  locale,
+  postal_code,
+  country,
+  details_tsvector);
 
 -- +migrate Down
 DROP TABLE _search_user;
