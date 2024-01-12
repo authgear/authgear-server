22cf1846f02c5282f2a8a864d337fc4f942ae504
Davis Tang <ngkaokis@gmail.com>
2024-01-12 13:46:32 +0800

    Add password input component
    ref #3560

--- pkg/lib/web/html.go
+++ pkg/lib/web/html.go
@@ -35,6 +35,7 @@ var TemplateWebAuthflowV2HeaderHTML = template.RegisterHTML("web/authflowv2/__he
 var TemplateWebAuthflowV2DividerHTML = template.RegisterHTML("web/authflowv2/__divider.html")
 var TemplateWebAuthflowV2AlertMessageHTML = template.RegisterHTML("web/authflowv2/__alert_message.html")
 var TemplateWebAuthflowV2OTPInputHTML = template.RegisterHTML("web/authflowv2/__otp_input.html")
+var TemplateWebAuthflowV2PasswordInputHTML = template.RegisterHTML("web/authflowv2/__password_input.html")
 
 var ComponentsHTML = []*template.HTML{
 	TemplateWebHTMLHeadHTML,
@@ -67,4 +68,5 @@ var ComponentsHTML = []*template.HTML{
 	TemplateWebAuthflowV2DividerHTML,
 	TemplateWebAuthflowV2AlertMessageHTML,
 	TemplateWebAuthflowV2OTPInputHTML,
+	TemplateWebAuthflowV2PasswordInputHTML,
 }
