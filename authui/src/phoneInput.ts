f51033e58f1332cad2857e2d5c8c7f9da6b30f5c
Newman Chow <newman@oursky.com>
2024-01-15 16:38:04 +0800

    Implement custom select for phone input #3561

--- authui/src/phoneInput.ts
+++ authui/src/phoneInput.ts
@@ -2,25 +2,28 @@ import { Controller } from "@hotwired/stimulus";
 import { countries, getCountryDataList, getEmojiFlag } from "countries-list";
 
 export class PhoneInputController extends Controller {
-  static targets = ["searchSelect"];
+  static targets = ["customSelect"];
 
-  declare readonly searchSelectTarget: HTMLInputElement;
+  declare readonly customSelectTarget: HTMLInputElement;
 
   connect(): void {
     this._initPhoneCode();
   }
 
   _initPhoneCode() {
-    const countriesData = getCountryDataList();
+    const countriesData = getCountryDataList().sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
     const options = countriesData.map((country) => {
       return {
-        label: `${getEmojiFlag(country.iso2)} +${country.phone}      ${
+        triggerLabel: `${getEmojiFlag(country.iso2)} +${country.phone}`,
+        label: `${getEmojiFlag(country.iso2)} +${country.phone}       ${
           country.name
         }`,
         value: country.iso2,
       };
     });
-    this.searchSelectTarget.setAttribute(
+    this.customSelectTarget.setAttribute(
       "data-custom-select-options-value",
       JSON.stringify(options)
     );
