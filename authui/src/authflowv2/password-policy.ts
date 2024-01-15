7a8aed0c37faa2e28b2c18ea37018d1619b17269
Davis Tang <ngkaokis@gmail.com>
2024-01-15 14:16:53 +0800

    Update to have 5 level of password strength
    ref #3560

--- authui/src/authflowv2/password-policy.ts
+++ authui/src/authflowv2/password-policy.ts
@@ -13,30 +13,30 @@ function checkPasswordStrength(
     "g"
   );
   currentMeter.className = currentMeter.className.replace(regx, "");
-  input.classList.remove("input--error");
 
   if (value === "") {
     return;
   }
 
   const result = zxcvbn(value);
-  // Note: confirming how many level of password strength
-  const score = Math.min(4, Math.max(1, result.score + 1));
+  // FIXME(davis): Confirming with designer on the level of password strength
+  const score = Math.min(5, Math.max(1, result.score + 1));
   let strengthClass = "";
   switch (score) {
     case 1:
       strengthClass = "password-strength-meter--very-weak";
-      input.classList.add("input--error");
       break;
     case 2:
       strengthClass = "password-strength-meter--weak";
-      input.classList.add("input--error");
       break;
     case 3:
       strengthClass = "password-strength-meter--medium";
       break;
     case 4:
       strengthClass = "password-strength-meter--strong";
+      break;
+    case 5:
+      strengthClass = "password-strength-meter--very-strong";
       break;
   }
   currentMeter.classList.add(strengthClass);
