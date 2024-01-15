aabb992ef4725948b0fed57c4ce94c0a458d2330
Davis Tang <ngkaokis@gmail.com>
2024-01-15 15:44:37 +0800

    Use <meter> to implment password strength meter
    ref #3560

--- authui/src/authflowv2/password-policy.ts
+++ authui/src/authflowv2/password-policy.ts
@@ -4,43 +4,20 @@ import zxcvbn from "zxcvbn";
 function checkPasswordStrength(
   value: string,
   input: HTMLInputElement,
-  currentMeter: HTMLElement,
+  currentMeter: HTMLMeterElement,
   currentMeterDescription: HTMLElement
 ) {
-  currentMeterDescription.textContent = "";
-  var regx = new RegExp(
-    "\\b" + "password-strength-meter--" + "[^ ]*[ ]?\\b",
-    "g"
-  );
-  currentMeter.className = currentMeter.className.replace(regx, "");
-
   if (value === "") {
+    currentMeter.classList.add("hidden");
     return;
   }
 
+  currentMeter.classList.remove("hidden");
   const result = zxcvbn(value);
   // FIXME(davis): Confirming with designer on the level of password strength
   const score = Math.min(5, Math.max(1, result.score + 1));
-  let strengthClass = "";
-  switch (score) {
-    case 1:
-      strengthClass = "password-strength-meter--very-weak";
-      break;
-    case 2:
-      strengthClass = "password-strength-meter--weak";
-      break;
-    case 3:
-      strengthClass = "password-strength-meter--medium";
-      break;
-    case 4:
-      strengthClass = "password-strength-meter--strong";
-      break;
-    case 5:
-      strengthClass = "password-strength-meter--very-strong";
-      break;
-  }
-  currentMeter.classList.add(strengthClass);
-  currentMeterDescription.textContent = currentMeterDescription.getAttribute(
+  currentMeter.value = score;
+  currentMeterDescription.textContent = currentMeter.getAttribute(
     "data-desc-" + score
   );
 }
@@ -49,7 +26,7 @@ export class PasswordPolicyController extends Controller {
   static targets = ["input", "currentMeter", "currentMeterDescription"];
 
   declare inputTarget: HTMLInputElement;
-  declare currentMeterTarget: HTMLElement;
+  declare currentMeterTarget: HTMLMeterElement;
   declare currentMeterDescriptionTarget: HTMLElement;
 
   check() {
