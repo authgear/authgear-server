fac01f047881b9a323bdea056f2092580c7fea79
Newman Chow <newman@oursky.com>
2024-01-16 19:34:44 +0800

    Replace countries-list with libphonenumbers-js #3561

---
+++ authui/src/getEmojiFlag.ts
@@ -0,0 +1,16 @@
+// Ref: https://github.com/annexare/Countries/blob/d92daf336b0d71cf265c5689e7a2b789832bf3bc/packages/countries/src/getEmojiFlag.ts
+
+// "Regional Indicator Symbol Letter A" - "Latin Capital Letter A"
+const UNICODE_BASE = 127462 - "A".charCodeAt(0);
+
+// Country code should contain exactly 2 uppercase characters from A..Z
+const COUNTRY_CODE_REGEX = /^[A-Z]{2}$/;
+
+export const getEmojiFlag = (countryCode: string): string =>
+  COUNTRY_CODE_REGEX.test(countryCode)
+    ? String.fromCodePoint(
+        ...countryCode
+          .split("")
+          .map((letter) => UNICODE_BASE + letter.toUpperCase().charCodeAt(0))
+      )
+    : "";
--- authui/src/phoneInput.ts
+++ authui/src/phoneInput.ts
@@ -1,10 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
-import {
-  ICountryData,
-  countries,
-  getCountryDataList,
-  getEmojiFlag,
-} from "countries-list";
+import { getCountries, getCountryCallingCode } from "libphonenumber-js";
+import defaultTerritories from "cldr-localenames-full/main/en/territories.json";
+import territoriesMap from "cldr-localenames-full/main/*/territories.json";
+import { getEmojiFlag } from "./getEmojiFlag";
+
+interface PhoneInputCountry {
+  flagEmoji: string;
+  localizedName: string;
+  name: string;
+  iso2: string;
+  phone: string;
+}
 
 export class PhoneInputController extends Controller {
   static targets = ["countrySelect", "input"];
@@ -15,14 +21,14 @@ export class PhoneInputController extends Controller {
   countryCode?: string;
   phoneNumber?: string;
 
-  _countriesData: ICountryData[] = [];
+  _countries: PhoneInputCountry[] = [];
 
   connect(): void {
     this._initPhoneCode();
   }
 
   updateValue(): void {
-    const country = this._countriesData.find(
+    const country = this._countries.find(
       (country) => country.iso2 === this.countryCode
     );
     const phoneCode = country?.phone[0];
@@ -48,15 +54,39 @@ export class PhoneInputController extends Controller {
     this.updateValue();
   }
 
-  _initPhoneCode() {
-    this._countriesData = getCountryDataList().sort((a, b) => {
-      return a.name.localeCompare(b.name);
-    });
-    const options = this._countriesData.map((country) => {
+  async _initPhoneCode() {
+    const lang = document.documentElement.lang || "en";
+    const countryCodes = getCountries();
+    const localizedTerritories = territoriesMap[lang];
+    const territories =
+      localizedTerritories?.main[lang as keyof typeof localizedTerritories.main]
+        ?.localeDisplayNames.territories ||
+      defaultTerritories.main.en.localeDisplayNames.territories;
+
+    this._countries = countryCodes
+      .map((countryCode) => {
+        const countryLocalizedName = territories[countryCode];
+        const countryName =
+          defaultTerritories.main.en.localeDisplayNames.territories[
+            countryCode
+          ];
+        const countryFlag = getEmojiFlag(countryCode);
+        const countryCallingCode = getCountryCallingCode(countryCode);
+        return {
+          flagEmoji: countryFlag,
+          localizedName: countryLocalizedName,
+          name: countryName,
+          iso2: countryCode,
+          phone: countryCallingCode,
+        };
+      })
+      .sort((a, b) => a.name.localeCompare(b.name));
+    const options = this._countries.map((country) => {
       return {
-        triggerLabel: `${getEmojiFlag(country.iso2)} +${country.phone}`,
-        prefix: `${getEmojiFlag(country.iso2)} +${country.phone}`,
-        label: country.name,
+        triggerLabel: `${country.flagEmoji} +${country.phone}`,
+        prefix: `${country.flagEmoji} +${country.phone}`,
+        searchLabel: country.name,
+        label: country.localizedName,
         value: country.iso2,
       };
     });
