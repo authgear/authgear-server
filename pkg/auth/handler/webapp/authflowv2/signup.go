59e03cddaadafe923b24216ecdaefe60809060ea
Tung Wu <tung@oursky.com>
2024-01-17 17:47:32 +0800

    Fix code after AdvanceOption was introduced in b354187ded89343c307f840fc39788bf71152850

--- pkg/auth/handler/webapp/authflowv2/signup.go
+++ pkg/auth/handler/webapp/authflowv2/signup.go
@@ -123,7 +123,7 @@ func (h *AuthflowV2SignupHandler) ServeHTTP(w http.ResponseWriter, r *http.Reque
 			"login_id":       loginID,
 		}
 
-		result, err := h.Controller.AdvanceWithInput(r, s, screen, input)
+		result, err := h.Controller.AdvanceWithInput(r, s, screen, input, nil)
 		if err != nil {
 			return err
 		}
