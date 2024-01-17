051c689545f0581c6a4be09d1e0648494193ece0
Tung Wu <tung@oursky.com>
2024-01-17 17:47:32 +0800

    Fix code after AdvanceOption was introduced in b354187ded89343c307f840fc39788bf71152850

--- pkg/auth/handler/webapp/authflowv2/enter_oob_otp.go
+++ pkg/auth/handler/webapp/authflowv2/enter_oob_otp.go
@@ -133,7 +133,7 @@ func (h *AuthflowV2EnterOOBOTPHandler) ServeHTTP(w http.ResponseWriter, r *http.
 			"request_device_token": requestDeviceToken,
 		}
 
-		result, err := h.Controller.AdvanceWithInput(r, s, screen, input)
+		result, err := h.Controller.AdvanceWithInput(r, s, screen, input, nil)
 		if err != nil {
 			return err
 		}
