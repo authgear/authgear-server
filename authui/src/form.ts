e27bf3de909936442132950a582e981cba821b39
Davis Tang <ngkaokis@gmail.com>
2024-01-23 16:13:56 +0800

    Add loading state to button for enter password page
    ref #3676

--- authui/src/form.ts
+++ authui/src/form.ts
@@ -6,6 +6,7 @@ import {
   hideProgressBar,
   showProgressBar,
   progressEventHandler,
+  displayButtonLoadingState,
 } from "./loading";
 import { handleAxiosError } from "./messageBar";
 
@@ -29,6 +30,7 @@ import { handleAxiosError } from "./messageBar";
 // See https://github.com/authgear/authgear-server/issues/2333
 export class XHRSubmitFormController extends Controller {
   revertDisabledButtons: { (): void } | null = null;
+  dismissButtonLoadingState: { (): void } | null = null;
   forms: HTMLFormElement[] = [];
 
   // Revert disabled buttons before Turbo caches the page
@@ -36,6 +38,9 @@ export class XHRSubmitFormController extends Controller {
   beforeCache = () => {
     if (this.revertDisabledButtons) {
       this.revertDisabledButtons();
+    }
+    if (this.dismissButtonLoadingState) {
+      this.dismissButtonLoadingState();
     }
   };
 
@@ -77,6 +82,9 @@ export class XHRSubmitFormController extends Controller {
       if (el) {
         const button = el as HTMLButtonElement;
         params.set(button.name, button.value);
+        if (button.getAttribute("data-button-loading-state") != null) {
+          this.dismissButtonLoadingState = displayButtonLoadingState(button);
+        }
       }
     }
 
@@ -117,6 +125,10 @@ export class XHRSubmitFormController extends Controller {
       if (this.revertDisabledButtons) {
         this.revertDisabledButtons();
         this.revertDisabledButtons = null;
+      }
+      if (this.dismissButtonLoadingState) {
+        this.dismissButtonLoadingState();
+        this.dismissButtonLoadingState = null;
       }
     } finally {
       hideProgressBar();
