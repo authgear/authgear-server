fb99048cd5896946dff73dca7f6b2036cb43d469
Davis Tang <ngkaokis@gmail.com>
2024-01-24 13:45:56 +0800

    Add loading spinner as pseudo element
    ref #3676

--- authui/src/authflowv2/loading.ts
+++ authui/src/authflowv2/loading.ts
@@ -14,6 +14,7 @@ interface LoadingEvent extends Event {
 
 export class LoadingController extends Controller {
   revertDisabledButtons: { (): void } | null = null;
+  button: HTMLButtonElement | null = null;
 
   // Revert disabled buttons before Turbo caches the page
   // To avoid flickering in the UI
@@ -27,8 +28,8 @@ export class LoadingController extends Controller {
     this.revertDisabledButtons = disableAllButtons();
     showProgressBar();
     if (e.detail?.button?.getAttribute("data-loading-state") != null) {
-      e.detail.button.innerHTML =
-        '<span class="primary-btn__loading-icon material-icons animate-spin">progress_activity</span>';
+      e.detail.button.classList.add("primary-btn--loading");
+      this.button = e.detail.button;
     }
   }
   onLoadingError(e: LoadingEvent) {
@@ -39,20 +40,24 @@ export class LoadingController extends Controller {
       this.revertDisabledButtons();
       this.revertDisabledButtons = null;
     }
+    this.button?.classList.remove("primary-btn--loading");
   }
   onLoadingEnd(e: LoadingEvent) {
     hideProgressBar();
+    this.button?.classList.remove("primary-btn--loading");
   }
 
   connect() {
     this.element.addEventListener("onLoading", this.onLoading);
     this.element.addEventListener("onLoadingError", this.onLoadingError);
     this.element.addEventListener("onLoadingEnd", this.onLoadingEnd);
+    document.addEventListener("turbo:before-cache", this.beforeCache);
   }
 
   disconnect() {
     this.element.removeEventListener("onLoading", this.onLoading);
     this.element.removeEventListener("onLoadingError", this.onLoadingError);
     this.element.removeEventListener("onLoadingEnd", this.onLoadingEnd);
+    document.removeEventListener("turbo:before-cache", this.beforeCache);
   }
 }
