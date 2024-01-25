17c2955ad0d4b1bb5dc85bafe516fbd2cc7e69f0
Louis Chan <louischan@oursky.com>
2024-01-25 19:32:36 +0800

    Output none and client_secret_post for token_endpoint_auth_methods_supported
    ref #3540

--- pkg/lib/oauth/metadata.go
+++ pkg/lib/oauth/metadata.go
@@ -14,7 +14,8 @@ func (p *MetadataProvider) PopulateMetadata(meta map[string]interface{}) {
 	meta["grant_types_supported"] = []string{"authorization_code", "refresh_token"}
 	meta["code_challenge_methods_supported"] = []string{pkce.CodeChallengeMethodS256}
 	meta["revocation_endpoint"] = p.Endpoints.RevokeEndpointURL().String()
-	// We do not support confidential client (i.e. client_secret) at the moment,
-	// so we do not perform authentication at all at the token endpoint.
-	meta["token_endpoint_auth_methods_supported"] = []string{}
+	// The default is client_secret_basic if this key is omitted.
+	// See https://openid.net/specs/openid-connect-discovery-1_0.html#:~:text=passed%20by%20reference.-,token_endpoint_auth_methods_supported,-OPTIONAL.%20JSON%20array
+	// See https://openid.net/specs/openid-connect-core-1_0.html#ClientAuthentication:~:text=The%20Client%20does%20not%20authenticate%20itself%20at%20the%20Token%20Endpoint
+	meta["token_endpoint_auth_methods_supported"] = []string{"none", "client_secret_post"}
 }
