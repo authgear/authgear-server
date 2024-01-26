570eee120949f49918c68077b4ca9a1cc55e8e35
Newman Chow <newman@oursky.com>
2024-01-26 19:03:25 +0800

    Add test for final newline rule #3666

---
+++ devtools/gotemplatelinter/final_newline_rule_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestFinalNewlineRule(t *testing.T) {
+	Convey("Given a empty file", t, func() {
+		html := ``
+		violations := FinalNewlineRule{}.Check(html, "test.html")
+		Convey("Then I expect to see a lint violation", func() {
+			So(len(violations), ShouldEqual, 0)
+		})
+	})
+
+	Convey("Given a file with a single line", t, func() {
+		html := `<!DOCTYPE html><html lang="en"><head><title>Test</title></head><body><p>Test</p></body></html>`
+		violations := FinalNewlineRule{}.Check(html, "test.html")
+		Convey("Then I expect to see a lint violation", func() {
+			So(len(violations), ShouldEqual, 1)
+			So(violations[0].Path, ShouldEqual, "test.html")
+			So(violations[0].Line, ShouldEqual, 1)
+			So(violations[0].Column, ShouldEqual, 95)
+			So(violations[0].Message, ShouldEqual, "File does not end with a newline (final-newline)")
+		})
+	})
+
+	Convey("Given a file with a single line and a newline", t, func() {
+		html := `<!DOCTYPE html><html lang="en"><head><title>Test</title></head><body><p>Test</p></body></html>
+`
+		violations := FinalNewlineRule{}.Check(html, "test.html")
+		Convey("Then I expect to see no violation", func() {
+			So(len(violations), ShouldEqual, 0)
+		})
+	})
+
+	Convey("Given a file with a single character and a newline", t, func() {
+		html := `A
+`
+		violations := FinalNewlineRule{}.Check(html, "test.html")
+		Convey("Then I expect to see no violation", func() {
+			So(len(violations), ShouldEqual, 0)
+		})
+	})
+
+	Convey("Given a file with multiple lines", t, func() {
+		html := `<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Test</title>
+</head>
+<body>
+<p>Test</p>
+</body>
+</html>`
+		violations := FinalNewlineRule{}.Check(html, "test.html")
+		Convey("Then I expect to see a lint violation", func() {
+			So(len(violations), ShouldEqual, 1)
+			So(violations[0].Path, ShouldEqual, "test.html")
+			So(violations[0].Line, ShouldEqual, 9)
+			So(violations[0].Column, ShouldEqual, 8)
+			So(violations[0].Message, ShouldEqual, "File does not end with a newline (final-newline)")
+		})
+	})
+}
