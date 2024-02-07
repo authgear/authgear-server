36598a21994b9d4599184655f4b6e1ec94fe0400
Davis Tang <ngkaokis@gmail.com>
2024-02-07 16:37:25 +0800

    Fix test
    ref #3695

--- pkg/auth/handler/webapp/authflow_controller_test.go
+++ pkg/auth/handler/webapp/authflow_controller_test.go
@@ -38,6 +38,9 @@ func (*NoopNavigator) NavigateNonRecoverableError(r *http.Request, u *url.URL, e
 func (*NoopNavigator) NavigateSelectAccount(result *webapp.Result) {
 }
 
+func (*NoopNavigator) NavigateChangePasswordSuccessPage(screen *webapp.AuthflowScreenWithFlowResponse, r *http.Request, webSessionID string, result *webapp.Result) {
+}
+
 func NewNoopAuthflowNavigator() *NoopNavigator {
 	return &NoopNavigator{}
 }
