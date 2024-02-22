dac9e842db538715d91712f003bcfe3435cf4ae9
Louis Chan <louischan@oursky.com>
2024-02-22 15:01:09 +0800

    Validate role group key

---
+++ pkg/lib/rolesgroups/key.go
@@ -0,0 +1,19 @@
+package rolesgroups
+
+import (
+	"github.com/authgear/authgear-server/pkg/util/validation"
+)
+
+var KeySchema = validation.NewSimpleSchema(`
+	{
+		"type": "string",
+		"minLength": 1,
+		"maxLength": 40,
+		"pattern": "[a-zA-Z_][a-zA-Z0-9:_]*",
+		"format": "x_role_group_key"
+	}
+`)
+
+func ValidateKey(key string) error {
+	return KeySchema.Validator().ValidateValue(key)
+}
