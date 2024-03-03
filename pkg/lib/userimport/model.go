89f0dd03197bc2d9b1985acefa7d9a7a30770c35
Louis Chan <louischan@oursky.com>
2024-03-03 22:51:34 +0800

    Prepare user import models

---
+++ pkg/lib/userimport/model.go
@@ -0,0 +1,314 @@
+package userimport
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+
+	"github.com/authgear/authgear-server/pkg/util/validation"
+)
+
+var standardAttributeKeys []string = []string{
+	"preferred_username",
+	"email",
+	"phone_number",
+	"name",
+	"given_name",
+	"family_name",
+	"middle_name",
+	"nickname",
+	"profile",
+	"picture",
+	"website",
+	"gender",
+	"birthdate",
+	"zoneinfo",
+	"locale",
+	"address",
+}
+
+func mapGetNullable[M ~map[string]interface{}, T constraints.Ordered | ~bool](m M, key string) (*T, bool) {
+	iface, ok := m[key]
+	if !ok {
+		return nil, false
+	}
+	if iface == nil {
+		return nil, true
+	}
+	v, ok := iface.(T)
+	if !ok {
+		var t T
+		panic(fmt.Errorf("%v is expected to be of type %T, but was %T", key, t, v))
+	}
+	return &v, true
+}
+
+func mapGetNonNull[M ~map[string]interface{}, T constraints.Ordered | ~bool](m M, key string) (T, bool) {
+	var t T
+	iface, ok := m[key]
+	if !ok {
+		return t, false
+	}
+	if iface == nil {
+		panic(fmt.Errorf("%v is expected to be non-null", key))
+	}
+	v, ok := iface.(T)
+	if !ok {
+		panic(fmt.Errorf("%v is expected to be of type %T, but was %T", key, t, v))
+	}
+	return v, true
+}
+
+func mapGetNonNullMap[M ~map[string]interface{}, T ~map[string]interface{}](m M, key string) (T, bool) {
+	iface, ok := m[key]
+	if !ok {
+		return nil, false
+	}
+	if iface == nil {
+		panic(fmt.Errorf("%v is expected to be non-null", key))
+	}
+	v, ok := iface.(T)
+	if !ok {
+		var t T
+		panic(fmt.Errorf("%v is expected to be of type %T, but was %T", key, t, v))
+	}
+	return v, true
+}
+
+func mapGetArrayOfNonNullItems[M ~map[string]interface{}, T constraints.Ordered | ~bool](m M, key string) ([]T, bool) {
+	iface, ok := m[key]
+	if !ok {
+		return nil, false
+	}
+
+	var ts []T
+	sliceIface, ok := iface.([]interface{})
+	if !ok {
+		panic(fmt.Errorf("%v is expected to be of type %T, but was %T", key, ts, iface))
+	}
+
+	for _, valueIface := range sliceIface {
+		v, ok := valueIface.(T)
+		if !ok {
+			panic(fmt.Errorf("%v is expected to be of type %T, but was %T", key, ts, iface))
+		}
+		ts = append(ts, v)
+	}
+
+	return ts, true
+}
+
+const (
+	IdentifierEmail             = "email"
+	IdentifierPreferredUsername = "preferred_username"
+	IdentifierPhoneNumber       = "phone_number"
+)
+
+const (
+	PasswordTypeBcrypt = "bcrypt"
+)
+
+type Password map[string]interface{}
+
+func (m Password) Type() string {
+	return m["type"].(string)
+}
+
+func (m Password) PasswordHash() string {
+	return m["password_hash"].(string)
+}
+
+type TOTP map[string]interface{}
+
+func (m TOTP) Secret() string {
+	return m["secret"].(string)
+}
+
+type MFA map[string]interface{}
+
+func (m MFA) Email() (*string, bool) {
+	return mapGetNullable[MFA, string](m, "email")
+}
+
+func (m MFA) PhoneNumber() (*string, bool) {
+	return mapGetNullable[MFA, string](m, "phone_number")
+}
+
+func (m MFA) Password() (map[string]interface{}, bool) {
+	return mapGetNonNullMap[MFA, map[string]interface{}](m, "password")
+}
+
+func (m MFA) TOTP() (map[string]interface{}, bool) {
+	return mapGetNonNullMap[MFA, map[string]interface{}](m, "totp")
+}
+
+type Record map[string]interface{}
+
+func (m Record) PreferredUsername() (*string, bool) {
+	return mapGetNullable[Record, string](m, "preferred_username")
+}
+
+func (m Record) Email() (*string, bool) {
+	return mapGetNullable[Record, string](m, "email")
+}
+
+func (m Record) PhoneNumber() (*string, bool) {
+	return mapGetNullable[Record, string](m, "phone_number")
+}
+
+func (m Record) Disabled() (bool, bool) {
+	return mapGetNonNull[Record, bool](m, "disabled")
+}
+
+func (m Record) EmailVerified() (bool, bool) {
+	return mapGetNonNull[Record, bool](m, "email_verified")
+}
+
+func (m Record) PhoneNumberVerified() (bool, bool) {
+	return mapGetNonNull[Record, bool](m, "phone_number_verified")
+}
+
+func (m Record) StandardAttributes() (map[string]interface{}, bool) {
+	attrs := make(map[string]interface{})
+	for key := range m {
+		for _, k := range standardAttributeKeys {
+			if key == k {
+				attrs[key] = m[key]
+			}
+		}
+	}
+	if len(attrs) > 0 {
+		return attrs, true
+	}
+	return nil, false
+}
+
+func (m Record) CustomAttributes() (map[string]interface{}, bool) {
+	return mapGetNonNullMap[Record, map[string]interface{}](m, "custom_attributes")
+}
+
+func (m Record) Roles() ([]string, bool) {
+	return mapGetArrayOfNonNullItems[Record, string](m, "roles")
+}
+
+func (m Record) Groups() ([]string, bool) {
+	return mapGetArrayOfNonNullItems[Record, string](m, "groups")
+}
+
+func (m Record) Password() (map[string]interface{}, bool) {
+	return mapGetNonNullMap[Record, map[string]interface{}](m, "password")
+}
+
+func (m Record) MFA() (map[string]interface{}, bool) {
+	return mapGetNonNullMap[Record, map[string]interface{}](m, "mfa")
+}
+
+var RecordSchema = validation.NewSimpleSchema(`
+{
+	"type": "object",
+	"additionalProperties": false,
+	"properties": {
+		"preferred_username": { "type": ["string", "null"] },
+		"email": { "type": ["string", "null"] },
+		"phone_number": { "type": ["string", "null"] },
+
+		"disabled": { "type": "boolean" },
+
+		"email_verified": { "type": "boolean" },
+		"phone_number_verified": { "type": "boolean" },
+
+		"name": { "type": ["string", "null"] },
+		"given_name": { "type": ["string", "null"] },
+		"family_name": { "type": ["string", "null"] },
+		"middle_name": { "type": ["string", "null"] },
+		"nickname": { "type": ["string", "null"] },
+		"profile": { "type": ["string", "null"] },
+		"picture": { "type": ["string", "null"] },
+		"website": { "type": ["string", "null"] },
+		"gender": { "type": ["string", "null"] },
+		"birthdate": { "type": ["string", "null"] },
+		"zoneinfo": { "type": ["string", "null"] },
+		"locale": { "type": ["string", "null"] },
+		"address": { "type": ["object", "null"] },
+
+		"custom_attributes": { "type": "object" },
+
+		"roles": { "type": "array", "items": { "type": "string" } },
+		"groups": { "type": "array", "items": { "type": "string" } },
+
+		"password": {
+			"type": "object",
+			"properties": {
+				"type": {
+					"type": "string",
+					"enum": ["bcrypt"]
+				},
+				"password_hash": {
+					"type": "string"
+				}
+			},
+			"required": ["type", "password_hash"]
+		},
+
+		"mfa": {
+			"type": "object",
+			"properties": {
+				"email": { "type": ["string", "null"] },
+				"phone_number": { "type": ["string", "null"] },
+				"password": {
+					"type": "object",
+					"properties": {
+						"type": {
+							"type": "string",
+							"enum": ["bcrypt"]
+						},
+						"password_hash": {
+							"type": "string"
+						}
+					},
+					"required": ["type", "password_hash"]
+				},
+				"totp": {
+					"type": "object",
+					"properties": {
+						"secret": { "type": "string" }
+					},
+					"required": ["secret"]
+				}
+			}
+		}
+	}
+}
+`)
+
+type Request struct {
+	Upsert     bool   `json:"upsert,omitempty"`
+	Identifier string `json:"identifier,omitempty"`
+	// Records is json.RawMessage because we want to delay the deserialization until we actually process the record.
+	Records []json.RawMessage `json:"records,omitempty"`
+}
+
+var RequestSchema = validation.NewSimpleSchema(`
+{
+	"type": "object",
+	"additionalProperties": false,
+	"properties": {
+		"upsert": {
+			"type": "boolean"
+		},
+		"identifier": {
+			"type": "string",
+			"enum": ["preferred_username", "email", "phone_number"]
+		},
+		"records": {
+			"type": "array",
+			"minItems": 1,
+			"items": {
+				"type": "object"
+			}
+		}
+	},
+	"required": ["identifier", "records"]
+}
+`)
---
+++ pkg/lib/userimport/model_test.go
@@ -0,0 +1,718 @@
+package userimport
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/authgear/authgear-server/pkg/util/httputil"
+)
+
+func TestRequest(t *testing.T) {
+	requestBody := `
+{
+	"upsert": true,
+	"identifier": "preferred_username",
+	"records": [
+		{
+			"preferred_username": "johndoe",
+			"email": "johndoe@example.com",
+			"phone_number": "+85298765432",
+
+			"disabled": true,
+
+			"email_verified": true,
+			"phone_number_verified": true,
+
+			"name": "John Doe",
+			"given_name": "John",
+			"family_name": "Doe",
+			"middle_name": "middle",
+			"nickname": "JohnDoe",
+			"profile": "https://example.com/profile",
+			"picture": "https://example.com/picture",
+			"website": "https://example.com/website",
+			"gender": "male",
+			"birthdate": "1970-01-01",
+			"zoneinfo": "Asia/Hong_Kong",
+			"locale": "zh-HK",
+			"address": {
+				"formatted": "1 Unnamed Road, Central, Hong Kong Island, HK",
+				"street_address": "1 Unnamed Road",
+				"locality": "Central",
+				"region": "Hong Kong Island",
+				"postal_code": "N/A",
+				"country": "HK"
+			},
+
+			"custom_attributes": {
+				"member_id": "123456789"
+			},
+
+			"roles": ["role_a", "role_b"],
+			"groups": ["group_a", "group_b"],
+
+			"password": {
+				"type": "bcrypt",
+				"password_hash": "$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy"
+			},
+
+			"mfa": {
+				"email": "johndoe@example.com",
+				"phone_number": "+85298765432",
+				"password": {
+					"type": "bcrypt",
+					"password_hash": "$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy"
+				},
+				"totp": {
+					"secret": "JBSWY3DPEHPK3PXP"
+				}
+			}
+		}
+	]
+}
+	`
+	Convey("Test serialization of Request", t, func() {
+		var request Request
+		err := json.Unmarshal([]byte(requestBody), &request)
+		So(err, ShouldBeNil)
+
+		serialized, err := json.Marshal(request)
+		So(err, ShouldBeNil)
+
+		So(string(serialized), ShouldEqualJSON, requestBody)
+	})
+
+	Convey("Request JSON Schema", t, func() {
+		test := func(requestBody string, errorString string) {
+			var request Request
+			r, _ := http.NewRequest("POST", "/", strings.NewReader(requestBody))
+			r.Header.Set("Content-Type", "application/json")
+			w := httptest.NewRecorder()
+
+			err := httputil.BindJSONBody(r, w, RequestSchema.Validator(), &request)
+			if errorString == "" {
+				So(err, ShouldBeNil)
+			} else {
+				So(err, ShouldBeError, errorString)
+			}
+		}
+
+		test(requestBody, "")
+		test("{}", `invalid request body:
+<root>: required
+  map[actual:<nil> expected:[identifier records] missing:[identifier records]]`)
+		test(`
+{
+	"identifier": "foobar",
+	"records": []
+}
+		`, `invalid request body:
+/identifier: enum
+  map[actual:foobar expected:[preferred_username email phone_number]]
+/records: minItems
+  map[actual:0 expected:1]`)
+	})
+}
+
+func TestRecord(t *testing.T) {
+	Convey("Record", t, func() {
+		Convey("preferred_username", func() {
+			r := Record{}
+
+			_, ok := r.PreferredUsername()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"preferred_username": nil,
+			}
+
+			v, ok := r.PreferredUsername()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldBeNil)
+
+			r = Record{
+				"preferred_username": "a",
+			}
+
+			v, ok = r.PreferredUsername()
+			So(ok, ShouldBeTrue)
+			So(*v, ShouldEqual, "a")
+		})
+
+		Convey("email", func() {
+			r := Record{}
+
+			_, ok := r.Email()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"email": nil,
+			}
+
+			v, ok := r.Email()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldBeNil)
+
+			r = Record{
+				"email": "a",
+			}
+
+			v, ok = r.Email()
+			So(ok, ShouldBeTrue)
+			So(*v, ShouldEqual, "a")
+		})
+
+		Convey("phone_number", func() {
+			r := Record{}
+
+			_, ok := r.PhoneNumber()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"phone_number": nil,
+			}
+
+			v, ok := r.PhoneNumber()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldBeNil)
+
+			r = Record{
+				"phone_number": "a",
+			}
+
+			v, ok = r.PhoneNumber()
+			So(ok, ShouldBeTrue)
+			So(*v, ShouldEqual, "a")
+		})
+
+		Convey("disabled", func() {
+			r := Record{}
+
+			_, ok := r.Disabled()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"disabled": nil,
+			}
+
+			So(func() {
+				r.Disabled()
+			}, ShouldPanicWith, fmt.Errorf("disabled is expected to be non-null"))
+
+			r = Record{
+				"disabled": true,
+			}
+
+			v, ok := r.Disabled()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldEqual, true)
+		})
+
+		Convey("email_verified", func() {
+			r := Record{}
+
+			_, ok := r.EmailVerified()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"email_verified": nil,
+			}
+
+			So(func() {
+				r.EmailVerified()
+			}, ShouldPanicWith, fmt.Errorf("email_verified is expected to be non-null"))
+
+			r = Record{
+				"email_verified": true,
+			}
+
+			v, ok := r.EmailVerified()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldEqual, true)
+		})
+
+		Convey("phone_number_verified", func() {
+			r := Record{}
+
+			_, ok := r.PhoneNumberVerified()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"phone_number_verified": nil,
+			}
+
+			So(func() {
+				r.PhoneNumberVerified()
+			}, ShouldPanicWith, fmt.Errorf("phone_number_verified is expected to be non-null"))
+
+			r = Record{
+				"phone_number_verified": true,
+			}
+
+			v, ok := r.PhoneNumberVerified()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldEqual, true)
+		})
+
+		Convey("standard_attributes", func() {
+			r := Record{}
+
+			_, ok := r.StandardAttributes()
+			So(ok, ShouldBeFalse)
+
+			recordString := `
+{
+	"preferred_username": "johndoe",
+	"email": "johndoe@example.com",
+	"phone_number": "+85298765432",
+
+	"disabled": true,
+
+	"email_verified": true,
+	"phone_number_verified": true,
+
+	"name": "John Doe",
+	"given_name": "John",
+	"family_name": "Doe",
+	"middle_name": "middle",
+	"nickname": "JohnDoe",
+	"profile": "https://example.com/profile",
+	"picture": "https://example.com/picture",
+	"website": "https://example.com/website",
+	"gender": "male",
+	"birthdate": "1970-01-01",
+	"zoneinfo": "Asia/Hong_Kong",
+	"locale": "zh-HK",
+	"address": {
+		"formatted": "1 Unnamed Road, Central, Hong Kong Island, HK",
+		"street_address": "1 Unnamed Road",
+		"locality": "Central",
+		"region": "Hong Kong Island",
+		"postal_code": "N/A",
+		"country": "HK"
+	},
+
+	"custom_attributes": {
+		"member_id": "123456789"
+	},
+
+	"roles": ["role_a", "role_b"],
+	"groups": ["group_a", "group_b"],
+
+	"password": {
+		"type": "bcrypt",
+		"password_hash": "$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy"
+	},
+
+	"mfa": {
+		"email": "johndoe@example.com",
+		"phone_number": "+85298765432",
+		"password": {
+			"type": "bcrypt",
+			"password_hash": "$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy"
+		},
+		"totp": {
+			"secret": "JBSWY3DPEHPK3PXP"
+		}
+	}
+}
+		`
+
+			r = Record{}
+			err := json.Unmarshal([]byte(recordString), &r)
+			So(err, ShouldBeNil)
+
+			attrs, ok := r.StandardAttributes()
+			So(ok, ShouldBeTrue)
+			So(attrs, ShouldResemble, map[string]interface{}{
+				"preferred_username": "johndoe",
+				"email":              "johndoe@example.com",
+				"phone_number":       "+85298765432",
+				"name":               "John Doe",
+				"given_name":         "John",
+				"family_name":        "Doe",
+				"middle_name":        "middle",
+				"nickname":           "JohnDoe",
+				"profile":            "https://example.com/profile",
+				"picture":            "https://example.com/picture",
+				"website":            "https://example.com/website",
+				"gender":             "male",
+				"birthdate":          "1970-01-01",
+				"zoneinfo":           "Asia/Hong_Kong",
+				"locale":             "zh-HK",
+				"address": map[string]interface{}{
+					"formatted":      "1 Unnamed Road, Central, Hong Kong Island, HK",
+					"street_address": "1 Unnamed Road",
+					"locality":       "Central",
+					"region":         "Hong Kong Island",
+					"postal_code":    "N/A",
+					"country":        "HK",
+				},
+			})
+		})
+
+		Convey("custom_attributes", func() {
+			r := Record{}
+
+			_, ok := r.CustomAttributes()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"custom_attributes": nil,
+			}
+
+			So(func() {
+				r.CustomAttributes()
+			}, ShouldPanicWith, fmt.Errorf("custom_attributes is expected to be non-null"))
+
+			r = Record{
+				"custom_attributes": map[string]interface{}{},
+			}
+
+			v, ok := r.CustomAttributes()
+			So(ok, ShouldBeTrue)
+			So(len(v), ShouldEqual, 0)
+
+			r = Record{
+				"custom_attributes": map[string]interface{}{
+					"a": "b",
+				},
+			}
+
+			v, ok = r.CustomAttributes()
+			So(ok, ShouldBeTrue)
+			So(len(v), ShouldEqual, 1)
+		})
+
+		Convey("roles", func() {
+			r := Record{}
+			_, ok := r.Roles()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"roles": nil,
+			}
+
+			So(func() {
+				r.Roles()
+			}, ShouldPanicWith, fmt.Errorf("roles is expected to be of type []string, but was <nil>"))
+
+			r = Record{
+				"roles": []interface{}{"a"},
+			}
+
+			v, ok := r.Roles()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldResemble, []string{"a"})
+		})
+
+		Convey("groups", func() {
+			r := Record{}
+			_, ok := r.Groups()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"groups": nil,
+			}
+
+			So(func() {
+				r.Groups()
+			}, ShouldPanicWith, fmt.Errorf("groups is expected to be of type []string, but was <nil>"))
+
+			r = Record{
+				"groups": []interface{}{"a"},
+			}
+
+			v, ok := r.Groups()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldResemble, []string{"a"})
+		})
+
+		Convey("password", func() {
+			r := Record{}
+			_, ok := r.Password()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"password": nil,
+			}
+
+			So(func() {
+				r.Password()
+			}, ShouldPanicWith, fmt.Errorf("password is expected to be non-null"))
+
+			r = Record{
+				"password": map[string]interface{}{},
+			}
+
+			v, ok := r.Password()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldNotBeNil)
+			So(len(v), ShouldEqual, 0)
+		})
+
+		Convey("mfa", func() {
+			r := Record{}
+			_, ok := r.MFA()
+			So(ok, ShouldBeFalse)
+
+			r = Record{
+				"mfa": nil,
+			}
+
+			So(func() {
+				r.MFA()
+			}, ShouldPanicWith, fmt.Errorf("mfa is expected to be non-null"))
+
+			r = Record{
+				"mfa": map[string]interface{}{},
+			}
+
+			v, ok := r.MFA()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldNotBeNil)
+			So(len(v), ShouldEqual, 0)
+		})
+	})
+}
+
+func TestPassword(t *testing.T) {
+	Convey("Password", t, func() {
+		p := Password{
+			"type":          "bcrypt",
+			"password_hash": "hash",
+		}
+
+		So(p.Type(), ShouldEqual, PasswordTypeBcrypt)
+		So(p.PasswordHash(), ShouldEqual, "hash")
+	})
+}
+
+func TestTOTP(t *testing.T) {
+	Convey("TOTP", t, func() {
+		p := TOTP{
+			"secret": "secret",
+		}
+
+		So(p.Secret(), ShouldEqual, "secret")
+	})
+}
+
+func TestMFA(t *testing.T) {
+	Convey("MFA", t, func() {
+		Convey("email", func() {
+			r := MFA{}
+
+			_, ok := r.Email()
+			So(ok, ShouldBeFalse)
+
+			r = MFA{
+				"email": nil,
+			}
+
+			v, ok := r.Email()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldBeNil)
+
+			r = MFA{
+				"email": "a",
+			}
+
+			v, ok = r.Email()
+			So(ok, ShouldBeTrue)
+			So(*v, ShouldEqual, "a")
+		})
+
+		Convey("phone_number", func() {
+			r := MFA{}
+
+			_, ok := r.PhoneNumber()
+			So(ok, ShouldBeFalse)
+
+			r = MFA{
+				"phone_number": nil,
+			}
+
+			v, ok := r.PhoneNumber()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldBeNil)
+
+			r = MFA{
+				"phone_number": "a",
+			}
+
+			v, ok = r.PhoneNumber()
+			So(ok, ShouldBeTrue)
+			So(*v, ShouldEqual, "a")
+		})
+
+		Convey("password", func() {
+			r := MFA{}
+			_, ok := r.Password()
+			So(ok, ShouldBeFalse)
+
+			r = MFA{
+				"password": nil,
+			}
+
+			So(func() {
+				r.Password()
+			}, ShouldPanicWith, fmt.Errorf("password is expected to be non-null"))
+
+			r = MFA{
+				"password": map[string]interface{}{},
+			}
+
+			v, ok := r.Password()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldNotBeNil)
+			So(len(v), ShouldEqual, 0)
+		})
+
+		Convey("totp", func() {
+			r := MFA{}
+			_, ok := r.TOTP()
+			So(ok, ShouldBeFalse)
+
+			r = MFA{
+				"totp": nil,
+			}
+
+			So(func() {
+				r.TOTP()
+			}, ShouldPanicWith, fmt.Errorf("totp is expected to be non-null"))
+
+			r = MFA{
+				"totp": map[string]interface{}{},
+			}
+
+			v, ok := r.TOTP()
+			So(ok, ShouldBeTrue)
+			So(v, ShouldNotBeNil)
+			So(len(v), ShouldEqual, 0)
+		})
+	})
+}
+
+func TestRecordSchema(t *testing.T) {
+	Convey("Record JSON Schema", t, func() {
+		recordString := `
+{
+	"preferred_username": "johndoe",
+	"email": "johndoe@example.com",
+	"phone_number": "+85298765432",
+
+	"disabled": true,
+
+	"email_verified": true,
+	"phone_number_verified": true,
+
+	"name": "John Doe",
+	"given_name": "John",
+	"family_name": "Doe",
+	"middle_name": "middle",
+	"nickname": "JohnDoe",
+	"profile": "https://example.com/profile",
+	"picture": "https://example.com/picture",
+	"website": "https://example.com/website",
+	"gender": "male",
+	"birthdate": "1970-01-01",
+	"zoneinfo": "Asia/Hong_Kong",
+	"locale": "zh-HK",
+	"address": {
+		"formatted": "1 Unnamed Road, Central, Hong Kong Island, HK",
+		"street_address": "1 Unnamed Road",
+		"locality": "Central",
+		"region": "Hong Kong Island",
+		"postal_code": "N/A",
+		"country": "HK"
+	},
+
+	"custom_attributes": {
+		"member_id": "123456789"
+	},
+
+	"roles": ["role_a", "role_b"],
+	"groups": ["group_a", "group_b"],
+
+	"password": {
+		"type": "bcrypt",
+		"password_hash": "$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy"
+	},
+
+	"mfa": {
+		"email": "johndoe@example.com",
+		"phone_number": "+85298765432",
+		"password": {
+			"type": "bcrypt",
+			"password_hash": "$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy"
+		},
+		"totp": {
+			"secret": "JBSWY3DPEHPK3PXP"
+		}
+	}
+}
+		`
+
+		test := func(recordString string, errorString string) {
+			var record Record
+			r, _ := http.NewRequest("POST", "/", strings.NewReader(recordString))
+			r.Header.Set("Content-Type", "application/json")
+			w := httptest.NewRecorder()
+
+			err := httputil.BindJSONBody(r, w, RecordSchema.Validator(), &record)
+			if errorString == "" {
+				So(err, ShouldBeNil)
+			} else {
+				So(err, ShouldBeError, errorString)
+			}
+		}
+
+		test(recordString, "")
+
+		allNulls := `
+{
+	"preferred_username": null,
+	"email": null,
+	"phone_number": null,
+
+	"name": null,
+	"given_name": null,
+	"family_name": null,
+	"middle_name": null,
+	"nickname": null,
+	"profile": null,
+	"picture": null,
+	"website": null,
+	"gender": null,
+	"birthdate": null,
+	"zoneinfo": null,
+	"locale": null,
+	"address": null,
+
+	"mfa": {
+		"email": null,
+		"phone_number": null
+	}
+}
+`
+		test(allNulls, "")
+
+		test(`{
+			"mfa": {
+				"password": {},
+				"totp": {}
+			}
+		}`, `invalid request body:
+/mfa/password: required
+  map[actual:<nil> expected:[password_hash type] missing:[password_hash type]]
+/mfa/totp: required
+  map[actual:<nil> expected:[secret] missing:[secret]]`)
+	})
+}
