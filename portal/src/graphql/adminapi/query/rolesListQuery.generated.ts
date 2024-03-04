982170f78c3f323d9e176519bb4a9e773ea910e8
Davis Tang <ngkaokis@gmail.com>
2024-03-04 16:17:51 +0800

    Integrate roles list query
    ref #3916

---
+++ portal/src/graphql/adminapi/query/rolesListQuery.generated.ts
@@ -0,0 +1,73 @@
+import * as Types from '../globalTypes.generated';
+
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+const defaultOptions = {} as const;
+export type RolesListFragment = { __typename?: 'RoleConnection', totalCount?: number | null, edges?: Array<{ __typename?: 'RoleEdge', cursor: string, node?: { __typename?: 'Role', id: string, createdAt: any, key: string, name?: string | null, description?: string | null } | null } | null> | null };
+
+export type RolesListQueryQueryVariables = Types.Exact<{
+  searchKeyword: Types.Scalars['String']['input'];
+  pageSize: Types.Scalars['Int']['input'];
+  cursor?: Types.InputMaybe<Types.Scalars['String']['input']>;
+}>;
+
+
+export type RolesListQueryQuery = { __typename?: 'Query', roles?: { __typename?: 'RoleConnection', totalCount?: number | null, edges?: Array<{ __typename?: 'RoleEdge', cursor: string, node?: { __typename?: 'Role', id: string, createdAt: any, key: string, name?: string | null, description?: string | null } | null } | null> | null } | null };
+
+export const RolesListFragmentDoc = gql`
+    fragment RolesList on RoleConnection {
+  edges {
+    node {
+      id
+      createdAt
+      key
+      name
+      description
+    }
+    cursor
+  }
+  totalCount
+}
+    `;
+export const RolesListQueryDocument = gql`
+    query RolesListQuery($searchKeyword: String!, $pageSize: Int!, $cursor: String) {
+  roles(first: $pageSize, after: $cursor, searchKeyword: $searchKeyword) {
+    ...RolesList
+  }
+}
+    ${RolesListFragmentDoc}`;
+
+/**
+ * __useRolesListQueryQuery__
+ *
+ * To run a query within a React component, call `useRolesListQueryQuery` and pass it any options that fit your needs.
+ * When your component renders, `useRolesListQueryQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useRolesListQueryQuery({
+ *   variables: {
+ *      searchKeyword: // value for 'searchKeyword'
+ *      pageSize: // value for 'pageSize'
+ *      cursor: // value for 'cursor'
+ *   },
+ * });
+ */
+export function useRolesListQueryQuery(baseOptions: Apollo.QueryHookOptions<RolesListQueryQuery, RolesListQueryQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<RolesListQueryQuery, RolesListQueryQueryVariables>(RolesListQueryDocument, options);
+      }
+export function useRolesListQueryLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<RolesListQueryQuery, RolesListQueryQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<RolesListQueryQuery, RolesListQueryQueryVariables>(RolesListQueryDocument, options);
+        }
+export function useRolesListQuerySuspenseQuery(baseOptions?: Apollo.SuspenseQueryHookOptions<RolesListQueryQuery, RolesListQueryQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<RolesListQueryQuery, RolesListQueryQueryVariables>(RolesListQueryDocument, options);
+        }
+export type RolesListQueryQueryHookResult = ReturnType<typeof useRolesListQueryQuery>;
+export type RolesListQueryLazyQueryHookResult = ReturnType<typeof useRolesListQueryLazyQuery>;
+export type RolesListQuerySuspenseQueryHookResult = ReturnType<typeof useRolesListQuerySuspenseQuery>;
+export type RolesListQueryQueryResult = Apollo.QueryResult<RolesListQueryQuery, RolesListQueryQueryVariables>;
