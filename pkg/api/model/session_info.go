752d26227c6a6989360a0ebbc0b1ca33a8c1ae28
Davis Tang <ngkaokis@gmail.com>
2024-03-05 13:09:47 +0800

    Insert role header only when effective roles > 0
    ref #3830

--- pkg/api/model/session_info.go
+++ pkg/api/model/session_info.go
@@ -45,7 +45,9 @@ func (i *SessionInfo) PopulateHeaders(rw http.ResponseWriter) {
 	rw.Header().Set(headerUserAnonymous, strconv.FormatBool(i.UserAnonymous))
 	rw.Header().Set(headerUserVerified, strconv.FormatBool(i.UserVerified))
 	rw.Header().Set(headerUserCanReauthenticate, strconv.FormatBool(i.UserCanReauthenticate))
-	rw.Header().Set(headerUserRoles, strings.Join(i.EffectiveRoles, ","))
+	if len(i.EffectiveRoles) > 0 {
+		rw.Header().Set(headerUserRoles, strings.Join(i.EffectiveRoles, ","))
+	}
 
 	rw.Header().Set(headerSessionAmr, strings.Join(i.SessionAMR, " "))
 	if !i.AuthenticatedAt.IsZero() {
