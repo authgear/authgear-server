9ad692bf8d109ebdd7e28c6d9808294422fd6b3e
Davis Tang <ngkaokis@gmail.com>
2024-03-05 15:22:07 +0800

    Configure routes for roles and groups
    ref #3901

--- portal/src/AppRoot.tsx
+++ portal/src/AppRoot.tsx
@@ -9,6 +9,10 @@ import ShowLoading from "./ShowLoading";
 import CookieLifetimeConfigurationScreen from "./graphql/portal/CookieLifetimeConfigurationScreen";
 import { AppContext } from "./context/AppContext";
 
+const RolesScreen = lazy(async () => import("./graphql/adminapi/RolesScreen"));
+const GroupsScreen = lazy(
+  async () => import("./graphql/adminapi/GroupsScreen")
+);
 const UsersScreen = lazy(async () => import("./graphql/adminapi/UsersScreen"));
 const AddUserScreen = lazy(
   async () => import("./graphql/adminapi/AddUserScreen")
@@ -198,101 +202,125 @@ const AppRoot: React.VFC = function AppRoot() {
               />
             </Route>
 
-            <Route path="users">
-              <Route
-                index={true}
-                element={
-                  <Suspense fallback={<ShowLoading />}>
-                    <UsersScreen />
-                  </Suspense>
-                }
-              />
-              <Route
-                path="add-user"
-                element={
-                  <Suspense fallback={<ShowLoading />}>
-                    <AddUserScreen />
-                  </Suspense>
-                }
-              />
-              <Route path=":userID">
+            <Route path="user-management">
+              <Route path="roles">
                 <Route
                   index={true}
-                  element={<Navigate to="details" replace={true} />}
+                  element={
+                    <Suspense fallback={<ShowLoading />}>
+                      <RolesScreen />
+                    </Suspense>
+                  }
                 />
-                <Route path="details">
+              </Route>
+
+              <Route path="groups">
+                <Route
+                  index={true}
+                  element={
+                    <Suspense fallback={<ShowLoading />}>
+                      <GroupsScreen />
+                    </Suspense>
+                  }
+                />
+              </Route>
+
+              <Route path="users">
+                <Route
+                  index={true}
+                  element={
+                    <Suspense fallback={<ShowLoading />}>
+                      <UsersScreen />
+                    </Suspense>
+                  }
+                />
+                <Route
+                  path="add-user"
+                  element={
+                    <Suspense fallback={<ShowLoading />}>
+                      <AddUserScreen />
+                    </Suspense>
+                  }
+                />
+                <Route path=":userID">
                   <Route
                     index={true}
-                    element={
-                      <Suspense fallback={<ShowLoading />}>
-                        <UserDetailsScreen />
-                      </Suspense>
-                    }
+                    element={<Navigate to="details" replace={true} />}
                   />
-                  <Route
-                    path="add-email"
-                    element={
-                      <Suspense fallback={<ShowLoading />}>
-                        <EmailScreen />
-                      </Suspense>
-                    }
-                  />
-                  <Route
-                    path="edit-email/:identityID"
-                    element={
-                      <Suspense fallback={<ShowLoading />}>
-                        <EmailScreen />
-                      </Suspense>
-                    }
-                  />
-                  <Route
-                    path="add-phone"
-                    element={
-                      <Suspense fallback={<ShowLoading />}>
-                        <PhoneScreen />
-                      </Suspense>
-                    }
-                  />
-                  <Route
-                    path="edit-phone/:identityID"
-                    element={
-                      <Suspense fallback={<ShowLoading />}>
-                        <PhoneScreen />
-                      </Suspense>
-                    }
-                  />
-                  <Route
-                    path="add-username"
-                    element={
-                      <Suspense fallback={<ShowLoading />}>
-                        <UsernameScreen />
-                      </Suspense>
-                    }
-                  />
-                  <Route
-                    path="edit-username/:identityID"
-                    element={
-                      <Suspense fallback={<ShowLoading />}>
-                        <UsernameScreen />
-                      </Suspense>
-                    }
-                  />
-                  <Route
-                    path="reset-password"
-                    element={
-                      <Suspense fallback={<ShowLoading />}>
-                        <ResetPasswordScreen />
-                      </Suspense>
-                    }
-                  />
-                  <Route
-                    path="edit-picture"
-                    element={
-                      <Suspense fallback={<ShowLoading />}>
-                        <EditPictureScreen />
-                      </Suspense>
-                    }
-                  />
+                  <Route path="details">
+                    <Route
+                      index={true}
+                      element={
+                        <Suspense fallback={<ShowLoading />}>
+                          <UserDetailsScreen />
+                        </Suspense>
+                      }
+                    />
+                    <Route
+                      path="add-email"
+                      element={
+                        <Suspense fallback={<ShowLoading />}>
+                          <EmailScreen />
+                        </Suspense>
+                      }
+                    />
+                    <Route
+                      path="edit-email/:identityID"
+                      element={
+                        <Suspense fallback={<ShowLoading />}>
+                          <EmailScreen />
+                        </Suspense>
+                      }
+                    />
+                    <Route
+                      path="add-phone"
+                      element={
+                        <Suspense fallback={<ShowLoading />}>
+                          <PhoneScreen />
+                        </Suspense>
+                      }
+                    />
+                    <Route
+                      path="edit-phone/:identityID"
+                      element={
+                        <Suspense fallback={<ShowLoading />}>
+                          <PhoneScreen />
+                        </Suspense>
+                      }
+                    />
+                    <Route
+                      path="add-username"
+                      element={
+                        <Suspense fallback={<ShowLoading />}>
+                          <UsernameScreen />
+                        </Suspense>
+                      }
+                    />
+                    <Route
+                      path="edit-username/:identityID"
+                      element={
+                        <Suspense fallback={<ShowLoading />}>
+                          <UsernameScreen />
+                        </Suspense>
+                      }
+                    />
+                    <Route
+                      path="reset-password"
+                      element={
+                        <Suspense fallback={<ShowLoading />}>
+                          <ResetPasswordScreen />
+                        </Suspense>
+                      }
+                    />
+                    <Route
+                      path="edit-picture"
+                      element={
+                        <Suspense fallback={<ShowLoading />}>
+                          <EditPictureScreen />
+                        </Suspense>
+                      }
+                    />
+                  </Route>
                 </Route>
               </Route>
             </Route>
---
+++ portal/src/graphql/adminapi/GroupsScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GroupsScreen: React.VFC = function GroupsScreen() {
+  return <></>;
+};
+
+export default GroupsScreen;
