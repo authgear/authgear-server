65c590e986793666d5792cf5a5aa35e230717de8
Tung Wu <tung@oursky.com>
2024-03-06 18:21:27 +0800

    Implement create role screen ui

--- portal/src/graphql/adminapi/AddRolesScreen.tsx
+++ portal/src/graphql/adminapi/AddRolesScreen.tsx
@@ -1,15 +1,32 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useContext, useMemo } from "react";
 import { useSimpleForm } from "../../hook/useSimpleForm";
 import { FormContainerBase } from "../../FormContainerBase";
 import { RoleAndGroupsFormLayout } from "../../RoleAndGroupsFormLayout";
 import { BreadcrumbItem } from "../../NavBreadcrumb";
-import { FormattedMessage } from "@oursky/react-messageformat";
+import {
+  Context as MessageContext,
+  FormattedMessage,
+} from "@oursky/react-messageformat";
+import FormTextField from "../../FormTextField";
+import WidgetDescription from "../../WidgetDescription";
+import PrimaryButton from "../../PrimaryButton";
+import DefaultButton from "../../DefaultButton";
 
-interface FormState {}
+interface FormState {
+  roleKey: string;
+  roleName: string;
+  roleDescription: string;
+}
 
-const defaultState: FormState = {};
+const defaultState: FormState = {
+  roleKey: "",
+  roleName: "",
+  roleDescription: "",
+};
 
 const AddRolesScreen: React.VFC = function AddRolesScreen() {
+  const { renderToString } = useContext(MessageContext);
+
   const submit = useCallback(async () => {}, []);
 
   const form = useSimpleForm({
@@ -18,6 +35,8 @@ const AddRolesScreen: React.VFC = function AddRolesScreen() {
     defaultState,
     submit,
   });
+
+  const { state: formState, setState: setFormState } = form;
 
   const breadcrumbs = useMemo<BreadcrumbItem[]>(() => {
     return [
@@ -29,11 +48,80 @@ const AddRolesScreen: React.VFC = function AddRolesScreen() {
     ];
   }, []);
 
+  const onFormStateChangeCallbacks = useMemo(() => {
+    const createCallback = (key: keyof FormState) => {
+      return (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const newValue = e.currentTarget.value;
+        setFormState((prev) => {
+          return { ...prev, [key]: newValue };
+        });
+      };
+    };
+    return {
+      roleKey: createCallback("roleKey"),
+      roleName: createCallback("roleName"),
+      roleDescription: createCallback("roleDescription"),
+    };
+  }, [setFormState]);
+
   return (
     <FormContainerBase form={form}>
       <RoleAndGroupsFormLayout
         breadcrumbs={breadcrumbs}
-      ></RoleAndGroupsFormLayout>
+        Footer={
+          <>
+            <PrimaryButton
+              type="submit"
+              text={<FormattedMessage id="create" />}
+            />
+            <DefaultButton
+              type="button"
+              text={<FormattedMessage id="cancel" />}
+            />
+          </>
+        }
+      >
+        <div>
+          <FormTextField
+            required={true}
+            fieldName="key"
+            parentJSONPointer=""
+            type="text"
+            label={renderToString("AddRolesScreen.roleKey.title")}
+            value={formState.roleKey}
+            onChange={onFormStateChangeCallbacks.roleKey}
+          />
+          <WidgetDescription className="mt-2">
+            <FormattedMessage id="AddRolesScreen.roleKey.description" />
+          </WidgetDescription>
+        </div>
+        <div>
+          <FormTextField
+            required={true}
+            fieldName="name"
+            parentJSONPointer=""
+            type="text"
+            label={renderToString("AddRolesScreen.roleName.title")}
+            value={formState.roleName}
+            onChange={onFormStateChangeCallbacks.roleName}
+          />
+          <WidgetDescription className="mt-2">
+            <FormattedMessage id="AddRolesScreen.roleName.description" />
+          </WidgetDescription>
+        </div>
+        <FormTextField
+          multiline={true}
+          resizable={false}
+          autoAdjustHeight={true}
+          rows={3}
+          fieldName="description"
+          parentJSONPointer=""
+          type="text"
+          label={renderToString("AddRolesScreen.roleDescription.title")}
+          value={formState.roleDescription}
+          onChange={onFormStateChangeCallbacks.roleDescription}
+        />
+      </RoleAndGroupsFormLayout>
     </FormContainerBase>
   );
 };
