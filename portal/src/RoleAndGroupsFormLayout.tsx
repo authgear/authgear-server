fd7d21010b517f80a74d73547abcca49b12a3a73
Tung Wu <tung@oursky.com>
2024-03-06 19:34:02 +0800

    Fix error message bar is missing in RoleAndGroupsFormLayout

--- portal/src/RoleAndGroupsFormLayout.tsx
+++ portal/src/RoleAndGroupsFormLayout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./RoleAndGroupsFormLayout.module.css";
 import NavBreadcrumb, { BreadcrumbItem } from "./NavBreadcrumb";
 import { useFormContainerBaseContext } from "./FormContainerBase";
+import { FormErrorMessageBar } from "./FormErrorMessageBar";
 
 interface RoleAndGroupsFormLayoutProps {
   breadcrumbs: BreadcrumbItem[];
@@ -14,12 +15,15 @@ export const RoleAndGroupsFormLayout: React.VFC<
   const { onSubmit } = useFormContainerBaseContext();
 
   return (
-    <form onSubmit={onSubmit} noValidate={true} className={styles.root}>
-      <NavBreadcrumb className={styles.header} items={breadcrumbs} />
-      <section className={styles.content}>{children}</section>
-      {Footer != null ? (
-        <footer className={styles.footer}>{Footer}</footer>
-      ) : null}
-    </form>
+    <div className={styles.root}>
+      <FormErrorMessageBar />
+      <form onSubmit={onSubmit} noValidate={true} className={styles.main}>
+        <NavBreadcrumb className={styles.header} items={breadcrumbs} />
+        <section className={styles.content}>{children}</section>
+        {Footer != null ? (
+          <footer className={styles.footer}>{Footer}</footer>
+        ) : null}
+      </form>
+    </div>
   );
 };
