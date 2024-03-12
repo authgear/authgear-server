28cef6e5344dc31252971ab9097a7aa8cbc1f9e1
Tung Wu <tung@oursky.com>
2024-03-12 19:35:59 +0800

    Implement AddGroupScreen

--- portal/src/graphql/adminapi/AddGroupScreen.tsx
+++ portal/src/graphql/adminapi/AddGroupScreen.tsx
@@ -1,7 +1,206 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect, useMemo } from "react";
+import {
+  RoleAndGroupsFormFooter,
+  RoleAndGroupsLayout,
+  RoleAndGroupsVeriticalFormLayout,
+} from "../../RoleAndGroupsLayout";
+import { RoleAndGroupsFormContainer } from "./RoleAndGroupsFormContainer";
+import { BreadcrumbItem } from "../../NavBreadcrumb";
+import {
+  FormattedMessage,
+  Context as MessageContext,
+} from "@oursky/react-messageformat";
+import { SimpleFormModel, useSimpleForm } from "../../hook/useSimpleForm";
+import { useNavigate } from "react-router-dom";
+import { useCreateGroupMutation } from "./mutations/createGroupMutation";
+import { APIError } from "../../error/error";
+import { makeLocalValidationError } from "../../error/validation";
+import { validateGroup } from "../../model/group";
+import { useFormContainerBaseContext } from "../../FormContainerBase";
+import { useFormTopErrors } from "../../form";
+import { useErrorMessageBarContext } from "../../ErrorMessageBar";
+import FormTextField from "../../FormTextField";
+import PrimaryButton from "../../PrimaryButton";
+import DefaultButton from "../../DefaultButton";
+import WidgetDescription from "../../WidgetDescription";
+
+interface FormState {
+  groupKey: string;
+  groupName: string;
+  groupDescription: string;
+}
+
+const defaultState: FormState = {
+  groupKey: "",
+  groupName: "",
+  groupDescription: "",
+};
+
+function AddGroupScreenForm() {
+  const {
+    form: { state: formState, setState: setFormState },
+    canSave,
+    isUpdating,
+  } = useFormContainerBaseContext<SimpleFormModel<FormState, string | null>>();
+  const navigate = useNavigate();
+
+  const errors = useFormTopErrors();
+  const { setErrors } = useErrorMessageBarContext();
+  useEffect(() => {
+    setErrors(errors);
+  }, [errors, setErrors]);
+
+  const cancel = useCallback(() => {
+    navigate("..", { replace: true });
+  }, [navigate]);
+
+  const onFormStateChangeCallbacks = useMemo(() => {
+    const createCallback = (key: keyof FormState) => {
+      return (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const newValue = e.currentTarget.value;
+        setFormState((prev) => {
+          return { ...prev, [key]: newValue };
+        });
+      };
+    };
+    return {
+      groupKey: createCallback("groupKey"),
+      groupName: createCallback("groupName"),
+      groupDescription: createCallback("groupDescription"),
+    };
+  }, [setFormState]);
+
+  const { renderToString } = useContext(MessageContext);
+
+  return (
+    <div>
+      <RoleAndGroupsVeriticalFormLayout>
+        <div>
+          <FormTextField
+            required={true}
+            fieldName="name"
+            parentJSONPointer=""
+            type="text"
+            label={renderToString("AddGroupScreen.groupName.title")}
+            value={formState.groupName}
+            onChange={onFormStateChangeCallbacks.groupName}
+          />
+          <WidgetDescription className="mt-2">
+            <FormattedMessage id="AddGroupScreen.groupName.description" />
+          </WidgetDescription>
+        </div>
+        <div>
+          <FormTextField
+            required={true}
+            fieldName="key"
+            parentJSONPointer=""
+            type="text"
+            label={renderToString("AddGroupScreen.groupKey.title")}
+            value={formState.groupKey}
+            onChange={onFormStateChangeCallbacks.groupKey}
+          />
+          <WidgetDescription className="mt-2">
+            <FormattedMessage id="AddGroupScreen.groupKey.description" />
+          </WidgetDescription>
+        </div>
+        <FormTextField
+          multiline={true}
+          resizable={false}
+          autoAdjustHeight={true}
+          rows={3}
+          fieldName="description"
+          parentJSONPointer=""
+          type="text"
+          label={renderToString("AddGroupScreen.groupDescription.title")}
+          value={formState.groupDescription}
+          onChange={onFormStateChangeCallbacks.groupDescription}
+        />
+      </RoleAndGroupsVeriticalFormLayout>
+      <RoleAndGroupsFormFooter className="mt-12">
+        <PrimaryButton
+          disabled={!canSave || isUpdating}
+          type="submit"
+          text={<FormattedMessage id="create" />}
+        />
+        <DefaultButton
+          disabled={isUpdating}
+          type="button"
+          onClick={cancel}
+          text={<FormattedMessage id="cancel" />}
+        />
+      </RoleAndGroupsFormFooter>
+    </div>
+  );
+}
 
 const AddGroupScreen: React.VFC = function AddGroupScreen() {
-  return <></>;
+  const breadcrumbs = useMemo<BreadcrumbItem[]>(() => {
+    return [
+      {
+        to: "~/user-management/groups",
+        label: <FormattedMessage id="GroupsScreen.title" />,
+      },
+      { to: ".", label: <FormattedMessage id="AddGroupScreen.title" /> },
+    ];
+  }, []);
+
+  const navigate = useNavigate();
+
+  const { createGroup } = useCreateGroupMutation();
+
+  const validate = useCallback((rawState: FormState): APIError | null => {
+    const [_, errors] = validateGroup({
+      key: rawState.groupKey,
+      name: rawState.groupName,
+      description: rawState.groupDescription,
+    });
+    if (errors.length > 0) {
+      return makeLocalValidationError(errors);
+    }
+    return null;
+  }, []);
+
+  const submit = useCallback(
+    async (rawState: FormState) => {
+      const [sanitizedRole, errors] = validateGroup({
+        key: rawState.groupKey,
+        name: rawState.groupName,
+        description: rawState.groupDescription,
+      });
+      if (errors.length > 0) {
+        throw new Error("unexpected validation errors");
+      }
+      return createGroup({
+        key: sanitizedRole.key,
+        name: sanitizedRole.name,
+        description: sanitizedRole.description,
+      });
+    },
+    [createGroup]
+  );
+
+  const form = useSimpleForm({
+    stateMode:
+      "ConstantInitialStateAndResetCurrentStatetoInitialStateAfterSave",
+    defaultState,
+    submit,
+    validate,
+  });
+
+  useEffect(() => {
+    if (form.submissionResult != null) {
+      const groupID = form.submissionResult;
+      navigate(`../${encodeURIComponent(groupID)}/details`, { replace: true });
+    }
+  }, [form.submissionResult, navigate]);
+
+  return (
+    <RoleAndGroupsLayout breadcrumbs={breadcrumbs}>
+      <RoleAndGroupsFormContainer form={form}>
+        <AddGroupScreenForm />
+      </RoleAndGroupsFormContainer>
+    </RoleAndGroupsLayout>
+  );
 };
 
 export default AddGroupScreen;
--- portal/src/model/group.ts
+++ portal/src/model/group.ts
@@ -1,4 +1,54 @@
+import { LocalValidationError } from "../error/validation";
 import { Group } from "../graphql/adminapi/globalTypes.generated";
+
+export interface CreatableGroup
+  extends Pick<Group, "key" | "name" | "description"> {}
+
+// Ref: pkg/lib/rolesgroups/key.go
+const KEY_REGEX = /^[a-zA-Z_][a-zA-Z0-9:_]*$/;
+const MAX_KEY_LENGTH = 40;
+
+export function validateGroup(
+  rawInput: CreatableGroup
+): [CreatableGroup, LocalValidationError[]] {
+  const input = sanitizeGroup(rawInput);
+  const errors: LocalValidationError[] = [];
+  if (!input.key) {
+    errors.push({
+      location: "/key",
+      messageID: "errors.validation.required",
+    });
+  } else if (!KEY_REGEX.test(input.key)) {
+    errors.push({
+      location: "/key",
+      messageID: "errors.groups.key.validation.format",
+    });
+  } else if (input.key.length > MAX_KEY_LENGTH) {
+    errors.push({
+      location: "/key",
+      messageID: "errors.validation.maxLength",
+      arguments: { expected: MAX_KEY_LENGTH },
+    });
+  }
+
+  if (!input.name) {
+    errors.push({
+      location: "/name",
+      messageID: "errors.validation.required",
+    });
+  }
+  return [input, errors];
+}
+
+export function sanitizeGroup(input: CreatableGroup): CreatableGroup {
+  const description = input.description?.trim();
+  const name = input.name?.trim();
+  return {
+    key: input.key.trim(),
+    name: name ? name : null,
+    description: description ? description : null,
+  };
+}
 
 export interface SearchableGroup extends Pick<Group, "id" | "key" | "name"> {}
 
