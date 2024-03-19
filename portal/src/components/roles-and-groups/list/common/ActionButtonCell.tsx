360eec8c0ecd9b48e3c274d780e86bea2a9ac978
Davis Tang <ngkaokis@gmail.com>
2024-03-19 16:26:09 +0800

    Disabled remove button if role is from groups
    ref #3981

--- portal/src/components/roles-and-groups/list/common/ActionButtonCell.tsx
+++ portal/src/components/roles-and-groups/list/common/ActionButtonCell.tsx
@@ -4,26 +4,36 @@ import ActionButton from "../../../../ActionButton";
 import styles from "./ActionButtonCell.module.css";
 import { Text } from "@fluentui/react";
 import { useSystemConfig } from "../../../../context/SystemConfigContext";
+import cn from "classnames";
 
 interface ActionButtonCellProps {
   text: string;
   onClick?: (e: any) => void;
+  disabled?: boolean;
 }
 
 function ActionButtonCell(props: ActionButtonCellProps): React.ReactElement {
-  const { text, onClick } = props;
+  const { text, onClick, disabled } = props;
   const { themes } = useSystemConfig();
   return (
     <BaseCell>
       <ActionButton
         text={
-          <Text className={styles.actionButtonText} theme={themes.destructive}>
+          <Text
+            // TO BE CONFIRMED: How the themes color work for different states
+            className={cn(
+              styles.actionButtonText,
+              disabled && "text-[#BFBFC3]"
+            )}
+            theme={themes.destructive}
+          >
             {text}
           </Text>
         }
         className={styles.actionButton}
         theme={themes.destructive}
         onClick={onClick}
+        disabled={disabled}
       />
     </BaseCell>
   );
