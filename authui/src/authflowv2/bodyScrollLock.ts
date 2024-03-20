55d2ef245b23fe08caec8608b281eb68eecc09df
Newman Chow <newman@oursky.com>
2024-03-20 17:40:05 +0800

    Use insertRule for body scroll lock style

--- authui/src/authflowv2/bodyScrollLock.ts
+++ authui/src/authflowv2/bodyScrollLock.ts
@@ -2,24 +2,39 @@ import { Controller } from "@hotwired/stimulus";
 
 export class BodyScrollLockController extends Controller {
   scrollPosition = 0;
+  styleElement: HTMLStyleElement | null = null;
+
+  get style() {
+    const height = window.visualViewport
+      ? `${window.visualViewport.height}px`
+      : "100vh";
+
+    return `
+      body {
+        position: fixed;
+        top: -${this.scrollPosition}px;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: ${height};
+      }
+    `;
+  }
 
   lock() {
     this.scrollPosition = document.documentElement.scrollTop;
-    document.body.style.overflow = "hidden";
-    document.body.style.position = "fixed";
-    document.body.style.top = `-${this.scrollPosition}px`;
-    document.body.style.width = "100%";
-    document.body.style.height = window.visualViewport?.height
-      ? `${window.visualViewport.height}px`
-      : "100vh";
+
+    this.styleElement = document.createElement("style");
+    document.head.appendChild(this.styleElement);
+    this.styleElement.sheet!.insertRule(this.style, 0);
   }
 
   unlock() {
-    document.body.style.removeProperty("overflow");
-    document.body.style.removeProperty("position");
-    document.body.style.removeProperty("top");
-    document.body.style.removeProperty("width");
-    document.body.style.removeProperty("height");
+    if (this.styleElement) {
+      document.head.removeChild(this.styleElement);
+      this.styleElement = null;
+    }
+
     window.scrollTo(0, this.scrollPosition);
   }
 }
