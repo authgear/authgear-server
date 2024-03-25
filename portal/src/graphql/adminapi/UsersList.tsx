f4d39a4e8e28eeccfb0abdcb476f6273725be7ac
Davis Tang <ngkaokis@gmail.com>
2024-03-25 17:16:30 +0800

    Remove redundant text style

--- portal/src/graphql/adminapi/UsersList.tsx
+++ portal/src/graphql/adminapi/UsersList.tsx
@@ -368,7 +368,7 @@ const UsersList: React.VFC<UsersListProps> = function UsersList(props) {
     }
     return (
       <BaseCell>
-        <Text className={"whitespace-normal text-inherit"}>{text}</Text>
+        <Text className={"whitespace-normal"}>{text}</Text>
       </BaseCell>
     );
   }, []);
@@ -381,7 +381,7 @@ const UsersList: React.VFC<UsersListProps> = function UsersList(props) {
     }
     return (
       <BaseCell>
-        <Text className={"whitespace-normal text-inherit"}>{text}</Text>
+        <Text className={"whitespace-normal"}>{text}</Text>
       </BaseCell>
     );
   }, []);
