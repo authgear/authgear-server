fbeabf3495681c7a9f28776d92fa4a68db7027e9
Newman Chow <newman@oursky.com>
2024-03-27 00:27:33 +0800

    Use local authgear instance for e2e

--- e2e/run.sh
+++ e2e/run.sh
@@ -4,11 +4,15 @@
 
 function setup {
     echo "[ ] Starting services..."
-    docker compose build authgear
     docker compose up -d
-    sleep 5
+    sleep 3
+
+    echo "[ ] Building authgear..."
+    make -C .. build BIN_NAME=dist/authgear TARGET=authgear
+    make -C .. build BIN_NAME=dist/authgear-portal TARGET=portal
 
     echo "[ ] Starting authgear..."
+    ../dist/authgear start > authgear.log 2>&1 &
     for i in $(seq 10); do \
         if [ "$(curl -sL -w '%{http_code}' -o /dev/null ${MAIN_LISTEN_ADDR}/healthz)" = "200" ]; then
             echo "    - started authgear."
@@ -23,40 +27,31 @@ function setup {
     fi
 
     echo "[ ] DB migration..."
-    docker compose exec authgear bash -c "
-        authgear database migrate up
-        authgear audit database migrate up
-        authgear images database migrate up
-    "
-    docker compose exec portal bash -c "
-        authgear-portal database migrate up
-    "
+    ../dist/authgear database migrate up
+    ../dist/authgear audit database migrate up
+    ../dist/authgear images database migrate up
+    ../dist/authgear-portal database migrate up
 
     [ -d ./fixtures ] && for f in ./fixtures/*; do
         if [ -d "$f" ]; then
             echo "[ ] Creating project $f..."
-            docker compose exec portal bash -c "
-                authgear-portal internal configsource create $f \
-                    --database-schema=\"$DATABASE_SCHEMA\" \
-                    --database-url=\"$DATABASE_URL\"
-            "
-            docker compose exec authgear bash -c "
-                authgear internal e2e import-users --config-source-dir=\"$f\"
-            "
+            ../dist/authgear-portal internal configsource create $f \
+                --database-schema="$DATABASE_SCHEMA" \
+                --database-url="$DATABASE_URL"
+            ../dist/authgear internal e2e import-users --config-source-dir="$f"
         fi
     done
 
     echo "[ ] Creating default domain..."
-    docker compose exec portal bash -c "
-        authgear-portal internal domain create-default \
-            --database-schema=\"$DATABASE_SCHEMA\" \
-            --database-url=\"$DATABASE_URL\" \
-            --default-domain-suffix=\".portal.localhost\"
-    "
+    ../dist/authgear-portal internal domain create-default \
+        --database-schema="$DATABASE_SCHEMA" \
+        --database-url="$DATABASE_URL" \
+        --default-domain-suffix=".portal.localhost"
 }
 
 function teardown {
     echo "[ ] Teardown..."
+    kill -9 $(lsof -ti:4000) > /dev/null 2>&1 || true
     docker compose down
 }
 
