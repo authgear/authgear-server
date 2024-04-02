9f18db0464b3026c22fb6e5a2bede91018832daf
Newman Chow <newman@oursky.com>
2024-04-02 18:55:59 +0800

    Respect feature config deterministic code for email link

--- pkg/lib/authn/otp/form.go
+++ pkg/lib/authn/otp/form.go
@@ -35,39 +35,50 @@ func (f Form) GenerateCode(cfg *config.TestModeConfig, featureCfg *config.TestMo
 	codeType := f.codeType()
 	switch c := codeType.(type) {
 	case secretcode.OOBOTPSecretCodeType:
-		if cfg.FixedOOBOTP.Enabled {
-			if r, ok := cfg.FixedOOBOTP.MatchTarget(target); ok {
-				fixedOTP := r.FixedCode
-				if fixedOTP == "" {
-					fixedOTP = featureCfg.FixedOOBOTP.Code
-				}
-				if fixedOTP == "" {
-					fixedOTP = c.Generate()
-				}
-				return c.GenerateFixed(fixedOTP)
-			}
-		}
-		if featureCfg.FixedOOBOTP.Enabled {
-			return c.GenerateFixed(featureCfg.FixedOOBOTP.Code)
-		}
-		return c.Generate()
+		return f.generateOOBOTPCode(c, cfg, featureCfg, target)
 	case secretcode.LinkOTPSecretCodeType:
-		if cfg.Email.Enabled {
-			if r, ok := cfg.Email.MatchTarget(target); ok {
-				fixedCode := r.FixedCode
-				if fixedCode == "" {
-					fixedCode = c.Generate()
-				}
-				return c.GenerateFixed(fixedCode)
+		return f.generateLinkOTPCode(c, cfg, featureCfg, target, userID)
+	default:
+		panic("unknown otp form")
+	}
+}
+
+func (f Form) generateOOBOTPCode(c secretcode.OOBOTPSecretCodeType, cfg *config.TestModeConfig, featureCfg *config.TestModeFeatureConfig, target string) string {
+	if cfg.FixedOOBOTP.Enabled {
+		if r, ok := cfg.FixedOOBOTP.MatchTarget(target); ok {
+			fixedOTP := r.FixedCode
+			if fixedOTP == "" {
+				fixedOTP = featureCfg.FixedOOBOTP.Code
 			}
-		}
-		if featureCfg.DeterministicLinkOTP.Enabled {
-			return c.GenerateDeterministic(userID)
-		} else {
-			return c.Generate()
+			if fixedOTP == "" {
+				fixedOTP = c.Generate()
+			}
+			return c.GenerateFixed(fixedOTP)
 		}
 	}
-	panic("unknown otp form")
+	if featureCfg.FixedOOBOTP.Enabled {
+		return c.GenerateFixed(featureCfg.FixedOOBOTP.Code)
+	}
+	return c.Generate()
+}
+
+func (f Form) generateLinkOTPCode(c secretcode.LinkOTPSecretCodeType, cfg *config.TestModeConfig, featureCfg *config.TestModeFeatureConfig, target string, userID string) string {
+	if cfg.Email.Enabled {
+		if r, ok := cfg.Email.MatchTarget(target); ok {
+			fixedOTP := r.FixedCode
+			if fixedOTP == "" && featureCfg.DeterministicLinkOTP.Enabled {
+				return c.GenerateDeterministic(userID)
+			}
+			if fixedOTP == "" {
+				fixedOTP = c.Generate()
+			}
+			return c.GenerateFixed(fixedOTP)
+		}
+	}
+	if featureCfg.DeterministicLinkOTP.Enabled {
+		return c.GenerateDeterministic(userID)
+	}
+	return c.Generate()
 }
 
 func (f Form) VerifyCode(input string, expected string) bool {
