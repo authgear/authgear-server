411bff545dfcb598656d6065b8f65268d447025c
Newman Chow <newman@oursky.com>
2024-04-08 15:12:27 +0800

    Break down test case function

--- e2e/pkg/testrunner/testcase.go
+++ e2e/pkg/testrunner/testcase.go
@@ -64,98 +64,133 @@ func RunTestCase(t *testing.T, testCase TestCase) {
 		TestCase: testCase,
 	}
 
+	// Create project per test case
 	err := e2eCmd.CreateConfigSource()
 	if err != nil {
 		t.Errorf("failed to create config source: %v", err)
 		return
 	}
 
+	// Execute before hooks to prepare fixtures
+	ok := executeBeforeAll(t, e2eCmd, testCase)
+	if !ok {
+		return
+	}
+
+	client := authflowclient.NewClient(
+		ctx,
+		"localhost:4000",
+		httputil.HTTPHost(fmt.Sprintf("%s.portal.localhost:4000", appID)),
+	)
+
+	var state string
+
+	for i, step := range testCase.Steps {
+		if step.Name == "" {
+			step.Name = fmt.Sprintf("step %d", i+1)
+		}
+
+		state, ok = executeStep(t, e2eCmd, client, step, state)
+		if !ok {
+			return
+		}
+	}
+}
+
+func executeBeforeAll(t *testing.T, e2eCmd *End2EndCmd, testCase TestCase) (ok bool) {
 	for _, beforeHook := range testCase.Before {
 		switch beforeHook.Type {
 		case BeforeHookTypeUserImport:
-			err = e2eCmd.ImportUsers(beforeHook.UserImport)
+			err := e2eCmd.ImportUsers(beforeHook.UserImport)
 			if err != nil {
 				t.Errorf("failed to import users: %v", err)
-				return
+				return false
 			}
 		case BeforeHookTypeCustomSQL:
-			err = e2eCmd.ExecuteCustomSQL(beforeHook.CustomSQL.Path)
+			err := e2eCmd.ExecuteCustomSQL(beforeHook.CustomSQL.Path)
 			if err != nil {
 				t.Errorf("failed to execute custom SQL: %v", err)
-				return
+				return false
 			}
 		default:
 			t.Errorf("unknown before hook type: %s", beforeHook.Type)
+			return false
 		}
 	}
 
-	hostName := httputil.HTTPHost(fmt.Sprintf("%s.portal.localhost:4000", appID))
-	client := authflowclient.NewClient(ctx, "localhost:4000", hostName)
+	return true
+}
 
-	var stateToken string
+func executeStep(t *testing.T, e2eCmd *End2EndCmd, client *authflowclient.Client, step Step, state string) (nextState string, ok bool) {
+	var flowResponse *authflowclient.FlowResponse
+	var flowErr error
 
-	for i, step := range testCase.Steps {
-		var stepName = step.Name
-		if stepName == "" {
-			stepName = fmt.Sprintf("step %d", i+1)
+	nextState = state
+
+	switch step.Action {
+	case StepActionCreate:
+		var flowReference authflowclient.FlowReference
+		err := json.Unmarshal([]byte(step.Input), &flowReference)
+		if err != nil {
+			t.Errorf("failed to parse input in '%s': %v\n", step.Name, err)
+			return
 		}
 
-		var flowResponse *authflowclient.FlowResponse
-		var flowErr error
+		flowResponse, flowErr = client.Create(flowReference, "")
 
-		switch step.Action {
-		case StepActionCreate:
-			var flowReference authflowclient.FlowReference
-			err := json.Unmarshal([]byte(step.Input), &flowReference)
-			if err != nil {
-				t.Errorf("failed to parse input in '%s': %v\n", stepName, err)
-				return
-			}
-
-			flowResponse, flowErr = client.Create(flowReference, "")
-
-		case StepActionInput:
-			fallthrough
-
-		default:
-			var input map[string]interface{}
-			err := json.Unmarshal([]byte(step.Input), &input)
-			if err != nil {
-				t.Errorf("failed to parse JSON input in '%s': %v\n", stepName, err)
-				return
-			}
-
-			flowResponse, flowErr = client.Input(nil, nil, stateToken, input)
+	case StepActionInput:
+		fallthrough
+	default:
+		var input map[string]interface{}
+		err := json.Unmarshal([]byte(step.Input), &input)
+		if err != nil {
+			t.Errorf("failed to parse JSON input in '%s': %v\n", step.Name, err)
+			return
 		}
 
-		if flowResponse != nil {
-			stateToken = flowResponse.StateToken
-		}
+		flowResponse, flowErr = client.Input(nil, nil, state, input)
+	}
 
-		if step.Output != nil {
-			errorViolations, resultViolations, err := MatchOutput(*step.Output, flowResponse, flowErr)
-			if err != nil {
-				t.Errorf("failed to match output in '%s': %v\n", stepName, err)
-				t.Errorf("  result: %v\n", flowResponse)
-				t.Errorf("  error: %v\n", flowErr)
-				return
-			}
-			if len(errorViolations) > 0 {
-				t.Errorf("error output mismatch in '%s': %v\n", stepName, flowErr)
-				for _, violation := range errorViolations {
-					t.Errorf("  %s: %s. Expected %s, got %s", violation.Path, violation.Message, violation.Expected, violation.Actual)
-				}
-				return
-			}
-			if len(resultViolations) > 0 {
-				t.Errorf("result output mismatch in '%s': %v\n", stepName, flowResponse)
-				for _, violation := range resultViolations {
-					t.Errorf("  %s: %s. Expected %s, got %s", violation.Path, violation.Message, violation.Expected, violation.Actual)
-				}
-				return
-			}
+	if step.Output != nil {
+		ok := validateOutput(t, step, flowResponse, flowErr)
+		if !ok {
+			return "", false
 		}
 	}
+
+	if flowResponse != nil {
+		nextState = flowResponse.StateToken
+	}
+
+	return nextState, true
+}
+
+func validateOutput(t *testing.T, step Step, flowResponse *authflowclient.FlowResponse, flowErr error) (ok bool) {
+	errorViolations, resultViolations, err := MatchOutput(*step.Output, flowResponse, flowErr)
+	if err != nil {
+		t.Errorf("failed to match output in '%s': %v\n", step.Name, err)
+		t.Errorf("  result: %v\n", flowResponse)
+		t.Errorf("  error: %v\n", flowErr)
+		return false
+	}
+
+	if len(errorViolations) > 0 {
+		t.Errorf("error output mismatch in '%s': %v\n", step.Name, flowErr)
+		for _, violation := range errorViolations {
+			t.Errorf("  %s: %s. Expected %s, got %s", violation.Path, violation.Message, violation.Expected, violation.Actual)
+		}
+		return false
+	}
+
+	if len(resultViolations) > 0 {
+		t.Errorf("result output mismatch in '%s': %v\n", step.Name, flowResponse)
+		for _, violation := range resultViolations {
+			t.Errorf("  %s: %s. Expected %s, got %s", violation.Path, violation.Message, violation.Expected, violation.Actual)
+		}
+		return false
+	}
+
+	return true
 }
 
 func generateAppID() string {
