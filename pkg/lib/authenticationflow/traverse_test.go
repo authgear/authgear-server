bedd8159be54a1ed04c3c12d79cd34fcf6d900b7
Tung Wu <tung@oursky.com>
2024-04-18 12:47:50 +0800

    Add tests to traverse functions

--- pkg/lib/authenticationflow/traverse_test.go
+++ pkg/lib/authenticationflow/traverse_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestTraverse(t *testing.T) {
-	Convey("Traverse", t, func() {
+	Convey("TraverseFlow", t, func() {
 		test := func(w *Flow, expectedHistory []string) {
 			var actualHistory []string
 			err := TraverseFlow(Traverser{
@@ -69,6 +69,138 @@ func TestTraverse(t *testing.T) {
 			"node0-1",
 			"intent0-1",
 			"node0-2",
+			"intent0-0",
+		})
+	})
+
+	Convey("TraverseFlowIntentFirst", t, func() {
+		test := func(w *Flow, expectedHistory []string) {
+			var actualHistory []string
+			err := TraverseFlowIntentFirst(Traverser{
+				Intent: func(intent Intent, w *Flow) error {
+					if i, ok := intent.(*testMarshalIntent0); ok {
+						actualHistory = append(actualHistory, i.Intent0)
+					}
+					return nil
+				},
+				NodeSimple: func(nodeSimple NodeSimple, w *Flow) error {
+					if n, ok := nodeSimple.(*testMarshalNode0); ok {
+						actualHistory = append(actualHistory, n.Node0)
+					}
+					return nil
+				},
+			}, w)
+			So(err, ShouldBeNil)
+			So(actualHistory, ShouldResemble, expectedHistory)
+		}
+
+		test(&Flow{
+			FlowID:     "wf-0",
+			StateToken: "wf-0-state-0",
+			Intent: &testMarshalIntent0{
+				Intent0: "intent0-0",
+			},
+			Nodes: []Node{
+				Node{
+					Type: NodeTypeSimple,
+					Simple: &testMarshalNode0{
+						Node0: "node0-0",
+					},
+				},
+				Node{
+					Type: NodeTypeSubFlow,
+					SubFlow: &Flow{
+						Intent: &testMarshalIntent0{
+							Intent0: "intent0-1",
+						},
+						Nodes: []Node{
+							Node{
+								Type: NodeTypeSimple,
+								Simple: &testMarshalNode0{
+									Node0: "node0-1",
+								},
+							},
+						},
+					},
+				},
+				Node{
+					Type: NodeTypeSimple,
+					Simple: &testMarshalNode0{
+						Node0: "node0-2",
+					},
+				},
+			},
+		}, []string{
+			"intent0-0",
+			"node0-0",
+			"intent0-1",
+			"node0-1",
+			"node0-2",
+		})
+	})
+
+	Convey("TraverseFlowReverse", t, func() {
+		test := func(w *Flow, expectedHistory []string) {
+			var actualHistory []string
+			err := TraverseFlowReverse(Traverser{
+				Intent: func(intent Intent, w *Flow) error {
+					if i, ok := intent.(*testMarshalIntent0); ok {
+						actualHistory = append(actualHistory, i.Intent0)
+					}
+					return nil
+				},
+				NodeSimple: func(nodeSimple NodeSimple, w *Flow) error {
+					if n, ok := nodeSimple.(*testMarshalNode0); ok {
+						actualHistory = append(actualHistory, n.Node0)
+					}
+					return nil
+				},
+			}, w)
+			So(err, ShouldBeNil)
+			So(actualHistory, ShouldResemble, expectedHistory)
+		}
+
+		test(&Flow{
+			FlowID:     "wf-0",
+			StateToken: "wf-0-state-0",
+			Intent: &testMarshalIntent0{
+				Intent0: "intent0-0",
+			},
+			Nodes: []Node{
+				Node{
+					Type: NodeTypeSimple,
+					Simple: &testMarshalNode0{
+						Node0: "node0-0",
+					},
+				},
+				Node{
+					Type: NodeTypeSubFlow,
+					SubFlow: &Flow{
+						Intent: &testMarshalIntent0{
+							Intent0: "intent0-1",
+						},
+						Nodes: []Node{
+							Node{
+								Type: NodeTypeSimple,
+								Simple: &testMarshalNode0{
+									Node0: "node0-1",
+								},
+							},
+						},
+					},
+				},
+				Node{
+					Type: NodeTypeSimple,
+					Simple: &testMarshalNode0{
+						Node0: "node0-2",
+					},
+				},
+			},
+		}, []string{
+			"node0-2",
+			"node0-1",
+			"intent0-1",
+			"node0-0",
 			"intent0-0",
 		})
 	})
