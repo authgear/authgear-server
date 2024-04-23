9d8ae97d38b96609b8b3ac5451e9e43d739588ea
Louis Chan <louischan@oursky.com>
2024-04-23 12:39:00 +0800

    Forward client IP

--- posthog/posthog.ts
+++ posthog/posthog.ts
@@ -38,26 +38,34 @@ function findNewClients(oldClients: Client[], newClients: Client[]): Client[] {
   return output;
 }
 
-async function identify(
-  user_id: string,
-  email: string | undefined,
-): Promise<void> {
+async function identify(options: {
+  user_id: string;
+  ip: string | undefined;
+  email: string | undefined;
+}): Promise<void> {
   // https://posthog.com/docs/api/post-only-endpoints
   const url = new URL("/capture", POSTHOG_ENDPOINT);
 
   const $set: any = {};
-  if (email != null) {
-    $set.email = email;
+  if (options.email != null) {
+    $set.email = options.email;
+  }
+
+  const properties: any = {
+    $set,
+  };
+  if (options.ip != null) {
+    properties["$ip"] = options.ip;
+  } else {
+    properties["$geoip_disable"] = true;
   }
 
   let body: any = {
     api_key: POSTHOG_API_KEY,
     // https://posthog.com/docs/api/post-only-endpoints#identify
     event: "$identify",
-    distinct_id: user_id,
-    properties: {
-      $set,
-    },
+    distinct_id: options.user_id,
+    properties,
   };
   const resp = await fetch(url, {
     method: "POST",
@@ -72,18 +80,29 @@ async function identify(
   }
 }
 
-async function groupIdentify(project_id: string): Promise<void> {
+async function groupIdentify(options: {
+  project_id: string;
+  ip: string | undefined;
+}): Promise<void> {
   // https://posthog.com/docs/api/post-only-endpoints
   const url = new URL("/capture", POSTHOG_ENDPOINT);
+
+  const properties: any = {
+    $group_type: "project",
+    $group_key: options.project_id,
+  };
+  if (options.ip != null) {
+    properties["$ip"] = options.ip;
+  } else {
+    properties["$geoip_disable"] = true;
+  }
+
   let body: any = {
     api_key: POSTHOG_API_KEY,
     // https://posthog.com/docs/api/post-only-endpoints#group-identify
     event: "$groupidentify",
     distinct_id: "groups_setup_id",
-    properties: {
-      $group_type: "project",
-      $group_key: project_id,
-    },
+    properties,
   };
   const resp = await fetch(url, {
     method: "POST",
@@ -98,21 +117,35 @@ async function groupIdentify(project_id: string): Promise<void> {
   }
 }
 
-async function capture(
-  distinct_id: string,
-  event: string,
-  properties?: Record<string, unknown>,
-): Promise<void> {
+async function capture(options: {
+  user_id: string;
+  event: string;
+  ip: string | undefined;
+  additionalProperties?: Record<string, unknown>;
+}): Promise<void> {
   // https://posthog.com/docs/api/post-only-endpoints
   const url = new URL("/capture", POSTHOG_ENDPOINT);
-  let body: any = {
-    api_key: POSTHOG_API_KEY,
-    event,
-    distinct_id,
-  };
-  if (properties != null) {
-    body.properties = properties;
+
+  let properties: any = {};
+  if (options.ip != null) {
+    properties["$ip"] = options.ip;
+  } else {
+    properties["$geoip_disable"] = true;
   }
+  if (options.additionalProperties != null) {
+    properties = {
+      ...properties,
+      ...options.additionalProperties,
+    };
+  }
+
+  const body: any = {
+    api_key: POSTHOG_API_KEY,
+    event: options.event,
+    distinct_id: options.user_id,
+    properties,
+  };
+
   const resp = await fetch(url, {
     method: "POST",
     headers: {
@@ -131,26 +164,42 @@ export default async function (e: any): Promise<void> {
   switch (e.type) {
     case "user.created": {
       const user_id = e.context.user_id;
+      const ip = e.context.ip_address;
       const email = e.payload.user?.standard_attributes?.email;
       // We must call identify here so that even tracking is blocked in the browser,
       // we can still identify the user.
-      await identify(user_id, email);
-      await capture(user_id, "signedUp");
+      await identify({
+        ip,
+        user_id,
+        email,
+      });
+      await capture({
+        ip,
+        user_id,
+        event: "signedUp",
+      });
       break;
     }
     case "project.app.created": {
+      const ip = e.context.ip_address;
       const actor_user_id = e.context.audit_context.actor_user_id;
       const projectID = e.context.app_id;
-      await groupIdentify(projectID);
-      await capture(actor_user_id, "createdProject", {
-        projectID,
-        $groups: {
+      await groupIdentify({
+        project_id: projectID,
+        ip,
+      });
+      await capture({
+        ip,
+        user_id: actor_user_id,
+        event: "createdProject",
+        additionalProperties: {
           project: projectID,
         },
       });
       break;
     }
     case "project.app.updated": {
+      const ip = e.context.ip_address;
       const actor_user_id = e.context.audit_context.actor_user_id;
       const projectID = e.context.app_id;
       const { app_config_old, app_config_new } = e.payload;
@@ -158,25 +207,42 @@ export default async function (e: any): Promise<void> {
       const clients_new: Client[] = app_config_new.oauth?.clients ?? [];
       const newClients = findNewClients(clients_old, clients_new);
       for (const c of newClients) {
-        await groupIdentify(projectID);
-        await capture(actor_user_id, "createdApplication", {
-          projectID,
-          applicationType: c.x_application_type,
-          $groups: {
-            project: projectID,
+        await groupIdentify({
+          project_id: projectID,
+          ip,
+        });
+        await capture({
+          ip,
+          user_id: actor_user_id,
+          event: "createdApplication",
+          additionalProperties: {
+            projectID,
+            applicationType: c.x_application_type,
+            $groups: {
+              project: projectID,
+            },
           },
         });
       }
       break;
     }
     case "project.collaborator.invitation.accepted": {
+      const ip = e.context.ip_address;
       const actor_user_id = e.context.audit_context.actor_user_id;
       const projectID = e.context.app_id;
-      await groupIdentify(projectID);
-      await capture(actor_user_id, "acceptedInvite", {
-        projectID,
-        $groups: {
-          project: projectID,
+      await groupIdentify({
+        project_id: projectID,
+        ip,
+      });
+      await capture({
+        ip,
+        user_id: actor_user_id,
+        event: "acceptedInvite",
+        additionalProperties: {
+          projectID,
+          $groups: {
+            project: projectID,
+          },
         },
       });
       break;
