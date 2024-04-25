1f7d7e9876d63db8f1aac1ef3df064dbf0512f14
Louis Chan <louischan@oursky.com>
2024-04-25 15:39:54 +0800

    Simplify translationlinter
    It does not use authgear-server packages.

--- devtools/translationlinter/main.go
+++ devtools/translationlinter/main.go
@@ -2,84 +2,78 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
-	. "github.com/authgear/authgear-server/pkg/util/template"
+	gomessageformat "github.com/iawaknahc/gomessageformat"
+	"golang.org/x/text/language"
 )
 
 const DevelopmentLanguage = "en"
 
-type errKeys struct {
+type errMissingKeys struct {
 	MissingKeysByLang map[string][]string
-	ExtraKeysByLang   map[string][]string
 }
 
-func (e errKeys) Error() string {
+func (e errMissingKeys) Error() string {
 	var buf strings.Builder
 
-	if len(e.MissingKeysByLang) > 0 {
-		fmt.Fprintf(&buf, "The following languages have missing keys:\n")
-
-		for lang, keys := range e.MissingKeysByLang {
-			fmt.Fprintf(&buf, "  %v:\n", lang)
-			for _, key := range keys {
-				fmt.Fprintf(&buf, "    %v\n", key)
-			}
-		}
-	}
-
-	if len(e.ExtraKeysByLang) > 0 {
-		fmt.Fprintf(&buf, "The following languages have extra keys:\n")
-
-		for lang, keys := range e.ExtraKeysByLang {
-			fmt.Fprintf(&buf, "  %v:\n", lang)
-			for _, key := range keys {
-				fmt.Fprintf(&buf, "    %v\n", key)
-			}
+	fmt.Fprintf(&buf, "The following languages have missing keys:\n")
+	for lang, keys := range e.MissingKeysByLang {
+		fmt.Fprintf(&buf, "  %v:\n", lang)
+		for _, key := range keys {
+			fmt.Fprintf(&buf, "    %v\n", key)
 		}
 	}
 
 	return buf.String()
 }
 
-type errKeyRender struct {
-	Path string
-	Key  string
-	Err  error
+type errExtraKeys struct {
+	ExtraKeysByLang map[string][]string
 }
 
-func (e errKeyRender) Error() string {
-	return fmt.Sprintf("failed to render translation key %v: %v", e.Key, e.Err)
-}
-
-type errKeyRenders []errKeyRender
-
-func (e errKeyRenders) Error() string {
+func (e errExtraKeys) Error() string {
 	var buf strings.Builder
 
-	errsByPath := make(map[string][]errKeyRender)
-	for _, err := range e {
-		errsByPath[err.Path] = append(errsByPath[err.Path], err)
-	}
-
-	for path, errs := range errsByPath {
-		fmt.Fprintf(&buf, "The following keys failed to render for %v:\n", path)
-		for _, err := range errs {
-			if err.Key != "" {
-				fmt.Fprintf(&buf, "  %v: %v\n", err.Key, err.Err)
-			} else {
-				fmt.Fprintf(&buf, "  %v\n", err.Err)
-			}
+	fmt.Fprintf(&buf, "The following languages have extra keys:\n")
+	for lang, keys := range e.ExtraKeysByLang {
+		fmt.Fprintf(&buf, "  %v:\n", lang)
+		for _, key := range keys {
+			fmt.Fprintf(&buf, "    %v\n", key)
 		}
 	}
 
 	return buf.String()
 }
 
-func check(expectedKeys map[string]struct{}, jsonObject map[string]interface{}) (missingKeys []string, extraKeys []string) {
+type InvalidKey struct {
+	Key   string
+	Error error
+}
+
+type errInvalidKeys struct {
+	InvalidKeysByLang map[string][]InvalidKey
+}
+
+func (e errInvalidKeys) Error() string {
+	var buf strings.Builder
+
+	fmt.Fprintf(&buf, "The following languages have invalid keys:\n")
+	for lang, invalidKeys := range e.InvalidKeysByLang {
+		fmt.Fprintf(&buf, "  %v\n", lang)
+		for _, invalidKey := range invalidKeys {
+			fmt.Fprintf(&buf, "    %v: %v\n", invalidKey.Key, invalidKey.Error)
+		}
+	}
+
+	return buf.String()
+}
+
+func check(expectedKeys map[string]struct{}, jsonObject map[string]string) (missingKeys []string, extraKeys []string) {
 	for missing := range expectedKeys {
 		_, ok := jsonObject[missing]
 		if !ok {
@@ -103,9 +97,11 @@ func doMain() (err error) {
 		return
 	}
 
+	// Prepare the expected keys.
 	expectedKeys := make(map[string]struct{})
 	for _, match := range matches {
 		langTag := filepath.Base(filepath.Dir(match))
+		// The keys appearing in DevelopmentLanguage are the expected keys.
 		if langTag != DevelopmentLanguage {
 			continue
 		}
@@ -130,11 +126,11 @@ func doMain() (err error) {
 
 	missingKeysByLang := make(map[string][]string)
 	extraKeysByLang := make(map[string][]string)
+	invalidKeysByLang := make(map[string][]InvalidKey)
+
 	for _, match := range matches {
 		langTag := filepath.Base(filepath.Dir(match))
-		if langTag == DevelopmentLanguage {
-			continue
-		}
+		tag := language.Make(langTag)
 
 		var f *os.File
 		f, err = os.Open(match)
@@ -143,7 +139,7 @@ func doMain() (err error) {
 		}
 		defer f.Close()
 
-		var jsonObject map[string]interface{}
+		var jsonObject map[string]string
 		err = json.NewDecoder(f).Decode(&jsonObject)
 		if err != nil {
 			return
@@ -156,35 +152,33 @@ func doMain() (err error) {
 		if len(extraKeys) > 0 {
 			extraKeysByLang[langTag] = extraKeys
 		}
-	}
 
-	if len(missingKeysByLang) > 0 || len(extraKeysByLang) > 0 {
-		err = errKeys{
-			MissingKeysByLang: missingKeysByLang,
-			ExtraKeysByLang:   extraKeysByLang,
-		}
-	}
-
-	renderErrs := errKeyRenders{}
-
-	engine := Engine{Resolver: &EngineTemplateResolverImpl{}}
-	for _, match := range matches {
-		langTag := filepath.Base(filepath.Dir(match))
-		translationMap, err := engine.Translation([]string{langTag})
-		if err != nil {
-			renderErrs = append(renderErrs, errKeyRender{Path: match, Err: err})
-			continue
-		}
-		for key := range expectedKeys {
-			_, err := translationMap.RenderText(key, nil)
-			if err != nil {
-				renderErrs = append(renderErrs, errKeyRender{Path: match, Key: key, Err: err})
+		for key, pattern := range jsonObject {
+			_, parseErr := gomessageformat.FormatTemplateParseTree(tag, pattern)
+			if parseErr != nil {
+				invalidKeysByLang[langTag] = append(invalidKeysByLang[langTag], InvalidKey{
+					Key:   key,
+					Error: parseErr,
+				})
 			}
 		}
 	}
 
-	if len(renderErrs) > 0 {
-		err = renderErrs
+	if len(missingKeysByLang) > 0 {
+		err = errors.Join(err, errMissingKeys{
+			MissingKeysByLang: missingKeysByLang,
+		})
+	}
+
+	if len(extraKeysByLang) > 0 {
+		err = errors.Join(err, errExtraKeys{
+			ExtraKeysByLang: extraKeysByLang,
+		})
+	}
+	if len(invalidKeysByLang) > 0 {
+		err = errors.Join(err, errInvalidKeys{
+			InvalidKeysByLang: invalidKeysByLang,
+		})
 	}
 
 	return
