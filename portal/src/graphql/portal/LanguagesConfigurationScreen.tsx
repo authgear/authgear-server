745652e84a6d070e2f952cd025c20d3837545beb
YinYin Chiu <cychiuae@connect.ust.hk>
2024-06-12 15:03:44 +0800

    Scaffold languages configuration screen

---
+++ portal/src/graphql/portal/LanguagesConfigurationScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const LanguagesConfigurationScreen: React.VFC =
+  function LanguagesConfigurationScreen() {
+    return <div>LanguagesConfigurationScreen</div>;
+  };
+
+export default LanguagesConfigurationScreen;
