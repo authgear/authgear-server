d8fe1aa55e95754a6edc3a98d6eed3e1aa9798a8
YinYin Chiu <cychiuae@connect.ust.hk>
2024-06-12 15:52:31 +0800

    Configure app config form

--- portal/src/graphql/portal/LanguagesConfigurationScreen.tsx
+++ portal/src/graphql/portal/LanguagesConfigurationScreen.tsx
@@ -1,8 +1,48 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { produce } from "immer";
+import { PortalAPIAppConfig } from "../../types";
+import { clearEmptyObject } from "../../util/misc";
+import { useAppConfigForm } from "../../hook/useAppConfigForm";
+import FormContainer from "../../FormContainer";
+
+interface ConfigFormState {
+  supportedLanguages: string[];
+  fallbackLanguage: string;
+}
+
+function constructFormState(config: PortalAPIAppConfig): ConfigFormState {
+  const fallbackLanguage = config.localization?.fallback_language ?? "en";
+  return {
+    fallbackLanguage,
+    supportedLanguages: config.localization?.supported_languages ?? [
+      fallbackLanguage,
+    ],
+  };
+}
+
+function constructConfig(
+  config: PortalAPIAppConfig,
+  _initialState: ConfigFormState,
+  currentState: ConfigFormState
+): PortalAPIAppConfig {
+  return produce(config, (config) => {
+    config.localization = config.localization ?? {};
+    config.localization.fallback_language = currentState.fallbackLanguage;
+    config.localization.supported_languages = currentState.supportedLanguages;
+    clearEmptyObject(config);
+  });
+}
 
 const LanguagesConfigurationScreen: React.VFC =
   function LanguagesConfigurationScreen() {
-    return <div>LanguagesConfigurationScreen</div>;
+    const { appID } = useParams() as { appID: string };
+    const appConfigForm = useAppConfigForm({
+      appID,
+      constructFormState,
+      constructConfig,
+    });
+    return <FormContainer form={appConfigForm} canSave={true}></FormContainer>;
   };
 
 export default LanguagesConfigurationScreen;
