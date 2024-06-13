cf2d773769e9914b1bdca560f9d5a9bb3701e60f
pkong-ds <pkong.work@gmail.com>
2024-06-13 21:19:28 +0800

    Add new config fields to captcha

--- pkg/lib/config/captcha.go
+++ pkg/lib/config/captcha.go
@@ -5,14 +5,42 @@ var _ = Schema.Add("CaptchaConfig", `
 	"type": "object",
 	"additionalProperties": false,
 	"properties": {
+		"enabled": { "type": "boolean" },
+		"providers": {
+			"type": "array",
+			"items": { "$ref": "#/$defs/CaptchaProvider" }
+		},
 		"provider": { "$ref": "#/$defs/LegacyCaptchaProvider" }
+	}
+}
+`)
+
+var _ = Schema.Add("CaptchaProvider", `
+{
+	"type": "object",
+	"additionalProperties": false,
+	"required": ["type", "alias"],
+	"properties": {
+		"type": { "type": "string" },
+		"alias": { "type": "string" },
+		"site_key": { "type": "string" }
 	}
 }
 `)
 
 type CaptchaConfig struct {
 	LegacyProvider *LegacyCaptchaProvider `json:"provider,omitempty"`
+	Enabled        bool                   `json:"enabled,omitempty"`
+	Providers      []*CaptchaProvider     `json:"providers,omitempty"`
 }
+
+type CaptchaProvider struct {
+	Type    string `json:"type,omitempty"`
+	Alias   string `json:"alias,omitempty"`
+	SiteKey string `json:"site_key,omitempty"` // only for some providers
+}
+
+// legacy code below
 
 var _ = Schema.Add("LegacyCaptchaProvider", `
 {
