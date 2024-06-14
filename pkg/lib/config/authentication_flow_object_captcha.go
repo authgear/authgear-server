bf7e99d26026417fc22b5f55f4b8ce266e94465e
pkong-ds <pkong.work@gmail.com>
2024-06-14 14:12:50 +0800

    Add auth flow step captcha config

--- pkg/lib/config/authentication_flow_object_captcha.go
+++ pkg/lib/config/authentication_flow_object_captcha.go
@@ -14,3 +14,18 @@ var _ = Schema.Add("AuthenticationFlowObjectCaptchaConfig", `
 type AuthenticationFlowObjectCaptchaConfig struct {
 	Enabled *bool `json:"enabled,omitempty"`
 }
+
+var _ = Schema.Add("AuthenticationFlowCaptcha", `
+{
+	"type": "object",
+	"additionalProperties": false,
+	"required": ["required"],
+	"properties": {
+		"required": { "type": "boolean" }
+	}
+}
+`)
+
+type AuthenticationFlowCaptcha struct {
+	Required *bool `json:"required,omitempty"`
+}
