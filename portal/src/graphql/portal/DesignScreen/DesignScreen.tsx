fc33208c7bb68b6344d653ba65832f009d1979b3
YinYin Chiu <cychiuae@connect.ust.hk>
2024-06-21 14:08:07 +0800

    Layout design page skeleton

--- portal/src/graphql/portal/DesignScreen/DesignScreen.tsx
+++ portal/src/graphql/portal/DesignScreen/DesignScreen.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { DefaultEffects } from "@fluentui/react";
+import cn from "classnames";
+
 import { useParams } from "react-router-dom";
 import FormContainer from "../../../FormContainer";
 import ShowError from "../../../ShowError";
 import ShowLoading from "../../../ShowLoading";
 import { useBrandDesignForm } from "./form";
+
+const ConfigurationPanel: React.VFC = function ConfigurationPanel() {
+  return <div></div>;
+};
 
 const DesignScreen: React.VFC = function DesignScreen() {
   const { appID } = useParams() as { appID: string };
@@ -17,7 +24,25 @@ const DesignScreen: React.VFC = function DesignScreen() {
     return <ShowError error={form.loadError} onRetry={form.reload} />;
   }
 
-  return <FormContainer form={form} canSave={true}></FormContainer>;
+  return (
+    <FormContainer className={cn("h-full")} form={form} canSave={true}>
+      <div className={cn("h-full", "flex")}>
+        <div className={cn("flex-1", "h-full", "p-6")}>
+          <div
+            className={cn("flex-1", "rounded-xl", "h-full")}
+            style={{
+              boxShadow: DefaultEffects.elevation4,
+            }}
+          >
+            Preview
+          </div>
+        </div>
+        <div className={cn("w-80", "p-6", "overflow-auto")}>
+          <ConfigurationPanel />
+        </div>
+      </div>
+    </FormContainer>
+  );
 };
 
 export default DesignScreen;
