7145461d9cf41623894361b4b576d1f0f58e9c07
YinYin Chiu <cychiuae@connect.ust.hk>
2024-06-21 12:36:24 +0800

    Move form out into own file

--- portal/src/graphql/portal/DesignScreen/DesignScreen.tsx
+++ portal/src/graphql/portal/DesignScreen/DesignScreen.tsx
@@ -1,104 +1,13 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
-import { parse as parseCSS } from "postcss";
-import { useResourceForm } from "../../../hook/useResourceForm";
-import { RESOURCE_AUTHGEAR_AUTHFLOW_V2_LIGHT_THEME_CSS } from "../../../resources";
-import {
-  Resource,
-  ResourceSpecifier,
-  expandSpecifier,
-} from "../../../util/resource";
-import {
-  CssAstVisitor,
-  CustomisableTheme,
-  CustomisableThemeStyleGroup,
-  DEFAULT_LIGHT_THEME,
-  StyleCssVisitor,
-  ThemeTargetSelector,
-} from "../../../model/themeAuthFlowV2";
 import FormContainer from "../../../FormContainer";
 import ShowError from "../../../ShowError";
 import ShowLoading from "../../../ShowLoading";
-
-const THEME_RESOURCE_DEFINITIONS = [
-  RESOURCE_AUTHGEAR_AUTHFLOW_V2_LIGHT_THEME_CSS,
-];
-
-interface ResourcesFormState {
-  orignalResources: Resource[];
-  customisableTheme: CustomisableTheme;
-}
-
-function constructResourcesFormStateFromResources(
-  resources: Resource[]
-): ResourcesFormState {
-  const lightTheme = (() => {
-    const lightThemeResource = resources.find((r) => {
-      return (
-        r.nullableValue != null &&
-        r.specifier.def === RESOURCE_AUTHGEAR_AUTHFLOW_V2_LIGHT_THEME_CSS
-      );
-    });
-    if (lightThemeResource?.nullableValue == null) {
-      return DEFAULT_LIGHT_THEME;
-    }
-    const root = parseCSS(lightThemeResource.nullableValue);
-    const styleCSSVisitor = new StyleCssVisitor(
-      ThemeTargetSelector.Light,
-      new CustomisableThemeStyleGroup()
-    );
-    return styleCSSVisitor.getStyle(root);
-  })();
-  return {
-    orignalResources: resources,
-    customisableTheme: lightTheme,
-  };
-}
-
-function constructResourcesFromFormState(
-  state: ResourcesFormState
-): Resource[] {
-  const lightThemeResourceSpecifier = {
-    def: RESOURCE_AUTHGEAR_AUTHFLOW_V2_LIGHT_THEME_CSS,
-    locale: null,
-    extension: null,
-  };
-  return [
-    {
-      specifier: lightThemeResourceSpecifier,
-      path: expandSpecifier(lightThemeResourceSpecifier),
-      nullableValue: (() => {
-        const cssAstVisitor = new CssAstVisitor(ThemeTargetSelector.Light);
-        const styleGroup = new CustomisableThemeStyleGroup(
-          state.customisableTheme
-        );
-        styleGroup.acceptCssAstVisitor(cssAstVisitor);
-        return cssAstVisitor.getCSS().toResult().css;
-      })(),
-    },
-  ];
-}
+import { useBrandDesignForm } from "./form";
 
 const DesignScreen: React.VFC = function DesignScreen() {
   const { appID } = useParams() as { appID: string };
-  const specifiers = useMemo<ResourceSpecifier[]>(() => {
-    const specifiers: ResourceSpecifier[] = [];
-    for (const def of THEME_RESOURCE_DEFINITIONS) {
-      specifiers.push({
-        def,
-        locale: null,
-        extension: null,
-      });
-    }
-    return specifiers;
-  }, []);
-
-  const form = useResourceForm(
-    appID,
-    specifiers,
-    constructResourcesFormStateFromResources,
-    constructResourcesFromFormState
-  );
+  const form = useBrandDesignForm(appID);
 
   if (form.isLoading) {
     return <ShowLoading />;
---
+++ portal/src/graphql/portal/DesignScreen/form.ts
@@ -0,0 +1,104 @@
+import { useMemo } from "react";
+import { parse as parseCSS } from "postcss";
+import {
+  ResourceFormModel,
+  useResourceForm,
+} from "../../../hook/useResourceForm";
+import {
+  CssAstVisitor,
+  CustomisableTheme,
+  CustomisableThemeStyleGroup,
+  DEFAULT_LIGHT_THEME,
+  StyleCssVisitor,
+  ThemeTargetSelector,
+} from "../../../model/themeAuthFlowV2";
+import { RESOURCE_AUTHGEAR_AUTHFLOW_V2_LIGHT_THEME_CSS } from "../../../resources";
+import {
+  Resource,
+  ResourceSpecifier,
+  expandSpecifier,
+} from "../../../util/resource";
+
+const THEME_RESOURCE_DEFINITIONS = [
+  RESOURCE_AUTHGEAR_AUTHFLOW_V2_LIGHT_THEME_CSS,
+];
+
+export interface BranchDesignFormState {
+  orignalResources: Resource[];
+  customisableTheme: CustomisableTheme;
+}
+
+function constructResourcesFormStateFromResources(
+  resources: Resource[]
+): BranchDesignFormState {
+  const lightTheme = (() => {
+    const lightThemeResource = resources.find((r) => {
+      return (
+        r.nullableValue != null &&
+        r.specifier.def === RESOURCE_AUTHGEAR_AUTHFLOW_V2_LIGHT_THEME_CSS
+      );
+    });
+    if (lightThemeResource?.nullableValue == null) {
+      return DEFAULT_LIGHT_THEME;
+    }
+    const root = parseCSS(lightThemeResource.nullableValue);
+    const styleCSSVisitor = new StyleCssVisitor(
+      ThemeTargetSelector.Light,
+      new CustomisableThemeStyleGroup()
+    );
+    return styleCSSVisitor.getStyle(root);
+  })();
+  return {
+    orignalResources: resources,
+    customisableTheme: lightTheme,
+  };
+}
+
+function constructResourcesFromFormState(
+  state: BranchDesignFormState
+): Resource[] {
+  const lightThemeResourceSpecifier = {
+    def: RESOURCE_AUTHGEAR_AUTHFLOW_V2_LIGHT_THEME_CSS,
+    locale: null,
+    extension: null,
+  };
+  return [
+    {
+      specifier: lightThemeResourceSpecifier,
+      path: expandSpecifier(lightThemeResourceSpecifier),
+      nullableValue: (() => {
+        const cssAstVisitor = new CssAstVisitor(ThemeTargetSelector.Light);
+        const styleGroup = new CustomisableThemeStyleGroup(
+          state.customisableTheme
+        );
+        styleGroup.acceptCssAstVisitor(cssAstVisitor);
+        return cssAstVisitor.getCSS().toResult().css;
+      })(),
+    },
+  ];
+}
+
+export function useBrandDesignForm(
+  appID: string
+): ResourceFormModel<BranchDesignFormState> {
+  const specifiers = useMemo<ResourceSpecifier[]>(() => {
+    const specifiers: ResourceSpecifier[] = [];
+    for (const def of THEME_RESOURCE_DEFINITIONS) {
+      specifiers.push({
+        def,
+        locale: null,
+        extension: null,
+      });
+    }
+    return specifiers;
+  }, []);
+
+  const form = useResourceForm(
+    appID,
+    specifiers,
+    constructResourcesFormStateFromResources,
+    constructResourcesFromFormState
+  );
+
+  return form;
+}
