987a1f90a1bb06767d9960ed460334d297eb9b75
YinYin Chiu <cychiuae@connect.ust.hk>
2024-06-24 00:17:38 +0800

    Implement image picker

--- portal/src/graphql/portal/DesignScreen/Components.tsx
+++ portal/src/graphql/portal/DesignScreen/Components.tsx
@@ -11,6 +11,8 @@ import React, {
 import {
   Callout,
   ColorPicker as FluentUIColorPicker,
+  Image,
+  ImageFit,
   getColorFromString,
 } from "@fluentui/react";
 import {
@@ -29,6 +31,13 @@ import {
   BorderRadiusStyleType,
 } from "../../../model/themeAuthFlowV2";
 import TextField from "../../../TextField";
+import DefaultButton from "../../../DefaultButton";
+import {
+  base64EncodedDataToDataURI,
+  dataURIToBase64EncodedData,
+} from "../../../util/uri";
+import { useSystemConfig } from "../../../context/SystemConfigContext";
+import PrimaryButton from "../../../PrimaryButton";
 
 export const Separator: React.VFC = function Separator() {
   return <div className={cn("h-px", "my-12", "bg-separator")}></div>;
@@ -375,6 +384,147 @@ export const BorderRadius: React.VFC<BorderRadiusProps> = function BorderRadius(
           onChange={onBorderRadiusChange}
         />
       ) : null}
+    </div>
+  );
+};
+
+type ImageFileExtension = ".jpeg" | ".png" | ".gif";
+function mediaTypeToExtension(mime: string): ImageFileExtension {
+  switch (mime) {
+    case "image/png":
+      return ".png";
+    case "image/jpeg":
+      return ".jpeg";
+    case "image/gif":
+      return ".gif";
+    default:
+      throw new Error(`unsupported media type: ${mime}`);
+  }
+}
+
+interface ImagePickerProps {
+  base64EncodedData: string | null;
+  onChange: (
+    image: {
+      base64EncodedData: string;
+      extension: ImageFileExtension;
+    } | null
+  ) => void;
+}
+export const ImagePicker: React.VFC<ImagePickerProps> = function ImagePicker(
+  props
+) {
+  const { base64EncodedData, onChange } = props;
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const { themes } = useSystemConfig();
+
+  const onInputChange = useCallback(
+    (e?: React.FormEvent<HTMLInputElement>) => {
+      const target = e?.target;
+      if (target instanceof HTMLInputElement) {
+        const file = target.files?.[0];
+        if (file != null) {
+          const extension = mediaTypeToExtension(file.type);
+          const reader = new FileReader();
+          reader.addEventListener("load", function () {
+            const result = reader.result;
+            if (typeof result === "string") {
+              onChange({
+                base64EncodedData: dataURIToBase64EncodedData(result),
+                extension,
+              });
+            }
+          });
+          reader.readAsDataURL(file);
+        }
+      }
+    },
+    [onChange]
+  );
+
+  const onClickUploadButton = useCallback(
+    (e: React.MouseEvent<HTMLElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+      inputRef.current?.click();
+    },
+    []
+  );
+
+  const onClickRemoveButton = useCallback(
+    (e: React.MouseEvent<HTMLElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+      if (inputRef.current) {
+        inputRef.current.value = "";
+      }
+      onChange(null);
+    },
+    [onChange]
+  );
+
+  const onClickImage = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+      if (base64EncodedData) {
+        return;
+      }
+      inputRef.current?.click();
+    },
+    [base64EncodedData]
+  );
+
+  return (
+    <div className={cn("flex", "items-center", "gap-x-6")}>
+      <div
+        className={cn(
+          "flex",
+          "items-center",
+          "justify-center",
+          "h-30",
+          "w-30",
+          "bg-neutral-light",
+          "rounded",
+          "border",
+          "border-solid",
+          "border-neutral-tertiaryAlt"
+        )}
+        onClick={onClickImage}
+      >
+        {base64EncodedData == null ? (
+          <span className={styles.icImagePlaceholder}></span>
+        ) : (
+          <Image
+            src={base64EncodedDataToDataURI(base64EncodedData)}
+            className={cn("h-full", "w-full")}
+            imageFit={ImageFit.centerContain}
+            maximizeFrame={true}
+          />
+        )}
+      </div>
+      {base64EncodedData == null ? (
+        <DefaultButton
+          text={
+            <FormattedMessage id="DesignScreen.configuration.imagePicker.upload" />
+          }
+          onClick={onClickUploadButton}
+        />
+      ) : (
+        <PrimaryButton
+          theme={themes.destructive}
+          text={<FormattedMessage id={"ImageFilePicker.remove"} />}
+          onClick={onClickRemoveButton}
+        />
+      )}
+      <input
+        ref={inputRef}
+        className={cn("hidden")}
+        type="file"
+        accept="image/png, image/jpeg, image/gif"
+        onChange={onInputChange}
+      />
     </div>
   );
 };
