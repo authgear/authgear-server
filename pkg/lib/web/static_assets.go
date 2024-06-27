b7bb4d38725b9b82ce91643328caece3b9f4fb48
YinYin Chiu <cychiuae@connect.ust.hk>
2024-06-27 15:13:10 +0800

    Set size limit of each static image asset

--- pkg/lib/web/static_assets.go
+++ pkg/lib/web/static_assets.go
@@ -19,11 +19,11 @@ var AuthgearDarkThemeCSS = resource.RegisterResource(CSSDescriptor{
 	Path: path.Join(AppAssetsURLDirname, "authgear-dark-theme.css"),
 })
 
-var AppLogo = resource.RegisterResource(ImageDescriptor{Name: "app_logo"})
-var AppLogoDark = resource.RegisterResource(ImageDescriptor{Name: "app_logo_dark"})
-var Favicon = resource.RegisterResource(ImageDescriptor{Name: "favicon"})
-var AppBackgroundImage = resource.RegisterResource(ImageDescriptor{Name: "app_background_image"})
-var AppBackgroundImageDark = resource.RegisterResource(ImageDescriptor{Name: "app_background_image_dark"})
+var AppLogo = resource.RegisterResource(ImageDescriptor{Name: "app_logo", SizeLimit: 100 * 1024})
+var AppLogoDark = resource.RegisterResource(ImageDescriptor{Name: "app_logo_dark", SizeLimit: 100 * 1024})
+var Favicon = resource.RegisterResource(ImageDescriptor{Name: "favicon", SizeLimit: 100 * 1024})
+var AppBackgroundImage = resource.RegisterResource(ImageDescriptor{Name: "app_background_image", SizeLimit: 500 * 1024})
+var AppBackgroundImageDark = resource.RegisterResource(ImageDescriptor{Name: "app_background_image_dark", SizeLimit: 500 * 1024})
 
 var AuthgearAuthflowV2LightThemeCSS = resource.RegisterResource(CSSDescriptor{
 	Path: path.Join(AppAssetsURLDirname, "authgear-authflowv2-light-theme.css"),
