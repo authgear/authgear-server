0e373f22f3c36cd92eccd3b930e5da49331ebfcd
Tung Wu <tung@oursky.com>
2024-06-28 20:15:40 +0800

    Use map[string]struct{} as set

--- pkg/lib/oauth/grant_offline.go
+++ pkg/lib/oauth/grant_offline.go
@@ -291,19 +291,19 @@ func (g *OfflineGrant) HasValidTokens() bool {
 	return len(g.RefreshTokens) > 0
 }
 
-func (g *OfflineGrant) getScopesSet(clientID string) map[string]interface{} {
-	clientScopes := map[string]interface{}{}
+func (g *OfflineGrant) getScopesSet(clientID string) map[string]struct{} {
+	clientScopes := map[string]struct{}{}
 
 	if g.InitialClientID == clientID {
 		for _, scope := range g.Deprecated_Scopes {
-			clientScopes[scope] = scope
+			clientScopes[scope] = struct{}{}
 		}
 	}
 
 	for _, token := range g.RefreshTokens {
 		if token.ClientID == clientID {
 			for _, scope := range token.Scopes {
-				clientScopes[scope] = scope
+				clientScopes[scope] = struct{}{}
 			}
 		}
 	}
