cd21634d3f2476b67cbb11c0b1e6564b1aa17e31
pkong-ds <pkong.work@gmail.com>
2024-07-03 15:07:08 +0800

    Add errorutil.partition test case for order of joined errors

--- pkg/util/errorutil/partition_test.go
+++ pkg/util/errorutil/partition_test.go
@@ -65,4 +65,51 @@ func TestPartition(t *testing.T) {
 		So(errors.Is(notMatched, errE), ShouldBeTrue)
 		So(errors.Is(notMatched, errF), ShouldBeTrue)
 	})
+
+	Convey("should partition a joined error of many errors", t, func() {
+		errA := errors.New("a")
+		errB := errors.New("b")
+		errC := errors.New("c")
+		errD := errors.New("d")
+		errE := errors.New("e")
+		errF := errors.New("f")
+
+		joined := errors.Join(errA, errB, errC, errD, errE, errF)
+
+		matched, notMatched := errorutil.Partition(joined, func(err error) bool {
+			return errors.Is(err, errA)
+		})
+
+		So(errors.Is(matched, errA), ShouldBeTrue)
+		So(errors.Is(matched, errB), ShouldBeFalse)
+		So(errors.Is(matched, errC), ShouldBeFalse)
+		So(errors.Is(matched, errD), ShouldBeFalse)
+		So(errors.Is(matched, errE), ShouldBeFalse)
+		So(errors.Is(matched, errF), ShouldBeFalse)
+		So(errors.Is(notMatched, errA), ShouldBeFalse)
+		So(errors.Is(notMatched, errB), ShouldBeTrue)
+		So(errors.Is(notMatched, errC), ShouldBeTrue)
+		So(errors.Is(notMatched, errD), ShouldBeTrue)
+		So(errors.Is(notMatched, errE), ShouldBeTrue)
+		So(errors.Is(notMatched, errF), ShouldBeTrue)
+	})
+
+	Convey("should NOT mutate the order of joined errors after partition", t, func() {
+		errA := errors.New("a")
+		errB := errors.New("b")
+		errC := errors.New("c")
+		errD := errors.New("d")
+		errE := errors.New("e")
+		errF := errors.New("f")
+
+		joined := errors.Join(errA, errB, errC, errD, errE, errF) // Note order here is A,B,C,D,E,F
+		_, notMatched := errorutil.Partition(joined, func(err error) bool {
+			return errors.Is(err, errA)
+		})
+
+		uw, ok := notMatched.(interface{ Unwrap() []error })
+		So(ok, ShouldBeTrue)
+		notMatchedErrs := uw.Unwrap()
+		So(notMatchedErrs, ShouldResemble, []error{errB, errC, errD, errE, errF}) // Note order here is B,C,D,E,F
+	})
 }
