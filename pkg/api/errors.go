a656bc486a468a745965b03b76c02aad59992381
Louis Chan <louischan@oursky.com>
2024-07-04 13:54:45 +0800

    Change NoAuthenticator to its own error

--- pkg/api/errors.go
+++ pkg/api/errors.go
@@ -32,7 +32,7 @@ var ErrInvalidCredentials = InvalidCredentials.New("invalid credentials")
 var ErrOAuthProviderNotFound = apierrors.NotFound.WithReason("OAuthProviderNotFound").New("oauth provider not found")
 var ErrIdentityModifyDisabled = apierrors.Forbidden.WithReason("IdentityModifyDisabled").New("identity modification disabled")
 var ErrMismatchedUser = apierrors.InternalError.WithReason("MismatchedUser").New("mismatched user")
-var ErrNoAuthenticator = NewInvariantViolated("NoAuthenticator", "no authenticator", nil)
+var ErrNoAuthenticator = apierrors.InternalError.WithReason("NoAuthenticator").New("no authenticator")
 var ErrClaimNotVerifiable = NewInvariantViolated("ClaimNotVerifiable", "claim not verifiable", nil)
 
 var ChangePasswordFailed = apierrors.Invalid.WithReason("ChangePasswordFailed")
