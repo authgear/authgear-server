7c0b817b81d4ef8c25b1f9ce136d30769a824492
yyorkchan <yyorkchan@gmail.com>
2024-07-12 11:29:19 +0800

    Enable passkey button when javascript enabled

--- authui/src/passkey.ts
+++ authui/src/passkey.ts
@@ -423,6 +423,7 @@ export class AuthflowPasskeyRequestController extends Controller {
   declare hasButtonTarget: boolean;
 
   connect() {
+    this.buttonTarget.disabled = false;
     // Disable the button if PublicKeyCredential is unavailable.
     if (!passkeyIsAvailable()) {
       if (this.hasButtonTarget) {
@@ -490,6 +491,7 @@ export class AuthflowPasskeyCreationController extends Controller {
   declare optionsValue: string;
 
   connect() {
+    this.buttonTarget.disabled = false;
     // Disable the button if PublicKeyCredential is unavailable.
     if (!passkeyIsAvailable()) {
       if (this.hasButtonTarget) {
