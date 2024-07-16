ebb6cdcf5edd7a0cebf4ce0d4ca01652cf279521
pkong-ds <pkong.work@gmail.com>
2024-07-16 11:13:17 +0800

    Read color scheme from `<meta name=x-color-scheme>`

--- authui/src/authflowv2/botprotection/cloudflareTurnstile.ts
+++ authui/src/authflowv2/botprotection/cloudflareTurnstile.ts
@@ -1,23 +1,23 @@
 import { Controller } from "@hotwired/stimulus";
+import { getColorScheme } from "../../getColorScheme";
 
 export class CloudflareTurnstileController extends Controller {
   static values = {
     siteKey: { type: String },
-    theme: { type: String },
   };
 
   static targets = ["widget", "tokenInput"];
 
   declare siteKeyValue: string;
-  declare themeValue: string;
   declare widgetTarget: HTMLDivElement;
   declare tokenInputTargets: HTMLInputElement[];
 
   connect() {
     window.turnstile.ready(() => {
+      const colorScheme = getColorScheme();
       window.turnstile.render(this.widgetTarget, {
         sitekey: this.siteKeyValue,
-        theme: this.themeValue,
+        theme: colorScheme,
         callback: (token: string) => {
           for (const tokenInput of this.tokenInputTargets) {
             tokenInput.value = token;
--- authui/src/authflowv2/botprotection/recaptchav2.ts
+++ authui/src/authflowv2/botprotection/recaptchav2.ts
@@ -1,23 +1,23 @@
 import { Controller } from "@hotwired/stimulus";
+import { getColorScheme } from "../../getColorScheme";
 
 export class RecaptchaV2Controller extends Controller {
   static values = {
     siteKey: { type: String },
-    theme: { type: String },
   };
 
   static targets = ["widget", "tokenInput"];
 
   declare siteKeyValue: string;
-  declare themeValue: string;
   declare widgetTarget: HTMLDivElement;
   declare tokenInputTargets: HTMLInputElement[];
 
   connect() {
     window.grecaptcha.ready(() => {
+      const colorScheme = getColorScheme();
       window.grecaptcha.render(this.widgetTarget, {
         sitekey: this.siteKeyValue,
-        theme: this.themeValue,
+        theme: colorScheme,
         callback: (token: string) => {
           for (const tokenInput of this.tokenInputTargets) {
             tokenInput.value = token;
