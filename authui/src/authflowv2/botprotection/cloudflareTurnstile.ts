9af6babc62be8521fa2abb0c2629afc0a0338a9c
pkong-ds <pkong.work@gmail.com>
2024-07-18 13:44:17 +0800

    Add recaptchav2 & cloudflare turnstile controllers

---
+++ authui/src/authflowv2/botprotection/cloudflareTurnstile.ts
@@ -0,0 +1,43 @@
+import { Controller } from "@hotwired/stimulus";
+import { getColorScheme } from "../../getColorScheme";
+
+function parseTheme(theme: string): Turnstile.Theme {
+  switch (theme) {
+    case "light":
+      return "light";
+    case "dark":
+      return "dark";
+    case "auto":
+      return "auto";
+    default:
+      return "auto";
+  }
+}
+
+export class CloudflareTurnstileController extends Controller {
+  static values = {
+    siteKey: { type: String },
+  };
+
+  static targets = ["widget", "tokenInput"];
+
+  declare siteKeyValue: string;
+  declare widgetTarget: HTMLDivElement;
+  declare tokenInputTargets: HTMLInputElement[];
+
+  connect() {
+    window.turnstile.ready(() => {
+      const _theme: Turnstile.Theme = "auto";
+      const colorScheme = getColorScheme();
+      window.turnstile.render(this.widgetTarget, {
+        sitekey: this.siteKeyValue,
+        theme: parseTheme(colorScheme),
+        callback: (token: string) => {
+          for (const tokenInput of this.tokenInputTargets) {
+            tokenInput.value = token;
+          }
+        },
+      });
+    });
+  }
+}
