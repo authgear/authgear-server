9de2b912def305c3d88e1170d07416374adda0c5
Tung Wu <tung@oursky.com>
2024-07-19 20:15:11 +0800

    Add tests to demonstrate dpop errors returned

--- pkg/lib/dpop/provider.go
+++ pkg/lib/dpop/provider.go
@@ -39,52 +39,61 @@ const (
 
 func (p *Provider) ParseProof(jwtStr string) (*DPoPProof, error) {
 	jwtBytes := []byte(jwtStr)
-	now := p.Clock.NowUTC()
 
 	hdr, payload, err := jwtutil.SplitWithoutVerify(jwtBytes)
 	if err != nil {
 		return nil, ErrMalformedJwt
 	}
 
-	err = jwt.Validate(payload,
+	jwk, proof, err := p.validateProofJWT(hdr, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = jws.Verify(jwtBytes, jws.WithKey(hdr.Algorithm(), jwk))
+	if err != nil {
+		return nil, ErrInvalidJwtSignature
+	}
+
+	return proof, nil
+}
+
+func (p *Provider) validateProofJWT(header jws.Headers, payload jwt.Token) (jwk.Key, *DPoPProof, error) {
+	now := p.Clock.NowUTC()
+	err := jwt.Validate(payload,
 		jwt.WithClock(jwtClock{p.Clock}),
 		jwt.WithAcceptableSkew(duration.ClockSkew),
 	)
 	if err != nil {
-		return nil, ErrInvalidJwt
+		return nil, nil, ErrInvalidJwt
 	}
 
 	// Do not accept a proof issued a long time ago
 	// https://datatracker.ietf.org/doc/html/rfc9449#section-11.1
 	if payload.IssuedAt().Add(duration.Short).Before(now) {
-		return nil, ErrProofExpired
+		return nil, nil, ErrProofExpired
 	}
 
 	var key jwk.Key
-	if jwkIface, ok := hdr.Get("jwk"); ok {
+	if jwkIface, ok := header.Get("jwk"); ok {
 		var jwkBytes []byte
 		jwkBytes, err = json.Marshal(jwkIface)
 		if err != nil {
-			return nil, ErrInvalidJwk
+			return nil, nil, ErrInvalidJwk
 		}
 
 		var set jwk.Set
 		set, err = jwk.Parse(jwkBytes)
 		if err != nil {
-			return nil, ErrInvalidJwk
+			return nil, nil, ErrInvalidJwk
 		}
 
 		key, ok = set.Key(0)
 		if !ok {
-			return nil, ErrInvalidJwk
+			return nil, nil, ErrInvalidJwk
 		}
 	} else {
-		return nil, ErrInvalidJwtNoJwkProvided
-	}
-
-	_, err = jws.Verify(jwtBytes, jws.WithKey(hdr.Algorithm(), key))
-	if err != nil {
-		return nil, ErrInvalidJwtSignature
+		return nil, nil, ErrInvalidJwk
 	}
 
 	getPayloadAsString := func(key string) (string, bool) {
@@ -99,28 +108,28 @@ func (p *Provider) ParseProof(jwtStr string) (*DPoPProof, error) {
 		return valStr, true
 	}
 
-	if hdr.Type() != DPoPJWTTyp {
-		return nil, ErrInvalidJwtType
+	if header.Type() != DPoPJWTTyp {
+		return nil, nil, ErrInvalidJwtType
 	}
 
 	jti, ok := getPayloadAsString("jti")
 	if !ok {
-		return nil, ErrInvalidJwtPayload
+		return nil, nil, ErrInvalidJwtPayload
 	}
 
 	htm, ok := getPayloadAsString("htm")
 	if !ok {
-		return nil, ErrInvalidJwtPayload
+		return nil, nil, ErrInvalidJwtPayload
 	}
 
 	htu, ok := getPayloadAsString("htu")
 	if !ok {
-		return nil, ErrInvalidJwtPayload
+		return nil, nil, ErrInvalidJwtPayload
 	}
 
 	htuURI, err := url.Parse(htu)
 	if err != nil {
-		return nil, ErrInvalidHTU
+		return nil, nil, ErrInvalidHTU
 	}
 
 	thumbprint, err := key.Thumbprint(crypto.SHA256)
@@ -130,7 +139,7 @@ func (p *Provider) ParseProof(jwtStr string) (*DPoPProof, error) {
 
 	jkt := base64.RawURLEncoding.EncodeToString(thumbprint)
 
-	return &DPoPProof{
+	return key, &DPoPProof{
 		JTI: jti,
 		HTM: htm,
 		HTU: htuURI,
