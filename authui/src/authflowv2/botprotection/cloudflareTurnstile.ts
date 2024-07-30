00a36b04db77df93d288434c133c52192b015b88
pkong-ds <pkong.work@gmail.com>
2024-07-30 20:42:31 +0800

    Make cloudflareTurnstile & recaptchav2 expose renderWidget API

--- authui/src/authflowv2/botprotection/cloudflareTurnstile.ts
+++ authui/src/authflowv2/botprotection/cloudflareTurnstile.ts
@@ -36,6 +36,7 @@ export class CloudflareTurnstileController extends Controller {
   declare widgetTarget: HTMLDivElement;
   declare widgetContainer: HTMLDivElement | undefined;
   declare widgetID: string | undefined;
+  declare isReadyForRendering: boolean;
 
   hasExistingWidget = () => {
     return this.widgetContainer != null && this.widgetID != null;
@@ -47,77 +48,78 @@ export class CloudflareTurnstileController extends Controller {
     window.turnstile.reset(this.widgetID);
   };
 
-  connect() {
-    window.turnstile.ready(() => {
-      if (this.hasExistingWidget()) {
-        return;
-      }
-      const colorScheme = getColorScheme();
+  renderWidget() {
+    if (!this.isReadyForRendering) {
+      throw new Error("turnstile target is not ready for rendering");
+    }
+    if (this.hasExistingWidget()) {
+      return;
+    }
+    const colorScheme = getColorScheme();
 
-      // Note how we wrap an extra layer of div here, because on cleanup we can just remove this extra layer of div.
-      // container-container
-      //   container <-- can just remove this on cleanup
-      //     widget
-      const widgetContainer = document.createElement("div");
-      this.widgetContainer = widgetContainer;
-      this.widgetTarget.appendChild(widgetContainer);
-      const widgetID = window.turnstile.render(widgetContainer, {
-        sitekey: this.siteKeyValue,
-        theme: parseTheme(colorScheme),
-        language: this.langValue,
-        callback: (token: string) => {
-          dispatchBotProtectionEventVerified(token);
-        },
-        "error-callback": (errCode: string) => {
-          const {
-            error: parsedError,
-            shouldRetry,
-            shouldDisplayErrMsg,
-          } = parseCloudflareTurnstileErrorCode(errCode);
-          console.error(
-            `Cloudflare Turnstile failed with error code "${errCode}". Authgear parsed it as "${parsedError}".`
-          );
-          dispatchBotProtectionEventFailed(errCode);
+    // Note how we wrap an extra layer of div here, because on cleanup we can just remove this extra layer of div.
+    // container-container
+    //   container <-- can just remove this on cleanup
+    //     widget
+    const widgetContainer = document.createElement("div");
+    this.widgetContainer = widgetContainer;
+    this.widgetTarget.appendChild(widgetContainer);
+    const widgetID = window.turnstile.render(widgetContainer, {
+      sitekey: this.siteKeyValue,
+      theme: parseTheme(colorScheme),
+      language: this.langValue,
+      callback: (token: string) => {
+        dispatchBotProtectionEventVerified(token);
+      },
+      "error-callback": (errCode: string) => {
+        const {
+          error: parsedError,
+          shouldRetry,
+          shouldDisplayErrMsg,
+        } = parseCloudflareTurnstileErrorCode(errCode);
+        console.error(
+          `Cloudflare Turnstile failed with error code "${errCode}". Authgear parsed it as "${parsedError}".`
+        );
+        dispatchBotProtectionEventFailed(errCode);
 
-          if (shouldRetry) {
-            this.resetWidget();
-          }
-          if (shouldDisplayErrMsg) {
-            setErrorMessage(CLOUDFLARE_TURNSTILE_ERROR_MSG_ID);
-          }
-          return true; // return non-falsy value to tell cloudflare we handled error already
-        },
-        "expired-callback": (token: string) => {
-          dispatchBotProtectionEventExpired(token);
-        },
-        "timeout-callback": () => {
-          // reset the widget to allow a visitor to solve the challenge again
+        if (shouldRetry) {
           this.resetWidget();
-        },
-        "response-field": false,
+        }
+        if (shouldDisplayErrMsg) {
+          setErrorMessage(CLOUDFLARE_TURNSTILE_ERROR_MSG_ID);
+        }
+        return true; // return non-falsy value to tell cloudflare we handled error already
+      },
+      "expired-callback": (token: string) => {
+        dispatchBotProtectionEventExpired(token);
+      },
+      "timeout-callback": () => {
+        // reset the widget to allow a visitor to solve the challenge again
+        this.resetWidget();
+      },
+      "response-field": false,
 
-        // below are default values, added for clarity
-        size: "normal",
-        appearance: "always",
-        action: undefined, // no need differentiate widgets under same site key
-        cData: undefined, // no need customer data, already available server-side
-        "before-interactive-callback": undefined, // we do not track interactive callback
-        "after-interactive-callback": undefined, // we do not track interactive callback
-        "unsupported-callback": undefined, // we handled unsupported browser in error-callback by code 110500
-        tabindex: 0, // a11y
-        retry: "auto", // automatically retry to obtain a token on unsuccessful attempts
-        "retry-interval": 8000, // default
-        "refresh-expired": "auto", // automatically refresh expired token
-        "refresh-timeout": "auto", // automatically refreshes upon encountering an interactive timeout
+      // below are default values, added for clarity
+      size: "normal",
+      appearance: "always",
+      action: undefined, // no need differentiate widgets under same site key
+      cData: undefined, // no need customer data, already available server-side
+      "before-interactive-callback": undefined, // we do not track interactive callback
+      "after-interactive-callback": undefined, // we do not track interactive callback
+      "unsupported-callback": undefined, // we handled unsupported browser in error-callback by code 110500
+      tabindex: 0, // a11y
+      retry: "auto", // automatically retry to obtain a token on unsuccessful attempts
+      "retry-interval": 8000, // default
+      "refresh-expired": "auto", // automatically refresh expired token
+      "refresh-timeout": "auto", // automatically refreshes upon encountering an interactive timeout
 
-        // below fields are not available in @types/cloudflare-turnstile package yet, submitting a PR for it ref https://github.com/DefinitelyTyped/DefinitelyTyped/pull/70139
-        // execution: "render", // render is default, challenge runs automatically after calling the render() function.
-      });
-      this.widgetID = widgetID ?? undefined;
+      // below fields are not available in @types/cloudflare-turnstile package yet, submitting a PR for it ref https://github.com/DefinitelyTyped/DefinitelyTyped/pull/70139
+      // execution: "render", // render is default, challenge runs automatically after calling the render() function.
     });
+    this.widgetID = widgetID ?? undefined;
   }
 
-  disconnect() {
+  undoRenderWidget() {
     if (this.widgetID != null) {
       window.turnstile.remove(this.widgetID);
     }
@@ -127,5 +129,15 @@ export class CloudflareTurnstileController extends Controller {
       this.widgetTarget.removeChild(this.widgetContainer);
     }
     this.widgetContainer = undefined;
+  }
+
+  connect() {
+    window.turnstile.ready(() => {
+      this.isReadyForRendering = true;
+    });
+  }
+
+  disconnect() {
+    this.undoRenderWidget();
   }
 }
