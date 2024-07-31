1882aab0a26013a929b7bbdef942c6511ebab312
YinYin Chiu <cychiuae@connect.ust.hk>
2024-07-31 17:03:12 +0800

    Convert ldap identity to identity info

--- pkg/lib/authn/identity/ldap_identity.go
+++ pkg/lib/authn/identity/ldap_identity.go
@@ -1,6 +1,10 @@
 package identity
 
-import "time"
+import (
+	"time"
+
+	"github.com/authgear/authgear-server/pkg/api/model"
+)
 
 type LDAP struct {
 	ID                   string                 `json:"id"`
@@ -12,4 +16,31 @@ type LDAP struct {
 	UserIDAttributeValue string                 `json:"user_id_attribute_value"`
 	Claims               map[string]interface{} `json:"claims,omitempty"`
 	RawEntryJSON         map[string]interface{} `json:"raw_entry_json,omitempty"`
+}
+
+func (i *LDAP) ToInfo() *Info {
+	return &Info{
+		ID:        i.ID,
+		UserID:    i.UserID,
+		CreatedAt: i.CreatedAt,
+		UpdatedAt: i.UpdatedAt,
+		Type:      model.IdentityTypeLDAP,
+
+		LDAP: i,
+	}
+}
+
+func (i *LDAP) ToLDAPSpec() *LDAPSpec {
+	return &LDAPSpec{
+		ServerName:           i.ServerName,
+		UserIDAttribute:      i.UserIDAttribute,
+		UserIDAttributeValue: i.UserIDAttributeValue,
+		RawEntryJSON:         i.RawEntryJSON,
+	}
+}
+
+// TODO(DEV-1668)
+// We need to convert ldap entry attribute to identity aware standard claims
+func (i *LDAP) IdentityAwareStandardClaims() map[model.ClaimName]string {
+	return make(map[model.ClaimName]string)
 }
