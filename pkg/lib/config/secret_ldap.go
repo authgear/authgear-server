782c6f2e52db429acb77a8776ae3de912a48bbeb
yyorkchan <yyorkchan@gmail.com>
2024-08-01 23:01:26 +0800

    Move ldap secret config to secret_ldap.go

---
+++ pkg/lib/config/secret_ldap.go
@@ -0,0 +1,50 @@
+package config
+
+var _ = SecretConfigSchema.Add("LDAPServerUserCredentials", `
+{
+	"type": "object",
+	"additionalProperties": false,
+	"properties": {
+		"items": { "type": "array", "items": { "$ref": "#/$defs/LDAPServerUserCredentialsItem" } }
+	}
+}
+`)
+
+type LDAPServerUserCredentials struct {
+	Items []LDAPServerUserCredentialsItem `json:"items,omitempty"`
+}
+
+var _ = SecretConfigSchema.Add("LDAPServerUserCredentialsItem", `
+{
+	"type": "object",
+	"additionalProperties": false,
+	"properties": {
+		"name": { "type": "string", "minLength": 1 },
+		"dn": { "type": "string", "format": "ldap_dn" },
+		"password": { "type": "string" }
+	},
+	"required": ["name"],
+	"dependentRequired": {
+		"dn": ["password"],
+		"password": ["dn"]
+	}
+}
+`)
+
+type LDAPServerUserCredentialsItem struct {
+	Name     string `json:"name,omitempty"`
+	DN       string `json:"dn,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+func (c *LDAPServerUserCredentials) SensitiveStrings() []string {
+	var out []string
+	for _, item := range c.Items {
+		out = append(out, item.SensitiveStrings()...)
+	}
+	return out
+}
+
+func (c *LDAPServerUserCredentialsItem) SensitiveStrings() []string {
+	return []string{c.Password}
+}
