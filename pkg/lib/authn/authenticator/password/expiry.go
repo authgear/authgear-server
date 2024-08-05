2aa5ef7ad7535ad8d0f114a2dcec3d4916fd16e5
Louis Chan <louischan@oursky.com>
2024-08-05 15:50:15 +0800

    Make expire_after does not require password expiry to be enabled

--- pkg/lib/authn/authenticator/password/expiry.go
+++ pkg/lib/authn/authenticator/password/expiry.go
@@ -14,11 +14,12 @@ type Expiry struct {
 }
 
 func (pe *Expiry) Validate(authenticator *authenticator.Password) error {
+	if authenticator.ExpireAfter != nil && authenticator.ExpireAfter.Before(pe.Clock.NowUTC()) {
+		return PasswordExpiryForceChange.New("password expired")
+	}
+
 	// Authenticator is already verified with given password prior to this call.
 	if pe.ForceChangeEnabled {
-		if authenticator.ExpireAfter != nil && authenticator.ExpireAfter.Before(pe.Clock.NowUTC()) {
-			return PasswordExpiryForceChange.New("password expired")
-		}
 		if !authenticator.UpdatedAt.Add(pe.ForceChangeSinceLastUpdate.Duration()).After(pe.Clock.NowUTC()) {
 			return PasswordExpiryForceChange.New("password expired")
 		}
