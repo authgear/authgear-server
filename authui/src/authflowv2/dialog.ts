7dd7c3edb590166dd7da92f2ab1721d7277c5356
pkong-ds <pkong.work@gmail.com>
2024-08-06 19:51:10 +0800

    Implement dialog controller with styles

---
+++ authui/src/authflowv2/dialog.ts
@@ -0,0 +1,44 @@
+import { Controller } from "@hotwired/stimulus";
+
+/**
+ * Dispatch a custom event to set target dialog open
+ */
+export function dispatchDialogOpen(dialogID: string) {
+  document.dispatchEvent(new CustomEvent(`dialog-${dialogID}:open`));
+}
+
+/**
+ * Dispatch a custom event to set target dialog close
+ */
+export function dispatchDialogClose(dialogID: string) {
+  document.dispatchEvent(new CustomEvent(`dialog-${dialogID}:close`));
+}
+
+/**
+ * Controller for dialog display
+ *
+ * Expected usage:
+ * - Add `data-controller="dialog"` to a dialog
+ * - Specific `id` attribute to that HTML element
+ */
+export class DialogController extends Controller {
+  open = () => {
+    this.element.classList.remove("close");
+    this.element.classList.add("open");
+  };
+
+  close = () => {
+    this.element.classList.remove("open");
+    this.element.classList.add("close");
+  };
+
+  connect() {
+    document.addEventListener(`dialog-${this.element.id}:open`, this.open);
+    document.addEventListener(`dialog-${this.element.id}:close`, this.close);
+  }
+
+  disconnect() {
+    document.removeEventListener(`dialog-${this.element.id}:open`, this.open);
+    document.removeEventListener(`dialog-${this.element.id}:close`, this.close);
+  }
+}
