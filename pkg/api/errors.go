4be0f8a7694cf5238feb9da58b6bd9000e5c5830
yyorkchan <yyorkchan@gmail.com>
2024-08-06 17:39:14 +0800

    Introduce ldap client testConnection method

--- pkg/api/errors.go
+++ pkg/api/errors.go
@@ -36,3 +36,13 @@ var ErrAuthenticatorNotFound = apierrors.NotFound.WithReason("AuthenticatorNotFo
 var ChangePasswordFailed = apierrors.Invalid.WithReason("ChangePasswordFailed")
 var ErrNoPassword = ChangePasswordFailed.NewWithCause("the user does not have a password", apierrors.StringCause("NoPassword"))
 var ErrPasswordReused = ChangePasswordFailed.NewWithCause("password reused", apierrors.StringCause("PasswordReused"))
+
+var LDAPConnectionTestFailed = apierrors.ServiceUnavailable.WithReason("LDAPConnectionTestFailed")
+var ErrLDAPCannotConnect = LDAPConnectionTestFailed.NewWithCause("failed to connect", apierrors.StringCause("FailedToConnect"))
+var ErrLDAPInvalidBindUser = LDAPConnectionTestFailed.NewWithCause("failed to bind search user", apierrors.StringCause("FailedToBindSearchUser"))
+var ErrLDAPInvalidBaseDN = LDAPConnectionTestFailed.NewWithCause("invalid base DN", apierrors.StringCause("InvalidBaseDN"))
+var ErrLDAPInvalidFilterTemplate = LDAPConnectionTestFailed.NewWithCause("invalid filter template", apierrors.StringCause("InvalidSearchFilterTemplate"))
+var ErrLDAPInvalidUniqueAttribute = LDAPConnectionTestFailed.NewWithCause("invalid ID attribute", apierrors.StringCause("InvalidUserIDAttributeName"))
+var ErrLDAPEndUserSearchNotFound = LDAPConnectionTestFailed.NewWithCause("end user not found", apierrors.StringCause("TestingEndUserNotFound"))
+var ErrLDAPEndUserSearchMultiple = LDAPConnectionTestFailed.NewWithCause("multiple end users found", apierrors.StringCause("TestingEndUserMultipleFound"))
+var ErrLDAPMissingUniqueAttribute = LDAPConnectionTestFailed.NewWithCause("missing ID attribute", apierrors.StringCause("TestingEndUserMissingUserIDAttribute"))
