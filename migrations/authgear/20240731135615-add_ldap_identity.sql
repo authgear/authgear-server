ce5610f66f471a63a7ded5d269866879ff1ee5b1
YinYin Chiu <cychiuae@connect.ust.hk>
2024-08-06 14:46:17 +0800

    Add indices for list by claims

--- migrations/authgear/20240731135615-add_ldap_identity.sql
+++ migrations/authgear/20240731135615-add_ldap_identity.sql
@@ -12,6 +12,10 @@ CREATE TABLE _auth_identity_ldap
 ALTER TABLE _auth_identity_ldap
     ADD CONSTRAINT _auth_identity_ldap_unique UNIQUE (app_id, server_name, user_id_attribute_name, user_id_attribute_value);
 
+CREATE INDEX _auth_identity_ldap_claim_preferred_username ON _auth_identity_ldap (app_id, (claims ->> 'preferred_username'));
+CREATE INDEX _auth_identity_ldap_claim_phone_number ON _auth_identity_ldap (app_id, (claims ->> 'phone_number'));
+CREATE INDEX _auth_identity_ldap_claim_email ON _auth_identity_ldap (app_id, (claims ->> 'email'));
+
 -- +migrate Down
 DROP TABLE _auth_identity_ldap;
 DELETE FROM _auth_identity WHERE "type" = 'ldap';
