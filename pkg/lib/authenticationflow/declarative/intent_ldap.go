003338d7345b33040a94ed5cc0c086a2af147a39
YinYin Chiu <cychiuae@connect.ust.hk>
2024-08-06 14:22:16 +0800

    Conform IntentLDAP to Milestones

--- pkg/lib/authenticationflow/declarative/intent_ldap.go
+++ pkg/lib/authenticationflow/declarative/intent_ldap.go
@@ -6,6 +6,7 @@ import (
 	"github.com/iawaknahc/jsonschema/pkg/jsonpointer"
 
 	authflow "github.com/authgear/authgear-server/pkg/lib/authenticationflow"
+	"github.com/authgear/authgear-server/pkg/lib/config"
 )
 
 func init() {
@@ -17,8 +18,35 @@ type IntentLDAP struct {
 	UserID      string        `json:"user_id,omitempty"`
 }
 
+var _ authflow.Intent = &IntentLDAP{}
+var _ authflow.Milestone = &IntentLDAP{}
+var _ MilestoneIdentificationMethod = &IntentLDAP{}
+var _ MilestoneFlowCreateIdentity = &IntentLDAP{}
+var _ MilestoneFlowUseIdentity = &IntentLDAP{}
+
 func (*IntentLDAP) Kind() string {
 	return "IntentCreateIdentityLDAP"
+}
+
+func (*IntentLDAP) Milestone() {}
+
+func (i *IntentLDAP) MilestoneIdentificationMethod() config.AuthenticationFlowIdentification {
+	return config.AuthenticationFlowIdentificationLDAP
+}
+
+func (*IntentLDAP) MilestoneFlowCreateIdentity(flows authflow.Flows) (MilestoneDoCreateIdentity, authflow.Flows, bool) {
+	// Find IntentCheckConflictAndCreateIdenity
+	m, mFlows, ok := authflow.FindMilestoneInCurrentFlow[MilestoneFlowCreateIdentity](flows)
+	if !ok {
+		return nil, mFlows, false
+	}
+
+	// Delegate to IntentCheckConflictAndCreateIdenity
+	return m.MilestoneFlowCreateIdentity(mFlows)
+}
+
+func (*IntentLDAP) MilestoneFlowUseIdentity(flows authflow.Flows) (MilestoneDoUseIdentity, authflow.Flows, bool) {
+	return authflow.FindMilestoneInCurrentFlow[MilestoneDoUseIdentity](flows)
 }
 
 func (n *IntentLDAP) CanReactTo(ctx context.Context, deps *authflow.Dependencies, flows authflow.Flows) (authflow.InputSchema, error) {
