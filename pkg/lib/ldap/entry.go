0d1c9323bedaf0b90260a22e1b6124e892c32801
YinYin Chiu <cychiuae@connect.ust.hk>
2024-08-06 22:51:30 +0800

    Strip away user password from entry attribute
    We don't want to store user password in our database

--- pkg/lib/ldap/entry.go
+++ pkg/lib/ldap/entry.go
@@ -2,11 +2,24 @@ package ldap
 
 import "github.com/go-ldap/ldap/v3"
 
+func init() {
+	sensitiveAttributeList := []string{
+		AttributeNamePassword,
+	}
+	sensitiveAttributes = make(map[string]struct{})
+	for _, attr := range sensitiveAttributeList {
+		sensitiveAttributes[attr] = struct{}{}
+	}
+}
+
 const (
 	AttributeNameEmail    = "mail"
 	AttributeNameMobile   = "mobile"
 	AttributeNameUsername = "uid"
+	AttributeNamePassword = "userPassword"
 )
+
+var sensitiveAttributes map[string]struct{}
 
 type Entry struct {
 	*ldap.Entry
