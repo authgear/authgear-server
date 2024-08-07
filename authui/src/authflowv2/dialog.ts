a9150f283f3220b8a1ba4c63e2cb13215349c3f8
pkong-ds <pkong.work@gmail.com>
2024-08-08 02:12:28 +0800

    Use visibility over display for dialog animation

--- authui/src/authflowv2/dialog.ts
+++ authui/src/authflowv2/dialog.ts
@@ -1,7 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
 
-const CLOSE_ANIMATION_DURATION_MS = 280;
-
 /**
  * Dispatch a custom event to set target dialog open
  */
@@ -25,17 +23,11 @@ export function dispatchDialogClose(dialogID: string) {
  */
 export class DialogController extends Controller {
   open = () => {
-    this.element.classList.remove("close");
     this.element.classList.add("open");
   };
 
   close = () => {
-    this.element.classList.add("close");
-    // We want close animation to show before `display: none` is applied
-    setTimeout(
-      () => this.element.classList.remove("open"),
-      CLOSE_ANIMATION_DURATION_MS
-    );
+    this.element.classList.remove("open");
   };
 
   closeOnBackgroundClick = (e: Event) => {
