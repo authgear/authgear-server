c95b742c534d5c7f225183a7b7d03070b3491241
Tung Wu <tung@oursky.com>
2024-08-07 20:04:40 +0800

    Fix a typo in env variable

--- pkg/lib/config/saml_env.go
+++ pkg/lib/config/saml_env.go
@@ -1,5 +1,5 @@
 package config
 
 type SAMLEnvironmentConfig struct {
-	IdPEntityIDTemplate string `envconfig:"IDP_ENTITY_TEMPLATE" default:"urn:{{.app_id}}.localhost"`
+	IdPEntityIDTemplate string `envconfig:"IDP_ENTITY_ID_TEMPLATE" default:"urn:{{.app_id}}.localhost"`
 }
