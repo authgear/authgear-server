94de62f48d6323892b923b443457429e37452500
Louis Chan <louischan@oursky.com>
2024-08-08 11:16:00 +0800

    npm run prettier:format in authui

--- authui/src/base64.ts
+++ authui/src/base64.ts
@@ -4,14 +4,14 @@ function b64ToUint6(nChr: number): number {
   return nChr > 64 && nChr < 91
     ? nChr - 65
     : nChr > 96 && nChr < 123
-      ? nChr - 71
-      : nChr > 47 && nChr < 58
-        ? nChr + 4
-        : nChr === 43
-          ? 62
-          : nChr === 47
-            ? 63
-            : 0;
+    ? nChr - 71
+    : nChr > 47 && nChr < 58
+    ? nChr + 4
+    : nChr === 43
+    ? 62
+    : nChr === 47
+    ? 63
+    : 0;
 }
 
 export function base64DecToArr(
@@ -49,14 +49,14 @@ function uint6ToB64(nUint6: number) {
   return nUint6 < 26
     ? nUint6 + 65
     : nUint6 < 52
-      ? nUint6 + 71
-      : nUint6 < 62
-        ? nUint6 - 4
-        : nUint6 === 62
-          ? 43
-          : nUint6 === 63
-            ? 47
-            : 65;
+    ? nUint6 + 71
+    : nUint6 < 62
+    ? nUint6 - 4
+    : nUint6 === 62
+    ? 43
+    : nUint6 === 63
+    ? 47
+    : 65;
 }
 
 export function base64EncArr(aBytes: Uint8Array) {
