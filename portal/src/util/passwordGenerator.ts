a883e3536ee4656d8c30d2ec86b7de2185ed1fc8
Newman Chow <newman@oursky.com>
2024-08-09 19:00:30 +0800

    Align client-side password generator randsource interface with server side

--- portal/src/util/passwordGenerator.ts
+++ portal/src/util/passwordGenerator.ts
@@ -15,43 +15,35 @@ const DefaultMinLength = 8;
 const GuessableEnabledMinLength = 32;
 
 export interface RandSource {
-  randomBytes(n: number): Uint8Array;
-  shuffle(list: string): string;
+  intN(n: number): number;
 }
 
-const cryptoRandSource: RandSource = {
-  randomBytes: (n: number): Uint8Array => {
-    const array = new Uint8Array(n);
-    window.crypto.getRandomValues(array);
-    return array;
-  },
-  shuffle: (list: string): string => {
-    const array = list.split("");
-    for (let i = array.length - 1; i > 0; i--) {
-      const randomArray = new Uint8Array(1);
-      window.crypto.getRandomValues(randomArray);
-      const j = randomArray[0] % (i + 1);
-      [array[i], array[j]] = [array[j], array[i]];
+export const cryptoRandSource: RandSource = {
+  intN: (n: number): number => {
+    const MAX_SAFE_INTEGER = Math.pow(2, 32) - 1; // uint32
+    if (n > MAX_SAFE_INTEGER) {
+      throw new Error("n must be less than 2^32");
     }
-    return array.join("");
+
+    const randomArray = new Uint8Array(Math.max(4, n));
+    const dataView = new DataView(randomArray.buffer);
+
+    // Generate a random number in [0, n).
+    // It rejects numbers that are greater than or equal to
+    // Math.floor(MAX_SAFE_INTEGER / n) * n to avoid modulo bias.
+    let randomNumber;
+    do {
+      window.crypto.getRandomValues(randomArray);
+      randomNumber = dataView.getUint32(0, true);
+    } while (randomNumber >= Math.floor(MAX_SAFE_INTEGER / n) * n);
+
+    return randomNumber % n;
   },
 };
 
-const mathRandSource: RandSource = {
-  randomBytes: (n: number): Uint8Array => {
-    const array = new Uint8Array(n);
-    for (let i = 0; i < n; i++) {
-      array[i] = Math.floor(Math.random() * 256);
-    }
-    return array;
-  },
-  shuffle: (list: string): string => {
-    const array = list.split("");
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array.join("");
+export const mathRandSource: RandSource = {
+  intN: (n: number): number => {
+    return Math.floor(Math.random() * n);
   },
 };
 
@@ -64,47 +56,75 @@ export function generatePassword(policy: PasswordPolicyConfig): string | null {
   }
 }
 
-// eslint-disable-next-line complexity
 export function generatePasswordWithSource(
   source: RandSource,
   policy: PasswordPolicyConfig
 ): string | null {
+  const [password, _] = internalGeneratePasswordWithSource(source, policy);
+  return password;
+}
+
+export function internalGeneratePasswordWithSource(
+  source: RandSource,
+  policy: PasswordPolicyConfig
+): [string | null, number] {
   for (let i = 0; i < MaxTrials; i++) {
-    const minLength = determineMinLength(policy);
-    const charList = prepareCharList(policy);
-
-    let password = "";
-
-    if (policy.lowercase_required) {
-      password += pickRandChar(source, CharListLowercase);
-    }
-    if (policy.uppercase_required) {
-      password += pickRandChar(source, CharListUppercase);
-    }
-    if (
-      policy.alphabet_required &&
-      !policy.lowercase_required &&
-      !policy.uppercase_required
-    ) {
-      password += pickRandChar(source, CharListAlphabet);
-    }
-    if (policy.digit_required) {
-      password += pickRandChar(source, CharListDigit);
-    }
-    if (policy.symbol_required) {
-      password += pickRandChar(source, CharListSymbol);
-    }
-
-    for (let i = password.length; i < minLength; i++) {
-      password += pickRandChar(source, charList);
-    }
-
-    password = source.shuffle(password);
-
-    if (validatePassword(password, policy) === null) {
-      return password;
+    const password = generatePasswordOnce(source, policy);
+    if (password !== null) {
+      return [password, i];
     }
   }
+
+  return [null, -1];
+}
+
+// eslint-disable-next-line complexity
+function generatePasswordOnce(
+  source: RandSource,
+  policy: PasswordPolicyConfig
+): string | null {
+  const minLength = determineMinLength(policy);
+  const charList = prepareCharList(policy);
+
+  const passwordArray: string[] = [];
+
+  // Add required characters.
+  if (policy.lowercase_required) {
+    passwordArray.push(pickRandChar(source, CharListLowercase));
+  }
+  if (policy.uppercase_required) {
+    passwordArray.push(pickRandChar(source, CharListUppercase));
+  }
+  if (
+    policy.alphabet_required &&
+    !policy.lowercase_required &&
+    !policy.uppercase_required
+  ) {
+    passwordArray.push(pickRandChar(source, CharListAlphabet));
+  }
+  if (policy.digit_required) {
+    passwordArray.push(pickRandChar(source, CharListDigit));
+  }
+  if (policy.symbol_required) {
+    passwordArray.push(pickRandChar(source, CharListSymbol));
+  }
+
+  // Fill the rest of the password with random characters.
+  for (let i = passwordArray.length; i < minLength; i++) {
+    passwordArray.push(pickRandChar(source, charList));
+  }
+
+  // Shuffle the password since we have required characers at the beginning.
+  for (let i = passwordArray.length - 1; i > 0; i--) {
+    const j = source.intN(i + 1);
+    [passwordArray[i], passwordArray[j]] = [passwordArray[j], passwordArray[i]];
+  }
+
+  const password = passwordArray.join("");
+  if (validatePassword(password, policy) === null) {
+    return password;
+  }
+
   return null;
 }
 
@@ -158,14 +178,5 @@ export function determineMinLength(
 }
 
 function pickRandChar(source: RandSource, charList: string): string {
-  const randomBytes = source.randomBytes(1);
-  const maxByte = 256;
-  const discard = maxByte - (maxByte % charList.length);
-  let byte = randomBytes[0];
-
-  while (byte >= discard) {
-    byte = source.randomBytes(1)[0];
-  }
-
-  return charList[byte % charList.length];
+  return charList[source.intN(charList.length)];
 }
