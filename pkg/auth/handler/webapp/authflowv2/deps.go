1acf10ddf4705187dde9399e6bf229973702a370
yyorkchan <yyorkchan@gmail.com>
2024-08-09 17:29:15 +0800

    Make generate Dependency Injection

--- pkg/auth/handler/webapp/authflowv2/deps.go
+++ pkg/auth/handler/webapp/authflowv2/deps.go
@@ -38,4 +38,5 @@ var DependencySet = wire.NewSet(
 	wire.Struct(new(AuthflowV2FinishFlowHandler), "*"),
 	wire.Struct(new(AuthflowV2WechatHandler), "*"),
 	wire.Struct(new(AuthflowV2AccountLinkingHandler), "*"),
+	wire.Struct(new(AuthflowV2LDAPLoginHandler), "*"),
 )
