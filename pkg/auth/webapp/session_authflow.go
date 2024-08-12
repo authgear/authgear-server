8327ffc7eb737eac303f13bb6c2c0eee0e27c1ac
yyorkchan <yyorkchan@gmail.com>
2024-08-12 17:47:25 +0800

    Rename variables

--- pkg/auth/webapp/session_authflow.go
+++ pkg/auth/webapp/session_authflow.go
@@ -774,7 +774,7 @@ func (s *AuthflowScreenWithFlowResponse) Advance(p string, result *Result) {
 	result.RedirectURI = u.String()
 }
 
-func (s *AuthflowScreenWithFlowResponse) AdvanceWithQuery(p string, result *Result, query string) {
+func (s *AuthflowScreenWithFlowResponse) AdvanceWithQuery(route string, result *Result, query string) {
 	q := url.Values{}
 	q.Set(AuthflowQueryKey, s.Screen.StateToken.XStep)
 
@@ -788,7 +788,7 @@ func (s *AuthflowScreenWithFlowResponse) AdvanceWithQuery(p string, result *Resu
 		}
 	}
 
-	u, _ := url.Parse(p)
+	u, _ := url.Parse(route)
 	u.RawQuery = q.Encode()
 
 	result.NavigationAction = "advance"
