e3455271168aa69c676330c7a5ec640bd6cd81e7
Tung Wu <tung@oursky.com>
2024-08-14 19:37:50 +0800

    Define SAMLSession

--- pkg/auth/handler/saml/services.go
+++ pkg/auth/handler/saml/services.go
@@ -4,6 +4,5 @@ import "github.com/authgear/authgear-server/pkg/lib/saml"
 
 type HandlerSAMLService interface {
 	IdpMetadata(serviceProviderId string) (*saml.Metadata, error)
-	ParseAuthnRequest(input []byte) (*saml.AuthnRequest, error)
 	ValidateAuthnRequest(serviceProviderId string, authnRequest *saml.AuthnRequest) error
 }
--- pkg/lib/saml/authn_request.go
+++ pkg/lib/saml/authn_request.go
@@ -1,7 +1,11 @@
 package saml
 
 import (
+	"bytes"
+	"encoding/xml"
+
 	crewjamsaml "github.com/crewjam/saml"
+	xrv "github.com/mattermost/xml-roundtrip-validator"
 
 	"github.com/authgear/authgear-server/pkg/lib/saml/binding"
 )
@@ -12,4 +16,29 @@ type AuthnRequest struct {
 
 func (a *AuthnRequest) GetProtocolBinding() binding.SAMLBinding {
 	return binding.SAMLBinding(a.AuthnRequest.ProtocolBinding)
+}
+
+func (a *AuthnRequest) ToXMLBytes() []byte {
+	buf, err := xml.Marshal(a)
+	if err != nil {
+		panic(err)
+	}
+	return buf
+}
+
+func ParseAuthnRequest(input []byte) (*AuthnRequest, error) {
+	var req crewjamsaml.AuthnRequest
+	if err := xrv.Validate(bytes.NewReader(input)); err != nil {
+		return nil, err
+	}
+
+	if err := xml.Unmarshal(input, &req); err != nil {
+		return nil, err
+	}
+
+	authnRequest := &AuthnRequest{
+		AuthnRequest: req,
+	}
+
+	return authnRequest, nil
 }
