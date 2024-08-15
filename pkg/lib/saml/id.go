2ea16b20af80a6546d3f2c1b55151f0fe4ae00c5
Tung Wu <tung@oursky.com>
2024-08-15 18:31:36 +0800

    Give a better prefix to saml response id

--- pkg/lib/saml/id.go
+++ pkg/lib/saml/id.go
@@ -13,7 +13,12 @@ const (
 // It must start with a letter or underscore, and can only contain letters, digits, underscores, hyphens, and periods.
 // https://www.w3.org/TR/2012/REC-xmlschema11-2-20120405/datatypes.html#ID
 // https://www.w3.org/TR/2012/REC-xmlschema11-2-20120405/datatypes.html#NCName
-func GenerateID() string {
+
+func generateID(prefix string) string {
 	id := rand.StringWithAlphabet(32, idAlphabet, rand.SecureRand)
-	return fmt.Sprintf("id_%s", id)
+	return fmt.Sprintf("%s_%s", prefix, id)
+}
+
+func GenerateResponseID() string {
+	return generateID("samlresponse")
 }
