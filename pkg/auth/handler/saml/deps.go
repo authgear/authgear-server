6658eeba85a302916babaf28c998196e8780636a
Tung Wu <tung@oursky.com>
2024-08-19 15:18:29 +0800

    Setup login finish endpoint

--- pkg/auth/handler/saml/deps.go
+++ pkg/auth/handler/saml/deps.go
@@ -6,6 +6,8 @@ import (
 
 var DependencySet = wire.NewSet(
 	NewLoginHandlerLogger,
+	NewLoginFinishHandlerLogger,
 	wire.Struct(new(MetadataHandler), "*"),
 	wire.Struct(new(LoginHandler), "*"),
+	wire.Struct(new(LoginFinishHandler), "*"),
 )
