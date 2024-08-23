6c65438cbe5f3df4c441cc8d17f1e8507b729bc5
pkong-ds <pkong.work@gmail.com>
2024-08-23 14:58:15 +0800

    Add index on _audit_log_data_payload_recipient

---
+++ migrations/audit/20240822181609-add_data_payload_recipient_index_to_audit_logs.sql
@@ -0,0 +1,9 @@
+-- +migrate Up
+CREATE INDEX _audit_log_idx_app_id_data_payload_recipient_created_at ON _audit_log (
+  app_id,
+  (data -> 'payload' ->> 'recipient'),
+  created_at DESC
+);
+
+-- +migrate Down
+DROP INDEX _audit_log_idx_app_id_data_payload_recipient_created_at;
