489dc1c2654203d7c71425f603fd358f8f88f21f
Louis Chan <louischan@oursky.com>
2024-08-26 20:48:11 +0800

    Fix incorrect Input being used in TakeBranchResultInput

--- pkg/auth/webapp/session_authflow.go
+++ pkg/auth/webapp/session_authflow.go
@@ -837,7 +837,7 @@ func (s *AuthflowScreenWithFlowResponse) takeBranchCreateAuthenticator(input *Ta
 			options.DisableFallbackToSMS,
 		)
 		return TakeBranchResultInput{
-			Input: input,
+			Input: resultInput,
 			NewAuthflowScreenFull: func(flowResponse *authflow.FlowResponse, retriedForError error) *AuthflowScreenWithFlowResponse {
 				isContinuation := func(flowResponse *authflow.FlowResponse) bool {
 					return flowResponse.Action.Type == authflow.FlowActionType(config.AuthenticationFlowSignupFlowStepTypeCreateAuthenticator) &&
