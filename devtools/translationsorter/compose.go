2f0547a0c3b939f7c82633ab87ebb9102c58dd3d
pkong-ds <pkong.work@gmail.com>
2024-08-30 18:06:17 +0800

    Use %q to avoid unnecessary string manipulation

--- devtools/translationsorter/compose.go
+++ devtools/translationsorter/compose.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 func ComposeJSON(kvPairs []KeyValuePair, path string) (err error) {
@@ -23,8 +22,7 @@ func ComposeJSON(kvPairs []KeyValuePair, path string) (err error) {
 		if _, err = f.Write([]byte(fmt.Sprintf(lineStart + "  "))); err != nil {
 			return fmt.Errorf("failed to write: %w", err)
 		}
-		value := escapeSpecialChars(kvPair.Value)
-		byteArray := []byte(fmt.Sprintf("\"%s\": \"%s\"", kvPair.Key, value))
+		byteArray := []byte(fmt.Sprintf("%q: %q", kvPair.Key, kvPair.Value))
 		_, err = f.Write(byteArray)
 		if err != nil {
 			return fmt.Errorf("failed to write: %w", err)
@@ -34,20 +32,4 @@ func ComposeJSON(kvPairs []KeyValuePair, path string) (err error) {
 
 	f.Write([]byte("\n}\n"))
 	return
-}
-
-func escapeSpecialChars(s string) string {
-	// source - https://yourbasic.org/golang/multiline-string/#all-escape-characters
-	out := strings.ReplaceAll(s, "\\", "\\\\")
-	out = strings.ReplaceAll(out, "\a", "\\a")
-	out = strings.ReplaceAll(out, "\b", "\\b")
-	out = strings.ReplaceAll(out, "\t", "\\t")
-	out = strings.ReplaceAll(out, "\n", "\\n")
-	out = strings.ReplaceAll(out, "\f", "\\f")
-	out = strings.ReplaceAll(out, "\r", "\\r")
-	out = strings.ReplaceAll(out, "\v", "\\v")
-	out = strings.ReplaceAll(out, "\"", "\\\"")
-	out = strings.ReplaceAll(out, "\n", "\\n")
-
-	return out
 }
