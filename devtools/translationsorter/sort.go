87bb6b4ae93f676a140c8fc8fc7b193f04d8ba23
pkong-ds <pkong.work@gmail.com>
2024-08-30 16:55:53 +0800

    Implement sorting logic

--- devtools/translationsorter/sort.go
+++ devtools/translationsorter/sort.go
@@ -1,12 +1,34 @@
 package main
 
-func SortKeyValuePairs(kvPairs []KeyValuePair) []KeyValuePair {
-	var out []KeyValuePair
+import (
+	"sort"
+	"strings"
+)
 
-	// TODO: actually sort
-	out = append(out, KeyValuePair{
-		Key:   "foo",
-		Value: "bar",
+func SortKeyValuePairs(kvPairs []KeyValuePair) []KeyValuePair {
+	out := make([]KeyValuePair, len(kvPairs))
+	copy(out, kvPairs)
+
+	sort.SliceStable(out, func(i, j int) bool {
+		return cmpKey(out[i].Key, out[j].Key)
 	})
 	return out
+}
+
+func isV2Key(key string) bool {
+	return strings.HasPrefix(key, "v2")
+}
+
+func cmpKey(a, b string) bool {
+	switch {
+	case isV2Key(a) && !isV2Key(b):
+		return false
+	case !isV2Key(a) && isV2Key(b):
+		return true
+		// We want to sort V2 keys ONLY, v1 keys orders should be preserved
+	case !isV2Key(a) && !isV2Key(b):
+		return false
+	default:
+		return a < b
+	}
 }
