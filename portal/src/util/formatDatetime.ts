47f9e5efd4c773599037a1c78a965e10d3922ddc
mickmak-busicat <cos105hk@gmail.com>
2024-09-03 15:11:16 +0800

    Replace format date with custom date format option

--- portal/src/util/formatDatetime.ts
+++ portal/src/util/formatDatetime.ts
@@ -1,5 +1,16 @@
 import { DateTime } from "luxon";
 
+// Ref: https://tc39.es/ecma402/#sec-datetimeformat-abstracts
+const dateTimeWithTimezoneFormatOption = {
+  year: "numeric" as const,
+  month: "short" as const,
+  day: "numeric" as const,
+  hour: "numeric" as const,
+  minute: "numeric" as const,
+  second: "numeric" as const,
+  timeZoneName: "longOffset" as const,
+};
+
 export function formatDatetime(
   locale: string,
   date: Date | string | null
@@ -17,5 +28,5 @@ export function formatDatetime(
   }
 
   datetime = datetime.setLocale(locale);
-  return datetime.toFormat("ff ('UTC'ZZ)");
+  return datetime.toLocaleString(dateTimeWithTimezoneFormatOption);
 }
