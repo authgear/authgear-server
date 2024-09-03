83162256092ff2e9c3393efe2a817bc6d77ed4ba
YinYin Chiu <cychiuae@connect.ust.hk>
2024-09-03 19:10:40 +0800

    Implement country input

--- authui/src/authflowv2/countryInput.ts
+++ authui/src/authflowv2/countryInput.ts
@@ -1,3 +1,30 @@
 import { Controller } from "@hotwired/stimulus";
 
-export class CountryInputController extends Controller {}
+interface CountryOption {
+  code: string;
+  displayLabel: string;
+}
+
+export class CountryInputController extends Controller {
+  declare countrySelectTarget: HTMLElement;
+  declare countryOptionsValue: CountryOption[];
+
+  static targets = ["countrySelect"];
+
+  static values = {
+    countryOptions: Array,
+  };
+
+  connect(): void {
+    const selectOptions = this.countryOptionsValue.map((option) => ({
+      triggerLabel: option.displayLabel,
+      searchLabel: `${option.code} ${option.displayLabel}`,
+      label: option.displayLabel,
+      value: option.code,
+    }));
+    this.countrySelectTarget.setAttribute(
+      "data-custom-select-options-value",
+      JSON.stringify(selectOptions)
+    );
+  }
+}
