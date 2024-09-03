270be432448c1c7195733356d21141a0b9ad4025
YinYin Chiu <cychiuae@connect.ust.hk>
2024-09-03 18:43:14 +0800

    Scaffold edit address page

--- pkg/auth/handler/webapp/authflowv2/settings_profile_edit.go
+++ pkg/auth/handler/webapp/authflowv2/settings_profile_edit.go
@@ -17,6 +17,10 @@ import (
 
 func init() {
 	settingsProfileEditVariantToTemplate = make(map[string]*template.HTML)
+	settingsProfileEditVariantToTemplate["address"] = template.RegisterHTML(
+		"web/authflowv2/settings_profile_edit_address.html",
+		handlerwebapp.SettingsComponents...,
+	)
 	settingsProfileEditVariantToTemplate["birthdate"] = template.RegisterHTML(
 		"web/authflowv2/settings_profile_edit_birthdate.html",
 		handlerwebapp.SettingsComponents...,
