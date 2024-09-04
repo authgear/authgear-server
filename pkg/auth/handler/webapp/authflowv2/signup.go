7cc38ba16dbbe4f5c133668d9cf8a9b8e92492a7
Louis Chan <louischan@oursky.com>
2024-09-04 12:17:49 +0800

    Fix /signup can still be accessed if public_signup_disabled=true

--- pkg/auth/handler/webapp/authflowv2/signup.go
+++ pkg/auth/handler/webapp/authflowv2/signup.go
@@ -3,24 +3,35 @@ package authflowv2
 import (
 	"net/http"
 
+	"github.com/authgear/authgear-server/pkg/auth/webapp"
 	authflow "github.com/authgear/authgear-server/pkg/lib/authenticationflow"
 	"github.com/authgear/authgear-server/pkg/lib/config"
 )
 
 type AuthflowV2SignupHandler struct {
-	SignupLoginHandler InternalAuthflowV2SignupLoginHandler
-	UIConfig           *config.UIConfig
+	SignupLoginHandler   InternalAuthflowV2SignupLoginHandler
+	AuthenticationConfig *config.AuthenticationConfig
+	UIConfig             *config.UIConfig
 }
 
 func (h *AuthflowV2SignupHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h.AuthenticationConfig.PublicSignupDisabled {
+		path := "/login"
+		u := webapp.MakeRelativeURL(path, webapp.PreserveQuery(r.URL.Query()))
+		http.Redirect(w, r, u.String(), http.StatusFound)
+		return
+	}
+
 	flowType := authflow.FlowTypeSignup
 	canSwitchToLogin := true
 	uiVariant := AuthflowV2SignupUIVariantSignup
+
 	if h.UIConfig.SignupLoginFlowEnabled {
 		flowType = authflow.FlowTypeSignupLogin
 		canSwitchToLogin = false
 		uiVariant = AuthflowV2SignupUIVariantSignupLogin
 	}
+
 	h.SignupLoginHandler.ServeHTTP(w, r, AuthflowV2SignupServeOptions{
 		FlowType:         flowType,
 		CanSwitchToLogin: canSwitchToLogin,
