96e16603877191e3dc04342dadae81f4901f4772
Andy Chow <andychow@oursky.com>
2024-09-09 16:43:08 +0800

    Implement linkedin oauth provider as OIDC provider

--- pkg/lib/oauthrelyingparty/linkedin/provider.go
+++ pkg/lib/oauthrelyingparty/linkedin/provider.go
@@ -1,6 +1,8 @@
 package linkedin
 
 import (
+	"context"
+
 	"github.com/authgear/oauthrelyingparty/pkg/api/oauthrelyingparty"
 
 	"github.com/authgear/authgear-server/pkg/lib/authn/stdattrs"
@@ -18,11 +20,9 @@ const Type = liboauthrelyingparty.TypeLinkedin
 var _ oauthrelyingparty.Provider = Linkedin{}
 
 const (
-	linkedinAuthorizationURL string = "https://www.linkedin.com/oauth/v2/authorization"
 	// nolint: gosec
-	linkedinTokenURL   string = "https://www.linkedin.com/oauth/v2/accessToken"
-	linkedinMeURL      string = "https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName,profilePicture(displayImage~digitalmediaAsset:playableStreams))"
-	linkedinContactURL string = "https://api.linkedin.com/v2/clientAwareMemberHandles?q=members&projection=(elements*(primary,type,handle~))"
+	linkedinTokenURL                 string = "https://www.linkedin.com/oauth/v2/accessToken"
+	linkedinOIDCDiscoveryDocumentURL string = "https://www.linkedin.com/oauth/.well-known/openid-configuration"
 )
 
 type Linkedin struct{}
@@ -51,8 +51,8 @@ func (Linkedin) SetDefaults(cfg oauthrelyingparty.ProviderConfig) {
 }
 
 func (Linkedin) ProviderID(cfg oauthrelyingparty.ProviderConfig) oauthrelyingparty.ProviderID {
-	// Linkedin does NOT support OIDC.
-	// Linkedin user ID is scoped to client_id.
+	// Linkedin supports OIDC.
+	// sub is pairwise and is scoped to client_id.
 	// Therefore, ProviderID is Type + client_id.
 	//
 	// Rotating the OAuth application is problematic.
@@ -63,13 +63,16 @@ func (Linkedin) ProviderID(cfg oauthrelyingparty.ProviderConfig) oauthrelyingpar
 }
 
 func (Linkedin) scope() []string {
-	// https://docs.microsoft.com/en-us/linkedin/shared/references/v2/profile/lite-profile
-	// https://docs.microsoft.com/en-us/linkedin/shared/integrations/people/primary-contact-api?context=linkedin/compliance/context
-	return []string{"r_liteprofile", "r_emailaddress"}
+	// https://learn.microsoft.com/en-us/linkedin/consumer/integrations/self-serve/sign-in-with-linkedin-v2#authenticating-members
+	return []string{"openid", "profile", "email"}
 }
 
 func (p Linkedin) GetAuthorizationURL(deps oauthrelyingparty.Dependencies, param oauthrelyingparty.GetAuthorizationURLOptions) (string, error) {
-	return oauthrelyingpartyutil.MakeAuthorizationURL(linkedinAuthorizationURL, oauthrelyingpartyutil.AuthorizationURLParams{
+	d, err := oauthrelyingpartyutil.FetchOIDCDiscoveryDocument(deps.HTTPClient, linkedinOIDCDiscoveryDocumentURL)
+	if err != nil {
+		return "", err
+	}
+	return d.MakeOAuthURL(oauthrelyingpartyutil.AuthorizationURLParams{
 		ClientID:     deps.ProviderConfig.ClientID(),
 		RedirectURI:  param.RedirectURI,
 		Scope:        p.scope(),
@@ -81,7 +84,7 @@ func (p Linkedin) GetAuthorizationURL(deps oauthrelyingparty.Dependencies, param
 		// https://docs.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?tabs=HTTPS#step-2-request-an-authorization-code
 
 		// Nonce is unset
-	}.Query()), nil
+	}), nil
 }
 
 func (Linkedin) GetUserProfile(deps oauthrelyingparty.Dependencies, param oauthrelyingparty.GetUserProfileOptions) (authInfo oauthrelyingparty.UserProfile, err error) {
@@ -90,299 +93,69 @@ func (Linkedin) GetUserProfile(deps oauthrelyingparty.Dependencies, param oauthr
 		return
 	}
 
-	accessTokenResp, err := oauthrelyingpartyutil.FetchAccessTokenResp(
+	d, err := oauthrelyingpartyutil.FetchOIDCDiscoveryDocument(deps.HTTPClient, linkedinOIDCDiscoveryDocumentURL)
+	if err != nil {
+		return
+	}
+
+	keySet, err := d.FetchJWKs(deps.HTTPClient)
+	if err != nil {
+		return
+	}
+
+	var tokenResp oauthrelyingpartyutil.AccessTokenResp
+	jwtToken, err := d.ExchangeCode(
 		deps.HTTPClient,
+		deps.Clock,
 		code,
-		linkedinTokenURL,
-		param.RedirectURI,
+		keySet,
 		deps.ProviderConfig.ClientID(),
 		deps.ClientSecret,
+		param.RedirectURI,
+		param.Nonce,
+		&tokenResp,
 	)
 	if err != nil {
 		return
 	}
 
-	meResponse, err := oauthrelyingpartyutil.FetchUserProfile(deps.HTTPClient, accessTokenResp, linkedinMeURL)
+	claims, err := jwtToken.AsMap(context.TODO())
 	if err != nil {
 		return
 	}
 
-	contactResponse, err := oauthrelyingpartyutil.FetchUserProfile(deps.HTTPClient, accessTokenResp, linkedinContactURL)
+	iss, ok := claims["iss"].(string)
+	if !ok {
+		err = oauthrelyingpartyutil.OAuthProtocolError.New("iss not found in ID token")
+		return
+	}
+	if iss != d.Issuer {
+		err = oauthrelyingpartyutil.OAuthProtocolError.New("iss is not from LinkedIn")
+		return
+	}
+
+	sub, ok := claims["sub"].(string)
+	if !ok || sub == "" {
+		err = oauthrelyingpartyutil.OAuthProtocolError.New("sub not found in ID Token")
+		return
+	}
+
+	rawProfile, err := d.FetchUserInfo(deps.HTTPClient, tokenResp)
 	if err != nil {
 		return
 	}
 
-	// {
-	//     "primary_contact": {
-	//         "elements": [
-	//             {
-	//                 "handle": "urn:li:emailAddress:redacted",
-	//                 "handle~": {
-	//                     "emailAddress": "user@example.com"
-	//                 },
-	//                 "primary": true,
-	//                 "type": "EMAIL"
-	//             }
-	//         ]
-	//     },
-	//     "profile": {
-	//         "id": "redacted",
-	//         "localizedFirstName": "redacted",
-	//         "localizedLastName": "redacted",
-	//         "profilePicture": {
-	//             "displayImage": "urn:li:digitalmediaAsset:redacted",
-	//             "displayImage~": {
-	//                 "elements": [
-	//                     {
-	//                         "artifact": "urn:li:digitalmediaMediaArtifact:(urn:li:digitalmediaAsset:C5603AQE9WylLgWcyuA,urn:li:digitalmediaMediaArtifactClass:profile-displayphoto-shrink_100_100)",
-	//                         "authorizationMethod": "PUBLIC",
-	//                         "data": {
-	//                             "com.linkedin.digitalmedia.mediaartifact.StillImage": {
-	//                                 "displayAspectRatio": {
-	//                                     "formatted": "1.00:1.00",
-	//                                     "heightAspect": 1,
-	//                                     "widthAspect": 1
-	//                                 },
-	//                                 "displaySize": {
-	//                                     "height": 100,
-	//                                     "uom": "PX",
-	//                                     "width": 100
-	//                                 },
-	//                                 "mediaType": "image/jpeg",
-	//                                 "rawCodecSpec": {
-	//                                     "name": "jpeg",
-	//                                     "type": "image"
-	//                                 },
-	//                                 "storageAspectRatio": {
-	//                                     "formatted": "1.00:1.00",
-	//                                     "heightAspect": 1,
-	//                                     "widthAspect": 1
-	//                                 },
-	//                                 "storageSize": {
-	//                                     "height": 100,
-	//                                     "width": 100
-	//                                 }
-	//                             }
-	//                         },
-	//                         "identifiers": [
-	//                             {
-	//                                 "file": "urn:li:digitalmediaFile:(urn:li:digitalmediaAsset:C5603AQE9WylLgWcyuA,urn:li:digitalmediaMediaArtifactClass:profile-displayphoto-shrink_100_100,0)",
-	//                                 "identifier": "https://media-exp1.licdn.com/dms/image/C5603AQE9WylLgWcyuA/profile-displayphoto-shrink_100_100/0/1631684043723?e=1637193600&v=beta&t=h8Wz-EdTjSD9FxQL_oO6hrQ4DdwzGfl5fPPe2cEDPIs",
-	//                                 "identifierExpiresInSeconds": 1637193600,
-	//                                 "identifierType": "EXTERNAL_URL",
-	//                                 "index": 0,
-	//                                 "mediaType": "image/jpeg"
-	//                             }
-	//                         ]
-	//                     },
-	//                     {
-	//                         "artifact": "urn:li:digitalmediaMediaArtifact:(urn:li:digitalmediaAsset:C5603AQE9WylLgWcyuA,urn:li:digitalmediaMediaArtifactClass:profile-displayphoto-shrink_200_200)",
-	//                         "authorizationMethod": "PUBLIC",
-	//                         "data": {
-	//                             "com.linkedin.digitalmedia.mediaartifact.StillImage": {
-	//                                 "displayAspectRatio": {
-	//                                     "formatted": "1.00:1.00",
-	//                                     "heightAspect": 1,
-	//                                     "widthAspect": 1
-	//                                 },
-	//                                 "displaySize": {
-	//                                     "height": 200,
-	//                                     "uom": "PX",
-	//                                     "width": 200
-	//                                 },
-	//                                 "mediaType": "image/jpeg",
-	//                                 "rawCodecSpec": {
-	//                                     "name": "jpeg",
-	//                                     "type": "image"
-	//                                 },
-	//                                 "storageAspectRatio": {
-	//                                     "formatted": "1.00:1.00",
-	//                                     "heightAspect": 1,
-	//                                     "widthAspect": 1
-	//                                 },
-	//                                 "storageSize": {
-	//                                     "height": 200,
-	//                                     "width": 200
-	//                                 }
-	//                             }
-	//                         },
-	//                         "identifiers": [
-	//                             {
-	//                                 "file": "urn:li:digitalmediaFile:(urn:li:digitalmediaAsset:C5603AQE9WylLgWcyuA,urn:li:digitalmediaMediaArtifactClass:profile-displayphoto-shrink_200_200,0)",
-	//                                 "identifier": "https://media-exp1.licdn.com/dms/image/C5603AQE9WylLgWcyuA/profile-displayphoto-shrink_200_200/0/1631684043723?e=1637193600&v=beta&t=8CDBMjGCkpk_CO8VgAkVXWeKAu8gYiUTTXPbtMazMUg",
-	//                                 "identifierExpiresInSeconds": 1637193600,
-	//                                 "identifierType": "EXTERNAL_URL",
-	//                                 "index": 0,
-	//                                 "mediaType": "image/jpeg"
-	//                             }
-	//                         ]
-	//                     },
-	//                     {
-	//                         "artifact": "urn:li:digitalmediaMediaArtifact:(urn:li:digitalmediaAsset:C5603AQE9WylLgWcyuA,urn:li:digitalmediaMediaArtifactClass:profile-displayphoto-shrink_400_400)",
-	//                         "authorizationMethod": "PUBLIC",
-	//                         "data": {
-	//                             "com.linkedin.digitalmedia.mediaartifact.StillImage": {
-	//                                 "displayAspectRatio": {
-	//                                     "formatted": "1.00:1.00",
-	//                                     "heightAspect": 1,
-	//                                     "widthAspect": 1
-	//                                 },
-	//                                 "displaySize": {
-	//                                     "height": 400,
-	//                                     "uom": "PX",
-	//                                     "width": 400
-	//                                 },
-	//                                 "mediaType": "image/jpeg",
-	//                                 "rawCodecSpec": {
-	//                                     "name": "jpeg",
-	//                                     "type": "image"
-	//                                 },
-	//                                 "storageAspectRatio": {
-	//                                     "formatted": "1.00:1.00",
-	//                                     "heightAspect": 1,
-	//                                     "widthAspect": 1
-	//                                 },
-	//                                 "storageSize": {
-	//                                     "height": 400,
-	//                                     "width": 400
-	//                                 }
-	//                             }
-	//                         },
-	//                         "identifiers": [
-	//                             {
-	//                                 "file": "urn:li:digitalmediaFile:(urn:li:digitalmediaAsset:C5603AQE9WylLgWcyuA,urn:li:digitalmediaMediaArtifactClass:profile-displayphoto-shrink_400_400,0)",
-	//                                 "identifier": "https://media-exp1.licdn.com/dms/image/C5603AQE9WylLgWcyuA/profile-displayphoto-shrink_400_400/0/1631684043723?e=1637193600&v=beta&t=9tCLl0cAbswfKYUgJqDN41QT368cFsq_7TeXyPjixOY",
-	//                                 "identifierExpiresInSeconds": 1637193600,
-	//                                 "identifierType": "EXTERNAL_URL",
-	//                                 "index": 0,
-	//                                 "mediaType": "image/jpeg"
-	//                             }
-	//                         ]
-	//                     },
-	//                     {
-	//                         "artifact": "urn:li:digitalmediaMediaArtifact:(urn:li:digitalmediaAsset:C5603AQE9WylLgWcyuA,urn:li:digitalmediaMediaArtifactClass:profile-displayphoto-shrink_800_800)",
-	//                         "authorizationMethod": "PUBLIC",
-	//                         "data": {
-	//                             "com.linkedin.digitalmedia.mediaartifact.StillImage": {
-	//                                 "displayAspectRatio": {
-	//                                     "formatted": "1.00:1.00",
-	//                                     "heightAspect": 1,
-	//                                     "widthAspect": 1
-	//                                 },
-	//                                 "displaySize": {
-	//                                     "height": 800,
-	//                                     "uom": "PX",
-	//                                     "width": 800
-	//                                 },
-	//                                 "mediaType": "image/jpeg",
-	//                                 "rawCodecSpec": {
-	//                                     "name": "jpeg",
-	//                                     "type": "image"
-	//                                 },
-	//                                 "storageAspectRatio": {
-	//                                     "formatted": "1.00:1.00",
-	//                                     "heightAspect": 1,
-	//                                     "widthAspect": 1
-	//                                 },
-	//                                 "storageSize": {
-	//                                     "height": 800,
-	//                                     "width": 800
-	//                                 }
-	//                             }
-	//                         },
-	//                         "identifiers": [
-	//                             {
-	//                                 "file": "urn:li:digitalmediaFile:(urn:li:digitalmediaAsset:C5603AQE9WylLgWcyuA,urn:li:digitalmediaMediaArtifactClass:profile-displayphoto-shrink_800_800,0)",
-	//                                 "identifier": "https://media-exp1.licdn.com/dms/image/C5603AQE9WylLgWcyuA/profile-displayphoto-shrink_800_800/0/1631684043723?e=1637193600&v=beta&t=hvhZcRfvDrgE64iXNX1J2eWUMAytTtD8SdD006lc3_o",
-	//                                 "identifierExpiresInSeconds": 1637193600,
-	//                                 "identifierType": "EXTERNAL_URL",
-	//                                 "index": 0,
-	//                                 "mediaType": "image/jpeg"
-	//                             }
-	//                         ]
-	//                     }
-	//                 ],
-	//                 "paging": {
-	//                     "count": 10,
-	//                     "links": [],
-	//                     "start": 0
-	//                 }
-	//             }
-	//         }
-	//     }
-	// }
-	combinedResponse := map[string]interface{}{
-		"profile":         meResponse,
-		"primary_contact": contactResponse,
-	}
-
-	authInfo.ProviderRawProfile = combinedResponse
-	id, attrs := decodeLinkedIn(combinedResponse)
-	authInfo.ProviderUserID = id
+	authInfo.ProviderRawProfile = rawProfile
+	authInfo.ProviderUserID = sub
 
 	emailRequired := deps.ProviderConfig.EmailClaimConfig().Required()
-	attrs, err = stdattrs.Extract(attrs, stdattrs.ExtractOptions{
+	stdAttrs, err := stdattrs.Extract(rawProfile, stdattrs.ExtractOptions{
 		EmailRequired: emailRequired,
 	})
 	if err != nil {
 		return
 	}
-	authInfo.StandardAttributes = attrs
+	authInfo.StandardAttributes = stdAttrs
 
 	return
-}
-
-func decodeLinkedIn(userInfo map[string]interface{}) (string, stdattrs.T) {
-	profile := userInfo["profile"].(map[string]interface{})
-	id := profile["id"].(string)
-
-	// Extract email
-	email := ""
-	{
-		primaryContact, _ := userInfo["primary_contact"].(map[string]interface{})
-		elements, _ := primaryContact["elements"].([]interface{})
-		for _, e := range elements {
-			element, _ := e.(map[string]interface{})
-			if primary, ok := element["primary"].(bool); !ok || !primary {
-				continue
-			}
-			if typ, ok := element["type"].(string); !ok || typ != "EMAIL" {
-				continue
-			}
-			handleTilde, ok := element["handle~"].(map[string]interface{})
-			if !ok {
-				continue
-			}
-			email, _ = handleTilde["emailAddress"].(string)
-		}
-	}
-
-	// Extract given_name and family_name
-	firstName, _ := profile["localizedFirstName"].(string)
-	lastName, _ := profile["localizedLastName"].(string)
-
-	// Extract picture
-	var picture string
-	{
-		profilePicture, _ := profile["profilePicture"].(map[string]interface{})
-		displayImage, _ := profilePicture["displayImage~"].(map[string]interface{})
-		elements, _ := displayImage["elements"].([]interface{})
-		if len(elements) > 0 {
-			lastElementIface := elements[len(elements)-1]
-			lastElement, _ := lastElementIface.(map[string]interface{})
-			identifiers, _ := lastElement["identifiers"].([]interface{})
-			if len(identifiers) > 0 {
-				firstIdentifierIface := identifiers[0]
-				firstIdentifier, _ := firstIdentifierIface.(map[string]interface{})
-				picture, _ = firstIdentifier["identifier"].(string)
-			}
-		}
-	}
-
-	return id, stdattrs.T{
-		stdattrs.Email:      email,
-		stdattrs.GivenName:  firstName,
-		stdattrs.FamilyName: lastName,
-		stdattrs.Picture:    picture,
-	}
 }
--- pkg/lib/oauthrelyingparty/linkedin/provider_test.go
+++ pkg/lib/oauthrelyingparty/linkedin/provider_test.go
@@ -1,30 +1,75 @@
 package linkedin
 
 import (
+	"net/http"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
+	"gopkg.in/h2non/gock.v1"
 
 	"github.com/authgear/oauthrelyingparty/pkg/api/oauthrelyingparty"
 )
 
 func TestLinkedin(t *testing.T) {
 	Convey("Linkedin", t, func() {
+		client := &http.Client{}
+		gock.InterceptClient(client)
+		defer gock.Off()
+
 		deps := oauthrelyingparty.Dependencies{
 			ProviderConfig: oauthrelyingparty.ProviderConfig{
 				"client_id": "client_id",
 				"type":      Type,
 			},
+			HTTPClient: client,
 		}
 		g := Linkedin{}
 
+		gock.New(linkedinOIDCDiscoveryDocumentURL).Reply(200).BodyString(`
+{
+  "issuer": "https://www.linkedin.com/oauth",
+  "authorization_endpoint": "https://www.linkedin.com/oauth/v2/authorization",
+  "token_endpoint": "https://www.linkedin.com/oauth/v2/accessToken",
+  "userinfo_endpoint": "https://api.linkedin.com/v2/userinfo",
+  "jwks_uri": "https://www.linkedin.com/oauth/openid/jwks",
+  "response_types_supported": [
+    "code"
+  ],
+  "subject_types_supported": [
+    "pairwise"
+  ],
+  "id_token_signing_alg_values_supported": [
+    "RS256"
+  ],
+  "scopes_supported": [
+    "openid",
+    "profile",
+    "email"
+  ],
+  "claims_supported": [
+    "iss",
+    "aud",
+    "iat",
+    "exp",
+    "sub",
+    "name",
+    "given_name",
+    "family_name",
+    "picture",
+    "email",
+    "email_verified",
+    "locale"
+  ]
+}
+		`)
+		defer func() { gock.Flush() }()
+
 		u, err := g.GetAuthorizationURL(deps, oauthrelyingparty.GetAuthorizationURLOptions{
 			RedirectURI: "https://localhost/",
-			Nonce:       "nonce",
 			State:       "state",
 			Prompt:      []string{"login"},
 		})
 		So(err, ShouldBeNil)
-		So(u, ShouldEqual, "https://www.linkedin.com/oauth/v2/authorization?client_id=client_id&redirect_uri=https%3A%2F%2Flocalhost%2F&response_type=code&scope=r_liteprofile+r_emailaddress&state=state")
+		So(u, ShouldEqual, "https://www.linkedin.com/oauth/v2/authorization?client_id=client_id&redirect_uri=https%3A%2F%2Flocalhost%2F&response_type=code&scope=openid+profile+email&state=state")
 	})
 }
