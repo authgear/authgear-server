2bda8e9de6cb87cb95c8b5d777f8f8fe86fb8a29
pkong-ds <pkong.work@gmail.com>
2024-09-09 16:55:17 +0800

    Implement mobile activity type filter dropdown

--- portal/src/components/audit-log/ActivityTypeFilterDropdown.tsx
+++ portal/src/components/audit-log/ActivityTypeFilterDropdown.tsx
@@ -1,8 +1,14 @@
 import React, { useCallback, useContext, useMemo } from "react";
 import { Context as MessageContext } from "@oursky/react-messageformat";
-import { IContextualMenuItem, IContextualMenuProps } from "@fluentui/react";
+import {
+  Dropdown,
+  IContextualMenuItem,
+  IContextualMenuProps,
+  IDropdownOption,
+} from "@fluentui/react";
 import CommandBarButton from "../../CommandBarButton";
 import { AuditLogActivityType } from "../../graphql/adminapi/globalTypes.generated";
+import { useScreenBreakpoint } from "../../hook/useScreenBreakpoint";
 
 export type AuditLogActivityTypeAll = "ALL";
 export const ACTIVITY_TYPE_ALL: AuditLogActivityTypeAll = "ALL";
@@ -23,8 +29,8 @@ interface ActivityTypeFilterDropdownProps {
   availableActivityTypes: AuditLogActivityType[];
 }
 
-export const ActivityTypeFilterDropdown: React.VFC<ActivityTypeFilterDropdownProps> =
-  function ActivityTypeFilterDropdown({
+const DesktopActivityTypeFilterDropdown: React.VFC<ActivityTypeFilterDropdownProps> =
+  function DesktopActivityTypeFilterDropdown({
     className,
     value,
     onChange,
@@ -83,4 +89,61 @@ export const ActivityTypeFilterDropdown: React.VFC<ActivityTypeFilterDropdownPro
         text={placeholder}
       />
     );
+  };
+
+const MobileActivityTypeFilterDropdown: React.VFC<ActivityTypeFilterDropdownProps> =
+  function MobileActivityTypeFilterDropdown({
+    className,
+    value,
+    onChange,
+    availableActivityTypes,
+  }: ActivityTypeFilterDropdownProps) {
+    const { renderToString } = useContext(MessageContext);
+
+    const options = useMemo<IDropdownOption[]>(() => {
+      const options: IDropdownOption[] = [
+        {
+          key: ACTIVITY_TYPE_ALL,
+          text: renderToString("AuditLogActivityType.ALL"),
+        },
+      ];
+      for (const key of availableActivityTypes) {
+        options.push({
+          key: key,
+          text: renderToString("AuditLogActivityType." + key),
+        });
+      }
+      return options;
+    }, [availableActivityTypes, renderToString]);
+
+    const onChangeOption = useCallback(
+      (_e: unknown, option?: IDropdownOption) => {
+        if (option == null) {
+          return;
+        }
+        onChange(option.key as ActivityTypeFilterDropdownOptionKey);
+      },
+      [onChange]
+    );
+    return (
+      <Dropdown
+        className={className}
+        selectedKey={value}
+        options={options}
+        onChange={onChangeOption}
+      />
+    );
+  };
+
+export const ActivityTypeFilterDropdown: React.VFC<ActivityTypeFilterDropdownProps> =
+  function ActivityTypeFilterDropdown(props: ActivityTypeFilterDropdownProps) {
+    const screenBreakpoint = useScreenBreakpoint();
+
+    switch (screenBreakpoint) {
+      case "desktop":
+        return <DesktopActivityTypeFilterDropdown {...props} />;
+      case "mobile":
+      case "tablet":
+        return <MobileActivityTypeFilterDropdown {...props} />;
+    }
   };
