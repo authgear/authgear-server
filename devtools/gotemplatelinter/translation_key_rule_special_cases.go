1533f988650cdfaf85085c259a00103244894a60
pkong-ds <pkong.work@gmail.com>
2024-09-11 13:20:44 +0800

    Remove templates false positive from special cases
    These should have `.html` suffix. But we skip it to avoid merge conflicts for now

--- devtools/gotemplatelinter/translation_key_rule_special_cases.go
+++ devtools/gotemplatelinter/translation_key_rule_special_cases.go
@@ -7,32 +7,11 @@ var allowedLegitTranslationKeys []string = []string{
 	"terms-of-service-link",
 }
 
-var goTemplateFalstPositives []string = []string{
-	"widget",
-	"page-frame-content",
-	"page-content",
-	"dialog-attr",
-	"dialog-controller-str",
-	"dialog-content",
-	"dialog-close-btn",
-	"authflowv2/__error_account",
-	"page-navbar",
-	"__forgot_password_alternative",
-	"__settings_profile_item",
-	"__settings_profile_date_item",
-	"__settings_profile_address_item",
-	"__settings_profile_locale_item",
-	"__settings_gender_edit_custom_gender_input",
-}
-
 var AllowedKeys map[string]struct{}
 
 func init() {
 	AllowedKeys = make(map[string]struct{})
 	for _, k := range allowedLegitTranslationKeys {
-		AllowedKeys[k] = struct{}{}
-	}
-	for _, k := range goTemplateFalstPositives {
 		AllowedKeys[k] = struct{}{}
 	}
 }
