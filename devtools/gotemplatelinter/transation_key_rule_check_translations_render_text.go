33fabdc2376880f78df069f2da8e0d41fc64a1ed
pkong-ds <pkong.work@gmail.com>
2024-09-11 17:01:58 +0800

    Forbidden translationKeys.RenderText

--- devtools/gotemplatelinter/transation_key_rule_check_translations_render_text.go
+++ devtools/gotemplatelinter/transation_key_rule_check_translations_render_text.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"text/template/parse"
 )
 
@@ -9,15 +10,5 @@ import (
 // example: `($.Translations.RenderText "customer-support-link" nil)`
 // example: (.Translations.RenderText "terms-of-service-link" nil)
 func CheckCommandTranslationsRenderText(node *parse.CommandNode) (err error) {
-	// 2nd arg should be translation key
-	for idx, arg := range node.Args {
-		if idx == 1 {
-			err = CheckTranslationKeyNode(arg)
-			if err != nil {
-				return err
-			}
-		}
-
-	}
-	return
+	return fmt.Errorf("Translations.RenderText is forbidden: `%s`", node.String())
 }
