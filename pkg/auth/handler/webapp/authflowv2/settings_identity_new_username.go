e36f4361b839541cc2f034d16397c3667076406d
YinYin Chiu <cychiuae@connect.ust.hk>
2024-09-12 22:34:18 +0800

    Implement add new username

--- pkg/auth/handler/webapp/authflowv2/settings_identity_new_username.go
+++ pkg/auth/handler/webapp/authflowv2/settings_identity_new_username.go
@@ -2,18 +2,34 @@ package authflowv2
 
 import (
 	"net/http"
+	"net/url"
 
 	handlerwebapp "github.com/authgear/authgear-server/pkg/auth/handler/webapp"
 	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
+	"github.com/authgear/authgear-server/pkg/auth/webapp"
+	"github.com/authgear/authgear-server/pkg/lib/accountmanagement"
 	"github.com/authgear/authgear-server/pkg/lib/infra/db/appdb"
+	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/util/httproute"
 	"github.com/authgear/authgear-server/pkg/util/template"
+	"github.com/authgear/authgear-server/pkg/util/validation"
 )
 
 var TemplateSettingsIdentityNewUsernameTemplate = template.RegisterHTML(
 	"web/authflowv2/settings_identity_new_username.html",
 	handlerwebapp.SettingsComponents...,
 )
+
+var AuthflowV2SettingsIdentityNewUsernameSchema = validation.NewSimpleSchema(`
+	{
+		"type": "object",
+		"properties": {
+			"x_login_id_key": { "type": "string" },
+			"x_login_id": { "type": "string" }
+		},
+		"required": ["x_login_id_key", "x_login_id"]
+	}
+`)
 
 func ConfigureAuthflowV2SettingsIdentityNewUsername(route httproute.Route) httproute.Route {
 	return route.
@@ -27,6 +43,7 @@ type AuthflowV2SettingsIdentityNewUsernameViewModel struct {
 
 type AuthflowV2SettingsIdentityNewUsernameHandler struct {
 	Database          *appdb.Handle
+	AccountManagement *accountmanagement.Service
 	ControllerFactory handlerwebapp.ControllerFactory
 	BaseViewModel     *viewmodels.BaseViewModeler
 	Renderer          handlerwebapp.Renderer
@@ -64,6 +81,35 @@ func (h *AuthflowV2SettingsIdentityNewUsernameHandler) ServeHTTP(w http.Response
 			return err
 		}
 		h.Renderer.RenderHTML(w, r, TemplateSettingsIdentityNewUsernameTemplate, data)
+		return nil
+	})
+
+	ctrl.PostAction("", func() error {
+		err := AuthflowV2SettingsIdentityNewUsernameSchema.Validator().ValidateValue(handlerwebapp.FormToJSON(r.Form))
+		if err != nil {
+			return err
+		}
+		loginIDKey := r.Form.Get("x_login_id_key")
+		loginID := r.Form.Get("x_login_id")
+		resolvedSession := session.GetSession(r.Context())
+		_, err = h.AccountManagement.AddUsername(resolvedSession, &accountmanagement.AddUsernameInput{
+			LoginIDKey: loginIDKey,
+			LoginID:    loginID,
+		})
+		if err != nil {
+			return err
+		}
+
+		redirectURI, err := url.Parse(AuthflowV2RouteSettingsIdentityListUsername)
+		if err != nil {
+			return err
+		}
+		q := redirectURI.Query()
+		q.Set("q_login_id_key", loginIDKey)
+		redirectURI.RawQuery = q.Encode()
+		result := webapp.Result{RedirectURI: redirectURI.String()}
+		result.WriteResponse(w, r)
+
 		return nil
 	})
 }
