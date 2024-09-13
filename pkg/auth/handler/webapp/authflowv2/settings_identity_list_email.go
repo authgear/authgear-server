59279341f2a74a8c7ae1ea31380daf7b1121a50b
Zachary Tsang <u.quixz@gmail.com>
2024-09-13 11:40:54 +0800

    Compute view model for list email page

--- pkg/auth/handler/webapp/authflowv2/settings_identity_list_email.go
+++ pkg/auth/handler/webapp/authflowv2/settings_identity_list_email.go
@@ -2,9 +2,17 @@ package authflowv2
 
 import (
 	"net/http"
+	"sort"
 
+	"github.com/authgear/authgear-server/pkg/api/model"
 	handlerwebapp "github.com/authgear/authgear-server/pkg/auth/handler/webapp"
 	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
+	"github.com/authgear/authgear-server/pkg/lib/authn/identity"
+	identityservice "github.com/authgear/authgear-server/pkg/lib/authn/identity/service"
+	"github.com/authgear/authgear-server/pkg/lib/config"
+	"github.com/authgear/authgear-server/pkg/lib/feature/verification"
+	"github.com/authgear/authgear-server/pkg/lib/infra/db/appdb"
+	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/util/httproute"
 	"github.com/authgear/authgear-server/pkg/util/template"
 )
@@ -20,18 +28,69 @@ func ConfigureAuthflowV2SettingsIdentityListEmailRoute(route httproute.Route) ht
 		WithPathPattern(AuthflowV2RouteSettingsIdentityListEmail)
 }
 
+type AuthflowV2SettingsIdentityListEmailViewModel struct {
+	LoginIDKey      string
+	EmailIdentities []*identity.LoginID
+	Verifications   map[string][]verification.ClaimStatus
+	CreateDisabled  bool
+}
+
 type AuthflowV2SettingsIdentityListEmailHandler struct {
-	ControllerFactory        handlerwebapp.ControllerFactory
-	BaseViewModel            *viewmodels.BaseViewModeler
-	SettingsProfileViewModel *viewmodels.SettingsProfileViewModeler
-	Renderer                 handlerwebapp.Renderer
+	Database          *appdb.Handle
+	LoginIDConfig     *config.LoginIDConfig
+	Identities        *identityservice.Service
+	ControllerFactory handlerwebapp.ControllerFactory
+	BaseViewModel     *viewmodels.BaseViewModeler
+	Verification      handlerwebapp.SettingsVerificationService
+	Renderer          handlerwebapp.Renderer
 }
 
 func (h *AuthflowV2SettingsIdentityListEmailHandler) GetData(r *http.Request, rw http.ResponseWriter) (map[string]interface{}, error) {
+	loginIDKey := r.Form.Get("q_login_id_key")
 	data := map[string]interface{}{}
 
 	baseViewModel := h.BaseViewModel.ViewModel(r, rw)
 	viewmodels.Embed(data, baseViewModel)
+
+	userID := session.GetUserID(r.Context())
+
+	identities, err := h.Identities.LoginID.List(*userID)
+	if err != nil {
+		return nil, err
+	}
+
+	var emailIdentities []*identity.LoginID
+	var emailInfos []*identity.Info
+	for _, identity := range identities {
+		if identity.LoginIDType == model.LoginIDKeyTypeEmail {
+			if loginIDKey == "" || identity.LoginIDKey == loginIDKey {
+				emailIdentities = append(emailIdentities, identity)
+				emailInfos = append(emailInfos, identity.ToInfo())
+			}
+		}
+	}
+
+	sort.Slice(emailIdentities, func(i, j int) bool {
+		return emailIdentities[i].UpdatedAt.Before(emailIdentities[j].UpdatedAt)
+	})
+
+	verifications, err := h.Verification.GetVerificationStatuses(emailInfos)
+	if err != nil {
+		return nil, err
+	}
+
+	createDisabled := true
+	if loginIDConfig, ok := h.LoginIDConfig.GetKeyConfig(loginIDKey); ok {
+		createDisabled = *loginIDConfig.CreateDisabled
+	}
+
+	vm := AuthflowV2SettingsIdentityListEmailViewModel{
+		LoginIDKey:      loginIDKey,
+		EmailIdentities: emailIdentities,
+		Verifications:   verifications,
+		CreateDisabled:  createDisabled,
+	}
+	viewmodels.Embed(data, vm)
 
 	return data, nil
 }
@@ -42,10 +101,14 @@ func (h *AuthflowV2SettingsIdentityListEmailHandler) ServeHTTP(w http.ResponseWr
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	defer ctrl.ServeWithDBTx()
+	defer ctrl.ServeWithoutDBTx()
 
 	ctrl.Get(func() error {
-		data, err := h.GetData(r, w)
+		var data map[string]interface{}
+		err := h.Database.WithTx(func() error {
+			data, err = h.GetData(r, w)
+			return err
+		})
 		if err != nil {
 			return err
 		}
