b7377d7d70a3de034256322feb8073a49c6df4e5
YinYin Chiu <cychiuae@connect.ust.hk>
2024-09-13 14:41:20 +0800

    Implement edit username

--- pkg/auth/handler/webapp/authflowv2/settings_identity_edit_username.go
+++ pkg/auth/handler/webapp/authflowv2/settings_identity_edit_username.go
@@ -2,21 +2,37 @@ package authflowv2
 
 import (
 	"net/http"
+	"net/url"
 
 	handlerwebapp "github.com/authgear/authgear-server/pkg/auth/handler/webapp"
 	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
+	"github.com/authgear/authgear-server/pkg/auth/webapp"
+	"github.com/authgear/authgear-server/pkg/lib/accountmanagement"
 	"github.com/authgear/authgear-server/pkg/lib/authn/identity"
 	identityservice "github.com/authgear/authgear-server/pkg/lib/authn/identity/service"
 	"github.com/authgear/authgear-server/pkg/lib/infra/db/appdb"
 	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/util/httproute"
 	"github.com/authgear/authgear-server/pkg/util/template"
+	"github.com/authgear/authgear-server/pkg/util/validation"
 )
 
 var TemplateSettingsIdentityEditUsernameTemplate = template.RegisterHTML(
 	"web/authflowv2/settings_identity_edit_username.html",
 	handlerwebapp.SettingsComponents...,
 )
+
+var AuthflowV2SettingsIdentityEditUsernameSchema = validation.NewSimpleSchema(`
+	{
+		"type": "object",
+		"properties": {
+			"x_login_id_key": { "type": "string" },
+			"x_login_id": { "type": "string" },
+			"x_identity_id": { "type": "string" }
+		},
+		"required": ["x_login_id_key", "x_login_id", "x_identity_id"]
+	}
+`)
 
 func ConfigureAuthflowV2SettingsIdentityEditUsername(route httproute.Route) httproute.Route {
 	return route.
@@ -30,6 +46,7 @@ type AuthflowV2SettingsIdentityEditUsernameViewModel struct {
 
 type AuthflowV2SettingsIdentityEditUsernameHandler struct {
 	Database          *appdb.Handle
+	AccountManagement *accountmanagement.Service
 	Identities        *identityservice.Service
 	ControllerFactory handlerwebapp.ControllerFactory
 	BaseViewModel     *viewmodels.BaseViewModeler
@@ -74,6 +91,37 @@ func (h *AuthflowV2SettingsIdentityEditUsernameHandler) ServeHTTP(w http.Respons
 			return err
 		}
 		h.Renderer.RenderHTML(w, r, TemplateSettingsIdentityEditUsernameTemplate, data)
+		return nil
+	})
+
+	ctrl.PostAction("", func() error {
+		err := AuthflowV2SettingsIdentityEditUsernameSchema.Validator().ValidateValue(handlerwebapp.FormToJSON(r.Form))
+		if err != nil {
+			return err
+		}
+		identityID := r.Form.Get("x_identity_id")
+		loginIDKey := r.Form.Get("x_login_id_key")
+		loginID := r.Form.Get("x_login_id")
+		resolvedSession := session.GetSession(r.Context())
+		_, err = h.AccountManagement.UpdateUsername(resolvedSession, &accountmanagement.UpdateUsernameInput{
+			IdentityID: identityID,
+			LoginIDKey: loginIDKey,
+			LoginID:    loginID,
+		})
+		if err != nil {
+			return err
+		}
+
+		redirectURI, err := url.Parse(AuthflowV2RouteSettingsIdentityListUsername)
+		if err != nil {
+			return err
+		}
+		q := redirectURI.Query()
+		q.Set("q_login_id_key", loginIDKey)
+		redirectURI.RawQuery = q.Encode()
+		result := webapp.Result{RedirectURI: redirectURI.String()}
+		result.WriteResponse(w, r)
+
 		return nil
 	})
 }
