ffdc7bd31f193187445129a3d8b414f9444e9ba1
Zachary Tsang <u.quixz@gmail.com>
2024-09-16 16:34:50 +0800

    Create post handler for add email

--- pkg/auth/handler/webapp/authflowv2/settings_identity_add_email.go
+++ pkg/auth/handler/webapp/authflowv2/settings_identity_add_email.go
@@ -2,18 +2,33 @@ package authflowv2
 
 import (
 	"net/http"
+	"net/url"
 
+	"github.com/authgear/authgear-server/pkg/api/model"
 	handlerwebapp "github.com/authgear/authgear-server/pkg/auth/handler/webapp"
 	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
 	"github.com/authgear/authgear-server/pkg/auth/webapp"
+	"github.com/authgear/authgear-server/pkg/lib/accountmanagement"
+	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/util/httproute"
 	"github.com/authgear/authgear-server/pkg/util/template"
+	"github.com/authgear/authgear-server/pkg/util/validation"
 )
 
 var TemplateWebSettingsIdentityAddEmailHTML = template.RegisterHTML(
 	"web/authflowv2/settings_identity_add_email.html",
 	handlerwebapp.SettingsComponents...,
 )
+
+var AuthflowV2SettingsIdentityAddEmailSchema = validation.NewSimpleSchema(`
+	{
+		"type": "object",
+		"properties": {
+			"x_login_id": { "type": "string" }
+		},
+		"required": ["x_login_id"]
+	}
+`)
 
 func ConfigureAuthflowV2SettingsIdentityAddEmailRoute(route httproute.Route) httproute.Route {
 	return route.
@@ -29,6 +44,7 @@ type AuthflowV2SettingsIdentityAddEmailHandler struct {
 	ControllerFactory handlerwebapp.ControllerFactory
 	BaseViewModel     *viewmodels.BaseViewModeler
 	Renderer          handlerwebapp.Renderer
+	AccountManagement accountmanagement.Service
 }
 
 func (h *AuthflowV2SettingsIdentityAddEmailHandler) GetData(r *http.Request, rw http.ResponseWriter) (map[string]interface{}, error) {
@@ -65,8 +81,48 @@ func (h *AuthflowV2SettingsIdentityAddEmailHandler) ServeHTTP(w http.ResponseWri
 		return nil
 	})
 
-	ctrl.PostAction("save", func() error {
-		result := webapp.Result{RedirectURI: "/settings/identity/email"}
+	ctrl.PostAction("", func() error {
+		loginIDKey := r.Form.Get("q_login_id_key")
+
+		err := AuthflowV2SettingsIdentityAddEmailSchema.Validator().ValidateValue(handlerwebapp.FormToJSON(r.Form))
+		if err != nil {
+			return err
+		}
+
+		loginID := r.Form.Get("x_login_id")
+
+		s := session.GetSession(r.Context())
+		output, err := h.AccountManagement.StartCreateEmailIdentityWithVerification(s, &accountmanagement.StartCreateIdentityWithVerificationInput{
+			LoginID:    loginID,
+			LoginIDKey: loginIDKey,
+			Channel:    model.AuthenticatorOOBChannelEmail,
+		})
+		if err != nil {
+			return err
+		}
+
+		var redirectURI *url.URL
+		if output.NeedVerification {
+			redirectURI, err = url.Parse(AuthflowV2RouteSettingsIdentityVerifyEmail)
+
+			q := redirectURI.Query()
+			q.Set("q_login_id_key", loginIDKey)
+			q.Set("q_token", output.Token)
+
+			redirectURI.RawQuery = q.Encode()
+		} else {
+			redirectURI, err = url.Parse(AuthflowV2RouteSettingsIdentityListEmail)
+
+			q := redirectURI.Query()
+			q.Set("q_login_id_key", loginIDKey)
+
+			redirectURI.RawQuery = q.Encode()
+		}
+		if err != nil {
+			return err
+		}
+
+		result := webapp.Result{RedirectURI: redirectURI.String()}
 		result.WriteResponse(w, r)
 		return nil
 	})
