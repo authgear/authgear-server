3bc854839e5026f4737c75779df5d14358f616f1
Zachary Tsang <u.quixz@gmail.com>
2024-09-17 15:37:42 +0800

    Create viewmodel for view phone page

--- pkg/auth/handler/webapp/authflowv2/settings_identity_view_phone.go
+++ pkg/auth/handler/webapp/authflowv2/settings_identity_view_phone.go
@@ -6,8 +6,11 @@ import (
 	handlerwebapp "github.com/authgear/authgear-server/pkg/auth/handler/webapp"
 	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
 	"github.com/authgear/authgear-server/pkg/lib/authn/identity"
+	identityservice "github.com/authgear/authgear-server/pkg/lib/authn/identity/service"
+	"github.com/authgear/authgear-server/pkg/lib/config"
 	"github.com/authgear/authgear-server/pkg/lib/feature/verification"
 	"github.com/authgear/authgear-server/pkg/lib/infra/db/appdb"
+	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/util/httproute"
 	"github.com/authgear/authgear-server/pkg/util/template"
 )
@@ -25,7 +28,7 @@ func ConfigureAuthflowV2SettingsIdentityViewPhoneRoute(route httproute.Route) ht
 
 type AuthflowV2SettingsIdentityViewPhoneViewModel struct {
 	LoginIDKey     string
-	PhoneIdentity  identity.LoginID
+	PhoneIdentity  *identity.LoginID
 	Verifications  map[string][]verification.ClaimStatus
 	UpdateDisabled bool
 	DeleteDisabled bool
@@ -33,16 +36,50 @@ type AuthflowV2SettingsIdentityViewPhoneViewModel struct {
 
 type AuthflowV2SettingsIdentityViewPhoneHandler struct {
 	Database          *appdb.Handle
+	LoginIDConfig     *config.LoginIDConfig
+	Identities        *identityservice.Service
 	ControllerFactory handlerwebapp.ControllerFactory
 	BaseViewModel     *viewmodels.BaseViewModeler
+	Verification      handlerwebapp.SettingsVerificationService
 	Renderer          handlerwebapp.Renderer
 }
 
 func (h *AuthflowV2SettingsIdentityViewPhoneHandler) GetData(r *http.Request, rw http.ResponseWriter) (map[string]interface{}, error) {
 	data := map[string]interface{}{}
 
+	loginIDKey := r.Form.Get("q_login_id_key")
+	loginID := r.Form.Get("q_login_id")
+
 	baseViewModel := h.BaseViewModel.ViewModel(r, rw)
 	viewmodels.Embed(data, baseViewModel)
+
+	userID := session.GetUserID(r.Context())
+
+	phoneIdentity, err := h.Identities.LoginID.Get(*userID, loginID)
+	if err != nil {
+		return nil, err
+	}
+
+	verifications, err := h.Verification.GetVerificationStatuses([]*identity.Info{phoneIdentity.ToInfo()})
+	if err != nil {
+		return nil, err
+	}
+
+	updateDisabled := true
+	deleteDisabled := true
+	if loginIDConfig, ok := h.LoginIDConfig.GetKeyConfig(loginIDKey); ok {
+		updateDisabled = *loginIDConfig.UpdateDisabled
+		deleteDisabled = *loginIDConfig.DeleteDisabled
+	}
+
+	vm := AuthflowV2SettingsIdentityViewPhoneViewModel{
+		LoginIDKey:     loginIDKey,
+		PhoneIdentity:  phoneIdentity,
+		Verifications:  verifications,
+		UpdateDisabled: updateDisabled,
+		DeleteDisabled: deleteDisabled,
+	}
+	viewmodels.Embed(data, vm)
 
 	return data, nil
 }
