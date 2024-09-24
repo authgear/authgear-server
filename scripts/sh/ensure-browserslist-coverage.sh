c54cdb1701a74d565ccaa884664a6945b43424fb
pkong-ds <pkong.work@gmail.com>
2024-09-24 20:06:45 +0800

    Refine browserslist coverage script

--- scripts/sh/ensure-browserslist-coverage.sh
+++ scripts/sh/ensure-browserslist-coverage.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-# This script is used to test whether the ./.browerslistrc has enough coverage
 
-coverage=$(npx browserslist --json --coverage | python3 -c 'import json,sys;print(json.load(sys.stdin)["coverage"]["global"])')
-TARGET=90
-expr="$coverage > $TARGET"
-result=$(echo "$expr" | bc)
+# This script is used to test whether .browerslistrc has enough coverage
 
-echo 1>&2 "$expr: $result"
+echo 1>&2 "[INFO] Script executed from ${PWD}"
 
-if [ $result = 0 ]; then
-  exit 1
-fi
+main() {
+  TARGET=90
+  coverage=$(npx browserslist --json --coverage | python3 -c 'import json,sys;print(json.load(sys.stdin)["coverage"]["global"])')
+  expr="$coverage > $TARGET"
+  result=$(echo "$expr" | bc)
+  echo 1>&2 "[INFO] $expr is $result"
+  if [ $result = 0 ]; then
+
+    echo 1>&2 "[ERROR] Coverage $coverage is below $TARGET"
+    exit 1
+  fi
+}
+
+echo 1>&2 "[INFO] Start of script"
+main
+echo 1>&2 "[INFO] End of script"
