ed8acaf53b824699570e722769104f0c02335799
pkong-ds <pkong.work@gmail.com>
2024-09-26 16:46:02 +0800

    Implement set default brand logo height

--- pkg/lib/theme/logo.go
+++ pkg/lib/theme/logo.go
@@ -27,10 +27,53 @@ func MigrateSetDefaultLogoHeight(r io.Reader) (result []byte, err error) {
 		elements = append(elements, el)
 	}
 
-	elements = transform(elements)
+	elements = setDefaultHeight(elements)
 	var buf bytes.Buffer
 	stringify(&buf, elements)
 
 	result = buf.Bytes()
 	return
+}
+
+var LogoHeightPropertyKey string = "--brand-logo__height"
+var DefaultLogoHeight string = "40px"
+
+func setDefaultHeight(elements []element) (out []element) {
+	for _, el := range elements {
+		switch v := el.(type) {
+		case *ruleset:
+			if isLogoHeightSet(v) {
+				out = append(out, el)
+				continue
+			}
+
+			d := newLogoHeightDeclaration(DefaultLogoHeight)
+			newEl := &ruleset{
+				Selector:     v.Selector,
+				Declarations: append(v.Declarations, d),
+			}
+			out = append(out, newEl)
+		default:
+			out = append(out, el)
+		}
+	}
+
+	return
+}
+
+func isLogoHeightSet(rs *ruleset) bool {
+	for _, d := range rs.Declarations {
+		if d.Property == LogoHeightPropertyKey {
+			return true
+		}
+	}
+
+	return false
+}
+
+func newLogoHeightDeclaration(v string) *declaration {
+	return &declaration{
+		Property: LogoHeightPropertyKey,
+		Value:    v,
+	}
 }
