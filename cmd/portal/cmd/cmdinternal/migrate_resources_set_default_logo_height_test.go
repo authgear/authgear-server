567bb3287d33af5ad1ec41f3f715dbcf6f36e199
pkong-ds <pkong.work@gmail.com>
2024-10-03 15:34:49 +0800

    No light logo but has light theme should do nothing

--- cmd/portal/cmd/cmdinternal/migrate_resources_set_default_logo_height_test.go
+++ cmd/portal/cmd/cmdinternal/migrate_resources_set_default_logo_height_test.go
@@ -106,7 +106,24 @@ func TestMigrateSetDefaultLogoHeight(t *testing.T) {
 				)
 			})
 		})
-		Convey("!hasLightLogo && hasLightThemeCSS", func() {})
+
+		Convey("!hasLightLogo && hasLightThemeCSS", func() {
+			Convey("should do nothing because logo not set", func() {
+				originalCSS := toB64(`:root {
+  --layout__bg-color: #0047AB;
+}
+`)
+				test(
+					fmt.Sprintf(`{
+	"static_2f_authgear-authflowv_32_-light-theme.css": "%v"
+}`, originalCSS),
+					fmt.Sprintf(`{
+	"static_2f_authgear-authflowv_32_-light-theme.css": "%v"
+}`, originalCSS),
+					nil,
+				)
+			})
+		})
 
 		Convey("hasDarkLogo && !hasDarkThemeCSS", func() {})
 		Convey("hasDarkLogo && hasDarkThemeCSS && alreadySet", func() {})
