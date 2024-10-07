7b6077251acdef81428ee33c8ff4a5067c81d573
pkong-ds <pkong.work@gmail.com>
2024-10-07 17:19:01 +0800

    Add CreateCollection

---
+++ pkg/lib/opencvfr/api/collection.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/authgear/authgear-server/pkg/lib/opencvfr/openapi"
+)
+
+type CollectionHTTPClient interface {
+	Get(path string, query url.Values) (respBody []byte, err error)
+	Post(path string, body io.Reader, expectedStatus int) (respBody []byte, err error)
+	Patch(path string, body io.Reader) (respBody []byte, err error)
+	Delete(path string, targetID string) (err error)
+}
+
+type CollectionService struct {
+	HTTPClient CollectionHTTPClient
+}
+
+func (cs *CollectionService) Create(reqBody *openapi.CreateCollectionSchema) (c *openapi.CollectionSchema, err error) {
+	path := "/collection"
+
+	rbb, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := cs.HTTPClient.Post(path, bytes.NewBuffer(rbb), http.StatusCreated)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create collection - req: %v, err: %w", reqBody, err)
+	}
+
+	c = &openapi.CollectionSchema{}
+	err = c.UnmarshalJSON(body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse POST %v response body: %w", path, err)
+	}
+
+	return c, nil
+}
--- pkg/lib/opencvfr/api/deps.go
+++ pkg/lib/opencvfr/api/deps.go
@@ -6,6 +6,8 @@ import (
 
 var DependencySet = wire.NewSet(
 	wire.Struct(new(PersonService), "*"),
+	wire.Struct(new(CollectionService), "*"),
 	NewClient,
 	wire.Bind(new(PersonHTTPClient), new(*Client)),
+	wire.Bind(new(CollectionHTTPClient), new(*Client)),
 )
