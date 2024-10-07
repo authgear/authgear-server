a164a5bd67c1e884ae327e96d5ffdf8b461bb86b
pkong-ds <pkong.work@gmail.com>
2024-10-07 17:27:43 +0800

    Add GetCollection

--- pkg/lib/opencvfr/service.go
+++ pkg/lib/opencvfr/service.go
@@ -19,6 +19,7 @@ type PersonService interface {
 
 type CollectionService interface {
 	Create(reqBody *openapi.CreateCollectionSchema) (c *openapi.CollectionSchema, err error)
+	Get(id string) (c *openapi.CollectionSchema, err error)
 }
 
 type Service struct {
