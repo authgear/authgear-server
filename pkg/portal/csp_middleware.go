7dd85e20a72cbf7c5a6b0e1395f5c0f816b7f15a
Louis Chan <louischan@oursky.com>
2024-10-29 03:11:26 +0800

    Simplify the CSP of portal

--- pkg/portal/csp_middleware.go
+++ pkg/portal/csp_middleware.go
@@ -18,7 +18,6 @@ func PortalCSPMiddleware(next http.Handler) http.Handler {
 			httputil.CSPDirective{
 				Name: httputil.CSPDirectiveNameScriptSrc,
 				Value: httputil.CSPSources{
-					httputil.CSPSourceSelf,
 					// We used to include unsafe-eval here due to
 					// https://github.com/facebook/regenerator/issues/336
 					// and
@@ -26,66 +25,55 @@ func PortalCSPMiddleware(next http.Handler) http.Handler {
 					// But the two issues have been addressed since regenerator-runtime@0.13.8 (https://github.com/facebook/regenerator/commit/cc0cde9d90f975e5876df16c4b852c97f35da436)
 					// If you run `rg regenerator-runtime` in ./portal you will see we are on regenerator-runtime@0.13.9
 					// So we no longer need unsafe-eval anymore.
-					httputil.CSPHostSource{
-						Host: "cdn.jsdelivr.net",
-					},
-					httputil.CSPHostSource{
-						Host: "unpkg.com",
-					},
-					httputil.CSPHostSource{
-						Host: "www.googletagmanager.com",
-					},
-					httputil.CSPHostSource{
-						Host: "cdn.mxpnl.com",
-					},
-					httputil.CSPHostSource{
-						Host: "eu.posthog.com",
-					},
-					httputil.CSPHostSource{
-						Host: "eu-assets.i.posthog.com",
-					},
-					httputil.CSPHostSource{
-						Host: "cmp.osano.com",
-					},
-					httputil.CSPNonceSource{
-						Nonce: nonce,
-					},
-					httputil.CSPSourceStrictDynamic,
+					httputil.CSPSourceSelf,                // CSP1,CSP2
+					httputil.CSPSchemeSourceHTTPS,         // CSP1,CSP2
+					httputil.CSPNonceSource{Nonce: nonce}, // CSP2,CSP3
+					httputil.CSPSourceStrictDynamic,       // CSP3
+				},
+			},
+			// We did not specify style-src in the past.
+			// And not specify style-src means no restriction at all.
+			// So now we become more explicit, and specify it here.
+			// 'unsafe-inline': Allow fluent-ui to set the style attribute.
+			// 'unsafe-eval': Allow fluent-ui to call insertRule().
+			httputil.CSPDirective{
+				Name: httputil.CSPDirectiveNameStyleSrc,
+				Value: httputil.CSPSources{
+					httputil.CSPSourceSelf,         // CSP1,CSP2,CSP3
+					httputil.CSPSourceUnsafeInline, // CSP1,CSP2,CSP3
+					httputil.CSPSourceUnsafeEval,   // CSP1,CSP2,CSP3
+					httputil.CSPSchemeSourceHTTPS,  // CSP1,CSP2,CSP3
 				},
 			},
 			// monaco editor create worker with blob:
 			httputil.CSPDirective{
 				Name: httputil.CSPDirectiveNameWorkerSrc,
 				Value: httputil.CSPSources{
-					httputil.CSPSourceSelf,
-					httputil.CSPHostSource{
-						Host: "cdn.jsdelivr.net",
-					},
-					httputil.CSPSchemeSource{
-						Scheme: "blob",
-					},
+					httputil.CSPSourceSelf,                   // CSP1,CSP2,CSP3
+					httputil.CSPSchemeSourceHTTPS,            // CSP1,CSP2,CSP3
+					httputil.CSPSchemeSource{Scheme: "blob"}, // CSP1,CSP2,CSP3
 				},
 			},
 			httputil.CSPDirective{
 				Name: httputil.CSPDirectiveNameObjectSrc,
 				Value: httputil.CSPSources{
-					httputil.CSPSourceNone,
+					httputil.CSPSourceNone, // CSP1,CSP2,CSP3
 				},
 			},
 			httputil.CSPDirective{
 				Name: httputil.CSPDirectiveNameBaseURI,
 				Value: httputil.CSPSources{
-					httputil.CSPSourceNone,
+					httputil.CSPSourceNone, // CSP1,CSP2,CSP3
 				},
 			},
 			httputil.CSPDirective{
-				Name: httputil.CSPDirectiveNameBlockAllMixedContent,
+				Name: httputil.CSPDirectiveNameBlockAllMixedContent, // This is deprecated.
 			},
 			// This must be kept in sync with httputil.XFrameOptionsDeny
 			httputil.CSPDirective{
 				Name: httputil.CSPDirectiveNameFrameAncestors,
 				Value: httputil.CSPSources{
-					httputil.CSPSourceNone,
+					httputil.CSPSourceNone, // CSP2,CSP3
 				},
 			},
 		}
