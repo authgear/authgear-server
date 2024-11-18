693e7e4cc9ed4e2e590cd6664451d2fe3e0a46f6
Louis Chan <louischan@oursky.com>
2024-11-18 14:37:22 +0800

    Revert the old implementation of DEV-1468

--- pkg/lib/authenticationflow/declarative/node_do_force_change_password.go
+++ pkg/lib/authenticationflow/declarative/node_do_force_change_password.go
@@ -2,10 +2,7 @@ package declarative
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/authgear/authgear-server/pkg/api/event/nonblocking"
-	"github.com/authgear/authgear-server/pkg/api/model"
 	authflow "github.com/authgear/authgear-server/pkg/lib/authenticationflow"
 	"github.com/authgear/authgear-server/pkg/lib/authn/authenticator"
 )
@@ -31,42 +28,42 @@ func (n *NodeDoForceChangePassword) GetEffects(ctx context.Context, deps *authfl
 		authflow.RunEffect(func(ctx context.Context, deps *authflow.Dependencies) error {
 			return deps.Authenticators.Update(ctx, n.Authenticator)
 		}),
-		authflow.OnCommitEffect(func(ctx context.Context, deps *authflow.Dependencies) error {
-			reason := ""
-			if n.Reason != nil {
-				reason = string(*n.Reason)
-			}
-
-			switch n.Authenticator.Kind {
-			case authenticator.KindPrimary:
-				err := deps.Events.DispatchEventOnCommit(ctx, &nonblocking.PasswordPrimaryForceChangedEventPayload{
-					UserRef: model.UserRef{
-						Meta: model.Meta{
-							ID: n.Authenticator.UserID,
-						},
-					},
-					Reason: reason,
-				})
-				if err != nil {
-					return err
-				}
-				return nil
-			case authenticator.KindSecondary:
-				err := deps.Events.DispatchEventOnCommit(ctx, &nonblocking.PasswordSecondaryForceChangedEventPayload{
-					UserRef: model.UserRef{
-						Meta: model.Meta{
-							ID: n.Authenticator.UserID,
-						},
-					},
-					Reason: reason,
-				})
-				if err != nil {
-					return err
-				}
-				return nil
-			default:
-				panic(fmt.Errorf("unexpected authenticator kind: %v", n.Authenticator.Kind))
-			}
-		}),
+		// authflow.OnCommitEffect(func(ctx context.Context, deps *authflow.Dependencies) error {
+		// 	reason := ""
+		// 	if n.Reason != nil {
+		// 		reason = string(*n.Reason)
+		// 	}
+		//
+		// 	switch n.Authenticator.Kind {
+		// 	case authenticator.KindPrimary:
+		// 		err := deps.Events.DispatchEventOnCommit(ctx, &nonblocking.PasswordPrimaryForceChangedEventPayload{
+		// 			UserRef: model.UserRef{
+		// 				Meta: model.Meta{
+		// 					ID: n.Authenticator.UserID,
+		// 				},
+		// 			},
+		// 			Reason: reason,
+		// 		})
+		// 		if err != nil {
+		// 			return err
+		// 		}
+		// 		return nil
+		// 	case authenticator.KindSecondary:
+		// 		err := deps.Events.DispatchEventOnCommit(ctx, &nonblocking.PasswordSecondaryForceChangedEventPayload{
+		// 			UserRef: model.UserRef{
+		// 				Meta: model.Meta{
+		// 					ID: n.Authenticator.UserID,
+		// 				},
+		// 			},
+		// 			Reason: reason,
+		// 		})
+		// 		if err != nil {
+		// 			return err
+		// 		}
+		// 		return nil
+		// 	default:
+		// 		panic(fmt.Errorf("unexpected authenticator kind: %v", n.Authenticator.Kind))
+		// 	}
+		// }),
 	}, nil
 }
