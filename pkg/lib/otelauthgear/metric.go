ce356824c1365fffddf4eee6c5fda1eb8aef1691
Louis Chan <louischan@oursky.com>
2024-11-22 18:36:04 +0800

    Document how to add new metric

--- pkg/lib/otelauthgear/metric.go
+++ pkg/lib/otelauthgear/metric.go
@@ -10,6 +10,55 @@ import (
 	"github.com/authgear/authgear-server/pkg/util/otelutil"
 )
 
+// Suppose you have a task to add a new metric, what should you do?
+//
+// The first step is to learn what metric instruments you can use.
+// Read this.
+// https://opentelemetry.io/docs/concepts/signals/metrics/#metric-instruments
+//
+// Now that you know what metric instruments are available.
+// You need to understand the metric you need to track, and decide the instrument.
+// For example, suppose you are tasked to track the number of signups.
+// Since the number of signups only go up, you should use a Counter.
+// It is trivial to locate the location where a signup occurs in the codebase,
+// you should use the non-async version of a Counter.
+// The number of signups is an integer, so you should use a integer version of a Counter.
+// Therefore, you should use a Int64Counter.
+//
+// The next step is to define the instrument.
+// If you have read https://opentelemetry.io/docs/concepts/signals/metrics/#metric-instruments
+// then you already know an instrument have 4 properties, namely, name, kind, unit, and description.
+// We have figured out the kind of the instrument, so we have the remaining 3 to deal with.
+// Read https://opentelemetry.io/docs/specs/semconv/general/metrics/#general-guidelines
+// to understand the general naming guidelines.
+// In short, do this
+// - Use lowercase characters.
+// - Use dot (.) as separator.
+// - Use underscore (_) to separate words.
+// - Use "count" instead pluralization, https://opentelemetry.io/docs/specs/semconv/general/metrics/#use-count-instead-of-pluralization-for-updowncounters
+// The instrument name should be "authgear.signup.count"
+// For the description, use a short sentence that can describe the instrument.
+// For the unit, read this https://opentelemetry.io/docs/specs/semconv/general/metrics/#instrument-units
+// The unit in this case should be "{signup}".
+//
+// Then you need to decide whether the instrument has any meaningful attributes.
+// For signup, there is no.
+// You may be tempted to use attribute "status" with value "ok" and "error"
+// But that does not make much sense.
+// This is because a signup can fail for many reasons along the journey.
+// You cannot really pinpoint a location where you can say it is status=error.
+// What metric can have "status" then?
+// A good example is the metric for tracking the number of email delivery.
+// We know where the email delivery happens.
+// It happens when we call the SMTP server.
+// So that metric can have "status".
+//
+// In the last step, you locate the locations where you need to insert the instrumentation code.
+// You should use the helper functions defined in this package, like IntCounterAddOne.
+// These helper functions ensure the metric has necessary attributes attached with them.
+//
+// See https://github.com/authgear/authgear-server/pull/4906 for an example.
+
 // meter is the global meter for metrics produced by Authgear.
 // You use meter to define metrics in this package.
 var meter = otel.Meter("github.com/authgear/authgear-server/pkg/lib/otelauthgear")
