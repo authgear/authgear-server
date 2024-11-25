c6b1437bf7044b39e300d2ef7c14d8ce4fbf5d42
Tung Wu <tung@oursky.com>
2024-11-25 15:24:57 +0800

    Import cmdsearch in authgearx

--- custombuild/cmd/authgearx/main.go
+++ custombuild/cmd/authgearx/main.go
@@ -19,6 +19,7 @@ import (
 	_ "github.com/authgear/authgear-server/cmd/authgear/cmd/cmdimport"
 	_ "github.com/authgear/authgear-server/cmd/authgear/cmd/cmdinit"
 	_ "github.com/authgear/authgear-server/cmd/authgear/cmd/cmdinternal"
+	_ "github.com/authgear/authgear-server/cmd/authgear/cmd/cmdsearch"
 	_ "github.com/authgear/authgear-server/cmd/authgear/cmd/cmdstart"
 	_ "github.com/authgear/authgear-server/pkg/latte"
 	_ "github.com/authgear/authgear-server/pkg/lib/authenticationflow/declarative"
