8a479f773cdba665a3c84f73b790e52f2342aabc
Louis Chan <louischan@oursky.com>
2024-12-02 13:18:21 +0800

    Use import.meta.glob to handle CLDR data

--- authui/vite.config.ts
+++ authui/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 import { viteAuthgearAuthUI } from "vite-plugin-authgear-authui";
 
 export default defineConfig(() => ({
   plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "node_modules/cldr-localenames-full/main/*",
-          dest: "cldr-localenames-full",
-        },
-      ],
-    }),
     viteAuthgearAuthUI({
       input: {
         v1: "./src/build.html",
