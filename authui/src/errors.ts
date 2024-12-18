d1deb6cd3c5cbd43addabe6a9af69a5d6cf5ca0d
Louis Chan <louischan@oursky.com>
2024-12-18 15:49:16 +0800

    Document the modifications

--- authui/src/errors.ts
+++ authui/src/errors.ts
@@ -1,4 +1,10 @@
-// Modified from https://github.com/sindresorhus/is-network-error/blob/main/index.js
+// The original work is https://github.com/sindresorhus/is-network-error/blob/main/index.js
+// The modifications are
+// 1. Remove the error message checking for Node.js, since this code does not run on Node.js.
+// 2. The original work also check error.stack === undefined on Safari >= 17, but in our observation,
+//    error.stack IS NOT undefined, so we removed that checking.
+// 3. The original work call Object.prototype.toString on error, and check if the string is "[object Error]",
+//    we just use instanceof to check if it is an error.
 const errorMessages = new Set([
   "network error", // Chrome
   "Failed to fetch", // Chrome
