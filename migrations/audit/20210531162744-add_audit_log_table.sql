9a0336b20176037c99fbe74e4e126bb2f14c8abd
Louis Chan <louischan@oursky.com>
2025-01-02 17:20:59 +0800

    Explicitly support pg_partman 4.x and 5.x

--- migrations/audit/20210531162744-add_audit_log_table.sql
+++ migrations/audit/20210531162744-add_audit_log_table.sql
@@ -18,17 +18,40 @@ CREATE INDEX _audit_log_idx_created_at_brin ON _audit_log USING BRIN (created_at
 CREATE TABLE _audit_log_template (LIKE _audit_log);
 ALTER TABLE _audit_log_template ADD PRIMARY KEY (id);
 
-SELECT create_parent(
-  '{{ .SCHEMA }}._audit_log',
-  'created_at',
-  'native',
-  'monthly',
-  p_template_table := '{{ .SCHEMA }}._audit_log_template'
-);
+-- In case you wonder why we have to wrap this in StatementBegin and StatementEnd,
+-- please read https://github.com/rubenv/sql-migrate/issues/261#issuecomment-1918115180
+-- +migrate StatementBegin
+DO LANGUAGE 'plpgsql' $$DECLARE
+  pg_partman_version text;
+BEGIN
+  SELECT extversion INTO pg_partman_version FROM pg_extension WHERE extname = 'pg_partman';
+  IF pg_partman_version like '5.%' THEN
+    PERFORM create_parent(
+      p_parent_table := '{{ .SCHEMA }}._audit_log',
+      p_control := 'created_at',
+      p_interval := '1 month',
+      p_template_table := '{{ .SCHEMA }}._audit_log_template'
+    );
+    UPDATE part_config
+    SET retention = '90 days', retention_keep_table = FALSE
+    WHERE parent_table = '{{ .SCHEMA }}._audit_log';
+  ELSIF pg_partman_version like '4.%' THEN
+    PERFORM create_parent(
+      '{{ .SCHEMA }}._audit_log',
+      'created_at',
+      'native',
+      'monthly',
+      p_template_table := '{{ .SCHEMA }}._audit_log_template'
+    );
 
-UPDATE part_config
-SET retention = '90 days', retention_keep_table = FALSE
-WHERE parent_table = '{{ .SCHEMA }}._audit_log';
+    UPDATE part_config
+    SET retention = '90 days', retention_keep_table = FALSE
+    WHERE parent_table = '{{ .SCHEMA }}._audit_log';
+  ELSE
+    RAISE EXCEPTION 'unsupported pg_partman version %', pg_partman_version;
+  END IF;
+END$$;
+-- +migrate StatementEnd
 
 -- +migrate Down
 SELECT undo_partition(
