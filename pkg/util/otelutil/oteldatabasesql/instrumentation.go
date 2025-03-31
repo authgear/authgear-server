1a1a8be2b0f996dbb0bac396d8d0265afdc2b083
Louis Chan <louischan@oursky.com>
2025-03-31 20:25:38 +0800

    Track SQL database operation duration

--- pkg/util/otelutil/oteldatabasesql/instrumentation.go
+++ pkg/util/otelutil/oteldatabasesql/instrumentation.go
@@ -103,6 +103,24 @@ var DBClientConnectionUseTimeHistogram = mustFloat64Histogram(
 	),
 )
 
+// DBClientOperationDurationHistogram is https://opentelemetry.io/docs/specs/semconv/database/database-metrics/#metric-dbclientoperationduration
+var DBClientOperationDurationHistogram = mustFloat64Histogram(
+	semconv.DBClientOperationDurationName,
+	metric.WithDescription(semconv.DBClientOperationDurationDescription),
+	metric.WithUnit(semconv.DBClientOperationDurationUnit),
+	metric.WithExplicitBucketBoundaries(
+		0.001,
+		0.005,
+		0.01,
+		0.05,
+		0.1,
+		0.5,
+		1,
+		5,
+		10,
+	),
+)
+
 type OpenOptions struct {
 	DriverName string
 	DSN        string
@@ -110,6 +128,7 @@ type OpenOptions struct {
 	IdleMax    int
 }
 
+//nolint:gocognit
 func Open(opts OpenOptions) (*sql.DB, error) {
 	var commonAttrs []attribute.KeyValue
 	var poolAttrs []attribute.KeyValue
@@ -125,8 +144,8 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 	var wrapDriver func(d driver.Driver) driver.Driver
 	var wrapConnector func(c driver.Connector) driver.Connector
 	var wrapConn func(ctx context.Context, c driver.Conn) driver.Conn
-	var wrapStmt func(s driver.Stmt) driver.Stmt
-	var wrapTx func(t driver.Tx) driver.Tx
+	var wrapStmt func(ctx context.Context, s driver.Stmt, query string) driver.Stmt
+	var wrapTx func(ctx context.Context, t driver.Tx) driver.Tx
 	var wrapRows func(r driver.Rows) driver.Rows
 
 	wrapDriver = func(d driver.Driver) driver.Driver {
@@ -213,42 +232,107 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 			},
 			Begin: func(original databasesqlwrapper.Conn_Begin) databasesqlwrapper.Conn_Begin {
 				return func() (driver.Tx, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("BEGIN")),
+							metric.WithAttributes(semconv.DBQueryText("BEGIN")),
+						)
+					}()
 					tx, err := original()
 					if err != nil {
 						return nil, err
 					}
-					return wrapTx(tx), nil
+					return wrapTx(ctx, tx), nil
 				}
 			},
 			BeginTx: func(original databasesqlwrapper.Conn_BeginTx) databasesqlwrapper.Conn_BeginTx {
 				return func(ctx context.Context, opts driver.TxOptions) (driver.Tx, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("BEGIN")),
+							metric.WithAttributes(semconv.DBQueryText("BEGIN")),
+						)
+					}()
 					tx, err := original(ctx, opts)
 					if err != nil {
 						return nil, err
 					}
-					return wrapTx(tx), nil
+					return wrapTx(ctx, tx), nil
 				}
 			},
 			Prepare: func(original databasesqlwrapper.Conn_Prepare) databasesqlwrapper.Conn_Prepare {
 				return func(query string) (driver.Stmt, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("PREPARE")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					stmt, err := original(query)
 					if err != nil {
 						return nil, err
 					}
-					return wrapStmt(stmt), nil
+					return wrapStmt(ctx, stmt, query), nil
 				}
 			},
 			PrepareContext: func(original databasesqlwrapper.Conn_PrepareContext) databasesqlwrapper.Conn_PrepareContext {
 				return func(ctx context.Context, query string) (driver.Stmt, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("PREPARE")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					stmt, err := original(ctx, query)
 					if err != nil {
 						return nil, err
 					}
-					return wrapStmt(stmt), nil
+					return wrapStmt(ctx, stmt, query), nil
 				}
 			},
 			Exec: func(original databasesqlwrapper.Conn_Exec) databasesqlwrapper.Conn_Exec {
 				return func(query string, args []driver.Value) (driver.Result, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("Exec")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					result, err := original(query, args)
 					if err != nil {
 						return nil, err
@@ -258,6 +342,19 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 			},
 			ExecContext: func(original databasesqlwrapper.Conn_ExecContext) databasesqlwrapper.Conn_ExecContext {
 				return func(ctx context.Context, query string, args []driver.NamedValue) (driver.Result, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("Exec")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					result, err := original(ctx, query, args)
 					if err != nil {
 						return nil, err
@@ -267,6 +364,19 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 			},
 			Query: func(original databasesqlwrapper.Conn_Query) databasesqlwrapper.Conn_Query {
 				return func(query string, args []driver.Value) (driver.Rows, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("Query")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					rows, err := original(query, args)
 					if err != nil {
 						return nil, err
@@ -276,6 +386,19 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 			},
 			QueryContext: func(original databasesqlwrapper.Conn_QueryContext) databasesqlwrapper.Conn_QueryContext {
 				return func(ctx context.Context, query string, args []driver.NamedValue) (driver.Rows, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("Query")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					rows, err := original(ctx, query, args)
 					if err != nil {
 						return nil, err
@@ -286,10 +409,23 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 		})
 	}
 
-	wrapStmt = func(s driver.Stmt) driver.Stmt {
+	wrapStmt = func(ctx context.Context, s driver.Stmt, query string) driver.Stmt {
 		return databasesqlwrapper.WrapStmt(s, databasesqlwrapper.StmtInterceptor{
 			Exec: func(original databasesqlwrapper.Stmt_Exec) databasesqlwrapper.Stmt_Exec {
 				return func(args []driver.Value) (driver.Result, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("Exec")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					result, err := original(args)
 					if err != nil {
 						return nil, err
@@ -299,6 +435,19 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 			},
 			ExecContext: func(original databasesqlwrapper.Stmt_ExecContext) databasesqlwrapper.Stmt_ExecContext {
 				return func(ctx context.Context, args []driver.NamedValue) (driver.Result, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("Exec")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					result, err := original(ctx, args)
 					if err != nil {
 						return nil, err
@@ -308,6 +457,19 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 			},
 			Query: func(original databasesqlwrapper.Stmt_Query) databasesqlwrapper.Stmt_Query {
 				return func(args []driver.Value) (driver.Rows, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("Query")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					rows, err := original(args)
 					if err != nil {
 						return nil, err
@@ -317,6 +479,19 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 			},
 			QueryContext: func(original databasesqlwrapper.Stmt_QueryContext) databasesqlwrapper.Stmt_QueryContext {
 				return func(ctx context.Context, args []driver.NamedValue) (driver.Rows, error) {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("Query")),
+							metric.WithAttributes(semconv.DBQueryText(query)),
+						)
+					}()
 					rows, err := original(ctx, args)
 					if err != nil {
 						return nil, err
@@ -327,15 +502,41 @@ func Open(opts OpenOptions) (*sql.DB, error) {
 		})
 	}
 
-	wrapTx = func(t driver.Tx) driver.Tx {
+	wrapTx = func(ctx context.Context, t driver.Tx) driver.Tx {
 		return databasesqlwrapper.WrapTx(t, databasesqlwrapper.TxInterceptor{
 			Commit: func(original databasesqlwrapper.Tx_Commit) databasesqlwrapper.Tx_Commit {
 				return func() error {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("COMMIT")),
+							metric.WithAttributes(semconv.DBQueryText("COMMIT")),
+						)
+					}()
 					return original()
 				}
 			},
 			Rollback: func(original databasesqlwrapper.Tx_Rollback) databasesqlwrapper.Tx_Rollback {
 				return func() error {
+					// Intentionally not calling .UTC() to use monotonic clock.
+					startTime := time.Now()
+					defer func() {
+						elapsed := time.Since(startTime)
+						seconds := elapsed.Seconds()
+						DBClientOperationDurationHistogram.Record(
+							ctx,
+							seconds,
+							metric.WithAttributes(commonAttrs...),
+							metric.WithAttributes(semconv.DBOperationName("ROLLBACK")),
+							metric.WithAttributes(semconv.DBQueryText("ROLLBACK")),
+						)
+					}()
 					return original()
 				}
 			},
