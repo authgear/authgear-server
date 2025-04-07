48ea2070ff503e7d167c0ab21f6397fc89a94976
Tung Wu <tung@oursky.com>
2025-04-07 15:51:26 +0800

    Implement Stepper

---
+++ portal/src/components/v2/Stepper/StepIcon.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { StepIcon } from "./Stepper";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  component: StepIcon,
+  tags: ["autodocs"],
+  argTypes: {
+    text: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+  args: {
+    text: "1",
+  },
+} satisfies Meta<typeof StepIcon>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Dark: Story = {
+  args: {
+    darkMode: true,
+  },
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
+};
---
+++ portal/src/components/v2/Stepper/Stepper.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import cn from "classnames";
+import { CheckIcon } from "@radix-ui/react-icons";
+import styles from "./Stepper.module.css";
+
+export interface Step {
+  text: React.ReactNode;
+  checked: boolean;
+}
+
+export interface StepperProps {
+  darkMode?: boolean;
+  steps: Step[];
+}
+
+export function Stepper({ darkMode, steps }: StepperProps): React.ReactElement {
+  return (
+    <div className={cn(styles.stepper, darkMode ? "dark" : null)}>
+      {steps.map((step, idx) => {
+        const active = idx === 0 ? true : steps[idx - 1].checked;
+        if (idx === steps.length - 1) {
+          return (
+            <StepIcon
+              key={idx}
+              checked={step.checked}
+              active={active}
+              text={step.text}
+            />
+          );
+        }
+        return (
+          <React.Fragment key={idx}>
+            <StepIcon checked={step.checked} active={active} text={step.text} />
+            <StepTrail progress={step.checked ? 1 : active ? 0.5 : 0} />
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+}
+
+export interface StepIconProps {
+  darkMode?: boolean;
+  checked?: boolean;
+  active?: boolean;
+  text?: React.ReactNode;
+}
+
+export function StepIcon({
+  darkMode,
+  checked,
+  active,
+  text,
+}: StepIconProps): React.ReactElement {
+  return (
+    <div
+      className={cn(
+        styles.stepIcon,
+        active ? styles["stepIcon--active"] : null,
+        darkMode ? "dark" : null
+      )}
+    >
+      {checked ? <CheckIcon width="21px" height="21px" /> : text}
+    </div>
+  );
+}
+
+export interface StepTrailProps {
+  darkMode?: boolean;
+  progress: 0 | 0.5 | 1;
+}
+
+export function StepTrail({
+  darkMode,
+  progress,
+}: StepTrailProps): React.ReactElement {
+  return (
+    <div className={cn(styles.stepTrail, darkMode ? "dark" : null)}>
+      <div
+        className={styles.stepTrailFilling}
+        style={{ right: `${Math.round((1 - progress) * 100)}%` }}
+      />
+    </div>
+  );
+}
