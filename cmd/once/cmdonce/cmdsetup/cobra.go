258918f5dfcd6e207ab347c8179b7c6f809fa38f
Louis Chan <louischan@oursky.com>
2025-04-10 19:44:41 +0800

    Check if volume exists

--- cmd/once/cmdonce/cmdsetup/cobra.go
+++ cmd/once/cmdonce/cmdsetup/cobra.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"slices"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
@@ -24,6 +25,21 @@ var CmdSetup = &cobra.Command{
 		_, err := exec.LookPath(internal.BinDocker)
 		if err != nil {
 			return fmt.Errorf("%v is not installed on your machine. Please install it first. See https://docs.docker.com/get-started/get-docker/", internal.BinDocker)
+		}
+
+		volumes, err := internal.DockerVolumeLs(cmd.Context())
+		if err != nil {
+			return err
+		}
+
+		if slices.ContainsFunc(volumes, func(v internal.DockerVolume) bool {
+			return v.Name == internal.NameDockerVolume && v.Scope == internal.DockerVolumeScopeLocal
+		}) {
+			return fmt.Errorf(
+				"The docker volume %v exists already.\nHave you set up this before?\nIf yes, then you should the following to start Authgear\n\n  %v start\n",
+				internal.NameDockerVolume,
+				internal.ProgramName,
+			)
 		}
 
 		return nil
