cc342dee17e9c3f9820aa8f4f60317143a847f3d
Tung Wu <tung@oursky.com>
2025-04-25 18:51:03 +0800

    Add clear button to image input

---
+++ portal/src/components/v2/IconButton/IconButton.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { IconButton, IconButtonIcon } from "./IconButton";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  component: IconButton,
+  tags: ["autodocs"],
+  argTypes: {},
+  args: {
+    size: "2",
+  },
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: "default",
+    icon: IconButtonIcon.MagnifyingGlass,
+  },
+};
+
+export const Destroy: Story = {
+  args: {
+    variant: "destroy",
+    icon: IconButtonIcon.Trash,
+  },
+};
