98841934b3f95567db9b7dc43a6f2123941b94cd
Tung Wu <tung@oursky.com>
2025-04-28 14:54:08 +0800

    Add WhiteButton

---
+++ portal/src/components/v2/WhiteButton/WhiteButton.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { WhiteButton } from "./WhiteButton";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  component: WhiteButton,
+  tags: ["autodocs"],
+  argTypes: {
+    text: {
+      control: {
+        type: "text",
+      },
+    },
+  },
+  args: {
+    text: "Start",
+  },
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
+} satisfies Meta<typeof WhiteButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    size: "2",
+    disabled: false,
+  },
+};
