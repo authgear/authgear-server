98f67b5f12f5b2213dfdc707bfa371fb9653cb31
Louis Chan <louischan@oursky.com>
2025-05-07 12:44:11 +0800

    Implement LicenseScreen

---
+++ portal/src/graphql/portal/LicenseScreen.tsx
@@ -0,0 +1,74 @@
+import React, { useContext } from "react";
+import ScreenContent from "../../ScreenContent";
+import ScreenTitle from "../../ScreenTitle";
+import ScreenDescription from "../../ScreenDescription";
+import { useSystemConfig } from "../../context/SystemConfigContext";
+import { Label, Text, useTheme } from "@fluentui/react";
+import { FormattedMessage, Context } from "@oursky/react-messageformat";
+import TextField from "../../TextField";
+import { formatDateOnly } from "../../util/formatDateOnly";
+import styles from "./LicenseScreen.module.css";
+
+function LicenseScreen(): React.ReactElement {
+  const theme = useTheme();
+  const { locale, renderToString } = useContext(Context);
+  const {
+    authgearOnceLicenseKey,
+    authgearOnceLicenseeEmail,
+    authgearOnceLicenseExpireAt,
+  } = useSystemConfig();
+  const expireAt: Date | null =
+    authgearOnceLicenseExpireAt !== ""
+      ? new Date(authgearOnceLicenseExpireAt)
+      : null;
+
+  return (
+    <ScreenContent>
+      <ScreenTitle className={styles.widget}>
+        <FormattedMessage id="LicenseScreen.title" />
+      </ScreenTitle>
+      <ScreenDescription className={styles.widget}>
+        <FormattedMessage id="LicenseScreen.description" />
+      </ScreenDescription>
+      <TextField
+        className={styles.widget}
+        label={renderToString("LicenseScreen.license-key")}
+        readOnly={true}
+        value={authgearOnceLicenseKey}
+        styles={{
+          field: {
+            backgroundColor: theme.semanticColors.disabledBackground,
+          },
+        }}
+      />
+      <TextField
+        className={styles.widget}
+        label={renderToString("LicenseScreen.email")}
+        readOnly={true}
+        value={authgearOnceLicenseeEmail}
+        styles={{
+          field: {
+            backgroundColor: theme.semanticColors.disabledBackground,
+          },
+        }}
+      />
+      <div className={styles.widget}>
+        <Label>
+          <FormattedMessage id="LicenseScreen.lifetime-usage" />
+        </Label>
+        <Text as="p">
+          {expireAt != null ? (
+            <FormattedMessage
+              id="LicenseScreen.license-expiry"
+              values={{
+                expireAt: formatDateOnly(locale, expireAt) ?? "",
+              }}
+            />
+          ) : null}
+        </Text>
+      </div>
+    </ScreenContent>
+  );
+}
+
+export default LicenseScreen;
