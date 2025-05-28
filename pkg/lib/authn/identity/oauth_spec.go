b049013dbe3ccbaac0cb3aee5609c73c80c9498a
Louis Chan <louischan@oursky.com>
2025-05-28 16:45:29 +0800

    Add IncludeIdentityAttributesInIDToken to OAuthSpec

--- pkg/lib/authn/identity/oauth_spec.go
+++ pkg/lib/authn/identity/oauth_spec.go
@@ -7,9 +7,13 @@ import (
 )
 
 type OAuthSpec struct {
-	// DoNotStoreIdentityAttributes is not data, but a piece of information to be
+	// DoNotStoreIdentityAttributes is not part of the data of identity.Info, but a piece of information to be
 	// used by the identity service when creating a new identity.Info.
 	DoNotStoreIdentityAttributes bool `json:"do_not_store_identity_attributes"`
+
+	// IncludeIdentityAttributesInIDToken is read by authflow service.
+	IncludeIdentityAttributesInIDToken bool `json:"include_identity_attributes_in_id_token"`
+
 	// ProviderAlias is transient data, its sole purpose is to aid in creating
 	// the info object.
 	ProviderAlias string `json:"provider_alias"`
@@ -22,11 +26,12 @@ type OAuthSpec struct {
 
 func NewIncomingOAuthSpec(providerConfig oauthrelyingparty.ProviderConfig, userProfile oauthrelyingparty.UserProfile) *OAuthSpec {
 	return &OAuthSpec{
-		DoNotStoreIdentityAttributes: config.OAuthSSOProviderConfig(providerConfig).DoNotStoreIdentityAttributes(),
-		ProviderAlias:                config.OAuthSSOProviderConfig(providerConfig).Alias(),
-		ProviderID:                   providerConfig.ProviderID(),
-		SubjectID:                    userProfile.ProviderUserID,
-		RawProfile:                   userProfile.ProviderRawProfile,
-		StandardClaims:               userProfile.StandardAttributes,
+		DoNotStoreIdentityAttributes:       config.OAuthSSOProviderConfig(providerConfig).DoNotStoreIdentityAttributes(),
+		IncludeIdentityAttributesInIDToken: config.OAuthSSOProviderConfig(providerConfig).IncludeIdentityAttributesInIDToken(),
+		ProviderAlias:                      config.OAuthSSOProviderConfig(providerConfig).Alias(),
+		ProviderID:                         providerConfig.ProviderID(),
+		SubjectID:                          userProfile.ProviderUserID,
+		RawProfile:                         userProfile.ProviderRawProfile,
+		StandardClaims:                     userProfile.StandardAttributes,
 	}
 }
