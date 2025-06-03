f2f4063a87437c978c9902f839744709107b6b52
Tung Wu <tung@oursky.com>
2025-06-03 15:43:45 +0800

    Gather asserted identities

--- pkg/lib/authenticationflow/declarative/utils_event.go
+++ pkg/lib/authenticationflow/declarative/utils_event.go
@@ -8,16 +8,21 @@ import (
 	"github.com/authgear/authgear-server/pkg/api/model"
 	"github.com/authgear/authgear-server/pkg/lib/authenticationflow"
 	"github.com/authgear/authgear-server/pkg/lib/authn/authenticator"
+	"github.com/authgear/authgear-server/pkg/lib/authn/identity"
 	"github.com/authgear/authgear-server/pkg/lib/authn/user"
 	"github.com/authgear/authgear-server/pkg/util/accesscontrol"
 )
 
 func GetAuthenticationContext(ctx context.Context, flows authenticationflow.Flows, deps *authenticationflow.Dependencies) (*event.AuthenticationContext, error) {
-	authCtx := &event.AuthenticationContext{}
+	var authenticationFlow *event.AuthenticationFlowContext
+	var u *model.User
+	var amr []string
+	var assertedAuthenticators []model.Authenticator
+	var assertedIdentities []model.Identity
 
 	flowRef := authenticationflow.FindCurrentFlowReference(flows.Root)
 	if flowRef != nil {
-		authCtx.AuthenticationFlow = &event.AuthenticationFlowContext{
+		authenticationFlow = &event.AuthenticationFlowContext{
 			Type: string(flowRef.Type),
 			Name: flowRef.Name,
 		}
@@ -25,31 +30,44 @@ func GetAuthenticationContext(ctx context.Context, flows authenticationflow.Flow
 
 	userID, err := getUserID(flows)
 	if err == nil {
-		u, err := deps.Users.Get(ctx, userID, accesscontrol.RoleGreatest)
+		u, err = deps.Users.Get(ctx, userID, accesscontrol.RoleGreatest)
 		if err != nil && !errors.Is(err, user.ErrUserNotFound) {
 			return nil, err
 		}
-		authCtx.User = u
 	}
 
-	amr, err := collectAMR(ctx, deps, flows)
+	amr, err = collectAMR(ctx, deps, flows)
 	if err != nil {
 		return nil, err
 	}
-	authCtx.AMR = amr
 
 	auths, err := collectAuthenticators(ctx, deps, flows)
 	if err != nil {
 		return nil, err
 	}
 
-	assertedAuthenticators := make([]model.Authenticator, len(auths))
+	assertedAuthenticators = make([]model.Authenticator, len(auths))
 	for i, info := range auths {
 		assertedAuthenticators[i] = info.ToModel()
 	}
-	authCtx.AssertedAuthenticators = assertedAuthenticators
 
-	return authCtx, nil
+	ids, err := collectIdentities(ctx, deps, flows)
+	if err != nil {
+		return nil, err
+	}
+
+	assertedIdentities = make([]model.Identity, len(ids))
+	for i, info := range ids {
+		assertedIdentities[i] = info.ToModel()
+	}
+
+	return &event.AuthenticationContext{
+		AuthenticationFlow:     authenticationFlow,
+		User:                   u,
+		AMR:                    amr,
+		AssertedAuthenticators: assertedAuthenticators,
+		AssertedIdentities:     assertedIdentities,
+	}, nil
 }
 
 func collectAuthenticators(ctx context.Context, deps *authenticationflow.Dependencies, flows authenticationflow.Flows) (authenticators []*authenticator.Info, err error) {
@@ -87,6 +105,40 @@ func collectAuthenticators(ctx context.Context, deps *authenticationflow.Depende
 		dedupedAuthenticators = append(dedupedAuthenticators, info)
 	}
 	authenticators = dedupedAuthenticators
+
+	return
+}
+
+func collectIdentities(ctx context.Context, deps *authenticationflow.Dependencies, flows authenticationflow.Flows) (identities []*identity.Info, err error) {
+	err = authenticationflow.TraverseFlow(authenticationflow.Traverser{
+		NodeSimple: func(nodeSimple authenticationflow.NodeSimple, w *authenticationflow.Flow) error {
+			if n, ok := nodeSimple.(MilestoneDoUseIdentity); ok {
+				identities = append(identities, n.MilestoneDoUseIdentity())
+			}
+			return nil
+		},
+		Intent: func(intent authenticationflow.Intent, w *authenticationflow.Flow) error {
+			if i, ok := intent.(MilestoneDoUseIdentity); ok {
+				identities = append(identities, i.MilestoneDoUseIdentity())
+			}
+			return nil
+		},
+	}, flows.Root)
+	if err != nil {
+		return
+	}
+
+	// Deduplicate by Info.ID
+	seen := map[string]struct{}{}
+	var dedupedIdentities []*identity.Info
+	for _, info := range identities {
+		if _, ok := seen[info.ID]; ok {
+			continue
+		}
+		seen[info.ID] = struct{}{}
+		dedupedIdentities = append(dedupedIdentities, info)
+	}
+	identities = dedupedIdentities
 
 	return
 }
