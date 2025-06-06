16e6c20ff52eb0aa92bae91ecd6991c84736fe13
Tung Wu <tung@oursky.com>
2025-06-06 14:52:26 +0800

    Add authentication.pre_authenticated to login flow

---
+++ pkg/api/event/blocking/authentication_pre_authenticated.go
@@ -0,0 +1,48 @@
+package blocking
+
+import (
+	"context"
+
+	"github.com/authgear/authgear-server/pkg/api/event"
+)
+
+const (
+	AuthenticationPreAuthenticated event.Type = "authentication.pre_authenticated"
+)
+
+type AuthenticationPreAuthenticatedBlockingEventPayload struct {
+	Authentication event.AuthenticationContext `json:"authentication"`
+
+	Constraints *event.Constraints `json:"-"`
+}
+
+func (e *AuthenticationPreAuthenticatedBlockingEventPayload) BlockingEventType() event.Type {
+	return AuthenticationPreAuthenticated
+}
+
+func (e *AuthenticationPreAuthenticatedBlockingEventPayload) UserID() string {
+	if e.Authentication.User != nil {
+		return e.Authentication.User.ID
+	}
+	return ""
+}
+
+func (e *AuthenticationPreAuthenticatedBlockingEventPayload) GetTriggeredBy() event.TriggeredByType {
+	return event.TriggeredByTypeUser
+}
+
+func (e *AuthenticationPreAuthenticatedBlockingEventPayload) FillContext(ctx *event.Context) {}
+
+func (e *AuthenticationPreAuthenticatedBlockingEventPayload) ApplyHookResponse(ctx context.Context, response event.HookResponse) event.ApplyHookResponseResult {
+	if response.Constraints != nil {
+		e.Constraints = response.Constraints
+	}
+	return event.ApplyHookResponseResult{MutationsEverApplied: false}
+}
+
+func (e *AuthenticationPreAuthenticatedBlockingEventPayload) PerformEffects(ctx context.Context, effectCtx event.MutationsEffectContext) error {
+	// no-op
+	return nil
+}
+
+var _ event.BlockingPayload = &AuthenticationPreAuthenticatedBlockingEventPayload{}
