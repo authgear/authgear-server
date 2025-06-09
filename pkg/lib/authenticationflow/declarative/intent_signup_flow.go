db07c4f298b7116e8190d659298bb1f8d2bb3b71
Tung Wu <tung@oursky.com>
2025-06-09 11:58:15 +0800

    Add IntentSignupFlowEnsureContraintsFulfilled

--- pkg/lib/authenticationflow/declarative/intent_signup_flow.go
+++ pkg/lib/authenticationflow/declarative/intent_signup_flow.go
@@ -89,6 +89,21 @@ func (i *IntentSignupFlow) ReactTo(ctx context.Context, deps *authflow.Dependenc
 			UserID:        userID,
 		}), nil
 	case len(flows.Nearest.Nodes) == 2:
+		return NewNodePreAuthenticateNodeSimple(ctx, flows, deps)
+	case len(flows.Nearest.Nodes) == 3:
+		userID, _ := i.userID(flows)
+		if userID == "" {
+			panic(fmt.Errorf("expected userID to be non empty in IntentSignupFlow"))
+		}
+		i, err := NewIntentSignupFlowEnsureContraintsFulfilled(ctx, deps, flows, &IntentSignupFlowEnsureContraintsFulfilledOptions{
+			UserID:        userID,
+			FlowReference: i.FlowReference,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return authflow.NewSubFlow(i), nil
+	case len(flows.Nearest.Nodes) == 4:
 		userID, createUser := i.userID(flows)
 		n, err := NewNodeDoCreateSession(ctx, deps, flows, &NodeDoCreateSession{
 			UserID:       userID,
