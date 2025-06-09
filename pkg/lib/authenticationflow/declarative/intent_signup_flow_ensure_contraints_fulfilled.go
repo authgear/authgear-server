ad3c31a940abb9b5ffd5ed006fe7c83005228008
Tung Wu <tung@oursky.com>
2025-06-09 11:58:15 +0800

    Add IntentSignupFlowEnsureContraintsFulfilled

---
+++ pkg/lib/authenticationflow/declarative/intent_signup_flow_ensure_contraints_fulfilled.go
@@ -0,0 +1,121 @@
+package declarative
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/authgear/authgear-server/pkg/api/model"
+	"github.com/authgear/authgear-server/pkg/lib/authenticationflow"
+	authflow "github.com/authgear/authgear-server/pkg/lib/authenticationflow"
+	"github.com/authgear/authgear-server/pkg/lib/config"
+	"github.com/iawaknahc/jsonschema/pkg/jsonpointer"
+)
+
+func init() {
+	authflow.RegisterIntent(&IntentSignupFlowEnsureContraintsFulfilled{})
+}
+
+type IntentSignupFlowEnsureContraintsFulfilled struct {
+	UserID        string                                   `json:"user_id"`
+	FlowObject    *config.AuthenticationFlowSignupFlowStep `json:"flow_object"`
+	FlowReference authenticationflow.FlowReference         `json:"flow_reference,omitempty"`
+	JSONPointer   jsonpointer.T                            `json:"json_pointer,omitempty"`
+}
+
+type IntentSignupFlowEnsureContraintsFulfilledOptions struct {
+	UserID        string                           `json:"user_id"`
+	FlowReference authenticationflow.FlowReference `json:"flow_reference,omitempty"`
+}
+
+func NewIntentSignupFlowEnsureContraintsFulfilled(ctx context.Context, deps *authenticationflow.Dependencies, flows authenticationflow.Flows, opts *IntentSignupFlowEnsureContraintsFulfilledOptions) (*IntentSignupFlowEnsureContraintsFulfilled, error) {
+	var oneOfs []*config.AuthenticationFlowSignupFlowOneOf
+	recoveryCodeStep := &config.AuthenticationFlowSignupFlowStep{
+		Type: config.AuthenticationFlowSignupFlowStepTypeViewRecoveryCode,
+	}
+
+	addOneOf := func(am config.AuthenticationFlowAuthentication, bpGetter func(*config.AppConfig) (*config.AuthenticationFlowBotProtection, bool)) {
+
+		oneOf := &config.AuthenticationFlowSignupFlowOneOf{
+			Authentication: am,
+		}
+
+		if bpGetter != nil {
+			if bp, ok := bpGetter(deps.Config); ok {
+				oneOf.BotProtection = bp
+			}
+		}
+
+		oneOf.Steps = append(oneOf.Steps, recoveryCodeStep)
+		oneOfs = append(oneOfs, oneOf)
+	}
+
+	for _, authenticatorType := range *deps.Config.Authentication.SecondaryAuthenticators {
+		switch authenticatorType {
+		case model.AuthenticatorTypePassword:
+			addOneOf(config.AuthenticationFlowAuthenticationSecondaryPassword, nil)
+		case model.AuthenticatorTypeOOBEmail:
+			addOneOf(config.AuthenticationFlowAuthenticationSecondaryOOBOTPEmail, getBotProtectionRequirementsOOBOTPEmail)
+		case model.AuthenticatorTypeOOBSMS:
+			addOneOf(config.AuthenticationFlowAuthenticationSecondaryOOBOTPSMS, getBotProtectionRequirementsOOBOTPSMS)
+		case model.AuthenticatorTypeTOTP:
+			addOneOf(config.AuthenticationFlowAuthenticationSecondaryTOTP, nil)
+		case model.AuthenticatorTypePasskey:
+			// FIXME(tung): We don't have a step to force user create passkey at the moment
+		}
+	}
+
+	trueValue := true
+	// Generate a temporary config for this step only
+	flowObject := &config.AuthenticationFlowSignupFlowStep{
+		Type:                             config.AuthenticationFlowSignupFlowStepTypeCreateAuthenticator,
+		OneOf:                            oneOfs,
+		ShowUntilAMRConstraintsFulfilled: &trueValue,
+	}
+
+	return &IntentSignupFlowEnsureContraintsFulfilled{
+		UserID:        opts.UserID,
+		FlowReference: opts.FlowReference,
+		FlowObject:    flowObject,
+		JSONPointer:   jsonpointer.T{},
+	}, nil
+}
+
+var _ authenticationflow.Intent = &IntentSignupFlowEnsureContraintsFulfilled{}
+var _ authenticationflow.Milestone = &IntentSignupFlowEnsureContraintsFulfilled{}
+var _ MilestoneAuthenticationFlowObjectProvider = &IntentSignupFlowEnsureContraintsFulfilled{}
+
+func (*IntentSignupFlowEnsureContraintsFulfilled) Kind() string {
+	return "IntentSignupFlowEnsureContraintsFulfilled"
+}
+
+func (i *IntentSignupFlowEnsureContraintsFulfilled) CanReactTo(ctx context.Context, deps *authenticationflow.Dependencies, flows authenticationflow.Flows) (authenticationflow.InputSchema, error) {
+	switch len(flows.Nearest.Nodes) {
+	case 0:
+		return nil, nil
+	case 1:
+		return nil, authflow.ErrEOF
+	}
+	panic(fmt.Errorf("unexpected number of nodes"))
+}
+
+func (i *IntentSignupFlowEnsureContraintsFulfilled) ReactTo(ctx context.Context, deps *authenticationflow.Dependencies, flows authenticationflow.Flows, input authenticationflow.Input) (authenticationflow.ReactToResult, error) {
+	stepCreateAuthenticator, err := NewIntentSignupFlowStepCreateAuthenticator(ctx, deps, flows, &IntentSignupFlowStepCreateAuthenticator{
+		FlowReference: i.FlowReference,
+		StepName:      "",
+		JSONPointer:   i.JSONPointer,
+		UserID:        i.UserID,
+	}, i)
+	if err != nil {
+		return nil, err
+	}
+	return authflow.NewSubFlow(stepCreateAuthenticator), nil
+}
+
+func (i *IntentSignupFlowEnsureContraintsFulfilled) Milestone() {
+	return
+}
+
+// This is needed so that the child authenticate intents display a correct flow action
+func (i *IntentSignupFlowEnsureContraintsFulfilled) MilestoneAuthenticationFlowObjectProvider() config.AuthenticationFlowObject {
+	return i.FlowObject
+}
