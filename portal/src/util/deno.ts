55245d8e82fa7922a61a74c30d382a10d277c16c
Tung Wu <tung@oursky.com>
2025-06-20 17:54:43 +0800

    Support configuring authentication events in portal

--- portal/src/util/deno.ts
+++ portal/src/util/deno.ts
@@ -1,3 +1,7 @@
-const authgear_deno_hook_version = "v1.5.0";
+// FIXME(tung): Use the published version
+// const authgear_deno_hook_version = "v1.5.0";
 
-export const DENO_TYPES_URL = `https://deno.land/x/authgear_deno_hook@${authgear_deno_hook_version}/mod.ts`;
+// export const DENO_TYPES_URL = `https://deno.land/x/authgear_deno_hook@${authgear_deno_hook_version}/mod.ts`;
+
+export const DENO_TYPES_URL =
+  "https://raw.githubusercontent.com/authgear/authgear_deno_hook/e4436e275570a7e49deeaffcebb4a96b99ba3f34/mod.ts";
