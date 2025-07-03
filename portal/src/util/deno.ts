2efd3f0df8b86dbad8dfb7d274fb091f3904a3cc
Tung Wu <tung@oursky.com>
2025-07-03 13:08:49 +0800

    Update deno types to 1.6.1

--- portal/src/util/deno.ts
+++ portal/src/util/deno.ts
@@ -1,3 +1,3 @@
-const authgear_deno_hook_version = "v1.6.0";
+const authgear_deno_hook_version = "v1.6.1";
 
 export const DENO_TYPES_URL = `https://deno.land/x/authgear_deno_hook@${authgear_deno_hook_version}/mod.ts`;
