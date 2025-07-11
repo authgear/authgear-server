8f7e1b4d56c51cadfa58f34ef2d526982eaadeff
Louis Chan <louischan@oursky.com>
2025-07-11 11:51:44 +0800

    Add SkipLoggingHandler

---
+++ pkg/util/slogutil/skip_logging_handler.go
@@ -0,0 +1,143 @@
+package slogutil
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/lib/pq"
+	slogmulti "github.com/samber/slog-multi"
+
+	"github.com/authgear/authgear-server/pkg/util/errorutil"
+)
+
+// LoggingSkippable is an interface to be implemented by error.
+type LoggingSkippable interface{ SkipLogging() bool }
+
+const AttrKeySkipLogging = "__authgear_skip_logging"
+
+type SkipLoggingHandler struct {
+	Next slog.Handler
+}
+
+var _ slog.Handler = SkipLoggingHandler{}
+
+func NewSkipLoggingMiddleware() slogmulti.Middleware {
+	return func(next slog.Handler) slog.Handler {
+		return SkipLoggingHandler{
+			Next: next,
+		}
+	}
+}
+
+func (s SkipLoggingHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	// We want this handler to always run.
+	return true
+}
+
+func (s SkipLoggingHandler) Handle(ctx context.Context, record slog.Record) error {
+	shouldSkip := false
+
+	record.Attrs(func(attr slog.Attr) bool {
+		if attr.Key == AttrKeyError {
+			if err, ok := attr.Value.Any().(error); ok {
+				if IgnoreError(err) {
+					shouldSkip = true
+				}
+			}
+			// We have found the key, we can stop the iteration.
+			return false
+		}
+		return true
+	})
+
+	// We always call the next handler.
+	// The way we skip handler is to add an attribute for downstream handler to read.
+	if shouldSkip {
+		record = record.Clone()
+		record.AddAttrs(slog.Bool(AttrKeySkipLogging, true))
+	}
+
+	return s.Next.Handle(ctx, record)
+}
+
+func (s SkipLoggingHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return SkipLoggingHandler{
+		Next: s.Next.WithAttrs(attrs),
+	}
+}
+
+func (s SkipLoggingHandler) WithGroup(name string) slog.Handler {
+	return SkipLoggingHandler{
+		Next: s.Next.WithGroup(name),
+	}
+}
+
+func IgnoreError(err error) (ignore bool) {
+	// IsForceLogging overrides everything.
+	if errorutil.IsForceLogging(err) {
+		ignore = false
+		return
+	}
+
+	// In most case, when the HTTP request was canceled,
+	// the error would be context.Canceled.
+	// However, by observation, other errors may occur too.
+	// We consider all of them as request canceled and do not log them.
+	if errors.Is(err, context.Canceled) {
+		ignore = true
+	}
+
+	// DeadlineExceeded occurs when we shutdown the daemon but encounter a timeout.
+	if errors.Is(err, context.DeadlineExceeded) {
+		ignore = true
+	}
+
+	// ErrAbortHandler is a sentinel panic value to abort a handler.
+	// net/http does NOT log this error, so do we.
+	if errors.Is(err, http.ErrAbortHandler) {
+		ignore = true
+	}
+
+	// http.MaxBytesReader will *http.MaxBytesError when the body is too large.
+	// We do not want to log this.
+	var maxBytesError *http.MaxBytesError
+	if errors.As(err, &maxBytesError) {
+		ignore = true
+	}
+
+	// json.Unmarshal returns a SyntaxError if the JSON can't be parsed.
+	// https://pkg.go.dev/encoding/json#SyntaxError
+	var jsonSyntaxError *json.SyntaxError
+	if errors.As(err, &jsonSyntaxError) {
+		ignore = true
+	}
+
+	var pqError *pq.Error
+	if errors.As(err, &pqError) {
+		// https://www.postgresql.org/docs/13/errcodes-appendix.html
+		// 57014 is query_canceled
+		if pqError.Code == "57014" {
+			ignore = true
+		}
+	}
+
+	if errors.Is(err, sql.ErrTxDone) {
+		// The sql package will rollback the transaction when the context is canceled.
+		// https://pkg.go.dev/database/sql/driver#ConnBeginTx
+		// So when we call Rollback() again, this error will be returned.
+		ignore = true
+	}
+
+	var skippable LoggingSkippable
+	if errors.As(err, &skippable) {
+		if skippable.SkipLogging() {
+			ignore = true
+		}
+	}
+
+	return
+}
---
+++ pkg/util/slogutil/skip_logging_handler_test.go
@@ -0,0 +1,189 @@
+package slogutil
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"strings"
+	"syscall"
+	"testing"
+
+	"github.com/lib/pq"
+	slogmulti "github.com/samber/slog-multi"
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/authgear/authgear-server/pkg/util/errorutil"
+)
+
+type mockLoggingSkippable struct {
+	skip bool
+}
+
+func (m mockLoggingSkippable) SkipLogging() bool {
+	return m.skip
+}
+
+func (m mockLoggingSkippable) Error() string {
+	return "mock error"
+}
+
+func TestNewSkipLoggingMiddleware(t *testing.T) {
+	Convey("NewSkipLoggingMiddleware", t, func() {
+		var w strings.Builder
+		logger := slog.New(slogmulti.Pipe(NewSkipLoggingMiddleware()).Handler(NewHandlerForTesting(&w)))
+
+		Convey("normal logging should pass through", func() {
+			logger.Info("test message")
+			So(w.String(), ShouldEqual, "level=INFO msg=\"test message\"\n")
+		})
+
+		Convey("error logging should be skipped based on error type", func() {
+			testCases := []struct {
+				name     string
+				err      error
+				expected string
+			}{
+				{
+					name:     "context.Canceled should be skipped",
+					err:      context.Canceled,
+					expected: "level=ERROR msg=\"test message\" error=\"context canceled\" __authgear_skip_logging=true\n",
+				},
+				{
+					name:     "context.DeadlineExceeded should be skipped",
+					err:      context.DeadlineExceeded,
+					expected: "level=ERROR msg=\"test message\" error=\"context deadline exceeded\" __authgear_skip_logging=true\n",
+				},
+				{
+					name:     "http.ErrAbortHandler should be skipped",
+					err:      http.ErrAbortHandler,
+					expected: "level=ERROR msg=\"test message\" error=\"net/http: abort Handler\" __authgear_skip_logging=true\n",
+				},
+				{
+					name:     "http.MaxBytesError should be skipped",
+					err:      &http.MaxBytesError{Limit: 100},
+					expected: "level=ERROR msg=\"test message\" error=\"http: request body too large\" __authgear_skip_logging=true\n",
+				},
+				{
+					name:     "json.SyntaxError should be skipped",
+					err:      &json.SyntaxError{},
+					expected: "level=ERROR msg=\"test message\" error=\"\" __authgear_skip_logging=true\n",
+				},
+				{
+					name:     "pq.Error with query_canceled should be skipped",
+					err:      &pq.Error{Code: "57014"},
+					expected: "level=ERROR msg=\"test message\" error=\"pq: \" __authgear_skip_logging=true\n",
+				},
+				{
+					name:     "sql.ErrTxDone should be skipped",
+					err:      sql.ErrTxDone,
+					expected: "level=ERROR msg=\"test message\" error=\"sql: transaction has already been committed or rolled back\" __authgear_skip_logging=true\n",
+				},
+				{
+					name:     "LoggingSkippable with skip=true should be skipped",
+					err:      mockLoggingSkippable{skip: true},
+					expected: "level=ERROR msg=\"test message\" error=\"mock error\" __authgear_skip_logging=true\n",
+				},
+				{
+					name:     "LoggingSkippable with skip=false should not be skipped",
+					err:      mockLoggingSkippable{skip: false},
+					expected: "level=ERROR msg=\"test message\" error=\"mock error\"\n",
+				},
+				{
+					name:     "regular error should not be skipped",
+					err:      fmt.Errorf("regular error"),
+					expected: "level=ERROR msg=\"test message\" error=\"regular error\"\n",
+				},
+			}
+
+			for _, tc := range testCases {
+				Convey(tc.name, func() {
+					logger.Error("test message", Err(tc.err))
+					So(w.String(), ShouldEqual, tc.expected)
+				})
+			}
+		})
+
+		Convey("ForceLogging should override skip behavior", func() {
+			forcedErr := errorutil.ForceLogging(context.Canceled)
+			logger.Error("test message", Err(forcedErr))
+			So(w.String(), ShouldContainSubstring, "level=ERROR")
+			So(w.String(), ShouldContainSubstring, "msg=\"test message\"")
+		})
+	})
+}
+
+func TestIgnoreError(t *testing.T) {
+	Convey("IgnoreError", t, func() {
+		Convey("should return false for ForceLogging errors", func() {
+			err := errorutil.ForceLogging(context.Canceled)
+			So(IgnoreError(err), ShouldBeFalse)
+		})
+
+		Convey("should return true for context.Canceled", func() {
+			So(IgnoreError(context.Canceled), ShouldBeTrue)
+		})
+
+		Convey("should return true for context.DeadlineExceeded", func() {
+			So(IgnoreError(context.DeadlineExceeded), ShouldBeTrue)
+		})
+
+		Convey("should return true for http.ErrAbortHandler", func() {
+			So(IgnoreError(http.ErrAbortHandler), ShouldBeTrue)
+		})
+
+		Convey("should return true for http.MaxBytesError", func() {
+			err := &http.MaxBytesError{Limit: 100}
+			So(IgnoreError(err), ShouldBeTrue)
+		})
+
+		Convey("should return true for json.SyntaxError", func() {
+			err := &json.SyntaxError{}
+			So(IgnoreError(err), ShouldBeTrue)
+		})
+
+		Convey("should return true for pq.Error with query_canceled code", func() {
+			err := &pq.Error{Code: "57014"}
+			So(IgnoreError(err), ShouldBeTrue)
+		})
+
+		Convey("should return false for pq.Error with other codes", func() {
+			err := &pq.Error{Code: "23505"}
+			So(IgnoreError(err), ShouldBeFalse)
+		})
+
+		Convey("should return false for syscall.EPIPE", func() {
+			err := syscall.EPIPE
+			So(IgnoreError(err), ShouldBeFalse)
+		})
+
+		Convey("should return false for syscall.ECONNREFUSED", func() {
+			err := syscall.ECONNREFUSED
+			So(IgnoreError(err), ShouldBeFalse)
+		})
+
+		Convey("should return true for sql.ErrTxDone", func() {
+			So(IgnoreError(sql.ErrTxDone), ShouldBeTrue)
+		})
+
+		Convey("should handle LoggingSkippable interface", func() {
+			skippable := mockLoggingSkippable{skip: true}
+			So(IgnoreError(skippable), ShouldBeTrue)
+
+			notSkippable := mockLoggingSkippable{skip: false}
+			So(IgnoreError(notSkippable), ShouldBeFalse)
+		})
+
+		Convey("should return false for regular errors", func() {
+			err := fmt.Errorf("regular error")
+			So(IgnoreError(err), ShouldBeFalse)
+		})
+
+		Convey("should handle wrapped errors", func() {
+			wrappedCanceled := fmt.Errorf("wrapped: %w", context.Canceled)
+			So(IgnoreError(wrappedCanceled), ShouldBeTrue)
+		})
+	})
+}
