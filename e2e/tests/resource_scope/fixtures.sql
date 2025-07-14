261fcc28b340c6e4d48d5f7959bf31e09b7f385b
Tung Wu <tung@oursky.com>
2025-07-14 17:54:53 +0800

    Add e2e tests for update resuorce and scope

--- e2e/tests/resource_scope/fixtures.sql
+++ e2e/tests/resource_scope/fixtures.sql
@@ -15,3 +15,23 @@ INSERT INTO _auth_resource (
   'Fixture Resource 1',
   '{}'
 );
+
+INSERT INTO _auth_resource_scope (
+  "id",
+  "app_id",
+  "created_at",
+  "updated_at",
+  "resource_id",
+  "scope",
+  "description",
+  "metadata"
+) VALUES (
+  '{{ .AppID }}-fixture-scope-01',
+  '{{ .AppID }}',
+  NOW(),
+  NOW(),
+  '{{ .AppID }}-fixture-resource-01',
+  'fixturescope1',
+  'Fixture Scope 1',
+  '{}'
+);
