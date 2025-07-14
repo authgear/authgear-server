f734a71ba5518eaea5bce17c0c2f40674ef8bff4
Tung Wu <tung@oursky.com>
2025-07-14 18:28:32 +0800

    Add e2e tests for addScopesToClientID

--- e2e/tests/resource_scope/fixtures.sql
+++ e2e/tests/resource_scope/fixtures.sql
@@ -35,3 +35,58 @@ INSERT INTO _auth_resource_scope (
   'Fixture Scope 1',
   '{}'
 );
+
+-- Fixture for addScopesToClientID test
+INSERT INTO _auth_resource (
+  "id",
+  "app_id",
+  "created_at",
+  "updated_at",
+  "uri",
+  "name",
+  "metadata"
+) VALUES (
+  '{{ .AppID }}-fixture-resource-02',
+  '{{ .AppID }}',
+  NOW(),
+  NOW(),
+  'https://fixtureresource/2',
+  'Fixture Resource 2',
+  '{}'
+);
+
+INSERT INTO _auth_client_resource (
+  "id",
+  "app_id",
+  "created_at",
+  "updated_at",
+  "client_id",
+  "resource_id"
+) VALUES (
+  '{{ .AppID }}-fixture-client-resource-01',
+  '{{ .AppID }}',
+  NOW(),
+  NOW(),
+  'portal',
+  '{{ .AppID }}-fixture-resource-02'
+);
+
+INSERT INTO _auth_resource_scope (
+  "id",
+  "app_id",
+  "created_at",
+  "updated_at",
+  "resource_id",
+  "scope",
+  "description",
+  "metadata"
+) VALUES (
+  '{{ .AppID }}-fixture-scope-02',
+  '{{ .AppID }}',
+  NOW(),
+  NOW(),
+  '{{ .AppID }}-fixture-resource-02',
+  'fixturescope2',
+  'Fixture Scope 2',
+  '{}'
+);
