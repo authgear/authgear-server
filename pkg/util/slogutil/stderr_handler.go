5308dc66690c10ae6bb7613a5568e29589cb3817
Louis Chan <louischan@oursky.com>
2025-07-15 16:16:14 +0800

    Set AddSource to true when level <= slog.LevelDebug

--- pkg/util/slogutil/stderr_handler.go
+++ pkg/util/slogutil/stderr_handler.go
@@ -9,5 +9,7 @@ func NewStderrHandler(strLevel string) *slog.TextHandler {
 	level := ParseLevel(strLevel)
 	return slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 		Level: level,
+		// Include source when the global log level is debug or lower.
+		AddSource: level <= slog.LevelDebug,
 	})
 }
