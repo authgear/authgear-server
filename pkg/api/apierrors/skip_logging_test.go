eb7d2d987e61bb48b818f7a4bf9e850fc17e3685
Louis Chan <louischan@oursky.com>
2025-07-15 15:00:56 +0800

    Rewrite pkg/api/apierrors/skip_logging_test.go

--- pkg/api/apierrors/skip_logging_test.go
+++ pkg/api/apierrors/skip_logging_test.go
@@ -1,53 +1,75 @@
 package apierrors
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"testing"
 
-	"github.com/sirupsen/logrus"
+	slogmulti "github.com/samber/slog-multi"
 	. "github.com/smartystreets/goconvey/convey"
 
-	"github.com/authgear/authgear-server/pkg/util/log"
+	"github.com/authgear/authgear-server/pkg/util/slogutil"
 )
 
-type myhook struct {
+type myhandler struct {
 	IsSkipped bool
 }
 
-func (*myhook) Levels() []logrus.Level { return logrus.AllLevels }
+var _ slog.Handler = (*myhandler)(nil)
 
-func (h *myhook) Fire(entry *logrus.Entry) error {
-	if log.IsLoggingSkipped(entry) {
-		h.IsSkipped = true
+// Enabled implements slog.Handler.
+func (m *myhandler) Enabled(context.Context, slog.Level) bool {
+	return true
+}
+
+// Handle implements slog.Handler.
+func (m *myhandler) Handle(ctx context.Context, record slog.Record) error {
+	if slogutil.IsLoggingSkipped(record) {
+		m.IsSkipped = true
 	}
-
 	return nil
+}
+
+// WithAttrs implements slog.Handler.
+func (m *myhandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return m
+}
+
+// WithGroup implements slog.Handler.
+func (m *myhandler) WithGroup(name string) slog.Handler {
+	return m
 }
 
 func TestSkipLogging(t *testing.T) {
 	Convey("SkipLogging", t, func() {
-		myhook := &myhook{}
-		logger := logrus.New()
-		logger.AddHook(log.SkipLoggingHook{})
-		logger.AddHook(myhook)
+		ctx := context.Background()
+		handler := &myhandler{}
+		rootLogger := slog.New(slogmulti.Pipe(slogutil.NewSkipLoggingMiddleware()).Handler(handler))
+		ctx = slogutil.SetContextLogger(ctx, rootLogger)
+
+		logger := slogutil.NewLogger("logger")
 
 		Convey("Ignore apierrors with some specific kind", func() {
+			logger := logger.GetLogger(ctx)
 			err := InternalError.WithReason("Ignore").SkipLoggingToExternalService().New("ignore")
-			logger.WithError(err).Error("error")
-			So(myhook.IsSkipped, ShouldBeTrue)
+			logger.WithError(err).Error(ctx, "error")
+			So(handler.IsSkipped, ShouldBeTrue)
 		})
 
 		Convey("Ignore wrapped apierrors", func() {
+			logger := logger.GetLogger(ctx)
 			err := InternalError.WithReason("Ignore").SkipLoggingToExternalService().New("ignore")
 			err = fmt.Errorf("wrap: %w", err)
-			logger.WithError(err).Error("error")
-			So(myhook.IsSkipped, ShouldBeTrue)
+			logger.WithError(err).Error(ctx, "error")
+			So(handler.IsSkipped, ShouldBeTrue)
 		})
 
 		Convey("Do not apierrors", func() {
+			logger := logger.GetLogger(ctx)
 			err := InternalError.WithReason("DO_NOT_IGNORE").New("DO_NOT_IGNORE")
-			logger.WithError(err).Error("error")
-			So(myhook.IsSkipped, ShouldBeFalse)
+			logger.WithError(err).Error(ctx, "error")
+			So(handler.IsSkipped, ShouldBeFalse)
 		})
 	})
 }
