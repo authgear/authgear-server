389c505761f2334013513026108ce1400e5d0389
Tung Wu <tung@oursky.com>
2025-07-16 18:08:11 +0800

    Fix a typo

--- pkg/lib/oauth/grant_offline.go
+++ pkg/lib/oauth/grant_offline.go
@@ -64,7 +64,7 @@ type OfflineGrant struct {
 	// Note that ExpireAtForResolvedSession will keep changing if idle timeout is enabled.
 	// This is NOT supposed to be stored, hence it is json-ignored.
 	// This field is always the expiry of the OfflineGrant, but not the expiry of any of the children refresh tokens
-	// Exipry of children refresh tokens could be shorter due to diffrent last access time
+	// Expiry of children refresh tokens could be shorter due to different last access time
 	ExpireAtForResolvedSession time.Time `json:"-"`
 }
 
