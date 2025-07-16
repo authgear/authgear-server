f0b78ff885d4280a658a6711bcd0d17e0144e9c7
Tung Wu <tung@oursky.com>
2025-07-16 16:00:35 +0800

    Always take resourceID to query scope
    So that caller must first query the resource, then a correct error will be returned if resource not found

--- pkg/lib/resourcescope/store_scope.go
+++ pkg/lib/resourcescope/store_scope.go
@@ -218,13 +218,8 @@ func (s *Store) applyListScopesOptions(q db.SelectBuilder, alias string, options
 	return q
 }
 
-func (s *Store) GetScope(ctx context.Context, resourceURI string, scope string) (*Scope, error) {
-	resource, err := s.GetResourceByURI(ctx, resourceURI)
-	if err != nil {
-		return nil, err
-	}
-
-	q := s.selectScopeQuery("s").Where("s.resource_id = ? AND s.scope = ?", resource.ID, scope)
+func (s *Store) GetResourceScope(ctx context.Context, resourceID string, scope string) (*Scope, error) {
+	q := s.selectScopeQuery("s").Where("s.resource_id = ? AND s.scope = ?", resourceID, scope)
 	row, err := s.SQLExecutor.QueryRowWith(ctx, q)
 	if err != nil {
 		return nil, err
