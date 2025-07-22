18918336b21acae34fc453428005248ef291dd4e
Tung Wu <tung@oursky.com>
2025-07-22 22:33:26 +0800

    Add resources query

--- portal/src/screens/APIResources/APIResourcesScreen.tsx
+++ portal/src/screens/APIResources/APIResourcesScreen.tsx
@@ -4,9 +4,24 @@ import ScreenTitle from "../../ScreenTitle";
 import ScreenDescription from "../../ScreenDescription";
 import ScreenContentHeader from "../../ScreenContentHeader";
 import { FormattedMessage } from "@oursky/react-messageformat";
-import { ResourceListEmptyView } from "../../components/api-resources/ResourceListEmptyView";
+import { ResourceList } from "../../components/api-resources/ResourceList";
+import { useResourcesQueryQuery } from "../../graphql/adminapi/query/resourcesQuery.generated";
+import ShowError from "../../ShowError";
 
 const APIResourcesScreen: React.VFC = function APIResourcesScreen() {
+  const { data, loading, error, refetch } = useResourcesQueryQuery();
+
+  const resources =
+    data?.resources?.edges
+      ?.map((edge) => edge?.node)
+      .filter((resource): resource is NonNullable<typeof resource> =>
+        Boolean(resource)
+      ) ?? [];
+
+  if (error != null) {
+    return <ShowError error={error} onRetry={refetch} />;
+  }
+
   return (
     <ScreenContent>
       <ScreenContentHeader
@@ -22,7 +37,7 @@ const APIResourcesScreen: React.VFC = function APIResourcesScreen() {
         }
       />
       <div className="col-span-full p-8">
-        <ResourceListEmptyView />
+        <ResourceList resources={resources} loading={loading} />
       </div>
     </ScreenContent>
   );
