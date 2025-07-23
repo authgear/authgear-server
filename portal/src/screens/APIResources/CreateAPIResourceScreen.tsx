8dcf2c749100d6ca25ca94f2700508c7e656713b
Tung Wu <tung@oursky.com>
2025-07-23 20:50:42 +0800

    Create resource on submit

--- portal/src/screens/APIResources/CreateAPIResourceScreen.tsx
+++ portal/src/screens/APIResources/CreateAPIResourceScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ScreenContent from "../../ScreenContent";
 import ScreenContentHeader from "../../ScreenContentHeader";
 import { FormattedMessage } from "@oursky/react-messageformat";
@@ -6,10 +6,12 @@ import NavBreadcrumb from "../../NavBreadcrumb";
 import {
   ResourceForm,
   ResourceFormState,
+  sanitizeFormState,
 } from "../../components/api-resources/ResourceForm";
 import { useSimpleForm } from "../../hook/useSimpleForm";
 import { FormContainerBase } from "../../FormContainerBase";
 import { ErrorMessageBarContextProvider } from "../../ErrorMessageBar";
+import { useCreateResourceMutationMutation } from "../../graphql/adminapi/mutations/createResourceMutation.generated";
 
 const defaultState: ResourceFormState = {
   name: "",
@@ -17,14 +19,28 @@ const defaultState: ResourceFormState = {
 };
 
 const CreateAPIResourceScreen: React.VFC = function CreateAPIResourceScreen() {
+  const [createResource] = useCreateResourceMutationMutation();
+
   const form = useSimpleForm<ResourceFormState>({
     defaultState,
-    submit: async () => {
-      // TODO
+    submit: async (s) => {
+      const state = sanitizeFormState(s);
+      await createResource({
+        variables: {
+          input: {
+            name: state.name,
+            resourceURI: state.resourceURI,
+          },
+        },
+      });
     },
     stateMode:
       "ConstantInitialStateAndResetCurrentStatetoInitialStateAfterSave",
   });
+
+  useEffect(() => {
+    // TODO: Go to edit screen
+  }, [form.isSubmitted]);
 
   return (
     <ErrorMessageBarContextProvider>
