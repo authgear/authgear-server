2dd1db31d1bcf9994647f37b98355fa403162fa6
Tung Wu <tung@oursky.com>
2025-07-23 16:16:53 +0800

    Add action buttons to ResourceList

--- portal/src/components/api-resources/ResourceList.tsx
+++ portal/src/components/api-resources/ResourceList.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useContext } from "react";
+import React, { useMemo, useContext, useCallback } from "react";
 import cn from "classnames";
 import { Resource } from "../../graphql/adminapi/globalTypes.generated";
 import { ResourceListEmptyView } from "./ResourceListEmptyView";
@@ -11,18 +11,22 @@ import {
 import { Context } from "@oursky/react-messageformat";
 import PaginationWidget, { PaginationProps } from "../../PaginationWidget";
 import styles from "./ResourceList.module.css";
+import { useSystemConfig } from "../../context/SystemConfigContext";
+import ActionButton from "../../ActionButton";
 
 interface ResourceListProps {
   className?: string;
   resources: Resource[];
   loading: boolean;
   pagination: PaginationProps;
+  onEdit: (resource: Resource) => void;
+  onDelete: (resource: Resource) => void;
 }
 
 export const ResourceList: React.VFC<ResourceListProps> = function ResourceList(
   props
 ) {
-  const { className, resources, loading, pagination } = props;
+  const { className, resources, loading, pagination, onDelete, onEdit } = props;
   const { renderToString } = useContext(Context);
 
   const columns: IColumn[] = useMemo(
@@ -31,7 +35,7 @@ export const ResourceList: React.VFC<ResourceListProps> = function ResourceList(
         key: "name",
         name: renderToString("ResourceList.columns.name"),
         minWidth: 200,
-        maxWidth: 300,
+        maxWidth: 400,
         isResizable: true,
         fieldName: "name",
       },
@@ -39,12 +43,31 @@ export const ResourceList: React.VFC<ResourceListProps> = function ResourceList(
         key: "identifier",
         name: renderToString("ResourceList.columns.identifier"),
         minWidth: 200,
-        maxWidth: 300,
         isResizable: true,
         fieldName: "resourceURI",
       },
+      {
+        key: "actions",
+        name: "",
+        minWidth: 100,
+        maxWidth: 100,
+        isResizable: false,
+        // eslint-disable-next-line react/no-unstable-nested-components
+        onRender: (item?: Resource, _0?: number, _1?: IColumn) => {
+          if (item == null) {
+            return null;
+          }
+          return (
+            <ActionButtonsColumn
+              resource={item}
+              onDelete={onDelete}
+              onEdit={onEdit}
+            />
+          );
+        },
+      },
     ],
-    [renderToString]
+    [onDelete, onEdit, renderToString]
   );
 
   if (resources.length === 0) {
@@ -71,3 +94,42 @@ export const ResourceList: React.VFC<ResourceListProps> = function ResourceList(
     </div>
   );
 };
+
+interface ActionButtonsColumnProps {
+  resource: Resource;
+  onEdit: (resource: Resource) => void;
+  onDelete: (resource: Resource) => void;
+}
+
+function ActionButtonsColumn({
+  resource,
+  onEdit,
+  onDelete,
+}: ActionButtonsColumnProps) {
+  const { renderToString } = useContext(Context);
+  const { themes } = useSystemConfig();
+  return (
+    <div className="flex items-center">
+      <ActionButton
+        text={renderToString("edit")}
+        styles={{
+          label: { fontWeight: 600 },
+        }}
+        theme={themes.actionButton}
+        onClick={useCallback(() => {
+          onEdit(resource);
+        }, [onEdit, resource])}
+      />
+      <ActionButton
+        text={renderToString("delete")}
+        styles={{
+          label: { fontWeight: 600 },
+        }}
+        theme={themes.destructive}
+        onClick={useCallback(() => {
+          onDelete(resource);
+        }, [onDelete, resource])}
+      />
+    </div>
+  );
+}
