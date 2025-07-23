0218d1e49536359b8098e09e52e4982ab7be7ba3
Tung Wu <tung@oursky.com>
2025-07-23 19:50:43 +0800

    Add screen title to CreateAPIResourceScreen

--- portal/src/screens/APIResources/CreateAPIResourceScreen.tsx
+++ portal/src/screens/APIResources/CreateAPIResourceScreen.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import ScreenContent from "../../ScreenContent";
+import ScreenContentHeader from "../../ScreenContentHeader";
+import { FormattedMessage } from "@oursky/react-messageformat";
+import NavBreadcrumb from "../../NavBreadcrumb";
 
 const CreateAPIResourceScreen: React.VFC = function CreateAPIResourceScreen() {
-  return null;
+  return (
+    <ScreenContent className="flex-1" layout="list">
+      <ScreenContentHeader
+        title={
+          <NavBreadcrumb
+            items={[
+              {
+                to: "~/api-resources",
+                label: <FormattedMessage id="APIResourcesScreen.title" />,
+              },
+              {
+                to: "",
+                label: <FormattedMessage id="CreateAPIResourceScreen.title" />,
+              },
+            ]}
+          />
+        }
+      />
+    </ScreenContent>
+  );
 };
 
 export default CreateAPIResourceScreen;
