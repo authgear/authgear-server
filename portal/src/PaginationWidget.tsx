fd609608dc8917b83cbc6cd34cc1ea584ac00092
Tung Wu <tung@oursky.com>
2025-07-23 15:50:30 +0800

    Add pagination to ResourceList

--- portal/src/PaginationWidget.tsx
+++ portal/src/PaginationWidget.tsx
@@ -6,12 +6,15 @@ import { getPaginationRenderData } from "./util/pagination";
 import styles from "./PaginationWidget.module.css";
 import DefaultButton from "./DefaultButton";
 
-export interface Props {
-  className?: string;
+export interface PaginationProps {
   offset: number;
   pageSize: number;
   totalCount?: number;
   onChangeOffset?: (offset: number) => void;
+}
+
+export interface PaginationWidgetProps extends PaginationProps {
+  className?: string;
 }
 
 const iconFirst: IIconProps = {
@@ -52,131 +55,130 @@ const pageButtonStyles: IButtonStyles = {
   },
 };
 
-const PaginationWidget: React.VFC<Props> = function PaginationWidget(
-  props: Props
-) {
-  const { className, offset, pageSize, totalCount, onChangeOffset } = props;
+const PaginationWidget: React.VFC<PaginationWidgetProps> =
+  function PaginationWidget(props: PaginationWidgetProps) {
+    const { className, offset, pageSize, totalCount, onChangeOffset } = props;
 
-  const { renderToString } = useContext(Context);
+    const { renderToString } = useContext(Context);
 
-  const {
-    currentOffset,
-    offsets,
-    firstPageButtonEnabled,
-    prevPageButtonEnabled,
-    nextPageButtonEnabled,
-    lastPageButtonEnabled,
-    maxOffset,
-  } = useMemo(() => {
-    return getPaginationRenderData({
-      offset,
-      pageSize,
-      totalCount,
-    });
-  }, [offset, pageSize, totalCount]);
+    const {
+      currentOffset,
+      offsets,
+      firstPageButtonEnabled,
+      prevPageButtonEnabled,
+      nextPageButtonEnabled,
+      lastPageButtonEnabled,
+      maxOffset,
+    } = useMemo(() => {
+      return getPaginationRenderData({
+        offset,
+        pageSize,
+        totalCount,
+      });
+    }, [offset, pageSize, totalCount]);
 
-  const onClickFirst = useCallback(
-    (e: React.MouseEvent<HTMLElement>) => {
-      e.preventDefault();
-      e.stopPropagation();
-      onChangeOffset?.(0);
-    },
-    [onChangeOffset]
-  );
+    const onClickFirst = useCallback(
+      (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        onChangeOffset?.(0);
+      },
+      [onChangeOffset]
+    );
 
-  const onClickPrev = useCallback(
-    (e: React.MouseEvent<HTMLElement>) => {
-      e.preventDefault();
-      e.stopPropagation();
-      onChangeOffset?.(currentOffset - pageSize);
-    },
-    [currentOffset, pageSize, onChangeOffset]
-  );
+    const onClickPrev = useCallback(
+      (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        onChangeOffset?.(currentOffset - pageSize);
+      },
+      [currentOffset, pageSize, onChangeOffset]
+    );
 
-  const onClickNext = useCallback(
-    (e: React.MouseEvent<HTMLElement>) => {
-      e.preventDefault();
-      e.stopPropagation();
-      onChangeOffset?.(currentOffset + pageSize);
-    },
-    [currentOffset, pageSize, onChangeOffset]
-  );
+    const onClickNext = useCallback(
+      (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        onChangeOffset?.(currentOffset + pageSize);
+      },
+      [currentOffset, pageSize, onChangeOffset]
+    );
 
-  const onClickLast = useCallback(
-    (e: React.MouseEvent<HTMLElement>) => {
-      e.preventDefault();
-      e.stopPropagation();
-      if (maxOffset != null) {
-        onChangeOffset?.(maxOffset);
-      }
-    },
-    [maxOffset, onChangeOffset]
-  );
+    const onClickLast = useCallback(
+      (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        if (maxOffset != null) {
+          onChangeOffset?.(maxOffset);
+        }
+      },
+      [maxOffset, onChangeOffset]
+    );
 
-  const labelFirst = renderToString("PaginationWidget.First");
-  const labelPrev = renderToString("PaginationWidget.Prev");
-  const labelNext = renderToString("PaginationWidget.Next");
-  const labelLast = renderToString("PaginationWidget.Last");
+    const labelFirst = renderToString("PaginationWidget.First");
+    const labelPrev = renderToString("PaginationWidget.Prev");
+    const labelNext = renderToString("PaginationWidget.Next");
+    const labelLast = renderToString("PaginationWidget.Last");
 
-  return (
-    <div className={cn(className, styles.root)}>
-      <IconButton
-        title={labelFirst}
-        ariaLabel={labelFirst}
-        styles={iconButtonStyles}
-        iconProps={iconFirst}
-        disabled={!firstPageButtonEnabled}
-        onClick={onClickFirst}
-      />
-      <IconButton
-        title={labelPrev}
-        ariaLabel={labelPrev}
-        styles={iconButtonStyles}
-        iconProps={iconPrev}
-        disabled={!prevPageButtonEnabled}
-        onClick={onClickPrev}
-      />
-      <div className={styles.pages}>
-        {offsets.map((offset) => {
-          const page = offset / pageSize + 1;
-          const label = renderToString("PaginationWidget.Page", {
-            PAGE: page,
-          });
-          return (
-            <DefaultButton
-              key={offset}
-              title={label}
-              ariaLabel={label}
-              styles={pageButtonStyles}
-              disabled={currentOffset === offset}
-              onClick={(e: React.MouseEvent<HTMLElement>) => {
-                e.preventDefault();
-                e.stopPropagation();
-                onChangeOffset?.(offset);
-              }}
-              text={String(page)}
-            />
-          );
-        })}
+    return (
+      <div className={cn(className, styles.root)}>
+        <IconButton
+          title={labelFirst}
+          ariaLabel={labelFirst}
+          styles={iconButtonStyles}
+          iconProps={iconFirst}
+          disabled={!firstPageButtonEnabled}
+          onClick={onClickFirst}
+        />
+        <IconButton
+          title={labelPrev}
+          ariaLabel={labelPrev}
+          styles={iconButtonStyles}
+          iconProps={iconPrev}
+          disabled={!prevPageButtonEnabled}
+          onClick={onClickPrev}
+        />
+        <div className={styles.pages}>
+          {offsets.map((offset) => {
+            const page = offset / pageSize + 1;
+            const label = renderToString("PaginationWidget.Page", {
+              PAGE: page,
+            });
+            return (
+              <DefaultButton
+                key={offset}
+                title={label}
+                ariaLabel={label}
+                styles={pageButtonStyles}
+                disabled={currentOffset === offset}
+                onClick={(e: React.MouseEvent<HTMLElement>) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  onChangeOffset?.(offset);
+                }}
+                text={String(page)}
+              />
+            );
+          })}
+        </div>
+        <IconButton
+          title={labelNext}
+          ariaLabel={labelNext}
+          styles={iconButtonStyles}
+          iconProps={iconNext}
+          disabled={!nextPageButtonEnabled}
+          onClick={onClickNext}
+        />
+        <IconButton
+          title={labelLast}
+          ariaLabel={labelLast}
+          styles={iconButtonStyles}
+          iconProps={iconLast}
+          disabled={!lastPageButtonEnabled}
+          onClick={onClickLast}
+        />
       </div>
-      <IconButton
-        title={labelNext}
-        ariaLabel={labelNext}
-        styles={iconButtonStyles}
-        iconProps={iconNext}
-        disabled={!nextPageButtonEnabled}
-        onClick={onClickNext}
-      />
-      <IconButton
-        title={labelLast}
-        ariaLabel={labelLast}
-        styles={iconButtonStyles}
-        iconProps={iconLast}
-        disabled={!lastPageButtonEnabled}
-        onClick={onClickLast}
-      />
-    </div>
-  );
-};
+    );
+  };
 
 export default PaginationWidget;
--- portal/src/components/api-resources/ResourceList.tsx
+++ portal/src/components/api-resources/ResourceList.tsx
@@ -1,35 +1,73 @@
-import React from "react";
+import React, { useMemo, useContext } from "react";
+import cn from "classnames";
 import { Resource } from "../../graphql/adminapi/globalTypes.generated";
 import { ResourceListEmptyView } from "./ResourceListEmptyView";
+import {
+  DetailsListLayoutMode,
+  IColumn,
+  ShimmeredDetailsList,
+  SelectionMode,
+} from "@fluentui/react";
+import { Context } from "@oursky/react-messageformat";
+import PaginationWidget, { PaginationProps } from "../../PaginationWidget";
+import styles from "./ResourceList.module.css";
 
 interface ResourceListProps {
+  className?: string;
   resources: Resource[];
   loading: boolean;
+  pagination: PaginationProps;
 }
 
 export const ResourceList: React.VFC<ResourceListProps> = function ResourceList(
   props
 ) {
-  const { resources, loading } = props;
+  const { className, resources, loading, pagination } = props;
+  const { renderToString } = useContext(Context);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const columns: IColumn[] = useMemo(
+    () => [
+      {
+        key: "name",
+        name: renderToString("ResourceList.columns.name"),
+        minWidth: 200,
+        maxWidth: 300,
+        isResizable: true,
+        fieldName: "name",
+      },
+      {
+        key: "identifier",
+        name: renderToString("ResourceList.columns.identifier"),
+        minWidth: 200,
+        maxWidth: 300,
+        isResizable: true,
+        fieldName: "resourceURI",
+      },
+    ],
+    [renderToString]
+  );
 
   if (resources.length === 0) {
     return <ResourceListEmptyView />;
   }
 
   return (
-    <>
-      {resources.map((resource) => (
-        <div key={resource!.id}>
-          <h3>{resource!.name || resource!.resourceURI}</h3>
-          <p>URI: {resource!.resourceURI}</p>
-          <p>Created: {resource!.createdAt}</p>
-          <p>Updated: {resource!.updatedAt}</p>
-        </div>
-      ))}
-    </>
+    <div className={cn(className, styles.listRoot)}>
+      <div
+        // For DetailList to correctly know what to display
+        // https://developer.microsoft.com/en-us/fluentui#/controls/web/detailslist
+        data-is-scrollable="true"
+        className={styles.listWrapper}
+      >
+        <ShimmeredDetailsList
+          items={resources}
+          enableShimmer={loading}
+          columns={columns}
+          layoutMode={DetailsListLayoutMode.justified}
+          selectionMode={SelectionMode.none}
+        />
+      </div>
+      <PaginationWidget className={styles.paginator} {...pagination} />
+    </div>
   );
 };
