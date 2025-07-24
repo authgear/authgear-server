cc38ca8062b7af2e82cc25215deef8df21c57edb
Tung Wu <tung@oursky.com>
2025-07-24 16:38:35 +0800

    Load resource in APIResourceDetailsScreen

--- portal/src/screens/APIResources/APIResourceDetailsScreen.tsx
+++ portal/src/screens/APIResources/APIResourceDetailsScreen.tsx
@@ -1,8 +1,38 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useResourceQueryQuery } from "../../graphql/adminapi/query/resourceQuery.generated";
+import { useLoadableView } from "../../hook/useLoadableView";
 
 const APIResourceDetailsScreen: React.VFC =
   function APIResourceDetailsScreen() {
-    return null;
+    const { resourceID } = useParams<{ resourceID: string }>();
+    const { data, loading, error, refetch } = useResourceQueryQuery({
+      variables: { id: resourceID! },
+    });
+
+    return useLoadableView({
+      loadables: [
+        {
+          isLoading: loading,
+          loadError: error,
+          reload: refetch,
+          data: data,
+        },
+      ],
+      render: ([query]) => {
+        const { data } = query;
+        const resource =
+          data?.node && data.node.__typename === "Resource" ? data.node : null;
+        if (!resource) {
+          return null;
+        }
+        return (
+          <div>
+            <div>{resource.name}</div>
+          </div>
+        );
+      },
+    });
   };
 
 export default APIResourceDetailsScreen;
