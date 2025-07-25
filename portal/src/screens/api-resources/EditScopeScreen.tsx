242c11ede4b37ea9699f3b4555f8dbc249be7ee8
Tung Wu <tung@oursky.com>
2025-07-25 16:21:21 +0800

    Add edit scope screen

---
+++ portal/src/screens/api-resources/EditScopeScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditScopeScreen: React.VFC = function EditScopeScreen() {
+  return <div>Edit Scope Screen (to be implemented)</div>;
+};
+
+export default EditScopeScreen;
