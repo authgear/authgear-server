6fd0eac6cb93cd03585739e12d68784ae879ee71
Tung Wu <tung@oursky.com>
2025-07-28 21:41:54 +0800

    Add edit application scopes screen

---
+++ portal/src/screens/api-resources/EditApplicationScopesScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const EditApplicationScopesScreen: React.VFC =
+  function EditApplicationScopesScreen() {
+    return (
+      <div>
+        <div>Edit Application Scope</div>
+        {/* TODO: Implement Edit Application Scope Screen */}
+      </div>
+    );
+  };
+
+export default EditApplicationScopesScreen;
