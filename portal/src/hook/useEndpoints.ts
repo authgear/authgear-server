d0cbcd8bffb03cfd1e6894cf2cbe22be70852a58
Tung Wu <tung@oursky.com>
2025-07-31 15:21:33 +0800

    Show basic information of m2m client in detail screen

--- portal/src/hook/useEndpoints.ts
+++ portal/src/hook/useEndpoints.ts
@@ -1,25 +1,41 @@
 import { useMemo } from "react";
+import { OAuthClientConfig } from "../types";
 
 interface Endpoints {
   openidConfiguration: string;
-  authorize: string;
+  authorize: string | null;
   token: string;
-  userinfo: string;
-  endSession: string;
+  userinfo: string | null;
+  endSession: string | null;
   jwksUri: string;
 }
 
-export function useEndpoints(publicOrigin: string): Endpoints {
+export function useEndpoints(
+  publicOrigin: string,
+  applicationType: OAuthClientConfig["x_application_type"]
+): Endpoints {
   const endpoints = useMemo(() => {
-    return {
-      openidConfiguration: `${publicOrigin}/.well-known/openid-configuration`,
-      authorize: `${publicOrigin}/oauth2/authorize`,
-      token: `${publicOrigin}/oauth2/token`,
-      userinfo: `${publicOrigin}/oauth2/userinfo`,
-      endSession: `${publicOrigin}/oauth2/end_session`,
-      jwksUri: `${publicOrigin}/oauth2/jwks`,
-    };
-  }, [publicOrigin]);
+    switch (applicationType) {
+      case "m2m":
+        return {
+          openidConfiguration: `${publicOrigin}/.well-known/openid-configuration`,
+          token: `${publicOrigin}/oauth2/token`,
+          jwksUri: `${publicOrigin}/oauth2/jwks`,
+          authorize: null,
+          userinfo: null,
+          endSession: null,
+        };
+      default:
+        return {
+          openidConfiguration: `${publicOrigin}/.well-known/openid-configuration`,
+          authorize: `${publicOrigin}/oauth2/authorize`,
+          token: `${publicOrigin}/oauth2/token`,
+          userinfo: `${publicOrigin}/oauth2/userinfo`,
+          endSession: `${publicOrigin}/oauth2/end_session`,
+          jwksUri: `${publicOrigin}/oauth2/jwks`,
+        };
+    }
+  }, [applicationType, publicOrigin]);
 
   return endpoints;
 }
