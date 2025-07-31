78e5fae08a8e26221cec084fc9ccabf28172a208
Tung Wu <tung@oursky.com>
2025-07-31 20:37:04 +0800

    Add manage scopes screen in edit client

--- portal/src/components/api-resources/ApplicationResourcesList.tsx
+++ portal/src/components/api-resources/ApplicationResourcesList.tsx
@@ -10,6 +10,8 @@ import Toggle from "../../Toggle";
 import { Context } from "@oursky/react-messageformat";
 import PaginationWidget, { PaginationProps } from "../../PaginationWidget";
 import styles from "./ApplicationResourcesList.module.css";
+import { useSystemConfig } from "../../context/SystemConfigContext";
+import ActionButton from "../../ActionButton";
 
 export interface ApplicationResourceListItem {
   id: string;
@@ -28,13 +30,21 @@ interface ApplicationResourcesListProps {
     isAuthorized: boolean
   ) => void;
   disabledToggleClientIDs?: string[];
+  onManageScopes: (item: ApplicationResourceListItem) => void;
 }
 
 export const ApplicationResourcesList: React.FC<ApplicationResourcesListProps> =
   function ApplicationResourcesList(props) {
-    const { className, resources, loading, pagination, onToggleAuthorization } =
-      props;
+    const {
+      className,
+      resources,
+      loading,
+      pagination,
+      onToggleAuthorization,
+      onManageScopes,
+    } = props;
     const { renderToString } = useContext(Context);
+    const { themes } = useSystemConfig();
 
     const renderAuthorizedToggle = useCallback(
       (item: ApplicationResourceListItem) => {
@@ -70,8 +80,42 @@ export const ApplicationResourcesList: React.FC<ApplicationResourcesListProps> =
           isResizable: true,
           onRender: renderAuthorizedToggle,
         },
+        {
+          key: "actions",
+          name: "",
+          minWidth: 100,
+          maxWidth: 100,
+          isResizable: false,
+          // eslint-disable-next-line react/no-unstable-nested-components
+          onRender: (item: ApplicationResourceListItem) => {
+            if (!item.isAuthorized) {
+              return null;
+            }
+            const handleClick = () => {
+              onManageScopes(item);
+            };
+            return (
+              <ActionButton
+                text={renderToString(
+                  "ApplicationResourcesList.columns.manageScopes"
+                )}
+                styles={{
+                  label: { fontWeight: 600 },
+                  root: { height: "auto" },
+                }}
+                theme={themes.actionButton}
+                onClick={handleClick}
+              />
+            );
+          },
+        },
       ],
-      [renderToString, renderAuthorizedToggle]
+      [
+        renderToString,
+        renderAuthorizedToggle,
+        themes.actionButton,
+        onManageScopes,
+      ]
     );
 
     return (
