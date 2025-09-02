cc7df34bc0b644caa0fed8f9d75bbc4c2d666106
Louis Chan <louischan@oursky.com>
2025-09-02 15:48:42 +0800

    Use a dynamic approach to support different host requirement in dialog and overlay

--- authui/src/authflowv2/overlay.ts
+++ authui/src/authflowv2/overlay.ts
@@ -6,12 +6,63 @@ export class OverlayController extends Controller {
   declare readonly defaultOpenValue: boolean;
 
   connect(): void {
+    this.element.addEventListener("transitionstart", this.transitionstart);
+    this.element.addEventListener("transitionend", this.transitionend);
     if (this.defaultOpenValue) {
       this.open();
     }
   }
 
+  disconnect(): void {
+    this.element.removeEventListener("transitionstart", this.transitionstart);
+    this.element.removeEventListener("transitionend", this.transitionend);
+  }
+
+  transitionstart = (e: Event) => {
+    if (e instanceof TransitionEvent) {
+      // This is intentionally empty as we have nothing to do here.
+    }
+  };
+
+  transitionend = (e: Event) => {
+    if (e instanceof TransitionEvent) {
+      const host = this.getHost();
+      if (host != null) {
+        this.revertPrepareHost(host);
+      }
+    }
+  };
+
+  private getHost(): HTMLElement | null {
+    const dialogHost = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue("--overlay-host");
+
+    const host = document.querySelector(dialogHost);
+    if (host == null) {
+      return null;
+    }
+    if (host instanceof HTMLElement) {
+      return host;
+    }
+    return null;
+  }
+
+  private prepareHost(host: HTMLElement): void {
+    host.classList.add("relative");
+  }
+
+  private revertPrepareHost(host: HTMLElement): void {
+    host.classList.remove("relative");
+  }
+
   open() {
+    const host = this.getHost();
+    if (host == null) {
+      return;
+    }
+
+    this.prepareHost(host);
     this.element.classList.add("open");
   }
 
