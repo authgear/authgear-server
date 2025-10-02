43374c47798626e4a275c41d69dff845ed35c94e
Louis Chan <louischan@oursky.com>
2025-10-02 18:32:04 +0800

    Update authgear_deno_hook to v2.0.0

--- portal/src/util/deno.ts
+++ portal/src/util/deno.ts
@@ -1,4 +1,3 @@
-// FIXME(dev-3060): Update the version.
-const authgear_deno_hook_version = "v1.6.1";
+const authgear_deno_hook_version = "v2.0.0";
 
 export const DENO_TYPES_URL = `https://deno.land/x/authgear_deno_hook@${authgear_deno_hook_version}/mod.ts`;
