e763b05f82b22682a5d1f69055d149d174357c75
Tung Wu <tung@oursky.com>
2025-12-16 20:02:35 +0800

    Log an error when someone see direct access blocking

--- pkg/auth/handler/webapp/auth_entry_point_middleware.go
+++ pkg/auth/handler/webapp/auth_entry_point_middleware.go
@@ -1,6 +1,7 @@
 package webapp
 
 import (
+	"log/slog"
 	"net/http"
 
 	"github.com/authgear/authgear-server/pkg/auth/handler/webapp/viewmodels"
@@ -8,8 +9,11 @@ import (
 	"github.com/authgear/authgear-server/pkg/lib/config"
 	"github.com/authgear/authgear-server/pkg/lib/session"
 	"github.com/authgear/authgear-server/pkg/util/httputil"
+	"github.com/authgear/authgear-server/pkg/util/slogutil"
 	"github.com/authgear/authgear-server/pkg/util/template"
 )
+
+var AuthEntryPointMiddlewareLogger = slogutil.NewLogger("auth_entry_point_middleware")
 
 var TemplateDirectAccessDisable = template.RegisterHTML(
 	"web/authflowv2/direct_access_disabled.html",
@@ -28,8 +32,9 @@ type AuthEntryPointMiddleware struct {
 
 func (m *AuthEntryPointMiddleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		userID := session.GetUserID(r.Context())
-		webSession := webapp.GetSession(r.Context())
+		ctx := r.Context()
+		userID := session.GetUserID(ctx)
+		webSession := webapp.GetSession(ctx)
 
 		fromAuthzEndpoint := false
 		if webSession != nil {
@@ -47,6 +52,29 @@ func (m *AuthEntryPointMiddleware) Handle(next http.Handler) http.Handler {
 
 			http.Redirect(w, r, redirectURI, http.StatusFound)
 		} else if userID == nil && !fromAuthzEndpoint && directAccessDisabled {
+			logger := AuthEntryPointMiddlewareLogger.GetLogger(ctx)
+			var webSessionID string
+			var oauthSessionID string
+			var samlSessionID string
+			var cookies []string = []string{}
+			if webSession != nil {
+				webSessionID = webSession.ID
+				oauthSessionID = webSession.OAuthSessionID
+				samlSessionID = webSession.SAMLSessionID
+			}
+			allCookies := r.Cookies()
+			for _, cookie := range allCookies {
+				if cookie.Value != "" {
+					// Log all existing cookies
+					cookies = append(cookies, cookie.Name)
+				}
+			}
+			logger.WithSkipStackTrace().WithSkipLogging().Error(ctx, "auth direct access blocked",
+				slog.String("web_session_id", webSessionID),
+				slog.String("oauth_session_id", oauthSessionID),
+				slog.String("saml_session_id", samlSessionID),
+				slog.Any("cookies", cookies),
+			)
 			m.renderBlocked(w, r)
 		} else {
 			next.ServeHTTP(w, r)
