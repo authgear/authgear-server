65285492d8852d4568ae970d45389ef0d2655c0f
Tung Wu <tung@oursky.com>
2025-12-19 14:57:41 +0800

    Implement IP blocklist screen ui

--- portal/src/graphql/portal/IPBlocklistScreen.tsx
+++ portal/src/graphql/portal/IPBlocklistScreen.tsx
@@ -1,14 +1,68 @@
-import React from "react";
+import NavBreadcrumb, { BreadcrumbItem } from "../../NavBreadcrumb";
 import { FormattedMessage } from "@oursky/react-messageformat";
-import ScreenTitle from "../../ScreenTitle";
+import React, { useMemo } from "react";
+import ScreenContent from "../../ScreenContent";
+import styles from "./IPBlocklistScreen.module.css";
+import ScreenDescription from "../../ScreenDescription";
+import FormContainer from "../../FormContainer";
+import { useAppConfigForm } from "../../hook/useAppConfigForm";
+import { useParams } from "react-router-dom";
+import { PortalAPIAppConfig } from "../../types";
+import { IPBlocklistForm } from "../../components/ipblocklist/IPBlocklistForm";
+
+interface FormState {}
+
+function constructFormState(_config: PortalAPIAppConfig): FormState {
+  return {};
+}
+
+function constructConfig(
+  config: PortalAPIAppConfig,
+  _initialState: FormState,
+  _currentState: FormState,
+  _effectiveConfig: PortalAPIAppConfig
+): PortalAPIAppConfig {
+  return {
+    ...config,
+  };
+}
 
 const IPBlocklistScreen: React.FC = function IPBlocklistScreen() {
+  const navBreadcrumbItems: BreadcrumbItem[] = useMemo(() => {
+    return [
+      {
+        to: "~/attack-protection",
+        label: <FormattedMessage id="AttackProtectionScreen.title" />,
+      },
+      { to: ".", label: <FormattedMessage id="IPBlocklistScreen.title" /> },
+    ];
+  }, []);
+
+  const { appID } = useParams() as { appID: string };
+
+  const appConfigForm = useAppConfigForm({
+    appID,
+    constructFormState,
+    constructConfig,
+  });
+
   return (
-    <div>
-      <ScreenTitle>
-        <FormattedMessage id="IPBlocklistScreen.title" />
-      </ScreenTitle>
-    </div>
+    <FormContainer
+      form={appConfigForm}
+      canSave={true}
+      stickyFooterComponent={true}
+      showDiscardButton={true}
+    >
+      <ScreenContent>
+        <NavBreadcrumb className={styles.widget} items={navBreadcrumbItems} />
+        <ScreenDescription className={styles.widget}>
+          <FormattedMessage id="IPBlocklistScreen.description" />
+        </ScreenDescription>
+      </ScreenContent>
+      <div className={styles.widget}>
+        <IPBlocklistForm />
+      </div>
+    </FormContainer>
   );
 };
 
