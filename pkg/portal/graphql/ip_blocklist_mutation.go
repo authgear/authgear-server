fe2caca95306ba89ad47c3fe45beb1bb7af7724f
Tung Wu <tung@oursky.com>
2025-12-19 15:42:43 +0800

    Setup checkIP mutation

---
+++ pkg/portal/graphql/ip_blocklist_mutation.go
@@ -0,0 +1,82 @@
+package graphql
+
+import (
+	"github.com/graphql-go/graphql"
+
+	relay "github.com/authgear/authgear-server/pkg/graphqlgo/relay"
+
+	"github.com/authgear/authgear-server/pkg/api/apierrors"
+)
+
+var checkIPInput = graphql.NewInputObject(graphql.InputObjectConfig{
+	Name: "CheckIPInput",
+	Fields: graphql.InputObjectConfigFieldMap{
+		"appID": &graphql.InputObjectFieldConfig{
+			Type:        graphql.NewNonNull(graphql.ID),
+			Description: "App ID.",
+		},
+		"ipAddress": &graphql.InputObjectFieldConfig{
+			Type:        graphql.NewNonNull(graphql.String),
+			Description: "The IP address to check.",
+		},
+		"cidrs": &graphql.InputObjectFieldConfig{
+			Type:        graphql.NewNonNull(graphql.NewList(graphql.NewNonNull(graphql.String))),
+			Description: "List of CIDRs to check against.",
+		},
+		"countryCodes": &graphql.InputObjectFieldConfig{
+			Type:        graphql.NewNonNull(graphql.NewList(graphql.NewNonNull(graphql.String))),
+			Description: "List of alpha-2 country codes to check against.",
+		},
+	},
+})
+
+var _ = registerMutationField(
+	"checkIP",
+	&graphql.Field{
+		Description: "Check an IP address against blocklists.",
+		Type:        graphql.Boolean,
+		Args: graphql.FieldConfigArgument{
+			"input": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(checkIPInput),
+			},
+		},
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			input := p.Args["input"].(map[string]interface{})
+			appNodeID := input["appID"].(string)
+			ipAddress := input["ipAddress"].(string)
+			cidrs := input["cidrs"].([]interface{})
+			countryCodes := input["countryCodes"].([]interface{})
+
+			resolvedNodeID := relay.FromGlobalID(appNodeID)
+			if resolvedNodeID == nil || resolvedNodeID.Type != typeApp {
+				return nil, apierrors.NewInvalid("invalid app ID")
+			}
+			appID := resolvedNodeID.ID
+
+			ctx := p.Context
+			gqlCtx := GQLContext(ctx)
+
+			// Access control: collaborator.
+			_, err := gqlCtx.AuthzService.CheckAccessOfViewer(ctx, appID)
+			if err != nil {
+				return nil, err
+			}
+
+			cidrsStr := make([]string, len(cidrs))
+			for i, v := range cidrs {
+				cidrsStr[i] = v.(string)
+			}
+
+			countryCodesStr := make([]string, len(countryCodes))
+			for i, v := range countryCodes {
+				countryCodesStr[i] = v.(string)
+			}
+
+			ok, err := gqlCtx.IPBlocklistService.CheckIP(ctx, ipAddress, cidrsStr, countryCodesStr)
+			if err != nil {
+				return false, err
+			}
+			return ok, nil
+		},
+	},
+)
