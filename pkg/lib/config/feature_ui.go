626d31a87cbcd5f326158fc842ffdf8d587dcd2f
Carmen <carmen@oursky.com>
2026-01-27 10:28:23 +0000

    Add `ui.phone_input.allowlist` to features config

--- pkg/lib/config/feature_ui.go
+++ pkg/lib/config/feature_ui.go
@@ -1,17 +1,21 @@
 package config
 
+import "github.com/authgear/authgear-server/pkg/util/phone"
+
 var _ = FeatureConfigSchema.Add("UIFeatureConfig", `
 {
 	"type": "object",
 	"additionalProperties": false,
 	"properties": {
-		"white_labeling": { "$ref": "#/$defs/WhiteLabelingFeatureConfig" }
+		"white_labeling": { "$ref": "#/$defs/WhiteLabelingFeatureConfig" },
+		"phone_input": { "$ref": "#/$defs/PhoneInputFeatureConfig" }
 	}
 }
 `)
 
 type UIFeatureConfig struct {
 	WhiteLabeling *WhiteLabelingFeatureConfig `json:"white_labeling,omitempty"`
+	PhoneInput    *PhoneInputFeatureConfig    `json:"phone_input,omitempty"`
 }
 
 var _ MergeableFeatureConfig = &UIFeatureConfig{}
@@ -36,3 +40,19 @@ var _ = FeatureConfigSchema.Add("WhiteLabelingFeatureConfig", `
 type WhiteLabelingFeatureConfig struct {
 	Disabled bool `json:"disabled,omitempty"`
 }
+
+var _ = FeatureConfigSchema.Add("PhoneInputFeatureConfig", `
+{
+	"type": "object",
+	"additionalProperties": false,
+	"properties": {
+		"allowlist": { "type": "array", "items": { "$ref": "#/$defs/ISO31661Alpha2" } }
+	}
+}
+`)
+
+type PhoneInputFeatureConfig struct {
+	AllowList []string `json:"allowlist,omitempty"`
+}
+
+var _ = FeatureConfigSchema.Add("ISO31661Alpha2", phone.JSONSchemaString)
