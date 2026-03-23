845aaae0bf0517cc40c65ca24db92cc09250550f
Carmen <carmen@oursky.com>
2026-03-23 10:00:29 +0000

    Fix error response model in API codegen

--- pkg/siteadmin/transport/response.go
+++ pkg/siteadmin/transport/response.go
@@ -5,13 +5,12 @@ import (
 	"net/http"
 
 	"github.com/authgear/authgear-server/pkg/api/apierrors"
+	"github.com/authgear/authgear-server/pkg/siteadmin/model"
 )
 
 func writeError(w http.ResponseWriter, r *http.Request, err error) {
 	apiErr := apierrors.AsAPIErrorWithContext(r.Context(), err)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(apiErr.Code)
-	_ = json.NewEncoder(w).Encode(struct {
-		Error *apierrors.APIError `json:"error"`
-	}{apiErr})
+	_ = json.NewEncoder(w).Encode(model.ErrorEnvelope{Error: *apiErr})
 }
