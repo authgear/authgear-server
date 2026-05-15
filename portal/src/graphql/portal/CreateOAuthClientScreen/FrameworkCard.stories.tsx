aaedd78a4639a618b97a18353c371329d6bd8c14
fungc-io <fung@oursky.com>
2026-05-15 19:41:36 +0100

    Add Storybook stories for wizard subcomponents
    ref DEV-2392

---
+++ portal/src/graphql/portal/CreateOAuthClientScreen/FrameworkCard.stories.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { FrameworkCard } from "./FrameworkCard";
+import { findFramework } from "./frameworks";
+
+const meta = {
+  title: "portal/CreateOAuthClient/FrameworkCard",
+  component: FrameworkCard,
+  args: {
+    framework: findFramework("react")!,
+    selected: false,
+    onSelect: () => {},
+  },
+} satisfies Meta<typeof FrameworkCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Selected: Story = {
+  args: {
+    selected: true,
+  },
+};
+
+export const Django: Story = {
+  args: {
+    framework: findFramework("django")!,
+  },
+};
+
+export const NextJs: Story = {
+  args: {
+    framework: findFramework("nextjs")!,
+    selected: true,
+  },
+};
+
+export const ReactNative: Story = {
+  args: {
+    framework: findFramework("react-native")!,
+  },
+};
